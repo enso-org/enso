cab65d139d158f10c7c5b41db4aaedef945b11b2
Wojciech Danilo <wojciech.danilo@gmail.com>
2016-02-07 17:11:14 +0100

    Added Monad, Functors and some other instances to the ImpossibleM type

--- src/Data/Impossible.hs
+++ src/Data/Impossible.hs
@@ -13,17 +13,21 @@ data Impossible = Impossible deriving (Show)
 type ImpossibleM = ImpossibleM1
 type ImpossibleT = ImpossibleM2
 
-data ImpossibleM1 t1             = ImpossibleM1 deriving (Show)
+data ImpossibleM1 t1             = ImpossibleM1 deriving (Show, Functor, Traversable, Foldable)
 data ImpossibleM2 t1 t2          = ImpossibleM2 deriving (Show)
 data ImpossibleM3 t1 t2 t3       = ImpossibleM3 deriving (Show)
 data ImpossibleM4 t1 t2 t3 t4    = ImpossibleM4 deriving (Show)
 data ImpossibleM5 t1 t2 t3 t4 t5 = ImpossibleM5 deriving (Show)
 
 -- ImpNat is defined as the maximum Int on 64 bit architecture. If GHC has some optimizations of Nat Values, it is nice not to exceed the value.
-type ImpossibleNat = 9223372036854775807 
+type ImpossibleNat = 9223372036854775807
 
+-- === Dummy instances === --
 
--- === Utils ===
+instance Applicative ImpossibleM1
+instance Monad       ImpossibleM1
+
+-- === Utils === --
 
 impossible :: a
 impossible = error "Impossible happened."
