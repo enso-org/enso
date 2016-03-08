6aa86d3290b4b1761b7e0536c82d74c8edadeceb
Marcin Kostrzewa <marcinkostrzewa@yahoo.com>
2016-03-08 15:35:57 +0100

    reexport hell

--- core/src/Luna/Syntax/Model/Network/Builder.hs
+++ core/src/Luna/Syntax/Model/Network/Builder.hs
@@ -3,8 +3,7 @@
 
 module Luna.Syntax.Model.Network.Builder (module Luna.Syntax.Model.Network.Builder, module X) where
 
-import           Data.Graph.Builder.Ref                       as X
-import           Data.Graph.Builder.Class                     as X
+import           Data.Graph.Builder                           as X
 import           Luna.Syntax.Model.Layer                      as X
 import           Luna.Syntax.Model.Network.Builder.Term       as X
 import           Luna.Syntax.Model.Network.Builder.Layer      as X
