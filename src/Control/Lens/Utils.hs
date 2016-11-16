37a676e1ece1a349630f907726b493b777e1a4ae
Wojciech Danilo <wojciech.danilo@gmail.com>
2016-11-16 03:38:56 +0100

    Refactoring, WIP

--- src/Control/Lens/Utils.hs
+++ src/Control/Lens/Utils.hs
@@ -3,3 +3,21 @@ module Control.Lens.Utils (module Control.Lens.Utils, module X) where
 import Control.Lens               as X
 import Control.Lens.Wrapped.Utils as X
 
+
+import Data.Monoid
+import Control.Lens.TH     (LensRules)
+import Language.Haskell.TH (Name, DecsQ, nameBase, mkName)
+import Data.Char           (toLower)
+import Control.Lens.Internal.FieldTH (_fieldToDef)
+
+
+makePfxLenses :: Name -> DecsQ
+makePfxLenses = makeLensesWith (lensRules {_fieldToDef = typePrefixNamer})
+
+typePrefixNamer :: FieldNamer
+typePrefixNamer tn _ n = case nb of
+    '_':_ -> [TopName . mkName $ ltn <> nb]
+    _     -> []
+    where nb     = nameBase n
+          (s:ss) = nameBase tn
+          ltn    = toLower s : ss
