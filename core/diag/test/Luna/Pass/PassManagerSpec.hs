4b9dce585c6da363583643d6c43491eb5ac7c554
Marcin Kostrzewa <marcinkostrzewa@yahoo.com>
2017-01-20 22:28:40 +0100

    resolve problems with restoring output-only attrs

--- core/diag/test/Luna/Pass/PassManagerSpec.hs
+++ core/diag/test/Luna/Pass/PassManagerSpec.hs
@@ -26,12 +26,24 @@ type instance Inputs  Event TestManagerPass = '[]
 type instance Outputs Event TestManagerPass = '[]
 type instance Preserves     TestManagerPass = '[]
 
+data TestOutputPass
+type instance Abstract TestOutputPass = TestOutputPass
+type instance Inputs  Net   TestOutputPass = '[]
+type instance Outputs Net   TestOutputPass = '[]
+type instance Inputs  Layer TestOutputPass = '[]
+type instance Outputs Layer TestOutputPass = '[]
+type instance Inputs  Attr  TestOutputPass = '[]
+type instance Outputs Attr  TestOutputPass = '[TestAttr]
+type instance Inputs  Event TestOutputPass = '[]
+type instance Outputs Event TestOutputPass = '[]
+type instance Preserves     TestOutputPass = '[]
+
 readWriteAttr :: (MonadRef m, MonadIO m, MonadPassManager m) => Int -> SubPass TestManagerPass m Int
 readWriteAttr i = do
     writeAttr @TestAttr $ TestAttr i
     unwrap <$> readAttr @TestAttr
 
-writeTestAttr :: (MonadRef m, MonadIO m, MonadPassManager m) => Int -> SubPass TestManagerPass m ()
+writeTestAttr :: (MonadRef m, MonadIO m, Writers Attr '[TestAttr] m) => Int -> m ()
 writeTestAttr = writeAttr @TestAttr . TestAttr
 
 readTestAttr :: (MonadRef m, MonadIO m, MonadPassManager m) => SubPass TestManagerPass m Int
@@ -44,7 +56,7 @@ testAttrVisibilityBetweenPasses :: Int -> IO Int
 testAttrVisibilityBetweenPasses i = fmap fromRight $ tryAll $ dropLogs $ runRefCache $ evalIRBuilder' $ evalPassManager' $ do
     runRegs
     setAttr (getTypeDesc @TestAttr) $ TestAttr -11
-    Pass.eval' $ writeTestAttr i
+    Pass.eval' @TestManagerPass $ writeTestAttr i
     res <- Pass.eval' readTestAttr
     return res
 
@@ -54,9 +66,19 @@ testAttrVisibilityInsidePass i = fmap fromRight $ tryAll $ dropLogs $ runRefCach
     setAttr (getTypeDesc @TestAttr) $ TestAttr -11
     Pass.eval' $ readWriteAttr i
 
+testAttrVisibilityWhenAttrIsNotInput :: Int -> IO Int
+testAttrVisibilityWhenAttrIsNotInput i = fmap fromRight $ tryAll $ dropLogs $ runRefCache $ evalIRBuilder' $ evalPassManager' $ do
+    runRegs
+    setAttr (getTypeDesc @TestAttr) $ TestAttr -11
+    Pass.eval' @TestOutputPass $ writeTestAttr i
+    res <- Pass.eval' readTestAttr
+    return res
+
 spec = do
     describe "Attr passing" $ do
         it "should pass changes in attributes between pass runs" $ do
             testAttrVisibilityBetweenPasses 42 `shouldReturn` 42
         it "should retain changes in attributes during a single pass run" $ do
             testAttrVisibilityInsidePass 42 `shouldReturn` 42
+        it "should work with passes that only output a given Attr" $ do
+            testAttrVisibilityWhenAttrIsNotInput 42 `shouldReturn` 42
