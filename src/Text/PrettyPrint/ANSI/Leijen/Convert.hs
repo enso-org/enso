8ad4a9ce566e316aa85f9ec7fe845cf80fb8c784
Wojciech Danilo <wojciech.danilo@gmail.com>
2017-03-26 23:05:03 +0200

    New convert instances

--- src/Text/PrettyPrint/ANSI/Leijen/Convert.hs
+++ src/Text/PrettyPrint/ANSI/Leijen/Convert.hs
@@ -1,14 +1,20 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Text.PrettyPrint.ANSI.Leijen.Convert where
 
 import Text.PrettyPrint.ANSI.Leijen
 import Data.Convert.Class
 import Data.Default
-
+import Data.Text
+import Data.Convert.Instances.Text
 
 type IsDoc t = Convertible t Doc
 
 instance Default Doc where
-    def = convert "" ; {-# INLINE def #-}
+    def = convert "" 
 
 instance Convertible String Doc where
-    convert s = text s ; {-# INLINE convert #-}
+    convert s = text s 
+
+instance Convertible Text Doc where
+    convert = convertVia @String
