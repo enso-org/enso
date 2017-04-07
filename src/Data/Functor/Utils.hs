8a5a3c6c132b71eed041269c17182100ff98603f
Wojciech Danilo <wojciech.danilo@gmail.com>
2017-04-08 01:24:13 +0200

    New utils

--- src/Data/Functor/Utils.hs
+++ src/Data/Functor/Utils.hs
@@ -4,30 +4,31 @@ module Data.Functor.Utils where
 
 import Prelude hiding ((.))
 import GHC.Exts (Constraint)
+import Data.Traversable (mapM)
 
 -- Nested fmaps
 
-fmap1 = fmap       ; {-# INLINE fmap1 #-}
-fmap2 = fmap.fmap  ; {-# INLINE fmap2 #-}
-fmap3 = fmap.fmap2 ; {-# INLINE fmap3 #-}
-fmap4 = fmap.fmap3 ; {-# INLINE fmap4 #-}
-fmap5 = fmap.fmap4 ; {-# INLINE fmap5 #-}
-fmap6 = fmap.fmap5 ; {-# INLINE fmap6 #-}
-fmap7 = fmap.fmap6 ; {-# INLINE fmap7 #-}
-fmap8 = fmap.fmap7 ; {-# INLINE fmap8 #-}
-fmap9 = fmap.fmap8 ; {-# INLINE fmap9 #-}
+fmap1 = fmap
+fmap2 = fmap.fmap
+fmap3 = fmap.fmap2
+fmap4 = fmap.fmap3
+fmap5 = fmap.fmap4
+fmap6 = fmap.fmap5
+fmap7 = fmap.fmap6
+fmap8 = fmap.fmap7
+fmap9 = fmap.fmap8
 
 -- Dots
 
-dot1 = (.)         ; {-# INLINE dot1 #-}
-dot2 = dot1 . dot1 ; {-# INLINE dot2 #-}
-dot3 = dot1 . dot2 ; {-# INLINE dot3 #-}
-dot4 = dot1 . dot3 ; {-# INLINE dot4 #-}
-dot5 = dot1 . dot4 ; {-# INLINE dot5 #-}
-dot6 = dot1 . dot5 ; {-# INLINE dot6 #-}
-dot7 = dot1 . dot6 ; {-# INLINE dot7 #-}
-dot8 = dot1 . dot7 ; {-# INLINE dot8 #-}
-dot9 = dot1 . dot8 ; {-# INLINE dot9 #-}
+dot1 = (.)
+dot2 = dot1 . dot1
+dot3 = dot1 . dot2
+dot4 = dot1 . dot3
+dot5 = dot1 . dot4
+dot6 = dot1 . dot5
+dot7 = dot1 . dot6
+dot8 = dot1 . dot7
+dot9 = dot1 . dot8
 
 -- Operators
 
@@ -36,11 +37,11 @@ infixr 9 ∘∘
 infixr 9 ∘∘∘
 infixr 9 ∘∘∘∘
 infixr 9 ∘∘∘∘∘
-(∘)      = fmap ; {-# INLINE (∘)     #-}
-(∘∘)     = dot2 ; {-# INLINE (∘∘)    #-}
-(∘∘∘)    = dot3 ; {-# INLINE (∘∘∘)   #-}
-(∘∘∘∘)   = dot4 ; {-# INLINE (∘∘∘∘)  #-}
-(∘∘∘∘∘)  = dot5 ; {-# INLINE (∘∘∘∘∘) #-}
+(∘)      = fmap
+(∘∘)     = dot2
+(∘∘∘)    = dot3
+(∘∘∘∘)   = dot4
+(∘∘∘∘∘)  = dot5
 
 infixr 9 .
 infixr 9 .:
@@ -52,15 +53,15 @@ infixr 9 .:::.
 infixr 9 .::::
 infixr 9 .::::.
 (.) :: Functor f => (a -> b) -> f a -> f b
-(.)      = fmap ; {-# INLINE (.)      #-}
-(.:)     = dot2 ; {-# INLINE (.:)     #-}
-(.:.)    = dot3 ; {-# INLINE (.:.)    #-}
-(.::)    = dot4 ; {-# INLINE (.::)    #-}
-(.::.)   = dot5 ; {-# INLINE (.::.)   #-}
-(.:::)   = dot6 ; {-# INLINE (.:::)   #-}
-(.:::.)  = dot7 ; {-# INLINE (.:::.)  #-}
-(.::::)  = dot8 ; {-# INLINE (.::::)  #-}
-(.::::.) = dot9 ; {-# INLINE (.::::.) #-}
+(.)      = fmap
+(.:)     = dot2
+(.:.)    = dot3
+(.::)    = dot4
+(.::.)   = dot5
+(.:::)   = dot6
+(.:::.)  = dot7
+(.::::)  = dot8
+(.::::.) = dot9
 
 
 
@@ -69,23 +70,23 @@ infixl 4 <∘∘>
 infixl 4 <∘∘∘>
 infixl 4 <∘∘∘∘>
 infixl 4 <∘∘∘∘∘>
-f <∘>     a = fmap  f ∘     a ; {-# INLINE (<∘>)     #-}
-f <∘∘>    a = fmap  f ∘∘    a ; {-# INLINE (<∘∘>)    #-}
-f <∘∘∘>   a = fmap  f ∘∘∘   a ; {-# INLINE (<∘∘∘>)   #-}
-f <∘∘∘∘>  a = fmap  f ∘∘∘∘  a ; {-# INLINE (<∘∘∘∘>)  #-}
-f <∘∘∘∘∘> a = fmap  f ∘∘∘∘∘ a ; {-# INLINE (<∘∘∘∘∘>) #-}
+f <∘>     a = fmap  f ∘     a
+f <∘∘>    a = fmap  f ∘∘    a
+f <∘∘∘>   a = fmap  f ∘∘∘   a
+f <∘∘∘∘>  a = fmap  f ∘∘∘∘  a
+f <∘∘∘∘∘> a = fmap  f ∘∘∘∘∘ a
 
 
 
-f <<∘>>  a = fmap2 f ∘  a ; {-# INLINE (<<∘>> ) #-}
-f <<∘∘>> a = fmap2 f ∘∘ a ; {-# INLINE (<<∘∘>>) #-}
+f <<∘>>  a = fmap2 f ∘  a
+f <<∘∘>> a = fmap2 f ∘∘ a
 
 infixl 4 <<$>>
-f <<$>> a = fmap f <$> a ; {-# INLINE (<<$>>) #-}
+f <<$>> a = fmap f <$> a
 
 infixl 4 <<*>>
 (<<*>>) :: (Applicative f, Applicative g) => f (g (a -> b)) -> f (g a) -> f (g b)
-(<<*>>) = (<*>) . fmap (<*>) ; {-# INLINE (<<*>>) #-}
+(<<*>>) = (<*>) . fmap (<*>)
 
 -- nested lenses
 -- | following functions are usefull when operating on nested structures with lenses, for example
@@ -97,9 +98,15 @@ instance (Functor m, Functor n) => Functor (NestedFunctor m n) where fmap f = Ne
 
 --nested :: (Functor m, Functor n) => Lens a b c d -> (c -> m (n d)) -> (a -> m (n b))
 nested l f = fromNestedFunctor . l (fmap NestedFunctor f)
-{-# INLINE nested #-}
 
 
 type family Functors lst :: Constraint where
     Functors '[]       = ()
     Functors (f ': fs) = (Functor f, Functors fs)
+
+
+-- === Monadic interface === --
+
+infixl 4 <$$>
+(<$$>) :: (Traversable t, Monad m) => (a -> m b) -> t a -> m (t b)
+(<$$>) = mapM
