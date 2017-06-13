fdf24cf7b5e2f0e6dbbe9b1ed930031e85ec2e8a
Wojciech Danilo <wojciech.danilo@gmail.com>
2017-06-13 20:15:25 +0200

    Adding BlockBuilder

--- src/Data/Layout.hs
+++ src/Data/Layout.hs
@@ -6,13 +6,13 @@
 module Data.Layout where
 
 import qualified Prologue as P
-import Prologue hiding ((:>), Empty, Bounded, div, simple, concat, putStr, swapped, length, putStrLn, take, drop)
+import Prologue hiding ((:>), Empty, Bounded, div, simple, concat, putStr, swapped, length, putStrLn, take, drop, nested)
 
 import qualified Data.Text                 as Text
 import qualified Data.Text.IO              as Text
 import qualified Data.Text.Lazy            as LazyText
 import qualified Data.Text.Lazy.Builder    as Text
-import           Data.Text.Terminal
+import           Data.Text.Terminal        hiding (plain) -- FIXME[WD]: TerminalText instances might not suit this module well
 
 
 instance Default a => Default (NonEmpty a) where def    = def    :| mempty
@@ -127,12 +127,32 @@ render (Block s t) = case t of
     Concat d l r -> concat d (render l) (render r)
 
 -- | Renders anything that is similar to Text by rendering line by line. It is useful when implementic horizontal and vertical block concatenation.
-textRender :: forall a. (ElemRenderer a (Bounded [a]), Concatenable (Bounded [a]), IsString a, Monoid a) => Block a -> a
-textRender block = mconcat (intersperse "\n" lineList) where
-    Bounded _ lineList = render block :: Bounded [a]
+renderText :: (ElemRenderer a (Bounded [a]), Concatenable (Bounded [a]), IsString a, Monoid a) => Block a -> a
+renderText block = mconcat (intersperse "\n" $ renderLines block)
+
+-- | Renders anything that is similar to Text by rendering line by line. It is useful when implementic horizontal and vertical block concatenation.
+renderLines :: (ElemRenderer a (Bounded [a]), Concatenable (Bounded [a])) => Block a -> [a]
+renderLines = unbound . render
 
 
--- === General utils === --
+-- === Block modification === --
+
+type FromBlock t = Convertible1' Block t
+fromBlock :: FromBlock t => forall a. Block a -> t a
+fromBlock = convert1'
+
+append :: Block a -> Block a -> Block a
+append a block@(Block s b) = case b of
+    Concat d l r -> Block s $ Concat d l (append a r)
+    _            -> block <> a
+
+prepend :: Block a -> Block a -> Block a
+prepend a block@(Block s b) = case b of
+    Concat d l r -> Block s $ Concat d (prepend a l) r
+    _            -> a <> block
+
+
+-- === Concatenation utils === --
 
 betweenWith :: (a -> a -> a) -> a -> a -> a -> a
 betweenWith f l r m = l `f` m `f` r
@@ -147,41 +167,34 @@ between' :: Semigroup a => a -> a -> a
 between' a = between a a
 
 
--- === General combinators === --
+-- === Combinators === --
 
-plain2 :: Measurable a => a -> Block a
-plain2 a = Block (measure a) (Plain a)
+phantom :: FromBlock t => t a
+phantom = fromBlock $ Block mempty Phantom
 
-append :: Block a -> Block a -> Block a
-append a block@(Block s b) = case b of
-    Concat d l r -> Block s $ Concat d l (append a r)
-    _            -> block <> a
+plain :: (Measurable a, FromBlock t) => a -> t a
+plain a = fromBlock $ Block (measure a) (Plain a)
 
-prepend :: Block a -> Block a -> Block a
-prepend a block@(Block s b) = case b of
-    Concat d l r -> Block s $ Concat d (prepend a l) r
-    _            -> a <> block
-
-text :: Text -> Block Text
-text = plain2
+text :: (FromBlock t, FromText s, Measurable s) => Text -> t s
+text = plain . fromText
 
 hcat, vcat :: Concatenable a => a -> a -> a
 hcat = concat Horizontal
 vcat = concat Vertical
 
-hspacing, vspacing :: Delta -> Block a
-hspacing d = Block (Bounds d 1) Phantom
-vspacing d = Block (Bounds 1 d) Phantom
+hspacing, vspacing :: FromBlock t => Delta -> t a
+hspacing d = fromBlock $ Block (Bounds d 1) Phantom
+vspacing d = fromBlock $ Block (Bounds 1 d) Phantom
 
-indented :: Block a -> Block a
+indented :: (FromBlock t, Semigroup (t a)) => t a -> t a
 indented a = hspacing 4 <> a
 
-infixr 5 </>
+infixr 6 </>
 (</>) :: Concatenable a => a -> a -> a
 (</>) = vcat
 
-infixr 5 <+>
-(<+>) :: Block a -> Block a -> Block a
+infixr 6 <+>
+(<+>) :: (FromBlock t, Semigroup (t a)) => t a -> t a -> t a
 (<+>) = mappendWith $ hspacing 1
 
 
@@ -215,10 +228,63 @@ instance Concatenable (Block a) where
     concat d a b = Block bounds $ Concat d a b where
         bounds = concat d (measure a) (measure b)
 
-instance (Measurable a, IsString a)          => IsString           (Block a) where fromString = plain2 . fromString
-instance (Measurable a, Convertible' Text a) => Convertible Text   (Block a) where convert    = plain2 . convert'
+instance (Measurable a, IsString a)          => IsString           (Block a) where fromString = plain . fromString
+instance (Measurable a, Convertible' Text a) => Convertible Text   (Block a) where convert    = plain . convert'
 instance (Measurable a, Convertible' Text a) => Convertible String (Block a) where convert    = convertVia @Text
 instance (Measurable a, Convertible' Text a) => Convertible Char   (Block a) where convert    = convertVia @String
+
+
+
+--------------------------
+-- === BlockBuilder === --
+--------------------------
+
+-- === Definition === --
+
+data BlockBuilder a = PhantomBuilder
+                    | PlainBuilder  !(Block a)
+                    | ConcatBuilder !Dir !(BlockBuilder a) !(BlockBuilder a)
+                    | SubBuilder    !(BlockBuilder a)
+                    deriving (Show)
+
+
+-- === Constructors === --
+
+nested :: BlockBuilder a -> BlockBuilder a
+nested = SubBuilder
+
+
+-- === Builders === --
+
+runLineBuilder :: BlockBuilder a -> Block a
+runLineBuilder bb = foldl (</>) mempty $ (mconcat <$> lines) where
+    lines  = reverse $ reverse <$> rlines
+    rlines = uncurry (flip (:)) $ runLineBuilder' mempty mempty bb
+
+runLineBuilder' :: [[Block a]] -> [Block a] -> BlockBuilder a -> ([[Block a]], [Block a])
+runLineBuilder' lines line = \case
+    PhantomBuilder       -> (lines, line)
+    PlainBuilder   a     -> (lines, a:line)
+    SubBuilder     b     -> (lines, runLineBuilder b:line)
+    ConcatBuilder  d l r -> case d of
+        Horizontal -> uncurry runLineBuilder' (runLineBuilder' lines line l) r
+        Vertical   -> runLineBuilder' (uncurry (flip (:)) (runLineBuilder' lines line l)) mempty r
+
+
+-- === Instances === --
+
+instance Mempty    (BlockBuilder a) where mempty = PhantomBuilder
+instance Semigroup (BlockBuilder a) where
+    PhantomBuilder <> a = a
+    a <> PhantomBuilder = a
+    a <> b = concat Horizontal a b
+
+instance Concatenable (BlockBuilder a) where
+    concat d a b = ConcatBuilder d a b
+
+
+instance Convertible1 Block BlockBuilder where convert1 = PlainBuilder
+instance (Measurable a, IsString a) => IsString (BlockBuilder a) where fromString = plain . fromString
 
 
 
@@ -281,22 +347,22 @@ instance Concatenable (Bounded [TermText]) where
 
 
 -- === Examples
---
--- main :: IO ()
--- main = do
---     let b1  = "foo" :: Block TermText
---         b2  = "foo" </> vspacing 1 </> "a" :: Block TermText
---         b'  = b1 <+> b2
---         out = textRender (b' <> styled [fg $ dark green, blinking] b' <> b')
---     putStrLn out
---     main2
---     print "end"
---
---
--- main2 :: IO ()
--- main2 = do
---     let t1 = "hello" :: TermText
---         tx = withStyle [fg $ dark green] $ t1 <> styled [fg $ dark red, italic, blinking] t1 <> t1
---     putStrLn tx
---     -- print tx
---     putStrLn ""
+
+main :: IO ()
+main = do
+    let b1  = "foo" :: Block TermText
+        b2  = "foo" </> vspacing 1 </> "a" :: Block TermText
+        b'  = b1 <+> b2
+        out = renderText (b' <> styled [fg $ dark green, blinking] b' <> b')
+    putStrLn out
+    main2
+    print "end"
+
+
+main2 :: IO ()
+main2 = do
+    let t1 = "hello" :: BlockBuilder TermText
+        tx = t1 <> (t1 </> t1)
+    putStrLn $ renderText (runLineBuilder $ nested tx <> tx)
+    -- print tx
+    putStrLn ""
