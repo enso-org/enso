4d4914483e7f207c94fcadf8f7f2736367165451
Wojciech Danilo <wojciech.danilo@gmail.com>
2017-06-20 01:16:03 +0200

    Minor changes

--- src/Data/Maps.hs
+++ src/Data/Maps.hs
@@ -1,9 +1,3 @@
----------------------------------------------------------------------------
--- Copyright (C) Flowbox, Inc - All Rights Reserved
--- Unauthorized copying of this file, via any medium is strictly prohibited
--- Proprietary and confidential
--- Flowbox Team <contact@flowbox.io>, 2013
----------------------------------------------------------------------------
 
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
