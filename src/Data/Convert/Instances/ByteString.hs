bb3f1b5ebce451a8d8a2f8536c9fa51e3f4bcc47
Wojciech Danilo <wojciech.danilo@gmail.com>
2017-07-06 04:14:31 +0200

    Adding new instances

--- src/Data/Convert/Instances/ByteString.hs
+++ src/Data/Convert/Instances/ByteString.hs
@@ -7,3 +7,9 @@ import Data.Convert.Class
 
 instance Convertible Strict.ByteString String            where convert = UTF8.toString
 instance Convertible String            Strict.ByteString where convert = UTF8.fromString
+
+instance Convertible Lazy.ByteString String          where convert = convertVia @Strict.ByteString
+instance Convertible String          Lazy.ByteString where convert = convertVia @Strict.ByteString
+
+instance Convertible Lazy.ByteString   Strict.ByteString where convert = Lazy.toStrict
+instance Convertible Strict.ByteString Lazy.ByteString   where convert = Lazy.fromStrict
