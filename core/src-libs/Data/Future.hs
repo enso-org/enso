dd894ffb991eb930e004f8634d98a5f1b610df64
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2017-08-02 16:47:47 +0200

    Delay works on restricted amount of capabilities

--- core/src-libs/Data/Future.hs
+++ core/src-libs/Data/Future.hs
@@ -3,17 +3,40 @@ module Data.Future where
 import Prologue
 import Control.Concurrent.MVar
 import Control.Concurrent
+import Data.IORef
 import System.IO.Unsafe
+
+minCapabilityNumber :: IORef Int
+minCapabilityNumber = unsafePerformIO $ newIORef 0
+{-# NOINLINE minCapabilityNumber #-}
+
+availableCapabilities :: IORef [Int]
+availableCapabilities = unsafePerformIO $ do
+    caps <- getNumCapabilities
+    let maxCap = caps - 1
+    newIORef $ cycle [0..maxCap]
+{-# NOINLINE availableCapabilities #-}
+
+updateCapabilities :: IO ()
+updateCapabilities = do
+    caps   <- getNumCapabilities
+    minCap <- readIORef minCapabilityNumber
+    let maxCap = caps - 1
+    writeIORef availableCapabilities $ cycle [minCap..maxCap]
 
 mkFuture :: IO (a, a -> IO ())
 mkFuture = do
     v <- newEmptyMVar
     return (unsafePerformIO $ readMVar v, putMVar v)
 
+nextCapability :: IO Int
+nextCapability = atomicModifyIORef availableCapabilities $ \(a:rest) -> (rest, a)
+
 delay :: IO a -> IO a
 delay act = do
     (fut, resolve) <- mkFuture
-    forkIO $ act >>= resolve
+    cap            <- nextCapability
+    forkOn cap $ act >>= resolve
     return fut
 
 
