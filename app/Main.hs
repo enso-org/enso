cceac2b39bce4d2fd3b9e5695a9bbe3f17091bab
piotrMocz <piotr.moczurad@gmail.com>
2018-03-08 13:30:15 +0100

    Fib ;)

--- app/Main.hs
+++ app/Main.hs
@@ -8,21 +8,21 @@ import Luna.IR.Term.Basic       (passTest_run)
 import System.Remote.Monitoring as EKG
 
 sleep :: Int -> IO ()
-sleep = threadDelay . (* 10e6)
+sleep = threadDelay . (* 1e6)
 
 main :: IO ()
 main = do
     EKG.forkServer "localhost" 8888
     putStrLn "Running Luna pass test..."
     passTest_run
-    sleep 1
-    passTest_run
-    sleep 2
-    passTest_run
-    sleep 3
-    passTest_run
-    sleep 4
-    passTest_run
     sleep 5
+    passTest_run
+    sleep 8
+    passTest_run
+    sleep 13
+    passTest_run
+    sleep 21
+    passTest_run
+    sleep 34
     passTest_run
     putStrLn "Done!"
