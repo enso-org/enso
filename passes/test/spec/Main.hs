84691634169368214c99f8b56686b576f0423950
Ara Adkins <iamrecursion@users.noreply.github.com>
2018-04-04 14:30:53 +0100

    Refactor tests to centralise the test main for CI (#199)

--- passes/test/spec/Main.hs
+++ passes/test/spec/Main.hs
@@ -2,28 +2,10 @@ module Main where
 
 import Prelude
 
-import Data.Maybe         (fromJust, isJust)
-import System.Directory   (createDirectoryIfMissing)
-import System.Environment (lookupEnv)
-import System.FilePath    ((</>))
+import qualified Luna.CI as CI
 
-import Test.Hspec.Runner
-import Test.Hspec.Formatters.Jenkins
 import qualified Spec
 
-
 main :: IO ()
-main = do
-    buildInCI <- isJust <$> lookupEnv "CIRCLECI"
+main = CI.main Spec.spec
 
-    config <-
-        if buildInCI then do
-            -- if in CI, CIRCLE_TEST_REPORTS is always defined
-            circleTestPath <- fmap fromJust $ lookupEnv "CIRCLE_TEST_REPORTS"
-            let testPath = circleTestPath </> "hspec"
-            createDirectoryIfMissing True testPath
-            return $ defaultConfig { configFormatter = Just xmlFormatter
-                                   , configOutputFile = Right $ testPath </> "output.xml" }
-        else return defaultConfig
-
-    hspecWith config Spec.spec
