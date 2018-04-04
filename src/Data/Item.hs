e2038b2e25f112b6d730b4c0b0a06eb0d37b7719
Wojciech Danilo <wojciech.danilo@gmail.com>
2018-04-04 05:00:34 +0200

    Adding sequence class

--- src/Data/Item.hs
+++ src/Data/Item.hs
@@ -1,10 +1,17 @@
 module Data.Item where
 
+import Prelude
+
 import qualified Data.Map        as MapLazy
 import qualified Data.Map.Strict as MapStrict
+import qualified Data.Text       as Strict
+import qualified Data.Text.Lazy  as Lazy
+
 
 type family Item a
 
 type instance Item [a] = a
 type instance Item (MapLazy.Map   k v) = (k,v)
 type instance Item (MapStrict.Map k v) = (k,v)
+type instance Item Strict.Text         = Char
+type instance Item Lazy.Text           = Char
