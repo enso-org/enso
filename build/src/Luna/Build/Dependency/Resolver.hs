3349b4e5e6d5eba01a0f2aa946166d7d152e6ff2
Ara Adkins <me@ara.io>
2018-04-12 13:21:39 +0100

    More experimentation.

--- build/src/Luna/Build/Dependency/Resolver.hs
+++ build/src/Luna/Build/Dependency/Resolver.hs
@@ -1,8 +1,4 @@
-{-# LANGUAGE DeriveAnyClass #-}
-
 module Luna.Build.Dependency.Resolver ( solveConstraints ) where
-
-import Data.Generics hiding (Generic)
 
 import Prologue hiding ((.>))
 
@@ -29,76 +25,59 @@ solverConfig :: SMTConfig
 solverConfig = defaultSMTCfg -- current default is Z3, so leave it as is
 
 data RVersion = RVersion
-    { __major             :: SInteger
-    , __minor             :: SInteger
-    , __patch             :: SInteger
-    , __prerelease        :: SInteger
-    , __prereleaseVersion :: SInteger
-    } deriving (Eq, Generic, Show, Typeable)
-makeLenses ''RVersion
+    { __major :: Integer
+    , __minor :: Integer
+    , __patch :: Integer
+    , __prerelease :: Integer
+    , __prereleaseVersion :: Integer
+    } deriving (Eq, Ord, Show, Read, Data)
 
-instance Mergeable RVersion
-instance HasKind RVersion
-instance Data RVersion
-instance Ord RVersion
-instance Read RVersion
 instance SymWord RVersion
+instance HasKind RVersion
 instance SatModel RVersion
 
-instance EqSymbolic RVersion where
-    (RVersion majorL minorL patchL prereleaseL prereleaseVersionL) .==
-        (RVersion majorR minorR patchR prereleaseR prereleaseVersionR) =
-        (majorL             .== majorR) &&&
-        (minorL             .== minorR) &&&
-        (patchL             .== patchR) &&&
-        (prereleaseL        .== prereleaseR) &&&
-        (prereleaseVersionL .== prereleaseVersionR)
+type SVersion = SBV RVersion
 
-instance OrdSymbolic RVersion where
-    (RVersion majorL minorL patchL prereleaseL prereleaseVersionL) .<
-        (RVersion majorR minorR patchR prereleaseR prereleaseVersionR) =
-        ite (majorL             .< majorR) true       $
-        ite (majorL             .> majorR) false      $
-        ite (minorL             .< minorR) true       $
-        ite (minorL             .> minorR) false      $
-        ite (patchL             .< patchR) true       $
-        ite (patchL             .> patchR) false      $
-        ite (prereleaseL        .< prereleaseR) true  $
-        ite (prereleaseL        .> prereleaseR) false $
-        ite (prereleaseVersionL .< prereleaseVersionR) true false
+mkSVersion :: Integer -> Integer -> Integer -> Integer -> Integer -> SVersion
+mkSVersion a b c d e = literal $ RVersion a b c d e
 
-type SRVersion = SBV RVersion
+sVersion :: String -> Symbolic SVersion
+sVersion = free
 
-mkSRVersion :: SInteger -> SInteger -> SInteger -> SInteger -> SInteger
-            -> SRVersion
-mkSRVersion a b c d e = literal $ RVersion a b c d e
+sVersion_ :: Symbolic SVersion
+sVersion_ = free_
 
-symbolicRVersion :: String -> Symbolic RVersion
-symbolicRVersion name = do
-    maj  <- symbolic $ name <> "_major"
-    min  <- symbolic $ name <> "_minor"
-    pat  <- symbolic $ name <> "_patch"
-    pre  <- symbolic $ name <> "_prerelease"
-    preV <- symbolic $ name <> "_preVersion"
-    pure $ RVersion maj min pat pre preV
+data Foo = Foo SInt8 SInt8 deriving Generic
+
+instance EqSymbolic Foo where
+  Foo x1 y1 .== Foo x2 y2 = (x1, y1) .== (x2, y2)
+
+instance Mergeable Foo
+
+instance OrdSymbolic Foo where
+  Foo x1 y1 .< Foo x2 y2 = (x1, y1) .< (x2, y2)
+
+instance Provable p => Provable (Foo -> p) where
+  forAll_ f = forAll_ (\(x,y) -> f (Foo x y))
+  forAll ns f = forAll ns (\(x,y) -> f (Foo x y))
+  forSome_ f = forSome_ (\(x,y) -> f (Foo x y))
+  forSome ns f = forSome ns (\(x,y) -> f (Foo x y))
+
+mkSFoo :: Int8 -> Int8 -> Symbolic Foo
+mkSFoo i j = do
+  i' <- pure (literal i)
+  j' <- pure (literal j)
+  pure $ Foo i' j'
 
 constraintPredicate :: ConstraintMap -> Predicate
 constraintPredicate constraints = do
     let keys = M.keys constraints
         elems = M.elems constraints
 
-    {- traceShowM keys -}
-    {- traceShowM elems -}
+    staticV1 <- mkSFoo 1 2
+    varV1 <- Foo <$> sInt8 "i1" <*> sInt8 "i2"
 
-    foo <- symbolicRVersion "foo"
-    {- bar <- symbolic "bar" -}
-
-    let ver1 = mkSRVersion 0 0 1 3 0
-        ver2 = mkSRVersion 1 1 2 3 0
-
-    {- traceShowM $ isSymbolic ver1 -}
-
-    pure $ ver1 .< ver2
+    pure $ varV1 .< staticV1
 
 -- TODO [Ara] Needs to take list of available versions. (foo == a ||| b ||| c)
 -- TODO [Ara] Turn result into resolved deps
