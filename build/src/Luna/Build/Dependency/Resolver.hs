490ecd78b5bd8f5206380a609d3ecf01143040d8
Ara Adkins <me@ara.io>
2018-04-12 10:28:47 +0100

    Actually do something with the solver result.

--- build/src/Luna/Build/Dependency/Resolver.hs
+++ build/src/Luna/Build/Dependency/Resolver.hs
@@ -24,27 +24,27 @@ import Debug.Trace
 solverConfig :: SMTConfig
 solverConfig = defaultSMTCfg -- current default is Z3, so leave it as is
 
-data SVersion = SVersion
+data RVersion = RVersion
     { __major             :: SInteger
     , __minor             :: SInteger
     , __patch             :: SInteger
     , __prerelease        :: SInteger
     , __prereleaseVersion :: SInteger
     } deriving (Generic, Show, Typeable)
-makeLenses ''SVersion
+makeLenses ''RVersion
 
-instance EqSymbolic SVersion where
-    (SVersion majorL minorL patchL prereleaseL prereleaseVersionL) .==
-        (SVersion majorR minorR patchR prereleaseR prereleaseVersionR) =
+instance EqSymbolic RVersion where
+    (RVersion majorL minorL patchL prereleaseL prereleaseVersionL) .==
+        (RVersion majorR minorR patchR prereleaseR prereleaseVersionR) =
         (majorL             .== majorR) &&&
         (minorL             .== minorR) &&&
         (patchL             .== patchR) &&&
         (prereleaseL        .== prereleaseR) &&&
         (prereleaseVersionL .== prereleaseVersionR)
 
-instance OrdSymbolic SVersion where
-    (SVersion majorL minorL patchL prereleaseL prereleaseVersionL) .<
-        (SVersion majorR minorR patchR prereleaseR prereleaseVersionR) =
+instance OrdSymbolic RVersion where
+    (RVersion majorL minorL patchL prereleaseL prereleaseVersionL) .<
+        (RVersion majorR minorR patchR prereleaseR prereleaseVersionR) =
         ite (majorL             .< majorR) true       $
         ite (majorL             .> majorR) false      $
         ite (minorL             .< minorR) true       $
@@ -55,31 +55,60 @@ instance OrdSymbolic SVersion where
         ite (prereleaseL        .> prereleaseR) false $
         ite (prereleaseVersionL .< prereleaseVersionR) true false
 
-instance Mergeable SVersion
+instance Mergeable RVersion
+instance HasKind RVersion
+instance Eq RVersion
+instance Data RVersion
+instance Ord RVersion
+instance Read RVersion
+instance SymWord RVersion
+
+type SRVersion = SBV RVersion
+
+mkSRVersion :: SInteger -> SInteger -> SInteger -> SInteger -> SInteger
+            -> SRVersion
+mkSRVersion a b c d e = literal $ RVersion a b c d e
+
+symbolicRVersion :: String -> Symbolic SRVersion
+symbolicRVersion name = do
+    maj  <- symbolic $ name <> "_major"
+    min  <- symbolic $ name <> "_minor"
+    pat  <- symbolic $ name <> "_patch"
+    pre  <- symbolic $ name <> "_prerelease"
+    preV <- symbolic $ name <> "_preVersion"
+    pure $ mkSRVersion maj min pat pre preV
 
 constraintPredicate :: ConstraintMap -> Predicate
 constraintPredicate constraints = do
     let keys = M.keys constraints
         elems = M.elems constraints
 
-    traceShowM keys
-    traceShowM elems
+    {- traceShowM keys -}
+    {- traceShowM elems -}
 
     {- foo <- symbolic "foo" -}
     {- bar <- symbolic "bar" -}
 
-    let ver1 = SVersion 0 0 1 3 0
-        ver2 = SVersion 1 1 2 3 0
+    let ver1 = RVersion 0 0 1 3 0
+        ver2 = RVersion 1 1 2 3 0
 
     {- traceShowM $ isSymbolic ver1 -}
 
-    pure $ ver1 .< ver2
+    pure $ ver1 .> ver2
 
--- TODO [Ara] Make this actually do something with the result
+-- TODO [Ara] Turn result into resolved deps
 solveConstraints :: (MonadIO m) => ConstraintMap -> m (Maybe Int)
 solveConstraints constraints = do
-    liftIO $ runSolver constraints
-    pure $ Just 1
+    r@(SatResult modelResult) <- liftIO $ runSolver constraints
+    traceShowM r
+    case modelResult of
+        Unsatisfiable _ -> pure $ Just 0
+        Satisfiable _ model -> do
+            traceShowM model
+            pure $ Just 1
+        SatExtField _ model -> pure $ Just 1
+        Unknown _ reason -> traceShowM reason >> pure $ Just 0
+        ProofError _ xs -> traceShowM xs >> pure $ Just 0
 
 runSolver :: ConstraintMap -> IO SatResult
 runSolver constraints = sat $ constraintPredicate constraints
