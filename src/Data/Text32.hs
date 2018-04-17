96f87f2034c94f14bb9819f4a7a1d00a858c39f1
Wojciech Danilo <wojciech.danilo@gmail.com>
2018-04-17 21:22:03 +0200

    Exposing new utils

--- src/Data/Text32.hs
+++ src/Data/Text32.hs
@@ -2,7 +2,7 @@ module Data.Text32 (module Data.Text32, module X) where
 
 import Prelude
 import Data.Container.Vector as X
-import Data.Vector.Unboxed   as X (Vector, all)
+import Data.Vector.Unboxed   as X (Vector, all, toList)
 
 type Text32 = Vector Char
 
