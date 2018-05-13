d84723d2bd3b45b52f5f69c37a5df3020dbf2b20
Wojciech Danilo <wojciech.danilo@gmail.com>
2018-05-13 20:18:26 +0200

    Integrating MultiState with graph state

--- core/src/Data/Graph/Class.hs
+++ core/src/Data/Graph/Class.hs
@@ -1,21 +1,25 @@
+{-# LANGUAGE UndecidableInstances #-}
+
 module Data.Graph.Class where
 
 import Prologue
 
+import qualified Control.Monad.State.Layered as State
+import qualified Data.TypeMap.Strict         as TypeMap
+import qualified OCI.Pass.State.Runtime      as MultiState
 
+import OCI.Pass.State.Runtime (MultiStateT)
 
--------------------
--- === Graph === --
--------------------
-
--- === Definition === --
-
-newtype Graph tag m a = Graph (IdentityT m a)
-    deriving (Applicative, Functor, Monad, MonadIO, MonadTrans)
-makeLenses ''Graph
 
 type family Components      tag      :: [Type]
 type family ComponentLayers tag comp :: [Type]
+
+type StateData tag = '[]
+type State tag = TypeMap.TypeMap (StateData tag)
+
+
+
+
 
 
 type family DiscoverGraphTag (m :: Type -> Type) :: Type -- where
@@ -27,9 +31,39 @@ type DiscoverComponentLayers m comp = ComponentLayers (DiscoverGraphTag m) comp
 
 
 
+-------------------
+-- === Graph === --
+-------------------
+
+-- === Definition === --
+
+type    Graph  tag     = GraphT tag IO
+newtype GraphT tag m a = GraphT (MultiStateT (StateData tag) m a)
+    deriving ( Applicative, Alternative, Functor, Monad, MonadFail, MonadFix
+             , MonadIO, MonadPlus, MonadThrow, MonadTrans)
+makeLenses ''GraphT
+
+
 -- === API === --
 
-run :: ∀ t m a. Graph t m a -> m a
-run = runIdentityT . unwrap ; {-# INLINE run #-}
+run :: ∀ t m a. Functor m => GraphT t m a -> m a
+run g = MultiState.evalT (unwrap g) TypeMap.empty ; {-# INLINE run #-}
 
+-- run  :: ∀ tag m a. Monad m => GraphT tag m a -> State tag -> m (a, State tag)
+-- exec :: ∀ tag m a. Monad m => GraphT tag m a -> State tag -> m (State tag)
+-- eval :: ∀ tag m a. Monad m => GraphT tag m a -> State tag -> m a
+-- run  = MultiState.runT  . unwrap ; {-# INLINE run  #-}
+-- exec = MultiState.execT . unwrap ; {-# INLINE exec #-}
+-- eval = MultiState.evalT . unwrap ; {-# INLINE eval #-}
+
+
+-- === State === --
+
+instance (Monad m, State.Getter a (MultiStateT (StateData tag) m))
+      => State.Getter a (GraphT tag m) where
+     get = wrap $ State.get @a ; {-# INLINE get #-}
+
+instance (Monad m, State.Setter a (MultiStateT (StateData tag) m))
+      => State.Setter a (GraphT tag m) where
+     put = wrap . State.put @a ; {-# INLINE put #-}
 
