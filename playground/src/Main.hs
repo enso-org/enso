0a193937ba6b09f4163f9182a72861609db2eb75
Wojciech Danilo <wojciech.danilo@gmail.com>
2018-06-09 00:59:24 +0200

    Fixing playground

--- playground/src/Main.hs
+++ playground/src/Main.hs
@@ -1,39 +1,36 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings    #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module Main where
 
 import Prologue
-import qualified Luna.Shell as Shell
-import qualified Data.List as List
-import           Data.Map (Map)
-import qualified Data.Map  as Map
-import qualified Data.Text as Text
-import qualified System.Environment as System
-import qualified Text.PrettyPrint.ANSI.Leijen as Doc
 
-import qualified Control.Monad.State as State
-
-import qualified Luna.Pass as Pass
-import qualified Luna.Pass.Attr as Attr
-import qualified Luna.Pass.Scheduler as Scheduler
-import qualified Luna.Runner as Runner
-import qualified Luna.Syntax.Text.Parser.Pass as Parser
+import qualified Control.Monad.State                 as State
+import qualified Data.Graph.Data.Graph.Class         as Graph
+import qualified Data.List                           as List
+import qualified Data.Map                            as Map
+import qualified Data.Set                            as Set
+import qualified Data.Tag                            as Tag
+import qualified Data.Text                           as Text
+import qualified Luna.Debug.IR.Visualizer            as Vis
+import qualified Luna.IR                             as IR
+import qualified Luna.IR.Layer                       as Layer
+import qualified Luna.Pass                           as Pass
+import qualified Luna.Pass.Attr                      as Attr
+import qualified Luna.Pass.Basic                     as Pass
+import qualified Luna.Pass.Scheduler                 as Scheduler
+import qualified Luna.Shell                          as Shell
 import qualified Luna.Syntax.Text.Parser.Data.Result as Parser
-{-import qualified Luna.Syntax.Text.Parser.IR.Class as Parser-}
-import qualified Luna.Syntax.Text.Source as Parser
-import qualified Luna.IR                        as IR
-import qualified Luna.IR.Layer as Layer
-import qualified OCI.IR.Layout as IR
+import qualified Luna.Syntax.Text.Parser.Pass        as Parser
+import qualified Luna.Syntax.Text.Source             as Parser
+import qualified System.Environment                  as System
+import qualified Text.PrettyPrint.ANSI.Leijen        as Doc
 
-import qualified Data.PtrList.Mutable as PtrList
 
-import qualified Data.Tag as Tag
+import Data.Map (Map)
 import Data.Set (Set)
-import qualified Data.Set as Set
 
 
-import qualified Luna.Debug.IR.Visualizer as Vis
 -------------------
 -- === Shell === --
 -------------------
@@ -43,7 +40,7 @@ data XD = XD
 
 type instance Pass.Spec XD t = XDSpec t
 type family XDSpec t where
-    XDSpec (Pass.In Pass.Attrs) = '[World]
+    XDSpec (Pass.In Pass.Attrs) = Pass.List '[World]
     XDSpec t = Pass.BasicPassSpec t
 
 newtype World = World (IR.Term IR.Unit)
@@ -57,26 +54,19 @@ instance Pass.Definition XD where
         print root
         Vis.displayVisualization "foo" root
         u@(IR.Unit imphub units cls) <- IR.model root
-        length <$> IR.inputs u >>= print
-        IR.source imphub >>= Layer.read @IR.Model >>= \h -> do
-              length <$> IR.inputs h >>= print
-              print "to import hub!"
+        print =<< IR.inputs root
+        x <- Layer.read @IR.Source imphub
+        print =<< IR.inputs x
         bar <- IR.var "bar"
         foo <- IR.acc bar "foo"
-        length <$> (IR.inputs =<< Layer.read @IR.Model bar) >>= print
-        length <$> (IR.inputs =<< Layer.read @IR.Model foo) >>= print
+        print =<< IR.inputs bar
+        print =<< IR.inputs foo
         return ()
 
 main :: IO ()
-main = Scheduler.runManual reg sched where
-    reg = do
-        Runner.registerAll
-        Parser.registerStatic
-    sched = do
+main = Graph.encodeAndEval @Pass.Compilation $ Scheduler.evalT $ do
         let lunafilePath = "/Users/marcinkostrzewa/code/luna/stdlib/Std/src/System.luna"
         lunafile <- readFile lunafilePath
-
-        Parser.registerDynamic
         Scheduler.registerAttr @World
         Scheduler.enableAttrByType @World
         Scheduler.registerPass @XD
@@ -87,28 +77,26 @@ main = Scheduler.runManual reg sched where
         print "parsed"
         Scheduler.runPassByType @XD
 
-Pass.cache_phase1 ''XD
-Pass.cache_phase2 ''XD
 
-stdlibPath :: IO FilePath
-stdlibPath = do
-    env     <- Map.fromList <$> Env.getEnvironment
-    exePath <- fst <$> splitExecutablePath
-    let (<</>>)        = (FilePath.</>)  -- purely for convenience,
-                                         -- because </> is defined elswhere
-        parent         = let p = FilePath.takeDirectory
-                         in \x -> if FilePath.hasTrailingPathSeparator x
-                                  then p (p x)
-                                  else p x
-        defaultStdPath = (parent . parent . parent $ exePath)
-                    <</>> "config"
-                    <</>> "env"
-        envStdPath     = Map.lookup Project.lunaRootEnv env
-        stdPath        = fromMaybe defaultStdPath envStdPath <</>> "Std"
-    exists <- doesDirectoryExist stdPath
-    if exists
-        then putStrLn $ "Found the standard library at: " <> stdPath
-        else die $ "Standard library not found. Set the "
-                <> Project.lunaRootEnv
-                <> " environment variable"
-    return stdPath
+-- stdlibPath :: IO FilePath
+-- stdlibPath = do
+--     env     <- Map.fromList <$> System.getEnvironment
+--     exePath <- fst <$> splitExecutablePath
+--     let (<</>>)        = (FilePath.</>)  -- purely for convenience,
+--                                          -- because </> is defined elswhere
+--         parent         = let p = FilePath.takeDirectory
+--                          in \x -> if FilePath.hasTrailingPathSeparator x
+--                                   then p (p x)
+--                                   else p x
+--         defaultStdPath = (parent . parent . parent $ exePath)
+--                     <</>> "config"
+--                     <</>> "env"
+--         envStdPath     = Map.lookup Project.lunaRootEnv env
+--         stdPath        = fromMaybe defaultStdPath envStdPath <</>> "Std"
+--     exists <- doesDirectoryExist stdPath
+--     if exists
+--         then putStrLn $ "Found the standard library at: " <> stdPath
+--         else die $ "Standard library not found. Set the "
+--                 <> Project.lunaRootEnv
+--                 <> " environment variable"
+--     return stdPath
