05ab8fe679dc4237ec74d6bd9263aca2a2a562a2
Ara Adkins <me@ara.io>
2019-01-03 14:04:09 +0000

    Add some utilities to TypeMap.

--- lib/data-typemap/src/Data/TypeMap/Strict.hs
+++ lib/data-typemap/src/Data/TypeMap/Strict.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE NoStrict #-}
-{-# LANGUAGE NoStrictData #-}
+{-# LANGUAGE NoStrict             #-}
+{-# LANGUAGE NoStrictData         #-}
+{-# LANGUAGE PolyKinds            #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module Data.TypeMap.Strict where
@@ -20,18 +21,32 @@ newtype TypeMap (ts :: [Type]) = TypeMap (TypeMapData ts)
 type TypeMapData ts = Tuple.FromList ts
 makeLenses ''TypeMap
 
-
--- === API === --
-
 type ElemGetter el ts = Tuple.ElemGetter         el (TypeMapData ts)
 type ElemSetter el ts = Tuple.ElemSetterKeepType el (TypeMapData ts)
 type ElemEditor el ts = (ElemGetter el ts, ElemSetter el ts)
 
+type Prependable t ts = ( Tuple.Prepended t (Tuple.FromList ts)
+                        ~ Tuple.FromList (t : ts)
+                        , Tuple.Prependable t (Tuple.FromList ts)
+                        )
+
+type SplitHead t ts =
+    ( Tuple.Head (Tuple.FromList (t ': ts)) ~ t
+    , Tuple.Tail (Tuple.FromList (t ': ts)) ~ Tuple.FromList ts
+    , Tuple.HeadGetter (Tuple.FromList (t ': ts))
+    , Tuple.TailGetter (Tuple.FromList (t ': ts))
+    )
+
+
+-- === API === --
+
 getElem :: ∀ el ts. ElemGetter el ts => TypeMap ts -> el
-getElem t = Tuple.getElem (unwrap t) ; {-# INLINE getElem #-}
+getElem t = Tuple.getElem (unwrap t)
+{-# INLINE getElem #-}
 
 setElem :: ∀ el ts. ElemSetter el ts => el -> TypeMap ts -> TypeMap ts
-setElem v = wrapped %~ (Tuple.setElemKeepType @el v) ; {-# INLINE setElem #-}
+setElem v = wrapped %~ (Tuple.setElemKeepType @el v)
+{-# INLINE setElem #-}
 
 modifyElem_ :: ∀ el ts. ElemEditor el ts
             => (el -> el) -> TypeMap ts -> TypeMap ts
@@ -40,22 +55,17 @@ modifyElem_ f tm = setElem @el el' tm where
 {-# INLINE modifyElem_ #-}
 
 empty :: TypeMap '[]
-empty = wrap Tuple.T0 ; {-# INLINE empty #-}
+empty = wrap Tuple.T0
+{-# INLINE empty #-}
 
-
-type Prependable t ts = ( Tuple.Prepended t (Tuple.FromList ts)
-                        ~ Tuple.FromList (t : ts)
-                        , Tuple.Prependable t (Tuple.FromList ts)
-                        )
 prepend :: Prependable t ts => t -> TypeMap ts -> TypeMap (t ': ts)
-prepend t tm = wrap $ Tuple.prepend t (unwrap tm) ; {-# INLINE prepend #-}
+prepend t tm = wrap $ Tuple.prepend t (unwrap tm)
+{-# INLINE prepend #-}
 
-type SplitHead t ts =
-    ( Tuple.Head (Tuple.FromList (t ': ts)) ~ t
-    , Tuple.Tail (Tuple.FromList (t ': ts)) ~ Tuple.FromList ts
-    , Tuple.HeadGetter (Tuple.FromList (t ': ts))
-    , Tuple.TailGetter (Tuple.FromList (t ': ts))
-    )
+prependDef :: forall t ts . (Default t, Prependable t ts) => TypeMap ts
+    -> TypeMap (t ': ts)
+prependDef tm = wrap $ Tuple.prepend (def @t) (unwrap tm)
+{-# INLINE prependDef #-}
 
 splitHead :: SplitHead t ts => TypeMap (t ': ts) -> (t, TypeMap ts)
 splitHead = \tm -> wrap <$> Tuple.splitHead (unwrap tm)
@@ -70,6 +80,9 @@ class SetElemsFromList (els :: [Type]) t ts where
 instance SetElemsFromList '[] t ts where
     setElemsFromList _ = id ; {-# INLINE setElemsFromList #-}
 
+-- FIXME [Ara, WD]: unsafeCoerce is very unsafe here. We should consider making
+-- this safer. We cannot use `coerce` because it requires having far too many
+-- constructors in scope in client code.
 instance ( ElemSetter s ts
          , SetElemsFromList ss t ts
          ) => SetElemsFromList (s ': ss) t ts where
@@ -77,11 +90,22 @@ instance ( ElemSetter s ts
         []     -> impossible
         (s:ss) -> setElemsFromList @ss ss
                 $ setElem (unsafeCoerce s :: s) t
--- FIXME[WD]: unsafeCoerce is very unsafe here. We should consider making
---            it safer. We cannot use `coerce` because it requires having
---            a lot of constructors in scope in clients code.
     {-# INLINE setElemsFromList #-}
 
+
+-- === MakeDefault === --
+
+class MakeDefault (ts :: [Type]) where
+    makeDefault :: TypeMap ts
+
+instance (MakeDefault ts, Prependable t ts, Default t) =>
+    MakeDefault ((t ': ts) :: [Type]) where
+    makeDefault = prepend (def @t) $ makeDefault @ts
+    {-# INLINE makeDefault #-}
+
+instance MakeDefault ('[] :: [Type]) where
+    makeDefault = empty
+    {-# INLINE makeDefault #-}
 
 
 -- === Instances === --
@@ -89,7 +113,6 @@ instance ( ElemSetter s ts
 deriving instance Show    (TypeMapData ts) => Show    (TypeMap ts)
 deriving instance Mempty  (TypeMapData ts) => Mempty  (TypeMap ts)
 deriving instance Default (TypeMapData ts) => Default (TypeMap ts)
-
 
 
 
@@ -107,7 +130,6 @@ class FieldEncoder field a m where
 
 
 -- === Instances === --
-
 
 instance Applicative m
       => Encoder '[] a m where
