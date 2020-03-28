f39686e15257a15997e0eb441e5fc0b45314add4
Michał Wawrzyniec Urbańczyk <mwu-tow@gazeta.pl>
2020-03-28 02:10:09 +0100

    Proc macro for #[derive(CloneRef)] (https://github.com/enso-org/ide/pull/315)
    Original commit: https://github.com/enso-org/ide/commit/6378d9ef1107751cf7f424e9cf4447bb2f5654c1

---
+++ gui/src/rust/lib/shapely/impl/tests/derive_clone_ref.rs
@@ -0,0 +1,34 @@
+// This module contains dead code. Its purpose is making sure that it compiles
+#![allow(dead_code)]
+
+use enso_prelude::*;
+
+use shapely::*;
+
+#[derive(Clone,CloneRef)] struct StructUnit;
+
+#[derive(Clone,CloneRef)] struct StructUnnamedEmpty();
+
+#[derive(Clone,CloneRef)] struct StructUnnamed(Rc<i32>,Rc<String>);
+
+#[derive(Clone,CloneRef)] struct StructNamedEmpty{}
+
+#[derive(Clone,CloneRef)] struct StructNamed{named0:Rc<i32>,named1:Rc<String>}
+
+#[derive(Clone,CloneRef)] enum EnumEmpty {}
+
+#[derive(Clone,CloneRef)] enum Enum {
+    VariantUnit,
+    VariantNamedEmpty {},
+    VariantNamed {named0:Rc<i32>,named1:Rc<String>},
+    VariantUnnamedEmpty(),
+    VariantUnnamed(Rc<i32>,Rc<String>),
+}
+
+#[derive(CloneRef,Derivative)]
+#[derivative(Clone(bound=""))]
+struct StructUnnamedUnbound<T>(Rc<T>);
+
+#[derive(CloneRef,Clone)]
+#[clone_ref(bound="T:CloneRef")]
+struct StructUnnamedBound<T>(T);
