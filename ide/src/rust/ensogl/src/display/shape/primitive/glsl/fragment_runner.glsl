07f6cfbac1a408ed48032cf88bd1739c73e8fd1d
Michael Mauderer <michael@michaelmauderer.com>
2020-04-16 17:02:51 +0200

    Add a debug view that renders shape IDs (https://github.com/enso-org/ide/pull/356)
    * Add display mode that can visualise shape IDs.
    * Add key toggle on "2" to switch to debug view.
    
    Original commit: https://github.com/enso-org/ide/commit/3a868ea2828e89c52b22917e02a121e41c7b0941

--- ide/src/rust/ensogl/src/display/shape/primitive/glsl/fragment_runner.glsl
+++ ide/src/rust/ensogl/src/display/shape/primitive/glsl/fragment_runner.glsl
@@ -1,9 +1,15 @@
 /// This code is the body of the fragment shader main function of a GLSL shape.
 
-Env   env      = Env(1);
-vec2  position = input_local.xy ;
-Shape shape    = run(env,position);
-float alpha  = shape.color.color.raw.a;
+Env   env        = Env(1);
+vec2  position   = input_local.xy ;
+Shape shape      = run(env,position);
+float alpha      = shape.color.color.raw.a;
+uint alpha_no_aa = alpha > 0.5 ? uint(1) : uint(0);
+
+output_id = uvec4(input_symbol_id,input_instance_id,0,alpha_no_aa);
+output_id.r *= alpha_no_aa;
+output_id.g *= alpha_no_aa;
+output_id.b *= alpha_no_aa;
 
 if (input_display_mode == 0) {
     output_color = srgba(unpremultiply(shape.color)).raw;
@@ -11,11 +17,10 @@ if (input_display_mode == 0) {
 } else if (input_display_mode == 1) {
     Rgb col = distance_meter(shape.sdf.distance, 200.0 * input_zoom * input_pixel_ratio, 200.0/input_zoom * input_pixel_ratio);
     output_color = rgba(col).raw;
+} else if (input_display_mode == 2) {
+    float object_hue  = float((input_instance_id * 7) % 100) / 100.0;
+    Srgb object_color = srgb(hsv(object_hue, 1.0, 0.5));
+    output_color.rgb  = object_color.raw.rgb;
+    output_color.a    = float(alpha_no_aa);
+    output_color.rgb *= float(alpha_no_aa);
 }
-
-uint alpha_no_aa = alpha > 0.5 ? uint(1) : uint(0);
-
-output_id = uvec4(input_symbol_id,input_instance_id,0,alpha_no_aa);
-output_id.r *= alpha_no_aa;
-output_id.g *= alpha_no_aa;
-output_id.b *= alpha_no_aa;
