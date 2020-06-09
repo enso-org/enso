d7fe344326f49d375dfc12ab23a03d34ce6606c3
Danilo Guanabara <danguafer@gmail.com>
2020-06-09 02:33:06 -0300

    Improved node searcher (https://github.com/enso-org/ide/pull/519)
    Original commit: https://github.com/enso-org/ide/commit/be88749d884731fba5a272bf2f24d906d2df9492

--- ide/src/rust/ide/src/view/layout.rs
+++ ide/src/rust/ide/src/view/layout.rs
@@ -9,10 +9,12 @@ use crate::view::text_editor::TextEditor;
 use crate::view::node_editor::NodeEditor;
 use crate::view::node_searcher::NodeSearcher;
 
+use enso_callback as callback;
 use enso_frp::io::keyboard;
 use ensogl::application::Application;
 use ensogl::display::shape::text::glyph::font;
 use ensogl::display::traits::*;
+use ensogl::display::Uniform;
 use ensogl::display::world::World;
 use nalgebra::Vector2;
 use nalgebra::zero;
@@ -30,11 +32,15 @@ shared! { ViewLayout
 /// Initial implementation of ViewLayout with a TextEditor and NodeEditor.
 #[derive(Debug)]
 pub struct ViewLayoutData {
-    text_editor   : TextEditor,
-    node_editor   : NodeEditor,
-    node_searcher : NodeSearcher,
-    size          : Vector2<f32>,
-    logger        : Logger
+    text_editor               : TextEditor,
+    node_editor               : NodeEditor,
+    node_searcher             : NodeSearcher,
+    size                      : Vector2<f32>,
+    logger                    : Logger,
+    /// FIXME[dg]: This is a provisional code. Getting the mouse position from Uniform is bad. Mouse
+    /// position should be retrieved from the frp network instead.
+    mouse_position            : Uniform<Vector2<i32>>,
+    node_searcher_show_action : Option<callback::Handle>
 }
 
 impl {
@@ -96,25 +102,38 @@ impl ViewLayout {
     , visualization_controller : controller::Visualization
     , fonts                    : &mut font::Registry
     ) -> FallibleResult<Self> {
-        let logger           = logger.sub("ViewLayout");
-        let world            = &application.display;
-        let text_editor      = TextEditor::new(&logger,world,text_controller,kb_actions,fonts);
-        let graph            = graph_controller.graph.clone_ref();
-        let node_searcher    = NodeSearcher::new(world,&logger,graph,fonts);
-        let graph_controller = graph_controller.clone_ref();
-        let node_editor      = NodeEditor::new
+        let logger        = logger.sub("ViewLayout");
+        let world         = &application.display;
+        let text_editor   = TextEditor::new(&logger,world,text_controller,kb_actions,fonts);
+        let graph         = graph_controller.graph.clone_ref();
+        let node_editor   = NodeEditor::new
             (&logger,application,graph_controller,visualization_controller).await?;
+        let node_searcher = NodeSearcher::new(world,&logger,node_editor.clone_ref(),graph,fonts);
         world.add_child(&text_editor.display_object());
         world.add_child(&node_editor);
         world.add_child(&node_searcher);
-        let size = zero();
-        let data = ViewLayoutData {text_editor,node_editor,node_searcher,size,logger};
+        let size                      = zero();
+        let mouse_position            = world.scene().mouse.position.clone_ref();
+        let node_searcher_show_action = None;
+        let data = ViewLayoutData{text_editor,node_editor,node_searcher,size,logger,mouse_position,
+            node_searcher_show_action};
         let rc   = Rc::new(RefCell::new(data));
         Ok(Self {rc}.init(world,kb_actions))
     }
 
-    fn init_keyboard(self, _keyboard_actions:&mut keyboard::Actions) -> Self {
+    fn init_keyboard(self, keyboard_actions:&mut keyboard::Actions) -> Self {
         // TODO[ao] add here some useful staff (quitting project for example)
+        let layout                    = self.rc.clone_ref();
+        let keys                      = &[keyboard::Key::Tab];
+        let node_searcher_show_action = keyboard_actions.add_action(keys, move || {
+            let mut layout             = layout.borrow_mut();
+            let position               = layout.mouse_position.get();
+            //TODO[dg]: Test it when graph scene panning is working.
+            let node_searcher_position = Vector3::new(position.x as f32,position.y as f32,0.0);
+            layout.node_searcher.set_position(node_searcher_position);
+            layout.node_searcher.show();
+        });
+        self.rc.borrow_mut().node_searcher_show_action = Some(node_searcher_show_action);
         self
     }
 
