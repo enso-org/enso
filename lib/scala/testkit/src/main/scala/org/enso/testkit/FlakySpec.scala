5a17a4c943ff23bc7812571a44cde4035f523515
Dmitry Bushev <bushevdv@gmail.com>
2020-07-17 17:53:21 +0300

    doc: update FlakySpec

--- lib/scala/testkit/src/main/scala/org/enso/testkit/FlakySpec.scala
+++ lib/scala/testkit/src/main/scala/org/enso/testkit/FlakySpec.scala
@@ -6,6 +6,9 @@ import org.scalatest._
   * Trait is used to mark the tests in the suite as _flaky_ and make them
   * pass. It changes the behavior for failed tests to return 'pending' instead
   * of failing the suite.
+  *
+  * Note that the trait is disabled by default and enabled by setting
+  * the `CI_TEST_FLAKY_ENABLE` environment variable.
   */
 trait FlakySpec extends TestSuite {
 
