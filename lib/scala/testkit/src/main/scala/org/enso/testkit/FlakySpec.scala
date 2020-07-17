7bdbf706002027ba8df0ac48baa56735788a4f59
Dmitry Bushev <bushevdv@gmail.com>
2020-07-17 16:15:12 +0300

    test: disable FlakySpec by default

--- lib/scala/testkit/src/main/scala/org/enso/testkit/FlakySpec.scala
+++ lib/scala/testkit/src/main/scala/org/enso/testkit/FlakySpec.scala
@@ -10,11 +10,15 @@ import org.scalatest._
 trait FlakySpec extends TestSuite {
 
   /** Tags test as _flaky_. */
-  object Flaky extends Tag("org.enso.test.flaky")
+  object Flaky extends Tag("org.enso.test.flaky") {
+
+    val isEnabled = sys.env.contains("CI_TEST_FLAKY_ENABLE")
+  }
 
   override def withFixture(test: NoArgTest): Outcome =
     super.withFixture(test) match {
-      case Failed(_) | Canceled(_) if test.tags.contains(Flaky.name) =>
+      case Failed(_) | Canceled(_)
+          if Flaky.isEnabled && test.tags.contains(Flaky.name) =>
         Pending
       case outcome =>
         outcome
