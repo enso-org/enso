4d81217ce4d2afe25610bfe9cfa0efee59b10f05
Dmitry Bushev <bushevdv@gmail.com>
2020-08-04 09:34:53 +0300

    build release version on ci

--- project/GenerateAST.scala
+++ project/GenerateAST.scala
@@ -1,5 +1,3 @@
-import java.io.IOException
-
 import sbt.Keys._
 import sbt._
 import sbt.internal.util.ManagedLogger
@@ -8,8 +6,9 @@ import scala.sys.process._
 
 object GenerateAST {
 
-  val rustVersion = settingKey[String]("rustc version used in the project")
-  private val cargoCmd = "cargo"
+  val rustVersion          = settingKey[String]("rustc version used in the project")
+  private val cargoCmd     = "cargo"
+  private val BuildProfile = "BUILD_PROFILE"
 
   lazy val task = Def.task {
     val log    = state.value.log
@@ -18,29 +17,32 @@ object GenerateAST {
     val output = sourceManaged.value / "main/org/enso/ast/Ast.scala"
     val cache  = streams.value.cacheStoreFactory.make("ast_source")
 
-    Tracked.diffInputs(cache, FileInfo.lastModified)(Set(source))
-    { source: ChangeReport[File] =>
-      if (source.modified.nonEmpty) {
+    Tracked.diffInputs(cache, FileInfo.lastModified)(Set(source)) {
+      source: ChangeReport[File] =>
+        if (source.modified.nonEmpty) {
           output.getParentFile.mkdirs
           generateAST(output, log)
-      }
+        }
     }
 
     Seq(output)
   }
 
   /**
-    * Generates the Scala AST in the specified file. All errors are reported in 
+    * Generates the Scala AST in the specified file. All errors are reported in
     * stderr and raise a runtime exception.
     *
     * @param out the file where the generated AST is going to be placed
     */
   private def generateAST(out: File, log: ManagedLogger): Unit = {
-    val command = s"$cargoCmd run -p ast -- --generate-scala-ast $out"
+    val profile = sys.env.getOrElse(BuildProfile, "dev")
+    val command =
+      s"$cargoCmd run -Z unstable-options --profile $profile -p ast -- --generate-scala-ast $out"
 
     log.info(s"Generating AST with the command: $command:")
 
-    try {command.!!} catch {
+    try { command.!! }
+    catch {
       case ex: RuntimeException =>
         log.error(s"Generation of the Scala AST failed.")
         throw ex
