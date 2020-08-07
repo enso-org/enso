5bdc2db2596f86922b3405e5bb9628c2ed1b9022
Radosław Waśko <radoslaw.wasko@enso.org>
2020-08-07 16:45:44 +0200

    Fix timeout

--- engine/launcher/src/test/scala/org/enso/launcher/NativeTest.scala
+++ engine/launcher/src/test/scala/org/enso/launcher/NativeTest.scala
@@ -21,7 +21,7 @@ import scala.jdk.StreamConverters._
   */
 trait NativeTest extends AnyWordSpec with Matchers with TimeLimitedTests {
 
-  override val timeLimit: Span               = 1 seconds
+  override val timeLimit: Span               = 15 seconds
   override val defaultTestSignaler: Signaler = _.interrupt()
 
   /**
@@ -147,39 +147,49 @@ trait NativeTest extends AnyWordSpec with Matchers with TimeLimitedTests {
     )
   }
 
+  /**
+    * Runs the provided `command`.
+    *
+    * `extraEnv` may be provided to extend the environment. Care must be taken
+    * on Windows where environment variables are (mostly) case-insensitive.
+    *
+    * If `waitForDescendants` is set, tries to wait for descendants of the
+    * launched process to finish too. Especially important on Windows where
+    * child processes may run after the launcher parent has been terminated.
+    */
   private def run(
     command: Seq[String],
     extraEnv: Seq[(String, String)],
     waitForDescendants: Boolean = true
   ): RunResult = {
-    try {
-      val builder = new JProcessBuilder(command: _*)
-      val newKeys = extraEnv.map(_._1.toLowerCase)
-      if (newKeys.distinct.size < newKeys.size) {
-        throw new IllegalArgumentException(
-          "The extra environment keys have to be unique"
-        )
-      }
+    val builder = new JProcessBuilder(command: _*)
+    val newKeys = extraEnv.map(_._1.toLowerCase)
+    if (newKeys.distinct.size < newKeys.size) {
+      throw new IllegalArgumentException(
+        "The extra environment keys have to be unique"
+      )
+    }
 
-      lazy val existingKeys =
-        builder.environment().keySet().asScala
-      for ((key, value) <- extraEnv) {
-        if (OS.isWindows) {
-          def shadows(key1: String, key2: String): Boolean =
-            key1.toLowerCase == key2.toLowerCase && key1 != key2
+    lazy val existingKeys =
+      builder.environment().keySet().asScala
+    for ((key, value) <- extraEnv) {
+      if (OS.isWindows) {
+        def shadows(key1: String, key2: String): Boolean =
+          key1.toLowerCase == key2.toLowerCase && key1 != key2
 
-          existingKeys.find(shadows(_, key)) match {
-            case Some(oldKey) =>
-              throw new IllegalArgumentException(
-                s"The environment key $key may be shadowed by $oldKey " +
-                s"already existing in the environment. Please use `$oldKey`."
-              )
-            case None =>
-          }
+        existingKeys.find(shadows(_, key)) match {
+          case Some(oldKey) =>
+            throw new IllegalArgumentException(
+              s"The environment key `$key` may be shadowed by `$oldKey` " +
+              s"already existing in the environment. Please use `$oldKey`."
+            )
+          case None =>
         }
-        builder.environment().put(key, value)
       }
+      builder.environment().put(key, value)
+    }
 
+    try {
       val process = builder.start()
 
       try {
