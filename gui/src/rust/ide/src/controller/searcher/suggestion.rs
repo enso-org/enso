666906fa887c476d7b0042835e4c6bea1f542558
Adam Obuchowicz <adam.obuchowicz@luna-lang.org>
2020-08-14 12:38:03 +0200

    Searcher controller: Filtering suggestions (https://github.com/enso-org/ide/pull/717)
    Original commit: https://github.com/enso-org/ide/commit/e39878050dc63c6640fc3bcdbb35c31a58f676b1

---
+++ gui/src/rust/ide/src/controller/searcher/suggestion.rs
@@ -0,0 +1,161 @@
+//! All structures related to the suggestion list provided by SearcherController.
+
+use crate::prelude::*;
+
+
+// ===================
+// === Suggestion ===
+// ===================
+
+/// Suggestion for input completion: possible functions, arguments, etc.
+pub type Completion = Rc<model::suggestion_database::Entry>;
+
+/// A single suggestion on the Searcher suggestion list.
+#[derive(Clone,CloneRef,Debug,Eq,PartialEq)]
+pub enum Suggestion {
+    /// Suggestion for input completion: possible functions, arguments, etc.
+    Completion(Completion)
+    // In future, other suggestion types will be added (like suggestions of actions, etc.).
+}
+
+impl Suggestion {
+    /// The suggestion caption (suggested function name, or action name, etc.).
+    pub fn caption(&self) -> &String {
+        match self {
+            Self::Completion(completion) => &completion.name
+        }
+    }
+}
+
+
+
+// ==================
+// === List Entry ===
+// ==================
+
+/// Information how the Suggestion list entry matches the filtering pattern.
+#[allow(missing_docs)]
+#[derive(Clone,Debug,PartialEq)]
+pub enum MatchInfo {
+    DoesNotMatch,
+    Matches {subsequence:fuzzly::Subsequence}
+}
+
+/// The single suggestion list entry.
+#[allow(missing_docs)]
+#[derive(Clone,Debug)]
+pub struct ListEntry {
+    pub match_info : MatchInfo,
+    pub suggestion : Suggestion,
+}
+
+impl ListEntry {
+    /// Update the current match info according to the new filtering pattern.
+    pub fn update_matching_info(&mut self, pattern:impl Str) {
+        let matches     = fuzzly::matches(self.suggestion.caption(),pattern.as_ref());
+        let subsequence = matches.and_option_from(|| {
+            let metric = fuzzly::metric::default();
+            fuzzly::find_best_subsequence(self.suggestion.caption(),pattern,metric)
+        });
+        self.match_info = match subsequence {
+            Some(subsequence) => MatchInfo::Matches {subsequence},
+            None              => MatchInfo::DoesNotMatch,
+        };
+    }
+
+    /// Compare how two entries matches filtering pattern.
+    ///
+    /// The "greater" entry matches better.
+    pub fn compare_match_scores(&self, rhs:&ListEntry) -> std::cmp::Ordering {
+        use MatchInfo::*;
+        use std::cmp::Ordering::*;
+        match (&self.match_info,&rhs.match_info) {
+            (DoesNotMatch             ,DoesNotMatch             ) => Equal,
+            (DoesNotMatch             ,Matches {..}             ) => Less,
+            (Matches {..}             ,DoesNotMatch             ) => Greater,
+            (Matches {subsequence:lhs},Matches {subsequence:rhs}) => lhs.compare_scores(rhs),
+        }
+    }
+}
+
+impl From<Suggestion> for ListEntry {
+    fn from(suggestion:Suggestion) -> Self {
+        let subsequence = default();
+        let match_info  = MatchInfo::Matches {subsequence};
+        ListEntry {match_info,suggestion}
+    }
+}
+
+
+
+// ============
+// === List ===
+// ============
+
+/// Suggestion list.
+///
+/// This structure should be notified about filtering changes. using `update_filtering` function.
+#[derive(Clone,Debug,Default)]
+pub struct List {
+    entries : RefCell<Vec<ListEntry>>
+}
+
+impl List {
+    /// Create new empty list.
+    pub fn new() -> Self {
+        default()
+    }
+
+    /// Create list from suggestions.
+    ///
+    /// The list will assume that the filtering pattern is an empty string.
+    pub fn from_suggestions(suggestions:impl IntoIterator<Item=Suggestion>) -> Self {
+        Self {entries:RefCell::new(suggestions.into_iter().map(ListEntry::from).collect())}
+    }
+
+    /// Update the list filtering.
+    ///
+    /// The "matching score" of each entry is recalculated against the given pattern and the entries
+    /// are re-ordered, so the best matches will go first.
+    pub fn update_filtering(&self, pattern:impl Str) {
+        let mut entries_mut = self.entries.borrow_mut();
+        for entry in entries_mut.iter_mut() {
+            entry.update_matching_info(pattern.as_ref());
+        }
+        entries_mut.sort_by(|l,r| l.compare_match_scores(r).reverse());
+    }
+
+    /// Length of the suggestion list.
+    pub fn len(&self) -> usize { self.entries.borrow().len() }
+
+    /// Check if list is empty.
+    pub fn is_empty(&self) -> bool { self.entries.borrow().is_empty() }
+
+    /// Iterate over suggestion entries.
+    pub fn iter<'a>(&'a self) -> impl Iterator<Item=ListEntry> + 'a {
+        let existing_ids = (0..self.len()).take_while(move |id| *id < self.len());
+        existing_ids.filter_map(move |id| self.entries.borrow().get(id).cloned())
+    }
+
+    /// Extend the list with new suggestions.
+    ///
+    /// The new suggestions will be put at end, regardless the current filtering. This function
+    /// is meant to be a part of list's initialization.
+    pub fn extend<T:IntoIterator<Item=Suggestion>>(&self, iter: T) {
+        self.entries.borrow_mut().extend(iter.into_iter().map(ListEntry::from))
+    }
+
+    /// Convert to the suggestion vector.
+    ///
+    /// Used for testing.
+    pub fn to_suggestion_vec(&self) -> Vec<Suggestion> {
+        self.entries.borrow().iter().map(|entry| entry.suggestion.clone_ref()).collect()
+    }
+}
+
+impl<IntoIter> From<IntoIter> for List
+where IntoIter : IntoIterator<Item=Suggestion> {
+    fn from(suggestions:IntoIter) -> Self {
+        Self::from_suggestions(suggestions)
+    }
+}
