7bcca50d0e7ce37db43c4794b094b4e132f08387
Michael Mauderer <michael.mauderer@enso.org>
2020-08-18 13:24:11 +0200

    fix: Fix naming clash. (https://github.com/enso-org/ide/pull/728)
    Original commit: https://github.com/enso-org/ide/commit/9b5d4df05c7b76150a92359c20e5f9102aabb7b2

--- gui/src/rust/ide/lib/view/src/graph_editor/component/visualization/container.rs
+++ gui/src/rust/ide/lib/view/src/graph_editor/component/visualization/container.rs
@@ -279,8 +279,9 @@ impl ContainerModel {
         self
     }
 
-    /// Indicates whether the visualization is visible.
-    pub fn is_visible(&self) -> bool {
+    /// Indicates whether the visualization container is visible and active.
+    /// Note: can't be called `is_visible` due to a naming conflict with `display::object::class`.
+    pub fn is_active(&self) -> bool {
         self.view.has_parent()
     }
 }
@@ -308,7 +309,7 @@ impl ContainerModel {
     }
 
     fn toggle_visibility(&self) {
-        self.set_visibility(!self.is_visible())
+        self.set_visibility(!self.is_active())
     }
 
     fn set_visualization(&self, visualization:Option<visualization::Instance>) {
