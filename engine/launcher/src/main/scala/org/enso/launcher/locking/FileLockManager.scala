0fac7ff307d9be0289b705fbc3808f18133e19fc
Radosław Waśko <radoslaw.wasko@enso.org>
2020-09-18 17:02:59 +0200

    Document shared locks

--- engine/launcher/src/main/scala/org/enso/launcher/locking/FileLockManager.scala
+++ engine/launcher/src/main/scala/org/enso/launcher/locking/FileLockManager.scala
@@ -3,6 +3,8 @@ package org.enso.launcher.locking
 import java.nio.channels.{FileChannel, FileLock}
 import java.nio.file.{Files, Path, StandardOpenOption}
 
+import org.enso.launcher.Logger
+
 import scala.util.control.NonFatal
 
 /**
@@ -10,6 +12,15 @@ import scala.util.control.NonFatal
   *
   * This is the [[LockManager]] that should be used in production as it is able
   * to synchronize locks between different processes.
+  *
+  * Under the hood, it uses the [[FileLock]] mechanism. This mechanism specifies
+  * that on some platforms shared locks may not be supported and all shared
+  * locks are treated as exclusive locks there. That would be harmful to the
+  * user experience by allowing only a single Enso process to be running at the
+  * same time as we use shared locks extensively. However, all the platforms we
+  * currently support (Linux, macOS, Windows), do support shared locks, so this
+  * should not be an issue for us. However, if a shared lock request returns an
+  * exclusive lock, a warning is issued, just in case.
   */
 abstract class FileLockManager extends LockManager {
 
@@ -75,7 +86,11 @@ abstract class FileLockManager extends LockManager {
   }
 
   private def lockChannel(channel: FileChannel, lockType: LockType): Lock = {
-    WrapLock(channel.lock(0L, Long.MaxValue, isShared(lockType)), channel)
+    WrapLock(
+      channel.lock(0L, Long.MaxValue, isShared(lockType)),
+      channel,
+      lockType
+    )
   }
 
   private def tryLockChannel(
@@ -83,10 +98,23 @@ abstract class FileLockManager extends LockManager {
     lockType: LockType
   ): Option[Lock] =
     Option(channel.tryLock(0L, Long.MaxValue, isShared(lockType)))
-      .map(WrapLock(_, channel))
+      .map(WrapLock(_, channel, lockType))
 
-  private case class WrapLock(fileLock: FileLock, channel: FileChannel)
-      extends Lock {
+  private case class WrapLock(
+    fileLock: FileLock,
+    channel: FileChannel,
+    lockType: LockType
+  ) extends Lock {
+
+    if (isShared(lockType) && !fileLock.isShared) {
+      Logger.warn(
+        "A request for a shared lock returned an exclusive lock. " +
+        "The platform that you are running on may not support shared locks, " +
+        "this may result in only a single Enso instance being able to run at " +
+        "any time."
+      )
+    }
+
     override def release(): Unit = {
       fileLock.release()
       channel.close()
