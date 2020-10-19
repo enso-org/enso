39604356d903b0a25088e6eff6c28a7a5835d8cf
Adam Obuchowicz <adam.obuchowicz@luna-lang.org>
2020-10-19 17:01:10 +0200

    Fix disconnecting (https://github.com/enso-org/ide/pull/854)
    Implement the "connection removed" event properly.
    
    Original commit: https://github.com/enso-org/ide/commit/55d8eea028c8156e22dba2299cbb4798b2ca72e9

--- ide/src/rust/ide/view/graph-editor/src/lib.rs
+++ ide/src/rust/ide/view/graph-editor/src/lib.rs
@@ -1314,6 +1314,7 @@ impl GraphEditorModel {
                 self.refresh_edge_position(edge_id);
                 self.refresh_edge_source_size(edge_id);
             }
+
         }
     }
 
@@ -1987,9 +1988,13 @@ fn new_graph_editor(app:&Application) -> GraphEditor {
     eval edge_source_click (((edge_id, _)) model.remove_edge_source(*edge_id));
     eval edge_target_click (((edge_id, _)) model.remove_edge_target(*edge_id));
 
+    outputs.edge_source_unset <+ edge_source_click.map(|(edge_id,_)| *edge_id);
+    outputs.edge_target_unset <+ edge_target_click.map(|(edge_id,_)| *edge_id);
+
     }
 
     // === Edge creation  ===
+
     frp::extend! { network
 
     on_node_output_touch <- node_output_touch.down.constant(());
@@ -2432,7 +2437,9 @@ fn new_graph_editor(app:&Application) -> GraphEditor {
     both_endpoints_set         <- edge_endpoint_set.map(f!((id) model.is_connection(id)));
     new_connection             <- edge_endpoint_set.gate(&both_endpoints_set);
     outputs.connection_added   <+ new_connection;
-    outputs.connection_removed <+ outputs.edge_removed;
+    outputs.connection_removed <+ any
+        (outputs.edge_removed,outputs.edge_source_unset,outputs.edge_target_unset);
+    trace outputs.connection_removed;
 
 
     // === Status ===
