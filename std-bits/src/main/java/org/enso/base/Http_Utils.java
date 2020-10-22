6987209aee35b2e3bf44f5165f0328a1bf03dbbe
Dmitry Bushev <bushevdv@gmail.com>
2020-10-23 02:04:05 +0300

    impl post form

--- std-bits/src/main/java/org/enso/base/Http_Utils.java
+++ std-bits/src/main/java/org/enso/base/Http_Utils.java
@@ -2,6 +2,10 @@ package org.enso.base;
 
 import java.net.Authenticator;
 import java.net.PasswordAuthentication;
+import java.net.http.HttpHeaders;
+import java.util.AbstractMap;
+import java.util.List;
+import java.util.Map;
 
 /** Utils for standard HTTP library. */
 public class Http_Utils {
@@ -23,8 +27,27 @@ public class Http_Utils {
     }
   }
 
+  /**
+   * Create an authenticator for http basic auth.
+   *
+   * @param user the user name.
+   * @param password the password.
+   * @return a new basic authenticator.
+   */
   public static Authenticator basic_authenticator(String user, String password) {
     return new BasicAuthenticator(user, password);
   }
 
+  /**
+   * Get the response headers as a list of map entries.
+   *
+   * @param headers the http headers.
+   * @return the key-value list of headers.
+   */
+  public static Object[] get_headers(HttpHeaders headers) {
+    Map<String, List<String>> map = headers.map();
+    return map.keySet().stream()
+        .flatMap(k -> map.get(k).stream().map(v -> new AbstractMap.SimpleImmutableEntry<>(k, v)))
+        .toArray();
+  }
 }
