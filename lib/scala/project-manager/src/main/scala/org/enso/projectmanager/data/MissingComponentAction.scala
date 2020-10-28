dd2bd00775cdd2888472a82dae70a83d138f36e1
Radosław Waśko <radoslaw.wasko@enso.org>
2020-10-28 16:37:35 +0100

    docs

--- lib/scala/project-manager/src/main/scala/org/enso/projectmanager/data/MissingComponentAction.scala
+++ lib/scala/project-manager/src/main/scala/org/enso/projectmanager/data/MissingComponentAction.scala
@@ -3,7 +3,17 @@ package org.enso.projectmanager.data
 /** Specifies how to handle missing components. */
 sealed trait MissingComponentAction
 object MissingComponentAction {
-  case object Fail               extends MissingComponentAction
-  case object Install            extends MissingComponentAction
+
+  /** Specifies that an action requiring a missing component should fail. */
+  case object Fail extends MissingComponentAction
+
+  /** Specifies that an action requiring a missing component should install it,
+    * unless it is broken.
+    */
+  case object Install extends MissingComponentAction
+
+  /** Specifies that an action requiring a missing component should forcibly
+    * install it, even if it is broken.
+    */
   case object ForceInstallBroken extends MissingComponentAction
 }
