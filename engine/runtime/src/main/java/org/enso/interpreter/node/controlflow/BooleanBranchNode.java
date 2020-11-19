429c37e97e126e4c1812314bde0b30f33ed58b56
BinarySoftware <mkmikolajek@icloud.com>
2020-11-19 14:44:34 +0100

    Revert "Merge remote-tracking branch 'origin/main' into wip/mm/backport2test"
    This reverts commit 35b51749ec75321181fb4afee7721f204845e166, reversing
    changes made to 2c2b73cb5201ddff08bdb94531571b2398835b99.

--- engine/runtime/src/main/java/org/enso/interpreter/node/controlflow/BooleanBranchNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/controlflow/BooleanBranchNode.java
@@ -1,11 +1,9 @@
 package org.enso.interpreter.node.controlflow;
 
-import com.oracle.truffle.api.RootCallTarget;
 import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.FrameUtil;
 import com.oracle.truffle.api.frame.VirtualFrame;
-import com.oracle.truffle.api.nodes.DirectCallNode;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import com.oracle.truffle.api.profiles.ConditionProfile;
 import org.enso.interpreter.node.ExpressionNode;
@@ -15,18 +13,19 @@ import org.enso.interpreter.node.callable.function.CreateFunctionNode;
 import org.enso.interpreter.runtime.callable.atom.Atom;
 import org.enso.interpreter.runtime.callable.atom.AtomConstructor;
 import org.enso.interpreter.runtime.callable.function.Function;
-import org.enso.interpreter.runtime.state.Stateful;
 import org.enso.interpreter.runtime.type.TypesGen;
 
 /** An implementation of the case expression specialised to working on booleans. */
 @NodeInfo(shortName = "BooleanMatch")
 public abstract class BooleanBranchNode extends BranchNode {
   private final boolean matched;
+  private @Child ExpressionNode branch;
+  private @Child ExecuteCallNode executeCallNode = ExecuteCallNodeGen.create();
   private final ConditionProfile profile = ConditionProfile.createCountingProfile();
 
-  BooleanBranchNode(boolean matched, RootCallTarget branch) {
-    super(branch);
+  BooleanBranchNode(boolean matched, CreateFunctionNode branch) {
     this.matched = matched;
+    this.branch = branch;
   }
 
   /**
@@ -36,17 +35,41 @@ public abstract class BooleanBranchNode extends BranchNode {
    * @param branch the expression to be executed if (@code matcher} matches
    * @return a node for matching in a case expression
    */
-  public static BooleanBranchNode build(boolean matched, RootCallTarget branch) {
+  public static BooleanBranchNode build(boolean matched, CreateFunctionNode branch) {
     return BooleanBranchNodeGen.create(matched, branch);
   }
 
+  /**
+   * Handles the boolean scrutinee case.
+   *
+   * @param frame the stack frame in which to execute
+   * @param target the atom to destructure
+   */
   @Specialization
-  void doAtom(VirtualFrame frame, Object state, boolean target) {
+  public void doAtom(VirtualFrame frame, boolean target) {
+    Object state = FrameUtil.getObjectSafe(frame, getStateFrameSlot());
     if (profile.profile(matched == target)) {
-      accept(frame, state, new Object[0]);
+      Function function = TypesGen.asFunction(branch.executeGeneric(frame));
+
+      // Note [Caller Info For Case Branches]
+      throw new BranchSelectedException(
+          executeCallNode.executeCall(function, null, state, new Object[0]));
     }
   }
 
+  /**
+   * The fallback specialisation for executing the boolean branch node.
+   *
+   * @param frame the stack frame in which to execute
+   * @param target the object to execute on
+   */
   @Fallback
-  void doFallback(VirtualFrame frame, Object state, Object target) {}
+  public void doFallback(VirtualFrame frame, Object target) {}
+
+  /* Note [Caller Info For Case Branches]
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   * It is assumed that functions serving as pattern match logic branches are always function
+   * literals, not references, curried functions etc. Therefore, as function literals, they
+   * have no way of accessing the caller frame and can safely be passed null.
+   */
 }
