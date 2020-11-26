ae5bafb53e4a6d406ebbe3244a98b4e4ddc9f336
Maciej Mikołajek <mkmikolajek@icloud.com>
2020-11-27 00:21:16 +0100

    Quicktake: Fix textual visualizations look. (https://github.com/enso-org/ide/pull/988)
    Original commit: https://github.com/enso-org/ide/commit/2af0efb311a965a1b5a3ce9fa2bddbaf898f3ba9

--- ide/src/rust/ide/view/graph-editor/src/builtin/visualization/native/raw_text.rs
+++ ide/src/rust/ide/view/graph-editor/src/builtin/visualization/native/raw_text.rs
@@ -18,6 +18,13 @@ use ensogl::system::web::AttributeSetter;
 
 
 
+// =================
+// === Constants ===
+// =================
+const PADDING_TEXT: f32 = 10.0;
+
+
+
 // ===============
 // === RawText ===
 // ===============
@@ -77,37 +84,31 @@ pub struct RawTextModel {
 impl RawTextModel {
     /// Constructor.
     fn new(scene:&Scene) -> Self {
-        let logger  = Logger::new("RawText");
-        let div     = web::create_div();
-        let dom     = DomSymbol::new(&div);
-        let size    = Rc::new(Cell::new(Vector2(200.0,200.0)));
+        let logger = Logger::new("RawText");
+        let div    = web::create_div();
+        let dom    = DomSymbol::new(&div);
+        let size   = Rc::new(Cell::new(Vector2(200.0,200.0)));
 
         // FIXME : StyleWatch is unsuitable here, as it was designed as an internal tool for shape system (#795)
-        let styles     = StyleWatch::new(&scene.style_sheet);
-        let text_color = styles.get_color(ensogl_theme::graph_editor::visualization::text);
-        let text_color = color::Rgba::from(text_color);
-        let _red       = text_color.red * 255.0;
-        let _green     = text_color.green * 255.0;
-        let _blue      = text_color.blue * 255.0;
-
-        let bg_color   = styles.get_color(ensogl_theme::graph_editor::visualization::background);
-        let bg_color   = color::Rgba::from(bg_color);
-        let text_color = format!("rgba({},{},{},{})",_red,_green,_blue,text_color.alpha);
-
-        let bg_red   = bg_color.red*255.0;
-        let bg_green = bg_color.green*255.0;
-        let bg_blue  = bg_color.blue*255.0;
-        let bg_hex   = format!("rgba({},{},{},{})",bg_red,bg_green,bg_blue,bg_color.alpha);
+        let styles       = StyleWatch::new(&scene.style_sheet);
+        let text_color   = styles.get_color(ensogl_theme::graph_editor::visualization::text);
+        let text_color   = color::Rgba::from(text_color);
+        let _red         = text_color.red * 255.0;
+        let _green       = text_color.green * 255.0;
+        let _blue        = text_color.blue * 255.0;
+        let text_color   = format!("rgba({},{},{},{})",_red,_green,_blue,text_color.alpha);
+        let padding_text = format!("{}px",PADDING_TEXT);
 
         dom.dom().set_attribute_or_warn("class","visualization scrollable",&logger);
-        dom.dom().set_style_or_warn("background"    ,bg_hex          ,&logger);
-        dom.dom().set_style_or_warn("white-space"   ,"pre"           ,&logger);
-        dom.dom().set_style_or_warn("overflow-y"    ,"auto"          ,&logger);
-        dom.dom().set_style_or_warn("overflow-x"    ,"auto"          ,&logger);
-        dom.dom().set_style_or_warn("font-family"   ,"dejavuSansMono",&logger);
-        dom.dom().set_style_or_warn("font-size"     ,"11px"          ,&logger);
-        dom.dom().set_style_or_warn("color"         ,text_color      ,&logger);
-        dom.dom().set_style_or_warn("pointer-events","auto"          ,&logger);
+        dom.dom().set_style_or_warn("white-space"   ,"pre"                ,&logger);
+        dom.dom().set_style_or_warn("overflow-y"    ,"auto"               ,&logger);
+        dom.dom().set_style_or_warn("overflow-x"    ,"auto"               ,&logger);
+        dom.dom().set_style_or_warn("font-family"   ,"DejaVuSansMonoBook" ,&logger);
+        dom.dom().set_style_or_warn("font-size"     ,"12px"               ,&logger);
+        dom.dom().set_style_or_warn("padding-left"  ,&padding_text        ,&logger);
+        dom.dom().set_style_or_warn("padding-top"   ,&padding_text        ,&logger);
+        dom.dom().set_style_or_warn("color"         ,text_color           ,&logger);
+        dom.dom().set_style_or_warn("pointer-events","auto"               ,&logger);
 
         scene.dom.layers.back.manage(&dom);
         RawTextModel{dom,logger,size}.init()
@@ -119,6 +120,9 @@ impl RawTextModel {
     }
 
     fn set_size(&self, size:Vector2) {
+        let x_mod = size.x - PADDING_TEXT;
+        let y_mod = size.y - PADDING_TEXT;
+        let size  = Vector2(x_mod,y_mod);
         self.size.set(size);
         self.reload_style();
     }
@@ -130,7 +134,6 @@ impl RawTextModel {
         };
         let data_str = serde_json::to_string_pretty(&**data_inner);
         let data_str = data_str.unwrap_or_else(|e| format!("<Cannot render data: {}>", e));
-        let data_str = format!("\n{}",data_str);
         self.dom.dom().set_inner_text(&data_str);
         Ok(())
     }
