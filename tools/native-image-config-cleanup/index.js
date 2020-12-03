e3b94eff8966e474a8713ec5e261a406fd76bd79
Radosław Waśko <radoslaw.wasko@enso.org>
2020-12-03 13:02:56 +0100

    Make reflection config stable

--- tools/native-image-config-cleanup/index.js
+++ tools/native-image-config-cleanup/index.js
@@ -1,36 +1,67 @@
 const fs = require("fs");
 const path = require("path");
-const process = require('child_process');
+const process = require("child_process");
 
-const configPaths = ["../../engine/launcher/native-image-config", "../../lib/scala/project-manager/native-image-config"];
+/// List of configs to clean.
+const configPaths = [
+  "../../engine/launcher/native-image-config",
+  "../../lib/scala/project-manager/native-image-config",
+];
 
+/// Checks if the entry is ephemeral (contains a pointer in its name).
 function isEntryEphemeralClass(entry) {
   const name = entry["name"];
-	if (name === undefined) {
-		return false;
-	}
-	return name.indexOf("/0x00") >= 0;
+  if (name === undefined) {
+    return false;
+  }
+  return name.indexOf("/0x00") >= 0;
 }
 
-function cleanEphemeralClasses(reflectConfigPath) {
-	const data = fs.readFileSync(reflectConfigPath, "utf-8");
-	const parsed = JSON.parse(data);
-	const withoutEphemeral = parsed.filter(entry => !isEntryEphemeralClass(entry));
-	if (withoutEphemeral.length == parsed.length) {
-		console.log("No ephemeral classes found for " + reflectConfigPath + ", no changes.");
-	} else {
-	  const serialized = JSON.stringify(withoutEphemeral);
-		fs.writeFileSync(reflectConfigPath, serialized);
-		console.log("Rewritten " + reflectConfigPath);
-	}
+/// Sorts the list of entries in a predictable order.
+function sortEntries(entries) {
+  const copy = Array.from(entries);
+  copy.sort((first, second) => {
+    const firstName = first["name"];
+    const secondName = second["name"];
+    if (firstName !== undefined && secondName !== undefined) {
+      return firstName < secondName;
+    } else if (firstName === undefined && secondName === undefined) {
+      return JSON.stringify(first) < JSON.stringify(second);
+    } else {
+      return firstName === undefined;
+    }
+  });
+  return copy;
 }
 
+/// Removes ephemeral classes from the reflection config and ensures it has a
+/// stable order of entries.
+function cleanReflectionConfig(reflectConfigPath) {
+  const data = fs.readFileSync(reflectConfigPath, "utf-8");
+  const parsed = JSON.parse(data);
+  const withoutEphemeral = parsed.filter(
+    (entry) => !isEntryEphemeralClass(entry)
+  );
+  const sorted = sortEntries(withoutEphemeral);
+  const serialized = JSON.stringify(sorted);
+  const hasChanges = serialized !== JSON.stringify(parsed);
+  if (hasChanges) {
+    fs.writeFileSync(reflectConfigPath, serialized);
+    console.log("Rewritten " + reflectConfigPath);
+  } else {
+    console.log("No changes in " + reflectConfigPath);
+  }
+}
+
+/// Runs prettier on the provided path.
 function runPrettier(configPath) {
-	console.log("Running prettier for " + configPath);
-	process.spawn("npx", ["prettier", "--write", configPath], {stdio: "inherit"});
+  console.log("Running prettier for " + configPath);
+  process.spawn("npx", ["prettier", "--write", configPath], {
+    stdio: "inherit",
+  });
 }
 
-configPaths.forEach(function(configPath) {
-	cleanEphemeralClasses(path.join(configPath, "reflect-config.json"));
-	runPrettier(configPath);
+configPaths.forEach(function (configPath) {
+  cleanReflectionConfig(path.join(configPath, "reflect-config.json"));
+  runPrettier(configPath);
 });
