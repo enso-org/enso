0358686f6b7bb55d90ed7542bef46f5f777db9e5
Michael Mauderer <michael.mauderer@enso.org>
2020-12-15 21:07:29 +0100

    Avoid changing edit status of project name when selecting it. (https://github.com/enso-org/ide/pull/1057)
    Original commit: https://github.com/enso-org/ide/commit/7f7a9329336dfd8e7bcf2653e8bfd75328b0ded0

--- ide/src/rust/ide/view/graph-editor/src/component/breadcrumbs/project_name.rs
+++ ide/src/rust/ide/view/graph-editor/src/component/breadcrumbs/project_name.rs
@@ -70,7 +70,9 @@ ensogl::define_endpoints! {
        /// Commit current project name.
        commit             (),
        outside_press      (),
+       /// Indicates that this is the currenlty active breadcrumb.
        select             (),
+       /// Indicates that this is not the currenlty active breadcrumb.
        deselect           (),
        /// Indicates the IDE is in edit mode. This means a click on some editable text should
        /// start editing it.
@@ -280,7 +282,10 @@ impl ProjectName {
 
             edit_click    <- model.view.events.mouse_down.gate(&frp.ide_text_edit_mode);
             start_editing <- any(edit_click,frp.input.start_editing);
-            eval_ start_editing ( text.set_cursor_at_mouse_position() );
+            eval_ start_editing ({
+                text.set_focus(true);
+                text.set_cursor_at_mouse_position()
+            });
             frp.source.edit_mode <+ start_editing.to_true();
 
             outside_press <- any(&frp.outside_press,&frp.deselect);
@@ -315,20 +320,16 @@ impl ProjectName {
 
             // === Selection ===
 
-            select <- any(&frp.select,&frp.input.start_editing);
-            eval_  select([text,animations]{
-                text.set_focus(true);
-                animations.color.set_target_value(selected_color);
-            });
-            frp.output.source.selected <+ select.to_true();
+            eval_ frp.select( animations.color.set_target_value(selected_color) );
+            frp.output.source.selected <+ frp.select.to_true();
 
-            deselect <- any3(&frp.deselect,&end_edit_mode,&outside_press);
-            eval_ deselect ([text,animations]{
+            set_inactive <- any3(&frp.deselect,&end_edit_mode,&outside_press);
+            eval_ set_inactive ([text,animations]{
                 text.set_focus(false);
                 text.remove_all_cursors();
                 animations.color.set_target_value(deselected_color);
             });
-            frp.output.source.selected <+ deselect.to_false();
+            frp.output.source.selected <+ set_inactive.to_false();
 
 
             // === Animations ===
