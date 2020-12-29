e91230bc01f6b93e6941bd3451714cebb3e1ed32
Wojciech Daniło <wojciech.danilo@gmail.com>
2020-12-29 13:50:20 +0100

    Fixing "Improper behavior while trying to reconnect node with arguments" (https://github.com/enso-org/ide/pull/1077)
    Original commit: https://github.com/enso-org/ide/commit/cbd2760e5c173e201e0a57dc4d63b4cf163b1fd6

--- ide/src/rust/ensogl/lib/core/src/data.rs
+++ ide/src/rust/ensogl/lib/core/src/data.rs
@@ -8,14 +8,6 @@ pub mod mix;
 pub mod seq;
 
 pub use enso_data::hash_map_tree;
-
 pub use enso_data::hash_map_tree::HashMapTree;
 pub use enso_data::index::Index;
 pub use enso_data::opt_vec::OptVec;
-
-/// A type alias for color representation used in the GUI. It is implemented as the perceptual
-/// uniform LCH color space because it provides the best visual properties of all available color
-/// spaces implemented in this library (there are color spaces which have even better properties
-/// but they cannot be implemented using only 3 components, which makes them almost useless when
-/// drawing with GPU support).
-pub type Color = color::Lch;
