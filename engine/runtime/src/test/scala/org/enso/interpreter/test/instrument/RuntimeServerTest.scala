6f0b4d9c55a881f9ef1bc7c8c7a74e32d6fd19ca
Ara Adkins <me@ara.io>
2021-01-18 12:53:56 +0000

    ?????

--- engine/runtime/src/test/scala/org/enso/interpreter/test/instrument/RuntimeServerTest.scala
+++ engine/runtime/src/test/scala/org/enso/interpreter/test/instrument/RuntimeServerTest.scala
@@ -1583,7 +1583,7 @@ class RuntimeServerTest
               Some(Constants.INTEGER),
               None,
               Vector(ProfilingInfo.ExecutionTime(0)),
-              false
+              true
             )
           )
         )
@@ -1611,7 +1611,7 @@ class RuntimeServerTest
               Some(Constants.INTEGER),
               None,
               Vector(ProfilingInfo.ExecutionTime(0)),
-              true
+              false
             )
           )
         )
