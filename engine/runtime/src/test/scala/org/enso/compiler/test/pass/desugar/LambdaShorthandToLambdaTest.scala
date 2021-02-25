40bd63457f936745807dadfc68aaedd512fb0c51
Ara Adkins <iamrecursion@users.noreply.github.com>
2021-02-25 11:44:13 +0000

    Desugar blanks in function position (#1512)

--- engine/runtime/src/test/scala/org/enso/compiler/test/pass/desugar/LambdaShorthandToLambdaTest.scala
+++ engine/runtime/src/test/scala/org/enso/compiler/test/pass/desugar/LambdaShorthandToLambdaTest.scala
@@ -497,4 +497,68 @@ class LambdaShorthandToLambdaTest extends CompilerTest {
       arg.defaultValue.get shouldBe an[IR.Function.Lambda]
     }
   }
+
+  "Lambda shorthand in nested functions" should {
+    "correctly translate the section-function in an application" in {
+      implicit val ctx: InlineContext = mkInlineContext
+
+      val ir =
+        """(_ + 5) 5
+          |""".stripMargin.preprocessExpression.get.desugar
+
+      ir shouldBe an[IR.Application.Prefix]
+      val app = ir.asInstanceOf[IR.Application.Prefix]
+      app.function shouldBe an[IR.Function.Lambda]
+      val lam = app.function.asInstanceOf[IR.Function.Lambda]
+      lam.arguments.length shouldEqual 1
+      val lamArg1Name = lam.arguments.head
+        .asInstanceOf[IR.DefinitionArgument.Specified]
+        .name
+        .name
+      val lamBody = lam.body.asInstanceOf[IR.Application.Prefix]
+      lamBody.arguments.length shouldEqual 2
+      val appArg1Name = lamBody.arguments.head
+        .asInstanceOf[IR.CallArgument.Specified]
+        .value
+        .asInstanceOf[IR.Name.Literal]
+        .name
+      lamArg1Name shouldEqual appArg1Name
+    }
+
+    "correctly translate the function in an application" in {
+      implicit val ctx: InlineContext = mkInlineContext
+
+      val ir =
+        """(f _ _ b) b
+          |""".stripMargin.preprocessExpression.get.desugar
+
+      ir shouldBe an[IR.Function.Lambda]
+      val firstLam = ir.asInstanceOf[IR.Function.Lambda]
+      firstLam.arguments.length shouldEqual 1
+      val firstLamArgName = firstLam.arguments.head
+        .asInstanceOf[IR.DefinitionArgument.Specified]
+        .name
+        .name
+      val secondLam = firstLam.body.asInstanceOf[IR.Function.Lambda]
+      val secondLamArgName = secondLam.arguments.head
+        .asInstanceOf[IR.DefinitionArgument.Specified]
+        .name
+        .name
+      val app = secondLam.body.asInstanceOf[IR.Application.Prefix]
+      app.arguments.length shouldEqual 4
+      val appArg1Name = app.arguments.head
+        .asInstanceOf[IR.CallArgument.Specified]
+        .value
+        .asInstanceOf[IR.Name]
+        .name
+      val appArg2Name = app
+        .arguments(1)
+        .asInstanceOf[IR.CallArgument.Specified]
+        .value
+        .asInstanceOf[IR.Name]
+        .name
+      firstLamArgName shouldEqual appArg1Name
+      secondLamArgName shouldEqual appArg2Name
+    }
+  }
 }
