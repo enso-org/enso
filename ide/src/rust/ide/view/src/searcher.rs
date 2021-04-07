afff39193b55292a57d7c3af3ea229642147b4a8
Adam Obuchowicz <adam.obuchowicz@luna-lang.org>
2021-04-07 17:53:06 +0200

    Minor fixes (https://github.com/enso-org/ide/pull/1446)
    Original commit: https://github.com/enso-org/ide/commit/33f73066f3fe5ba78510ac8fb51369d01d6ec0fb

--- ide/src/rust/ide/view/src/searcher.rs
+++ ide/src/rust/ide/view/src/searcher.rs
@@ -28,8 +28,8 @@ pub const SEARCHER_WIDTH:f32 = 480.0;
 /// Height of searcher panel in pixels.
 ///
 /// Because we don't implement clipping yet, the best UX is when searcher height is almost multiple
-/// of entry height.
-pub const SEARCHER_HEIGHT:f32 = 179.5;
+/// of entry height + padding.
+pub const SEARCHER_HEIGHT:f32 = 184.5;
 
 const ACTION_LIST_GAP     : f32 = 180.0;
 const LIST_DOC_GAP        : f32 = 15.0;
