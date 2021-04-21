c21694a7e71d9da19ab05d3dd2a29a86db1d1319
Radosław Waśko <radoslaw.wasko@enso.org>
2021-04-21 16:46:12 +0200

    Fix changelog diff

--- tools/ci/check-changelog.js
+++ tools/ci/check-changelog.js
@@ -6,15 +6,24 @@ const changelogPath = process.argv[2];
 const baseRef = process.argv[3];
 
 function wasChangelogModified() {
-  const diffArgs = ["--no-pager", "diff", "--exit-code", ref, changelogPath];
-  const exitCode = proc.spawnSync("git", diffArgs).status;
+  const diffArgs = [
+    "--no-pager",
+    "diff",
+    "--exit-code",
+    baseRef,
+    "--",
+    changelogPath,
+  ];
+  const result = proc.spawnSync("git", diffArgs);
+  const exitCode = result.status;
+  console.log(result.stdout.toString("utf-8"));
   const noDifference = exitCode == 0;
   return !noDifference;
 }
 
 function isChangelogSkipped() {
   const logArgs = ["--no-pager", "log", "HEAD~3...HEAD", "--pretty=oneline"];
-  const output = proc.spawnSync("git", diffArgs).stdout.toString("utf-8");
+  const output = proc.spawnSync("git", logArgs).stdout.toString("utf-8");
   const containsSkipCommit = output.indexOf(skipChangelogInfix) >= 0;
   return containsSkipCommit;
 }
