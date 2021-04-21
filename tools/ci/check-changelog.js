66804ba9effadd6452547971a970250a603f7ef1
Radosław Waśko <radoslaw.wasko@enso.org>
2021-04-21 15:11:07 +0200

    Formatting

--- tools/ci/check-changelog.js
+++ tools/ci/check-changelog.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const proc = require('child_process');
+const fs = require("fs");
+const proc = require("child_process");
 
-const skipChangelogInfix = "[no-changelog]"
+const skipChangelogInfix = "[no-changelog]";
 const changelogPath = process.argv[2];
 const baseRef = process.argv[3];
 
@@ -13,7 +13,7 @@ function wasChangelogModified() {
 }
 
 function isChangelogSkipped() {
-  const logArgs = ["--no-pager", "log", "HEAD~3...HEAD", "--pretty=oneline"]
+  const logArgs = ["--no-pager", "log", "HEAD~3...HEAD", "--pretty=oneline"];
   const output = proc.spawnSync("git", diffArgs).stdout.toString("utf-8");
   const containsSkipCommit = output.indexOf(skipChangelogInfix) >= 0;
   return containsSkipCommit;
@@ -25,7 +25,11 @@ if (wasChangelogModified()) {
 } else {
   console.log("No changes to the changelog");
   if (isChangelogSkipped()) {
-    console.log("But one of the commits within the PR includes " + skipChangelogInfix + ", so the check is skipped.");
+    console.log(
+      "But one of the commits within the PR includes " +
+        skipChangelogInfix +
+        ", so the check is skipped."
+    );
     process.exit(0);
   } else {
     process.exit(1);
