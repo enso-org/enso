0a77f8b5e687758889b8c607c56643ec6859ee9a
Ara Adkins <me@ara.io>
2021-05-06 15:41:37 +0100

    Add a test file that checks if the example methods execute

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/GetQualifiedTypeNameNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/GetQualifiedTypeNameNode.java
@@ -1,6 +1,7 @@
 package org.enso.interpreter.node.expression.builtin.meta;
 
 import com.oracle.truffle.api.nodes.Node;
+import org.enso.interpreter.dsl.AcceptsError;
 import org.enso.interpreter.dsl.BuiltinMethod;
 import org.enso.interpreter.runtime.data.text.Text;
 import org.enso.interpreter.runtime.type.Types;
@@ -10,7 +11,7 @@ import org.enso.interpreter.runtime.type.Types;
     name = "get_qualified_type_name",
     description = "Returns a qualified type name of the given value.")
 public class GetQualifiedTypeNameNode extends Node {
-  Text execute(Object _this, Object value) {
+  Text execute(@AcceptsError Object _this, Object value) {
     var typeName = Types.getName(value);
     return Text.create(typeName);
   }
