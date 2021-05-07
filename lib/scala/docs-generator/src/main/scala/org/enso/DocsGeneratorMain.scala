290bb68ec2b449c5d9b6544b25ec74bdea4ffadb
BinarySoftware <mkmikolajek@icloud.com>
2021-05-07 17:37:27 +0200

    restructure

--- lib/scala/docs-generator/src/main/scala/org/enso/DocsGeneratorMain.scala
+++ lib/scala/docs-generator/src/main/scala/org/enso/DocsGeneratorMain.scala
@@ -1,66 +1,55 @@
 package org.enso
 
 import java.io._
-
 import scala.util.Using
 import scala.io.Source
 import scalatags.Text.{all => HTML}
 import HTML._
+import docsgenerator.DocsGenerator._
+import docsgenerator.TreeOfCommonPrefixes._
 
 /** The Docs Generator script.
   * Used to create HTML documentation for Enso Standard Library, and also JS
   * files with react components for the "component reference" website.
   */
 object DocsGeneratorMain extends App {
-  import docsgenerator.DocsGenerator._
-  import docsgenerator.TreeOfCommonPrefixes._
 
   /// Files
   val path = "./distribution/std-lib/Standard/src"
-  val allFiles = traverse(new File(path))
-    .filter(f => f.isFile && f.getName.endsWith(".enso"))
-  val allFileNames =
-    allFiles.map(_.getPath.replace(path + "/", "").replace(".enso", ""))
-  val allPrograms =
-    allFiles
-      .map(f => Using(Source.fromFile(f, "UTF-8")) { s => s.mkString })
-      .toList
+  val allFiles = traverse(new File(path)).filter(f =>
+    f.isFile && f.getName.endsWith(".enso")
+  )
+  val allFileNames = allFiles.map(
+    _.getPath
+      .replace(path + "/", "")
+      .replace(".enso", "")
+  )
 
   /// HTML's w/o react & tree (for IDE)
-  val allDocs =
-    allPrograms
-      .map(s => generate(s.getOrElse("")))
-      .map(mapIfEmpty)
-      .map(removeUnnecessaryDivs)
+  val allPrograms = allFiles
+    .map(f => Using(Source.fromFile(f, "UTF-8")) { s => s.mkString })
+    .toList
+  val allDocs = allPrograms
+    .map(s => generate(s.getOrElse("")))
+    .map(mapIfEmpty)
+    .map(removeUnnecessaryDivs)
   val allDocFiles = allFiles.map(
-    _.getPath.replace(".enso", ".html").replace("Standard/src", "docs")
+    _.getPath
+      .replace(".enso", ".html")
+      .replace("Standard/src", "docs")
   )
   val zipped = allDocFiles.zip(allDocs)
-  zipped.foreach(x => {
-    val file = new File(x._1)
-    val dir  = new File(x._1.replaceAll("\\/[a-zA-Z_]*\\.[a-zA-Z]*", ""))
-    dir.mkdirs()
-    file.createNewFile();
-    val bw = new BufferedWriter(new FileWriter(file))
-    bw.write(x._2)
-    bw.close()
-  })
+  zipped.foreach(createDocHTMLFile)
 
   /// HTML's for syntax website.
-  val treeNames = groupByPrefix(allFileNames.toList).filter(_.elems.nonEmpty)
-  val jsTemplate = new File(
-    "./lib/scala/docs-generator/src/main/scala/org/enso/docsgenerator/template.js"
-  )
-  val templateCode = Using(Source.fromFile(jsTemplate, "UTF-8")) { s =>
-    s.mkString
-  }
-  val treeStyleFile = new File(
-    "./lib/scala/docs-generator/src/main/scala/org/enso/docsgenerator/treeStyle.css"
-  )
-  val treeStyleCode = Using(Source.fromFile(treeStyleFile, "UTF-8")) { s =>
-    s.mkString
-  }
-  val treeStyle = "<style jsx>{`" + treeStyleCode.getOrElse("") + "`}</style>"
+  val libPath =
+    "./lib/scala/docs-generator/src/main/scala/org/enso/docsgenerator/"
+  val treeNames    = groupByPrefix(allFileNames.toList).filter(_.elems.nonEmpty)
+  val jsTemplate   = new File(libPath + "template.js")
+  val templateCode = Using(Source.fromFile(jsTemplate, "UTF-8")) { _.mkString }
+  val styleFile    = new File(libPath + "treeStyle.css")
+  val styleCode    = Using(Source.fromFile(styleFile, "UTF-8")) { _.mkString }
+  val treeStyle    = "<style jsx>{`" + styleCode.getOrElse("") + "`}</style>"
   val allDocJSFiles = allFiles.map { x =>
     val outDir = "docs-js"
     val name = x.getPath
@@ -73,8 +62,30 @@ object DocsGeneratorMain extends App {
   val dir = new File(allDocJSFiles.head.split("docs-js").head + "docs-js/")
   dir.mkdirs()
   val zippedJS = allDocJSFiles.zip(allDocs)
-  zippedJS.foreach(x => {
-    val file = new File(x._1)
+  zippedJS.foreach(createDocJSFile)
+
+  /** Takes a tuple of file path and documented HTML code, saving the file
+    * in the given directory.
+    */
+  private def createDocHTMLFile(x: (String, String)): Unit = {
+    val path                   = x._1
+    val file                   = new File(path)
+    val fileWithExtensionRegex = "\\/[a-zA-Z_]*\\.[a-zA-Z]*"
+    val dir                    = new File(path.replaceAll(fileWithExtensionRegex, ""))
+    dir.mkdirs()
+    file.createNewFile();
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.write(x._2)
+    bw.close()
+  }
+
+  /** Takes a tuple of file path and documented HTML code, and generates JS doc
+    * file with react components for Enso website.
+    */
+  private def createDocJSFile(x: (String, String)): Unit = {
+    val path     = x._1
+    val htmlCode = x._2
+    val file     = new File(path)
     file.createNewFile();
     val bw = new BufferedWriter(new FileWriter(file))
     var treeCode =
@@ -82,11 +93,15 @@ object DocsGeneratorMain extends App {
         .ul(treeNames.map(_.html()))
         .render
         .replace("class=", "className=") + "</div>"
-    if (x._1.contains("/index.js")) {
+    if (path.contains("/index.js")) {
       treeCode = treeCode.replace("a href=\"", "a href=\"reference/")
     }
-    val partials =
-      x._1.split("docs-js/").tail.head.replace(".js", "").split("-")
+    val partials = path
+      .split("docs-js/")
+      .tail
+      .head
+      .replace(".js", "")
+      .split("-")
     for (i <- 1 to partials.length) {
       val id  = partials.take(i).mkString("-")
       val beg = "<input type=\"checkbox\" id=\"" + id + "\" "
@@ -97,7 +112,7 @@ object DocsGeneratorMain extends App {
         .getOrElse("")
         .replace(
           "{/*PAGE*/}",
-          x._2
+          htmlCode
             .replace("doc-copy-btn flex", "doc-copy-btn none")
             .replace("{", "&#123;")
             .replace("}", "&#125;")
@@ -105,5 +120,5 @@ object DocsGeneratorMain extends App {
         .replace("{/*BREADCRUMBS*/}", treeCode)
     )
     bw.close()
-  })
+  }
 }
