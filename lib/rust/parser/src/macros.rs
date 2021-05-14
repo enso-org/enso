b765dbfc76b05cf17edae1a3749abceb861c340e
Ara Adkins <me@ara.io>
2021-05-14 10:02:23 +0100

    Fix logging in lexer

--- lib/rust/parser/src/macros.rs
+++ lib/rust/parser/src/macros.rs
@@ -11,6 +11,13 @@ use crate::macros::definition::Definition;
 use crate::macros::registry::Registry;
 
 
+// ====================
+// === Type Aliases ===
+// ====================
+
+type DebugLevel = crate::prelude::logger::entry::level::Debug;
+
+
 
 // ================
 // === Resolver ===
@@ -25,7 +32,7 @@ pub struct Resolver<Logger> {
 }
 
 impl<Logger> Resolver<Logger>
-where Logger : AnyLogger<Owned=Logger> {
+where Logger : AnyLogger<Owned=Logger> + LoggerOps<DebugLevel> {
     /// Constructor.
     pub fn new(macros:Vec<Definition>, parent_logger:&Logger) -> Self {
         let logger   = <Logger>::sub(parent_logger,"Resolver");
