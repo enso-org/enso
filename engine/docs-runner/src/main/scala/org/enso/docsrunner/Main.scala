822d8fbc0ca94b9e95c9ba66f31fdbb36099a81e
BinarySoftware <mkmikolajek@icloud.com>
2021-06-02 20:03:58 +0200

    save

--- engine/docs-runner/src/main/scala/org/enso/docsrunner/Main.scala
+++ engine/docs-runner/src/main/scala/org/enso/docsrunner/Main.scala
@@ -1,11 +1,13 @@
 package org.enso.docsrunner
 
+import java.io.File
+import org.enso.interpreter.runtime.Module
 import org.enso.interpreter.runtime.{Context => EnsoContext}
 import org.enso.languageserver.io.{
   ObservableOutputStream,
   ObservablePipedInputStream
 }
-import org.enso.polyglot.{LanguageInfo, MethodNames}
+import org.enso.polyglot.{LanguageInfo, MethodNames, PolyglotContext}
 import org.graalvm.polyglot.Context
 
 object Main {
@@ -14,19 +16,27 @@ object Main {
   val stdInSink = new ObservableOutputStream
   val stdIn     = new ObservablePipedInputStream(stdInSink)
 
-  val executionContext = Context
-    .newBuilder()
-    .allowAllAccess(true)
-    .allowExperimentalOptions(true)
-    .out(stdOut)
-    .err(stdErr)
-    .in(stdIn)
-    .build()
+  def generateFrom(file: File): Unit = {
+    val executionContext = Context
+      .newBuilder()
+      .allowAllAccess(true)
+      .allowExperimentalOptions(true)
+      .out(stdOut)
+      .err(stdErr)
+      .in(stdIn)
+      .build()
 
-  val languageContext = executionContext
-    .getBindings(LanguageInfo.ID)
-    .invokeMember(MethodNames.TopScope.LEAK_CONTEXT)
-    .asHostObject[EnsoContext]
+    val polyCon = new PolyglotContext(executionContext)
 
-  // languageContext.getCompiler.generateDocs()
+    val polyModule = polyCon.evalModule(file)
+
+    val languageContext = executionContext
+      .getBindings(LanguageInfo.ID)
+      .invokeMember(MethodNames.TopScope.LEAK_CONTEXT)
+      .asHostObject[EnsoContext]
+
+    languageContext.getCompiler.generateDocs(
+      polyModule
+    ) // PolyModule => Module ?
+  }
 }
