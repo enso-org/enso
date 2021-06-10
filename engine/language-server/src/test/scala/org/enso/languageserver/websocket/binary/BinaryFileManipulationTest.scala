947d17f923637732dc159a10880b07a8e423bcef
Ara Adkins <me@ara.io>
2021-06-10 14:19:51 +0100

    Implement checksumBytes in the protocol

--- engine/language-server/src/test/scala/org/enso/languageserver/websocket/binary/BinaryFileManipulationTest.scala
+++ engine/language-server/src/test/scala/org/enso/languageserver/websocket/binary/BinaryFileManipulationTest.scala
@@ -8,6 +8,7 @@ import org.enso.testkit.FlakySpec
 
 import java.io.File
 import java.nio.ByteBuffer
+import java.nio.file.Files
 import java.security.MessageDigest
 import java.util.UUID
 import scala.io.Source
@@ -118,11 +119,13 @@ class BinaryFileManipulationTest extends BaseBinaryServerTest with FlakySpec {
       val digest = payload.checksum().bytesAsByteBuffer()
 
       digest shouldEqual expectedChecksum
+
+      Files.delete(barFile.toPath)
     }
 
     "Return a `FileNotFound` error if the file does not exist" in {
       val requestId = UUID.randomUUID()
-      val filename  = "bar.bin"
+      val filename  = "does_not_exist.bin"
 
       val client = newWsClient()
       client.send(createSessionInitCmd())
@@ -148,11 +151,64 @@ class BinaryFileManipulationTest extends BaseBinaryServerTest with FlakySpec {
     }
 
     "Return a `ReadOutOfBounds` error if the byte range is out of bounds" in {
-      pending
+      val requestId = UUID.randomUUID()
+      val filename  = "bar.bin"
+      val barFile   = new File(testContentRoot.toFile, filename)
+      val contents  = Array[Byte](65, 66, 67) //ABC
+      FileUtils.writeByteArrayToFile(barFile, contents)
+
+      val client = newWsClient()
+      client.send(createSessionInitCmd())
+      client.expectFrame()
+      val checksumBytesCmd = createChecksumBytesCommandPacket(
+        requestId,
+        Seq(filename),
+        testContentRootId,
+        byteOffset = 3,
+        length     = 2
+      )
+
+      client.send(checksumBytesCmd)
+      val checksumResponse = client
+        .receiveMessage[OutboundMessage]()
+        .getOrElse(fail("Should be right"))
+      checksumResponse.payloadType() shouldEqual OutboundPayload.ERROR
+      val payload = checksumResponse
+        .payload(new Error)
+        .asInstanceOf[Error]
+      payload.code() shouldEqual 1009
+      payload.message() shouldEqual "Read is out of bounds for the file"
+      payload.dataType() shouldEqual ErrorPayload.READ_OOB
+      val data = payload
+        .data(new ReadOutOfBoundsError)
+        .asInstanceOf[ReadOutOfBoundsError]
+      data.fileLength() shouldEqual 3
     }
 
     "Return a `NotFile` error if the provided path is not a file" in {
-      pending
+      val requestId = UUID.randomUUID()
+
+      val client = newWsClient()
+      client.send(createSessionInitCmd())
+      client.expectFrame()
+      val checksumBytesCmd = createChecksumBytesCommandPacket(
+        requestId,
+        Seq(),
+        testContentRootId,
+        byteOffset = 3,
+        length     = 2
+      )
+
+      client.send(checksumBytesCmd)
+      val checksumResponse = client
+        .receiveMessage[OutboundMessage]()
+        .getOrElse(fail("Should be right"))
+      checksumResponse.payloadType() shouldEqual OutboundPayload.ERROR
+      val payload = checksumResponse
+        .payload(new Error)
+        .asInstanceOf[Error]
+      payload.code() shouldEqual 1007
+      payload.message() shouldEqual "Path is not a file"
     }
   }
 
