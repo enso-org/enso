289da0b095f9668fa2cf7bbf234e09ed8217c442
Radosław Waśko <radoslaw.wasko@enso.org>
2021-06-16 18:55:56 +0200

    Implement the base server and docs

---
+++ tools/simple-library-server/main.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+const express = require("express");
+const yargs = require("yargs");
+
+const argv = yargs
+  .usage(
+    "$0",
+    "Allows to host Enso libraries and editions from the local filesystem through HTTP."
+  )
+  .option("port", {
+    description: "The port to listen on.",
+    type: "number",
+    default: 8080,
+  })
+  .option("root", {
+    description:
+      "The root of the repository. It should contain a `libraries` or `editions` directory. See the documentation for more details.",
+    type: "string",
+    default: ".",
+  })
+  .help()
+  .alias("help", "h").argv;
+
+console.log(
+  `Serving the repository located under ${argv.root} on port ${argv.port}.`
+);
+
+const app = express();
+app.use(express.static(argv.root));
+app.listen(argv.port);
