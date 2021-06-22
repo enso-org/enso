382a51f6b2bd80772dbe72a4ce8cd7a6e391da0d
Adam Obuchowicz <adam.obuchowicz@luna-lang.org>
2021-06-22 14:04:30 +0200

    Alive Message to Mixpanel with flag (https://github.com/enso-org/ide/pull/1637)
    Original commit: https://github.com/enso-org/ide/commit/c1ae1876fe4964d8ea706c15fcb8177ebeb022d8

--- ide/src/rust/ide/src/ide.rs
+++ ide/src/rust/ide/src/ide.rs
@@ -7,7 +7,11 @@ use crate::prelude::*;
 use crate::controller::project::INITIAL_MODULE_NAME;
 use crate::ide::integration::Integration;
 
+use analytics::AnonymousData;
+use enso_frp as frp;
 use ensogl::application::Application;
+use ensogl::system::web::sleep;
+use std::time::Duration;
 
 pub use initializer::Initializer;
 
@@ -20,6 +24,8 @@ pub use initializer::Initializer;
 /// Text that shows up in the statusbar when any of the backend connections is lost.
 pub const BACKEND_DISCONNECTED_MESSAGE:&str =
     "Connection to the backend has been lost. Please try restarting IDE.";
+
+const ALIVE_LOG_INTERVAL_SEC:u64 = 60;
 
 
 
@@ -35,6 +41,7 @@ pub const BACKEND_DISCONNECTED_MESSAGE:&str =
 pub struct Ide {
     application : Application,
     integration : Integration,
+    network     : frp::Network,
 }
 
 impl Ide {
@@ -43,7 +50,39 @@ impl Ide {
     (application:Application, view:ide_view::project::View, controller:controller::Ide)
     -> Self {
         let integration = integration::Integration::new(controller,view);
-        Ide {application,integration}
+        let network     = frp::Network::new("Ide");
+        Ide {application,integration,network} . init()
+    }
+
+    fn init(self) -> Self {
+        executor::global::spawn(self.alive_log_sending_loop());
+        self
+    }
+
+    fn alive_log_sending_loop(&self) -> impl Future<Output=()> + 'static {
+        let network  = &self.network;
+        let scene    = self.application.display.scene();
+        let mouse    = &scene.mouse.frp;
+        let keyboard = &scene.keyboard.frp;
+
+        enso_frp::extend! { TRACE_ALL network
+            on_log_sent          <- source::<()>();
+            mouse_moved          <- mouse.position.constant(());
+            any_mouse_press      <- any(mouse.up,mouse.down).constant(());
+            any_mouse_event      <- any(any_mouse_press,mouse_moved,mouse.wheel);
+            any_keyboard_event   <- any(keyboard.down,keyboard.up).constant(());
+            any_input_event      <- any(any_mouse_event,any_keyboard_event);
+            // True if any input event was captured since the last "alive" log sending.
+            input_event_received <- bool(&on_log_sent,&any_input_event).sampler();
+        }
+        async move {
+            loop {
+                let value = AnonymousData(input_event_received.value());
+                analytics::remote_log_value("alive", "input_event_received",value);
+                on_log_sent.emit(());
+                sleep(Duration::from_secs(ALIVE_LOG_INTERVAL_SEC)).await;
+            }
+        }
     }
 }
 
