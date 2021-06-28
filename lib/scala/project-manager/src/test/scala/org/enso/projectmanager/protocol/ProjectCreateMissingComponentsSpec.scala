732e5c57802374d8511cf4ce7d2a5a013c96942f
Radosław Waśko <radoslaw.wasko@enso.org>
2021-06-28 19:02:43 +0200

    Add tests for the fix, changelog

--- lib/scala/project-manager/src/test/scala/org/enso/projectmanager/protocol/ProjectCreateMissingComponentsSpec.scala
+++ lib/scala/project-manager/src/test/scala/org/enso/projectmanager/protocol/ProjectCreateMissingComponentsSpec.scala
@@ -1,43 +1,6 @@
 package org.enso.projectmanager.protocol
 
-import io.circe.Json
-import io.circe.literal.JsonStringContext
-import nl.gn0s1s.bump.SemVer
-import org.enso.projectmanager.data.MissingComponentAction
-import org.enso.projectmanager.{BaseServerSpec, ProjectManagementOps}
-import org.enso.testkit.RetrySpec
-import org.enso.editions.SemVerJson._
-
-class ProjectCreateMissingComponentsSpec
-    extends BaseServerSpec
-    with RetrySpec
-    with ProjectManagementOps
-    with MissingComponentBehavior {
-  override def buildRequest(
-    version: SemVer,
-    missingComponentAction: MissingComponentAction
-  ): Json =
-    json"""
-        { "jsonrpc": "2.0",
-          "method": "project/create",
-          "id": 1,
-          "params": {
-            "name": "Testproj",
-            "missingComponentAction": $missingComponentAction,
-            "version": $version
-          }
-        }
-        """
-
-  override def isSuccess(json: Json): Boolean = {
-    val projectId = for {
-      obj    <- json.asObject
-      result <- obj("result").flatMap(_.asObject)
-      id     <- result("projectId")
-    } yield id
-    projectId.isDefined
-  }
-
+class ProjectCreateMissingComponentsSpec extends ProjectCreateSpecBase {
   "project/create" should {
     behave like correctlyHandleMissingComponents()
   }
