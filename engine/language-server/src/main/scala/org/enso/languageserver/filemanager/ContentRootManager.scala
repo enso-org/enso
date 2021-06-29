67826d27d225140d43bfb1d0fda32791dd6f193a
Radosław Waśko <radoslaw.wasko@enso.org>
2021-06-29 17:49:37 +0200

    cr: add param comments

--- engine/language-server/src/main/scala/org/enso/languageserver/filemanager/ContentRootManager.scala
+++ engine/language-server/src/main/scala/org/enso/languageserver/filemanager/ContentRootManager.scala
@@ -13,12 +13,26 @@ import scala.concurrent.{ExecutionContext, Future}
   */
 trait ContentRootManager {
 
-  /** Lists the content roots currently available. */
+  /** Lists the content roots currently available.
+    *
+    * @param ec the execution context on which to wait for the reply from the
+    *           content root manager
+    * @return a future that will complete with a list of currently available
+    *         content roots
+    */
   def getContentRoots(implicit
     ec: ExecutionContext
   ): Future[List[ContentRootWithFile]]
 
-  /** Finds the content root with the given id. */
+  /** Finds the content root with the given id.
+    *
+    * @param id the id of the content root to get
+    * @param ec the execution context on which to wait for the reply from the
+    *           content root manager
+    * @return a future that will complete either with the found content root or
+    *         with an error if a content root with the requested id was not
+    *         found
+    */
   def findContentRoot(id: UUID)(implicit
     ec: ExecutionContext
   ): Future[Either[ContentRootNotFound.type, ContentRootWithFile]]
@@ -29,6 +43,12 @@ trait ContentRootManager {
     * The implementation should pick the most specific content root for the
     * path, i.e. the one whose root location shares the longest common prefix
     * with the path that is being resolved.
+    *
+    * @param path the path to convert
+    * @param ec the execution context on which to wait for the reply from the
+    *           content root manager
+    * @return a future that will complete with the relativized path or None if
+    *         the path was not relative to any of the available roots
     */
   def findRelativePath(path: File)(implicit
     ec: ExecutionContext
