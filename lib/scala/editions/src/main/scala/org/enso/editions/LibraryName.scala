8d71145d57ca1c5d496b6cf2a02f438358edd9c6
Radosław Waśko <radoslaw.wasko@enso.org>
2021-07-05 11:23:29 +0200

    Add File System Path to the Content Roots (#1827)

--- lib/scala/editions/src/main/scala/org/enso/editions/LibraryName.scala
+++ lib/scala/editions/src/main/scala/org/enso/editions/LibraryName.scala
@@ -4,14 +4,16 @@ import io.circe.{Decoder, DecodingFailure}
 
 /** Represents a library name that should uniquely identify the library.
   *
-  * The prefix is either a special prefix or a username.
+  * @param namespace library's namespace - either a special reserved prefix or
+  *                  the username of the main author
+  * @param name library's name
   */
-case class LibraryName(prefix: String, name: String) {
+case class LibraryName(namespace: String, name: String) {
 
   /** The qualified name of the library consists of its prefix and name
     * separated with a dot.
     */
-  def qualifiedName: String = s"$prefix.$name"
+  def qualifiedName: String = s"$namespace.$name"
 
   /** @inheritdoc */
   override def toString: String = qualifiedName
