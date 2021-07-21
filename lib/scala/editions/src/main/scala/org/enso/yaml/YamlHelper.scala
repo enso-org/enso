43d129b0f228da9c75204487bb7dc16f44e254a4
Radosław Waśko <radoslaw.wasko@enso.org>
2021-07-21 20:48:57 +0200

    WIP uploading packages

--- lib/scala/editions/src/main/scala/org/enso/yaml/YamlHelper.scala
+++ lib/scala/editions/src/main/scala/org/enso/yaml/YamlHelper.scala
@@ -1,6 +1,11 @@
 package org.enso.yaml
 
-import io.circe.{yaml, Decoder}
+import io.circe.yaml.Printer
+import io.circe.{yaml, Decoder, Encoder}
+
+import java.io.FileReader
+import java.nio.file.Path
+import scala.util.{Try, Using}
 
 /** A helper for parsing YAML configs. */
 object YamlHelper {
@@ -14,4 +19,15 @@ object YamlHelper {
       .flatMap(_.as[R])
       .left
       .map(ParseError(_))
+
+  def load[R](path: Path)(implicit decoder: Decoder[R]): Try[R] =
+    Using(new FileReader(path.toFile)) { reader =>
+      yaml.parser
+        .parse(reader)
+        .flatMap(_.as[R])
+        .toTry
+    }.flatten
+
+  def toYaml[A](obj: A)(implicit encoder: Encoder[A]): String =
+    Printer.spaces2.copy(preserveOrder = true).pretty(encoder(obj))
 }
