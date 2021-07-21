3e5f30bc272293278a4b5c3d31c824436ee8e5c8
Radosław Waśko <radoslaw.wasko@enso.org>
2021-07-21 19:19:10 +0200

    checkpoint

---
+++ lib/scala/library-manager/src/main/scala/org/enso/libraryupload/LibraryUploader.scala
@@ -0,0 +1,10 @@
+package org.enso.libraryupload
+
+import java.nio.file.Path
+import scala.util.Try
+
+object LibraryUploader {
+  def uploadLibrary(projectRoot: Path, authToken: auth.Token): Try[Unit] = Try {
+    ???
+  }
+}
