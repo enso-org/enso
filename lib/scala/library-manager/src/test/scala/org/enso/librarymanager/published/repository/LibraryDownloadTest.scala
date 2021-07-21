ce6b12f3aaed1cadc01adc536b101c7b056070ed
Radosław Waśko <radoslaw.wasko@enso.org>
2021-07-21 20:50:57 +0200

    Release locks to ensure temporary directory may be cleared

--- lib/scala/library-manager/src/test/scala/org/enso/librarymanager/published/repository/LibraryDownloadTest.scala
+++ lib/scala/library-manager/src/test/scala/org/enso/librarymanager/published/repository/LibraryDownloadTest.scala
@@ -35,64 +35,69 @@ class LibraryDownloadTest
       val lockManager =
         new ThreadSafeFileLockManager(getTestDirectory.resolve("locks"))
       val resourceManager = new ResourceManager(lockManager)
-      val cache = new DownloadingLibraryCache(
-        cacheRoot = getTestDirectory.resolve("cache"),
-        temporaryDirectoryManager = new TemporaryDirectoryManager(
-          getTestDirectory.resolve("tmp"),
-          resourceManager
-        ),
-        resourceManager = resourceManager,
-        lockUserInterface = new LockUserInterface {
-          override def startWaitingForResource(resource: Resource): Unit =
-            println(s"Waiting for ${resource.name}")
-
-          override def finishWaitingForResource(resource: Resource): Unit =
-            println(s"${resource.name} is ready")
-        },
-        progressReporter = new ProgressReporter {
-          override def trackProgress(
-            message: String,
-            task: TaskProgress[_]
-          ): Unit = {}
-        }
-      )
-
-      val server = repo.startServer(port, repoRoot)
       try {
-        cache.findCachedLibrary(
-          repo.testLib.libraryName,
-          repo.testLib.version
-        ) shouldBe empty
+        val cache = new DownloadingLibraryCache(
+          cacheRoot = getTestDirectory.resolve("cache"),
+          temporaryDirectoryManager = new TemporaryDirectoryManager(
+            getTestDirectory.resolve("tmp"),
+            resourceManager
+          ),
+          resourceManager = resourceManager,
+          lockUserInterface = new LockUserInterface {
+            override def startWaitingForResource(resource: Resource): Unit =
+              println(s"Waiting for ${resource.name}")
 
-        val (libPath, logs) = TestLogger.gatherLogs {
-          cache
-            .findOrInstallLibrary(
-              repo.testLib.libraryName,
-              repo.testLib.version,
-              Editions
-                .Repository("test_repo", s"http://localhost:$port/libraries")
-            )
-            .get
-        }
-        val pkg = PackageManager.Default.loadPackage(libPath.toFile).get
-        pkg.name shouldEqual "Bar"
-        val sources = pkg.listSources
-        sources should have size 1
-        sources.head.file.getName shouldEqual "Main.enso"
-        assert(
-          Files.notExists(libPath.resolve("LICENSE.md")),
-          "The license file should not exist as it was not provided " +
-          "in the repository."
+            override def finishWaitingForResource(resource: Resource): Unit =
+              println(s"${resource.name} is ready")
+          },
+          progressReporter = new ProgressReporter {
+            override def trackProgress(
+              message: String,
+              task: TaskProgress[_]
+            ): Unit = {}
+          }
         )
-        logs should contain(
-          TestLogMessage(
-            LogLevel.Warning,
-            "License file for library [Foo.Bar:1.0.0] was missing."
+
+        val server = repo.startServer(port, repoRoot)
+        try {
+          cache.findCachedLibrary(
+            repo.testLib.libraryName,
+            repo.testLib.version
+          ) shouldBe empty
+
+          val (libPath, logs) = TestLogger.gatherLogs {
+            cache
+              .findOrInstallLibrary(
+                repo.testLib.libraryName,
+                repo.testLib.version,
+                Editions
+                  .Repository("test_repo", s"http://localhost:$port/libraries")
+              )
+              .get
+          }
+          val pkg = PackageManager.Default.loadPackage(libPath.toFile).get
+          pkg.name shouldEqual "Bar"
+          val sources = pkg.listSources
+          sources should have size 1
+          sources.head.file.getName shouldEqual "Main.enso"
+          assert(
+            Files.notExists(libPath.resolve("LICENSE.md")),
+            "The license file should not exist as it was not provided " +
+            "in the repository."
           )
-        )
+          logs should contain(
+            TestLogMessage(
+              LogLevel.Warning,
+              "License file for library [Foo.Bar:1.0.0] was missing."
+            )
+          )
+        } finally {
+          server.kill(killDescendants    = true)
+          server.join(waitForDescendants = true)
+        }
       } finally {
-        server.kill(killDescendants    = true)
-        server.join(waitForDescendants = true)
+        resourceManager.releaseMainLock()
+        resourceManager.unlockTemporaryDirectory()
       }
     }
   }
