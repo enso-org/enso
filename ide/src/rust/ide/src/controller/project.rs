ab80b1f7d1dab6d4611c17dc7139a513494378c8
Maciej Mikołajek <mkmikolajek@icloud.com>
2021-08-20 10:41:16 +0200

    Bump Engine version to 0.2.26 and edition to 2021.14 (https://github.com/enso-org/ide/pull/1801)
    Original commit: https://github.com/enso-org/ide/commit/6b384196e0194ce7e92c39dcfc644b0b047da681

--- ide/src/rust/ide/src/controller/project.rs
+++ ide/src/rust/ide/src/controller/project.rs
@@ -25,14 +25,14 @@ pub const COMPILING_STDLIB_LABEL:&str = "Compiling standard library. It can take
 
 /// The requirements for Engine's version, in format understandable by
 /// [`semver::VersionReq::parse`].
-pub const ENGINE_VERSION_SUPPORTED        : &str = "^0.2.24";
+pub const ENGINE_VERSION_SUPPORTED        : &str = "^0.2.26";
 
 /// The Engine version used in projects created in IDE.
 // Usually it is a good idea to synchronize this version with the bundled Engine version in
 // src/js/lib/project-manager/src/build.ts. See also https://github.com/enso-org/ide/issues/1359
-pub const ENGINE_VERSION_FOR_NEW_PROJECTS : &str = "0.2.24";
+pub const ENGINE_VERSION_FOR_NEW_PROJECTS : &str = "0.2.26";
 /// The minimum edition that is guaranteed to work with the IDE.
-pub const MINIMUM_EDITION_SUPPORTED : &str = "2021.12";
+pub const MINIMUM_EDITION_SUPPORTED : &str = "2021.14";
 
 /// The name of the module initially opened in the project view.
 ///
