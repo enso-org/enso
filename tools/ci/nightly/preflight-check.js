e3fb88593dfd0ca39c565a162938520201b7c419
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2021-11-05 18:58:34 +0100

    Prettier

--- tools/ci/nightly/preflight-check.js
+++ tools/ci/nightly/preflight-check.js
@@ -1,69 +1,67 @@
-const fs = require("fs");
-const github = require("./github");
+const fs = require('fs')
+const github = require('./github')
 
-const currentHeadSha = process.argv[2];
-const buildConfigPath = "../../../build.sbt";
+const currentHeadSha = process.argv[2]
+const buildConfigPath = '../../../build.sbt'
 
 /// Returns the current date formatted as 'YYYY-mm-dd'.
 function isoDate() {
-  const now = new Date();
-  const year = "" + now.getFullYear();
-  let month = "" + (now.getMonth() + 1);
-  let day = "" + now.getDate();
-  if (month.length < 2) {
-    month = "0" + month;
-  }
-  if (day.length < 2) {
-    day = "0" + day;
-  }
-  return year + "-" + month + "-" + day;
+    const now = new Date()
+    const year = '' + now.getFullYear()
+    let month = '' + (now.getMonth() + 1)
+    let day = '' + now.getDate()
+    if (month.length < 2) {
+        month = '0' + month
+    }
+    if (day.length < 2) {
+        day = '0' + day
+    }
+    return year + '-' + month + '-' + day
 }
 
 /// Sets the step output 'proceed'.
 function setProceed(proceed) {
-  console.log("::set-output name=proceed::" + proceed);
+    console.log('::set-output name=proceed::' + proceed)
 }
 
 /// Sets the step output 'nightly-version'.
 function setVersionString(name) {
-  console.log("::set-output name=nightly-version::" + name);
+    console.log('::set-output name=nightly-version::' + name)
 }
 
 /// Sets the step output 'nightly-edition'.
 function setEditionName(name) {
-  console.log("::set-output name=nightly-edition::" + name);
+    console.log('::set-output name=nightly-edition::' + name)
 }
 
 /** Checks if there are any new changes to see if the nightly build should
  * proceed.
  */
 function checkProceed(nightlies) {
-  if (nightlies.length == 0) {
-    console.log(
-      "No prior nightly releases found. Proceeding with the first release."
-    );
-    return true;
-  }
+    if (nightlies.length == 0) {
+        console.log('No prior nightly releases found. Proceeding with the first release.')
+        return true
+    }
 
-  const first = nightlies[0];
-  const firstNightlySha = first.target_commitish;
-  if (firstNightlySha == currentHeadSha) {
-    console.log(
-      "Current commit (" +
-        currentHeadSha +
-        ") is the same as for the most recent nightly build. A new build is not needed."
-    );
-    return false;
-  } else {
-    console.log(
-      "Current commit (" +
-        currentHeadSha +
-        ") is different from the most recent nightly build (" +
-        firstNightlySha +
-        "). Proceeding with a new nightly build."
-    );
-    return true;
-  }
+    const first = nightlies[0]
+    const firstNightlySha = first.target_commitish
+    if (firstNightlySha == currentHeadSha) {
+        console.log(
+            'Current commit (' +
+                currentHeadSha +
+                ') is the same as for the most recent nightly build. A new build is not needed.'
+        )
+        return false
+    } else {
+        console.log(
+            'Current commit (' +
+                currentHeadSha +
+                ') is different from the most recent nightly build (' +
+                firstNightlySha +
+                '). Proceeding with a new nightly build.'
+        )
+        return true
+    }
 }
 
 /** Prepares a version string and edition name for the nightly build.
@@ -73,60 +71,60 @@ function checkProceed(nightlies) {
  * increasing numeric suffix is added.
  */
 function prepareVersions(nightlies) {
-  function isTaken(suffix) {
-    return nightlies.some((entry) => entry.tag_name.endsWith(suffix));
-  }
-
-  const content = fs.readFileSync(buildConfigPath, { encoding: "utf-8" });
-  const match = content.match(/val ensoVersion += +"(.*)"/);
-  if (!match) {
-    console.error("Could not find the version string in configuration!");
-    process.exit(1);
-  }
-
-  const version = match[1];
-  let baseName = version;
-  if (!baseName.endsWith("SNAPSHOT")) {
-    baseName += "-SNAPSHOT";
-  }
-
-  const now = isoDate();
-  function makeSuffix(ix) {
-    if (ix == 0) {
-      return now;
-    } else {
-      return now + "." + ix;
+    function isTaken(suffix) {
+        return nightlies.some(entry => entry.tag_name.endsWith(suffix))
     }
-  }
 
-  let ix = 0;
-  while (isTaken(makeSuffix(ix))) {
-    ix++;
-  }
+    const content = fs.readFileSync(buildConfigPath, { encoding: 'utf-8' })
+    const match = content.match(/val ensoVersion += +"(.*)"/)
+    if (!match) {
+        console.error('Could not find the version string in configuration!')
+        process.exit(1)
+    }
 
-  const suffix = makeSuffix(ix);
-  const versionName = baseName + "." + suffix;
-  const edition = "nightly-" + suffix;
-  console.log("The build will be using version '" + versionName + "'");
-  console.log("The build will be using edition '" + edition + "'");
-  return {
-    version: versionName,
-    edition: edition,
-  };
+    const version = match[1]
+    let baseName = version
+    if (!baseName.endsWith('SNAPSHOT')) {
+        baseName += '-SNAPSHOT'
+    }
+
+    const now = isoDate()
+    function makeSuffix(ix) {
+        if (ix == 0) {
+            return now
+        } else {
+            return now + '.' + ix
+        }
+    }
+
+    let ix = 0
+    while (isTaken(makeSuffix(ix))) {
+        ix++
+    }
+
+    const suffix = makeSuffix(ix)
+    const versionName = baseName + '.' + suffix
+    const edition = 'nightly-' + suffix
+    console.log("The build will be using version '" + versionName + "'")
+    console.log("The build will be using edition '" + edition + "'")
+    return {
+        version: versionName,
+        edition: edition,
+    }
 }
 
 async function main() {
-  const nightlies = await github.fetchNightlies();
-  const shouldProceed = checkProceed(nightlies);
-  setProceed(shouldProceed);
-  if (shouldProceed) {
-    const versions = prepareVersions(nightlies);
-    setVersionString(versions.version);
-    setEditionName(versions.edition);
-  }
+    const nightlies = await github.fetchNightlies()
+    const shouldProceed = checkProceed(nightlies)
+    setProceed(shouldProceed)
+    if (shouldProceed) {
+        const versions = prepareVersions(nightlies)
+        setVersionString(versions.version)
+        setEditionName(versions.edition)
+    }
 }
 
-main().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+main().catch(err => {
+    console.error(err)
+    process.exit(1)
+})
--- tools/ci/releases/is-broken.js
+++ tools/ci/releases/is-broken.js
@@ -1,39 +1,39 @@
 #!/usr/bin/env node
-const fs = require("fs");
+const fs = require('fs')
 let usage = `Usage: is-broken.js PATH
 
 Reads the release metadata at PATH (in JSON format) and checks if it contains
 the broken mark. Exit code 0 indicates that the release contains the broken
 mark. Other exit codes mean that the release either could not be loaded or is
 not marked broken. If the release is marked as broken, it also prints the URL to
-download the broken mark file.`;
+download the broken mark file.`
 
 if (process.argv.length != 3) {
-  console.log(usage);
-  process.exit(2);
+    console.log(usage)
+    process.exit(2)
 }
 
-let path = process.argv[2];
+let path = process.argv[2]
 
 function findBrokenMark(release) {
-  let assets = release["assets"];
-  return assets.find((asset) => asset["name"] == "broken");
+    let assets = release['assets']
+    return assets.find(asset => asset['name'] == 'broken')
 }
 
-fs.readFile(path, "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    process.exit(2);
-  }
+fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+        console.error(err)
+        process.exit(2)
+    }
 
-  let release = JSON.parse(data);
-  let mark = findBrokenMark(release);
-  if (mark) {
-    console.error("Release is marked as broken.");
-    console.log(mark["url"]);
-    process.exit(0);
-  } else {
-    console.error("Release is NOT marked as broken.");
-    process.exit(1);
-  }
-});
+    let release = JSON.parse(data)
+    let mark = findBrokenMark(release)
+    if (mark) {
+        console.error('Release is marked as broken.')
+        console.log(mark['url'])
+        process.exit(0)
+    } else {
+        console.error('Release is NOT marked as broken.')
+        process.exit(1)
+    }
+})
