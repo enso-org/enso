e36f6ff92699ac494426307d61304d21b772ccf6
Ilya Bogdanov <fumlead@gmail.com>
2021-11-25 12:41:18 +0300

    refactor: split SideMenu constructor into functions

--- app/gui/view/welcome-screen/src/side_menu.rs
+++ app/gui/view/welcome-screen/src/side_menu.rs
@@ -32,38 +32,32 @@ impl SideMenu {
         let logger = Logger::new_sub(logger, "SideMenu");
         let root_dom = web::create_element("aside");
         root_dom.set_class_name(crate::SIDE_MENU);
-
-
-        // === Header ===
-
-        let header = {
-            let header = web::create_element("h2");
-            header.set_text_content(Some("Your projects"));
-            header
-        };
+        let header = Self::create_header("Your projects");
         root_dom.append_or_warn(&header, &logger);
-
-
-        // === Projects list ===
-
-        let projects_list = {
-            let projects_list = web::create_element("ul");
-            projects_list.set_id("projects-list");
-            projects_list
-        };
-
-
-        // === New project button ===
-
-        let new_project_button = {
-            let button = web::create_element("li");
-            button.set_id("projects-list-new-project");
-            button.set_inner_html(r#"<img src="/assets/new-project.svg" />Create a new project"#);
-            projects_list.append_or_warn(&button, &logger);
-            button
-        };
+        let projects_list = Self::create_projects_list();
         root_dom.append_or_warn(&projects_list, &logger);
+        let new_project_button = Self::create_new_project_button(&logger, &projects_list);
 
         Self { logger, root_dom, projects_list, new_project_button, closures: default() }
+    }
+
+    fn create_header(text: &str) -> Element {
+        let header = web::create_element("h2");
+        header.set_text_content(Some(text));
+        header
+    }
+
+    fn create_projects_list() -> Element {
+        let projects_list = web::create_element("ul");
+        projects_list.set_id("projects-list");
+        projects_list
+    }
+
+    fn create_new_project_button(logger: &Logger, projects_list: &Element) -> Element {
+        let button = web::create_element("li");
+        button.set_id("projects-list-new-project");
+        button.set_inner_html(r#"<img src="/assets/new-project.svg" />Create a new project"#);
+        projects_list.append_or_warn(&button, logger);
+        button
     }
 }
