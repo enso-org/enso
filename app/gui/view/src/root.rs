4eb1fc02725d42afef75fb49161295459b35ee5f
Ilya Bogdanov <fumlead@gmail.com>
2021-12-06 12:05:09 +0300

    fix: trigger position update for Code Editor
    [finishes #180438664]

--- app/gui/view/src/root.rs
+++ app/gui/view/src/root.rs
@@ -83,11 +83,6 @@ impl Model {
     fn init_project_view(&self) {
         if self.project_view.get().is_none() {
             let view = self.app.new_view::<crate::project::View>();
-            // This is a quickfix for the bug #180438664.
-            {
-                view.code_editor().toggle();
-                view.code_editor().toggle();
-            }
             self.project_view.set(Some(view));
         }
     }
