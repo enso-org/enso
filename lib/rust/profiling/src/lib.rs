1cd3c14b42bef835fbdb777874a293b3dfc64191
Michael Mauderer <mail@michaelmauderer.de>
2021-12-13 10:13:14 +0000

    Update docs.

--- lib/rust/profiling/src/lib.rs
+++ lib/rust/profiling/src/lib.rs
@@ -12,21 +12,20 @@
 //! let some_task = || "DoWork";
 //!
 //! // Manually start and end the measurement.
-//! let gui_init = profiling::start_task("GUI initialization");
+//! let gui_init = profiling::start_task!("GUI initialization");
 //! some_task();
 //! gui_init.end();
 //! // Or use the `measure` method.
-//! profiling::task_measure("GUI initialization", some_task);
+//! profiling::task_measure!("GUI initialization", some_task);
 //! ```
 //!
 //! Note that this API and encoding formats for messages are synced with the JS equivalent in
 //! `app/ide-desktop/lib/profiling/src/profiling.ts`.
 //!
-//! Note on adding new profiling levels. Remember to
-//! * also update the JS API
-//! * add a new feature to this crate that can toggle the new profiling level to be on/off
-//! * update our build script to pass the toggle to the crate compilation (see
-//!   `build/run.js/make_profiling_feature_string`)
+//! Note on adding new profiling levelsThis is done via the
+//! `../../../../../app/ide-desktop/lib/profiling/src/profilers.json` config file. Just add a new
+//! profiler there and add a new feature to this crate that can toggle the new profiling level to be
+//! on/off.
 #![warn(missing_debug_implementations)]
 #![warn(missing_docs)]
 #![warn(trivial_casts)]
