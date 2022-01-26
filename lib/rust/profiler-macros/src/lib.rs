be08ade8b8d68438e5ee315f0c625c2cd25e31f1
Kaz Wesley <kaz@lambdaverse.org>
2022-01-26 06:18:25 -0800

    profiling instrumentation

---
+++ lib/rust/profiler-macros/src/lib.rs
@@ -0,0 +1,303 @@
+#![feature(proc_macro_span)]
+use inflector::Inflector;
+use quote::ToTokens;
+use std::env;
+use syn::parse::Parser;
+use syn::punctuated;
+
+
+// =========================
+// === define_hierarchy! ===
+// =========================
+
+#[allow(non_snake_case)]
+/// Define a profiler type for an enabled profiling level.
+fn enabled_profiler(snake_ident: &syn::Ident, ObjIdent: &syn::Ident) -> proc_macro::TokenStream {
+    let start = quote::format_ident!("start_{}", snake_ident);
+    let with_same_start = quote::format_ident!("{}_with_same_start", snake_ident);
+    let ts = quote::quote! {
+        // =================================
+        // === Profiler (e.g. Objective) ===
+        // =================================
+
+        #[derive(Copy, Clone, Debug)]
+        pub struct #ObjIdent(ProfilerId);
+
+
+        // === Trait Implementations ===
+
+        impl Profiler for #ObjIdent {
+            type Started = ProfilerData;
+            fn finish(self, data: &Self::Started) {
+                data.finish(self.0);
+            }
+        }
+
+
+        // === Constructor macros ===
+
+        #[macro_export]
+        macro_rules! #start {
+            ($parent: expr, $label: expr) => {{
+                use profiler::Parent;
+                let label = concat!($label, " (", file!(), ":", line!(), ")");
+                let profiler: profiler::Started<profiler::#ObjIdent> = $parent.new_child(label);
+                profiler
+            }}
+        }
+
+        #[macro_export]
+        macro_rules! #with_same_start {
+            ($parent: expr, $label: expr) => {{
+                use profiler::Parent;
+                let label = concat!($label, " (", file!(), ":", line!(), ")");
+                let profiler: profiler::Started<profiler::#ObjIdent> =
+                    $parent.new_child_same_start(label);
+                profiler
+            }}
+        }
+    };
+    ts.into()
+}
+
+/// Define a profiler type for a disabled profiling level.
+#[allow(non_snake_case)]
+fn disabled_profiler(snake_ident: &syn::Ident, ObjIdent: &syn::Ident) -> proc_macro::TokenStream {
+    let start = quote::format_ident!("start_{}", snake_ident);
+    let with_same_start = quote::format_ident!("{}_with_same_start", snake_ident);
+    let ts = quote::quote! {
+        // =================================
+        // === Profiler (e.g. Objective) ===
+        // =================================
+
+        #[derive(Copy, Clone, Debug)]
+        pub struct #ObjIdent(());
+
+
+        // === Trait Implementations ===
+
+        impl Profiler for #ObjIdent {
+            type Started = ();
+            fn finish(self, _: &Self::Started) {}
+        }
+
+
+        // === Constructor macros ===
+
+        #[macro_export]
+        macro_rules! #start {
+            ($parent: expr, $label: expr) => {
+                profiler::Started { profiler: profiler::#ObjIdent(()), data: () }
+            }
+        }
+        #[macro_export]
+        macro_rules! #with_same_start {
+            ($parent: expr, $label: expr) => {
+                profiler::Started { profiler: profiler::#ObjIdent(()), data: () }
+            }
+        }
+    };
+    ts.into()
+}
+
+/// impl Parent<T> for U, when U is a profiler of an enabled level.
+#[allow(non_snake_case)]
+fn enabled_impl_parent(
+    ParentIdent: &syn::Ident,
+    ChildIdent: &syn::Ident,
+) -> proc_macro::TokenStream {
+    let ts = quote::quote! {
+        impl Parent<#ChildIdent> for #ParentIdent {
+            fn new_child(&self, label:Label) -> Started<#ChildIdent> {
+                let profiler = #ChildIdent(ProfilerId::new());
+                let parent = self.0;
+                let start = Some(Timestamp::now());
+                let data = ProfilerData { parent, start, label };
+                Started { profiler, data }
+            }
+            fn new_child_same_start(&self, label:Label) -> Started<#ChildIdent> {
+                let profiler = #ChildIdent(ProfilerId::new());
+                let parent = self.0;
+                let start = None;
+                let data = ProfilerData { parent, start, label };
+                Started { profiler, data }
+            }
+        }
+    };
+    ts.into()
+}
+
+/// impl Parent<T> for U, when U is an profiler of a disabled level.
+#[allow(non_snake_case)]
+fn disabled_impl_parent(
+    ParentIdent: &syn::Ident,
+    ChildIdent: &syn::Ident,
+) -> proc_macro::TokenStream {
+    let ts = quote::quote! {
+        impl Parent<#ChildIdent> for #ParentIdent {
+            fn new_child(&self, label: Label) -> Started<#ChildIdent> {
+                self.new_child_same_start(label)
+            }
+            fn new_child_same_start(&self, _label: Label) -> Started<#ChildIdent> {
+                let profiler = #ChildIdent(());
+                let data = ();
+                Started { profiler, data }
+            }
+        }
+    };
+    ts.into()
+}
+
+/// Return the numeric Profiling Level (counting from 0 = top-level only).
+fn get_enabled_level(levels: &[impl AsRef<str>]) -> usize {
+    const PROFILING_LEVEL_ENV_VAR: &str = "ENSO_MAX_PROFILING_LEVEL";
+    let enabled = match env::var(PROFILING_LEVEL_ENV_VAR) {
+        Ok(level) => level,
+        // If the variable isn't set, we default to the minimum.
+        Err(_) => return 0,
+    };
+    for (i, name) in levels.iter().enumerate() {
+        if &enabled[..] == name.as_ref() {
+            return i;
+        }
+    }
+    panic!("{} set to unknown profiling level: {}", PROFILING_LEVEL_ENV_VAR, enabled);
+}
+
+/// Defines a hierarchy of profiler levels.
+#[proc_macro]
+#[allow(non_snake_case)]
+pub fn define_hierarchy(ts: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let parser = punctuated::Punctuated::<syn::Ident, syn::Token![,]>::parse_terminated;
+    let idents: Vec<_> = parser.parse(ts).unwrap().into_iter().collect();
+    let level_names: Vec<_> = idents.iter().map(|id| id.to_string().to_snake_case()).collect();
+    let enabled_level = get_enabled_level(&level_names);
+    let mut out = proc_macro::TokenStream::new();
+    for ((i, ObjIdent), level_name) in idents.iter().enumerate().zip(level_names.iter()) {
+        let snake_ident = syn::Ident::new(level_name, proc_macro2::Span::call_site());
+        let profiler_def = match enabled_level >= i {
+            true => enabled_profiler(&snake_ident, ObjIdent),
+            false => disabled_profiler(&snake_ident, ObjIdent),
+        };
+        out.extend(profiler_def);
+        for (j, ChildIdent) in idents[i..].iter().enumerate() {
+            let parent_impl = match enabled_level >= i + j {
+                true => enabled_impl_parent(ObjIdent, ChildIdent),
+                false => disabled_impl_parent(ObjIdent, ChildIdent),
+            };
+            out.extend(parent_impl);
+        }
+    }
+    out
+}
+
+
+
+// ================
+// === profile! ===
+// ================
+
+/// Instruments a function.
+#[proc_macro_attribute]
+pub fn profile(
+    args: proc_macro::TokenStream,
+    ts: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    assert!(args.is_empty(), "#[profile] does not expect any arguments");
+    let mut func = syn::parse_macro_input!(ts as syn::ItemFn);
+    let impl_name = &format!("__profiler_wrapped__{}", func.sig.ident);
+    let impl_ident = syn::Ident::new(impl_name, proc_macro2::Span::call_site());
+    let wrapper_ident = std::mem::replace(&mut func.sig.ident, impl_ident);
+    let wrapper = make_wrapper(wrapper_ident, &func);
+    func.vis = syn::Visibility::Inherited;
+
+    // Emit wrapped function and wrapper..
+    let mut out = proc_macro2::TokenStream::new();
+    wrapper.to_tokens(&mut out);
+    func.to_tokens(&mut out);
+    out.into()
+}
+
+fn make_wrapper(wrapper_ident: syn::Ident, func: &syn::ItemFn) -> proc_macro2::TokenStream {
+    let profiler_label = make_label(&wrapper_ident);
+
+    // Create the wrapper signature, based on the impl signature.
+    let vis = func.vis.clone();
+    let mut wrapper_sig = func.sig.clone();
+    wrapper_sig.ident = wrapper_ident;
+    let impl_ident = &func.sig.ident;
+
+    // Gather argument idents for call in wrapper
+    let args = forward_args(&wrapper_sig.inputs);
+
+    // Parse profiler argument in signature.
+    let last_arg = match wrapper_sig.inputs.last_mut().unwrap() {
+        syn::FnArg::Typed(pat_type) => pat_type,
+        _ => panic!("Last argument to function annotated with #[profile] must be a profiler type"),
+    };
+    let profiler_ident = last_arg.pat.clone();
+
+    // Replace profiler type `T `in sig with `impl Parent<T>`.
+    let profiler_type_in = last_arg.ty.clone();
+    let profiler_generic_parent = quote::quote! { impl profiler::Parent<#profiler_type_in> };
+    last_arg.ty = Box::new(syn::Type::Verbatim(profiler_generic_parent));
+
+    let call = match func.sig.asyncness {
+        None => quote::quote! { #impl_ident(#args) },
+        Some(_) => quote::quote! { #impl_ident(#args).await },
+    };
+    quote::quote! {
+        #vis #wrapper_sig {
+            let _profiler = #profiler_ident.new_child(#profiler_label);
+            let #profiler_ident = _profiler.profiler;
+            #call
+        }
+    }
+}
+
+/// Create a Measurement label, with its file:line info determined by the proc_macro's call site.
+fn make_label(ident: &syn::Ident) -> String {
+    let span = proc_macro::Span::call_site();
+    let file = span.source_file().path();
+    let path = file.as_path().to_string_lossy();
+    let line = span.start().line;
+    format!("{} ({}:{})", ident, path, line)
+}
+
+type SigInputs = punctuated::Punctuated<syn::FnArg, syn::Token![,]>;
+type CallArgs = punctuated::Punctuated<syn::Expr, syn::Token![,]>;
+
+// Given a function's inputs, produce the argument list the function would use to recurse with all
+// its original arguments--or call another function with the same signature.
+fn forward_args(inputs: &SigInputs) -> CallArgs {
+    let mut args = CallArgs::new();
+    for input in inputs {
+        args.push(match input {
+            syn::FnArg::Receiver(r) => ident_to_expr(r.self_token.into()),
+            syn::FnArg::Typed(pat_type) => pat_to_expr(&pat_type.pat),
+        });
+    }
+    args
+}
+
+fn pat_to_expr(pat: &syn::Pat) -> syn::Expr {
+    match pat {
+        syn::Pat::Ident(syn::PatIdent { ident, .. }) => ident_to_expr(ident.to_owned()),
+        syn::Pat::Reference(syn::PatReference { pat, mutability, .. }) => {
+            let expr = Box::new(pat_to_expr(pat));
+            let mutability = mutability.to_owned();
+            let attrs = Default::default();
+            let and_token = Default::default();
+            let raw = Default::default();
+            syn::ExprReference { expr, mutability, attrs, and_token, raw }.into()
+        }
+        _ => unimplemented!("destructuring-bind in signature of fn wrapped by #[profile]"),
+    }
+}
+
+fn ident_to_expr(ident: syn::Ident) -> syn::Expr {
+    let path = ident.into();
+    let attrs = Default::default();
+    let qself = Default::default();
+    syn::ExprPath { path, qself, attrs }.into()
+}
---
+++ lib/rust/profiler/src/lib.rs
@@ -0,0 +1,333 @@
+use std::cell;
+use std::num;
+use std::str;
+
+
+
+// =============
+// === Label ===
+// =============
+
+/// The label of a profiler; this includes the name given at its creation, along with file and
+/// line-number information.
+pub type Label = &'static str;
+
+
+
+// =================
+// === Timestamp ===
+// =================
+
+/// Time elapsed since the time origin (https://www.w3.org/TR/hr-time-2/#sec-time-origin).
+///
+/// Stored in units of 100us, because that is maximum resolution of performance.now():
+/// - in specification, 100us is the limit: https://www.w3.org/TR/hr-time-3
+/// - in practice, as observed in debug consoles: Chromium 97 (100us) and Firefox 95 (1ms)
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct Timestamp(num::NonZeroU64);
+
+impl Timestamp {
+    /// Return the current time, relative to the time origin.
+    pub fn now() -> Self {
+        Self::from_ms(js::performance::now())
+    }
+
+    /// Return the timestamp corresponding to an offset from the time origin, in ms.
+    pub fn from_ms(ms: f64) -> Self {
+        let ticks = (ms * 10.0).round() as u64;
+        // Safety: ticks + 1 will not be 0 unless a Timestamp wraps.
+        // It takes (2 ** 64) * 100us = 58_455_453 years for a Timestamp to wrap.
+        unsafe { Self(num::NonZeroU64::new_unchecked(ticks + 1)) }
+    }
+
+    /// Convert to an offset from the time origin, in ms.
+    pub fn into_ms(self) -> f64 {
+        (self.0.get() - 1) as f64 / 10.0
+    }
+}
+
+// === FFI ===
+
+pub mod js {
+    pub mod performance {
+        use wasm_bindgen::prelude::*;
+
+        #[wasm_bindgen]
+        extern "C" {
+            #[wasm_bindgen(js_namespace = performance)]
+            pub fn now() -> f64;
+        }
+    }
+}
+
+
+
+// ==================
+// === ProfilerId ===
+// ==================
+
+/// Uniquely identifies a runtime measurement.
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct ProfilerId(u32);
+
+impl ProfilerId {
+    fn new() -> Self {
+        thread_local! {
+            pub static NEXT_ID: cell::Cell<u32> = cell::Cell::new(1);
+        }
+        ProfilerId(NEXT_ID.with(|next_id| {
+            let id = next_id.get();
+            next_id.set(id + 1);
+            id
+        }))
+    }
+}
+
+/// Pseudo-profiler serving as the root of the measurement hierarchy.
+pub const APP_LIFETIME: Objective = Objective(ProfilerId(0));
+
+
+
+// =======================
+// === StartedProfiler ===
+// =======================
+
+/// The interface supported by profiler-data objects.
+pub trait StartedProfiler {
+    fn finish(&self, profiler: ProfilerId);
+}
+
+// === Implementation for disabled profilers ===
+
+impl StartedProfiler for () {
+    fn finish(&self, _: ProfilerId) {}
+}
+
+// ====================
+// === ProfilerData ===
+// ====================
+
+/// Data used by a started Measurement for an enabled profile level.
+pub struct ProfilerData {
+    pub parent: ProfilerId,
+    pub start:  Option<Timestamp>,
+    pub label:  Label,
+}
+
+// === Trait Implementations ===
+
+impl StartedProfiler for ProfilerData {
+    fn finish(&self, profiler: ProfilerId) {
+        let parent = self.parent;
+        let start = self.start;
+        let label = self.label;
+        let end = Timestamp::now();
+        let measurement = Measurement { parent, profiler, start, end, label };
+        crate::MEASUREMENTS.with(move |log| log.push(measurement));
+    }
+}
+
+
+
+// ===================
+// === Measurement ===
+// ===================
+
+/// Identifies a profiled section, the parent it was reached by, and its entry and exit times.
+pub struct Measurement {
+    pub parent:   ProfilerId,
+    pub profiler: ProfilerId,
+    pub start:    Option<Timestamp>,
+    pub end:      Timestamp,
+    pub label:    Label,
+}
+
+pub mod internal {
+    use crate::*;
+
+    use std::cell;
+    use std::mem;
+
+    // =======================
+    // === LocalVecBuilder ===
+    // =======================
+
+    /// Data structure supporting limited interior mutability, to build up a collection.
+    pub struct LocalVecBuilder<T>(cell::UnsafeCell<Vec<T>>);
+    impl<T> LocalVecBuilder<T> {
+        #[allow(clippy::new_without_default)]
+        /// Create a new, empty vec builder.
+        pub fn new() -> Self {
+            Self(cell::UnsafeCell::new(vec![]))
+        }
+
+        /// Push an element.
+        pub fn push(&self, element: T) {
+            // Note [LocalVecBuilder Safety]
+            unsafe {
+                (&mut *self.0.get()).push(element);
+            }
+        }
+
+        // Note [LocalVecBuilder Safety]
+        // =============================
+        // When obtaining a reference from the UnsafeCell, all accessors follow these rules:
+        // - There must be a scope that the reference doesn't escape.
+        // - There must be no other references obtained in the same scope.
+        // Consistently following these rules ensures the no-alias rule of mutable references is
+        // satisfied.
+
+        /// Return (and consume) all elements pushed so far.
+        pub fn build(&self) -> Vec<T> {
+            // Note [LocalVecBuilder Safety]
+            unsafe { mem::take(&mut *self.0.get()) }
+        }
+    }
+
+    // ====================
+    // === MEASUREMENTS ===
+    // ====================
+
+    thread_local! {
+        pub static MEASUREMENTS: LocalVecBuilder<Measurement> = LocalVecBuilder::new();
+    }
+}
+
+/// Global log of Measurements.
+pub use internal::MEASUREMENTS;
+
+/// Gather all measurements taken since the last time take_log() was called.
+pub fn take_log() -> Vec<Measurement> {
+    MEASUREMENTS.with(|log| log.build())
+}
+
+
+// ================
+// === Profiler ===
+// ================
+
+/// The interface supported by profilers of all profiling levels.
+pub trait Profiler: Copy {
+    type Started: StartedProfiler;
+    fn finish(self, data: &Self::Started);
+}
+
+
+
+// ==============
+// === Parent ===
+// ==============
+
+/// Any object representing a profiler that is a valid parent for a profiler of type T.
+pub trait Parent<T: Profiler> {
+    fn new_child(&self, label: Label) -> Started<T>;
+    fn new_child_same_start(&self, label: Label) -> Started<T>;
+}
+
+
+
+// ===============
+// === Started ===
+// ===============
+
+/// A profiler that has a start time set, and will complete its measurement when dropped.
+pub struct Started<T: Profiler> {
+    profiler: T,
+    data:     T::Started,
+}
+
+
+// === Trait Implementations ===
+
+impl<T: Profiler> Drop for Started<T> {
+    fn drop(&mut self) {
+        let profiler = self.profiler;
+        profiler.finish(&self.data);
+    }
+}
+
+impl<T, U> Parent<T> for Started<U>
+where
+    U: Parent<T> + Profiler,
+    T: Profiler,
+{
+    fn new_child(&self, label: Label) -> Started<T> {
+        self.profiler.new_child(label)
+    }
+
+    fn new_child_same_start(&self, label: Label) -> Started<T> {
+        self.profiler.new_child_same_start(label)
+    }
+}
+
+
+
+// ===================================
+// === profiler_macros Invocations ===
+// ===================================
+
+#[doc(inline)]
+pub use profiler_macros::profile;
+
+profiler_macros::define_hierarchy![Objective, Task, Detail, Debug];
+
+
+
+// =============
+// === Tests ===
+// =============
+
+#[cfg(test)]
+mod test {
+    use crate as profiler;
+    use profiler::profile;
+
+    use wasm_bindgen_test::wasm_bindgen_test;
+
+    #[wasm_bindgen_test]
+    fn root() {
+        {
+            // In any other crate we would refer to the macro as `profiler::start_objective!`, but
+            // "macro-expanded `macro_export` macros from the current crate cannot be referred to
+            // by absolute paths" (https://github.com/rust-lang/rust/issues/52234).
+            let _profiler = start_objective!(profiler::APP_LIFETIME, "test");
+        }
+        let measurements = profiler::take_log();
+        assert_eq!(measurements.len(), 1);
+        assert_eq!(measurements[0].parent, profiler::APP_LIFETIME.0);
+        assert!(measurements[0].label.starts_with("test "));
+        assert!(measurements[0].end >= measurements[0].start.unwrap());
+    }
+
+    #[wasm_bindgen_test]
+    fn with_same_start() {
+        {
+            let _profiler0 = start_objective!(profiler::APP_LIFETIME, "test0");
+            let _profiler1 = objective_with_same_start!(_profiler0, "test1");
+        }
+        let measurements = profiler::take_log();
+        assert_eq!(measurements.len(), 2);
+        // _profiler1 is with_same_start, indicated by None in the log
+        // Note: _profiler1 is _measurements[0] because position in the log is determined by end
+        // order, not start order.
+        assert_eq!(measurements[0].start, None);
+        // _profiler0 has a start time
+        assert!(measurements[1].start.is_some());
+    }
+
+    #[wasm_bindgen_test]
+    fn profile() {
+        #[profile]
+        fn profilee(_profiler: profiler::Objective) {}
+        profilee(profiler::APP_LIFETIME);
+        let measurements = profiler::take_log();
+        assert_eq!(measurements.len(), 1);
+    }
+
+    #[wasm_bindgen_test]
+    fn profile_async() {
+        #[profile]
+        async fn profilee(_profiler: profiler::Objective) {}
+        let _future = profilee(profiler::APP_LIFETIME);
+    }
+}
