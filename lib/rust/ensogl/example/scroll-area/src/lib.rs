969164d3e55092b1e2740bb04a4a5e2e836b1822
Mateusz Czapliński <mateusz.czaplinski@enso.org>
2022-02-10 11:33:19 +0100

    remove debugging code

--- lib/rust/ensogl/example/scroll-area/src/lib.rs
+++ lib/rust/ensogl/example/scroll-area/src/lib.rs
@@ -31,7 +31,6 @@ use ensogl_core::display::shape::ShapeSystem;
 use ensogl_core::display::Sprite;
 use ensogl_core::system::web;
 use ensogl_hardcoded_theme as theme;
-use ensogl_label::Label;
 use ensogl_scroll_area::ScrollArea;
 use ensogl_text_msdf_sys::run_once_initialized;
 
@@ -76,9 +75,9 @@ fn init(app: &Application) {
     let background_system = ShapeSystem::new(scene, background_shape);
     let background: Sprite = background_system.new_instance();
     scene.add_child(&background);
-    background.size.set(Vector2::new(300.0, 300.0));
-    background.set_position_x(150.0);
-    background.set_position_y(-150.0);
+    background.size.set(Vector2::new(200.0, 200.0));
+    background.set_position_x(100.0);
+    background.set_position_y(-100.0);
     std::mem::forget(background);
 
 
@@ -100,79 +99,6 @@ fn init(app: &Application) {
     sprite.set_position_x(100.0);
     sprite.set_position_y(-100.0);
     std::mem::forget(sprite);
-
-    let label0 = Label::new(app);
-    scroll_area.content.add_child(&label0);
-    label0.frp.set_content(
-        "FOO BAR BAZ\n\
-        SOME MORE CONTENT\n\
-        INSIDE SCROLLBAR HOPEFULLY");
-    std::mem::forget(label0);
-
-    let label = Label::new(app);
-    // TODO: 'app.display' or 'scene' ?
-    app.display.add_child(&label);
-    label.frp.set_content(
-        "Some relatively long text with no super important meaning that should be known,\n\
-        however, spanning multiple lines and hopefully also being vertically extensive enough to matter.\n\
-        line 3\n\
-        line 4\n\
-        line 5\n\
-        line 6\n\
-        line 7\n\
-        line 8\n\
-        line 9\n\
-        line 10");
-    std::mem::forget(label);
-
-
-
-
-    use ensogl_core::display::symbol::geometry::SpriteSystem;
-    let sprite_system = SpriteSystem::new(&app.display);
-    // let sprite_system = SpriteSystem::new(&world);
-
-    let sprite2 = sprite_system.new_instance();
-    let sprite1 = sprite_system.new_instance();
-    sprite1.size.set(Vector2::new(15.0, 15.0));
-    sprite2.size.set(Vector2::new(15.0, 15.0));
-    Leak::new(sprite1);
-    Leak::new(sprite2);
-
-    scroll_area.content.add_child(&sprite_system);
-    // scene.add_child(&sprite_system);
-
-
-
-    // let sprite_system = ShapeSystem::new(scene, &Circle(50.px()));
-    let circle = Circle(50.px()).fill(color::Rgb::new(1.0, 0.0, 0.0));
-    let sprite_system = ShapeSystem::new(&app.display, &circle);
-    // let sprite_system = ShapeSystem::new(&app.display, &Circle(50.px()));
-    let sprite: Sprite = sprite_system.new_instance();
-    // scene.add_child(&sprite);
-    app.display.add_child(&sprite);
-    sprite.size.set(Vector2::new(100.0, 100.0));
-    // sprite.set_position_x(100.0);
-    // sprite.set_position_y(-100.0);
-    std::mem::forget(sprite);
-
-
-
-    // based on shape-system demo:
-
-    let circle1 = Circle(50.px())
-        .translate_x(-(50.0.px()))
-        .fill(color::Rgb::new(1.0, 0.0, 0.0));
-    let sprite_system = ShapeSystem::new(scene, &circle1);
-    let sprite = sprite_system.new_instance();
-
-    sprite.size.set(Vector2::new(300.0, 300.0));
-    // sprite.set_position_x(100.0);
-    // sprite.set_position_y(-100.0);
-
-    scroll_area.content.add_child(&sprite_system);
-    Leak::new(sprite);
-
 
 
     std::mem::forget(scroll_area);
