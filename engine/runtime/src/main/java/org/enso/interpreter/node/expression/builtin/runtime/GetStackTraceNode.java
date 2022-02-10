50f19558e5979f972ca98b9f64b858823a0844cd
Marcin Kostrzewa <marckostrzewa@gmail.com>
2022-02-10 17:16:27 +0100

    Enso-level stacktraces

---
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/runtime/GetStackTraceNode.java
@@ -0,0 +1,28 @@
+package org.enso.interpreter.node.expression.builtin.runtime;
+
+import com.oracle.truffle.api.TruffleStackTrace;
+import com.oracle.truffle.api.nodes.Node;
+import org.enso.interpreter.dsl.BuiltinMethod;
+import org.enso.interpreter.node.callable.thunk.ThunkExecutorNode;
+import org.enso.interpreter.runtime.data.Array;
+import org.enso.interpreter.runtime.error.PanicException;
+
+@BuiltinMethod(
+    type = "Runtime",
+    name = "primitive_get_stack_trace",
+    description = "Gets the current execution stacktrace.")
+public class GetStackTraceNode extends Node {
+  private @Child ThunkExecutorNode thunkExecutorNode = ThunkExecutorNode.build();
+
+  Array execute(Object _this) {
+    var exception = new PanicException(null, this);
+    TruffleStackTrace.fillIn(exception);
+    var elements = TruffleStackTrace.getStackTrace(exception);
+    var ret = new Array(elements.size());
+    for (int i = 0; i < elements.size(); i++) {
+      var element = elements.get(i);
+      ret.getItems()[i] = element.getGuestObject();
+    }
+    return ret;
+  }
+}
---
+++ engine/runtime/src/main/java/org/enso/interpreter/runtime/data/EnsoSource.java
@@ -0,0 +1,44 @@
+package org.enso.interpreter.runtime.data;
+
+import com.oracle.truffle.api.source.Source;
+import com.oracle.truffle.api.source.SourceSection;
+
+public class EnsoSource {
+    private final Source source;
+
+    public EnsoSource(Source source) {
+        this.source = source;
+    }
+
+    public String getLanguage() {
+        return source.getLanguage();
+    }
+
+    public String getName() {
+        return source.getName();
+    }
+
+    public String getPath() {
+        return source.getPath();
+    }
+
+    public boolean isInternal() {
+        return source.isInternal();
+    }
+
+    public CharSequence getCharacters() {
+        return source.getCharacters();
+    }
+
+    public int getLength() {
+        return source.getLength();
+    }
+
+    public CharSequence getCharacters(int lineNumber) {
+        return source.getCharacters(lineNumber);
+    }
+
+    public int getLineCount() {
+        return source.getLineCount();
+    }
+}
