1b64766d200da8e49f423f3bbe441faad4e11dea
Ilya Bogdanov <fumlead@gmail.com>
2022-02-14 15:58:59 +0300

    finish implementation

--- lib/rust/ensogl/core/src/display/navigation/navigator.rs
+++ lib/rust/ensogl/core/src/display/navigation/navigator.rs
@@ -14,6 +14,18 @@ use events::ZoomEvent;
 
 
 
+// =================
+// === Constants ===
+// =================
+
+/// Default maximum zoom factor (100x). This value can be changed by
+/// [`NavigatorModel::set_max_zoom`].
+const MAX_ZOOM: f32 = 100.0;
+/// Default minimum zoom factor (0.15x).
+const MIN_ZOOM: f32 = 0.15;
+
+
+
 // ======================
 // === NavigatorModel ===
 // ======================
@@ -36,21 +48,27 @@ impl NavigatorModel {
     pub fn new(scene: &Scene, camera: &Camera2d) -> Self {
         let zoom_speed = Rc::new(Cell::new(Switch::On(10.0 / 1000.0)));
         let pan_speed = Rc::new(Cell::new(Switch::On(1.0)));
-        let min_zoom = 10.0;
-        let max_zoom = 10000.0;
         let disable_events = Rc::new(Cell::new(true));
         let max_zoom_limit = default();
         let (simulator, resize_callback, _events) = Self::start_navigator_events(
             scene,
             camera,
-            min_zoom,
-            max_zoom,
+            MIN_ZOOM,
+            MAX_ZOOM,
             Rc::clone(&zoom_speed),
             Rc::clone(&pan_speed),
             Rc::clone(&disable_events),
             Rc::clone(&max_zoom_limit),
         );
-        Self { _events, simulator, resize_callback, zoom_speed, pan_speed, disable_events, max_zoom_limit }
+        Self {
+            _events,
+            simulator,
+            resize_callback,
+            zoom_speed,
+            pan_speed,
+            disable_events,
+            max_zoom_limit,
+        }
     }
 
     fn create_simulator(camera: &Camera2d) -> physics::inertia::DynSimulator<Vector3> {
@@ -72,7 +90,7 @@ impl NavigatorModel {
         zoom_speed: SharedSwitch<f32>,
         pan_speed: SharedSwitch<f32>,
         disable_events: Rc<Cell<bool>>,
-        min_zoom_limit: Rc<Cell<Option<f32>>>,
+        max_zoom_limit: Rc<Cell<Option<f32>>>,
     ) -> (physics::inertia::DynSimulator<Vector3>, callback::Handle, NavigatorEvents) {
         let simulator = Self::create_simulator(camera);
         let panning_callback = enclose!((scene,camera,mut simulator,pan_speed) move |pan: PanEvent| {
@@ -94,7 +112,7 @@ impl NavigatorModel {
             }),
         );
 
-        let zoom_callback = enclose!((scene,camera,simulator,min_zoom_limit) move |zoom:ZoomEvent| {
+        let zoom_callback = enclose!((scene,camera,simulator,max_zoom_limit) move |zoom:ZoomEvent| {
             let point       = zoom.focus;
             let normalized  = normalize_point2(point,scene.shape().value().into());
             let normalized  = normalized_to_range2(normalized, -1.0, 1.0);
@@ -106,19 +124,25 @@ impl NavigatorModel {
             let z              = half_height / camera.half_fovy_slope();
             let direction      = Vector3(x,y,z).normalize();
             let mut position   = simulator.target_value();
-            let min_zoom       = camera.clipping().near + min_zoom;
             let zoom_amount    = zoom.amount * position.z;
-            let direction      = direction   * zoom_amount;
-            DEBUG!("Position: {position.z}");
-            DEBUG!("Direction: {direction.z}");
-            let max_zoom_limit = max_zoom - position.z;
-            let min_zoom_limit = if let Some(limit) = min_zoom_limit.get() { limit } else { min_zoom - position.z };
-            let too_far        = direction.z > max_zoom_limit;
-            let too_close      = direction.z < min_zoom_limit;
-            let zoom_factor    = if too_far   { max_zoom_limit / direction.z }
-                            else if too_close { min_zoom_limit / direction.z }
-                            else              { 1.0 };
-            position          += direction * zoom_factor;
+            let translation    = direction   * zoom_amount;
+
+            // Usage of min/max prefixes might be confusing here. Remember that the max zoom means
+            // the maximum visible size, thus the minimal distance from the camera and vice versa.
+            let fovy_slope               = camera.half_fovy_slope();
+            let distance_to_show_full_ui = scene.shape().value().height / 2.0 / fovy_slope;
+            let max_zoom                 = max_zoom_limit.get().unwrap_or(max_zoom);
+            let min_distance =  1.0 / max_zoom * distance_to_show_full_ui + camera.clipping().near;
+            let max_distance = (1.0 / min_zoom * distance_to_show_full_ui).min(camera.clipping().far);
+
+            let max_translation_limit = max_distance - position.z;
+            let min_translation_limit = min_distance - position.z;
+            let too_far               = translation.z > max_translation_limit;
+            let too_close             = translation.z < min_translation_limit;
+            let limiting_factor       = if too_far   { max_translation_limit / translation.z }
+                                        else if too_close { min_translation_limit / translation.z }
+                                        else              { 1.0 };
+            position                  += translation * limiting_factor;
             simulator.set_target_value(position);
         });
         (
@@ -147,7 +171,9 @@ impl NavigatorModel {
         self.disable_events.set(false);
     }
 
-    pub fn set_max_zoom_level(&self, value: Option<f32>) {
+    /// Enable or disable the restriction of maximum zoom factor. Enabling it does change camera
+    /// zoom immediately, but the restriction will be applied on the next zoom event.
+    pub fn set_max_zoom(&self, value: Option<f32>) {
         self.max_zoom_limit.set(value);
     }
 }
