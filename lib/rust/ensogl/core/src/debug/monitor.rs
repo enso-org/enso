9957857b2dc971a5d3eebf5c45ae0eaa68f4b892
Mateusz Czapliński <mateusz.czaplinski@enso.org>
2022-02-15 14:51:09 +0100

    review: adjust comment in test

--- lib/rust/ensogl/core/src/debug/monitor.rs
+++ lib/rust/ensogl/core/src/debug/monitor.rs
@@ -912,7 +912,8 @@ mod tests {
         // Frame 1: simulate we managed to complete the work in 10ms, and then we wait 6ms before
         // starting next frame.
         //
-        // Note: there is no frame before "Frame 1", so the previous frame will not be tested.
+        // Note: we expect no data to be returned for the time period before the 1st frame was
+        // started.
         test_and_advance_frame!(test, None; next: 10.0, 6.0);
 
         // Frame 2: simulate we managed to complete the work in 5ms, and then we wait 11ms before
@@ -942,10 +943,8 @@ mod tests {
         // Frame 1: simulate we managed to complete the work in 10ms, and then we wait 6ms before
         // starting next frame.
         //
-        // Note: there is no earlier frame before "Frame 1", so the tested value for the previous
-        // frame will always be 0.0 and [`Correct`] at this point (which is a special case -
-        // depending on tested stat, for later frames 0.0 could result in a threshold
-        // warning/error).
+        // Note: we expect no data to be returned for the time period before the 1st frame was
+        // started.
         test_and_advance_frame!(test, None; next: 10.0, 6.0);
 
         // Frame 2: simulate we managed to complete the work in 5ms, and then we wait 11.67ms before
