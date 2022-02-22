a141a44af6b38d3f181005ef4b6472e12d3ca6c1
Michael Mauderer <michael.mauderer@enso.org>
2022-02-22 10:50:23 +0000

    Refactor and code clean-up.

--- lib/rust/ensogl/component/flame-graph/src/lib.rs
+++ lib/rust/ensogl/component/flame-graph/src/lib.rs
@@ -1,5 +1,15 @@
 //! An EnsoGL implementation of a basic flame graph.
 
+#![warn(missing_copy_implementations)]
+#![warn(missing_debug_implementations)]
+#![warn(missing_docs)]
+#![warn(trivial_casts)]
+#![warn(trivial_numeric_casts)]
+#![warn(unsafe_code)]
+#![warn(unused_import_braces)]
+#![warn(unused_qualifications)]
+
+
 use ensogl_core::prelude::*;
 
 use enso_frp as frp;
@@ -16,204 +26,211 @@ use ensogl_text as text;
 
 
 
-// ==========================
-// === Shapes Definitions ===
-// ==========================
-
-mod background {
-    use super::*;
-    ensogl_core::define_shape_system! {
-        (style:Style) {
-            let width  : Var<Pixels> = "input_size.x".into();
-            let height : Var<Pixels> = "input_size.y".into();
-            let zoom                 = Var::<f32>::from("1.0/zoom()");
-            let base_color           = style.get_color("flame_graph_color");
-
-            let shape = Rect((&width,&height));
-
-            let border_width: Var<Pixels> = (zoom * 2.0).into();
-
-            let right = Rect((&border_width,&height));
-            let right = right.translate_x(&width/2.0);
-
-            let left = Rect((&border_width,&height));
-            let left = left.translate_x(-&width/2.0);
-
-            let shape = shape - left;
-            let shape = shape - right;
-            let shape = shape.fill(base_color);
-
-            (shape).into()
-        }
-    }
-}
-
-
-
-// ===========
-// === FRP ===
-// ===========
-
-ensogl_core::define_endpoints! {
-    Input {
-        set_content(String),
-        set_position(Vector2),
-        set_size(Vector2)
-    }
-    Output {}
-}
-
-
-
-// =============
-// === Model ===
-// =============
-
-#[derive(Clone, Debug)]
-struct Model {
-    app:            Application,
-    background:     background::View,
-    label:          text::Area,
-    display_object: display::object::Instance,
-}
-
-impl Model {
-    fn new(app: Application) -> Self {
-        let scene = app.display.scene();
-        let logger = Logger::new("FlameGraphBlock");
-        let display_object = display::object::Instance::new(&logger);
-
-        let label = app.new_view::<text::Area>();
-
-        let background = background::View::new(&logger);
-
-        display_object.add_child(&background);
-        display_object.add_child(&label);
-
-        let app = app.clone_ref();
-        let model = Model { app, background, label, display_object };
-        model.set_layers(&scene.layers.tooltip, &scene.layers.tooltip_text);
-        model
-    }
-
-
-    /// Set scene layers for background and text respectively.
-    pub fn set_layers(&self, background_layer: &Layer, text_layer: &Layer) {
-        // FIXME[MM/WD]: Depth sorting of labels to in front of everything else in the scene.
-        //  Temporary solution. The depth management needs to allow defining relative position of
-        //  the text and background and let the whole component to be set to am an arbitrary layer.
-        background_layer.add_exclusive(&self.background);
-        self.label.add_to_scene_layer(text_layer);
-    }
-
-    fn set_size(&self, size: Vector2) {
-        self.background.size.set(size);
-        let text_size = self.label.height.value();
-        const TEXT_OFFSET_X: f32 = 4.0;
-        const TEXT_OFFSET_Y: f32 = -2.0;
-        let text_origin =
-            Vector2(TEXT_OFFSET_X - size.x / 2.0, TEXT_OFFSET_Y + text_size as f32 / 2.0);
-        self.label.set_position_xy(text_origin);
-    }
-
-    fn set_content(&self, t: &str) {
-        self.label.set_content(t)
-    }
-
-    fn set_label_opacity(&self, opacity: f32) {
-        let color = self.label.default_color.value();
-        let color: color::Rgba = color.opaque.with_alpha(opacity);
-        self.label.set_color_all.emit(color);
-        self.label.set_default_color.emit(color);
-    }
-}
-
-
-
 // =======================
 // === Block Component ===
 // =======================
 
-#[allow(missing_docs)]
-#[derive(Clone, CloneRef, Debug)]
-pub struct Block {
-    model:   Rc<Model>,
-    pub frp: Rc<Frp>,
-}
+/// A single block that makes up a flame graph.
+mod block {
+    use super::*;
 
-impl Block {
-    /// Constructor.
-    pub fn new(app: &Application) -> Self {
-        let frp = Rc::new(Frp::new());
-        let model = Rc::new(Model::new(app.clone_ref()));
-        Block { model, frp }.init()
+
+    // === Layout Constants ===
+
+    const TEXT_OFFSET_X: f32 = 4.0;
+    const TEXT_OFFSET_Y: f32 = -2.0;
+
+
+    // === Shapes Definitions ===
+
+    mod background {
+        use super::*;
+        ensogl_core::define_shape_system! {
+            (style:Style) {
+                let width  : Var<Pixels> = "input_size.x".into();
+                let height : Var<Pixels> = "input_size.y".into();
+                let zoom                 = Var::<f32>::from("1.0/zoom()");
+                let base_color           = style.get_color("flame_graph_color");
+
+                let shape = Rect((&width,&height));
+
+                let border_width: Var<Pixels> = (zoom * 2.0).into();
+
+                let right = Rect((&border_width,&height));
+                let right = right.translate_x(&width/2.0);
+
+                let left = Rect((&border_width,&height));
+                let left = left.translate_x(-&width/2.0);
+
+                let shape = shape - left;
+                let shape = shape - right;
+                let shape = shape.fill(base_color);
+
+                (shape).into()
+            }
+        }
     }
 
-    fn init(self) -> Self {
-        let frp = &self.frp;
-        let network = &frp.network;
-        let model = &self.model;
 
-        let label_opacity = Animation::<f32>::new(network);
+    // === FRP ===
 
-        frp::extend! { network
-           eval frp.set_content((t) model.set_content(t));
-           eval frp.set_size((size) model.set_size(*size));
+    ensogl_core::define_endpoints! {
+        Input {
+            set_content(String),
+            set_position(Vector2),
+            set_size(Vector2)
+        }
+        Output {}
+    }
 
-            label_opacity.target <+ model.background.events.mouse_over.constant(1.0);
-            label_opacity.target <+ model.background.events.mouse_out.constant(0.0);
-            eval label_opacity.value ((t) model.set_label_opacity(*t));
+    // === Model ===
 
+    #[derive(Clone, Debug)]
+    struct Model {
+        app:            Application,
+        background:     background::View,
+        label:          text::Area,
+        display_object: display::object::Instance,
+    }
+
+    impl Model {
+        fn new(app: Application) -> Self {
+            let scene = app.display.scene();
+            let logger = Logger::new("FlameGraphBlock");
+            let display_object = display::object::Instance::new(&logger);
+
+            let label = app.new_view::<text::Area>();
+
+            let background = background::View::new(&logger);
+
+            display_object.add_child(&background);
+            display_object.add_child(&label);
+
+            let app = app.clone_ref();
+            let model = Model { app, background, label, display_object };
+            model.set_layers(&scene.layers.tooltip, &scene.layers.tooltip_text);
+            model
         }
 
-        label_opacity.target.emit(0.0);
-        model.set_label_opacity(0.0);
 
-        self
+        /// Set scene layers for background and text respectively.
+        pub fn set_layers(&self, background_layer: &Layer, text_layer: &Layer) {
+            // FIXME[MM/WD]: Depth sorting of labels to in front of everything else in the scene.
+            //  Temporary solution. The depth management needs to allow defining relative position
+            // of  the text and background and let the whole component to be set to am
+            // an arbitrary layer.
+            background_layer.add_exclusive(&self.background);
+            self.label.add_to_scene_layer(text_layer);
+        }
+
+        fn set_size(&self, size: Vector2) {
+            self.background.size.set(size);
+            let text_size = self.label.height.value();
+            let text_origin =
+                Vector2(TEXT_OFFSET_X - size.x / 2.0, TEXT_OFFSET_Y + text_size / 2.0);
+            self.label.set_position_xy(text_origin);
+        }
+
+        fn set_content(&self, t: &str) {
+            self.label.set_content(t)
+        }
+
+        fn set_label_opacity(&self, opacity: f32) {
+            let color = self.label.default_color.value();
+            let color: color::Rgba = color.opaque.with_alpha(opacity);
+            self.label.set_color_all.emit(color);
+            self.label.set_default_color.emit(color);
+        }
+    }
+
+
+    // === Main Component ===
+
+    #[allow(missing_docs)]
+    #[derive(Clone, CloneRef, Debug)]
+    pub struct Block {
+        model:   Rc<Model>,
+        pub frp: Rc<Frp>,
+    }
+
+    impl Block {
+        /// Constructor.
+        pub fn new(app: &Application) -> Self {
+            let frp = Rc::new(Frp::new());
+            let model = Rc::new(Model::new(app.clone_ref()));
+            Block { model, frp }.init()
+        }
+
+        fn init(self) -> Self {
+            let frp = &self.frp;
+            let network = &frp.network;
+            let model = &self.model;
+
+            let label_opacity = Animation::<f32>::new(network);
+
+            frp::extend! { network
+               eval frp.set_content((t) model.set_content(t));
+               eval frp.set_size((size) model.set_size(*size));
+
+                label_opacity.target <+ model.background.events.mouse_over.constant(1.0);
+                label_opacity.target <+ model.background.events.mouse_out.constant(0.0);
+                eval label_opacity.value ((t) model.set_label_opacity(*t));
+
+            }
+
+            label_opacity.target.emit(0.0);
+            model.set_label_opacity(0.0);
+
+            self
+        }
+    }
+
+    impl Deref for Block {
+        type Target = Frp;
+
+        fn deref(&self) -> &Self::Target {
+            &self.frp
+        }
+    }
+
+    impl display::Object for Block {
+        fn display_object(&self) -> &display::object::Instance {
+            &self.model.display_object
+        }
+    }
+
+    impl application::command::FrpNetworkProvider for Block {
+        fn network(&self) -> &frp::Network {
+            &self.frp.network
+        }
+    }
+
+    impl application::View for Block {
+        fn label() -> &'static str {
+            "FlameGraphBlock"
+        }
+
+        fn new(app: &Application) -> Self {
+            Block::new(app)
+        }
+
+        fn app(&self) -> &Application {
+            &self.model.app
+        }
+
+        fn default_shortcuts() -> Vec<shortcut::Shortcut> {
+            default()
+        }
     }
 }
 
-impl Deref for Block {
-    type Target = Frp;
 
-    fn deref(&self) -> &Self::Target {
-        &self.frp
-    }
-}
-
-impl display::Object for Block {
-    fn display_object(&self) -> &display::object::Instance {
-        &self.model.display_object
-    }
-}
-
-impl application::command::FrpNetworkProvider for Block {
-    fn network(&self) -> &frp::Network {
-        &self.frp.network
-    }
-}
-
-impl application::View for Block {
-    fn label() -> &'static str {
-        "FlameGraphBlock"
-    }
-
-    fn new(app: &Application) -> Self {
-        Block::new(app)
-    }
-
-    fn app(&self) -> &Application {
-        &self.model.app
-    }
-
-    fn default_shortcuts() -> Vec<shortcut::Shortcut> {
-        default()
-    }
-}
 
 // ===================
 // === Flame Graph ===
 // ===================
+
+pub use block::Block;
 
 
 // === Layout Constants ===
@@ -224,12 +241,15 @@ const ROW_PADDING: f64 = 5.0;
 
 // === Implementation ===
 
-
+/// EnsoGL FlameGraph component. Consists of stacked blocks that indicate hierarchical time
+/// intervals. The blocks can be hovered to reveal a label associated with the block.
+#[derive(Debug)]
 pub struct FlameGraph {
     display_object: display::object::Instance,
     blocks:         Vec<Block>,
 }
 
+/// Instantiate a `Block` shape for the given block data from the profiler.
 fn shape_from_block(block: profiler::flame_graph::Block, app: &Application) -> Block {
     let component = app.new_view::<Block>();
 
@@ -246,6 +266,7 @@ fn shape_from_block(block: profiler::flame_graph::Block, app: &Application) -> B
 }
 
 impl FlameGraph {
+    /// Create a `FlameGraph` EnsoGL component from the given flame graph data from the profiler.
     pub fn from_data(data: profiler::flame_graph::FlameGraph, app: &Application) -> Self {
         let logger = Logger::new("FlameGraph");
         let display_object = display::object::Instance::new(&logger);
@@ -264,6 +285,7 @@ impl FlameGraph {
         Self { display_object, blocks }
     }
 
+    /// Return a reference to the blocks that make up the flame graph.
     pub fn blocks(&self) -> &[Block] {
         &self.blocks
     }
