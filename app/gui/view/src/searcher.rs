c97a2c54e92f615841a1d285d01184366d5638ca
Adam Obuchowicz <adam.obuchowicz@enso.org>
2022-02-24 17:01:54 +0100

    Creating a new node with the (+) button (#3278)
    [The Task](https://www.pivotaltracker.com/story/show/180887253)
    
    A new (+) button on the left-bottom corner appeared. It may be clicked to open searcher in the middle of the scene, as an alternative to tab key.
    
    https://user-images.githubusercontent.com/3919101/154514279-7972ed6a-0203-47cb-9a09-82dba948cf2f.mp4
    
    * The window_control_buttons::common was extracted to separate crate `ensogl-component-button` almost without change.
    * This includes a severe refactoring of adding nodes in general in the Graph Editor. The whole responsibility of adding new nodes (and starting their editing) was moved to Graph Editor - the Project View only reacts for GE events to show searcher properly.
    * The status bar was moved from the bottom-left corner to the middle-top of the scene. It does not collide with (+) button, and plays "notification" role anyway.
    * The `interface` debug scene was buggy. The problem was with one expression's span-tree. When I replaced it, the scene works.
    * I've removed "new searcher" API, as it is completely outdated.
    * I've changed code owners of integration tests to GUI team, as it is the team writing mostly the integration tests (int rust)

--- app/gui/view/src/searcher.rs
+++ app/gui/view/src/searcher.rs
@@ -23,7 +23,6 @@ use ensogl_component::list_view::ListView;
 // ==============
 
 pub mod icons;
-pub mod new;
 
 pub use ensogl_component::list_view::entry;
 
@@ -114,7 +113,6 @@ struct Model {
     logger:         Logger,
     display_object: display::object::Instance,
     list:           ListView<Entry>,
-    new_view:       new::View<usize>,
     documentation:  documentation::View,
     doc_provider:   Rc<CloneRefCell<AnyDocumentationProvider>>,
 }
@@ -126,7 +124,6 @@ impl Model {
         let logger = Logger::new("SearcherView");
         let display_object = display::object::Instance::new(&logger);
         let list = app.new_view::<ListView<Entry>>();
-        let new_view = new::View::new();
         let documentation = documentation::View::new(scene);
         let doc_provider = default();
         scene.layers.above_nodes.add_exclusive(&list);
@@ -143,7 +140,7 @@ impl Model {
         list.set_position_x(ACTION_LIST_X);
         documentation.set_position_x(DOCUMENTATION_X);
         documentation.set_position_y(-action_list_gap);
-        Self { app, logger, display_object, list, new_view, documentation, doc_provider }
+        Self { app, logger, display_object, list, documentation, doc_provider }
     }
 
     fn docs_for(&self, id: Option<entry::Id>) -> String {
@@ -252,14 +249,6 @@ impl View {
             source.used_as_suggestion <+ opt_picked_entry.gate(&is_entry_enabled);
             source.editing_committed  <+ model.list.chosen_entry.gate(&is_entry_enabled);
 
-            // New searcher
-            let is_selected           =  model.new_view.focused.clone_ref();
-            selected_id               <- model.new_view.highlight.map(|id| id.last().copied());
-            opt_picked_entry          <- selected_id.sample(&frp.use_as_suggestion);
-            source.used_as_suggestion <+ opt_picked_entry.gate(&is_selected);
-            opt_chosen_id             <- model.new_view.entry_chosen.map(|id| id.last().copied());
-            source.editing_committed  <+ opt_chosen_id.gate(&is_selected);
-
             eval displayed_doc ((data) model.documentation.frp.display_documentation(data));
         };
 
@@ -285,11 +274,6 @@ impl View {
     pub fn clear_actions(&self) {
         let provider = Rc::new(list_view::entry::EmptyProvider);
         self.set_actions(provider);
-    }
-
-    /// The FRP interface of new searcher.
-    pub fn new_frp(&self) -> &new::Frp<usize> {
-        &self.model.new_view.frp
     }
 }
 
