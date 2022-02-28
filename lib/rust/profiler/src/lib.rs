9c6af68321a9c7602fbaa197c6e70fd4c35eef9d
Kaz Wesley <kaz@lambdaverse.org>
2022-02-28 20:39:15 +0000

    `#[profile]` async fn: output lifetime bounds
    Support async-fns that desugar to Futures with lifetime bounds that
    aren't reflected in the Output types.

--- lib/rust/profiler/src/lib.rs
+++ lib/rust/profiler/src/lib.rs
@@ -589,6 +589,45 @@ mod compile_tests {
     #[profile(Objective)]
     async fn profiled_async() {}
 
+    #[profile(Objective)]
+    async fn polymorphic_return() -> Box<dyn PartialEq<u32>> {
+        Box::new(23)
+    }
+
+    #[profile(Objective)]
+    async fn borrows_input_references_in_output(x: &u32) -> &u32 {
+        x
+    }
+
+    #[profile(Objective)]
+    #[allow(clippy::needless_lifetimes)]
+    async fn borrows_input_references_in_output_explicitly<'a>(x: &'a u32) -> &'a u32 {
+        x
+    }
+
+    #[profile(Objective)]
+    async fn borrows_input_doesnt_use(_: &u32) -> u32 {
+        4
+    }
+
+    #[profile(Objective)]
+    async fn borrows_input_uses(x: &u32) -> u32 {
+        *x
+    }
+
+    #[profile(Objective)]
+    async fn borrows_two_args(x: &u32, y: &u32) -> u32 {
+        *x + *y
+    }
+
+    struct Foo(u32);
+    impl Foo {
+        #[profile(Objective)]
+        async fn borrows_self_and_arg(&self, x: &u32) -> u32 {
+            self.0 + *x
+        }
+    }
+
     #[profile(Detail)]
     #[allow(unsafe_code)]
     unsafe fn profiled_unsafe() {}
