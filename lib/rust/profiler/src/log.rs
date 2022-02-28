a3914f33c13b94cd7bfba69b8c818db39327d381
Kaz Wesley <kaz@lambdaverse.org>
2022-02-28 01:55:56 -0800

    API for storing metadata (#3291)
    API for storing metadata.
    
    See: https://www.pivotaltracker.com/story/show/181149277
    
    # Important Notes
    **New APIs**:
    - Storing metadata is implemented with `profiler::MetadataLogger`.
    - A full metadata storage/retrieval example is in [the top-level doctests](https://github.com/enso-org/enso/blob/wip/kw/profiling-metadata-api/lib/rust/profiler/data/src/lib.rs) for profiler::data, a crate which implements an API for profiling data consumers (it abstracts away the low-level details of the event log, and checks its invariants in the process) [after review of this new API here I'll open a PR to add it to the design doc].
    
    **Implementation**:
    - `profiler::Event` is parameterized by a metadata type, so that different types of metadata can be dependency-injected into it.
    - A data consumer defines its metadata type as an enum of all the kinds of metadata it is interested in.
    - Producing the metadata enum is accomplished without defining its type (which would require dependencies from around the app): A `MetadataLogger` internally use a serialization helper `Variant` to serialize its variant of the metadata enum without knowledge of the other possible variants.
    
    **Performance impact**: still in the low ns/measurement range, comparable to pushing to a vec.
    
    *Note*: `LocalVecBuilder` is currently present under the name `Log`, which is accurate but probably too overloaded. I'd like to find the right name for it, document it with examples, and move it to its own crate under data-structures, but I don't want doing that to hold up this PR.

---
+++ lib/rust/profiler/src/log.rs
@@ -0,0 +1,120 @@
+//! Data structure supporting limited interior mutability, to build up a collection.
+//!
+//! # Implementation
+//!
+//! Note [Log Safety]
+//! =============================
+//! When obtaining a reference from the UnsafeCell, all accessors follow these rules:
+//! - There must be a scope that the reference doesn't escape.
+//! - There must be no other references obtained in the same scope.
+//! Consistently following these rules ensures the no-alias rule of mutable references is
+//! satisfied.
+
+use std::cell;
+use std::mem;
+
+
+
+// ===========
+// === Log ===
+// ===========
+
+/// Data structure supporting limited interior mutability, to build up a collection.
+#[derive(Debug)]
+pub struct Log<T>(cell::UnsafeCell<Vec<T>>);
+
+#[allow(unsafe_code)]
+impl<T> Log<T> {
+    #[allow(clippy::new_without_default)]
+    /// Create a new, empty vec builder.
+    pub fn new() -> Self {
+        Self(cell::UnsafeCell::new(vec![]))
+    }
+
+    /// Push an element.
+    pub fn append(&self, element: T) {
+        // Note [Log Safety]
+        unsafe {
+            (&mut *self.0.get()).push(element);
+        }
+    }
+
+    /// Return (and consume) all elements pushed so far.
+    pub fn take_all(&self) -> Vec<T> {
+        // Note [Log Safety]
+        unsafe { mem::take(&mut *self.0.get()) }
+    }
+
+    /// The number of elements that are currently available.
+    pub fn len(&self) -> usize {
+        // Note [Log Safety]
+        unsafe { &*self.0.get() }.len()
+    }
+
+    /// Returns true if no elements are available.
+    pub fn is_empty(&self) -> bool {
+        // Note [Log Safety]
+        unsafe { &*self.0.get() }.is_empty()
+    }
+}
+
+#[allow(unsafe_code)]
+impl<T: Clone> Log<T> {
+    /// Return clones of all elements pushed so far.
+    pub fn clone_all(&self) -> Vec<T> {
+        // Note [Log Safety]
+        unsafe { &*self.0.get() }.to_vec()
+    }
+}
+
+// This can't be derived without requiring T: Default, which is not otherwise needed.
+// See: https://github.com/rust-lang/rust/issues/26925
+impl<T> Default for Log<T> {
+    fn default() -> Self {
+        Self(Default::default())
+    }
+}
+
+
+
+// ======================
+// === ThreadLocalLog ===
+// ======================
+
+/// Wraps a thread-local [`Log`] instance.
+#[derive(Debug)]
+pub struct ThreadLocalLog<T: 'static>(std::thread::LocalKey<Log<T>>);
+
+impl<T: 'static> ThreadLocalLog<T> {
+    /// New.
+    pub const fn new(log: std::thread::LocalKey<Log<T>>) -> Self {
+        Self(log)
+    }
+
+    /// Push an element.
+    pub fn append(&'static self, element: T) {
+        self.0.with(|log| log.append(element))
+    }
+
+    /// Return (and consume) all elements pushed so far.
+    pub fn take_all(&'static self) -> Vec<T> {
+        self.0.with(|log| log.take_all())
+    }
+
+    /// The number of elements that are currently available.
+    pub fn len(&'static self) -> usize {
+        self.0.with(|log| log.len())
+    }
+
+    /// Returns true if no elements are available.
+    pub fn is_empty(&'static self) -> bool {
+        self.0.with(|log| log.is_empty())
+    }
+}
+
+impl<T: Clone> ThreadLocalLog<T> {
+    /// Return clones of all elements pushed so far.
+    pub fn clone_all(&'static self) -> Vec<T> {
+        self.0.with(|log| log.clone_all())
+    }
+}
