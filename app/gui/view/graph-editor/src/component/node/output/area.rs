807506485def428093cb746c755377e0eb1a3c92
Mateusz Czapliński <mateusz.czaplinski@enso.org>
2022-03-01 14:59:36 +0100

    Remove mod-level "allow missing docs" in graph-editor; add entity-level FIXMEs. (#3307)
    Remove a module-level `#![allow(missing_docs)]` attribute from 2 modules in `graph-editor` crate. Instead, add the same attribute with a `FIXME` comment to lower-level entities.
    
    See discussion at: https://discord.com/channels/401396655599124480/947797676823560193
    
    # Important Notes
    There are still 37 module-level `allow(missing_docs)` attributes present in the codebase after this change:
    
    ```
    $ git grep '^#!.allow.missing_docs.' | wc -l
    22
    $ git grep -A1 '^#.allow.missing_docs.' | grep -w mod | wc -l
    15
    ```
    
    [ci no changelog needed]

--- app/gui/view/graph-editor/src/component/node/output/area.rs
+++ app/gui/view/graph-editor/src/component/node/output/area.rs
@@ -59,6 +59,7 @@ pub type PortRefMut<'a> = span_tree::node::RefMut<'a, port::Model>;
 
 /// Specialized version of `node::Expression`, containing the port information.
 #[derive(Default)]
+#[allow(missing_docs)]
 pub struct Expression {
     pub code:            Option<String>,
     pub span_tree:       SpanTree,
@@ -69,6 +70,7 @@ pub struct Expression {
 }
 
 impl Expression {
+    #[allow(missing_docs)] // FIXME[everyone] All pub functions should have docs, always.
     pub fn code(&self) -> String {
         self.code.clone().unwrap_or_default()
     }
@@ -416,6 +418,7 @@ impl Model {
 /// about this design decision, please read the docs for the [`node::Node`].
 #[derive(Clone, CloneRef, Debug)]
 pub struct Area {
+    #[allow(missing_docs)]
     pub frp: Frp,
     model:   Rc<Model>,
 }
@@ -429,6 +432,7 @@ impl Deref for Area {
 
 
 impl Area {
+    #[allow(missing_docs)] // FIXME[everyone] All pub functions should have docs, always.
     pub fn new(logger: impl AnyLogger, app: &Application) -> Self {
         let frp = Frp::new();
         let model = Rc::new(Model::new(logger, app, &frp));
@@ -489,6 +493,7 @@ impl Area {
         Self { frp, model }
     }
 
+    #[allow(missing_docs)] // FIXME[everyone] All pub functions should have docs, always.
     pub fn port_type(&self, crumbs: &Crumbs) -> Option<Type> {
         let expression = self.model.expression.borrow();
         expression
@@ -499,10 +504,12 @@ impl Area {
             .and_then(|t| t.frp.as_ref().and_then(|frp| frp.tp.value()))
     }
 
+    #[allow(missing_docs)] // FIXME[everyone] All pub functions should have docs, always.
     pub fn get_crumbs_by_id(&self, id: ast::Id) -> Option<Crumbs> {
         self.model.id_crumbs_map.borrow().get(&id).cloned()
     }
 
+    #[allow(missing_docs)] // FIXME[everyone] All pub functions should have docs, always.
     pub fn whole_expr_id(&self) -> Option<ast::Id> {
         self.model.expression.borrow().whole_expr_id
     }
