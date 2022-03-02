55c4aeadd3931403c7b5468a950e6f24a7f10462
Mateusz Czapliński <mateusz.czaplinski@enso.org>
2022-03-02 12:32:44 +0100

    Revert "WIP"
    This reverts commit a68ba550ca6c34ae0c64bed92184d7c9eab726a9.

--- app/gui/view/graph-editor/src/component/node.rs
+++ app/gui/view/graph-editor/src/component/node.rs
@@ -706,13 +706,7 @@ impl Node {
             out.source.bounding_box <+ bounding_box_input.map(
                 |(size,position,visualization_enabled)| {
                     let position = position - Vector2::new(0.0,size.y / 2.0);
-                    // FIXME 'expression'? 'text'? ...?
-                    let expression_area_bounding_box =
-                        BoundingBox::from_position_and_size(position,*size);
-                    if *visualization_enabled {
-                        let _ = (); //let visualization_po
-                    }
-                    expression_area_bounding_box
+                    BoundingBox::from_position_and_size(position,*size)
                 });
 
 
