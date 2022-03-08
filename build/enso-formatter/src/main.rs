2b64f746a0ef357b646e1d1046cdc30a43453bd0
Wojciech Danilo <wojciech.danilo@gmail.com>
2022-03-08 04:40:26 +0100

    Refactoring

--- build/enso-formatter/src/main.rs
+++ build/enso-formatter/src/main.rs
@@ -1,3 +1,13 @@
+//! This crate implements code formatter rules that are not implemented in rustfmt. These rules
+//! are this codebase specific, and they may not be desired in other code bases, including:
+//! - Sorting imports into groups (e.g. local imports, pub imports, etc.).
+//! - Sorting module attributes into groups.
+//! - Adding standard lint configuration to `lib.rs` and `main.rs` files.
+//!
+//! Possible extensions not yet implemented:
+//! - Emitting warnings about star imports that are not ending with `traits::*` nor `prelude::*`.
+//! - Sections are automatically keeping spacing.
+
 // === Standard Linter Configuration ===
 #![deny(unconditional_recursion)]
 #![warn(missing_copy_implementations)]
@@ -20,7 +30,35 @@ use std::path::PathBuf;
 
 
 
+// =================
+// === Constants ===
+// =================
+
+/// Standard linter configuration. It will be used in every `main.rs` and `lib.rs` file in the
+/// codebase.
+const STD_LINTER_ATTRIBS: &[&str] = &[
+    "deny(unconditional_recursion)",
+    "warn(missing_copy_implementations)",
+    "warn(missing_debug_implementations)",
+    "warn(missing_docs)",
+    "warn(trivial_casts)",
+    "warn(trivial_numeric_casts)",
+    "warn(unsafe_code)",
+    "warn(unused_import_braces)",
+    "warn(unused_qualifications)",
+];
+
+
+
+// ===================
+// === HeaderToken ===
+// ===================
+
+use HeaderToken::*;
+
+/// A token that can be found in the header of a file.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+#[allow(missing_docs)]
 pub enum HeaderToken {
     Attrib,
     ModuleAttrib,
@@ -41,12 +79,11 @@ pub enum HeaderToken {
     PubUse,
     PubUseStar,
     PubMod,
-
+    /// Special header token that is never parsed, but can be injected by the code.
     StandardLinterConfig,
 }
 
-use HeaderToken::*;
-
+/// A header token with the matched string and possibly attached attributes.
 #[derive(Clone)]
 pub struct HeaderElement {
     attrs:     Vec<String>,
@@ -61,98 +98,79 @@ impl Debug for HeaderElement {
 }
 
 impl HeaderElement {
+    /// Constructor.
     pub fn new(token: HeaderToken, reg_match: String) -> Self {
         let attrs = Default::default();
         Self { attrs, token, reg_match }
     }
 
+    /// Length of the splice. Includes the length of the matched string and all attached attributes.
     pub fn len(&self) -> usize {
         let args_len: usize = self.attrs.iter().map(|t| t.len()).sum();
         self.reg_match.len() + args_len
     }
 
+    /// Convert the element to a string representation.
     pub fn to_string(&self) -> String {
         format!("{}{}", self.attrs.join(""), self.reg_match)
     }
 }
 
-fn find_with<'s, 't, T>(
-    input: &'s str,
-    regex: &'t Regex,
-    f: impl FnOnce(String) -> T,
-) -> Result<(), T> {
+/// Wrappers for [`Regex::find`] which returns [`Result::Err`] on element found. It allows combining
+/// multiple calls to this function with the Rust `?` syntax.
+fn find_with<T>(input: &str, regex: &Regex, f: impl FnOnce(String) -> T) -> Result<(), T> {
     match regex.find(input) {
         Some(t) => Err(f(t.as_str().into())),
         None => Ok(()),
     }
 }
 
+/// Regex constructor that starts on the beginning of a line, can be surrounded by whitespaces and
+/// ends with a line break.
 fn re(input: &str) -> Regex {
     let str = format!(r"^ *{} *(; *)?((\r\n?)|\n)", input);
     Regex::new(&str).unwrap()
 }
 
-const STD_LINTER_ATTRIBS: &[&str] = &[
-    "deny(unconditional_recursion)",
-    "warn(missing_copy_implementations)",
-    "warn(missing_debug_implementations)",
-    "warn(missing_docs)",
-    "warn(trivial_casts)",
-    "warn(trivial_numeric_casts)",
-    "warn(unsafe_code)",
-    "warn(unused_import_braces)",
-    "warn(unused_qualifications)",
-];
+macro_rules! define_rules {
+    ($($name:ident = $re:tt;)*) => {
+        #[allow(non_upper_case_globals)]
+        mod static_re {
+            use super::*;
+            lazy_static! {
+                $(
+                    pub static ref $name: Regex = re($re);
+                )*
+            }
+        }
 
-#[rustfmt::skip]
-lazy_static! {
-    static ref r_empty_line: Regex             = re(r"");
-    static ref r_module_comment: Regex         = re(r"//![^\n\r]*");
-    static ref r_comment: Regex                = re(r"//[^\n\r]*");
-    static ref r_crate_use: Regex              = re(r"use +crate( *:: *[\w]+)*( +as +[\w]+)?");
-    static ref r_crate_use_star: Regex         = re(r"use +crate( *:: *[\w*]+)*");
-    static ref r_crate_pub_use: Regex          = re(r"pub +use +crate( *:: *[\w]+)*( +as +[\w]+)?");
-    static ref r_crate_pub_use_star: Regex     = re(r"pub +use +crate( *:: *[\w*]+)*");
-    static ref r_use: Regex                    = re(r"use +[\w]+( *:: *[\w]+)*( +as +[\w]+)?");
-    static ref r_use_star: Regex               = re(r"use +[\w]+( *:: *[\w*]+)*");
-    static ref r_pub_use: Regex                = re(r"pub +use +[\w]+( *:: *[\w]+)*( +as +[\w]+)?");
-    static ref r_pub_use_star: Regex           = re(r"pub +use +[\w]+( *:: *[\w*]+)*");
-    static ref r_module_attrib_feature: Regex  = re(r"#!\[feature[^\]]*\]");
-    static ref r_module_attrib_feature2: Regex = re(r"#!\[allow\(incomplete_features\)\]");
-    static ref r_module_attrib_warn: Regex     = re(r"#!\[warn[^\]]*\]");
-    static ref r_module_attrib_allow: Regex    = re(r"#!\[allow[^\]]*\]");
-    static ref r_module_attrib_deny: Regex     = re(r"#!\[deny[^\]]*\]");
-    static ref r_module_attrib: Regex          = re(r"#!\[[^\]]*\]");
-    static ref r_attrib: Regex                 = re(r"#\[[^\]]*\]");
-    static ref r_pub_mod: Regex                = re(r"pub +mod +[\w]+");
+        fn match_header_internal(input: &str) -> Result<(), HeaderElement> {
+            $( find_with(input, &static_re::$name, |t| HeaderElement::new($name, t))?; )*
+            Ok(())
+        }
+    };
 }
 
-
-fn match_header_internal(input: &str) -> Result<(), HeaderElement> {
-    find_with(input, &r_empty_line, |t| HeaderElement::new(EmptyLine, t))?;
-    find_with(input, &r_module_comment, |t| HeaderElement::new(ModuleDoc, t))?;
-    find_with(input, &r_comment, |t| HeaderElement::new(Comment, t))?;
-
-    find_with(input, &r_crate_use, |t| HeaderElement::new(CrateUse, t))?;
-    find_with(input, &r_crate_use_star, |t| HeaderElement::new(CrateUseStar, t))?;
-    find_with(input, &r_crate_pub_use, |t| HeaderElement::new(CratePubUse, t))?;
-    find_with(input, &r_crate_pub_use_star, |t| HeaderElement::new(CratePubUseStar, t))?;
-
-    find_with(input, &r_use, |t| HeaderElement::new(Use, t))?;
-    find_with(input, &r_use_star, |t| HeaderElement::new(UseStar, t))?;
-    find_with(input, &r_pub_use, |t| HeaderElement::new(PubUse, t))?;
-    find_with(input, &r_pub_use_star, |t| HeaderElement::new(PubUseStar, t))?;
-
-    find_with(input, &r_module_attrib_feature, |t| HeaderElement::new(ModuleAttribFeature, t))?;
-    find_with(input, &r_module_attrib_feature2, |t| HeaderElement::new(ModuleAttribFeature2, t))?;
-    find_with(input, &r_module_attrib_warn, |t| HeaderElement::new(ModuleAttribWarn, t))?;
-    find_with(input, &r_module_attrib_allow, |t| HeaderElement::new(ModuleAttribAllow, t))?;
-    find_with(input, &r_module_attrib_deny, |t| HeaderElement::new(ModuleAttribDeny, t))?;
-    find_with(input, &r_module_attrib, |t| HeaderElement::new(ModuleAttrib, t))?;
-
-    find_with(input, &r_attrib, |t| HeaderElement::new(Attrib, t))?;
-    find_with(input, &r_pub_mod, |t| HeaderElement::new(PubMod, t))?;
-    Ok(())
+define_rules! {
+    EmptyLine            = r"";
+    ModuleDoc            = r"//![^\n\r]*";
+    Comment              = r"//[^\n\r]*";
+    CrateUse             = r"use +crate( *:: *[\w]+)*( +as +[\w]+)?";
+    CrateUseStar         = r"use +crate( *:: *[\w*]+)*";
+    CratePubUse          = r"pub +use +crate( *:: *[\w]+)*( +as +[\w]+)?";
+    CratePubUseStar      = r"pub +use +crate( *:: *[\w*]+)*";
+    Use                  = r"use +[\w]+( *:: *[\w]+)*( +as +[\w]+)?";
+    UseStar              = r"use +[\w]+( *:: *[\w*]+)*";
+    PubUse               = r"pub +use +[\w]+( *:: *[\w]+)*( +as +[\w]+)?";
+    PubUseStar           = r"pub +use +[\w]+( *:: *[\w*]+)*";
+    ModuleAttribFeature  = r"#!\[feature[^\]]*\]";
+    ModuleAttribFeature2 = r"#!\[allow\(incomplete_features\)\]";
+    ModuleAttribWarn     = r"#!\[warn[^\]]*\]";
+    ModuleAttribAllow    = r"#!\[allow[^\]]*\]";
+    ModuleAttribDeny     = r"#!\[deny[^\]]*\]";
+    ModuleAttrib         = r"#!\[[^\]]*\]";
+    Attrib               = r"#\[[^\]]*\]";
+    PubMod               = r"pub +mod +[\w]+";
 }
 
 fn match_header(input: &str) -> Option<HeaderElement> {
@@ -162,11 +180,70 @@ fn match_header(input: &str) -> Option<HeaderElement> {
     }
 }
 
-fn main() {
-    // process_file("./lib/rust/ensogl/core/src/lib.rs", false, true);
-    process_path(".");
+
+
+// =======================
+// === Pretty printing ===
+// =======================
+
+/// Prints H1 section if any of the provided tokens was used in the file being formatted.
+fn print_h1(
+    out: &mut String,
+    map: &HashMap<HeaderToken, Vec<String>>,
+    tokens: &[HeaderToken],
+    str: &str,
+) {
+    if tokens.iter().map(|tok| map.contains_key(tok)).any(|t| t) {
+        out.push_str("\n");
+        out.push_str(&format!("// ===={}====\n", "=".repeat(str.len())));
+        out.push_str(&format!("// === {} ===\n", str));
+        out.push_str(&format!("// ===={}====\n", "=".repeat(str.len())));
+        out.push_str("\n");
+    }
 }
 
+/// Prints H2 section if any of the provided tokens was used in the file being formatted.
+fn print_h2(
+    out: &mut String,
+    map: &HashMap<HeaderToken, Vec<String>>,
+    tokens: &[HeaderToken],
+    str: &str,
+) {
+    if tokens.iter().map(|tok| map.contains_key(tok)).any(|t| t) {
+        out.push_str(&format!("// === {} ===\n", str));
+    }
+}
+
+/// Prints all the entries associated with the provided tokens. If at least one entry was printed,
+/// an empty line will be added in the end.
+fn print(out: &mut String, map: &mut HashMap<HeaderToken, Vec<String>>, tokens: &[HeaderToken]) {
+    let sub_results: Vec<bool> = tokens.iter().map(|t| print_single(out, map, *t)).collect();
+    if sub_results.iter().any(|t| *t) {
+        out.push_str("\n");
+    }
+}
+
+/// Print all the entries associated with the provided token.
+fn print_single(
+    out: &mut String,
+    map: &mut HashMap<HeaderToken, Vec<String>>,
+    token: HeaderToken,
+) -> bool {
+    match map.remove(&token) {
+        None => false,
+        Some(t) => {
+            out.push_str(&t.join(""));
+            true
+        }
+    }
+}
+
+
+// =============
+// === Logic ===
+// =============
+
+/// Process all files of the given path recursively.
 fn process_path(path: impl AsRef<Path>) {
     let paths = discover_paths(path);
     let total = paths.len();
@@ -179,6 +256,7 @@ fn process_path(path: impl AsRef<Path>) {
     }
 }
 
+/// Discover all paths containing Rust sources, recursively.
 fn discover_paths(path: impl AsRef<Path>) -> Vec<PathBuf> {
     let mut vec = Vec::default();
     discover_paths_internal(&mut vec, path);
@@ -198,12 +276,12 @@ fn discover_paths_internal(vec: &mut Vec<PathBuf>, path: impl AsRef<Path>) {
     }
 }
 
+/// Process a single source file.
 fn process_file(path: impl AsRef<Path>, preview: bool, is_main_file: bool) {
     let path = path.as_ref();
     let str = fs::read_to_string(path).unwrap();
 
     let mut str_ptr: &str = &str;
-
     let mut attrs = vec![];
     let mut header = vec![];
     loop {
@@ -225,78 +303,70 @@ fn process_file(path: impl AsRef<Path>, preview: bool, is_main_file: bool) {
         }
     }
 
-    // header.reverse();
+    // Do not consume the leading comments.
     let mut ending: Vec<&HeaderElement> = header
         .iter()
         .rev()
         .take_while(|t| (t.token == Comment) || (t.token == EmptyLine))
         .collect();
     ending.reverse();
-
     let incorrect_ending_len = ending.into_iter().skip_while(|t| t.token == EmptyLine).count();
-
     header.truncate(header.len() - incorrect_ending_len);
-
     let total_len: usize = header.iter().map(|t| t.len()).sum();
 
-    let mut header_map = HashMap::<HeaderToken, Vec<String>>::new();
-
-
+    // Build a mapping between tokens and registered entries.
+    let mut map = HashMap::<HeaderToken, Vec<String>>::new();
     for elem in header {
-        header_map.entry(elem.token).or_default().push(elem.to_string());
+        map.entry(elem.token).or_default().push(elem.to_string());
     }
 
+    // Remove standard linter configuration from the configuration found in the file.
     if is_main_file {
-        let vec = header_map.entry(ModuleAttribAllow).or_default();
+        let vec = map.entry(ModuleAttribAllow).or_default();
         vec.retain(|t| !STD_LINTER_ATTRIBS.iter().map(|s| t.contains(s)).any(|b| b));
         if vec.is_empty() {
-            header_map.remove(&ModuleAttribAllow);
+            map.remove(&ModuleAttribAllow);
         }
 
-        let vec = header_map.entry(ModuleAttribDeny).or_default();
+        let vec = map.entry(ModuleAttribDeny).or_default();
         vec.retain(|t| !STD_LINTER_ATTRIBS.iter().map(|s| t.contains(s)).any(|b| b));
         if vec.is_empty() {
-            header_map.remove(&ModuleAttribDeny);
+            map.remove(&ModuleAttribDeny);
         }
 
-        let vec = header_map.entry(ModuleAttribWarn).or_default();
+        let vec = map.entry(ModuleAttribWarn).or_default();
         vec.retain(|t| !STD_LINTER_ATTRIBS.iter().map(|s| t.contains(s)).any(|b| b));
         if vec.is_empty() {
-            header_map.remove(&ModuleAttribWarn);
+            map.remove(&ModuleAttribWarn);
         }
 
         let std_linter_attribs = STD_LINTER_ATTRIBS.iter().map(|t| format!("#![{}]\n", t));
-        header_map.entry(StandardLinterConfig).or_default().extend(std_linter_attribs);
+        map.entry(StandardLinterConfig).or_default().extend(std_linter_attribs);
     }
 
-
+    // Print the results.
     let mut out = String::new();
-    print(&mut out, &mut header_map, &[ModuleDoc]);
-    print(&mut out, &mut header_map, &[ModuleAttrib]);
-    print_h2(&mut out, &header_map, &[StandardLinterConfig], "Standard Linter Configuration");
-    print(&mut out, &mut header_map, &[StandardLinterConfig]);
+    print(&mut out, &mut map, &[ModuleDoc]);
+    print(&mut out, &mut map, &[ModuleAttrib]);
+    print_h2(&mut out, &map, &[StandardLinterConfig], "Standard Linter Configuration");
+    print(&mut out, &mut map, &[StandardLinterConfig]);
     print_h2(
         &mut out,
-        &header_map,
+        &map,
         &[ModuleAttribAllow, ModuleAttribDeny, ModuleAttribWarn],
         "Non-Standard Linter Configuration",
     );
-    print(&mut out, &mut header_map, &[ModuleAttribAllow, ModuleAttribDeny, ModuleAttribWarn]);
-    print_h2(&mut out, &header_map, &[ModuleAttribFeature2, ModuleAttribFeature], "Features");
-    print(&mut out, &mut header_map, &[ModuleAttribFeature2, ModuleAttribFeature]);
+    print(&mut out, &mut map, &[ModuleAttribAllow, ModuleAttribDeny, ModuleAttribWarn]);
+    print_h2(&mut out, &map, &[ModuleAttribFeature2, ModuleAttribFeature], "Features");
+    print(&mut out, &mut map, &[ModuleAttribFeature2, ModuleAttribFeature]);
 
-    print(&mut out, &mut header_map, &[CrateUseStar, UseStar]);
-    print(&mut out, &mut header_map, &[CrateUse]);
-    print(&mut out, &mut header_map, &[Use]);
+    print(&mut out, &mut map, &[CrateUseStar, UseStar]);
+    print(&mut out, &mut map, &[CrateUse]);
+    print(&mut out, &mut map, &[Use]);
 
-    print_h1(
-        &mut out,
-        &header_map,
-        &[PubMod, CratePubUseStar, PubUseStar, CratePubUse, PubUse],
-        "Export",
-    );
-    print(&mut out, &mut header_map, &[PubMod]);
-    print(&mut out, &mut header_map, &[CratePubUseStar, PubUseStar, CratePubUse, PubUse]);
+    print_h1(&mut out, &map, &[PubMod, CratePubUseStar, PubUseStar, CratePubUse, PubUse], "Export");
+    print(&mut out, &mut map, &[PubMod]);
+    print(&mut out, &mut map, &[CratePubUseStar, PubUseStar, CratePubUse, PubUse]);
     out.push_str("\n\n");
 
     out.push_str(&str[total_len..]);
@@ -308,49 +378,9 @@ fn process_file(path: impl AsRef<Path>, preview: bool, is_main_file: bool) {
     }
 }
 
-fn print_h1(
-    out: &mut String,
-    map: &HashMap<HeaderToken, Vec<String>>,
-    tokens: &[HeaderToken],
-    str: &str,
-) {
-    if tokens.iter().map(|tok| map.contains_key(tok)).any(|t| t) {
-        out.push_str("\n");
-        out.push_str(&format!("// ===={}====\n", "=".repeat(str.len())));
-        out.push_str(&format!("// === {} ===\n", str));
-        out.push_str(&format!("// ===={}====\n", "=".repeat(str.len())));
-        out.push_str("\n");
-    }
-}
 
-fn print_h2(
-    out: &mut String,
-    map: &HashMap<HeaderToken, Vec<String>>,
-    tokens: &[HeaderToken],
-    str: &str,
-) {
-    if tokens.iter().map(|tok| map.contains_key(tok)).any(|t| t) {
-        out.push_str(&format!("// === {} ===\n", str));
-    }
-}
 
-fn print(out: &mut String, map: &mut HashMap<HeaderToken, Vec<String>>, tokens: &[HeaderToken]) {
-    let sub_results: Vec<bool> = tokens.iter().map(|t| print_single(out, map, *t)).collect();
-    if sub_results.iter().any(|t| *t == true) {
-        out.push_str("\n");
-    }
-}
-
-fn print_single(
-    out: &mut String,
-    map: &mut HashMap<HeaderToken, Vec<String>>,
-    token: HeaderToken,
-) -> bool {
-    match map.remove(&token) {
-        None => false,
-        Some(t) => {
-            out.push_str(&t.join(""));
-            true
-        }
-    }
+fn main() {
+    // process_file("./lib/rust/ensogl/core/src/lib.rs", false, true);
+    process_path(".");
 }
