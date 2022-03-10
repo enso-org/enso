010b64c986e8af442f1c9187b95751e060463679
Dmitry Bushev <bushevdv@gmail.com>
2022-03-10 09:49:01 +0300

    feat: impl arguments

--- lib/scala/docs-generator/src/main/scala/org/enso/docs/generator/DocSectionsGenerator.scala
+++ lib/scala/docs-generator/src/main/scala/org/enso/docs/generator/DocSectionsGenerator.scala
@@ -10,12 +10,38 @@ final class DocSectionsGenerator {
   import DocSectionsGenerator._
 
   def generate(doc: Doc): DocSections = {
-    println("=======")
-    println(doc)
+    println(doc.body)
     DocSections(
-      tags     = doc.tags.map(tags => tags.elems.toList.map(TagRender.render)),
-      synopsis = doc.synopsis.map(SynopsisRender.render)
+      tags        = doc.tags.map(tags => tags.elems.toList.map(TagRender.render)),
+      description = doc.synopsis.map(SynopsisRender.render),
+      arguments   = doc.body.flatMap(buildArguments),
+      examples    = None,
+      icon        = None,
+      aliases     = None
     )
+  }
+
+  private def buildArguments(body: Doc.Body): Option[DocArguments] = {
+    val argumentsSection = body.elems.toList.find { section =>
+      section.elems.head.repr.build().contains("Arguments")
+    }
+    val argumentsOpt = argumentsSection.map { section =>
+      section.elems.tail
+        .flatMap {
+          case e: Doc.Elem.List => e.elems.toList
+          case _                => Seq()
+        }
+        .flatMap(buildArgument)
+    }
+    argumentsOpt.map(DocArguments)
+  }
+
+  private def buildArgument(elem: Doc.Elem): Option[DocArgument] = {
+    val line         = HtmlRender.render(elem.html)
+    val (name, text) = line.span(_ != ':')
+    Option.when(text.nonEmpty) {
+      DocArgument(name.trim, text.tail.trim)
+    }
   }
 }
 object DocSectionsGenerator {
@@ -52,6 +78,24 @@ object DocSectionsGenerator {
       HtmlRender.render(builder.result())
     }
   }
+
 }
 
-case class DocSections(tags: Option[Seq[String]], synopsis: Option[String])
+case class DocSections(
+  tags: Option[Seq[String]],
+  description: Option[String],
+  arguments: Option[DocArguments],
+  examples: Option[DocExamples],
+  icon: Option[String],
+  aliases: Option[DocAliases]
+)
+
+case class DocArguments(arguments: Seq[DocArgument])
+
+case class DocArgument(name: String, text: String)
+
+case class DocExamples(examples: Seq[DocExample])
+
+case class DocExample(text: String, code: String)
+
+case class DocAliases(aliases: Seq[String])
--- lib/scala/docs-generator/src/test/scala/org/enso/docs/generator/DocSectionsGeneratorTest.scala
+++ lib/scala/docs-generator/src/test/scala/org/enso/docs/generator/DocSectionsGeneratorTest.scala
@@ -11,7 +11,8 @@ class DocSectionsGeneratorTest extends AnyWordSpec with Matchers {
       val comment =
         """ UNSTABLE
           |""".stripMargin
-      val expected = DocSections(Some(Seq("UNSTABLE")), None)
+      val expected =
+        DocSections(Some(Seq("UNSTABLE")), None, None, None, None, None)
 
       DocParserWrapper.generateSections(comment) shouldEqual expected
     }
@@ -19,9 +20,17 @@ class DocSectionsGeneratorTest extends AnyWordSpec with Matchers {
     "generate multiple tags" in {
       val comment =
         """ UNSTABLE
-          |DEPRECATED
+          | DEPRECATED
           |""".stripMargin
-      val expected = DocSections(Some(Seq("UNSTABLE", "DEPRECATED")), None)
+      val expected =
+        DocSections(
+          Some(Seq("UNSTABLE", "DEPRECATED")),
+          None,
+          None,
+          None,
+          None,
+          None
+        )
 
       DocParserWrapper.generateSections(comment) shouldEqual expected
     }
@@ -30,7 +39,8 @@ class DocSectionsGeneratorTest extends AnyWordSpec with Matchers {
       val comment =
         """ hello world
           |""".stripMargin
-      val expected = DocSections(None, Some("<p>hello world</p>"))
+      val expected =
+        DocSections(None, Some("<p>hello world</p>"), None, None, None, None)
 
       DocParserWrapper.generateSections(comment) shouldEqual expected
     }
@@ -38,9 +48,65 @@ class DocSectionsGeneratorTest extends AnyWordSpec with Matchers {
     "generate synopsis multiline" in {
       val comment =
         """ hello world
-          |multiline
+          | multiline
           |""".stripMargin
-      val expected = DocSections(None, Some("<p>hello world multiline</p>"))
+      val expected = DocSections(
+        None,
+        Some("<p>hello world multiline</p>"),
+        None,
+        None,
+        None,
+        None
+      )
+
+      DocParserWrapper.generateSections(comment) shouldEqual expected
+    }
+
+    "generate arguments" in {
+      val comment =
+        """ Synopsis
+          |
+          | Arguments:
+          | - one: The first
+          |   second line
+          | - two: The second
+          |""".stripMargin
+      val expected =
+        DocSections(
+          None,
+          Some("<p>Synopsis </p>"),
+          Some(
+            DocArguments(
+              Seq(
+                DocArgument("one", "The first\n   second line"),
+                DocArgument("two", "The second")
+              )
+            )
+          ),
+          None,
+          None,
+          None
+        )
+
+      DocParserWrapper.generateSections(comment) shouldEqual expected
+    }
+
+    "generate examples" in {
+      val comment =
+        """ Synopsis
+          |
+          | > Example
+          | First line
+          | Second line
+          |
+          |     import Standard.Table
+          |
+          |     example =
+          |         1
+          |         2
+          |""".stripMargin
+      val expected =
+        DocSections(None, Some("<p>Synopsis </p>"), None, None, None, None)
 
       DocParserWrapper.generateSections(comment) shouldEqual expected
     }
