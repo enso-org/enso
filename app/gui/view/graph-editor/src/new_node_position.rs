46eb89cdc5acd2627a2f3753903d6803a4707955
Mateusz Czapliński <mateusz.czaplinski@enso.org>
2022-03-24 14:33:24 +0100

    more comments & naming

--- app/gui/view/graph-editor/src/new_node_position.rs
+++ app/gui/view/graph-editor/src/new_node_position.rs
@@ -68,7 +68,7 @@ pub fn under_selection(graph_editor: &GraphEditorModel) -> Vector2 {
         let node_bbox = graph_editor.node_bounding_box(node_id);
         min_bbox_bottom = min(min_bbox_bottom, node_bbox.bottom());
     }
-    below_line_and_pushed_left(graph_editor, min_bbox_bottom, x)
+    below_line_left_aligned(graph_editor, min_bbox_bottom, x)
 }
 
 /// Return a position for a newly created node. Returns a position closely below the `node_id` node
@@ -79,17 +79,25 @@ pub fn under_selection(graph_editor: &GraphEditorModel) -> Vector2 {
 pub fn under(graph_editor: &GraphEditorModel, node_id: NodeId) -> Vector2 {
     let above_node_pos = graph_editor.node_position(node_id);
     let above_node_bbox = graph_editor.node_bounding_box(node_id);
-    below_line_and_pushed_left(graph_editor, above_node_pos.bottom(), above_node_pos.x)
+    below_line_left_aligned(graph_editor, above_node_pos.bottom(), above_node_pos.x)
 }
 
-pub fn below_line_and_pushed_left(
+/// Return a position for a newly created node. Returns a position left-aligned to `align_x`
+/// closely below a horizontal line at `line_y`, or a first available position to the left if the
+/// initial position is not available.
+///
+/// "Closely below" means that a vertical gap is maintained between the line and the top border of
+/// a node placed at the returned position. The vertical gap is equal to
+/// [`theme::graph_editor::default_y_gap_between_nodes`].
+/// Availability of a position is defined in the docs of [`on_ray`].
+pub fn below_line_left_aligned(
     graph_editor: &GraphEditorModel,
-    below_y: f32,
-    max_x: f32,
+    line_y: f32,
+    align_x: f32,
 ) -> Vector2 {
     let y_gap = graph_editor.frp.default_y_gap_between_nodes.value();
     let y_offset = y_gap + node::HEIGHT / 2.0;
-    let starting_point = Vector2(max_x, below_y - y_offset);
+    let starting_point = Vector2(align_x, line_y - y_offset);
     let direction = Vector2(-1.0, 0.0);
     on_ray(graph_editor, starting_point, direction).unwrap()
 }
