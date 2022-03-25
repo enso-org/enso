a34c2bcfe607b9141bc8c2465679f3c74aa3650e
Dmitry Bushev <bushevdv@gmail.com>
2022-03-25 10:49:37 +0300

    Implement Documentation Sections API (#3360)

--- engine/runtime/src/main/scala/org/enso/compiler/context/SuggestionDiff.scala
+++ engine/runtime/src/main/scala/org/enso/compiler/context/SuggestionDiff.scala
@@ -21,15 +21,7 @@ object SuggestionDiff {
       .filter {
         case Api.SuggestionUpdate(
               _,
-              Api.SuggestionAction.Modify(
-                None,
-                None,
-                None,
-                None,
-                None,
-                None,
-                None
-              )
+              Api.SuggestionAction.Modify(None, None, None, None, None, None)
             ) =>
           false
         case _ =>
@@ -168,9 +160,6 @@ object SuggestionDiff {
     if (e1.documentation != e2.documentation) {
       op = op.copy(documentation = Some(e2.documentation))
     }
-    if (e1.documentationHtml != e2.documentationHtml) {
-      op = op.copy(documentationHtml = Some(e2.documentationHtml))
-    }
     Api.SuggestionUpdate(e1, op)
   }
 
@@ -190,9 +179,6 @@ object SuggestionDiff {
     }
     if (e1.documentation != e2.documentation) {
       op = op.copy(documentation = Some(e2.documentation))
-    }
-    if (e1.documentationHtml != e2.documentationHtml) {
-      op = op.copy(documentationHtml = Some(e2.documentationHtml))
     }
     Api.SuggestionUpdate(e1, op)
   }
