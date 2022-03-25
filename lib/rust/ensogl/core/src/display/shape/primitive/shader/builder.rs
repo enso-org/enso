e5a3d3e4b4615d87720a230afe5d3bc4a1dfee4e
Kaz Wesley <kaz@lambdaverse.org>
2022-03-25 13:14:26 -0700

    Build and memoize a GLSL prelude

--- lib/rust/ensogl/core/src/display/shape/primitive/shader/builder.rs
+++ lib/rust/ensogl/core/src/display/shape/primitive/shader/builder.rs
@@ -46,17 +46,8 @@ impl Builder {
         let defs =
             iformat!("{defs_header}\n\n{sdf_defs}\n\n\n\n{shape_header}\n\n{canvas.to_glsl()}");
 
-        let redirections = overload::builtin_redirections();
-        let math = overload::allow_overloading(MATH);
-        let color = overload::allow_overloading(COLOR);
-        let debug = overload::allow_overloading(DEBUG);
-        let shape = overload::allow_overloading(SHAPE);
-
         let defs = overload::allow_overloading(&defs);
-        let code = format!(
-            "{}\n\n{}\n\n{}\n\n{}\n\n{}\n\n{}",
-            redirections, math, color, debug, shape, defs
-        );
+        let code = [GLSL_PRELUDE.as_str(), &defs].join("\n\n");
         let main = format!(
             "bool pointer_events_enabled = {};\n{}",
             pointer_events_enabled, FRAGMENT_RUNNER
@@ -74,4 +65,20 @@ fn header(label: &str) -> String {
     let border_len = label.len() + 8;
     let border = "=".repeat(border_len);
     iformat!("// {border}\n// === {label} ===\n// {border}")
+}
+
+// == GLSL_PRELUDE ==
+
+lazy_static! {
+    /// A common preamble used to start every shader program.
+    static ref GLSL_PRELUDE: String = make_glsl_prelude();
+}
+
+fn make_glsl_prelude() -> String {
+    let redirections = overload::builtin_redirections();
+    let math = overload::allow_overloading(MATH);
+    let color = overload::allow_overloading(COLOR);
+    let debug = overload::allow_overloading(DEBUG);
+    let shape = overload::allow_overloading(SHAPE);
+    [redirections, math, color, debug, shape].join("\n\n")
 }
