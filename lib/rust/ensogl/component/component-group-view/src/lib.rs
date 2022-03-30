91fb726a74218362491762aa9c4404b7a1847517
Mateusz Czapliński <mateusz.czaplinski@enso.org>
2022-03-30 12:00:53 +0200

    WIP start adding files for CGV in lib/rust/ensogl/

---
+++ lib/rust/ensogl/component/component-group-view/src/lib.rs
@@ -0,0 +1,39 @@
+
+#![recursion_limit = "512"]
+// === Features ===
+#![feature(option_result_contains)]
+#![feature(trait_alias)]
+// === Standard Linter Configuration ===
+#![deny(non_ascii_idents)]
+#![warn(unsafe_code)]
+// === Non-Standard Linter Configuration ===
+#![warn(missing_copy_implementations)]
+#![warn(missing_debug_implementations)]
+#![warn(missing_docs)]
+#![warn(trivial_casts)]
+#![warn(trivial_numeric_casts)]
+#![warn(unused_import_braces)]
+#![warn(unused_qualifications)]
+
+
+
+/// Commonly used types and functions.
+pub mod prelude {
+    pub use ensogl_core::prelude::*;
+}
+
+use crate::prelude::*;
+
+use enso_frp as frp;
+use ensogl_core::application;
+use ensogl_core::application::shortcut;
+use ensogl_core::application::Application;
+use ensogl_core::display;
+use ensogl_core::display::scene::layer::LayerId;
+use ensogl_core::display::shape::*;
+use ensogl_core::DEPRECATED_Animation;
+use ensogl_hardcoded_theme as theme;
+use ensogl_shadow as shadow;
+
+
+
