cd5c38ef3bdfed3e90b02eb347b3a8fd0d9d92d9
Kaz Wesley <kaz@lambdaverse.org>
2022-03-31 13:36:30 -0700

    error reporting

--- lib/rust/ensogl/core/src/system/gpu/shader.rs
+++ lib/rust/ensogl/core/src/system/gpu/shader.rs
@@ -2,24 +2,13 @@
 #![allow(missing_docs)]
 
 use enso_prelude::*;
-use enso_web::traits::*;
 
 use crate::system::Context;
 
-use enso_web as web;
 use js_sys::Float32Array;
 use web_sys::WebGlBuffer;
 use web_sys::WebGlProgram;
 use web_sys::WebGlShader;
-
-
-// ==============
-// === Export ===
-// ==============
-
-#[warn(missing_docs)]
-pub mod glsl;
-pub mod compiler;
 
 
 
@@ -27,6 +16,9 @@ pub mod compiler;
 // === Exports ===
 // ===============
 
+#[warn(missing_docs)]
+pub mod glsl;
+pub mod compiler;
 /// Common types.
 pub mod types {
     pub use super::glsl;
@@ -47,229 +39,72 @@ pub type Program = WebGlProgram;
 
 
 
+// ==============
+// === VfPair ===
+// ==============
+
+#[derive(Copy, Clone, Debug)]
+pub struct VfPair<T> {
+    pub vertex:   T,
+    pub fragment: T,
+}
+
+impl<T> VfPair<T> {
+    /// Apply a function to both shaders.
+    pub fn for_each<F>(self, mut f: F) where F: FnMut(T) {
+        f(self.vertex);
+        f(self.fragment);
+    }
+
+    /// Apply a function to both shaders, stopping if an error is encountered.
+    pub fn try_for_each<F, E>(self, mut f: F) -> Result<(), E>
+        where F: FnMut(T) -> Result<(), E> {
+        f(self.vertex)?;
+        f(self.fragment)?;
+        Ok(())
+    }
+
+    /// Apply a function to both shaders; return the pair of results.
+    pub fn map<F, U>(self, mut f: F) -> VfPair<U> where F: FnMut(T) -> U {
+        let Self { vertex, fragment } = self;
+        let vertex = f(vertex);
+        let fragment = f(fragment);
+        VfPair { vertex, fragment }
+    }
+
+    /// Apply a function that returns an option to both shaders; return the pair of results, unless
+    /// the function returned None for either shader.
+    pub fn maybe_map<F, U>(self, mut f: F) -> Option<VfPair<U>> where F: FnMut(T) -> Option<U> {
+        let Self { vertex, fragment } = self;
+        let vertex = f(vertex)?;
+        let fragment = f(fragment)?;
+        Some(VfPair { vertex, fragment })
+    }
+
+    /// Combine the vertex components from each value, and the fragment components from each value,
+    /// into a new pair of tuples.
+    pub fn zip<U>(self, other: VfPair<U>) -> VfPair<(T, U)> {
+        let vertex = (self.vertex, other.vertex);
+        let fragment = (self.fragment, other.fragment);
+        VfPair { vertex, fragment }
+    }
+
+    /// Return a pair borrowing its values from this pair.
+    pub fn as_ref(&self) -> VfPair<&T> {
+        let vertex = &self.vertex;
+        let fragment = &self.fragment;
+        VfPair { vertex, fragment }
+    }
+}
+
+
+
 // ==================
 // === ShaderCode ===
 // ==================
 
 /// GLSL source code of a shader program.
-#[derive(Clone, Debug)]
-pub struct ShaderCode {
-    pub vertex:   String,
-    pub fragment: String,
-}
-
-
-
-// =============
-// === Error ===
-// =============
-
-#[derive(Debug, Fail)]
-pub enum SingleTargetError {
-    #[fail(display = "Unable to create {}.", target)]
-    Create { target: ErrorTarget },
-    #[fail(display = "Unable to compile {}.\n{}\n\n{}", target, message, preview_code)]
-    Compile { target: ErrorTarget, message: String, preview_code: String },
-}
-
-#[derive(Debug, Fail, From)]
-pub enum Error {
-    Create {
-        target: ErrorTarget,
-    },
-
-    Compile {
-        js_path:  String,
-        vertex:   Option<SingleTargetError>,
-        fragment: Option<SingleTargetError>,
-    },
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Create { target } => write!(f, "Unable to create {}", target),
-            Self::Compile { js_path, vertex, fragment } => {
-                let vtx_msg = vertex.as_ref().map(|t| format!("\n\n{}", t)).unwrap_or_default();
-                let frag_msg = fragment.as_ref().map(|t| format!("\n\n{}", t)).unwrap_or_default();
-                let run_msg = |n| {
-                    format!("Run `console.log({}.{})` to inspect the {} shader.", js_path, n, n)
-                };
-                let vtx_run_msg = run_msg("vertex");
-                let frag_run_msg = run_msg("fragment");
-                let err_msg = "Unable to create shader.";
-                write!(f, "{}\n{}\n{}{}{}", err_msg, vtx_run_msg, frag_run_msg, vtx_msg, frag_msg)
-            }
-        }
-    }
-}
-
-#[derive(Copy, Clone, Debug, Fail)]
-pub enum ErrorTarget {
-    #[fail(display = "shader")]
-    Shader,
-    #[fail(display = "program")]
-    Program,
-}
-
-
-
-// ===================
-// === Compilation ===
-// ===================
-
-/// Compilation error containing detailed error message.
-#[derive(Debug)]
-pub struct CompilationError(String);
-
-impl std::error::Error for CompilationError {}
-
-impl Display for CompilationError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-/// Abstraction for [`Shader`] and [`Program`] error handling.
-pub trait CompilationTarget {
-    /// Check whether the target was assembled correctly. In the context of [`Shader`], it checks if
-    /// the compilation was successful. In the context of [`Program`], it checks whether it was
-    /// linked successfully. In case of lost context, this function will succeed. For more
-    /// information of why, see: https://www.khronos.org/webgl/wiki/HandlingContextLost.
-    fn check(&self, ctx: &Context) -> Result<(), CompilationError>;
-}
-
-impl CompilationTarget for Shader {
-    fn check(&self, ctx: &Context) -> Result<(), CompilationError> {
-        let status = Context::COMPILE_STATUS;
-        let status_ok = ctx.get_shader_parameter(self, status).as_bool().unwrap_or(false);
-        let context_lost = ctx.is_context_lost();
-        let ok = (status_ok || context_lost).then_some(());
-        ok.ok_or_else(|| CompilationError(unwrap_error(ctx.get_shader_info_log(self))))
-    }
-}
-
-impl CompilationTarget for Program {
-    fn check(&self, ctx: &Context) -> Result<(), CompilationError> {
-        let status = Context::LINK_STATUS;
-        let status_ok = ctx.get_program_parameter(self, status).as_bool().unwrap_or(false);
-        let context_lost = ctx.is_context_lost();
-        let ok = (status_ok || context_lost).then_some(());
-        ok.ok_or_else(|| CompilationError(unwrap_error(ctx.get_program_info_log(self))))
-    }
-}
-
-fn unwrap_error(opt_err: Option<String>) -> String {
-    opt_err.unwrap_or_else(|| "Unknown error.".into())
-}
-
-
-
-// ======================
-// === Compile / Link ===
-// ======================
-
-pub fn compile_vertex_shader(ctx: &Context, src: &str) -> Result<Shader, SingleTargetError> {
-    compile_shader(ctx, Context::VERTEX_SHADER, src)
-}
-
-pub fn compile_fragment_shader(ctx: &Context, src: &str) -> Result<Shader, SingleTargetError> {
-    compile_shader(ctx, Context::FRAGMENT_SHADER, src)
-}
-
-pub fn compile_shader(ctx: &Context, tp: u32, src: &str) -> Result<Shader, SingleTargetError> {
-    let target = ErrorTarget::Shader;
-    let shader = ctx.create_shader(tp).ok_or(SingleTargetError::Create { target })?;
-    ctx.shader_source(&shader, src);
-    ctx.compile_shader(&shader);
-    match shader.check(ctx) {
-        Ok(_) => Ok(shader),
-        Err(CompilationError(message)) => {
-            let code: String = src.into();
-            let lines = code.split('\n').collect::<Vec<&str>>();
-            let lines_num = lines.len();
-            let lines_str_len = (lines_num as f32).log10().ceil() as usize;
-            let lines_enum = lines.into_iter().enumerate();
-            let lines_with_num =
-                lines_enum.map(|(n, l)| format!("{1:0$} : {2}", lines_str_len, n + 1, l));
-            let lines_with_num = lines_with_num.collect::<Vec<String>>();
-            let code_with_num = lines_with_num.join("\n");
-            let error_loc_pfx = "ERROR: 0:";
-            let preview_code = if let Some(msg) = message.strip_prefix(error_loc_pfx) {
-                let line_num: String = msg.chars().take_while(|c| c.is_digit(10)).collect();
-                let line_num = line_num.parse::<usize>().unwrap() - 1;
-                let preview_radius = 5;
-                let preview_line_start = std::cmp::max(0, line_num - preview_radius);
-                let preview_line_end = std::cmp::min(lines_num, line_num + preview_radius);
-                lines_with_num[preview_line_start..preview_line_end].join("\n")
-            } else {
-                code_with_num
-            };
-            Err(SingleTargetError::Compile { target, message, preview_code })
-        }
-    }
-}
-
-/// Structure representing one of two states – an error, or lack of values because of a lost
-/// context.
-#[derive(Debug)]
-pub enum ContextLossOrError {
-    ContextLoss,
-    Error(Error),
-}
-
-/// Link the provided vertex and fragment shaders into a program.
-pub fn link_program(
-    ctx: &Context,
-    vert_shader: &Shader,
-    frag_shader: &Shader,
-) -> Result<Program, ContextLossOrError> {
-    let target = ErrorTarget::Program;
-    match ctx.create_program() {
-        None => Err(if ctx.is_context_lost() {
-            ContextLossOrError::ContextLoss
-        } else {
-            ContextLossOrError::Error(Error::Create { target })
-        }),
-        Some(program) => {
-            ctx.attach_shader(&program, vert_shader);
-            ctx.attach_shader(&program, frag_shader);
-            ctx.link_program(&program);
-            Ok(program)
-        }
-    }
-}
-
-/// Compile the provided vertex and fragment shader sources and then link them into a program.
-pub fn compile_program(
-    ctx: &Context,
-    vert_src: &str,
-    frag_src: &str,
-) -> Result<Program, ContextLossOrError> {
-    let vert_shader = compile_vertex_shader(ctx, vert_src);
-    let frag_shader = compile_fragment_shader(ctx, frag_src);
-    match (vert_shader, frag_shader) {
-        (Ok(vert_shader), Ok(frag_shader)) => link_program(ctx, &vert_shader, &frag_shader),
-        (vert_shader, frag_shader) => {
-            let vertex = vert_shader.err();
-            let fragment = frag_shader.err();
-
-            // FIXME: this should be taken from config and refactored to a function
-            let path = &["enso", "debug", "shader"];
-            let shader_dbg = web::Reflect::get_nested_object_or_create(&web::window, path);
-            let shader_dbg = shader_dbg.unwrap();
-            let len = web::Object::keys(&shader_dbg).length();
-            let debug_var_name = format!("shader_{}", len);
-            let shader_tgt_dbg = web::Object::new();
-            web::Reflect::set(&shader_dbg, &(&debug_var_name).into(), &shader_tgt_dbg).ok();
-            web::Reflect::set(&shader_tgt_dbg, &"vertex".into(), &vert_src.into()).ok();
-            web::Reflect::set(&shader_tgt_dbg, &"fragment".into(), &frag_src.into()).ok();
-
-            let js_path = format!("window.{}.{}", path.join("."), debug_var_name);
-            Err(ContextLossOrError::Error(Error::Compile { js_path, vertex, fragment }))
-        }
-    }
-}
+pub type ShaderCode = VfPair<String>;
 
 
 
--- lib/rust/ensogl/core/src/system/gpu/shader/compiler.rs
+++ lib/rust/ensogl/core/src/system/gpu/shader/compiler.rs
@@ -1,15 +1,30 @@
 //! Asynchronous interface to WebGL GLSL shader program compilation.
 //!
-//! Follows
-//! [best practices](https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/WebGL_best_practices),
-//! in particular:
+//! # Performance
+//!
+//! In order to maximize parallelism and avoid blocking, follows
+//! [best practices](https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/WebGL_best_practices).
+//!
+//! In particular, these items from that document are implemented here:
 //! - "Compile Shaders and Link Programs in parallel"
 //! - "Prefer KHR_parallel_shader_compile"
 //! - "Don't check shader compile status unless linking fails"
+//!
+//! # Context loss
+//!
+//! Many functions in this module must handle
+//! [context loss](https://www.khronos.org/webgl/wiki/HandlingContextLost).
+//!
+//! In the case of context loss, no result is available, whether successful or an error; this
+//! possibility is represented here with the [`Option`] type.
+
+use enso_web as web;
+use enso_web::traits::*;
 
 use crate::prelude::*;
 use crate::system::gpu::shader::Context;
 use crate::system::gpu::shader::ShaderCode;
+use crate::system::gpu::shader::VfPair;
 
 use web_sys::WebGlProgram;
 use web_sys::WebGlShader;
@@ -32,8 +47,10 @@ pub struct Compiler {
 }
 
 impl Compiler {
-    /// Compile a shader program. The provided callback will be called when it is ready, which will
-    /// likely not occur until a later frame.
+    /// Compile a shader program.
+    ///
+    /// The provided callback will be called when compilation completes, unless the context is
+    /// lost.
     pub fn compile<F>(&mut self, code: ShaderCode, callback: Box<F>)
     where F: FnOnce(Result) + 'static {
         let work = CompilableShaders { code };
@@ -43,13 +60,12 @@ impl Compiler {
     /// Submit all new jobs to the WebGL engine for compilation.
     #[profile(Debug)]
     pub fn start_jobs(&mut self, context: &Context) {
-        // First start all shaders compiling, then start programs linking.
-        // This maximizes the browser's parallelism:
-        // https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/WebGL_best_practices#compile_shaders_and_link_programs_in_parallel
+        // To maximize parallelism, start all compilation processes before starting linking.
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/WebGL_best_practices#compile_shaders_and_link_programs_in_parallel
         let new_jobs = self.new_jobs.drain(..);
-        let compiling = new_jobs.filter_map(|job| job.map_work(|work| work.compile(context)));
+        let compiling = new_jobs.filter_map(|job| job.maybe_map_work(|work| work.compile(context)));
         let compiling = compiling.collect::<Vec<_>>().into_iter();
-        let linking = compiling.filter_map(|job| job.map_work(|work| work.link(context)));
+        let linking = compiling.filter_map(|job| job.maybe_map_work(|work| work.link(context)));
         self.started_jobs.extend(linking);
     }
 
@@ -57,8 +73,9 @@ impl Compiler {
     #[profile(Detail)]
     pub fn handle_finished_jobs(&mut self, context: &Context) {
         let ready_checker = self.ready_checker.get_or_insert_with(|| ReadyChecker::new(context));
-        let is_ready = |program| ready_checker.is_ready(context, program).unwrap_or(true);
-        let ready_jobs = self.started_jobs.drain_filter(|job| is_ready(&job.work));
+        let ready_jobs = self
+            .started_jobs
+            .drain_filter(|job| ready_checker.is_ready(context, &job.work).unwrap_or(true));
         for Job { callback, work } in ready_jobs {
             work.block_until_finished(context).for_each(callback);
         }
@@ -71,14 +88,120 @@ impl Compiler {
 // === Error ===
 // =============
 
-#[derive(Debug)]
+#[derive(Debug, Fail)]
 pub struct Error {
-    program_info_log:         Option<String>,
-    vertex_shader_info_log:   Option<String>,
-    fragment_shader_info_log: Option<String>,
+    /// Linking log, if present.
+    link_log: Option<String>,
+    /// Information about compiling shaders.
+    shaders:  VfPair<ShaderInfo>,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let js_path = write_debug_objects(&self.shaders);
+        let run_msg = VF_DBG_KEYS.clone().map(|n|
+            format!("Run `console.log({}.{})` to inspect the {} shader.", js_path, n, n));
+        let err_msg = "Unable to compile shader.";
+        write!(f, "{}\n{}\n{}", err_msg, run_msg.vertex, run_msg.fragment)?;
+        // If a compile-time failure occurred, we only need to report on the affected shader(s);
+        // if the error occurred when linking, report on all shaders.
+        let fail_time = match self.shaders.vertex.ok && self.shaders.fragment.ok {
+            true => FailTime::Link,
+            false => FailTime::Compile,
+        };
+        match fail_time {
+            FailTime::Compile => self.shaders.as_ref().try_for_each(|shader| {
+                if !shader.ok {
+                    write!(f, "\n\n{}", shader)?;
+                }
+                Ok(())
+            })?,
+            FailTime::Link => {
+                self.shaders.as_ref().try_for_each(|shader| write!(f, "\n\n{}", shader))?;
+                if let Some(link_log) = self.link_log.as_ref() {
+                    write!(f, "\n\n{}", &link_log)?;
+                }
+            }
+        }
+        Ok(())
+    }
 }
 
 pub type Result = std::result::Result<WebGlProgram, Error>;
+
+#[derive(PartialEq, Eq)]
+enum FailTime {
+    Compile,
+    Link,
+}
+
+// FIXME: this should be taken from config and refactored to a function
+const VF_DBG_KEYS: VfPair<&str> = VfPair { vertex: "vertex", fragment: "fragment" };
+
+fn write_debug_objects(shaders: &VfPair<ShaderInfo>) -> String {
+    // FIXME: this should be taken from config and refactored to a function along with
+    //  `VF_DBG_KEYS` above
+    let path = &["enso", "debug", "shader"];
+    let shader_dbg = web::Reflect::get_nested_object_or_create(&web::window, path);
+    let shader_dbg = shader_dbg.unwrap();
+    let len = web::Object::keys(&shader_dbg).length();
+    let debug_var_name = format!("shader_{}", len);
+    let shader_tgt_dbg = web::Object::new();
+    web::Reflect::set(&shader_dbg, &(&debug_var_name).into(), &shader_tgt_dbg).unwrap();
+    let keys = VF_DBG_KEYS.map(enso_web::JsValue::from);
+    shaders.as_ref().zip(keys).for_each(|(shader, key)| {
+        let code = shader.source_code.as_str().into();
+        web::Reflect::set(&shader_tgt_dbg, &key, &code).unwrap();
+    });
+
+    format!("window.{}.{}", path.join("."), debug_var_name)
+}
+
+
+
+// ==================
+// === ShaderInfo ===
+// ==================
+
+#[derive(Debug)]
+pub struct ShaderInfo {
+    ok:          bool,
+    info_log:    Option<String>,
+    source_code: String,
+}
+
+impl Display for ShaderInfo {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Shader info:\n")?;
+        let info_log = self.info_log.as_ref().map(String::as_str).unwrap_or(match self.ok {
+            true => "No compile-time error.",
+            false => "Unknown error.",
+        });
+        let preview_code = fmt_preview(self);
+        write!(f, "{}\n\n{}", info_log, preview_code)
+    }
+}
+
+fn fmt_preview(shader: &ShaderInfo) -> String {
+    let lines = shader.source_code.split('\n').collect::<Vec<&str>>();
+    let lines_num = lines.len();
+    let lines_str_len = (lines_num as f32).log10().ceil() as usize;
+    let lines_enum = lines.into_iter().enumerate();
+    let lines_with_num = lines_enum.map(|(n, l)| format!("{1:0$} : {2}", lines_str_len, n + 1, l));
+    let lines_with_num = lines_with_num.collect::<Vec<String>>();
+    let code_with_num = lines_with_num.join("\n");
+    let error_loc_pfx = "ERROR: 0:";
+    if let Some(msg) = shader.info_log.as_ref().map(String::as_str).unwrap_or_default().strip_prefix(error_loc_pfx) {
+        let line_num: String = msg.chars().take_while(|c| c.is_digit(10)).collect();
+        let line_num = line_num.parse::<usize>().unwrap() - 1;
+        let preview_radius = 5;
+        let preview_line_start = std::cmp::max(0, line_num - preview_radius);
+        let preview_line_end = std::cmp::min(lines_num, line_num + preview_radius);
+        lines_with_num[preview_line_start..preview_line_end].join("\n")
+    } else {
+        code_with_num
+    }
+}
 
 
 
@@ -98,10 +221,23 @@ struct Job<T> {
 
 impl<T> Job<T> {
     /// Return a new job with the work transformed by a function, if the function returns a value.
-    fn map_work<U, F>(self, f: F) -> Option<Job<U>> where F: FnOnce(T) -> Option<U> {
+    fn maybe_map_work<U, F>(self, f: F) -> Option<Job<U>>
+    where F: FnOnce(T) -> Option<U> {
         let Job { callback, work } = self;
         f(work).map(|work| Job { callback, work })
     }
+}
+
+
+
+// ==============
+// === Shader ===
+// ==============
+
+#[derive(Debug)]
+struct Shader {
+    shader:      WebGlShader,
+    source_code: String,
 }
 
 
@@ -113,24 +249,25 @@ impl<T> Job<T> {
 /// Complete program code, ready to be compiled.
 #[derive(Debug)]
 struct CompilableShaders {
-    code: ShaderCode,
+    code: VfPair<String>,
 }
 
 impl CompilableShaders {
     /// Starts shaders compiling, unless context is lost.
     fn compile(self, context: &Context) -> Option<LinkableShaders> {
-        let vertex = compile_shader(context, Context::VERTEX_SHADER, &self.code.vertex)?;
-        let fragment = compile_shader(context, Context::FRAGMENT_SHADER, &self.code.fragment)?;
-        Some(LinkableShaders { vertex, fragment })
+        let types = VfPair { vertex: Context::VERTEX_SHADER, fragment: Context::FRAGMENT_SHADER };
+        let shaders =
+            self.code.zip(types).maybe_map(|(code, tp)| compile_shader(context, tp, code))?;
+        Some(LinkableShaders { shaders })
     }
 }
 
 /// Returns a compiling shader, unless context is lost.
-fn compile_shader(context: &Context, tp: u32, src: &str) -> Option<WebGlShader> {
+fn compile_shader(context: &Context, tp: u32, source_code: String) -> Option<Shader> {
     let shader = context.create_shader(tp)?;
-    context.shader_source(&shader, src);
+    context.shader_source(&shader, &source_code);
     context.compile_shader(&shader);
-    Some(shader)
+    Some(Shader { shader, source_code })
 }
 
 
@@ -142,8 +279,7 @@ fn compile_shader(context: &Context, tp: u32, src: &str) -> Option<WebGlShader> 
 /// Shaders ready to be linked; they may still be compiling.
 #[derive(Debug)]
 struct LinkableShaders {
-    vertex:   WebGlShader,
-    fragment: WebGlShader,
+    shaders: VfPair<Shader>,
 }
 
 impl LinkableShaders {
@@ -154,10 +290,9 @@ impl LinkableShaders {
     /// linking are finished.
     fn link(self, context: &Context) -> Option<LinkingProgram> {
         let program = context.create_program()?;
-        context.attach_shader(&program, &self.vertex);
-        context.attach_shader(&program, &self.fragment);
+        let shaders = self.shaders;
+        shaders.as_ref().for_each(|shader| context.attach_shader(&program, &shader.shader));
         context.link_program(&program);
-        let shaders = self;
         Some(LinkingProgram { program, shaders })
     }
 }
@@ -172,7 +307,7 @@ impl LinkableShaders {
 #[derive(Debug)]
 struct LinkingProgram {
     program: WebGlProgram,
-    shaders: LinkableShaders,
+    shaders: VfPair<Shader>,
 }
 
 impl LinkingProgram {
@@ -180,21 +315,27 @@ impl LinkingProgram {
     /// lost.
     fn block_until_finished(self, context: &Context) -> Option<Result> {
         let LinkingProgram { program, shaders } = self;
-        let status = Context::LINK_STATUS;
-        let ok = context.get_program_parameter(&program, status).as_bool().unwrap_or(false);
-        if !ok {
-            if context.is_context_lost() {
-                return None;
-            }
-            let program_info_log = context.get_program_info_log(&program);
-            let vertex_shader_info_log = context.get_shader_info_log(&shaders.vertex);
-            let fragment_shader_info_log = context.get_shader_info_log(&shaders.fragment);
-            let error =
-                Error { program_info_log, vertex_shader_info_log, fragment_shader_info_log };
-            return Some(Err(error));
-        }
-        Some(Ok(program))
+        let program_ok = context.get_program_parameter(&program, Context::LINK_STATUS).as_bool()?;
+        Some(if program_ok {
+            Ok(program)
+        } else {
+            // If the context is lost we can't determine full error details, but in that case we
+            // don't need to handle the error anyway because we'll get another attempt at compiling
+            // (and analyzing any error) when we have a new context.
+            let link_log = context.get_program_info_log(&program);
+            let shaders = shaders.maybe_map(|shader| shader_info(context, shader))?;
+            Err(Error { link_log, shaders })
+        })
     }
+}
+
+/// Return information about shader compilation, unless context was lost.
+fn shader_info(context: &Context, shader: Shader) -> Option<ShaderInfo> {
+    let Shader { shader, source_code } = shader;
+    let param = Context::COMPILE_STATUS;
+    let ok = context.get_shader_parameter(&shader, param).as_bool()?;
+    let info_log = context.get_shader_info_log(&shader);
+    Some(ShaderInfo { ok, info_log, source_code })
 }
 
 
@@ -210,8 +351,8 @@ struct ReadyChecker {
 }
 
 impl ReadyChecker {
-    /// Select the best available ready-checker for the context.
-    pub fn new(context: &Context) -> Self {
+    /// Select the best available ready-checking implementation for the context.
+    fn new(context: &Context) -> Self {
         let khr = KhrParallelShaderCompile::new(context);
         if khr.is_none() && !context.is_context_lost() {
             // TODO: log
@@ -220,8 +361,8 @@ impl ReadyChecker {
     }
 
     /// Check if the job is ready, or return None if unknown.
-    pub fn is_ready(&self, context: &Context, job: &LinkingProgram) -> Option<bool> {
-        self.khr.and_then(|khr| khr.is_ready(context, job))
+    fn is_ready(&self, context: &Context, job: &LinkingProgram) -> Option<bool> {
+        self.khr.as_ref().and_then(|khr| khr.is_ready(context, job))
     }
 }
 
@@ -238,7 +379,7 @@ struct KhrParallelShaderCompile {
 
 impl KhrParallelShaderCompile {
     /// Try to obtain the extension.
-    pub fn new(context: &Context) -> Option<Self> {
+    fn new(context: &Context) -> Option<Self> {
         let ext = context.get_extension("KHR_parallel_shader_compile").ok()??;
         let completion_status_khr =
             js_sys::Reflect::get(&ext, &"COMPLETION_STATUS_KHR".into()).ok()?.as_f64()? as u32;
@@ -246,7 +387,7 @@ impl KhrParallelShaderCompile {
     }
 
     /// Asynchronously check if the job is ready.
-    pub fn is_ready(&self, context: &Context, job: &LinkingProgram) -> Option<bool> {
+    fn is_ready(&self, context: &Context, job: &LinkingProgram) -> Option<bool> {
         let param = self.completion_status_khr;
         context.get_program_parameter(&job.program, param).as_bool()
     }
