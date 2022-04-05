5c23f91b66b6c98ef6eb85bb41705b4ee824b47d
James Dunkerley <james.dunkerley@gmail.com>
2022-04-05 10:29:49 +0100

    WIP Push

--- std-bits/table/src/main/java/org/enso/table/aggregations/First.java
+++ std-bits/table/src/main/java/org/enso/table/aggregations/First.java
@@ -1,8 +1,10 @@
 package org.enso.table.aggregations;
 
 import org.enso.table.data.column.storage.Storage;
+import org.enso.table.data.index.MultiValueKey;
 import org.enso.table.data.table.Column;
 
+import java.util.Arrays;
 import java.util.List;
 
 /***
@@ -10,21 +12,47 @@ import java.util.List;
  */
 public class First extends Aggregator {
   private final Storage storage;
+  private final Storage[] ordering;
   private final boolean ignoreNothing;
 
   public First(String name, Column column, boolean ignoreNothing) {
+    this(name, column, new Column[0], ignoreNothing);
+  }
+
+  public First(String name, Column column, Column[] ordering, boolean ignoreNothing) {
     super(name, Storage.Type.OBJECT);
     this.storage = column.getStorage();
+    this.ordering = ordering == null ? new Storage[0] : Arrays.stream(ordering).toArray(Storage[]::new);
     this.ignoreNothing = ignoreNothing;
   }
 
   @Override
   public Object aggregate(List<Integer> indexes) {
-    for (int row: indexes) {
-      Object value = storage.getItemBoxed(row);
-      if (!ignoreNothing || value != null) {
-        return value;
+    if (ordering.length == 0) {
+      for (int row : indexes) {
+        Object value = storage.getItemBoxed(row);
+        if (!ignoreNothing || value != null) {
+          return value;
+        }
       }
+    } else {
+      MultiValueKey key = null;
+      Object current = null;
+
+      for (int row : indexes) {
+        Object value = storage.getItemBoxed(row);
+        if (ignoreNothing && value == null) {
+          continue;
+        }
+
+        MultiValueKey newKey = new MultiValueKey(Arrays.stream(this.ordering).map(o -> o.getItemBoxed(row)).toArray());
+        if (key == null || key.compareTo(newKey) > 0) {
+          key = newKey;
+          current = storage.getItemBoxed(row);
+        }
+      }
+
+      return current;
     }
     return null;
   }
--- std-bits/table/src/main/java/org/enso/table/data/index/MultiValueKey.java
+++ std-bits/table/src/main/java/org/enso/table/data/index/MultiValueKey.java
@@ -2,7 +2,7 @@ package org.enso.table.data.index;
 
 import java.util.Arrays;
 
-public class MultiValueKey {
+public class MultiValueKey implements Comparable<MultiValueKey> {
   private final Object[] values;
   private final int hashCodeValue;
   private final boolean allNull;
@@ -18,7 +18,7 @@ public class MultiValueKey {
     int h = 0;
     for (Object value: this.values) {
       if (value != null) {
-        Object folded = FoldObject(value);
+        Object folded = foldObject(value);
         floatValue = floatValue || (folded instanceof Double);
         h ^= folded.hashCode();
         allNull = false;
@@ -52,7 +52,7 @@ public class MultiValueKey {
 
   public boolean hasFloatValues() { return floatValue; }
 
-  protected static Object FoldObject(Object value) {
+  protected static Object foldObject(Object value) {
     if (value instanceof Long) {
       return value;
     } else if (value instanceof Integer) {
@@ -70,5 +70,95 @@ public class MultiValueKey {
     }
 
     return value;
+  }
+
+  public static int compareObjects(Object thisValue, Object thatValue) throws ClassCastException {
+    // NULLs
+    if (thisValue == null) {
+      if (thatValue != null) {
+        return -1;
+      }
+      return 0;
+    }
+    if (thatValue == null) {
+      return 1;
+    }
+
+    // Booleans
+    if (thisValue instanceof Boolean && thatValue instanceof Boolean) {
+      boolean thisBool = (Boolean)thisValue;
+      boolean thatBool = (Boolean)thatValue;
+      if (thisBool == thatBool) {
+        return 0;
+      }
+      return thisBool ? 1 : -1;
+    }
+
+    // Long this
+    if (thisValue instanceof Long) {
+      Long thisLong = (Long)thisValue;
+      if (thatValue instanceof Long) {
+        return thisLong.compareTo((Long)thatValue);
+      }
+      if (thatValue instanceof Double) {
+        Double thatDouble = (Double)thatValue;
+        if (thisLong > thatDouble) {
+          return 1;
+        }
+        if (thisLong < thatDouble) {
+          return -1;
+        }
+        return 0;
+      }
+    }
+
+    // Double this
+    if (thisValue instanceof Double) {
+      Double thisDouble = (Double)thisValue;
+      if (thatValue instanceof Double) {
+        return thisDouble.compareTo((Double)thatValue);
+      }
+      if (thatValue instanceof Long) {
+        Long thatLong = (Long)thatValue;
+        if (thisDouble > thatLong) {
+          return 1;
+        }
+        if (thisDouble < thatLong) {
+          return -1;
+        }
+        return 0;
+      }
+    }
+
+    // Text
+    if (thisValue instanceof String && thatValue instanceof String) {
+      // Needs to use ICU comparisons
+      return ((String)thisValue).compareTo((String)thatValue);
+    }
+
+    // DateTimes - not supported in table yet
+
+    // Give Up!
+    throw new ClassCastException("Incomparable keys.");
+  }
+
+  @Override
+  public int compareTo(MultiValueKey that) {
+    if (that == null) {
+      throw new NullPointerException();
+    }
+
+    if (that.values.length != values.length) {
+      throw new ClassCastException("Incomparable keys.");
+    }
+
+    for (int i = 0; i < values.length; i++) {
+      int comparison = compareObjects(values[i], that.values[i]);
+      if (comparison != 0) {
+        return comparison;
+      }
+    }
+
+    return 0;
   }
 }
