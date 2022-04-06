30e4bc4438a6c46ca7200ccc6426680a7305ef3f
Radosław Waśko <radoslaw.wasko@enso.org>
2022-04-06 15:24:07 +0200

    fixes for concat

--- std-bits/table/src/main/java/org/enso/table/aggregations/Concatenate.java
+++ std-bits/table/src/main/java/org/enso/table/aggregations/Concatenate.java
@@ -1,5 +1,6 @@
 package org.enso.table.aggregations;
 
+import javax.swing.JSeparator;
 import org.enso.table.data.column.storage.Storage;
 import org.enso.table.data.table.Column;
 import org.enso.table.data.table.problems.InvalidAggregation;
@@ -9,16 +10,16 @@ import java.util.List;
 
 public class Concatenate extends Aggregator {
   private final Storage storage;
-  private final String join;
+  private final String separator;
   private final String prefix;
   private final String suffix;
   private final String quote;
 
-  public Concatenate(String name, Column column, String join, String prefix, String suffix, String quote) {
+  public Concatenate(String name, Column column, String separator, String prefix, String suffix, String quote) {
     super(name, Storage.Type.STRING);
     this.storage = column.getStorage();
 
-    this.join = join == null ? "" : join;
+    this.separator = separator == null ? "" : separator;
     this.prefix = prefix;
     this.suffix = suffix;
     this.quote = quote == null ? "" : quote;
@@ -30,9 +31,9 @@ public class Concatenate extends Aggregator {
     for (int row: indexes) {
       Object value = storage.getItemBoxed(row);
       if (value == null || value instanceof String) {
-        String textValue = toQuotedString(value, quote, join);
+        String textValue = toQuotedString(value, quote, separator);
 
-        if (quote.equals("") && textValue.contains(join)) {
+        if (quote.equals("") && textValue.contains(separator)) {
           this.addProblem(new UnquotedDelimiter(this.getName(), row, "Unquoted delimiter."));
         }
 
@@ -40,7 +41,7 @@ public class Concatenate extends Aggregator {
           current = new StringBuilder();
           current.append(textValue);
         } else {
-          current.append(join);
+          current.append(separator);
           current.append(textValue);
         }
       } else {
@@ -58,14 +59,20 @@ public class Concatenate extends Aggregator {
     return current.toString();
   }
 
-  private static String toQuotedString(Object value, final String quote, final String join) {
+  private static String toQuotedString(Object value, final String quote, final String separator) {
     if (value == null) {
       return "";
     }
 
     String textValue = value.toString();
-    if (!quote.equals("") && (textValue.equals("") || textValue.contains(join))) {
-      return quote + textValue.replace(quote, quote + quote) + quote;
+    boolean quote_is_defined = !quote.isEmpty();
+    if (quote_is_defined) {
+      boolean includes_separator = !separator.isEmpty() && textValue.contains(separator);
+      boolean includes_quote = textValue.contains(quote);
+      boolean needs_quoting = textValue.isEmpty() || includes_separator || includes_quote;
+      if (needs_quoting) {
+        return quote + textValue.replace(quote, quote + quote) + quote;
+      }
     }
 
     return textValue;
