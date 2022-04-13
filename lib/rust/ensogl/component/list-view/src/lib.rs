4469aebad9a7a35bec7b2418d2e66bd4c345e074
Mateusz Czapliński <mateusz.czaplinski@enso.org>
2022-04-13 18:51:55 +0200

    review: improve formatting to better match rustfmt

--- lib/rust/ensogl/component/list-view/src/lib.rs
+++ lib/rust/ensogl/component/list-view/src/lib.rs
@@ -99,7 +99,7 @@ pub mod background {
 
     ensogl_core::define_shape_system! {
         below = [selection];
-        (style:Style,shadow_alpha:f32,corners_radius_px:f32,color:Vector4) {
+        (style:Style, shadow_alpha:f32, corners_radius_px:f32, color:Vector4) {
             let sprite_width  : Var<Pixels> = "input_size.x".into();
             let sprite_height : Var<Pixels> = "input_size.y".into();
             let width         = sprite_width - SHADOW_PX.px() * 2.0 - SHAPE_PADDING.px() * 2.0;
@@ -108,7 +108,7 @@ pub mod background {
             let rect          = Rect((&width,&height)).corners_radius(corners_radius_px);
             let shape         = rect.fill(color);
 
-            let shadow  = shadow::from_shape_with_alpha(rect.into(),&shadow_alpha,style);
+            let shadow = shadow::from_shape_with_alpha(rect.into(), &shadow_alpha, style);
 
             (shadow + shape).into()
         }
