8e7e15f743bd913ba72767e252527ace895f1e5f
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2022-04-21 11:52:01 +0200

    Cons/Nil are no longer Builtin_Types
    Looked like an unnecessary relict from the past.
    This also revealed a small issues where `Cons` fields `head` and `tail`
    conflicted with `List`s methods of the same name.
    Previously we would ignore attempt to overload the methods because List
    was builtin.

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/Cons.java
+++
@@ -1,6 +0,0 @@
-package org.enso.interpreter.node.expression.builtin;
-
-import org.enso.interpreter.dsl.BuiltinType;
-
-@BuiltinType(params="head,tail")
-public class Cons extends Builtin {}
--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/Nil.java
+++
@@ -1,6 +0,0 @@
-package org.enso.interpreter.node.expression.builtin;
-
-import org.enso.interpreter.dsl.BuiltinType;
-
-@BuiltinType
-public class Nil extends Builtin {}
