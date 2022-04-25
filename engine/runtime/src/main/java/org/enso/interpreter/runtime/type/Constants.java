c927582f32117481bcac7d903378b6406f6a89cd
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2022-04-25 17:10:20 +0200

    More Standard.Builtins purge

--- engine/runtime/src/main/java/org/enso/interpreter/runtime/type/Constants.java
+++ engine/runtime/src/main/java/org/enso/interpreter/runtime/type/Constants.java
@@ -1,6 +1,6 @@
 package org.enso.interpreter.runtime.type;
 
-/** Types defined in the Standard.Builtins module. */
+/** Hard-coded names of builtins. */
 public class Constants {
 
   public static final String ANY = "Standard.Base.Data.Any.Any";
@@ -11,12 +11,14 @@ public class Constants {
   public static final String FUNCTION = "Standard.Base.Function.Function";
   public static final String INTEGER = "Standard.Base.Data.Number.Internal.Integer";
   public static final String MANAGED_RESOURCE = "Standard.Base.Runtime.Resource.Managed_Resource";
-  public static final String MODULE_SCOPE = "Standard.Builtins.Main.Module_Scope";
   public static final String NOTHING = "Standard.Base.Nothing.Nothing";
   public static final String NUMBER = "Standard.Base.Data.Number.Internal.Number";
   public static final String PANIC = "Standard.Base.Error.Common.Panic";
   public static final String REF = "Standard.Base.Data.Ref.Ref";
   public static final String TEXT = "Standard.Base.Data.Text.Text";
+
+  // Remaining constants that are not Builtins but refer to builtin-related internals
+  public static final String MODULE_SCOPE = "Standard.Builtins.Main.Module_Scope";
   public static final String THUNK = "Standard.Builtins.Main.Thunk";
   public static final String UNRESOLVED_SYMBOL = "Standard.Builtins.Main.Unresolved_Symbol";
 }
