83fcad4ad477b704a2b9824a5a8a4ad4a3b66791
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2022-04-29 08:45:23 +0200

    paths

--- run.cmd
+++ run.cmd
@@ -1,1 +1,1 @@
-cargo build --profile buildscript --target-dir %~dp0target\enso-build --package enso-build3 && %~dp0target\enso-build\debug\enso-build3 %*
+cargo build --profile buildscript --target-dir %~dp0target\enso-build --package enso-build3 && %~dp0target\enso-build\buildscript\enso-build3 %*
--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+set -e
+# Get the directory of the script, as per https://stackoverflow.com/a/246128
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cargo build --profile buildscript --target-dir $SCRIPT_DIR/target/enso-build --package enso-build3 && $SCRIPT_DIR/target/enso-build/debug/enso-build3 "$@"
+cargo build --profile buildscript --target-dir $SCRIPT_DIR/target/enso-build --package enso-build3 --bin enso-build3
+$SCRIPT_DIR/target/enso-build/buildscript/enso-build3 $@
