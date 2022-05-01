26841f3a90e6a179d91468050bcbd9c760709793
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2022-05-01 18:14:34 +0200

    build

--- run.sh
+++ run.sh
@@ -4,10 +4,11 @@ set -e # Exit on error.
 # Get the directory of the script, as per https://stackoverflow.com/a/246128
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-TARGET_DIR="${SCRIPT_DIR}target/enso-build/"
+TARGET_DIR="${SCRIPT_DIR}/target/enso-build/"
 TARGET_EXE="${TARGET_DIR}buildscript/enso-build3"
-if [ ! -f "$TARGET_EXE" ]; then
-    cargo build --profile buildscript --target-dir "$SCRIPT_DIR" --package enso-build3
-fi
+#if [ ! -f "$TARGET_EXE" ]; then
+#    cargo build --profile buildscript --target-dir "$SCRIPT_DIR" --package enso-build3
+#fi
 
+cargo build --profile buildscript --target-dir "$SCRIPT_DIR" --package enso-build3
 "$TARGET_EXE" $@
