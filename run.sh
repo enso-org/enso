778121ce27ebc79e21f9279788c780634ce7b3a2
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2022-05-01 15:55:41 +0200

    improved sh wrapper

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-set -e
+set -e # Exit on error.
+
 # Get the directory of the script, as per https://stackoverflow.com/a/246128
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cargo build --profile buildscript --target-dir $SCRIPT_DIR/target/enso-build --package enso-build3 --bin enso-build3
-$SCRIPT_DIR/target/enso-build/buildscript/enso-build3 $@
+
+TARGET_DIR="${SCRIPT_DIR}target/enso-build/"
+TARGET_EXE="${TARGET_DIR}buildscript/enso-build3"
+if [ ! -f "$TARGET_EXE" ]; then
+    cargo build --profile buildscript --target-dir "$SCRIPT_DIR" --package enso-build3
+fi
+
+"$TARGET_EXE" $@
