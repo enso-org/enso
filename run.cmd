4ca35a73889ef4c5569e0f7897cbbae355461078
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2022-05-01 15:47:01 +0200

    improved cmd wrapper

--- run.cmd
+++ run.cmd
@@ -1,1 +1,6 @@
-cargo build --profile buildscript --target-dir %~dp0target\enso-build --package enso-build3 && %~dp0target\enso-build\buildscript\enso-build3 %*
+@ set TARGET_DIR=%~dp0target\enso-build\
+@ set TARGET_EXE=%TARGET_DIR%buildscript\enso-build3.exe
+if not exist "%TARGET_EXE%" (
+    cargo build --profile buildscript --target-dir "%TARGET_DIR%" --package enso-build3 || exit /b %errorlevel%
+)
+"%TARGET_EXE%" %*
