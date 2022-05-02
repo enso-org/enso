5156e0fdec251b42ec6cd0853b7184e14625fb8c
Mateusz Czapliński <mateusz.czaplinski@enso.org>
2022-05-02 14:34:42 +0200

    move font name in list_view theme for consistency

--- lib/rust/ensogl/app/theme/hardcoded/src/lib.rs
+++ lib/rust/ensogl/app/theme/hardcoded/src/lib.rs
@@ -546,8 +546,8 @@ define_themes! { [light:0, dark:1]
             text {
                 highlight = selection, Rgba(0.275,0.549,0.839,1.0); // ... , rgb(70 140 214)
                 selection = Lcha(0.7,0.0,0.125,0.7) , Lcha(0.7,0.0,0.125,0.7);
-                size      = 12.0, 12.0;
                 font      = "DejaVuSansMono", "DejaVuSansMono";
+                size      = 12.0, 12.0;
             }
             highlight {
                 corner_radius = 12.0, 12.0;
