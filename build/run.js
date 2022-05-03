390541197e04343ef1f199ca2a9569d6e32ca9e1
Michael Mauderer <michael.mauderer@enso.org>
2022-05-03 09:52:49 +0100

    Increase wasm size limit.

--- build/run.js
+++ build/run.js
@@ -207,7 +207,7 @@ commands.build.rust = async function (argv) {
         console.log('Minimizing the WASM binary.')
         await gzip(paths.wasm.main, paths.wasm.mainGz)
 
-        const releaseLimitMb = 4.1
+        const releaseLimitMb = 4.2
         let limitMb = releaseLimitMb + allowExtraMb
         await checkWasmSize(paths.wasm.mainGz, limitMb)
     }
