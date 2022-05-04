eef2c5bdd43ecfb1ddeb609061ca86a705f511e2
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2022-05-04 11:31:09 +0200

    bump, disable caching in cmd wrapper

--- run.cmd
+++ run.cmd
@@ -1,6 +1,7 @@
-@ set TARGET_DIR=%~dp0target\enso-build\
-@ set TARGET_EXE=%TARGET_DIR%buildscript\enso-build3.exe
-if not exist "%TARGET_EXE%" (
-    cargo build --profile buildscript --target-dir "%TARGET_DIR%" --package enso-build3 || exit /b %errorlevel%
-)
-"%TARGET_EXE%" %*
+@ set TARGET_DIR=%~dp0target\enso-build
+@ set TARGET_EXE=%TARGET_DIR%\buildscript\enso-build3.exe
+@REM if not exist "%TARGET_EXE%" (
+@REM     cargo build --profile buildscript --target-dir "%TARGET_DIR%" --package enso-build3 || exit /b %errorlevel%
+@REM )
+@REM "%TARGET_EXE%" %*
+cargo build --profile buildscript --target-dir "%TARGET_DIR%" --package enso-build3 && "%TARGET_EXE%" %*
