641d7e88fb7aa8e5e32e9d80c48546300e077e44
James Dunkerley <james.dunkerley@gmail.com>
2022-05-05 14:25:23 +0100

    Functional version

---
+++ std-bits/base/src/main/java/org/enso/base/statistics/CountMinMax.java
@@ -0,0 +1,37 @@
+package org.enso.base.statistics;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
+
+public class CountMinMax {
+  private static boolean isValid(Object v) {
+    return !(v == null || (v instanceof Double && Double.isNaN((Double)v)));
+  }
+
+  public final int count;
+  public final Object minimum;
+  public final Object maximum;
+
+  public CountMinMax(Stream<Object> values, Comparator<Object> objectComparator) {
+    BiFunction<Object, Object, Object> min_function = (current, value) -> current == null || objectComparator.compare(current, value) > 0 ? value : current;
+    BiFunction<Object, Object, Object> max_function = (current, value) -> current == null || objectComparator.compare(current, value) < 0 ? value : current;
+
+    int count = 0;
+    Object minimum = null;
+    Object maximum = null;
+
+    Iterator<Object> iterator = values.filter(CountMinMax::isValid).iterator();
+    while (iterator.hasNext()) {
+      Object value = iterator.next();
+      count++;
+      minimum = min_function.apply(minimum, value);
+      maximum = max_function.apply(maximum, value);
+    }
+
+    this.count = count;
+    this.minimum = minimum;
+    this.maximum = maximum;
+  }
+}
