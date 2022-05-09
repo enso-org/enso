enso-org/enso

commit 3ab2c8f8a0d258bde7f41b90c9e7e158e94c47cb

Delay conversion of Truffle function body nodes until the function is invoked (#3429)
Most of the functions in the standard library aren't gonna be invoked during particular program execution. It makes no sense to build their Truffle AST for the functions that are not executing. Let's delay the construction of the tree until a function is first executed.

engine/runtime/src/main/java/org/enso/interpreter/node/MethodRootNode.java
@@ -1,8 +1,10 @@
 package org.enso.interpreter.node;
 
 import com.oracle.truffle.api.dsl.ReportPolymorphism;
+import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import com.oracle.truffle.api.source.SourceSection;
+import java.util.function.Supplier;
 import org.enso.interpreter.Language;
 import org.enso.interpreter.runtime.callable.atom.AtomConstructor;
 import org.enso.interpreter.runtime.scope.LocalScope;
@@ -44,12 +46,30 @@ public class MethodRootNode extends ClosureRootNode {
    * @param language the language identifier
    * @param localScope a description of the local scope
    * @param moduleScope a description of the module scope
-   * @param body the program body to be executed
-   * @param section a mapping from {@code body} to the program source
+   * @param body the program provider to be executed
+   * @param section a mapping from {@code provider} to the program source
    * @param atomConstructor the constructor this method is defined for
    * @param methodName the name of this method
    * @return a node representing the specified closure
    */
+  public static MethodRootNode build(
+      Language language,
+      LocalScope localScope,
+      ModuleScope moduleScope,
+      Supplier<ExpressionNode> body,
+      SourceSection section,
+      AtomConstructor atomConstructor,
+      String methodName) {
+    return build(
+        language,
+        localScope,
+        moduleScope,
+        new LazyBodyNode(body),
+        section,
+        atomConstructor,
+        methodName);
+  }
+
   public static MethodRootNode build(
       Language language,
       LocalScope localScope,
@@ -86,5 +106,20 @@ public class MethodRootNode extends ClosureRootNode {
   /** @return the method name */
   public String getMethodName() {
     return methodName;
+  }
+
+  private static class LazyBodyNode extends ExpressionNode {
+    private final Supplier<ExpressionNode> provider;
+
+    LazyBodyNode(Supplier<ExpressionNode> body) {
+      this.provider = body;
+    }
+
+    @Override
+    public Object executeGeneric(VirtualFrame frame) {
+      ExpressionNode newNode = replace(provider.get());
+      notifyInserted(newNode);
+      return newNode.executeGeneric(frame);
+    }
   }
 }

engine/runtime/src/main/java/org/enso/interpreter/node/callable/function/BlockNode.java
@@ -30,6 +30,10 @@ public class BlockNode extends ExpressionNode {
     return new BlockNode(expressions, returnExpr);
   }
 
+  public static BlockNode buildSilent(ExpressionNode[] expressions, ExpressionNode returnExpr) {
+    return new BlockNode(expressions, returnExpr);
+  }
+
   /**
    * Executes the body of the function.
    *

engine/runtime/src/main/java/org/enso/interpreter/runtime/callable/atom/AtomConstructor.java
@@ -111,7 +111,7 @@ public final class AtomConstructor implements TruffleObject {
       ArgumentDefinition[] args) {
 
     ExpressionNode instantiateNode = InstantiateNode.build(this, varReads);
-    BlockNode instantiateBlock = BlockNode.build(assignments, instantiateNode);
+    BlockNode instantiateBlock = BlockNode.buildSilent(assignments, instantiateNode);
     RootNode rootNode =
         ClosureRootNode.build(
             null,

engine/runtime/src/main/scala/org/enso/compiler/codegen/IrToTruffle.scala
@@ -290,18 +290,19 @@ class IrToTruffle(
 
         val function = methodDef.body match {
           case fn: IR.Function =>
-            val (body, arguments) =
-              expressionProcessor.buildFunctionBody(fn.arguments, fn.body)
+            val bodyBuilder =
+              new expressionProcessor.BuildFunctionBody(fn.arguments, fn.body)
             val rootNode = MethodRootNode.build(
               language,
               expressionProcessor.scope,
               moduleScope,
-              body,
+              () => bodyBuilder.bodyNode(),
               makeSection(methodDef.location),
               cons,
               methodDef.methodName.name
             )
             val callTarget = Truffle.getRuntime.createCallTarget(rootNode)
+            val arguments  = bodyBuilder.args()
             new RuntimeFunction(
               callTarget,
               null,
@@ -348,18 +349,19 @@ class IrToTruffle(
 
         val function = methodDef.body match {
           case fn: IR.Function =>
-            val (body, arguments) =
-              expressionProcessor.buildFunctionBody(fn.arguments, fn.body)
+            val bodyBuilder =
+              new expressionProcessor.BuildFunctionBody(fn.arguments, fn.body)
             val rootNode = MethodRootNode.build(
               language,
               expressionProcessor.scope,
               moduleScope,
-              body,
+              () => bodyBuilder.bodyNode(),
               makeSection(methodDef.location),
               toType,
               methodDef.methodName.name
             )
             val callTarget = Truffle.getRuntime.createCallTarget(rootNode)
+            val arguments  = bodyBuilder.args()
             new RuntimeFunction(
               callTarget,
               null,
@@ -1187,59 +1189,72 @@ class IrToTruffle(
       * @return a node for the final shape of function body and pre-processed
       *         argument definitions.
       */
-    def buildFunctionBody(
-      arguments: List[IR.DefinitionArgument],
-      body: IR.Expression
-    ): (BlockNode, Array[ArgumentDefinition]) = {
-      val argFactory = new DefinitionArgumentProcessor(scopeName, scope)
+    class BuildFunctionBody(
+      val arguments: List[IR.DefinitionArgument],
+      val body: IR.Expression
+    ) {
+      private val argFactory = new DefinitionArgumentProcessor(scopeName, scope)
+      private lazy val slots = computeSlots()
+      private lazy val bodyN = computeBodyNode()
 
-      val argDefinitions = new Array[ArgumentDefinition](arguments.size)
-      val argExpressions = new ArrayBuffer[RuntimeExpression]
-      val seenArgNames   = mutable.Set[String]()
+      def args(): Array[ArgumentDefinition] = slots._2
+      def bodyNode(): BlockNode             = bodyN
 
-      // Note [Rewriting Arguments]
-      val argSlots =
-        arguments.zipWithIndex.map { case (unprocessedArg, idx) =>
-          val arg = argFactory.run(unprocessedArg, idx)
-          argDefinitions(idx) = arg
+      private def computeBodyNode(): BlockNode = {
+        val (argSlots, _, argExpressions) = slots
 
-          val occInfo = unprocessedArg
-            .unsafeGetMetadata(
-              AliasAnalysis,
-              "No occurrence on an argument definition."
+        val bodyExpr = body match {
+          case IR.Foreign.Definition(lang, code, _, _, _) =>
+            buildForeignBody(
+              lang,
+              code,
+              arguments.map(_.name.name),
+              argSlots
             )
-            .unsafeAs[AliasAnalysis.Info.Occurrence]
-
-          val slot = scope.createVarSlot(occInfo.id)
-          val readArg =
-            ReadArgumentNode.build(idx, arg.getDefaultValue.orElse(null))
-          val assignArg = AssignmentNode.build(readArg, slot)
-
-          argExpressions.append(assignArg)
-
-          val argName = arg.getName
-
-          if (seenArgNames contains argName) {
-            throw new IllegalStateException(
-              s"A duplicate argument name, $argName, was found during codegen."
-            )
-          } else seenArgNames.add(argName)
-          slot
+          case _ => ExpressionProcessor.this.run(body)
         }
-
-      val bodyExpr = body match {
-        case IR.Foreign.Definition(lang, code, _, _, _) =>
-          buildForeignBody(
-            lang,
-            code,
-            arguments.map(_.name.name),
-            argSlots
-          )
-        case _ => this.run(body)
+        BlockNode.build(argExpressions.toArray, bodyExpr)
       }
 
-      val fnBodyNode = BlockNode.build(argExpressions.toArray, bodyExpr)
-      (fnBodyNode, argDefinitions)
+      private def computeSlots(): (
+        List[FrameSlot],
+        Array[ArgumentDefinition],
+        ArrayBuffer[RuntimeExpression]
+      ) = {
+        val seenArgNames   = mutable.Set[String]()
+        val argDefinitions = new Array[ArgumentDefinition](arguments.size)
+        val argExpressions = new ArrayBuffer[RuntimeExpression]
+        // Note [Rewriting Arguments]
+        val argSlots = arguments.zipWithIndex.map {
+          case (unprocessedArg, idx) =>
+            val arg = argFactory.run(unprocessedArg, idx)
+            argDefinitions(idx) = arg
+
+            val occInfo = unprocessedArg
+              .unsafeGetMetadata(
+                AliasAnalysis,
+                "No occurrence on an argument definition."
+              )
+              .unsafeAs[AliasAnalysis.Info.Occurrence]
+
+            val slot = scope.createVarSlot(occInfo.id)
+            val readArg =
+              ReadArgumentNode.build(idx, arg.getDefaultValue.orElse(null))
+            val assignArg = AssignmentNode.build(readArg, slot)
+
+            argExpressions.append(assignArg)
+
+            val argName = arg.getName
+
+            if (seenArgNames contains argName) {
+              throw new IllegalStateException(
+                s"A duplicate argument name, $argName, was found during codegen."
+              )
+            } else seenArgNames.add(argName)
+            slot
+        }
+        (argSlots, argDefinitions, argExpressions)
+      }
     }
 
     private def buildForeignBody(
@@ -1269,18 +1284,18 @@ class IrToTruffle(
       body: IR.Expression,
       location: Option[IdentifiedLocation]
     ): CreateFunctionNode = {
-      val (fnBodyNode, argDefinitions) = buildFunctionBody(arguments, body)
+      val bodyBuilder = new BuildFunctionBody(arguments, body)
       val fnRootNode = ClosureRootNode.build(
         language,
         scope,
         moduleScope,
-        fnBodyNode,
+        bodyBuilder.bodyNode(),
         makeSection(location),
         scopeName
       )
       val callTarget = Truffle.getRuntime.createCallTarget(fnRootNode)
 
-      val expr = CreateFunctionNode.build(callTarget, argDefinitions)
+      val expr = CreateFunctionNode.build(callTarget, bodyBuilder.args())
 
       setLocation(expr, location)
     }

commit 23f63e4d11342d12a502b2d9a02e7288f22d3b0a

Fix infinite loop by doing deep copy
Patch provided by Jaroslav, appears to fix local issues with inifite loop
introduced in https://github.com/enso-org/enso/pull/3429.
This renders https://github.com/enso-org/enso/pull/3439 obsolete.

engine/runtime/src/main/java/org/enso/interpreter/node/ClosureRootNode.java
@@ -73,4 +73,8 @@ public class ClosureRootNode extends EnsoRootNode {
     state = FrameUtil.getObjectSafe(frame, this.getStateFrameSlot());
     return new Stateful(state, result);
   }
+
+  final ExpressionNode getBody() {
+    return body;
+  }
 }

engine/runtime/src/main/java/org/enso/interpreter/node/MethodRootNode.java
@@ -1,7 +1,9 @@
 package org.enso.interpreter.node;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.ReportPolymorphism;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import com.oracle.truffle.api.source.SourceSection;
 import java.util.function.Supplier;
@@ -108,6 +110,18 @@ public class MethodRootNode extends ClosureRootNode {
     return methodName;
   }
 
+  @Override
+  public Node deepCopy() {
+    LazyBodyNode.replaceLazyNode(getBody());
+    return super.deepCopy();
+  }
+
+  @Override
+  public Node copy() {
+    LazyBodyNode.replaceLazyNode(getBody());
+    return super.copy();
+  }
+
   private static class LazyBodyNode extends ExpressionNode {
     private final Supplier<ExpressionNode> provider;
 
@@ -115,11 +129,22 @@ public class MethodRootNode extends ClosureRootNode {
       this.provider = body;
     }
 
+    static void replaceLazyNode(Node n) {
+      if (n instanceof LazyBodyNode lazy) {
+        lazy.replaceItself();
+      }
+    }
+
     @Override
     public Object executeGeneric(VirtualFrame frame) {
-      ExpressionNode newNode = replace(provider.get());
-      notifyInserted(newNode);
+      ExpressionNode newNode = replaceItself();
       return newNode.executeGeneric(frame);
+    }
+
+    final ExpressionNode replaceItself() {
+        ExpressionNode newNode = replace(provider.get());
+        notifyInserted(newNode);
+        return newNode;
     }
   }
 }