37e6071cd5201617b808d71cd5269ebf778457af
Wojciech Danilo <wojciech.danilo@gmail.com>
2022-05-09 03:06:12 +0200

    Refactoring

--- lib/rust/parser/src/main.rs
+++ lib/rust/parser/src/main.rs
@@ -1,5 +1,79 @@
-//! Enso parser.
-//! TODO: Process description
+//! The Enso parser. Parsing is a multi-stage process:
+//!
+//! # Lexing.
+//! First, the source code is feed to [`lexer::Lexer`], which consumes it and outputs a stream of
+//! [`Token`]. Tokens are chunks of the input with a generic description attached, like "operator",
+//! or "identifier".
+//!
+//! # Building macro registry.
+//! Macros in Enso are a very powerful mechanism and are used to transform group of tokens into
+//! almost any statement. First, macros need to be discovered and registered. Currently, there is no
+//! real macro discovery process, as there is no support for user-defined macros. Instead, there is
+//! a set of hardcoded macros defined in the compiler.
+//!
+//! Each macro defines one or more segments. Every segment starts with a predefined token and can
+//! contain any number of other tokens. For example, the macro `if ... then ... else ...` contains
+//! three segments. Macros can also accept prefix tokens, a set of tokens on the left of the first
+//! segment. A good example is the lambda macro `... -> ...`.
+//!
+//! In this step, a [`MacroMatchTree`] is built. Basically, it is a map from the possible next
+//! segment name to information of what other segments are required and what is the macro definition
+//! in case these segments were found. For example, let's consider two macros: `if ... then ...`,
+//! and `if ... then ... else ...`. In such a case, the macro registry will contain only one entry,
+//! "if", and two sets of possible resolution paths: ["then"], and ["then", "else"], each associated
+//! with the corresponding macro definition.
+//!
+//! # Splitting the token stream by the macro segments.
+//! The input token stream is being iterated and is being split based on the segments of the
+//! registered macros. For example, for the input `if a b then c d else e f`, the token stream will
+//! be split into three segments, `a b`, `c d`, and `e f`, which will be associated with the
+//! `if ... then ... else ...` macro definition.
+//!
+//! The splitting process is hierarchical. It means that a new macro can start being resolved during
+//! resolution of a parent macro. For example, `if if a then b then c else d` is a correct
+//! expression. After finding the first `if` token, the token stream will be split. The next `if`
+//! token starts a new token stream splitting. The first `then` token belongs to the nested macro,
+//! however, as soon as the resolver sees the second `then` token, it will consider the nested macro
+//! to be finished, and will come back to parent macro resolution.
+//!
+//! # Resolving righ-hand-side patterns of macro segments.
+//! In the next steps, each macro is being analyzed, started from the most nested ones. For each
+//! macro, the [`Pattern`] of last segment is being run to check which tokens belong to that macro,
+//! and which tokens should be transferred to parent macro definition. For example, consider the
+//! following code `process (read file) content-> print content`. The `(...)` is a macro with two
+//! sections `(` and `)`. Let's mark the token splitting with `[` and `]` characters. The previous
+//! macro resolution steps would output such split of the token stream:
+//! `process [(read file][) content[-> print content]`. In this step, the most inner macro will be
+//! analyzed first. The pattern of the last segment of the inner macro (`->`) defines that it
+//! consumes all tokens, so all the tokens `print content` are left as they are. Now, the resolution
+//! moves to the parent macro. It's last segment starts with the `)` token, which pattern defines
+//! that it does not consume any tokens, so all of its current tokens (`content[-> print content]]`)
+//! are popped to a parent definition, forming `process [(read file][)] content[-> print content]`.
+//!
+//! Please note, that root of the expression is considered a special macro as well. It is done for
+//! the algorithm unification purposes.
+//!
+//! # Resolving left-hand-side patterns of macro segments.
+//! In this step, each macro is being analyzed, started from the most nested ones. For each macro,
+//! the [`Pattern`] of the macro prefix is being run to check which tokens belong to the prefix of
+//! the macro (in case the macro defines the prefix). In the example above, the macro `->` defines
+//! complex prefix rules: if the token on the left of the arrow used no space, then only a single
+//! token will be consumed. As a result of this step, the following token split will occur:
+//! `[process [(read file][)] [content-> print content]`, which is exactly what we wanted.
+//!
+//! # Resolving patterns of macro segments.
+//! In this step, all macro segment patterns are being resolved and errors are reported in case it
+//! was not possible. If tokens in a segment match the segment pattern, they are sent to the
+//! operator precedence resolver for final transformation.
+//!
+//! # Operator precedence resolution.
+//! Each token stream sent to the operator resolver is processed by a modified Shunting Yard
+//! algorithm, which handles such situations as multiple operators placed next to each other,
+//! multiple identifiers placed next to each other, and also takes spacing into consideration in
+//! order to implement spacing-aware precedence rules. After all segments are resolved, the macro
+//! is being treated as a single token in one of the segments of the parent macro, and is being
+//! processed by the operator precedence resolver as well. In the end, a single [`syntax::Tree] is
+//! produced, containing the parsed expression.
 
 #![recursion_limit = "256"]
 // === Features ===
@@ -56,25 +130,31 @@ pub mod prelude {
 
 
 
+// =================================
+// === SyntaxItemOrMacroResolver ===
+// =================================
+
+/// One of [`syntax::Item`] or [`MacroResolver`].
 #[derive(Debug)]
-pub enum TokenOrAstOrMacroResolver<'a> {
+#[allow(missing_docs)]
+pub enum SyntaxItemOrMacroResolver<'a> {
     SyntaxItem(syntax::Item),
     MacroResolver(MacroResolver<'a>),
 }
 
-impl<'a> From<syntax::Item> for TokenOrAstOrMacroResolver<'a> {
+impl<'a> From<syntax::Item> for SyntaxItemOrMacroResolver<'a> {
     fn from(t: syntax::Item) -> Self {
         Self::SyntaxItem(t)
     }
 }
 
-impl<'a> From<MacroResolver<'a>> for TokenOrAstOrMacroResolver<'a> {
+impl<'a> From<MacroResolver<'a>> for SyntaxItemOrMacroResolver<'a> {
     fn from(t: MacroResolver<'a>) -> Self {
         Self::MacroResolver(t)
     }
 }
 
-impl<'a> TryAsRef<syntax::Item> for TokenOrAstOrMacroResolver<'a> {
+impl<'a> TryAsRef<syntax::Item> for SyntaxItemOrMacroResolver<'a> {
     fn try_as_ref(&self) -> Option<&syntax::Item> {
         match self {
             Self::SyntaxItem(t) => Some(t),
@@ -138,15 +218,21 @@ impl<'a> MacroMatchTree<'a> {
 // === MacroResolver ===
 // =====================
 
+/// Enso macro resolver. See the docs of the main module to learn more about the macro resolution
+/// steps.
 #[derive(Debug)]
+#[allow(missing_docs)]
 pub struct MacroResolver<'a> {
-    current_segment:        MatchedSegment<'a>,
-    resolved_segments:      Vec<MatchedSegment<'a>>,
-    possible_next_segments: MacroMatchTree<'a>,
-    matched_macro_def:      Option<Rc<macros::Definition<'a>>>,
+    pub current_segment:        MatchedSegment<'a>,
+    pub resolved_segments:      Vec<MatchedSegment<'a>>,
+    pub possible_next_segments: MacroMatchTree<'a>,
+    pub matched_macro_def:      Option<Rc<macros::Definition<'a>>>,
 }
 
 impl<'a> MacroResolver<'a> {
+    /// A new macro resolver with a special "root" segment definition. The "root" segment does not
+    /// exist in the source code, it is simply the whole expression being parsed. It is treated as
+    /// a macro in order to unify the algorithms.
     pub fn new_root() -> Self {
         let current_segment = MatchedSegment {
             header: span::With::new_no_left_offset_no_len(Bytes::from(0), token::Type::newline()),
@@ -175,7 +261,7 @@ impl<'a> MacroResolver<'a> {
 #[derive(Debug)]
 pub struct MatchedSegment<'a> {
     header: Token,
-    body:   Vec<TokenOrAstOrMacroResolver<'a>>,
+    body:   Vec<SyntaxItemOrMacroResolver<'a>>,
 }
 
 impl<'a> MatchedSegment<'a> {
@@ -289,7 +375,7 @@ impl<'a> Resolver<'a> {
             let mut ss: Vec<syntax::Item> = vec![];
             for item in segment.body {
                 let resolved_token = match item {
-                    TokenOrAstOrMacroResolver::MacroResolver(m2) => {
+                    SyntaxItemOrMacroResolver::MacroResolver(m2) => {
                         if let Some(macro_def) = &m2.matched_macro_def
                             && let Some(pfx_pattern) = &macro_def.rev_prefix_pattern {
                             ss.reverse();
@@ -303,7 +389,7 @@ impl<'a> Resolver<'a> {
                             Self::resolve(lexer, m2, None).into()
                         }
                     },
-                    TokenOrAstOrMacroResolver::SyntaxItem(t) => t,
+                    SyntaxItemOrMacroResolver::SyntaxItem(t) => t,
                 };
                 ss.push(resolved_token);
             }
@@ -391,9 +477,8 @@ impl<'a> Resolver<'a> {
     }
 }
 
-// TODO: cargo build --timings
 
-
+// FIXME: hardcoded values + not finished implementation.
 pub fn precedence_of(operator: &str) -> usize {
     match operator {
         "+" => 3,
@@ -653,30 +738,6 @@ fn builtin_macros() -> MacroMatchTree<'static> {
 // === Main ===
 // ============
 
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    pub fn ident(repr: &str) -> syntax::Tree {
-        match token::Type::to_ident_unchecked(repr) {
-            token::Type::Ident(ident) => span::With::new_no_left_offset_no_start(
-                Bytes::from(repr.len()),
-                syntax::tree::Type::from(syntax::tree::Ident(ident)),
-            ),
-            _ => panic!(),
-        }
-    }
-
-    pub fn app_segment(
-        header: Token,
-        body: Option<syntax::Tree>,
-    ) -> syntax::tree::MultiSegmentAppSegment {
-        syntax::tree::MultiSegmentAppSegment { header, body }
-    }
-}
-
-
-
 fn main() {
     init_tracing(TRACE);
     // let str = "if a then b else c";
@@ -705,35 +766,37 @@ fn main() {
     );
     println!("{:#?}", source::With::new(str, &ast));
 
-    // let seg1 =
-    //     test::app_segment(Token::symbol("("),
-    // Some(syntax::Tree::opr_section_boundary(test::ident("a")))); let seg2 =
-    // test::app_segment(Token::symbol(")"), None);
-    //
-    // let ast2 = syntax::Tree::opr_section_boundary(location::With::new_with_len(
-    //     Bytes::from(0),
-    //     syntax::tree::Data::MultiSegmentApp(MultiSegmentApp {
-    //         prefix:   None,
-    //         segments: NonEmptyVec::try_from(vec![seg1, seg2]).unwrap(),
-    //     }),
-    // ));
-    //
-    // println!("{:#?}", &ast2);
-
     println!("\n\n==================\n\n");
-
-    // let mut ast2 = ast_builder! { {if} a {then} b {else} c};
-    // println!("{:#?}", ast2);
-    // ast.remove_span_info();
-    // ast2.remove_span_info();
-    // println!("{:#?}", ast);
-    // println!("{:#?}", ast2);
-    // println!("{:#?}", ast == ast2);
-
-    // let a = 5;
-    // let b = 5;
-    // let c = a + / b;
 }
+
+
+
+// =============
+// === Tests ===
+// =============
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    pub fn ident(repr: &str) -> syntax::Tree {
+        match token::Type::to_ident_unchecked(repr) {
+            token::Type::Ident(ident) => span::With::new_no_left_offset_no_start(
+                Bytes::from(repr.len()),
+                syntax::tree::Type::from(syntax::tree::Ident(ident)),
+            ),
+            _ => panic!(),
+        }
+    }
+
+    pub fn app_segment(
+        header: Token,
+        body: Option<syntax::Tree>,
+    ) -> syntax::tree::MultiSegmentAppSegment {
+        syntax::tree::MultiSegmentAppSegment { header, body }
+    }
+}
+
 
 
 #[cfg(test)]
@@ -768,96 +831,5 @@ mod tests {
         test_parse!("if a then b" = { {if} a {then} b });
         test_parse!("if a then b else c" = { {if} a {then} b {else} c });
         test_parse!("if a b then c d else e f" = { {if} a b {then} c d {else} e f });
-        // test_parse!("foo (a)" = { foo [{"("} a {")"}]});
     }
 }
-
-
-// let str = "if a then b else c";
-// let str = "if if * a + b * then y then b";
-// let str = "* a + b *";
-// let str = "* a + * b";
-// let str = "(a) (b) c";
-// let str = "if (a) then b";
-// let str = "foo a-> b";
-// let str = "a+b * c";
-// let str = "foo if a then b";
-// let str = "foo *(a)";
-// let str = "foo (a)";
-
-// app(["(","a","]"])
-
-/// Requirements:
-///
-/// - Parentheses have priority over space-aware precedence, e.g. `test (a + b)` is OK because `(a`
-///   is not interpreted as expression.
-///
-/// - Parentheses allow operators glued to them, e.g. `test *(a + b)` is OK, but `test *if a then b`
-///   is not.
-///
-/// - Lambdas have different precedence on left and right side, e.g. `test <| a -> (+1) <| a` should
-///   be interpreted as `test (a -> ((+1) a))`.
-///
-/// - Lambdas consume everything on the right-hand side even if left argument does not use space.
-///   `test a-> a + 1` is the same as `test (a -> a + 1)`.
-///
-/// - Macro sections have higher precedence then everything, including lambdas. `if a then x -> x +
-///   1 else x -> x - 1` should be OK.
-fn _tmp() {}
-
-// (.foo a)
-// (* foo + bar)
-// a + * b
-
-
-// a) {(a b) c x-> y}
-// b) {(a b) c x-> y -)}
-
-// Idea:
-// 1. Match macros as now.
-// 2. After closing a macro, do not build AST, build hierarchical macro resolution.
-//    Example a) would become [{[(a b][) c x[-> y]]][}] . Let's skip curly braces:
-//                              [(a b][) c x[-> y]]
-// 3. Then when closing macros (after parsing '}'), macros free right hand side:
-//                             [(a b][)] c x[-> y]
-// 4. Then macros are resolved left-to-right, including left-hand-sides.
-//                             [(a b)] c [x-> y]
-
-
-// TODO: what this should return?
-// if a then x -> x + 1 else c
-// a) (if a then x) -> x + 1 else c
-// b) if a then (x -> x + 1) else c
-// c) if a then+ y else z
-// d) if 1 + 2 > a then b else c
-
-// TODO: HOWEVER this should result in an error:
-// a +if x then y else z
-// but this should be fine:
-// a +(x y z)
-
-// TODO: AND this has to work:
-// foo x-> bar
-
-// So we should create passes:
-// 1. parentheses
-// 2. lambdas
-// 3. space-aware precedence
-// 4. All other macros
-
-// NO! Then `if a then x -> x + 1 else c` would not work – points 2 and 4 have to be 1 pass.
-//     If we want macros to be able to invalidate space aware precedence (like `foo x-> a + b`),
-//     then point 3 has to be deleted.
-//
-//     But how to handle `if a then+ y else z` or `foo +if a then b else c`? Probably by using some
-//     additional checks.
-
-// Do we even need parentheses as a separate pass now? Somehow this has to work `+(x y z)` and
-// this not `+if x then y else z`. Maybe additional checks for the macros again? This will make
-// macros even more powerful. But we don't need to expose all the power to users.
-
-
-
-// TODO: NOTE
-// probably we want to keep the struct AstOrTokenOrMatchedMacro = AstOrToken | MacroSomething
-// because we will be very fast going back to AstOrToken
