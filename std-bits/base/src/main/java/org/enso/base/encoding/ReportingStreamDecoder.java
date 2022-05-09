5236fcb2071d42678ab88b3ea873f3b0ad2a4398
Radosław Waśko <radoslaw.wasko@enso.org>
2022-05-09 15:53:15 +0200

    Add tests to check most codepaths

--- std-bits/base/src/main/java/org/enso/base/encoding/ReportingStreamDecoder.java
+++ std-bits/base/src/main/java/org/enso/base/encoding/ReportingStreamDecoder.java
@@ -154,10 +154,10 @@ public class ReportingStreamDecoder extends Reader {
    * left in write mode.
    */
   private void prepareOutputBuffer(int expectedCharactersCount) {
-    if (outputBuffer == null) {
+    assert outputBuffer == null || !outputBuffer.hasRemaining();
+    if (outputBuffer == null || outputBuffer.capacity() < expectedCharactersCount) {
       outputBuffer = CharBuffer.allocate(expectedCharactersCount);
     } else {
-      assert !outputBuffer.hasRemaining();
       outputBuffer.clear();
     }
   }
@@ -216,8 +216,10 @@ public class ReportingStreamDecoder extends Reader {
         outputBuffer.put(INVALID_CHARACTER);
         inputBuffer.position(inputBuffer.position() + cr.length());
       } else if (cr.isUnderflow()) {
+        System.out.println("U");
         break;
       } else if (cr.isOverflow()) {
+        System.out.println("O");
         growOutputBuffer();
       }
     }
@@ -256,6 +258,7 @@ public class ReportingStreamDecoder extends Reader {
    */
   private void ensureInputBufferHasEnoughFreeSpace(int bytesToRead) {
     if (inputBuffer == null) {
+      System.out.println("A");
       inputBuffer = ByteBuffer.allocate(bytesToRead);
     } else {
       int freeSpaceInInputBuffer = inputBuffer.capacity() - inputBuffer.remaining();
@@ -267,10 +270,12 @@ public class ReportingStreamDecoder extends Reader {
       inputBytesConsumedBeforeCurrentBuffer += inputBuffer.position();
 
       if (freeSpaceInInputBuffer < bytesToRead) {
+        System.out.println("B");
         var old = inputBuffer;
         inputBuffer = ByteBuffer.allocate(old.remaining() + bytesToRead);
         inputBuffer.put(old);
       } else {
+        System.out.println("C");
         inputBuffer.compact();
       }
     }
