e9d863860ac5ed897f224cb2d38e889e0f0ce805
James Dunkerley <james.dunkerley@gmail.com>
2022-05-18 11:43:57 +0100

    WIP

---
+++ std-bits/base/src/main/java/org/enso/base/statistics/Rank.java
@@ -0,0 +1,10 @@
+package org.enso.base.statistics;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Rank {
+  public static Object[] rank() {
+    return new Object[] { 1, 2.2, true, LocalDate.now(), LocalDateTime.now() };
+  }
+}
