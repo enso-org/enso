b5ea4b5381a8ea1e7950f3a531cb2d489c5dd23a
Radosław Waśko <radoslaw.wasko@enso.org>
2022-05-19 12:31:35 +0200

    doc

--- std-bits/table/src/main/java/org/enso/table/parsing/WhitespaceStrippingParser.java
+++ std-bits/table/src/main/java/org/enso/table/parsing/WhitespaceStrippingParser.java
@@ -3,7 +3,12 @@ package org.enso.table.parsing;
 import org.enso.table.data.column.builder.object.Builder;
 import org.enso.table.parsing.problems.ProblemAggregator;
 
-public class WhitespaceStrippingParser<PA extends ProblemAggregator> extends IncrementalDatatypeParser<PA> {
+/**
+ * An incremental parser which wraps another parser of that type, delegating the parsing logic to
+ * it, but first transforming the input text by stripping any leading and trailing whitespace.
+ */
+public class WhitespaceStrippingParser<PA extends ProblemAggregator>
+    extends IncrementalDatatypeParser<PA> {
   private final IncrementalDatatypeParser<PA> innerParser;
 
   public WhitespaceStrippingParser(IncrementalDatatypeParser<PA> innerParser) {
