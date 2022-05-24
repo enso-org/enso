8516c3577e04c387d2c6916045a6d672fb7181ed
James Dunkerley <james.dunkerley@gmail.com>
2022-05-24 17:44:50 +0100

    Adding Rank functionality to Java and Enso

--- std-bits/base/src/main/java/org/enso/base/statistics/Rank.java
+++ std-bits/base/src/main/java/org/enso/base/statistics/Rank.java
@@ -1,10 +1,58 @@
 package org.enso.base.statistics;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
 
 public class Rank {
-  public static Object[] rank() {
-    return new Object[] { 1, 2.2, true, LocalDate.now(), LocalDateTime.now() };
+  public enum Method {
+    AVERAGE,
+    MINIMUM,
+    MAXIMUM,
+    DENSE,
+    ORDINAL
+  }
+
+  private record ValueWithIndex(Object value, int index) {
+  }
+
+  public static double[] rank(Object[] input, Comparator<Object> comparator, Method method)
+  {
+    Comparator<ValueWithIndex> tupleComparator = (a, b) -> {
+      int c = comparator.compare(a.value, b.value);
+      return c == 0 ? Integer.compare(a.index, b.index) : c;
+    };
+    ValueWithIndex[] tuples = IntStream.range(0, input.length).mapToObj(i -> new ValueWithIndex(input[i], i)).toArray(ValueWithIndex[]::new);
+    Arrays.sort(tuples, tupleComparator);
+
+    double[] output = new double[input.length];
+
+    int index = 0;
+    int dense = 0;
+    while (index < tuples.length)
+    {
+      dense++;
+      int bottom = index;
+
+      // Find Top
+      while (index < tuples.length && comparator.compare(tuples[bottom].value,  tuples[index].value) == 0) {
+        index++;
+      }
+
+      // Build Rank
+      for (int i = bottom; i < index; i++) {
+        double rank = switch (method) {
+          case MINIMUM -> bottom + 1;
+          case MAXIMUM -> index;
+          case DENSE -> dense;
+          case AVERAGE -> (bottom + 1 + index) / 2.0;
+          case ORDINAL -> i + 1;
+        };
+
+        output[tuples[i].index] = rank;
+      }
+    }
+
+    return output;
   }
 }
