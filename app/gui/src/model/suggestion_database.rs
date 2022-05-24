d7c561a922a2a2adabf89e43442ba59e63900720
Mateusz Czapliński <mateusz.czaplinski@enso.org>
2022-05-24 11:29:52 +0200

    review: move swap_... func to helpers section

--- app/gui/src/model/suggestion_database.rs
+++ app/gui/src/model/suggestion_database.rs
@@ -88,36 +88,6 @@ impl QualifiedNameToIdMap {
         path: &entry::QualifiedName,
         value: Option<entry::Id>,
     ) -> Option<entry::Id> {
-        // A helper of the [`replace_value_and_traverse_back_pruning_empty_subtrees`] function.
-        // Performs the same operation but the replaced value is swapped with `value`
-        // instead of being returned.
-        fn swap_value_and_traverse_back_pruning_empty_subtrees<P, I>(
-            node: &mut HashMapTree<entry::QualifiedNameSegment, Option<entry::Id>>,
-            mut path: P,
-            value: &mut Option<entry::Id>,
-        ) where
-            P: Iterator<Item = I>,
-            I: Into<entry::QualifiedNameSegment>,
-        {
-            use std::collections::hash_map::Entry;
-            match path.next() {
-                None => std::mem::swap(&mut node.value, value),
-                Some(key) => match node.branches.entry(key.into()) {
-                    Entry::Occupied(mut entry) => {
-                        let node = entry.get_mut();
-                        swap_value_and_traverse_back_pruning_empty_subtrees(node, path, value);
-                        if node.value.is_none() && node.is_leaf() {
-                            entry.remove_entry();
-                        }
-                    }
-                    Entry::Vacant(entry) =>
-                        if let Some(v) = value.take() {
-                            entry.insert(default()).set(path, Some(v));
-                        },
-                },
-            }
-        }
-
         let mut path_iter = path.into_iter();
         let mut swapped_value = value;
         swap_value_and_traverse_back_pruning_empty_subtrees(
@@ -391,6 +361,51 @@ impl SuggestionDatabase {
 impl From<language_server::response::GetSuggestionDatabase> for SuggestionDatabase {
     fn from(database: language_server::response::GetSuggestionDatabase) -> Self {
         Self::from_ls_response(database)
+    }
+}
+
+
+
+// ===============
+// === Helpers ===
+// ===============
+
+
+// === QualifiedNameToIdMap helpers ===
+
+/// Replaces the value at `path` in `node` with `value` (sets `value` to `None` if there
+/// was no node at `path`). Then visits nodes on the `path` in reverse order and removes every
+/// visited empty leaf node from its parent.
+///
+/// In this function, a node is defined as empty when it contains a `None` value. A node is a leaf
+/// when its [`is_leaf`] method returns `true`.
+///
+/// The function is optimized to not create new empty nodes if they would be deleted by the
+/// function before it returns.
+fn swap_value_and_traverse_back_pruning_empty_subtrees<P, I>(
+    node: &mut HashMapTree<entry::QualifiedNameSegment, Option<entry::Id>>,
+    mut path: P,
+    value: &mut Option<entry::Id>,
+) where
+    P: Iterator<Item = I>,
+    I: Into<entry::QualifiedNameSegment>,
+{
+    use std::collections::hash_map::Entry;
+    match path.next() {
+        None => std::mem::swap(&mut node.value, value),
+        Some(key) => match node.branches.entry(key.into()) {
+            Entry::Occupied(mut entry) => {
+                let node = entry.get_mut();
+                swap_value_and_traverse_back_pruning_empty_subtrees(node, path, value);
+                if node.value.is_none() && node.is_leaf() {
+                    entry.remove_entry();
+                }
+            }
+            Entry::Vacant(entry) =>
+                if let Some(v) = value.take() {
+                    entry.insert(default()).set(path, Some(v));
+                },
+        },
     }
 }
 
