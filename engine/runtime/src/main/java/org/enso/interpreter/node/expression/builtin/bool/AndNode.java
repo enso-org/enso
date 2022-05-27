13ee6d7d5822099666e185500a9d2b025376ba62
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2022-05-27 23:32:00 +0200

    Don't evaluate RHS of || and && eagerly
    Lazy evaluation of RHS for || and && is typically taken for granted
    by users of other PLs. This change makes it happen for Enso.

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/bool/AndNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/bool/AndNode.java
@@ -1,14 +1,41 @@
 package org.enso.interpreter.node.expression.builtin.bool;
 
 import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.profiles.ConditionProfile;
 import org.enso.interpreter.dsl.BuiltinMethod;
+import org.enso.interpreter.dsl.MonadicState;
+import org.enso.interpreter.dsl.Suspend;
+import org.enso.interpreter.node.BaseNode;
+import org.enso.interpreter.node.callable.thunk.ThunkExecutorNode;
+import org.enso.interpreter.runtime.Context;
+import org.enso.interpreter.runtime.error.DataflowError;
+import org.enso.interpreter.runtime.error.PanicException;
+import org.enso.interpreter.runtime.state.Stateful;
 
 @BuiltinMethod(
     type = "Boolean",
     name = "&&",
     description = "Computes the logical conjunction of two booleans")
 public class AndNode extends Node {
-  boolean execute(boolean _this, boolean that) {
-    return _this && that;
+
+  private @Child
+  ThunkExecutorNode rhsThunkExecutorNode = ThunkExecutorNode.build();
+  private final ConditionProfile condProfile = ConditionProfile.createCountingProfile();
+
+  Stateful execute(@MonadicState Object state, boolean _this, @Suspend Object that) {
+    if (condProfile.profile(_this)) {
+      return ensureBooleanOrDataflowError(rhsThunkExecutorNode.executeThunk(that, state, BaseNode.TailStatus.TAIL_DIRECT));
+    } else {
+      return new Stateful(state, false);
+    }
+  }
+
+  private Stateful ensureBooleanOrDataflowError(Stateful v) {
+    if ((v.getValue() instanceof Boolean) || (v.getValue() instanceof DataflowError)) {
+      return v;
+    } else {
+      var typeError = Context.get(this).getBuiltins().error().makeTypeError("Boolean", v.getValue(), "bool");
+      throw new PanicException(typeError, this);
+    }
   }
 }
