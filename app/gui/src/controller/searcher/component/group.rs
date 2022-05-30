1f6133bca3008819e4f62e49cb6e8f3150054342
Adam Obuchowicz <adam.obuchowicz@enso.org>
2022-05-30 10:50:54 +0200

    Component list and generation.

--- app/gui/src/controller/searcher/component/group.rs
+++ app/gui/src/controller/searcher/component/group.rs
@@ -3,15 +3,15 @@ use crate::model::suggestion_database;
 use crate::prelude::*;
 
 #[derive(Clone, Debug)]
-struct Data {
+pub struct Data {
     pub name:    ImString,
     pub entries: RefCell<Vec<Component>>,
-    pub visible: bool,
+    pub visible: Cell<bool>,
 }
 
 impl Data {
     fn new_empty_visible(name: impl Into<ImString>) -> Self {
-        Data { name: name.into(), entries: default(), visible: true }
+        Data { name: name.into(), entries: default(), visible: Cell::new(true) }
     }
 }
 
@@ -23,11 +23,18 @@ pub struct Group {
 impl Group {
     pub fn from_entry(entry: &suggestion_database::Entry) -> Self {
         let name: String = if entry.module.is_top_module() {
-            entry.module.into()
+            (&entry.module).into()
         } else {
             entry.module.name().into()
         };
         Self { data: Rc::new(Data::new_empty_visible(name)) }
+    }
+
+    pub fn check_visibility(&self) {
+        let entries = self.entries.borrow();
+        let filtered_out = entries.iter().filter(|c| c.is_filtered_out());
+        let visible = filtered_out.count() < entries.len();
+        self.visible.set(visible);
     }
 }
 
@@ -36,14 +43,22 @@ pub struct List {
     groups: Rc<Vec<Group>>,
 }
 
+impl List {
+    fn iter_visible(&self) -> impl Iterator<Item = &Group> {
+        self.groups.iter().filter(|g| g.visible.get())
+    }
+}
+
 #[derive(Clone, Debug, Default, AsRef, Deref, AsMut, DerefMut)]
 pub struct ListBuilder {
     pub groups: Vec<Group>,
 }
 
 impl ListBuilder {
-    fn build(mut self) -> List {
-        self.groups.sort_unstable_by_key(|group| &group.name);
+    pub fn build(mut self) -> List {
+        // The `sort_unstable_by_key` method is not suitable here, because the closure it takes
+        // cannot return reference, and we don't want to copy strings here.
+        self.groups.sort_unstable_by(|a, b| a.name.cmp(&b.name));
         List { groups: Rc::new(self.groups) }
     }
 }
