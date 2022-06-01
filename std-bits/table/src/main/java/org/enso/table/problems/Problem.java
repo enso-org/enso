23119ae35f08ee1b2d88c815c9b838e2e9d61305
James Dunkerley <james.dunkerley@gmail.com>
2022-06-01 09:56:40 +0100

    Update doc string

--- std-bits/table/src/main/java/org/enso/table/problems/Problem.java
+++ std-bits/table/src/main/java/org/enso/table/problems/Problem.java
@@ -1,7 +1,6 @@
 package org.enso.table.problems;
 
 /**
- * A parent class for parsing problems which may be reported as warnings or errors, depending on the
- * setup.
+ * A parent class for problems which may be reported as warnings or errors, depending on the setup.
  */
 public interface Problem {}
