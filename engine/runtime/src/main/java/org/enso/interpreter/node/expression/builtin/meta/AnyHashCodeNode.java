40fad7471573f6ea65c52990f6b0d9c88e397daa
James Dunkerley <james.dunkerley@gmail.com>
2022-06-02 08:37:46 +0100

    Make constants

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/AnyHashCodeNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/AnyHashCodeNode.java
@@ -16,6 +16,10 @@ import java.math.BigInteger;
     name = "hash_code",
     description = "Gets the Hashcode value of an Enso object.")
 public abstract class AnyHashCodeNode extends Node {
+  private static final int booleanTypeHash = Boolean.class.hashCode();
+  private static final int bigIntegerTypeHash = BigInteger.class.hashCode();
+  private static final int longTypeHash = Long.class.hashCode();
+  private static final int doubleTypeHash = Double.class.hashCode();
   private static final BigInteger bigInteger_MAX_LONG = BigInteger.valueOf(Long.MAX_VALUE);
   private static final BigInteger bigInteger_MIN_LONG = BigInteger.valueOf(Long.MIN_VALUE);
 
@@ -26,29 +30,27 @@ public abstract class AnyHashCodeNode extends Node {
   public abstract Object execute(Object _this);
 
   private static Long combineTypeAndHashCodes(int typeCode, int hashCode) {
-    return Long.valueOf(typeCode << 7 + typeCode + hashCode);
+    int mergedHash = typeCode << 7 + typeCode + hashCode;
+    return Long.valueOf(mergedHash);
   }
 
   @Specialization
   Object doBoolean(Boolean _this) {
-    // _this.getClass().toString().hashCode() == 1335156652
-    return combineTypeAndHashCodes(1335156652, _this.hashCode());
+    return combineTypeAndHashCodes(booleanTypeHash, _this.hashCode());
   }
 
   @Specialization
   Object doBigInteger(EnsoBigInteger _this) {
-    // _this.getClass().toString().hashCode() == 310304960
     BigInteger bigInteger = _this.getValue();
     if (bigInteger.compareTo(bigInteger_MAX_LONG) < 0 && bigInteger.compareTo(bigInteger_MIN_LONG) > 0) {
       return this.doLong(bigInteger.longValue());
     }
-    return combineTypeAndHashCodes(310304960, bigInteger.hashCode());
+    return combineTypeAndHashCodes(bigIntegerTypeHash, bigInteger.hashCode());
   }
 
   @Specialization
   Object doLong(Long _this) {
-    // _this.getClass().toString().hashCode() == -1228562056
-    return combineTypeAndHashCodes(-1228562056, _this.hashCode());
+    return combineTypeAndHashCodes(longTypeHash, _this.hashCode());
   }
 
   @Specialization
@@ -57,7 +59,7 @@ public abstract class AnyHashCodeNode extends Node {
     if (_this % 1 == 0 && _this <= Long.MAX_VALUE && _this >= Long.MIN_VALUE) {
       return this.doLong(_this.longValue());
     }
-    return combineTypeAndHashCodes(239044557, Double.hashCode(_this));
+    return combineTypeAndHashCodes(doubleTypeHash, Double.hashCode(_this));
   }
 
   @Specialization
