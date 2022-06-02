1919c24ce21661e1f6551d81e6a1ed9f392d0e0c
Mateusz Czapliński <mateusz.czaplinski@enso.org>
2022-06-02 13:50:39 +0200

    cargo fmt

--- app/gui/src/model/execution_context/synchronized.rs
+++ app/gui/src/model/execution_context/synchronized.rs
@@ -79,10 +79,12 @@ impl ExecutionContext {
             match this.load_component_groups().await {
                 Ok(_) => info!(this.logger, "Loaded component groups."),
                 Err(err) => {
-                    let msg = iformat!("Failed to load component groups. No groups will appear \
-                        in the Favorites section of the Component Browser. Error: {err}");
+                    let msg = iformat!(
+                        "Failed to load component groups. No groups will appear \
+                        in the Favorites section of the Component Browser. Error: {err}"
+                    );
                     error!(this.logger, "{msg}");
-                },
+                }
             }
             Ok(this)
         }
