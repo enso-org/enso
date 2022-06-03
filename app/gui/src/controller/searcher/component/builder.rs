88d195a9e4f755af01c08e723ced280a780fd0a3
Mateusz Czapliński <mateusz.czaplinski@enso.org>
2022-06-03 12:45:43 +0200

    tweak extend_favorites

--- app/gui/src/controller/searcher/component/builder.rs
+++ app/gui/src/controller/searcher/component/builder.rs
@@ -112,15 +112,13 @@ impl List {
     }
 
     /// Extend the list of favorites with new groups.
-    pub fn extend_favorites(
-        &mut self,
-        groups: impl IntoIterator<Item = execution_context::ComponentGroup>,
-    ) {
+    pub fn extend_favorites<I>(&mut self, groups: G)
+    where G: IntoIterator<Item = execution_context::ComponentGroup> {
         let suggestion_db = &self.suggestion_db;
-        let favorites = groups
+        let new_favorites = groups
             .into_iter()
             .map(|g| component::Group::from_execution_context_component_group(g, suggestion_db));
-        self.favorites.extend(favorites);
+        self.favorites.extend(new_favorites);
     }
 
     fn lookup_module_group(&mut self, module: &module::QualifiedName) -> Option<&mut ModuleGroups> {
