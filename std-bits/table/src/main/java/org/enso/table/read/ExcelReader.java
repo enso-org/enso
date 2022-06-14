4f63d7f59588523db3cf5541894b4df220a2f4e7
James Dunkerley <james.dunkerley@gmail.com>
2022-06-14 15:54:06 +0100

    Fix most of the issues with headers...

--- std-bits/table/src/main/java/org/enso/table/read/ExcelReader.java
+++ std-bits/table/src/main/java/org/enso/table/read/ExcelReader.java
@@ -1,13 +1,7 @@
 package org.enso.table.read;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.Name;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.enso.table.data.column.builder.object.Builder;
@@ -15,6 +9,7 @@ import org.enso.table.data.column.builder.object.InferredBuilder;
 import org.enso.table.data.table.Column;
 import org.enso.table.data.table.Table;
 import org.enso.table.format.xlsx.Range;
+import org.enso.table.util.NameDeduplicator;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,7 +21,12 @@ import java.util.stream.IntStream;
 /** A table reader for MS Excel files. */
 public class ExcelReader {
   private static Table readSheetToTable(
-      Workbook workbook, int sheetIndex, Range range, int skipRows, int rowCount) {
+      Workbook workbook,
+      int sheetIndex,
+      Range range,
+      HeaderBehavior headers,
+      int skipRows,
+      int rowCount) {
     Sheet sheet = workbook.getSheetAt(sheetIndex);
 
     // Row Range
@@ -40,26 +40,45 @@ public class ExcelReader {
     boolean wholeRow = range == null || range.isWholeRow();
     int startCol = wholeRow ? 1 : range.getLeftColumn();
     int endCol = wholeRow ? -1 : range.getRightColumn();
+
+    // Headers
+    NameDeduplicator deduplicator = new NameDeduplicator();
+    String[] columnNames = switch (headers) {
+      case EXCEL_COLUMN_NAMES -> null;
+      case USE_FIRST_ROW_AS_HEADERS ->
+        readRowAsHeaders(getRow(startRow, sheet, firstRow, lastRow), startCol, endCol, deduplicator);
+      case INFER ->
+        inferHeaders(
+            getRow(startRow, sheet, firstRow, lastRow),
+            getRow(startRow + 1, sheet, firstRow, lastRow),
+            startCol, endCol, deduplicator);
+    };
+    if (columnNames != null) {
+      startRow++;
+    }
+
+    // Set up Storage
     int size = Math.min(rowCount, endRow - startRow + 1);
     List<Builder> builders =
         wholeRow
             ? new ArrayList<>()
             : IntStream.range(startCol, endCol + 1)
-                .mapToObj(i -> new InferredBuilder(size))
-                .collect(Collectors.toList());
+            .mapToObj(i -> new InferredBuilder(size))
+            .collect(Collectors.toList());
 
     // Read Cell Data
     int row = startRow;
     while (row <= endRow && (row - startRow) < rowCount) {
-      Row currentRow;
-      if (row < firstRow || row > lastRow || (currentRow = sheet.getRow(row - 1)) == null) {
+      Row currentRow = row < firstRow || row > lastRow ? null : sheet.getRow(row - 1);
+      if (currentRow == null) {
         builders.forEach(b -> b.append(null));
       } else {
-        int currentEndCol = endCol == -1 ? currentRow.getLastCellNum() + 1 : endCol;
-        expandBuilders(builders, size, currentEndCol - startCol, row - startRow);
-
         int firstCol = currentRow.getFirstCellNum() + 1;
         int lastCol = currentRow.getLastCellNum();
+
+        int currentEndCol = endCol == -1 ? Math.max(lastCol, startCol + builders.size() - 1) : endCol;
+        expandBuilders(builders, size, currentEndCol - startCol, row - startRow);
+
         for (int col = startCol; col <= currentEndCol; col++) {
           Object value =
               col < firstCol || col > lastCol ? null : getCellValue(currentRow.getCell(col - 1));
@@ -73,7 +92,7 @@ public class ExcelReader {
     // Special case for stopping before firstRow
     if (wholeRow && (rowCount == 0 || row < firstRow)) {
       Row currentRow = sheet.getRow(firstRow - 1);
-      int currentEndCol = currentRow.getLastCellNum() + 1;
+      int currentEndCol = currentRow.getLastCellNum();
       expandBuilders(builders, size, currentEndCol - startCol, size);
     }
 
@@ -83,11 +102,85 @@ public class ExcelReader {
             .mapToObj(
                 idx ->
                     new Column(
-                        CellReference.convertNumToColString(startCol + idx - 1),
+                        getColumnName(idx + startCol, startCol, columnNames, deduplicator),
                         builders.get(idx).seal()))
             .toArray(Column[]::new);
 
     return new Table(columns);
+  }
+
+  private static Row getRow(int row, Sheet sheet, int firstRow, int lastRow) {
+    return row < firstRow || row > lastRow ? null : sheet.getRow(row - 1);
+  }
+
+  private static String[] readRowAsHeaders(Row row, int startCol, int endCol, NameDeduplicator deduplicator) {
+    if (row == null) {
+      return new String[0];
+    }
+
+    int currentEndCol = endCol == -1 ? row.getLastCellNum() + 1 : endCol;
+    DataFormatter formatter = new DataFormatter();
+
+    int firstCol = row.getFirstCellNum() + 1;
+    int lastCol = row.getLastCellNum();
+
+    String[] output = new String[currentEndCol - startCol + 1];
+    for (int col = startCol; col <= currentEndCol; col++) {
+      String name = col < firstCol || col > lastCol ? "" : formatter.formatCellValue(row.getCell(col - 1));
+      if (!name.isEmpty()) {
+        name = deduplicator.makeUnique(name);
+      }
+      output[col - startCol] = name;
+    }
+    return output;
+  }
+
+  private static String[] inferHeaders(Row row, Row nextRow, int startCol, int endCol, NameDeduplicator deduplicator) {
+    String[] rowNames = getRowAsStrings(row, startCol, endCol);
+    if (rowNames == null) {
+      return null;
+    }
+
+    String[] nextNames = getRowAsStrings(nextRow, startCol, endCol);
+    if (nextNames != null) {
+      return null;
+    }
+
+    return deduplicator.makeUnique(rowNames);
+  }
+
+  private static String[] getRowAsStrings(Row row, int startCol, int endCol) {
+    if (row == null) {
+      return null;
+    }
+
+    int currentEndCol = endCol == -1 ? row.getLastCellNum() : endCol;
+    int firstCol = row.getFirstCellNum() + 1;
+    int lastCol = row.getLastCellNum();
+
+    String[] output = new String[currentEndCol - startCol + 1];
+    for (int col = startCol; col <= currentEndCol; col++) {
+      Cell cell = col < firstCol || col > lastCol ? null : row.getCell(col - 1);
+      CellType type = getCellType(cell);
+      if (type != CellType._NONE && type != CellType.STRING) {
+        return null;
+      }
+      output[col - startCol] = type == CellType.STRING && cell != null ? cell.getStringCellValue() : "";
+    }
+    return output;
+  }
+
+  private static String getColumnName(int column, int startCol, String[] columnNames, NameDeduplicator deduplicator) {
+    if (columnNames == null) {
+      return CellReference.convertNumToColString(column - 1);
+    }
+
+    int idx = column - startCol;
+    String name = idx < columnNames.length ? columnNames[idx] : "";
+    if (name == null || name.isEmpty()) {
+      name = deduplicator.makeUnique(name);
+    }
+    return name;
   }
 
   private static void expandBuilders(List<Builder> builders, int size, int columnCount, int rows) {
@@ -109,15 +202,7 @@ public class ExcelReader {
   }
 
   private static Object getCellValue(Cell cell) {
-    if (cell == null) {
-      return null;
-    }
-
-    CellType cellType = cell.getCellType();
-    if (cellType == CellType.FORMULA) {
-      cellType = cell.getCachedFormulaResultType();
-    }
-
+    CellType cellType = getCellType(cell);
     switch (cellType) {
       case NUMERIC:
         if (DateUtil.isCellDateFormatted(cell)) {
@@ -129,12 +214,22 @@ public class ExcelReader {
         return cell.getStringCellValue();
       case BOOLEAN:
         return cell.getBooleanCellValue();
-      case ERROR:
-      case BLANK:
-      case _NONE:
+      default:
         return null;
     }
-    return null;
+  }
+
+  private static CellType getCellType(Cell cell) {
+    if (cell == null) {
+      return CellType._NONE;
+    }
+
+    CellType cellType = cell.getCellType();
+    if (cellType == CellType.FORMULA) {
+      cellType = cell.getCachedFormulaResultType();
+    }
+
+    return cellType;
   }
 
   /**
@@ -181,7 +276,12 @@ public class ExcelReader {
    * @throws IOException when the input stream cannot be read.
    */
   public static Table readSheetByName(
-      InputStream stream, String sheetName, int skip_rows, Integer row_limit, boolean xls_format)
+      InputStream stream,
+      String sheetName,
+      HeaderBehavior headers,
+      int skip_rows,
+      Integer row_limit,
+      boolean xls_format)
       throws IOException, IllegalArgumentException {
     Workbook workbook = getWorkbook(stream, xls_format);
 
@@ -191,7 +291,12 @@ public class ExcelReader {
     }
 
     return readSheetToTable(
-        workbook, sheetIndex, null, skip_rows, row_limit == null ? Integer.MAX_VALUE : row_limit);
+        workbook,
+        sheetIndex,
+        null,
+        headers,
+        skip_rows,
+        row_limit == null ? Integer.MAX_VALUE : row_limit);
   }
 
   /**
@@ -206,7 +311,12 @@ public class ExcelReader {
    * @throws IOException when the input stream cannot be read.
    */
   public static Table readSheetByIndex(
-      InputStream stream, int index, int skip_rows, Integer row_limit, boolean xls_format)
+      InputStream stream,
+      int index,
+      HeaderBehavior headers,
+      int skip_rows,
+      Integer row_limit,
+      boolean xls_format)
       throws IOException, IllegalArgumentException {
     Workbook workbook = getWorkbook(stream, xls_format);
 
@@ -217,7 +327,12 @@ public class ExcelReader {
     }
 
     return readSheetToTable(
-        workbook, index - 1, null, skip_rows, row_limit == null ? Integer.MAX_VALUE : row_limit);
+        workbook,
+        index - 1,
+        null,
+        headers,
+        skip_rows,
+        row_limit == null ? Integer.MAX_VALUE : row_limit);
   }
 
   /**
@@ -234,6 +349,7 @@ public class ExcelReader {
   public static Table readRangeByName(
       InputStream stream,
       String rangeNameOrAddress,
+      HeaderBehavior headers,
       int skip_rows,
       Integer row_limit,
       boolean xls_format)
@@ -242,7 +358,7 @@ public class ExcelReader {
 
     Name name = workbook.getName(rangeNameOrAddress);
     Range range = new Range(name == null ? rangeNameOrAddress : name.getRefersToFormula());
-    return readRange(workbook, range, skip_rows, row_limit);
+    return readRange(workbook, range, headers, skip_rows, row_limit);
   }
 
   /**
@@ -257,23 +373,34 @@ public class ExcelReader {
    * @throws IOException when the input stream cannot be read.
    */
   public static Table readRange(
-      InputStream stream, Range range, int skip_rows, Integer row_limit, boolean xls_format)
+      InputStream stream,
+      Range range,
+      HeaderBehavior headers,
+      int skip_rows,
+      Integer row_limit,
+      boolean xls_format)
       throws IOException {
-    return readRange(getWorkbook(stream, xls_format), range, skip_rows, row_limit);
+    return readRange(getWorkbook(stream, xls_format), range, headers, skip_rows, row_limit);
   }
 
   private static Workbook getWorkbook(InputStream stream, boolean xls_format) throws IOException {
     return xls_format ? new HSSFWorkbook(stream) : new XSSFWorkbook(stream);
   }
 
-  private static Table readRange(Workbook workbook, Range range, int skip_rows, Integer row_limit) {
+  private static Table readRange(
+      Workbook workbook, Range range, HeaderBehavior headers, int skip_rows, Integer row_limit) {
     int sheetIndex = getSheetIndex(workbook, range.getSheetName());
     if (sheetIndex == -1) {
       throw new IllegalArgumentException("Unknown sheet '" + range.getSheetName() + "'.");
     }
 
     return readSheetToTable(
-        workbook, sheetIndex, range, skip_rows, row_limit == null ? Integer.MAX_VALUE : row_limit);
+        workbook,
+        sheetIndex,
+        range,
+        headers,
+        skip_rows,
+        row_limit == null ? Integer.MAX_VALUE : row_limit);
   }
 
   /** Specifies how to set the headers for the returned table. */
