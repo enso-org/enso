c140cfb6795a8b456c7ff231e47ab5c0d1557859
Radosław Waśko <radoslaw.wasko@enso.org>
2022-06-20 14:58:38 +0200

    implement date formatters

---
+++ std-bits/table/src/main/java/org/enso/table/formatting/DateTimeFormatter.java
@@ -0,0 +1,30 @@
+package org.enso.table.formatting;
+
+import java.time.LocalDateTime;
+import java.util.Locale;
+
+public class DateTimeFormatter implements DataFormatter {
+    private final java.time.format.DateTimeFormatter formatter;
+
+    public DateTimeFormatter(String formatString, Locale locale) {
+        formatter = java.time.format.DateTimeFormatter.ofPattern(formatString, locale);
+    }
+
+    @Override
+    public String format(Object value) {
+        if (value == null) {
+            return NULL_REPRESENTATION;
+        }
+
+        if (value instanceof LocalDateTime date) {
+            return date.format(formatter);
+        }
+
+        throw new IllegalArgumentException("Unsupported type for DateTimeFormatter.");
+    }
+
+    @Override
+    public boolean canFormat(Object value) {
+        return value instanceof LocalDateTime;
+    }
+}
