64827e707f9f1c977a54914d41ebbb9f6d07e1f9
Radosław Waśko <radoslaw.wasko@enso.org>
2022-06-21 11:33:59 +0200

    date formatting tests

--- std-bits/table/src/main/java/org/enso/table/formatting/DateTimeFormatter.java
+++ std-bits/table/src/main/java/org/enso/table/formatting/DateTimeFormatter.java
@@ -1,6 +1,7 @@
 package org.enso.table.formatting;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.Locale;
 
 public class DateTimeFormatter implements DataFormatter {
@@ -20,11 +21,16 @@ public class DateTimeFormatter implements DataFormatter {
       return date.format(formatter);
     }
 
+    // Currently Enso uses ZonedDateTime for the date-time type. This should be revisited along with the Datetime API.
+    if (value instanceof ZonedDateTime date) {
+      return date.toLocalDateTime().format(formatter);
+    }
+
     throw new IllegalArgumentException("Unsupported type for DateTimeFormatter.");
   }
 
   @Override
   public boolean canFormat(Object value) {
-    return value instanceof LocalDateTime;
+    return value instanceof LocalDateTime || value instanceof ZonedDateTime;
   }
 }
