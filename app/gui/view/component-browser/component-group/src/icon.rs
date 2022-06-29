b60c56f0999c63f9ab760e62f3619bbdb63c4049
Mateusz Czapliński <mateusz.czaplinski@enso.org>
2022-06-29 17:16:06 +0200

    line width 1.5

--- app/gui/view/component-browser/component-group/src/icon.rs
+++ app/gui/view/component-browser/component-group/src/icon.rs
@@ -920,7 +920,7 @@ crate::define_icons! {
         ensogl::define_shape_system! {
             above = [crate::background, ensogl_list_view::background, ensogl_list_view::selection];
             (style: Style, strong_color: Vector4, weak_color: Vector4) {
-                let circle = Circle(7.0.px()) - Circle(5.0.px());
+                let circle = Circle(7.0.px()) - Circle(5.5.px());
                 let shape = circle.fill(strong_color);
                 let shape = shape.shrink(SHRINK_AMOUNT.px());
                 shape.into()
@@ -933,7 +933,7 @@ crate::define_icons! {
         ensogl::define_shape_system! {
             above = [crate::background, ensogl_list_view::background, ensogl_list_view::selection];
             (style: Style, strong_color: Vector4, weak_color: Vector4) {
-                let triangle = path(2.0, &[
+                let triangle = path(1.5, &[
                     // TODO: fix dimensions to make this possibly equilateral
                     (7.0, 0.0),
                     (-3.5, -7.0),
@@ -965,7 +965,7 @@ crate::define_icons! {
         ensogl::define_shape_system! {
             above = [crate::background, ensogl_list_view::background, ensogl_list_view::selection];
             (style: Style, strong_color: Vector4, weak_color: Vector4) {
-                let rhomb = path(2.0, &[
+                let rhomb = path(1.5, &[
                     (7.0, 0.0),
                     (0.0, -7.0),
                     (-7.0, 0.0),
