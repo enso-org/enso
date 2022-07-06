4acd605e04794404f43671fabfca6d30471110be
Mateusz Czapliński <mateusz.czaplinski@enso.org>
2022-07-06 18:04:55 +0200

    CLEANUP DEBUG

--- app/gui/view/component-browser/component-group/src/wide.rs
+++ app/gui/view/component-browser/component-group/src/wide.rs
@@ -317,7 +317,6 @@ impl<const COLUMNS: usize> Column<COLUMNS> {
         let half_height = height / 2.0;
         let background_bottom = -bg_height / 2.0;
         let pos_y = background_bottom + half_height + padding;
-        DEBUG!("MCDBG resize_and_place self.id=" self.id;? ", bg_height=" bg_height;? ", height=" height;? ", padding=" padding;? ", pos_y=" pos_y;?);
         self.list_view.set_position_x(pos_x);
         self.list_view.set_position_y(pos_y);
     }
