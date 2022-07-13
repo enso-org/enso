f2c6ea19dfa749ef057776738a00568f7f70a48e
Jaroslav Tulach <jaroslav.tulach@enso.org>
2022-07-13 08:11:27 +0200

    Support for parsing Date

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/date/ParseNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/date/ParseNode.java
@@ -3,8 +3,11 @@ package org.enso.interpreter.node.expression.builtin.date;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.nodes.Node;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import org.enso.interpreter.dsl.BuiltinMethod;
 import org.enso.interpreter.runtime.data.text.Text;
+import org.enso.interpreter.runtime.error.PanicException;
 
 @BuiltinMethod(
     type = "Date_Internal",
@@ -18,7 +21,21 @@ public abstract class ParseNode extends Node {
   abstract Object execute(Object self, Text text, Object pattern);
 
   @Specialization
-  Object doParse(Object self, Text text, Object pattern) {
-    return self;
+  Object doParse(Object self, Text text, Long noPattern) {
+    try {
+      return new EnsoDate(LocalDate.parse(text.toString()));
+    } catch (DateTimeParseException ex) {
+      throw new PanicException(ex, this);
+    }
+  }
+
+  @Specialization
+  Object doParse(Object self, Text text, Text pattern) {
+    try {
+      var formatter = DateTimeFormatter.ofPattern(pattern.toString());
+      return new EnsoDate(LocalDate.parse(text.toString(), formatter));
+    } catch (DateTimeParseException ex) {
+      throw new PanicException(ex.getMessage(), this);
+    }
   }
 }
