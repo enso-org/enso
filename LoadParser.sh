e45d2f5cf8c3b9b28f4b430a66973dec86f631ec
Jaroslav Tulach <jaroslav.tulach@enso.org>
2022-07-21 08:21:05 +0200

    Include GeneratedFormatTests check into the LoadParser.sh script

--- LoadParser.sh
+++ LoadParser.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+set -e
+
 GRAALVM=$HOME/bin/graalvm/
 OUT=build/parser
 
@@ -7,6 +9,10 @@ mkdir -p build/parser
 cargo run -p enso-parser-generate-java --bin enso-parser-generate-java -- $OUT
 
 $GRAALVM/bin/javac $OUT/*java --source-path ./lib/rust/parser/generate-java/java/ -d $OUT
+
+# run test
+cargo run -p enso-parser-generate-java --bin java-tests > $OUT/GeneratedFormatTests.java
+$GRAALVM/bin/java -cp $OUT GeneratedFormatTests
 
 cargo build -p enso-parser-cdylib
 $GRAALVM/bin/java -cp $OUT LoadParser.java
