9bc0146c6a7bdae6352c542fe67741a0c8c6ee62
Radosław Waśko <radoslaw.wasko@enso.org>
2022-08-29 12:57:55 +0200

    Update parsers to use the new builders

--- std-bits/table/src/main/java/org/enso/table/parsing/DateTimeParser.java
+++ std-bits/table/src/main/java/org/enso/table/parsing/DateTimeParser.java
@@ -1,10 +1,18 @@
 package org.enso.table.parsing;
 
-import java.time.LocalDateTime;
+import org.enso.table.data.column.builder.object.Builder;
+import org.enso.table.data.column.builder.object.DateTimeBuilder;
+
+import java.time.ZonedDateTime;
 import java.util.Locale;
 
 public class DateTimeParser extends BaseTimeParser {
   public DateTimeParser(String[] formats, Locale locale) {
-    super(formats, locale, LocalDateTime::parse);
+    super(formats, locale, ZonedDateTime::parse);
+  }
+
+  @Override
+  protected Builder makeBuilderWithCapacity(int capacity) {
+    return new DateTimeBuilder(capacity);
   }
 }
