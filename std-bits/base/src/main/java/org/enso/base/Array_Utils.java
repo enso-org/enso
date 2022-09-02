db03d685a3d7f8d3c37d1644aa053e99b91a0fad
Jaroslav Tulach <jaroslav.tulach@enso.org>
2022-09-02 18:38:02 +0200

    Mitigating missing List.add by using Array_Utils as the builder interface

--- std-bits/base/src/main/java/org/enso/base/Array_Utils.java
+++ std-bits/base/src/main/java/org/enso/base/Array_Utils.java
@@ -3,7 +3,13 @@ package org.enso.base;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Array_Utils {
+public class Array_Utils<T> {
+  private final List<T> builder;
+
+  private Array_Utils(int capacity) {
+    this.builder = new ArrayList<>(capacity);
+  }
+
   /**
    * This function forces the polyglot conversion of an Enso array into a `byte[]`. This allows for
    * asserting that it is a valid `byte[]`.
@@ -15,8 +21,35 @@ public class Array_Utils {
     return input;
   }
 
+  /** Creates new builder */
+  public static <V> Array_Utils<V> newBuilder(int capacity) {
+      return new Array_Utils<>(capacity);
+  }
+
+  /** Is the builder empty? */
+  public boolean isEmpty() {
+      return builder.isEmpty();
+  }
+
+  /** Adds an element to the builder
+     * @param e the element to add
+     */
+  public void add(T e) {
+      builder.add(e);
+  }
+
+  /** Obtains an element from the builder */
+  public T get(int index) {
+      return builder.get(index);
+  }
+
   /** A temporary workaround to be able to efficiently append an array to `ArrayList`. */
-  public static <T> void appendToArrayList(ArrayList<T> builder, List<T> list) {
+  public void appendTo(List<T> list) {
     builder.addAll(list);
+  }
+
+  /** Converts current content to a new array. */
+  public Object[] toArray() {
+      return builder.toArray();
   }
 }
