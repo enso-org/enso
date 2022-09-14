c9c51adf0022f05579e495efe3bf26ed3b0324df
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2022-09-14 12:34:06 +0200

    Test setup for checking cache invalidation logic
    Using _x, where x is the change step, to simulate modifications to files
    and thus trigger cache invalidation that should propagate to upstream
    modules.

---
+++ engine/runtime/src/test/scala/org/enso/compiler/test/CachingTest.scala
@@ -0,0 +1,22 @@
+package org.enso.compiler.test
+
+import org.enso.interpreter.test.InterpreterException
+
+class CachingTest extends ModifiedTest {
+  "Atoms and methods" should "be available for import" in {
+    evalTestProject("Test_Caching_Invalidation", iteration = 1)
+    val outLines = consumeOut
+    outLines(0) shouldEqual "hmm..."
+
+    evalTestProject("Test_Caching_Invalidation", iteration = 2)
+    val outLines2 = consumeOut
+    outLines2(0) shouldEqual "hmm..."
+
+    the[InterpreterException] thrownBy (evalTestProject(
+      "Test_Caching_Invalidation",
+      iteration = 3
+    )) should have message "Compilation aborted due to errors."
+    val outLines3 = consumeOut
+    outLines3(2) should endWith("The name `foo` could not be found.")
+  }
+}
