f16175be75d37c73a33a1ac7dc2a4024c6a79a50
Jaroslav Tulach <jaroslav.tulach@enso.org>
2022-09-20 05:40:36 +0200

    No cargo calls. Enough to invoke sbt bootstrap and buildEngineDistribution.

--- LoadParser.sh
+++ LoadParser.sh
@@ -6,10 +6,6 @@ GRAALVM=$HOME/bin/graalvm/
 
 # build runtime.jar including the new parser classes
 sbt --java-home $GRAALVM bootstrap
-sbt --java-home $GRAALVM syntax-rust-definition/compile
-
-cargo build -p enso-parser-jni
-
 sbt --java-home $GRAALVM buildEngineDistribution
 
 # run test: parser all .enso files in the repository
