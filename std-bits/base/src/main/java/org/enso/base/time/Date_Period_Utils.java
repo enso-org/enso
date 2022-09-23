c738bbfc363a692a8587d9e5e76e63b2ab072e16
Radosław Waśko <radoslaw.wasko@enso.org>
2022-09-23 12:56:53 +0200

    formatting, tests

--- std-bits/base/src/main/java/org/enso/base/time/Date_Period_Utils.java
+++ std-bits/base/src/main/java/org/enso/base/time/Date_Period_Utils.java
@@ -25,6 +25,7 @@ public class Date_Period_Utils implements TimeUtilsBase {
       };
 
   public static TemporalAdjuster end_of_week(DayOfWeek first_day) {
-      return (Temporal temporal) -> temporal.with(TemporalAdjusters.next(first_day)).minus(1, ChronoUnit.DAYS);
+    return (Temporal temporal) ->
+        temporal.with(TemporalAdjusters.next(first_day)).minus(1, ChronoUnit.DAYS);
   }
 }
