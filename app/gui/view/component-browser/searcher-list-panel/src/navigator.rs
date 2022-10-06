5efe2e8a0a89d4d8edb0b2028c9710ef83db9514
Mateusz Czapliński <mateusz.czaplinski@enso.org>
2022-10-06 13:10:10 +0200

    some cleanups

--- app/gui/view/component-browser/searcher-list-panel/src/navigator.rs
+++ app/gui/view/component-browser/searcher-list-panel/src/navigator.rs
@@ -125,11 +125,10 @@ impl Navigator {
             });
 
 
-            // === Tooltip when hovering the Marketplace button
+            // === Show tooltip when hovering the Marketplace button
 
-            // FIXME[mc]: const
+            // FIXME[mc]: const for `1`
             marketplace_button_selected <- top_buttons.selected_entry.map(|id| *id == Some(1));
-            // marketplace_button_selected <- marketplace_button_selected.on_change();
             marketplace_button_hovered <- marketplace_button_selected && top_buttons.is_mouse_over;
             marketplace_button_hovered <- marketplace_button_hovered.on_change();
             tooltip.frp.set_style <+ marketplace_button_hovered.map(|hovered| if *hovered {
@@ -142,17 +141,6 @@ impl Navigator {
             eval marketplace_button_hovered([](hovered) {
                 tracing::warn!("MCDBG marketplace_button_hovered {hovered}")
             });
-            // tooltip.frp.set_style <+ top_buttons.selected_entry.map(|id| match *id {
-            //     Some(id) if id == 1 => 
-            //         // FIXME[mc]: const TOOLTIP_LOCATION
-            //         // tooltip::Style::set_label(text.into()), // .with_placement(TOOLTIP_LOCATION)
-            //         tooltip::Style::set_label("Marketplace will be available soon".into()).with_placement(tooltip::Placement::Bottom),
-            //     _ => tooltip::Style::unset_label(),
-            // });
-            // eval top_buttons.selected_entry([](id) match id {
-            //     Some(id) => tracing::warn!("MCDBG btn id {id}"),
-            //     None => {},
-            // });
         }
         bottom_buttons.select_entry(Some(2));
 
