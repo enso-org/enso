d9af10e58c96d7e75955e6e86896975e41866026
Pavel Marek <pavel.marek@enso.org>
2022-10-14 17:26:38 +0200

    Revert: Rethrow exceptions during parsing of a foreign method in an installed language

--- engine/runtime-language-epb/src/main/java/org/enso/interpreter/epb/node/ForeignEvalNode.java
+++ engine/runtime-language-epb/src/main/java/org/enso/interpreter/epb/node/ForeignEvalNode.java
@@ -4,8 +4,10 @@ import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.exception.AbstractTruffleException;
 import com.oracle.truffle.api.frame.FrameDescriptor;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.nodes.RootNode;
 import com.oracle.truffle.api.source.Source;
 import java.util.Arrays;
@@ -22,6 +24,8 @@ public class ForeignEvalNode extends RootNode {
   private final EpbParser.Result code;
   private @Child ForeignFunctionCallNode foreign;
   private @Child ContextRewrapNode rewrapNode = ContextRewrapNode.build();
+
+  private @Child ContextRewrapExceptionNode rewrapExceptionNode = ContextRewrapExceptionNode.build();
   private @CompilationFinal ForeignParsingException parseException;
   private final String[] argNames;
 
@@ -116,28 +120,42 @@ public class ForeignEvalNode extends RootNode {
       CallTarget ct = EpbContext.get(this).getEnv().parsePublic(source);
       Object fn = rewrapNode.execute(ct.call(), inner, outer);
       foreign = insert(JsForeignNode.build(fn, argNames.length));
+    } catch (Throwable e) {
+      if (InteropLibrary.getUncached().isException(e)) {
+        throw rewrapExceptionNode.execute((AbstractTruffleException) e, inner, outer);
+      } else {
+        throw e;
+      }
     } finally {
       inner.leave(this, p);
     }
   }
 
   private void parsePy() {
-    String args = Arrays.stream(argNames).collect(Collectors.joining(","));
-    String head =
-        "import polyglot\n"
-            + "@polyglot.export_value\n"
-            + "def polyglot_enso_python_eval("
-            + args
-            + "):\n";
-    String indentLines =
-        code.getForeignSource().lines().map(l -> "    " + l).collect(Collectors.joining("\n"));
-    Source source =
-        Source.newBuilder(code.getLanguage().getTruffleId(), head + indentLines, "").build();
-    EpbContext context = EpbContext.get(this);
-    CallTarget ct = context.getEnv().parsePublic(source);
-    ct.call();
-    Object fn = context.getEnv().importSymbol("polyglot_enso_python_eval");
-    foreign = insert(PyForeignNodeGen.create(fn));
+    try {
+      String args = Arrays.stream(argNames).collect(Collectors.joining(","));
+      String head =
+          "import polyglot\n"
+              + "@polyglot.export_value\n"
+              + "def polyglot_enso_python_eval("
+              + args
+              + "):\n";
+      String indentLines =
+          code.getForeignSource().lines().map(l -> "    " + l).collect(Collectors.joining("\n"));
+      Source source =
+          Source.newBuilder(code.getLanguage().getTruffleId(), head + indentLines, "").build();
+      EpbContext context = EpbContext.get(this);
+      CallTarget ct = context.getEnv().parsePublic(source);
+      ct.call();
+      Object fn = context.getEnv().importSymbol("polyglot_enso_python_eval");
+      foreign = insert(PyForeignNodeGen.create(fn));
+    } catch (Throwable e) {
+      if (InteropLibrary.getUncached().isException(e)) {
+        throw (AbstractTruffleException) e;
+      } else {
+        throw e;
+      }
+    }
   }
 
   private void parseR() {
@@ -155,6 +173,12 @@ public class ForeignEvalNode extends RootNode {
       CallTarget ct = EpbContext.get(this).getEnv().parsePublic(source);
       Object fn = rewrapNode.execute(ct.call(), inner, outer);
       foreign = insert(RForeignNodeGen.create(fn));
+    }  catch (Throwable e) {
+      if (InteropLibrary.getUncached().isException(e)) {
+        throw rewrapExceptionNode.execute((AbstractTruffleException) e, inner, outer);
+      } else {
+        throw e;
+      }
     } finally {
       inner.leave(this, p);
     }
