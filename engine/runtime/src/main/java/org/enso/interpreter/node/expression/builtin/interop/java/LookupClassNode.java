de00c579a7bb0e3e2a3f3e8a2b5f7271525d9366
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2022-10-14 12:21:24 +0200

    more cleanup

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/interop/java/LookupClassNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/interop/java/LookupClassNode.java
@@ -14,9 +14,9 @@ public abstract class LookupClassNode extends Node {
   }
 
   @Specialization
-  Object doExecute(Object self, Object name, @Cached("build()") ExpectStringNode expectStringNode) {
+  Object doExecute(Object name, @Cached("build()") ExpectStringNode expectStringNode) {
     return Context.get(this).getEnvironment().lookupHostSymbol(expectStringNode.execute(name));
   }
 
-  abstract Object execute(Object self, Object name);
+  abstract Object execute(Object name);
 }
