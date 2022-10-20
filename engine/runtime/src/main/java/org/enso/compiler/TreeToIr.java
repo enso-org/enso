8ad76cc45a9e5dcbff7cd17b3281e40881008248
Kaz Wesley <kaz@lambdaverse.org>
2022-10-20 22:14:00 +0000

    translate unary minus

--- engine/runtime/src/main/java/org/enso/compiler/TreeToIr.java
+++ engine/runtime/src/main/java/org/enso/compiler/TreeToIr.java
@@ -669,16 +669,11 @@ final class TreeToIr {
           default -> ast;
         };
       }
-      case Tree.UnaryOprApp un -> switch (translateExpression(un.getRhs(), false)) {
-        case IR$Literal$Number n when "-".equals(un.getOpr().codeRepr()) -> n.copy(
-          n.copy$default$1(),
-          "-" + n.copy$default$2(),
-          n.copy$default$3(),
-          n.copy$default$4(),
-          n.copy$default$5(),
-          n.copy$default$6()
-        );
-        case IR.Expression expr -> expr;
+      case Tree.UnaryOprApp un -> {
+        var expr = translateExpression(un.getRhs(), false);
+        var negate = new IR$Name$Literal("negate", true, Option.empty(), meta(), diag());
+        var arg = new IR$CallArgument$Specified(Option.empty(), expr, expr.location(), meta(), diag());
+        yield new IR$Application$Prefix(negate, cons(arg, nil()), false, expr.location(), meta(), diag());
       };
       case Tree.TypeSignature sig -> {
         var methodName = buildName(sig.getVariable());
