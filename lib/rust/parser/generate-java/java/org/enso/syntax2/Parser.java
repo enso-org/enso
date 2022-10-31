e0f3dba4d0f014c90d3370345b78d2ed6c9a0030
Jaroslav Tulach <jaroslav.tulach@enso.org>
2022-10-31 11:00:58 +0100

    Locate the enso_parser library in native-libraries directory

--- lib/rust/parser/generate-java/java/org/enso/syntax2/Parser.java
+++ lib/rust/parser/generate-java/java/org/enso/syntax2/Parser.java
@@ -1,7 +1,7 @@
 package org.enso.syntax2;
 
-import org.enso.syntax2.Message;
 import java.io.File;
+import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.charset.StandardCharsets;
@@ -9,26 +9,39 @@ import java.nio.charset.StandardCharsets;
 public final class Parser implements AutoCloseable {
   static {
     String os = System.getProperty("os.name");
-    File dir = new File(".").getAbsoluteFile();
-    for (; ; ) {
-      File parser;
-      if (os.startsWith("Mac")) {
-        parser = new File(dir, "target/rust/debug/libenso_parser.dylib");
-      } else if (os.startsWith("Windows")) {
-        parser = new File(dir, "target/rust/debug/enso_parser.dll");
-      } else {
-        parser = new File(dir, "target/rust/debug/libenso_parser.so");
-      }
-      try {
-        System.load(parser.getAbsolutePath());
-        break;
-      } catch (LinkageError e) {
-        dir = dir.getParentFile();
-        if (dir == null) {
-          throw e;
+    String name;
+    if (os.startsWith("Mac")) {
+      name = "libenso_parser.dylib";
+    } else if (os.startsWith("Windows")) {
+      name = "enso_parser.dll";
+    } else {
+      name = "libenso_parser.so";
+    }
+
+    File parser = null;
+    try {
+      var whereAmI = Parser.class.getProtectionDomain().getCodeSource().getLocation();
+      File dir = new File(new File(whereAmI.toURI()).getParentFile().getParentFile(), "native-libraries");
+      parser = new File(dir, name);
+      System.load(parser.getAbsolutePath());
+    } catch (URISyntaxException | LinkageError e) {
+      System.err.println("Cannot load " + parser);
+      File root = new File(".").getAbsoluteFile();
+      for (var dir = new File(new File(new File(root, "target"), "rust"), "debug");;) {
+        parser = new File(dir, name);
+        try {
+          System.load(parser.getAbsolutePath());
+          System.err.println("Succeeded loading " + parser.getAbsolutePath());
+          break;
+        } catch (LinkageError err) {
+          dir = dir.getParentFile();
+          if (dir == null) {
+            throw err;
+          }
         }
       }
     }
+
   }
 
   private long state;
