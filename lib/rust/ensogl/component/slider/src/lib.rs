b5b4e7d952b239a8d3fbc2b39d71722a23867bf0
Galin Bajlekov <galin.bajlekov@enso.org>
2022-11-04 17:40:09 +0100

    Added bounds and basic precision scaling

--- lib/rust/ensogl/component/slider/src/lib.rs
+++ lib/rust/ensogl/component/slider/src/lib.rs
@@ -4,26 +4,30 @@
 
 pub mod model;
 
-use ensogl_core::prelude::*;
 use crate::model::*;
+use ensogl_core::prelude::*;
 
 use ensogl_core::application;
 use ensogl_core::application::Application;
-use ensogl_core::display;
-use ensogl_core::display::shape::StyleWatchFrp;
-
 use ensogl_core::data::color;
+use ensogl_core::display;
+
+
+
+// ========================
+// === Slider component ===
+// ========================
 
 pub struct Slider {
     pub frp: Rc<Frp>,
-    model: Rc<Model>,
+    model:   Rc<Model>,
     pub app: Application,
 }
 
 impl Slider {
     pub fn new(app: &Application) -> Self {
         let model = Rc::new(Model::new(&app));
-        let app = app.clone_ref();    
+        let app = app.clone_ref();
         let frp = Rc::new(Frp::new());
 
         Self { frp, model, app }.init()
@@ -64,6 +68,11 @@ impl application::View for Slider {
 }
 
 
+
+// ===========
+// === FRP ===
+// ===========
+
 ensogl_core::define_endpoints_2! {
     Input {
         set_width(f32),
@@ -78,10 +87,12 @@ ensogl_core::define_endpoints_2! {
 
         set_tooltip(Option<String>),
         set_label(Option<String>),
-        
-        resize(Vector2),
+
+        resize(Vector2), // remove in favor of set_width/height?
     }
     Output {
+        width(f32),
+        height(f32),
         value(f32),
     }
 }
@@ -96,17 +107,8 @@ impl Slider {
         let scene = &self.app.display.default_scene;
         let mouse = &scene.mouse.frp;
 
-        //mouse.
-
         frp::extend! { network
 
-            eval input.resize (
-                (v) {
-                    model.set_size(*v);
-                }
-            );
-
-            
             // User input
 
             background_click        <- model.background.events.mouse_down.constant(());
@@ -123,31 +125,79 @@ impl Slider {
 
             drag_pos_start          <- mouse.position.sample(&component_click);
             drag_pos_end            <- mouse.position.gate(&component_drag);
-            drag_pos_end            <- any2(&drag_pos_end, &drag_pos_start); // FIXME: Does not set to start position on click!
-            
-            //drag_delta              <- all2(&drag_pos_end, &drag_pos_start).map(|(end, start)| end - start);
-            drag_pos_prev           <- drag_pos_end.previous();
-            drag_delta              <- all2(&drag_pos_end, &drag_pos_prev).map(|(end, start)| end - start);
+            drag_pos_end            <- any2(&drag_pos_end, &drag_pos_start);
 
-            eval drag_delta (
-                (v) {
-                    model.drag(*v);
-                }
+
+            // FIXME: also update on set_width, set_height
+            component_width         <- input.resize.map(|v| v.x );
+            component_height        <- input.resize.map(|v| v.y );
+
+            // distance dragged
+            drag_delta              <- all2(&drag_pos_end, &drag_pos_start).map(|(end, start)| end - start);
+            // horizontal drag as fraction of element width
+            drag_x_fract            <- all2(&drag_delta, &component_width).map(
+                |(delta, width)| delta.x / width
+            );
+            // vertical drag as fraction of element height
+            drag_y_fract            <- all2(&drag_delta, &component_height).map(
+                |(delta, height)| delta.y / height
             );
 
+
+            // value calculation
+
+            precision               <- input.set_precision.sample(&component_click);
+            precision_adjusted      <- all2(&precision, &drag_y_fract).map(
+                |(base, offset)| *base * (offset/5.0).exp()
+            );
+            range                   <- all2(&input.set_min, &input.set_max).map(
+                |(min, max)| max - min
+            );
+
+            value_start             <- output.value.sample(&component_click);
+            value                   <- all4(&value_start, &range, &precision_adjusted, &drag_x_fract).map(
+                |(value, range, precision, delta)| value + delta * precision * range
+            );
+            value_clamped           <- all3(&value, &input.set_min, &input.set_max).map(
+                |(value, min, max)| value.max(*min).min(*max)
+            );
+            output.value            <+ any2(&input.set_value, &value_clamped);
+
+            track_pos               <- all3(&value_clamped, &input.set_min, &input.set_max).map(
+                |(value, min, max)| (value - min) / (max - min)
+            );
+
+
+            // model update
+
+            eval value_clamped (
+                (v) {
+                    model.update_value(*v);
+                }
+            );
+            eval track_pos (
+                (v) {
+                    model.update_track(*v);
+                }
+            );
             eval_ component_click (
                 model.set_active();
             );
-
             eval_ component_release (
                 model.set_inactive();
             );
-
+            eval input.resize (
+                (v) {
+                    model.set_size(*v);
+                }
+            );
         }
+
+        self.frp.set_min(0.0);
+        self.frp.set_max(5.0);
+        self.frp.set_value(0.5);
+        self.frp.set_precision(1.0);
 
         self
     }
 }
-
-
-
--- lib/rust/ensogl/component/slider/src/model.rs
+++ lib/rust/ensogl/component/slider/src/model.rs
@@ -1,22 +1,25 @@
 //! Model for the slider component
 
-use ensogl_core::prelude::*;
 use ensogl_core::display::shape::*;
+use ensogl_core::prelude::*;
 
 use ensogl_core::application::Application;
 use ensogl_core::data::color;
 use ensogl_core::display;
 use ensogl_text as text;
-use ensogl_shadow as shadow;
 
+
+
+// ========================
+// === Model definition ===
+// ========================
 
 pub struct Model {
-    pub background:         background::View,
-    pub track:              track::View,
-    pub label:              text::Text,
-    pub root:               display::object::Instance,
-    
-    value:                  Cell<f32>,
+    pub background: background::View,
+    pub track:      track::View,
+    pub label:      text::Text,
+    pub value:      text::Text,
+    pub root:       display::object::Instance,
 
     pub app: Application,
 }
@@ -25,33 +28,31 @@ impl Model {
     pub fn new(app: &Application) -> Self {
         let root = display::object::Instance::new();
         let label = app.new_view::<text::Text>();
+        let value = app.new_view::<text::Text>();
         let background = background::View::new();
         let track = track::View::new();
 
-        let value = Cell::new(0.5);
-
         background.size.set(Vector2::new(200.0, 200.0));
-        background.color.set(color::Rgba(0.8,0.8,0.8, 1.0).into());
+        background.color.set(color::Rgba(0.8, 0.8, 0.8, 1.0).into());
 
         track.size.set(Vector2::new(200.0, 200.0));
         track.color.set(color::Rgba(0.4, 0.4, 0.6, 1.0).into());
-        track.value.set(value.get().into());
+        track.value.set(0.0.into());
 
         let app = app.clone_ref();
         let scene = &app.display.default_scene;
 
-        root.add_child(&label);
         root.add_child(&background);
         root.add_child(&track);
+        root.add_child(&label);
+        root.add_child(&value);
 
-        Self {
-            background,
-            track,
-            label,
-            root,
-            value,
-            app,
-        }
+        value.remove_from_scene_layer(&scene.layers.main);
+        value.add_to_scene_layer(&scene.layers.label);
+        label.remove_from_scene_layer(&scene.layers.main);
+        label.add_to_scene_layer(&scene.layers.label);
+
+        Self { background, track, label, value, root, app }
     }
 
     pub fn set_size(&self, size: Vector2) {
@@ -63,11 +64,11 @@ impl Model {
         self.track.color.set(color.into());
     }
 
-    pub fn drag(&self, dist: Vector2) {
-        // FIXME: keep value in FRP and not in component model!
-        let value = self.value.get() + dist.x * 0.001;
-        let value = value.min(1.0).max(0.0);
-        self.value.set(value);
+    pub fn update_value(&self, value: f32) {
+        self.value.frp.set_content.emit(format!("{:.2}", value))
+    }
+
+    pub fn update_track(&self, value: f32) {
         self.track.value.set(value);
     }
 
@@ -87,20 +88,23 @@ impl display::Object for Model {
 }
 
 
+
+// ========================
+// === Shape definition ===
+// ========================
+
 struct Background {
-    pub width:          Var<Pixels>,
-    pub height:         Var<Pixels>,
-    pub shape:          AnyShape,
+    pub width:  Var<Pixels>,
+    pub height: Var<Pixels>,
+    pub shape:  AnyShape,
 }
 
 impl Background {
-    fn new(style: &StyleWatch) -> Self {
+    fn new() -> Self {
         let width: Var<Pixels> = "input_size.x".into();
         let height: Var<Pixels> = "input_size.y".into();
 
-        let shape = Rect((&width, &height))
-            .corners_radius(&height/2.0)
-            .into();
+        let shape = Rect((&width, &height)).corners_radius(&height / 2.0).into();
 
         Background { width, height, shape }
     }
@@ -110,8 +114,8 @@ mod background {
     use super::*;
 
     ensogl_core::define_shape_system! {
-        (style:Style, color:Vector4) {
-            Background::new(style)
+        (color:Vector4) {
+            Background::new()
                 .shape
                 .fill(color)
                 .into()
@@ -123,13 +127,13 @@ mod track {
     use super::*;
 
     ensogl_core::define_shape_system! {
-        (style:Style, value:f32, color:Vector4) {
+        (value:f32, color:Vector4) {
             let Background{
                 width,
                 height,
                 shape: background
-            } = Background::new(style);
-            
+            } = Background::new();
+
             Rect( (&width*&value, &height) )
                 .translate_x(&width*(&value-1.0)*0.5)
                 .intersection(background)
