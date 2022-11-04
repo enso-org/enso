21bb6338d4826157e4ab73148638860924af160d
Galin Bajlekov <galin.bajlekov@enso.org>
2022-11-04 17:40:27 +0100

    Cleaned up example scene

--- lib/rust/ensogl/example/slider/src/lib.rs
+++ lib/rust/ensogl/example/slider/src/lib.rs
@@ -26,12 +26,9 @@ use ensogl_core::prelude::*;
 use wasm_bindgen::prelude::*;
 
 use ensogl_core::application::Application;
-use ensogl_core::data::color;
 use ensogl_core::display::object::ObjectOps;
 use ensogl_hardcoded_theme as theme;
-use ensogl_selector as selector;
 use ensogl_slider as slider;
-use ensogl_selector::Bounds;
 use ensogl_text_msdf::run_once_initialized;
 
 
@@ -58,20 +55,6 @@ fn make_slider(app: &Application) -> Leak<slider::Slider> {
     Leak::new(slider)
 }
 
-fn make_number_picker(app: &Application) -> Leak<selector::NumberPicker> {
-    let slider = app.new_view::<selector::NumberPicker>();
-    slider.frp.resize(Vector2(200.0, 50.0));
-    app.display.add_child(&slider);
-    Leak::new(slider)
-}
-
-fn make_range_picker(app: &Application) -> Leak<selector::NumberRangePicker> {
-    let slider = app.new_view::<selector::NumberRangePicker>();
-    slider.frp.resize(Vector2(400.0, 50.0));
-    app.display.add_child(&slider);
-    Leak::new(slider)
-}
-
 
 
 // ========================
@@ -85,22 +68,4 @@ fn init(app: &Application) {
 
     let slider1 = make_slider(app);
     slider1.inner().frp.resize(Vector2(400.0, 50.0));
-    //slider1.inner().frp.allow_click_selection(true);
-
-    let slider2 = make_number_picker(app);
-    slider2.inner().frp.resize(Vector2(400.0, 50.0));
-    slider2.inner().frp.set_bounds.emit(Bounds::new(-100.0, 100.0));
-    slider2.inner().set_position_y(50.0);
-    slider2.inner().frp.use_overflow_bounds(Bounds::new(-150.0, 200.0));
-    slider2.inner().frp.set_caption(Some("Value:".to_string()));
-
-    let slider3 = make_range_picker(app);
-    slider3.inner().set_position_y(-100.0);
-    slider3.inner().set_track_color(color::Rgba::new(0.0, 0.80, 0.80, 1.0));
-
-    let slider4 = make_range_picker(app);
-    slider4.inner().set_position_y(-200.0);
-    slider4.inner().frp.use_overflow_bounds(Bounds::new(-2.0, 3.0));
-    slider4.inner().frp.set_caption(Some("Caption".to_string()));
-    slider4.inner().set_track_color(color::Rgba::new(0.5, 0.70, 0.70, 1.0));
 }
