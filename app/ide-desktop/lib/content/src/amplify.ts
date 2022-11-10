bdc7c727759718038438051ae724eef88349735f
Dmitry Bushev <bushevdv@gmail.com>
2022-11-10 19:36:15 +0300

    feat: minified example

--- app/ide-desktop/lib/content/src/amplify.ts
+++ app/ide-desktop/lib/content/src/amplify.ts
@@ -1,32 +1,17 @@
-if (!process.env.NEXT_PUBLIC_AUTH_USER_POOL_ID) {
-    throw new Error("NEXT_PUBLIC_AUTH_USER_POOL_ID");
-}
-if (!process.env.NEXT_PUBLIC_AUTH_USER_POOL_WEB_CLIENT_ID) {
-    throw new Error("NEXT_PUBLIC_AUTH_USER_POOL_WEB_CLIENT_ID");
-}
-if (!process.env.NEXT_PUBLIC_AUTH_DOMAIN) {
-    throw new Error("NEXT_PUBLIC_AUTH_DOMAIN");
-}
-if (!process.env.NEXT_PUBLIC_AUTH_REDIRECT_SIGN_IN) {
-    throw new Error("NEXT_PUBLIC_AUTH_REDIRECT_SIGN_IN");
-}
-if (!process.env.NEXT_PUBLIC_AUTH_REDIRECT_SIGN_OUT) {
-    throw new Error("NEXT_PUBLIC_AUTH_REDIRECT_SIGN_OUT");
-}
-
 // =====================
 // === amplifyConfig ===
 // =====================
 
+const IDENTITY_POOL_ID = process.env.NEXT_IDENTITY_POOL_ID
 // (required) - Amazon Cognito Region.
-const REGION = "eu-west-1";
+const REGION = process.env.REGION
 // (optional) - Amazon Cognito User Pool ID.
-const USER_POOL_ID = process.env.NEXT_PUBLIC_AUTH_USER_POOL_ID;
+const USER_POOL_ID = process.env.NEXT_PUBLIC_AUTH_USER_POOL_ID
 // (optional) - Amazon Cognito Web Client ID (26-char alphanumeric string, App client secret needs
 // to be disabled).
 const USER_POOL_WEB_CLIENT_ID = process.env.NEXT_PUBLIC_AUTH_USER_POOL_WEB_CLIENT_ID;
 // (required) - Domain that hosts the OAuth endpoints for login, logout, and token refresh.
-const DOMAIN = process.env.NEXT_PUBLIC_AUTH_DOMAIN;
+const DOMAIN = process.env.NEXT_PUBLIC_AUTH_DOMAIN
 // (optional) - Redirect to this URL back from Cognito after the user signs in.
 const REDIRECT_SIGN_IN = process.env.NEXT_PUBLIC_AUTH_REDIRECT_SIGN_IN;
 // (optional) - Redirect to this URL back from Cognito after the user signs out.
@@ -39,6 +24,7 @@ const RESPONSE_TYPE = "code";
 /// functionality via external OAuth providers (e.g. Google) through Amazon Cognito.
 export const amplifyConfig = {
     region: REGION,
+    identityPoolId: IDENTITY_POOL_ID,
     userPoolId: USER_POOL_ID,
     userPoolWebClientId: USER_POOL_WEB_CLIENT_ID,
     oauth: {
