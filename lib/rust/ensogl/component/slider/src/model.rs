d0fd6894b280a7ab87db2f1bddc0944d95d9e615
Galin Bajlekov <galin.bajlekov@enso.org>
2022-11-11 12:01:41 +0100

    Restructure model.rs

--- lib/rust/ensogl/component/slider/src/model.rs
+++ lib/rust/ensogl/component/slider/src/model.rs
@@ -19,111 +19,9 @@ const COMPONENT_MARGIN: f32 = 4.0;
 
 
 
-// ========================
-// === Model definition ===
-// ========================
-
-/// Slider model structure
-pub struct Model {
-    /// Background element
-    pub background:  background::View,
-    /// Slider track element that moves dependent on the value of the slider
-    pub track:       track::View,
-    /// Slider label
-    pub label:       text::Text,
-    /// Slider value text left of the decimal point
-    pub value_left:  text::Text,
-    /// Slider value text decimal point
-    pub value_dot:   text::Text,
-    /// Slider value text right of the decimal point
-    pub value_right: text::Text,
-
-    pub root: display::object::Instance,
-    pub app:  Application,
-}
-
-impl Model {
-    pub fn new(app: &Application) -> Self {
-        let root = display::object::Instance::new();
-        let label = app.new_view::<text::Text>();
-        let value_left = app.new_view::<text::Text>();
-        let value_dot = app.new_view::<text::Text>();
-        let value_right = app.new_view::<text::Text>();
-        let background = background::View::new();
-        let track = track::View::new();
-
-        let app = app.clone_ref();
-        let scene = &app.display.default_scene;
-
-        root.add_child(&background);
-        root.add_child(&track);
-        root.add_child(&label);
-        root.add_child(&value_left);
-        root.add_child(&value_dot);
-        root.add_child(&value_right);
-
-        value_left.add_to_scene_layer(&scene.layers.label);
-        value_dot.add_to_scene_layer(&scene.layers.label);
-        value_right.add_to_scene_layer(&scene.layers.label);
-        label.add_to_scene_layer(&scene.layers.label);
-
-        let model =
-            Self { background, track, label, value_left, value_dot, value_right, root, app };
-
-        model.set_size(Vector2::new(200.0, 50.0));
-        model.value_dot.set_content(".");
-
-        model
-    }
-
-    /// Set component size
-    pub fn set_size(&self, size: Vector2<f32>) {
-        let margin = Vector2(COMPONENT_MARGIN * 2.0, COMPONENT_MARGIN * 2.0);
-        self.background.size.set(size + margin);
-        self.track.size.set(size + margin);
-    }
-
-    /// Set slider track color
-    pub fn set_track_color(&self, color: color::Lcha) {
-        self.track.color.set(color::Rgba::from(color).into());
-    }
-    /// Set slider background color
-    pub fn set_background_color(&self, color: color::Lcha) {
-        self.background.color.set(color::Rgba::from(color).into());
-    }
-
-    /// Set slider label visibility
-    pub fn set_label_visible(&self, enabled: bool) {
-        if enabled {
-            self.root.add_child(&self.label);
-        } else {
-            self.root.remove_child(&self.label);
-        }
-    }
-
-    /// Set visibility of value text right of the decimal point
-    pub fn set_value_decimal_visible(&self, enabled: bool) {
-        if enabled {
-            self.root.add_child(&self.value_dot);
-            self.root.add_child(&self.value_right);
-        } else {
-            self.root.remove_child(&self.value_dot);
-            self.root.remove_child(&self.value_right);
-        }
-    }
-}
-
-impl display::Object for Model {
-    fn display_object(&self) -> &display::object::Instance {
-        &self.root
-    }
-}
-
-
-
-// ========================
-// === Shape definition ===
-// ========================
+// =====================================================
+// === Slider background and track shape definitions ===
+// =====================================================
 
 struct Background {
     pub width:  Var<Pixels>,
@@ -132,7 +30,7 @@ struct Background {
 }
 
 impl Background {
-    /// Create new rounded background shape
+    /// Create new rounded background rectangle
     fn new() -> Self {
         let width: Var<Pixels> = "input_size.x".into();
         let height: Var<Pixels> = "input_size.y".into();
@@ -181,5 +79,107 @@ mod track {
                 .fill(color)
                 .into()
         }
+    }
+}
+
+
+
+// ===============================
+// === Slider model definition ===
+// ===============================
+
+/// Slider model structure
+pub struct Model {
+    /// Background element
+    pub background:  background::View,
+    /// Slider track element that moves dependent on the value of the slider
+    pub track:       track::View,
+    /// Slider label
+    pub label:       text::Text,
+    /// Slider value text left of the decimal point
+    pub value_left:  text::Text,
+    /// Slider value text decimal point
+    pub value_dot:   text::Text,
+    /// Slider value text right of the decimal point
+    pub value_right: text::Text,
+
+    pub root: display::object::Instance,
+    pub app:  Application,
+}
+
+impl Model {
+    pub fn new(app: &Application) -> Self {
+        let root = display::object::Instance::new();
+        let label = app.new_view::<text::Text>();
+        let value_left = app.new_view::<text::Text>();
+        let value_dot = app.new_view::<text::Text>();
+        let value_right = app.new_view::<text::Text>();
+        let background = background::View::new();
+        let track = track::View::new();
+
+        let app = app.clone_ref();
+        let scene = &app.display.default_scene;
+
+        root.add_child(&background);
+        root.add_child(&track);
+        root.add_child(&label);
+        root.add_child(&value_left);
+        root.add_child(&value_dot);
+        root.add_child(&value_right);
+
+        value_left.add_to_scene_layer(&scene.layers.label);
+        value_dot.add_to_scene_layer(&scene.layers.label);
+        value_right.add_to_scene_layer(&scene.layers.label);
+        label.add_to_scene_layer(&scene.layers.label);
+
+        Self { background, track, label, value_left, value_dot, value_right, root, app }.init()
+    }
+
+    /// Initialise model
+    pub fn init(self) -> Self {
+        self.value_dot.set_content(".");
+        self
+    }
+
+    /// Set component size
+    pub fn set_size(&self, size: Vector2<f32>) {
+        let margin = Vector2(COMPONENT_MARGIN * 2.0, COMPONENT_MARGIN * 2.0);
+        self.background.size.set(size + margin);
+        self.track.size.set(size + margin);
+    }
+
+    /// Set slider track color
+    pub fn set_track_color(&self, color: color::Lcha) {
+        self.track.color.set(color::Rgba::from(color).into());
+    }
+    /// Set slider background color
+    pub fn set_background_color(&self, color: color::Lcha) {
+        self.background.color.set(color::Rgba::from(color).into());
+    }
+
+    /// Set slider label visibility
+    pub fn set_label_visible(&self, enabled: bool) {
+        if enabled {
+            self.root.add_child(&self.label);
+        } else {
+            self.root.remove_child(&self.label);
+        }
+    }
+
+    /// Set visibility of value text right of the decimal point
+    pub fn set_value_decimal_visible(&self, enabled: bool) {
+        if enabled {
+            self.root.add_child(&self.value_dot);
+            self.root.add_child(&self.value_right);
+        } else {
+            self.root.remove_child(&self.value_dot);
+            self.root.remove_child(&self.value_right);
+        }
+    }
+}
+
+impl display::Object for Model {
+    fn display_object(&self) -> &display::object::Instance {
+        &self.root
     }
 }
