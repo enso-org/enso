087d5a0eec61f0bddeed37ec9d06bd0066625904
Jaroslav Tulach <jaroslav.tulach@enso.org>
2022-11-15 11:42:14 +0100

    Description how to build and execute the parser in JVM and NI mode

--- lib/rust/parser/target/scala-2.13/classes/Load.java
+++ lib/rust/parser/target/scala-2.13/classes/Load.java
@@ -1,6 +1,22 @@
 
 import org.enso.syntax2.Parser;
 
+/**
+ * <h1>Verify this runs</h1>
+ * Use:
+ * <pre>
+ * lib/rust/parser/target/scala-2.13/classes$ /graalvm/bin/javac Load.java
+ * lib/rust/parser/target/scala-2.13/classes$ /graalvm/bin/java Load
+ * </pre>
+ * to execute the parser and print the result.
+ *
+ * <h1>Try Native Image</h1>
+ * Use:
+ * <pre>
+ * lib/rust/parser/target/scala-2.13/classes$ /graalvm/bin/javac Load.java
+ * lib/rust/parser/target/scala-2.13/classes$ /graalvm/bin/native-image -cp . Load && ./load
+ * </pre>
+ */
 class Load {
   public static void main(String... args) {
     var tree = Parser.create().parse("main = 42");
