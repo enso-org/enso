d22aed181d8c8cbc23461bfdc3af497f6b3f7626
Jaroslav Tulach <jaroslav.tulach@enso.org>
2022-11-15 11:51:50 +0100

    More complex example with --initialize-at-run-time

--- lib/rust/parser/target/scala-2.13/classes/Load.java
+++ lib/rust/parser/target/scala-2.13/classes/Load.java
@@ -16,8 +16,20 @@ import org.enso.syntax2.Parser;
  * lib/rust/parser/target/scala-2.13/classes$ /graalvm/bin/javac Load.java
  * lib/rust/parser/target/scala-2.13/classes$ /graalvm/bin/native-image -cp . Load && ./load
  * </pre>
+ * 
+ * <h1>Try more complex Native Image</h1>
+ * <pre>
+ * $ /graalvm/bin/javac Load.java
+ * $ /graalvm/bin/native-image \
+ *   --initialize-at-build-time \
+ *   --initialize-at-run-time=org.enso.syntax2.Parser \
+ *   -cp . Load
+ * </pre>
  */
 class Load {
+  static {
+    System.out.println("Initializing Load");
+  }
   public static void main(String... args) {
     var tree = Parser.create().parse("main = 42");
     System.out.println(tree);
