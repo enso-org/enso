b49d53e3a6e9e794f9e94617e0a5bf281a22bdc9
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2022-11-26 18:50:21 +0100

    add comment

--- app/ide-desktop/lib/client/tasks/signArchivesMacOs.ts
+++ app/ide-desktop/lib/client/tasks/signArchivesMacOs.ts
@@ -167,6 +167,9 @@ class ArchiveToSign implements Signable {
             if (isJar) {
                 run(`jar`, ['xf', this.path], workingDir)
             } else {
+                // We cannot use `unzip` here because of the following issue:
+                // https://unix.stackexchange.com/questions/115825/extra-bytes-error-when-unzipping-a-file
+                // This started to be an issue with GraalVM 22.3.0 release.
                 run(`7za`, ['X', `-o${workingDir}`, this.path])
             }
 
