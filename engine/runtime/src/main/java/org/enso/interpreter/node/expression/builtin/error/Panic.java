a8104e2b1a57542fd1776bf4c0009b1de06bfd2c
James Dunkerley <james.dunkerley@gmail.com>
2022-11-29 13:20:30 +0000

    Move Error and Panic to Standard.Base.

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/error/Panic.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/error/Panic.java
@@ -3,5 +3,5 @@ package org.enso.interpreter.node.expression.builtin.error;
 import org.enso.interpreter.dsl.BuiltinType;
 import org.enso.interpreter.node.expression.builtin.Builtin;
 
-@BuiltinType(name = "Standard.Base.Error.Common.Panic")
+@BuiltinType(name = "Standard.Base.Error.Panic")
 public class Panic extends Builtin {}
