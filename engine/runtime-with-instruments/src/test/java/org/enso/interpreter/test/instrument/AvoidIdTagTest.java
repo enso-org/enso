170a2e88a50adbbdf91d4bc121fdc607a872163c
Jaroslav Tulach <jaroslav.tulach@enso.org>
2022-12-12 14:35:22 +0100

    Dump where AvoidIdInstrumentationTag is placed

---
+++ engine/runtime-with-instruments/src/test/java/org/enso/interpreter/test/instrument/AvoidIdTagTest.java
@@ -0,0 +1,92 @@
+package org.enso.interpreter.test.instrument;
+import com.oracle.truffle.api.debug.Debugger;
+import com.oracle.truffle.api.instrumentation.InstrumentableNode;
+import com.oracle.truffle.api.instrumentation.StandardTags;
+import java.io.OutputStream;
+import java.nio.file.Paths;
+import java.util.Map;
+import org.enso.interpreter.runtime.tag.AvoidIdInstrumentationTag;
+import org.enso.interpreter.runtime.tag.IdentifiedTag;
+import org.enso.interpreter.test.NodeCountingTestInstrument;
+import org.enso.interpreter.test.instrument.RuntimeServerTest.TestContext;
+import org.enso.polyglot.RuntimeOptions;
+import org.enso.polyglot.runtime.Runtime$Api$InitializedNotification;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Engine;
+import org.graalvm.polyglot.Language;
+import org.junit.After;
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AvoidIdTagTest {
+
+  private Engine engine;
+  private Context context;
+
+  @Before
+  public void initContext() {
+    engine = Engine.newBuilder()
+        .allowExperimentalOptions(true)
+        .option(
+            RuntimeOptions.LANGUAGE_HOME_OVERRIDE,
+            Paths.get("../../distribution/component").toFile().getAbsolutePath()
+        )
+        .logHandler(OutputStream.nullOutputStream())
+        .build();
+
+    context = Context.newBuilder()
+        .engine(engine)
+        .allowExperimentalOptions(true)
+        .allowIO(true)
+        .allowAllAccess(true)
+        .build();
+
+    Map<String, Language> langs = engine.getLanguages();
+    Assert.assertNotNull("Enso found: " + langs, langs.get("enso"));
+  }
+
+  @After
+  public void disposeContext() {
+    context.close();
+    engine.close();
+  }
+
+  @Test
+  public void instrumentedNodes() {
+    var nodes = engine.getInstruments().get(NodeCountingTestInstrument.INSTRUMENT_ID).lookup(NodeCountingTestInstrument.class);
+    nodes.enable();
+
+    var code = """
+    from Standard.Base import all
+    import Standard.Visualization
+
+    run n = 0.up_to n . map i-> 1.noise * i
+    """;
+
+    var module = context.eval("enso", code);
+    var run = module.invokeMember("eval_expression", "run");
+    var res = run.execute(10000);
+    var found = nodes.assertNewNodes("Give me nodes", 0, 10000);
+
+    for (var nn : found.values()) {
+      for (var n : nn) {
+        var ss = n.getSourceSection();
+        if (ss == null) {
+          continue;
+        }
+        var st = ss.getCharacters().toString();
+        if (st.contains("noise")) {
+          System.err.println("code: " + st + " for node " + n.getClass().getName());
+          if (n instanceof InstrumentableNode in) {
+            System.err.println("  AvoidIdInstrumentationTag: " + in.hasTag(AvoidIdInstrumentationTag.class));
+            System.err.println("  IdentifiedTag: " + in.hasTag(IdentifiedTag.class));
+            System.err.println("  ExpressionTag: " + in.hasTag(StandardTags.ExpressionTag.class));
+          }
+        }
+      }
+    }
+  }
+}
