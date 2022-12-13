d6972cb2f5fbbe25a34a785ffa4d73be1296aa7b
James Dunkerley <james.dunkerley@gmail.com>
2022-12-13 23:32:18 +0000

    Statistics tests and more in Enso less in Java.

--- std-bits/base/src/main/java/org/enso/base/statistics/Moments.java
+++ std-bits/base/src/main/java/org/enso/base/statistics/Moments.java
@@ -1,69 +1,78 @@
 package org.enso.base.statistics;
 
-import java.util.Arrays;
-
 /** Set of descriptive statistics for numerical data sets */
 public class Moments {
 
-  /** Statistic to compute the total of the values. */
+  /**
+   * Statistic to compute the total of the values.
+   */
   public static final MomentStatistic SUM = new Sum();
 
-  /** Statistic to compute the mean average of the values. */
+  /**
+   * Statistic to compute the mean average of the values.
+   */
   public static final MomentStatistic MEAN = new Mean();
 
-  /** Statistic to compute the sample variance of the values. */
+  /**
+   * Statistic to compute the sample variance of the values.
+   */
   public static final MomentStatistic VARIANCE = new Variance(false);
 
-  /** Statistic to compute the population variance of the values. */
+  /**
+   * Statistic to compute the population variance of the values.
+   */
   public static final MomentStatistic VARIANCE_POPULATION = new Variance(true);
 
-  /** Statistic to compute the sample standard deviation of the values. */
+  /**
+   * Statistic to compute the sample standard deviation of the values.
+   */
   public static final MomentStatistic STANDARD_DEVIATION = new StandardDeviation(false);
 
-  /** Statistic to compute the population standard deviation of the values. */
+  /**
+   * Statistic to compute the population standard deviation of the values.
+   */
   public static final MomentStatistic STANDARD_DEVIATION_POPULATION = new StandardDeviation(true);
 
-  /** Statistic to compute the sample skewness of the values. */
+  /**
+   * Statistic to compute the sample skewness of the values.
+   */
   public static final MomentStatistic SKEW = new Skew(false);
 
-  /** Statistic to compute the population skewness of the values. */
+  /**
+   * Statistic to compute the population skewness of the values.
+   */
   public static final MomentStatistic SKEW_POPULATION = new Skew(true);
 
-  /** Statistic to compute the sample kurtosis of the values. */
+  /**
+   * Statistic to compute the sample kurtosis of the values.
+   */
   public static final MomentStatistic KURTOSIS = new Kurtosis();
 
+  private long count;
+  private double[] totals;
+
   /**
-   * Compute a set of statistics on a data set.
+   * Create a new instance.
    *
-   * @param data set of values.
-   * @param statistics set of statistics to compute.
-   * @return computed statistics.
+   * @param order the maximum order of moments to compute.
    */
-  public static double[] compute(Double[] data, MomentStatistic[] statistics) {
-    if (statistics.length == 0) {
-      return new double[0];
+  public Moments(int order) {
+    this.count = 0;
+    this.totals = new double[order];
+  }
+
+  public Moments add(double value) {
+    count++;
+    double v = value;
+    for (int i = 0; i < totals.length; i++) {
+      totals[i] += v;
+      v *= value;
     }
 
-    int order = Arrays.stream(statistics).mapToInt(s -> s == null ? 0 : s.order()).max().getAsInt();
+    return this;
+  }
 
-    long count = 0;
-    double[] totals = new double[order];
-    for (Double value : data) {
-      if (value == null || Double.isNaN(value)) {
-        continue;
-      }
-
-      count++;
-      double v = value;
-      for (int i = 0; i < order; i++) {
-        totals[i] += v;
-        v *= value;
-      }
-    }
-
-    final long _count = count;
-    return Arrays.stream(statistics)
-        .mapToDouble(s -> s == null ? Double.NaN : s.evaluate(_count, totals))
-        .toArray();
+  public double compute(MomentStatistic s) {
+    return s.evaluate(count, totals);
   }
 }
