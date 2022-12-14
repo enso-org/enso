3bea818df30d5856775d54bbca7c5a5483c6b868
James Dunkerley <james.dunkerley@gmail.com>
2022-12-14 07:03:09 +0000

    Java Format and missing import.

--- std-bits/base/src/main/java/org/enso/base/statistics/Moments.java
+++ std-bits/base/src/main/java/org/enso/base/statistics/Moments.java
@@ -3,49 +3,31 @@ package org.enso.base.statistics;
 /** Set of descriptive statistics for numerical data sets */
 public class Moments {
 
-  /**
-   * Statistic to compute the total of the values.
-   */
+  /** Statistic to compute the total of the values. */
   public static final MomentStatistic SUM = new Sum();
 
-  /**
-   * Statistic to compute the mean average of the values.
-   */
+  /** Statistic to compute the mean average of the values. */
   public static final MomentStatistic MEAN = new Mean();
 
-  /**
-   * Statistic to compute the sample variance of the values.
-   */
+  /** Statistic to compute the sample variance of the values. */
   public static final MomentStatistic VARIANCE = new Variance(false);
 
-  /**
-   * Statistic to compute the population variance of the values.
-   */
+  /** Statistic to compute the population variance of the values. */
   public static final MomentStatistic VARIANCE_POPULATION = new Variance(true);
 
-  /**
-   * Statistic to compute the sample standard deviation of the values.
-   */
+  /** Statistic to compute the sample standard deviation of the values. */
   public static final MomentStatistic STANDARD_DEVIATION = new StandardDeviation(false);
 
-  /**
-   * Statistic to compute the population standard deviation of the values.
-   */
+  /** Statistic to compute the population standard deviation of the values. */
   public static final MomentStatistic STANDARD_DEVIATION_POPULATION = new StandardDeviation(true);
 
-  /**
-   * Statistic to compute the sample skewness of the values.
-   */
+  /** Statistic to compute the sample skewness of the values. */
   public static final MomentStatistic SKEW = new Skew(false);
 
-  /**
-   * Statistic to compute the population skewness of the values.
-   */
+  /** Statistic to compute the population skewness of the values. */
   public static final MomentStatistic SKEW_POPULATION = new Skew(true);
 
-  /**
-   * Statistic to compute the sample kurtosis of the values.
-   */
+  /** Statistic to compute the sample kurtosis of the values. */
   public static final MomentStatistic KURTOSIS = new Kurtosis();
 
   private long count;
