d46b8570c58ad24ee1caa282f5e8128f85a568e8
Jaroslav Tulach <jaroslav.tulach@enso.org>
2022-12-27 06:41:09 +0100

    Close action on a project

--- tools/enso4igv/src/main/java/org/enso/tools/enso4igv/EnsoLogicalView.java
+++ tools/enso4igv/src/main/java/org/enso/tools/enso4igv/EnsoLogicalView.java
@@ -24,7 +24,7 @@ import org.openide.util.NbCollections;
 import org.openide.util.lookup.Lookups;
 
 @ActionReferences({
-    @ActionReference(position = 4100, id = @ActionID(category = "System", id = "org-openide-actions-EditAction"), path = "Projects/ensosbtprj/Actions", separatorBefore = 4000),
+    @ActionReference(position = 3100, id = @ActionID(category = "Project", id = "org-netbeans-modules-project-ui-CloseProject"), path = "Projects/ensosbtprj/Actions", separatorBefore = 3000),
 })
 final class EnsoLogicalView implements LogicalViewProvider  {
     private final EnsoSbtProject p;
