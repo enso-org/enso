3df23b3acdb2a9986d6694f0cffea2a8f9927c76
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-01-05 09:32:16 +0100

    Make fill function really a function that can be called

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/LazyAtomInstanceNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/LazyAtomInstanceNode.java
@@ -2,6 +2,7 @@ package org.enso.interpreter.node.expression.builtin.meta;
 
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.interop.ArityException;
 import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.TruffleObject;
@@ -11,10 +12,15 @@ import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
 import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.nodes.RootNode;
 import org.enso.interpreter.dsl.BuiltinMethod;
 import org.enso.interpreter.node.expression.builtin.mutable.CoerceArrayNode;
 import org.enso.interpreter.runtime.EnsoContext;
+import org.enso.interpreter.runtime.callable.argument.ArgumentDefinition;
+import org.enso.interpreter.runtime.callable.argument.CallArgumentInfo;
 import org.enso.interpreter.runtime.callable.atom.Atom;
+import org.enso.interpreter.runtime.callable.function.Function;
+import org.enso.interpreter.runtime.callable.function.FunctionSchema;
 import org.enso.interpreter.runtime.data.Array;
 import org.enso.interpreter.runtime.data.Vector;
 import org.enso.interpreter.runtime.error.PanicException;
@@ -33,15 +39,21 @@ public abstract class LazyAtomInstanceNode extends Node {
   abstract Vector execute(Object factory);
 
   @Specialization(guards = "iop.isExecutable(factory)")
-  Vector doExecute(Object factory, @CachedLibrary(limit="3") InteropLibrary iop, @Cached CoerceArrayNode coerce) {
+  Vector doExecute(
+    Object factory,
+    @CachedLibrary(limit="3") InteropLibrary iop,
+    @Cached CoerceArrayNode coerce,
+    @Cached SwapAtomFieldNode swapNode
+  ) {
     var ctx = EnsoContext.get(this);
-    var lazy = new HoleInAtom(null, -1, null);
+    var lazy = new HoleInAtom();
     try {
       var r = iop.execute(factory, lazy);
       if (r instanceof Atom a) {
         for (int i = 0; i < a.getFields().length; i++) {
           if (a.getFields()[i] == lazy) {
-            return Vector.fromArray(new Array(a, new HoleInAtom(a, i, lazy)));
+            var function = swapNode.createFn(a, i, lazy);
+            return Vector.fromArray(new Array(a, function));
           }
         }
       }
@@ -62,51 +74,56 @@ public abstract class LazyAtomInstanceNode extends Node {
 
   @ExportLibrary(InteropLibrary.class)
   static final class HoleInAtom implements TruffleObject {
-    private final Atom atom;
-    private final int index;
-    private final Object empty;
-
-    HoleInAtom(Atom atom, int index, Object empty) {
-      this.atom = atom;
-      this.index = index;
-      this.empty = empty;
+    HoleInAtom() {
     }
 
     @ExportMessage
-    Object invokeMember(String name, Object[] args, @CachedLibrary("this") InteropLibrary iop) throws UnsupportedMessageException {
-      if ("set".equals(name)) {
-        return execute(args, iop);
-      } else {
-        throw UnsupportedMessageException.create();
+    String toDisplayString(boolean pure) {
+      return "Meta.lazy_atom";
+    }
+  }
+  static final class SwapAtomFieldNode extends RootNode {
+    private final FunctionSchema schema;
+
+    private SwapAtomFieldNode() {
+      super(null);
+      this.schema = new FunctionSchema(FunctionSchema.CallerFrameAccess.NONE, new ArgumentDefinition[]{
+        new ArgumentDefinition(0, "instance", ArgumentDefinition.ExecutionMode.EXECUTE),
+        new ArgumentDefinition(1, "lazy_index", ArgumentDefinition.ExecutionMode.EXECUTE),
+        new ArgumentDefinition(2, "lazy", ArgumentDefinition.ExecutionMode.EXECUTE),
+        new ArgumentDefinition(3, "value", ArgumentDefinition.ExecutionMode.EXECUTE)
+      }, new boolean[]{
+        true, true, true, false
+      }, new CallArgumentInfo[0]);
+    }
+
+    static SwapAtomFieldNode create() {
+      return new SwapAtomFieldNode();
+    }
+
+    Function createFn(Atom atom, int index, HoleInAtom lazy) {
+      var preArgs = new Object[]{atom, index, lazy, null};
+      return new Function(
+              getCallTarget(),
+              null,
+              schema,
+              preArgs,
+              new Object[]{}
+      );
+    }
+
+    @Override
+    public Object execute(VirtualFrame frame) {
+      var args = Function.ArgumentsHelper.getPositionalArguments(frame.getArguments());
+      if (args[0] instanceof Atom replace) {
+        if (args[1] instanceof Number n) {
+          var fields = replace.getFields();
+          if (fields[n.intValue()] == args[2]) {
+            fields[n.intValue()] = args[3];
+          }
+        }
       }
-    }
-
-    @ExportMessage
-    boolean isMemberInvocable(String name) {
-      return "set".equals(name);
-    }
-
-    @ExportMessage boolean hasMembers() {
-      return true;
-    }
-
-    @ExportMessage
-    Object getMembers(boolean includeInternal) throws UnsupportedMessageException {
-      return new Array("set");
-    }
-
-    @ExportMessage
-    Object execute(Object[] args, @CachedLibrary("this") InteropLibrary iop) {
-      var prev = atom.getFields()[index];
-      if (prev == empty) {
-        atom.getFields()[index] = args[0];
-      }
-      return EnsoContext.get(iop).getBuiltins().nothing();
-    }
-
-    @ExportMessage
-    boolean isExecutable() {
-      return true;
+      return EnsoContext.get(this).getBuiltins().nothing();
     }
   }
 }
