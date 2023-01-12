e15583fe65b5846f93f8a947a8dd9d6eb538d135
Kaz Wesley <kaz@lambdaverse.org>
2023-01-12 08:51:44 -0800

    Parser: Support annotations in type defs (#4036)
    Support application of the new type of annotation to method bindings in type definitions.

--- engine/runtime/src/main/java/org/enso/compiler/TreeToIr.java
+++ engine/runtime/src/main/java/org/enso/compiler/TreeToIr.java
@@ -69,7 +69,6 @@ import org.enso.syntax2.Line;
 import org.enso.syntax2.TextElement;
 import org.enso.syntax2.Token;
 import org.enso.syntax2.Tree;
-import org.enso.syntax2.TypeDefStatement;
 
 import scala.Option;
 import scala.collection.immutable.LinearSeq;
@@ -207,25 +206,10 @@ final class TreeToIr {
     return switch (inputAst) {
       case null -> appendTo;
       case Tree.TypeDef def -> {
-        List<IR> irBody = nil();
         var typeName = buildName(def.getName(), true);
+        List<IR> irBody = nil();
         for (var line : def.getBody()) {
-          var definition = line.getStatement();
-          switch (definition) {
-            case null -> {}
-            case TypeDefStatement.Binding bind -> irBody = translateTypeBodyExpression(bind.getStatement(), irBody);
-            case TypeDefStatement.TypeConstructorDef cons -> {
-              if (cons.getDocumentation() != null) {
-                irBody = cons(translateComment(def, cons.getDocumentation()), irBody);
-              }
-              var constructorName = buildName(inputAst, cons.getConstructor());
-              List<IR.DefinitionArgument> args = translateArgumentsDefinition(cons.getArguments());
-              var cAt = getIdentifiedLocation(inputAst);
-              var ir = new IR$Module$Scope$Definition$Data(constructorName, args, cAt, meta(), diag());
-              irBody = cons(ir, irBody);
-            }
-            default -> {}
-          }
+          irBody = translateTypeBodyExpression(line.getExpression(), irBody);
         }
         List<IR.DefinitionArgument> args = translateArgumentsDefinition(def.getParams());
         var type = new IR$Module$Scope$Definition$SugaredType(
@@ -317,6 +301,13 @@ final class TreeToIr {
     return CollectionConverters.asScala(args.stream().map(p -> translateArgumentDefinition(p)).iterator()).toList();
   }
 
+  IR translateConstructorDefinition(Tree.ConstructorDefinition cons, Tree inputAst) {
+    var constructorName = buildName(inputAst, cons.getConstructor());
+    List<IR.DefinitionArgument> args = translateArgumentsDefinition(cons.getArguments());
+    var cAt = getIdentifiedLocation(inputAst);
+    return new IR$Module$Scope$Definition$Data(constructorName, args, cAt, meta(), diag());
+  }
+
   /** Translates any expression that can be found in the body of a type
     * declaration from [[AST]] into [[IR]].
     *
@@ -328,6 +319,7 @@ final class TreeToIr {
     var inputAst = maybeManyParensed(exp);
     return switch (inputAst) {
       case null -> appendTo;
+      case Tree.ConstructorDefinition cons -> cons(translateConstructorDefinition(cons, inputAst), appendTo);
       case Tree.TypeDef def -> {
         var ir = translateSyntaxError(def, IR$Error$Syntax$UnexpectedDeclarationInType$.MODULE$);
         yield cons(ir, appendTo);
