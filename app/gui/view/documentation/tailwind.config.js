5f83fd3f87f1ee4d96f9357bbf7921ee32d0e0db
Ilya Bogdanov <fumlead@gmail.com>
2023-01-16 16:32:30 +0400

    Remove unnecessary pattern from tailwind config

--- app/gui/view/documentation/tailwind.config.js
+++ app/gui/view/documentation/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['src/**/*.rs', 'assets/**/*.svg'],
+    content: ['src/**/*.rs'],
     theme: {
         extend: {
             colors: {
