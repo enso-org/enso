596d6bef357d5b34f6d18778f8eadf8589f9d565
Adam Obuchowicz <adam.obuchowicz@enso.org>
2023-01-26 10:21:00 +0100

    Automatic extending the texture

--- lib/rust/ensogl/core/src/display/render/passes/cache_shapes.rs
+++ lib/rust/ensogl/core/src/display/render/passes/cache_shapes.rs
@@ -1,6 +1,10 @@
+//! Pass where we checks what shapes created with [`cached_shape!`](crate::cached_shape) macro are
+//! ready to being rendered on the texture.
+
 use crate::prelude::*;
 
 use crate::data::bounding_box::BoundingBox;
+use crate::display;
 use crate::display::render::pass;
 use crate::display::render::pass::Instance;
 use crate::display::scene::Layer;
@@ -12,49 +16,52 @@ use crate::display::Context;
 use crate::display::Scene;
 use crate::gui::component::AnyShapeView;
 
-use crate::display;
 use itertools::iproduct;
 
-const TEX_SIZE: i32 = 1024;
+const INITIAL_TEXTURE_SIZE: i32 = 256;
 
 #[derive(Clone, Derivative)]
 #[derivative(Debug)]
 pub struct CacheShapesPass {
-    scene:       Scene,
-    framebuffer: Option<pass::Framebuffer>,
+    scene:        Scene,
+    framebuffer:  Option<pass::Framebuffer>,
     #[derivative(Debug = "ignore")]
-    shapes:      Vec<Rc<dyn AnyShapeView>>,
-    layer:       Layer,
+    shapes:       Vec<Rc<dyn AnyShapeView>>,
+    texture_size: Vector2<i32>,
+    layer:        Layer,
 }
 
 impl pass::Definition for CacheShapesPass {
     fn initialize(&mut self, instance: &Instance) {
+        let ArrangedShapes { texture_size, shapes } = display::world::CACHED_SHAPES_DEFINITIONS
+            .with_borrow(|cached_shapes| arrange_shapes_on_texture(cached_shapes));
+        self.shapes = shapes.into_iter().map(Box::into).collect();
+        self.texture_size = texture_size;
+
+        for shape in &self.shapes {
+            warn!("The position on initialization is {:?}", shape.xy());
+            self.scene.add_child(&**shape);
+            self.layer.add(&**shape);
+        }
+        self.layer.camera().set_screen(texture_size.x as f32, texture_size.y as f32);
+        self.layer.camera().update(&self.scene);
+        self.scene.display_object.update(&self.scene);
+        self.layer.update();
+
         let output = pass::OutputDefinition::new_rgba("color");
 
         // FIXME[ao]: duplicated with new_screen_texture?
         let context = &instance.context;
         let variables = &instance.variables;
         let name = "pass_cached_shapes";
-        let args = (TEX_SIZE, TEX_SIZE);
         let format = output.internal_format;
         let item_type = output.item_type;
+        let args = (texture_size.x, texture_size.y);
         let params = Some(output.texture_parameters);
         let texture = uniform::get_or_add_gpu_texture_dyn(
             context, variables, name, format, item_type, args, params,
         );
         self.framebuffer = Some(instance.new_framebuffer(&[&texture]));
-        self.shapes = display::world::CACHED_SHAPES_DEFINITIONS.with_borrow(|cached_shapes| {
-            arrange_shapes_on_texture(cached_shapes, TEX_SIZE).map(Box::into).collect()
-        });
-        for shape in &self.shapes {
-            warn!("The position on initialization is {:?}", shape.xy());
-            self.scene.add_child(&**shape);
-            self.layer.add(&**shape);
-        }
-        self.layer.camera().set_screen(TEX_SIZE as f32, TEX_SIZE as f32);
-        self.layer.camera().update(&self.scene);
-        self.scene.display_object.update(&self.scene);
-        self.layer.update();
     }
 
     fn run(&mut self, instance: &Instance, _update_status: UpdateStatus) {
@@ -65,7 +72,7 @@ impl pass::Definition for CacheShapesPass {
         if ready_shapes.peek().is_some() {
             if let Some(framebuffer) = self.framebuffer.as_ref() {
                 framebuffer.bind();
-                instance.context.viewport(0, 0, TEX_SIZE, TEX_SIZE);
+                instance.context.viewport(0, 0, self.texture_size.x, self.texture_size.y);
                 for shape in ready_shapes {
                     warn!("Rendering shape {}", shape.sprite().symbol.id);
                     // warn!("After update it is {}", shape.sprite().symbol.id);
@@ -97,129 +104,160 @@ impl pass::Definition for CacheShapesPass {
 impl CacheShapesPass {
     pub fn new(scene: &Scene) -> Self {
         Self {
-            framebuffer: default(),
-            shapes:      default(),
-            layer:       Layer::new("Cached Shapes"),
-            scene:       scene.clone_ref(),
+            framebuffer:  default(),
+            shapes:       default(),
+            layer:        Layer::new("Cached Shapes"),
+            scene:        scene.clone_ref(),
+            texture_size: default(),
         }
     }
 }
 
-struct ShapeBeingPlacedOnTexture<'a> {
-    definition:     &'a CachedShapeDefinition,
-    position_found: Option<Vector2>,
+
+fn find_free_place(
+    shape_size: Vector2<i32>,
+    tex_size: i32,
+    placed_so_far: &[BoundingBox],
+) -> Option<BoundingBox> {
+    let row_or_col_to_coord = |index| (index as f32) - (tex_size as f32) / 2.0;
+    let candidate_rows = (0..tex_size - shape_size.y + 1).map(row_or_col_to_coord);
+    let candidate_cols = (0..tex_size - shape_size.x + 1).map(row_or_col_to_coord);
+    let candidate_positions = iproduct!(candidate_rows, candidate_cols);
+    let mut candidate_bboxes = candidate_positions.map(|(y, x)| {
+        BoundingBox::from_position_and_size(Vector2(x, y), shape_size.map(|i| i as f32))
+    });
+    candidate_bboxes.find(|bbox| {
+        let is_collision = placed_so_far.iter().any(|placed| placed.interior_intersects(bbox));
+        !is_collision
+    })
 }
 
-impl<'a> ShapeBeingPlacedOnTexture<'a> {
-    fn new(definition: &'a CachedShapeDefinition) -> Self {
-        Self { definition, position_found: None }
-    }
-
-    fn put_on_free_place(
-        &mut self,
-        tex_size: i32,
-        placed_so_far: &[BoundingBox],
-    ) -> Option<BoundingBox> {
-        let free_place = self.find_free_place(tex_size, placed_so_far);
-        self.position_found = free_place.as_ref().map(|bbox| bbox.center());
-        if self.position_found.is_none() {
-            error!("Cannot insert shape! The texture size {tex_size} is too small!")
-        }
-        free_place
-    }
-
-    fn find_free_place(&self, tex_size: i32, placed_so_far: &[BoundingBox]) -> Option<BoundingBox> {
-        let shape_size = self.definition.size;
-        let row_or_col_to_coord = |index| (index as f32) - (tex_size as f32) / 2.0;
-        let candidate_rows = (0..tex_size - shape_size.y + 1).map(row_or_col_to_coord);
-        let candidate_cols = (0..tex_size - shape_size.x + 1).map(row_or_col_to_coord);
-        let candidate_positions = iproduct!(candidate_rows, candidate_cols);
-        let mut candidate_bboxes = candidate_positions.map(|(y, x)| {
-            BoundingBox::from_position_and_size(Vector2(x, y), shape_size.map(|i| i as f32))
-        });
-        candidate_bboxes.find(|bbox| {
-            let is_collision = placed_so_far.iter().any(|placed| placed.interior_intersects(bbox));
-            !is_collision
-        })
-    }
-
-    fn create_shape(self) -> Option<Box<dyn AnyShapeView>> {
-        self.position_found.map(|pos| {
-            let shape = (self.definition.cons)();
-            shape.set_xy(pos);
-            shape.set_size(self.definition.size.map(|i| i as f32));
-            shape
-        })
-    }
+struct ArrangedShapes {
+    texture_size: Vector2<i32>,
+    shapes:       Vec<Box<dyn AnyShapeView>>,
 }
+
+
 
 fn arrange_shapes_on_texture<'a>(
     shapes: impl IntoIterator<Item = &'a CachedShapeDefinition>,
-    tex_size: i32,
-) -> impl Iterator<Item = Box<dyn AnyShapeView>> + 'a {
-    let shapes = shapes.into_iter().map(ShapeBeingPlacedOnTexture::new);
+) -> ArrangedShapes {
     let sorted_shapes =
-        shapes.sorted_by_key(|shape| shape.definition.size.x * shape.definition.size.y).rev();
-    let mut placed_so_far: Vec<BoundingBox> = vec![];
-    sorted_shapes.filter_map(move |mut shape| {
-        if let Some(took_bbox) = shape.put_on_free_place(tex_size, &placed_so_far) {
-            placed_so_far.push(took_bbox);
+        shapes.into_iter().sorted_by_key(|shape| shape.size.x * shape.size.y).rev().collect_vec();
+    let mut tex_size = INITIAL_TEXTURE_SIZE;
+    loop {
+        if let Some(arranged) = try_to_fit_shapes_on_texture(&sorted_shapes, tex_size) {
+            break arranged;
         }
-        shape.create_shape()
-    })
+        tex_size *= 2;
+    }
+}
+
+fn try_to_fit_shapes_on_texture(
+    sorted_shapes: &[impl std::borrow::Borrow<CachedShapeDefinition>],
+    max_texture_size: i32,
+) -> Option<ArrangedShapes> {
+    let mut placed_so_far: Vec<BoundingBox> = vec![];
+    let mut texture_size: Vector2<i32> = default();
+    let sorted_shapes_ref = sorted_shapes.iter().map(|shape| shape.borrow());
+    let shape_view_iter = sorted_shapes_ref.map(move |shape_def| {
+        find_free_place(shape_def.size, max_texture_size, &placed_so_far).map(|bbox| {
+            texture_size.x = max(texture_size.x, bbox.right() as i32);
+            texture_size.y = max(texture_size.y, bbox.top() as i32);
+            placed_so_far.push(bbox);
+            let position = bbox.center();
+            let shape = (shape_def.cons)();
+            shape.set_size(shape_def.size);
+            shape.set_xy(position);
+            shape
+        })
+    });
+    let shape_views: Option<Vec<_>> = shape_view_iter.collect();
+    shape_views.map(|shapes| ArrangedShapes { shapes, texture_size })
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::display::shape::*;
-    use wasm_bindgen::prelude::*;
 
     mod mock_shape {
         use super::*;
 
-        crate::shape2! {
+        crate::shape! {
             (style: Style) {
                 Plane().into()
             }
         }
     }
 
+    fn shape_entries_from_sizes(
+        sizes: impl IntoIterator<Item = (i32, i32)>,
+    ) -> Vec<CachedShapeDefinition> {
+        sizes
+            .into_iter()
+            .map(|(width, height)| CachedShapeDefinition {
+                size: Vector2(width, height),
+                cons: Box::new(|| Box::new(mock_shape::View::new())),
+            })
+            .collect_vec()
+    }
+
     #[test]
-    fn arrange_shapes_in_texture() {
+    fn fitting_shapes_on_texture() {
         fn run_case<const N: usize>(
             tex_size: i32,
             sizes: [(i32, i32); N],
-            expected_position: [(f32, f32); N],
+            expected_position: Option<[(f32, f32); N]>,
         ) {
-            let shape_entries = sizes
-                .into_iter()
-                .map(|(width, height)| CachedShapeDefinition {
-                    size: Vector2(width, height),
-                    cons: Box::new(|| Box::new(mock_shape::View::new())),
-                })
-                .collect_vec();
-            let shapes = arrange_shapes_on_texture(&shape_entries, tex_size);
-            let positions = shapes.map(|shape| (shape.x(), shape.y())).collect_vec();
-            assert_eq!(&positions, &expected_position);
+            let shape_entries = shape_entries_from_sizes(sizes);
+            let result = try_to_fit_shapes_on_texture(&shape_entries, tex_size);
+            let positions = result.map(|shapes| {
+                shapes.shapes.iter().map(|shape| (shape.x(), shape.y())).collect_vec()
+            });
+            assert_eq!(
+                positions.as_ref().map(|vec| vec.as_slice()),
+                expected_position.as_ref().map(|arr| arr.as_slice())
+            );
         }
 
 
-        run_case(64, [(32, 32), (32, 32)], ([(-16.0, -16.0), (16.0, -16.0)]));
+        run_case(64, [(32, 32), (32, 32)], Some([(-16.0, -16.0), (16.0, -16.0)]));
+        run_case(63, [(32, 32), (32, 32)], None);
         run_case(
             64,
             [(32, 32), (32, 32), (32, 32), (32, 32)],
-            ([(-16.0, -16.0), (16.0, -16.0), (-16.0, 16.0), (16.0, 16.0)]),
+            Some([(-16.0, -16.0), (16.0, -16.0), (-16.0, 16.0), (16.0, 16.0)]),
         );
+        run_case(64, [(32, 32), (32, 32), (32, 32), (2, 33)], None);
         run_case(
             64,
             [(32, 32), (16, 16), (16, 16), (16, 16)],
-            ([(-16.0, -16.0), (8.0, -24.0), (24.0, -24.0), (8.0, -8.0)]),
+            Some([(-16.0, -16.0), (8.0, -24.0), (24.0, -24.0), (8.0, -8.0)]),
         );
         run_case(
             32,
             [(16, 8), (2, 32), (16, 2), (12, 2)],
-            ([(-8.0, -12.0), (1.0, 0.0), (-8.0, -7.0), (8.0, -15.0)]),
+            Some([(-8.0, -12.0), (1.0, 0.0), (-8.0, -7.0), (8.0, -15.0)]),
         );
+    }
+
+    #[test]
+    fn texture_size() {
+        fn run_case(shape_sizes: impl IntoIterator<Item = (i32, i32)>, expected_size: (i32, i32)) {
+            let shape_entries = shape_entries_from_sizes(shape_sizes);
+            let result = arrange_shapes_on_texture(shape_entries.iter());
+            assert_eq!(result.texture_size, IntoVector2::into_vector(expected_size));
+        }
+
+        run_case([(32, 32), (16, 16)], (48, 32));
+        run_case([(16, 2), (2, 20)], (22, 16));
+        run_case(iter::repeat((32, 32)).take(2), (64, 32));
+        run_case(iter::repeat((32, 32)).take(16), (512, 32));
+        run_case(iter::repeat((32, 32)).take(256), (512, 512));
+        // Shapes does not fit initial texture size: the texture is extended.
+        run_case(iter::repeat((32, 32)).take(257), (1024, 544));
+        // Several steps of extending the texture
+        run_case(iter::repeat((512, 512)).take(17), (4096, 1536));
     }
 }
