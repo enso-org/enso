d40afdfac9a4cfbba57372014f86d1842b21d365
Galin Bajlekov <galin.bajlekov@enso.org>
2023-01-30 18:33:05 +0100

    Clean up component entries lookup

--- app/gui/src/controller/searcher/component/builder.rs
+++ app/gui/src/controller/searcher/component/builder.rs
@@ -152,6 +152,24 @@ pub struct List {
     allowed_favorites:               HashSet<component::Id>,
 }
 
+struct ComponentWithDbEntry {
+    component: Component,
+    entry:     Rc<suggestion_database::Entry>,
+    id:        usize,
+}
+
+impl ComponentWithDbEntry {
+    fn new(db: &model::SuggestionDatabase, id: usize) -> Option<ComponentWithDbEntry> {
+        let entry = db.lookup(id).ok()?;
+        let component = Component::new_from_database_entry(id, entry.clone_ref());
+        if component.is_private() {
+            None
+        } else {
+            Some(ComponentWithDbEntry { component, entry, id })
+        }
+    }
+}
+
 impl List {
     /// Construct List builder without content.
     pub fn new() -> Self {
@@ -180,15 +198,8 @@ impl List {
     ) {
         use suggestion_database::entry::Kind;
         let local_scope_id = self.local_scope.component_id;
-        let id_and_looked_up_entry = |id| Some((id, db.lookup(id).ok()?));
-        let ids_and_entries = entry_ids.into_iter().filter_map(id_and_looked_up_entry);
-        let ids_entries_and_components = ids_and_entries.map(|(id, entry)| {
-            let component = Component::new_from_database_entry(id, entry.clone_ref());
-            (id, entry, component)
-        });
-        let ids_entries_and_components =
-            ids_entries_and_components.filter(|(_, _, component)| !component.is_private());
-        for (id, entry, component) in ids_entries_and_components {
+        let components = entry_ids.into_iter().filter_map(|id| ComponentWithDbEntry::new(db, id));
+        for ComponentWithDbEntry { component, entry, id } in components {
             self.allowed_favorites.insert(id);
             let mut component_inserted_somewhere = false;
             if let Some(parent_module) = entry.parent_module() {
