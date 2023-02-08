0ef1a008fca08c905572a24f0eb4b90f3be45dcd
Nikita Pekin <nikita@frecency.com>
2023-02-08 08:53:06 -0500

    fix eslint

--- app/ide-desktop/lib/authentication/src/index.ts
+++ app/ide-desktop/lib/authentication/src/index.ts
@@ -1,3 +1,9 @@
+// FIXME [NP]: Remove the `app/ide-desktop/.eslintrc.cjs` file
+// FIXME [NP]: Remove the `@typescript-eslint/eslint-plugin` dependency from parent `package.json`
+// FIXME [NP]: Remove the `@typescript-eslint/parser` dependency from parent `package.json`
+// FIXME [NP]: Remove the `eslint` dependency from parent `package.json`
+// FIXME [NP]: Remove the `eslint-plugin-jsdoc` dependency from parent `package.json`
+// FIXME [NP]: Rebuild the parent `package.json` file and commit the reverted `package-lock.json` file
 /** @file Authentication module used by Enso IDE & Cloud. */
 import * as app from 'ensogl_app'
 
