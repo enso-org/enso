6c0ec799af5c74c483031e381696cf9cb15b9b53
Nikita Pekin <nikita@frecency.com>
2023-02-08 13:18:34 -0500

    add auth to cloud

--- app/ide-desktop/lib/authentication/src/index.ts
+++ app/ide-desktop/lib/authentication/src/index.ts
@@ -8,13 +8,8 @@
 // FIXME [NP]: Remove the "strictNullChecks" rule from the parent `tsconfig.json` file
 // FIXME [NP]: Remove the `../tsconfig.json` file.
 /** @file Authentication module used by Enso IDE & Cloud. */
-import * as app from 'ensogl_app'
 
-// FIXME [NP]: Remove these after Wojciech's PR is merged.
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
-const logger = app.log.logger
-
-export function run () {
+function run (logger: any) {
     // FIXME [NP]: Remove these after Wojciech's PR is merged.
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     logger.log("FIXME [NP]: authentication/index.ts: run() is not implemented yet.")
--- app/ide-desktop/lib/cloud/src/index.tsx
+++ app/ide-desktop/lib/cloud/src/index.tsx
@@ -4,6 +4,11 @@ import * as ReactDOM from 'react-dom/client'
 
 import App from './components/app'
 import { BrowserRouter } from 'react-router-dom'
+// FIXME [NP]: remove this
+// @ts-ignore
+import * as authentication from 'enso-studio-authentication'
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+authentication.run(console)
 
 // Return interface for getElementById is element or null.
 // Since we are fetching root it is not possible to be null.
