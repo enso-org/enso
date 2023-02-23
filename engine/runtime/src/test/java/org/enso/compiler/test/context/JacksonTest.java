f413b626afc014520d71e9b02a09c8fc73ec08c0
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-02-23 20:07:21 +0100

    Can serialize Suggestion[]

--- engine/runtime/src/test/java/org/enso/compiler/test/context/JacksonTest.java
+++ engine/runtime/src/test/java/org/enso/compiler/test/context/JacksonTest.java
@@ -7,17 +7,18 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import scala.Option;
+import scala.collection.immutable.Seq;
 
 public class JacksonTest {
 
   @Test
   public void testSerdeOfSuggestion() throws Exception {
     Object shape = new Suggestion.Module(
-      "SampleModule", 
-      Option.apply("doc"), 
-      Option.apply("html"), 
-      Option.empty(), 
-      Option.empty()
+            "SampleModule",
+            Option.apply("doc"),
+            Option.apply("html"),
+            Option.empty(),
+            Option.empty()
     );
     final ObjectMapper m = new ObjectMapper().registerModule(new DefaultScalaModule());
     String result = m
@@ -28,5 +29,28 @@ public class JacksonTest {
     assertEquals("SampleModule", suggestion.name());
     assertEquals("doc", suggestion.documentation().get());
     assertEquals(Suggestion.Module.class, suggestion.getClass());
+  }
+
+  @Test
+  public void testArraySerdeOfSuggestion() throws Exception {
+    Object shape = new Suggestion[]{new Suggestion.Module(
+      "SampleModule",
+      Option.apply("doc"),
+      Option.apply("html"),
+      Option.empty(),
+      Option.empty()
+      )};
+    final ObjectMapper m = new ObjectMapper().registerModule(new DefaultScalaModule());
+    String result = m
+            .writerWithDefaultPrettyPrinter()
+            .writeValueAsString(shape);
+
+    var it = m.readerFor(Suggestion.class).readValues(result);
+    var suggestion = it.nextValue();
+    assertEquals(Suggestion.Module.class, suggestion.getClass());
+    if (suggestion instanceof Suggestion.Module module) {
+      assertEquals("SampleModule", module.name());
+      assertEquals("doc", module.documentation().get());
+    }
   }
 }
