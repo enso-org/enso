7d5f9e62fade900935ab1247afbb57380163d94f
Gregory Travis <greg.travis@enso.org>
2023-03-07 13:10:44 -0500

    convert regex exception

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/text/RegexCompileNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/text/RegexCompileNode.java
@@ -1,13 +1,17 @@
 package org.enso.interpreter.node.expression.builtin.text;
 
 import com.oracle.truffle.api.dsl.Cached;
-import org.enso.interpreter.dsl.BuiltinMethod;
-import org.enso.interpreter.runtime.data.text.Text;
-
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.exception.AbstractTruffleException;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.source.Source;
+
+import org.enso.interpreter.dsl.BuiltinMethod;
+import org.enso.interpreter.runtime.builtin.Builtins;
+import org.enso.interpreter.runtime.callable.atom.Atom;
+import org.enso.interpreter.runtime.data.text.Text;
 import org.enso.interpreter.runtime.EnsoContext;
+import org.enso.interpreter.runtime.error.PanicException;
 
 @BuiltinMethod(
     type = "Prim_Text_Helper",
@@ -45,11 +49,19 @@ public abstract class RegexCompileNode extends Node {
     var env = ctx.getEnvironment();
     var s = "Flavor=ECMAScript/" + pattern + "/" + options;
     var src =
-        Source.newBuilder("regex", s, "myRegex")
-            .mimeType("application/tregex")
-            .internal(true)
-            .build();
-    var regex = env.parseInternal(src).call();
-    return regex;
+            Source.newBuilder("regex", s, "myRegex")
+                    .mimeType("application/tregex")
+                    .internal(true)
+                    .build();
+
+    try {
+      var regex = env.parseInternal(src).call();
+      return regex;
+    } catch (AbstractTruffleException e) {
+      Builtins builtins = ctx.getBuiltins();
+      String msg = "Regex parse error: " + e.getMessage();
+      Atom err = builtins.error().makeSyntaxError(msg);
+      throw new PanicException(err, this);
+    }
   }
 }
