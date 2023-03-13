86d298ac27bc46d667adf7d70f82b19d1de52bc1
Michael Mauderer <michael.mauderer@enso.org>
2023-03-13 14:34:40 +0000

    Use the position of the drop event instead of the cursor position to place new nodes created from dropped files.

--- lib/rust/ensogl/component/drop-manager/src/lib.rs
+++ lib/rust/ensogl/component/drop-manager/src/lib.rs
@@ -21,6 +21,7 @@
 /// Commonly used utilities.
 pub mod prelude {
     pub use enso_prelude::*;
+    pub use enso_types::*;
 }
 
 use crate::prelude::*;
@@ -30,6 +31,7 @@ use enso_web as web;
 use enso_web::stream::BlobExt;
 use enso_web::stream::ReadableStreamDefaultReader;
 use enso_web::Closure;
+use ensogl_core::display::scene::Scene;
 
 #[cfg(target_arch = "wasm32")]
 use enso_web::JsCast;
@@ -112,6 +114,7 @@ impl File {
 
 type DropClosure = Closure<dyn Fn(web_sys::DragEvent)>;
 type DragOverClosure = Closure<dyn Fn(web_sys::DragEvent) -> bool>;
+type DropEventData = (Vector2, Vec<File>);
 
 /// The Manager of dropped files.
 ///
@@ -122,26 +125,27 @@ type DragOverClosure = Closure<dyn Fn(web_sys::DragEvent) -> bool>;
 pub struct Manager {
     #[allow(dead_code)]
     network:          frp::Network,
-    files_received:   frp::Source<Vec<File>>,
+    files_received:   frp::Source<DropEventData>,
     #[allow(dead_code)]
     drop_handle:      web::EventListenerHandle,
     #[allow(dead_code)]
     drag_over_handle: web::EventListenerHandle,
+    scene:            Scene,
 }
 
 impl Manager {
     /// Constructor, adding listener to the given target.
-    pub fn new(target: &enso_web::EventTarget) -> Self {
+    pub fn new(target: &enso_web::EventTarget, scene: &Scene) -> Self {
         debug!("Creating.");
         let network = frp::Network::new("DropFileManager");
         frp::extend! { network
             files_received <- source();
         }
 
-        let drop: DropClosure = Closure::new(f!([files_received](event:web_sys::DragEvent) {
+        let drop: DropClosure = Closure::new(f!([files_received,scene](event:web_sys::DragEvent) {
             debug!("Dropped files.");
             event.prevent_default();
-            Self::handle_drop_event(event,&files_received)
+            Self::handle_drop_event(event,&files_received, &scene);
         }));
         // To mark element as a valid drop target, the `dragover` event handler should return
         // `false`. See
@@ -152,15 +156,30 @@ impl Manager {
         });
         let drop_handle = web::add_event_listener(target, "drop", drop);
         let drag_over_handle = web::add_event_listener(target, "dragover", drag_over);
-        Self { network, files_received, drop_handle, drag_over_handle }
+        let scene = scene.clone_ref();
+        Self { network, files_received, drop_handle, drag_over_handle, scene }
     }
 
     /// The frp endpoint emitting signal when a file is dropped.
-    pub fn files_received(&self) -> &frp::Source<Vec<File>> {
+    pub fn files_received(&self) -> &frp::Source<DropEventData> {
         &self.files_received
     }
 
-    fn handle_drop_event(event: web_sys::DragEvent, files_received: &frp::Source<Vec<File>>) {
+    /// Retrieve the position of the drop event in the scene coordinates.
+    fn event_position(scene: &Scene, event: &web_sys::DragEvent) -> Vector2 {
+        let position = Vector2::new(event.client_x() as f32, event.client_y() as f32);
+        let pixel_ratio = scene.frp.shape.value().pixel_ratio;
+        let center = scene.frp.shape.value().center();
+        let position = position.map(|v| (v * pixel_ratio)) - center;
+        scene.screen_to_scene_coordinates(Vector3::new(position.x, -position.y, 0.0)).xy()
+    }
+
+    fn handle_drop_event(
+        event: web_sys::DragEvent,
+        files_received: &frp::Source<DropEventData>,
+        scene: &Scene,
+    ) {
+        let position = Self::event_position(scene, &event);
         let opt_files = event.data_transfer().and_then(|t| t.files());
         if let Some(js_files) = opt_files {
             let js_files_iter = (0..js_files.length()).filter_map(|i| js_files.get(i));
@@ -171,7 +190,8 @@ impl Manager {
                     None
                 }
             });
-            files_received.emit(files_iter.collect_vec());
+            let data: DropEventData = (position, files_iter.collect_vec());
+            files_received.emit(data);
         }
     }
 }
