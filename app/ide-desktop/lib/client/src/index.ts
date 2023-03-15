7798c768de4ebf57cee1d0cb7409f3c98d9f7d0d
James Dunkerley <jdunkerley@users.noreply.github.com>
2023-03-15 18:06:16 +0000

    Enable clipboard access.

--- app/ide-desktop/lib/client/src/index.ts
+++ app/ide-desktop/lib/client/src/index.ts
@@ -202,6 +202,16 @@ class App {
                     window.webContents.openDevTools()
                 }
 
+                const allowedPermissions = ['clipboard-read', 'clipboard-sanitized-write']
+                window.webContents.session.setPermissionRequestHandler((_webContents, permission, callback) => {
+                    if (allowedPermissions.includes(permission)) {
+                        callback(true)
+                    } else {
+                        console.error(`Denied permission check '${permission}'.`)
+                        callback(false)
+                    }
+                })
+
                 window.on('close', evt => {
                     if (!this.isQuitting && !this.args.groups.window.options.closeToQuit.value) {
                         evt.preventDefault()
