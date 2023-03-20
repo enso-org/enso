62b4a5ab9e48c4ea0da1eb992eb57574e6da162a
Nikita Pekin <nikita@frecency.com>
2023-03-20 19:05:53 +0200

    fixes

--- app/ide-desktop/lib/dashboard/src/authentication/src/authentication/components/forgotPassword.tsx
+++ app/ide-desktop/lib/dashboard/src/authentication/src/authentication/components/forgotPassword.tsx
@@ -4,6 +4,7 @@ import * as router from "react-router-dom";
 
 import * as app from "../../components/app";
 import * as auth from "../providers/auth";
+import * as common from "./common";
 import * as hooks from "../../hooks";
 import * as icons from "../../components/svg";
 import * as utils from "../../utils";
@@ -19,7 +20,10 @@ function ForgotPassword() {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
-      <div className="flex flex-col bg-white shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full max-w-md">
+      <div className={
+        "flex flex-col bg-white shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full " +
+        "max-w-md"
+      }>
         <div className="font-medium self-center text-xl sm:text-2xl uppercase text-gray-800">
           Forgot Your Password?
         </div>
@@ -37,16 +41,13 @@ function ForgotPassword() {
                 E-Mail Address:
               </label>
               <div className="relative">
-                <div className="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400">
-                  <icons.Svg data={icons.PATHS.at} />
-                </div>
+                <common.SvgIcon data={icons.PATHS.at} />
 
-                <input
+                <common.Input
                   {...bindEmail}
                   id="email"
                   type="email"
                   name="email"
-                  className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
                   placeholder="E-Mail Address"
                 />
               </div>
@@ -54,7 +55,11 @@ function ForgotPassword() {
             <div className="flex w-full">
               <button
                 type="submit"
-                className="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-blue-600 hover:bg-blue-700 rounded py-2 w-full transition duration-150 ease-in"
+                className={
+                  "flex items-center justify-center focus:outline-none text-white text-sm " +
+                  "sm:text-base bg-blue-600 hover:bg-blue-700 rounded py-2 w-full transition " +
+                  "duration-150 ease-in"
+                }
               >
                 <span className="mr-2 uppercase">Send link</span>
                 <span>
@@ -67,7 +72,10 @@ function ForgotPassword() {
         <div className="flex justify-center items-center mt-6">
           <router.Link
             to={app.LOGIN_PATH}
-            className="inline-flex items-center font-bold text-blue-500 hover:text-blue-700 text-xs text-center"
+            className={
+              "inline-flex items-center font-bold text-blue-500 hover:text-blue-700 text-xs " +
+              "text-center"
+            }
           >
             <span>
               <icons.Svg data={icons.PATHS.goBack} />
