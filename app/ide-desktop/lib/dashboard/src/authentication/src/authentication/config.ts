8a91085259912fff17eeee61774c44604d2099e2
Nikita Pekin <nikita@frecency.com>
2023-03-20 17:10:11 +0200

    fix

--- app/ide-desktop/lib/dashboard/src/authentication/src/authentication/config.ts
+++ app/ide-desktop/lib/dashboard/src/authentication/src/authentication/config.ts
@@ -86,7 +86,7 @@ export interface AmplifyConfig {
     region: AwsRegion
     userPoolId: UserPoolId
     userPoolWebClientId: UserPoolWebClientId
-    urlOpener: OAuthUrlOpener | null,
+    urlOpener: OAuthUrlOpener | null
     domain: OAuthDomain
     scope: OAuthScope[]
     redirectSignIn: OAuthRedirect
