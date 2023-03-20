b2281a84585b7ab1f454af5009c52f1c30d8197c
Gregory Travis <greg.travis@enso.org>
2023-03-20 17:10:30 -0400

    java fmt

--- std-bits/base/src/main/java/org/enso/base/Replacer_Cache.java
+++ std-bits/base/src/main/java/org/enso/base/Replacer_Cache.java
@@ -4,36 +4,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Replacer_Cache {
-    // Map from replacement string to replacement vector.
-    private static Map<String, Object> cache = new HashMap<String, Object>();
+  // Map from replacement string to replacement vector.
+  private static Map<String, Object> cache = new HashMap<String, Object>();
 
-    private static final int lruSize = 5;
+  private static final int lruSize = 5;
 
-    // Circular buffer containing the most recent cache keys.
-    private static final String lru[] = new String[lruSize];
+  // Circular buffer containing the most recent cache keys.
+  private static final String lru[] = new String[lruSize];
 
-    // Index into the circular buffer.
-    private static int nextSlot = 0;
+  // Index into the circular buffer.
+  private static int nextSlot = 0;
 
-    public static void put(String key, Object value) {
-        if (lru[nextSlot] != null) {
-            cache.remove(lru[nextSlot]);
-        }
-        lru[nextSlot] = key;
-        nextSlot = (nextSlot + 1) % lruSize;
-
-        cache.put(key, value);
+  public static void put(String key, Object value) {
+    if (lru[nextSlot] != null) {
+      cache.remove(lru[nextSlot]);
     }
+    lru[nextSlot] = key;
+    nextSlot = (nextSlot + 1) % lruSize;
 
-    public static Object get(String key) {
-        return cache.get(key);
-    }
+    cache.put(key, value);
+  }
 
-    public static int getLruArraySize() {
-        return lruSize;
-    }
+  public static Object get(String key) {
+    return cache.get(key);
+  }
 
-    public static int getLruCacheSize() {
-        return cache.size();
-    }
+  public static int getLruArraySize() {
+    return lruSize;
+  }
+
+  public static int getLruCacheSize() {
+    return cache.size();
+  }
 }
