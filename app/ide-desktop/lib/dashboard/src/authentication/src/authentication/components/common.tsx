d84842ea14297394fc1200b3d9bcd69c5a3fa12d
somebody1234 <ehern.lee@gmail.com>
2023-03-20 19:13:45 +1000

    Run prettier

--- app/ide-desktop/lib/dashboard/src/authentication/src/authentication/components/common.tsx
+++ app/ide-desktop/lib/dashboard/src/authentication/src/authentication/components/common.tsx
@@ -3,32 +3,32 @@
  * For example, this file contains the {@link SvgIcon} component, which is used by the
  * `Registration` and `Login` components. */
 
-import * as fontawesome from "@fortawesome/react-fontawesome";
-import * as fontawesomeIcons from "@fortawesome/free-brands-svg-icons";
+import * as fontawesome from '@fortawesome/react-fontawesome'
+import * as fontawesomeIcons from '@fortawesome/free-brands-svg-icons'
 
-import * as icons from "../../components/svg";
+import * as icons from '../../components/svg'
 
 // ===============
 // === SvgIcon ===
 // ===============
 
 interface SvgIconProps {
-  data: string;
+    data: string
 }
 
 export function SvgIcon(props: SvgIconProps) {
-  return (
-    <div
-      className={
-        "inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 " +
-        "text-gray-400"
-      }
-    >
-      <span>
-        <icons.Svg {...props} />
-      </span>
-    </div>
-  );
+    return (
+        <div
+            className={
+                'inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 ' +
+                'text-gray-400'
+            }
+        >
+            <span>
+                <icons.Svg {...props} />
+            </span>
+        </div>
+    )
 }
 
 // =======================
@@ -36,18 +36,18 @@ export function SvgIcon(props: SvgIconProps) {
 // =======================
 
 interface FontAwesomeIconProps {
-  icon: fontawesomeIcons.IconDefinition;
+    icon: fontawesomeIcons.IconDefinition
 }
 
 export function FontAwesomeIcon(props: FontAwesomeIconProps) {
-  return (
-    <span
-      className={
-        "absolute left-0 top-0 flex items-center justify-center h-full w-10 " +
-        "text-blue-500"
-      }
-    >
-      <fontawesome.FontAwesomeIcon icon={props.icon} />
-    </span>
-  );
+    return (
+        <span
+            className={
+                'absolute left-0 top-0 flex items-center justify-center h-full w-10 ' +
+                'text-blue-500'
+            }
+        >
+            <fontawesome.FontAwesomeIcon icon={props.icon} />
+        </span>
+    )
 }
