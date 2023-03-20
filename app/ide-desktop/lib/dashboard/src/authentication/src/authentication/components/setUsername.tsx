7705b6166f314999aee92d1773113ad69eeb0b56
Nikita Pekin <nikita@frecency.com>
2023-03-20 19:13:53 +0200

    fix formatting

--- app/ide-desktop/lib/dashboard/src/authentication/src/authentication/components/setUsername.tsx
+++ app/ide-desktop/lib/dashboard/src/authentication/src/authentication/components/setUsername.tsx
@@ -19,10 +19,12 @@ function SetUsername() {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
-      <div className={
-        "flex flex-col bg-white shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full " +
-        "max-w-md"
-      }>
+      <div
+        className={
+          "flex flex-col bg-white shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full " +
+          "max-w-md"
+        }
+      >
         <div className="font-medium self-center text-xl sm:text-2xl uppercase text-gray-800">
           Set your username
         </div>
