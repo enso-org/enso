edefbff5c76f8103e193eba709fa38cef74ae6e6
Gregory Travis <greg.travis@enso.org>
2023-03-21 14:25:07 -0400

    Value

--- std-bits/base/src/main/java/org/enso/base/Replacer_Cache.java
+++ std-bits/base/src/main/java/org/enso/base/Replacer_Cache.java
@@ -1,6 +1,7 @@
 package org.enso.base;
 
 import org.graalvm.collections.Pair;
+import org.graalvm.polyglot.Value;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,7 +11,7 @@ public class Replacer_Cache {
   private static final int lruSize = 5;
 
   // Circular buffer containing the most recent cache keys.
-  private static final ArrayList<Pair<String, Object>> lru = new ArrayList<>(lruSize);
+  private static final ArrayList<Pair<String, Value>> lru = new ArrayList<>(lruSize);
 
   static {
     for (int i=0; i < lruSize; ++i) {
@@ -21,14 +22,14 @@ public class Replacer_Cache {
   // Index into the circular buffer.
   private static int nextSlot = 0;
 
-  public static void put(String key, Object value) {
+  public static void put(String key, Value value) {
     lru.set(nextSlot, Pair.create(key, value));
     nextSlot = (nextSlot + 1) % lruSize;
   }
 
-  public static Object get(String key) {
+  public static Value get(String key) {
     for (int i=0; i < lruSize; ++i) {
-      Pair<String, Object> pair = lru.get(i);
+      Pair<String, Value> pair = lru.get(i);
       if (pair != null && pair.getLeft().equals(key)) {
         return lru.get(i).getRight();
       }
