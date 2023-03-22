e58c119a9106688df092cb04df5d4fd85e71ee3d
Paweł Buchowski <pawel.buchowski@enso.org>
2023-03-22 14:57:50 +0100

    Revert "Fix #5975 (#5976)"
    This reverts commit d38951d4e986236211dde17ee0e1359c031fc7c6.

--- app/ide-desktop/lib/dashboard/src/authentication/tailwind.config.ts
+++ app/ide-desktop/lib/dashboard/src/authentication/tailwind.config.ts
@@ -1,8 +1,7 @@
 /** @file Configuration for Tailwind. */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    // FIXME[sb]: Tailwind building should be in `dashboard/`.
-    content: ['../dashboard/src/authentication/src/**/*.tsx'],
+    content: ['./src/**/*.tsx'],
     theme: {
         extend: {},
     },
