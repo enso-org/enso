fc54df3aceed45153ff8d559a310c244f57b8c93
somebody1234 <ehern.lee@gmail.com>
2023-03-22 14:23:17 +1000

    Add FIXME

--- app/ide-desktop/lib/content/tailwind.config.ts
+++ app/ide-desktop/lib/content/tailwind.config.ts
@@ -1,6 +1,7 @@
 /** @file Configuration for Tailwind. */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    // FIXME[sb]: Tailwind building should be in `dashboard/`.
     content: ['../dashboard/src/authentication/src/**/*.tsx'],
     theme: {
         extend: {},
