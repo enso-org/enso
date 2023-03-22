b1ee3d989e4bc165d6d2298304276e17cb9cd523
somebody1234 <ehern.lee@gmail.com>
2023-03-22 01:59:20 +0000

    Rename `Brand` to `NewtypeVariant`

--- app/ide-desktop/lib/dashboard/src/authentication/src/newtype.ts
+++ app/ide-desktop/lib/dashboard/src/authentication/src/newtype.ts
@@ -1,6 +1,6 @@
 /** @file TypeScript's closest equivalent of `newtype`s. */
 
-interface Brand<TypeName extends string> {
+interface NewtypeVariant<TypeName extends string> {
     // eslint-disable-next-line @typescript-eslint/naming-convention
     _$type: TypeName
 }
@@ -17,8 +17,8 @@ interface Brand<TypeName extends string> {
  * It is similar to a `newtype` in other languages.
  * Note however because TypeScript is structurally typed,
  * a branded type is assignable to its base type:
- * `a: string = b as (string & Brand<'Name'>)` successfully typechecks. */
-export type Newtype<T, TypeName extends string> = Brand<TypeName> & T
+ * `a: string = asNewtype<Newtype<string, 'Name'>>(b)` successfully typechecks. */
+export type Newtype<T, TypeName extends string> = NewtypeVariant<TypeName> & T
 
 interface NotNewtype {
     // eslint-disable-next-line @typescript-eslint/naming-convention
