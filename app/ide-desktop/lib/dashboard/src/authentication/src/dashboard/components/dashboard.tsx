283802ec6fd3e239ac7798141f3591723c3ef453
Nctdtman <137032445@qq.com>
2023-03-23 21:22:05 +0800

    Feat: update templates styles

--- app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/dashboard.tsx
+++ app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/dashboard.tsx
@@ -1,17 +1,22 @@
 /** @file Main dashboard component, responsible for listing user's projects as well as other
  * interactive components. */
 
+import Templates from './templates'
+
 // =================
 // === Dashboard ===
 // =================
 
 function Dashboard() {
+    const onTemplateClick = (name: string) => {
+        console.log('name: %o', name)
+    }
     return (
         <>
             {/* These are placeholders. When implementing a feature,
-              * please replace the appropriate placeholder with the actual element.*/}
+             * please replace the appropriate placeholder with the actual element.*/}
             <div id="header" />
-            <div id="templates" />
+            <Templates onTemplateClick={onTemplateClick} />
             <div id="drive-header" />
             <div id="directory-listing" />
         </>
