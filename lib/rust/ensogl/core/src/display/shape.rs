f8c78ad22b9d698c60ff1704aae92419b724a429
Wojciech Danilo <wojciech.danilo@gmail.com>
2023-03-25 03:22:28 +0100

    Refactoring super-rectangle to EnsoGL

--- lib/rust/ensogl/core/src/display/shape.rs
+++ lib/rust/ensogl/core/src/display/shape.rs
@@ -9,9 +9,9 @@ pub mod compound;
 pub mod constants;
 pub mod primitive;
 
+pub use compound::rectangle::Rectangle;
 pub use constants::*;
 pub use primitive::*;
-
 
 
 // We have two Shape and two ShapeOps traits. This one takes precedence.
