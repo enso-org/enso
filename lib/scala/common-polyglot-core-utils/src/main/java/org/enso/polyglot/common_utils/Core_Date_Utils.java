bf2545fa049dee049cc850b36daf43ccdd161db9
James Dunkerley <jdunkerley@users.noreply.github.com>
2023-03-27 12:02:10 +0100

    Use new common parse method throwing less exceptions. (#6075)
    Avoiding exceptions by not using parseBest.
    
    Time now in CLI is 1.15s for 500k rows vs 1.65s in GUI.
    
    CLI:
    ![image](https://user-images.githubusercontent.com/4699705/227711266-bc005b0d-5011-450f-964b-65dd2e437c2e.png)
    
    GUI:
    ![image](https://user-images.githubusercontent.com/4699705/227711259-f7ddda29-86c7-4eef-a002-4bf0bda6063f.png)
    
    Added it as a function in the shared library so used by both engine and polyglot.

--- lib/scala/common-polyglot-core-utils/src/main/java/org/enso/polyglot/common_utils/Core_Date_Utils.java
+++ lib/scala/common-polyglot-core-utils/src/main/java/org/enso/polyglot/common_utils/Core_Date_Utils.java
@@ -1,7 +1,13 @@
 package org.enso.polyglot.common_utils;
 
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalQueries;
+
+import static java.time.temporal.ChronoField.INSTANT_SECONDS;
+import static java.time.temporal.ChronoField.NANO_OF_SECOND;
 
 public class Core_Date_Utils {
   /**
@@ -25,5 +31,41 @@ public class Core_Date_Utils {
         .optionalStart().parseLenient().appendOffsetId().optionalEnd()
         .optionalStart().appendLiteral('[').parseCaseSensitive().appendZoneRegionId().appendLiteral(']')
         .toFormatter();
+  }
+
+  /**
+   * Parse a date time string into a ZonedDateTime.
+   *
+   * @param dateString the date time string
+   * @param formatter the formatter to use
+   * @return the parsed date time
+   */
+  public static ZonedDateTime parseZonedDateTime(String dateString, DateTimeFormatter formatter) {
+    var resolved = formatter.parse(dateString);
+
+    try {
+      // Resolve Zone
+      var zone = resolved.query(TemporalQueries.zoneId());
+      zone = zone != null ? zone :
+              (resolved.isSupported(ChronoField.OFFSET_SECONDS)
+                      ? ZoneOffset.ofTotalSeconds(resolved.get(ChronoField.OFFSET_SECONDS))
+                      : ZoneId.systemDefault());
+
+      // Instant Based
+      if (resolved.isSupported(INSTANT_SECONDS)) {
+        long epochSecond = resolved.getLong(INSTANT_SECONDS);
+        int nanoOfSecond = resolved.get(NANO_OF_SECOND);
+        return ZonedDateTime.ofInstant(java.time.Instant.ofEpochSecond(epochSecond, nanoOfSecond), zone);
+      }
+
+      // Local Based
+      var localDate = LocalDate.from(resolved);
+      var localTime = LocalTime.from(resolved);
+      return ZonedDateTime.of(localDate, localTime, zone);
+    } catch (DateTimeException e) {
+      throw new DateTimeException("Unable to parse Text '" + dateString + "' to Date_Time.", e);
+    } catch (ArithmeticException e) {
+      throw new DateTimeException("Unable to parse Text '" + dateString + "' to Date_Time due to arithmetic error.", e);
+    }
   }
 }
--- std-bits/table/src/main/java/org/enso/table/parsing/DateTimeParser.java
+++ std-bits/table/src/main/java/org/enso/table/parsing/DateTimeParser.java
@@ -1,29 +1,14 @@
 package org.enso.table.parsing;
 
+import org.enso.polyglot.common_utils.Core_Date_Utils;
 import org.enso.table.data.column.builder.object.Builder;
 import org.enso.table.data.column.builder.object.DateTimeBuilder;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.Locale;
 
 public class DateTimeParser extends BaseTimeParser {
   public DateTimeParser(String[] formats, Locale locale) {
-    super(formats, locale, DateTimeParser::parse);
-  }
-
-  private static ZonedDateTime parse(String text, DateTimeFormatter formatter)
-      throws DateTimeParseException {
-    var datetime = formatter.parseBest(text, ZonedDateTime::from, LocalDateTime::from);
-    if (datetime instanceof ZonedDateTime zdt) {
-      return zdt;
-    } else if (datetime instanceof LocalDateTime ldt) {
-      return ldt.atZone(ZoneId.systemDefault());
-    }
-    throw new DateTimeParseException("Invalid date time", text, 0);
+    super(formats, locale, Core_Date_Utils::parseZonedDateTime);
   }
 
   @Override
