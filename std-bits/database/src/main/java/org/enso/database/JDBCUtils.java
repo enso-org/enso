7bbaa4ce4db1a3b9a2d206f678c3b3308d527747
Radosław Waśko <radoslaw.wasko@enso.org>
2023-03-30 14:17:26 +0200

    Fix Date_Time timezones, can only check the same instant is represented.

--- std-bits/database/src/main/java/org/enso/database/JDBCUtils.java
+++ std-bits/database/src/main/java/org/enso/database/JDBCUtils.java
@@ -1,14 +1,10 @@
 package org.enso.database;
 
-import org.postgresql.PGStatement;
-import org.postgresql.core.Provider;
-import org.postgresql.jdbc.PgStatement;
-import org.postgresql.jdbc.TimestampUtils;
-
-import java.sql.*;
-import java.time.LocalTime;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.OffsetDateTime;
 import java.time.ZonedDateTime;
-import java.util.TimeZone;
 
 public class JDBCUtils {
   /**
@@ -21,17 +17,8 @@ public class JDBCUtils {
     return Timestamp.from(zonedDateTime.toInstant());
   }
 
-  // TODO remove?
-  private static TimestampUtils PG_TIMESTAMP_UTILS = null;
-  private static TimestampUtils getPostgresTimestampUtils() {
-    if (PG_TIMESTAMP_UTILS == null) {
-      // We don't know what is the true setting for the given connection.
-      // But this setting is only relevant for binary helpers, so we just don't use those.
-      boolean usesDouble = true;
-      // Likewise, we just assume the timezone will not be used.
-      Provider<TimeZone> timeZoneProvider = () -> null;
-      PG_TIMESTAMP_UTILS = new TimestampUtils(usesDouble, timeZoneProvider);
-    }
-    return PG_TIMESTAMP_UTILS;
+  public static ZonedDateTime getZonedDateTime(ResultSet rs, int columnIndex) throws SQLException {
+    OffsetDateTime offsetDateTime = rs.getObject(columnIndex, OffsetDateTime.class);
+    return offsetDateTime.toZonedDateTime();
   }
 }
