95e8918c2bf7fbecb300097fe40f28bfc2b60eae
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-03-30 16:04:08 +0200

    Delay creation of project root EnsoFile until it is really needed

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/constant/EnsoProjectNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/constant/EnsoProjectNode.java
@@ -14,30 +14,32 @@ import java.util.Optional;
 
 @NodeInfo(description = "Returns a language-level representation of the given Enso project.")
 public class EnsoProjectNode extends RootNode {
-  private final Object result;
+  private final Optional<Package<TruffleFile>> pkgOpt;
 
   public EnsoProjectNode(
-      TruffleLanguage<?> language, EnsoContext context, Optional<Package<TruffleFile>> pkgOpt) {
+    TruffleLanguage<?> language, EnsoContext context, Optional<Package<TruffleFile>> pkgOpt
+  ) {
     super(language);
-    if (pkgOpt.isPresent()) {
-      Package<TruffleFile> pkg = pkgOpt.get();
-      EnsoFile rootPath = new EnsoFile(pkg.root().normalize());
-      Object cfg = context.getEnvironment().asGuestValue(pkg.config());
-      result =
-          context
-              .getBuiltins()
-              .getProjectDescription()
-              .getUniqueConstructor()
-              .newInstance(rootPath, cfg);
-    } else {
-      result =
-          DataflowError.withoutTrace(
-              context.getBuiltins().error().makeModuleNotInPackageError(), this);
-    }
+    this.pkgOpt = pkgOpt;
   }
 
   @Override
   public Object execute(VirtualFrame frame) {
-    return result;
+    var context = EnsoContext.get(this);
+    if (pkgOpt.isPresent()) {
+      Package<TruffleFile> pkg = pkgOpt.get();
+      EnsoFile rootPath = new EnsoFile(pkg.root().normalize());
+      Object cfg = context.getEnvironment().asGuestValue(pkg.config());
+      var result = context
+        .getBuiltins()
+        .getProjectDescription()
+        .getUniqueConstructor()
+        .newInstance(rootPath, cfg);
+      return result;
+    } else {
+      var result = DataflowError.withoutTrace(
+        context.getBuiltins().error().makeModuleNotInPackageError(), this);
+      return result;
+    }
   }
 }
