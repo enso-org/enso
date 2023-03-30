c70702fd8d9b76033f1ae4ff3f53276d57f33862
Radosław Waśko <radoslaw.wasko@enso.org>
2023-03-30 12:34:23 +0200

    Ensure Time_Of_Day is fetched with microsecond precision.

--- std-bits/database/src/main/java/org/enso/database/JDBCUtils.java
+++ std-bits/database/src/main/java/org/enso/database/JDBCUtils.java
@@ -1,7 +1,14 @@
 package org.enso.database;
 
-import java.sql.Timestamp;
+import org.postgresql.PGStatement;
+import org.postgresql.core.Provider;
+import org.postgresql.jdbc.PgStatement;
+import org.postgresql.jdbc.TimestampUtils;
+
+import java.sql.*;
+import java.time.LocalTime;
 import java.time.ZonedDateTime;
+import java.util.TimeZone;
 
 public class JDBCUtils {
   /**
@@ -12,5 +19,19 @@ public class JDBCUtils {
    */
   public static Timestamp getTimestamp(ZonedDateTime zonedDateTime) {
     return Timestamp.from(zonedDateTime.toInstant());
+  }
+
+  // TODO remove?
+  private static TimestampUtils PG_TIMESTAMP_UTILS = null;
+  private static TimestampUtils getPostgresTimestampUtils() {
+    if (PG_TIMESTAMP_UTILS == null) {
+      // We don't know what is the true setting for the given connection.
+      // But this setting is only relevant for binary helpers, so we just don't use those.
+      boolean usesDouble = true;
+      // Likewise, we just assume the timezone will not be used.
+      Provider<TimeZone> timeZoneProvider = () -> null;
+      PG_TIMESTAMP_UTILS = new TimestampUtils(usesDouble, timeZoneProvider);
+    }
+    return PG_TIMESTAMP_UTILS;
   }
 }
