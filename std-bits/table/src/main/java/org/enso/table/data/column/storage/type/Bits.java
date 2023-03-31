c1f69b43afe5ffd60858314d97cd1415ce908d22
Radosław Waśko <radoslaw.wasko@enso.org>
2023-03-31 12:30:15 +0200

    CR: 1

--- std-bits/table/src/main/java/org/enso/table/data/column/storage/type/Bits.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/storage/type/Bits.java
@@ -1,21 +1,22 @@
 package org.enso.table.data.column.storage.type;
 
-/** Represents sizes for some of our storages.
+/**
+ * Represents sizes for some of our storages.
  * <p>
  * This corresponds to the Enso type {@code Bits}.
  */
 public enum Bits {
-  BITS_8,
-  BITS_16,
-  BITS_32,
-  BITS_64;
+  BITS_8(8),
+  BITS_16(16),
+  BITS_32(32),
+  BITS_64(64);
+  private final int size;
+
+  Bits(int size) {
+    this.size = size;
+  }
 
   public int toInteger() {
-    return switch (this) {
-      case BITS_8 -> 8;
-      case BITS_16 -> 16;
-      case BITS_32 -> 32;
-      case BITS_64 -> 64;
-    };
+    return this.size;
   }
 }
