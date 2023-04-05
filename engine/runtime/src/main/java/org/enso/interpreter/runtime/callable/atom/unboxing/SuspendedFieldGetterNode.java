cad4e8e3e71b1379e3077a220bbe1d112ca718f7
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-04-05 09:08:03 +0200

    Keep and rethrow suspended exceptions

--- engine/runtime/src/main/java/org/enso/interpreter/runtime/callable/atom/unboxing/SuspendedFieldGetterNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/runtime/callable/atom/unboxing/SuspendedFieldGetterNode.java
@@ -1,5 +1,7 @@
 package org.enso.interpreter.runtime.callable.atom.unboxing;
 
+import com.oracle.truffle.api.exception.AbstractTruffleException;
+import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.nodes.Node;
 import org.enso.interpreter.node.callable.InvokeCallableNode;
 import org.enso.interpreter.node.callable.dispatch.InvokeFunctionNode;
@@ -9,7 +11,8 @@ import org.enso.interpreter.runtime.callable.atom.Atom;
 import org.enso.interpreter.runtime.callable.function.Function;
 import org.enso.interpreter.runtime.state.State;
 
-/** Getter node that reads a field value. If the value is a thunk the node
+/**
+ * Getter node that reads a field value. If the value is a thunk the node
  * evaluates it and replaces the original lazy value with the new value.
  *
  */
@@ -20,7 +23,7 @@ final class SuspendedFieldGetterNode extends UnboxingAtom.FieldGetterNode {
   private UnboxingAtom.FieldGetterNode get;
   @Node.Child
   private InvokeFunctionNode invoke = InvokeFunctionNode.build(
-    new CallArgumentInfo[0], InvokeCallableNode.DefaultsExecutionMode.EXECUTE, InvokeCallableNode.ArgumentsExecutionMode.EXECUTE
+          new CallArgumentInfo[0], InvokeCallableNode.DefaultsExecutionMode.EXECUTE, InvokeCallableNode.ArgumentsExecutionMode.EXECUTE
   );
 
   private SuspendedFieldGetterNode(UnboxingAtom.FieldGetterNode get, UnboxingAtom.FieldSetterNode set) {
@@ -36,13 +39,28 @@ final class SuspendedFieldGetterNode extends UnboxingAtom.FieldGetterNode {
   public Object execute(Atom atom) {
     java.lang.Object value = get.execute(atom);
     if (value instanceof Function fn && fn.isThunk()) {
-      org.enso.interpreter.runtime.EnsoContext ctx = EnsoContext.get(this);
-      java.lang.Object newValue = invoke.execute(fn, null, State.create(ctx), new Object[0]);
-      set.execute(atom, newValue);
-      return newValue;
+      try {
+        org.enso.interpreter.runtime.EnsoContext ctx = EnsoContext.get(this);
+        java.lang.Object newValue = invoke.execute(fn, null, State.create(ctx), new Object[0]);
+        set.execute(atom, newValue);
+        return newValue;
+      } catch (AbstractTruffleException ex) {
+        var rethrow = new SuspendedException(ex);
+        set.execute(atom, rethrow);
+        throw ex;
+      }
+    } else if (value instanceof SuspendedException suspended) {
+      throw suspended.ex;
     } else {
       return value;
     }
   }
 
+  private static final class SuspendedException implements TruffleObject {
+    final AbstractTruffleException ex;
+
+    SuspendedException(AbstractTruffleException ex) {
+      this.ex = ex;
+    }
+  }
 }
