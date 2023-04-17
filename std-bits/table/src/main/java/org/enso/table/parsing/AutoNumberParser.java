f6364f84a44b1d81542a9a570f7a560c9021eb42
James Dunkerley <jdunkerley@users.noreply.github.com>
2023-04-17 15:37:55 +0100

    Don't strip currency by default.

--- std-bits/table/src/main/java/org/enso/table/parsing/AutoNumberParser.java
+++ std-bits/table/src/main/java/org/enso/table/parsing/AutoNumberParser.java
@@ -12,17 +12,20 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-public class AutoNumberParser extends DatatypeParser {
+public class AutoNumberParser extends IncrementalDatatypeParser {
     private final static String SIGN = "(?<sign>[-+])?\\s*";
-    private final static String BRACKETS = "(?<sign>\\((?=.*\\)\\s*$)\\s*";
+    private final static String BRACKETS = "(?<sign>\\((?=.*\\)\\s*$))\\s*";
     private final static String BRACKET_CLOSE = "\\)\\s*";
     private final static String CCY = "(?<ccy>\\D+)?\\s*";
     private final static String[] SEPARATORS = new String[] {",.", ".,", " ,", "',"};
 
     private final static Map<String, Pattern> PATTERNS = new HashMap<>();
 
-    private static Pattern getPattern(boolean allowDecimal, int index) {
-        int separatorsIndex = index / 6;
+    private static Pattern getPattern(boolean allowDecimal, boolean allowCurrency, int index) {
+        int allowedSet = allowCurrency ? 6 : 2;
+        int separatorsIndex = index / allowedSet;
+        int patternIndex = index % allowedSet;
+
         if (separatorsIndex >= SEPARATORS.length) {
             return null;
         }
@@ -32,13 +35,12 @@ public class AutoNumberParser extends DatatypeParser {
         var INTEGER = "(?<integer>(\\d+)|(\\d{1,3}([" + separators.charAt(0) + "]\\d{3})))";
         var NUMBER = INTEGER + (allowDecimal ? "(?<decimal>[" + separators.charAt(1) + "]\\d+)?" : "") + "\\s*";
 
-        int patternIndex = index % 6;
         var pattern = switch (patternIndex) {
             case 0 -> SIGN + NUMBER;
-            case 1 -> SIGN + CCY + NUMBER;
-            case 2 -> CCY + SIGN + NUMBER;
-            case 3 -> SIGN + NUMBER + CCY;
-            case 4 -> BRACKETS + NUMBER + BRACKET_CLOSE;
+            case 1 -> BRACKETS + NUMBER + BRACKET_CLOSE;
+            case 2 -> SIGN + CCY + NUMBER;
+            case 3 -> CCY + SIGN + NUMBER;
+            case 4 -> SIGN + NUMBER + CCY;
             case 5 -> BRACKETS + CCY + NUMBER + BRACKET_CLOSE;
             default -> throw new IllegalArgumentException("Invalid pattern index: " + patternIndex);
         };
@@ -47,15 +49,17 @@ public class AutoNumberParser extends DatatypeParser {
     }
 
     private final boolean allowDecimal;
+    private final boolean allowCurrency;
 
-    public AutoNumberParser(boolean allowDecimal) {
+    public AutoNumberParser(boolean allowDecimal, boolean allowCurrency) {
         this.allowDecimal = allowDecimal;
+        this.allowCurrency = allowCurrency;
     }
 
     @Override
     protected Object parseSingleValue(String text, ProblemAggregator problemAggregator) {
         int index = 0;
-        var pattern = getPattern(allowDecimal, index);
+        var pattern = getPattern(allowDecimal, allowCurrency, index);
         while (pattern != null) {
             var value = innerParseSingleValue(text, pattern);
             if (value != null) {
@@ -63,7 +67,7 @@ public class AutoNumberParser extends DatatypeParser {
             }
 
             index++;
-            pattern = getPattern(allowDecimal, index);
+            pattern = getPattern(allowDecimal, allowCurrency, index);
         }
 
         problemAggregator.reportInvalidFormat(text);
@@ -73,7 +77,7 @@ public class AutoNumberParser extends DatatypeParser {
     @Override
     public WithProblems<Storage<?>> parseColumn(String columnName, Storage<String> sourceStorage) {
         int index = 0;
-        var pattern = getPattern(allowDecimal, index);
+        var pattern = getPattern(allowDecimal, allowCurrency, index);
 
         int bestIndex = 0;
         int bestCount = -1;
@@ -90,12 +94,12 @@ public class AutoNumberParser extends DatatypeParser {
             }
 
             index++;
-            pattern = getPattern(allowDecimal, index);
+            pattern = getPattern(allowDecimal, allowCurrency, index);
         }
 
         Builder fallback = makeBuilderWithCapacity(sourceStorage.size());
         ProblemAggregator aggregator = new ProblemAggregatorImpl(columnName);
-        parseColumnWithPattern(getPattern(allowDecimal, bestIndex), sourceStorage, fallback, aggregator);
+        parseColumnWithPattern(getPattern(allowDecimal, allowCurrency, bestIndex), sourceStorage, fallback, aggregator);
         return new WithProblems<>(fallback.seal(), aggregator.getAggregatedProblems());
     }
 
@@ -120,6 +124,7 @@ public class AutoNumberParser extends DatatypeParser {
         return -1;
     }
 
+    @Override
     protected Builder makeBuilderWithCapacity(int capacity) {
         return allowDecimal
                 ? NumericBuilder.createDoubleBuilder(capacity)
@@ -134,7 +139,7 @@ public class AutoNumberParser extends DatatypeParser {
 
         try {
             var sign = parsed.group("sign");
-            var sign_value = sign.equals("-") || sign.equals("(") ? -1 : 1;
+            var sign_value = sign != null && !sign.equals("+") ? -1 : 1;
 
             var integer = parsed.group("integer").replaceAll("\\D", "");
             if (allowDecimal) {
