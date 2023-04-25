0d84a601dde48af9701b6be891fb754ec3aa26ee
Michael Mauderer <mail@michaelmauderer.de>
2023-04-25 22:28:15 +0200

    Ability to change the execution environment between design and live. (#6341)
    Integrate the UI for electing the Execution Environment with the Language Server and unify existing uses. Implements  #5930 + actual integration instead of just mocking it.
    
    https://user-images.githubusercontent.com/1428930/232919438-6e1e295a-34fe-4756-86a4-5f5d8f718fa0.mp4
    
    # Important Notes
    The console output is only emitted as part of the `INFO` level. A better check would be to look at the messages sent to the backend in the developer console.

--- app/gui/view/examples/interface/src/lib.rs
+++ app/gui/view/examples/interface/src/lib.rs
@@ -25,6 +25,7 @@ use ensogl::gui::text;
 use ensogl::system::web;
 use ensogl_hardcoded_theme as theme;
 use ensogl_text_msdf::run_once_initialized;
+use ide_view::execution_environment_selector::make_dummy_execution_environments;
 use ide_view::graph_editor;
 use ide_view::graph_editor::component::node::vcs;
 use ide_view::graph_editor::component::node::Expression;
@@ -256,7 +257,7 @@ fn init(app: &Application) {
 
     // === Execution Modes ===
 
-    graph_editor.set_available_execution_modes(vec!["design".to_string(), "live".to_string()]);
+    graph_editor.set_available_execution_environments(make_dummy_execution_environments());
 
 
     // === Rendering ===
