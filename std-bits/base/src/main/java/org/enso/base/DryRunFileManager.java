e50fc26179e1f6a880d2fdba3a366a8cc5062f19
James Dunkerley <jdunkerley@users.noreply.github.com>
2023-04-28 13:30:38 +0100

    Java Format.

--- std-bits/base/src/main/java/org/enso/base/DryRunFileManager.java
+++ std-bits/base/src/main/java/org/enso/base/DryRunFileManager.java
@@ -6,33 +6,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DryRunFileManager {
-    static final Map<String, String> files = new HashMap<>();
+  static final Map<String, String> files = new HashMap<>();
 
-    /**
-     * Creates a temporary file for the given name.
-     *
-     * @param name the path to the file to make a temporary file of.
-     * @return the path to the temporary file.
-     */
-    public static String getTemporaryFile(String name) {
-        return files.computeIfAbsent(name, k -> {
-            if (files.containsValue(k)) {
-                // Existing temporary file so return this.
-                return k;
-            }
+  /**
+   * Creates a temporary file for the given name.
+   *
+   * @param name the path to the file to make a temporary file of.
+   * @return the path to the temporary file.
+   */
+  public static String getTemporaryFile(String name) {
+    return files.computeIfAbsent(
+        name,
+        k -> {
+          if (files.containsValue(k)) {
+            // Existing temporary file so return this.
+            return k;
+          }
 
-            var filename = new File(k).getName();
-            var lastDot = filename.lastIndexOf('.');
-            var prefix = lastDot == -1 ? filename : filename.substring(0, lastDot);
-            var extension = lastDot == -1 ? "" : filename.substring(lastDot);
+          var filename = new File(k).getName();
+          var lastDot = filename.lastIndexOf('.');
+          var prefix = lastDot == -1 ? filename : filename.substring(0, lastDot);
+          var extension = lastDot == -1 ? "" : filename.substring(lastDot);
 
-            try {
-                var temp = File.createTempFile(prefix, extension);
-                temp.deleteOnExit();
-                return temp.getAbsolutePath();
-            } catch (IOException e) {
-                return null;
-            }
+          try {
+            var temp = File.createTempFile(prefix, extension);
+            temp.deleteOnExit();
+            return temp.getAbsolutePath();
+          } catch (IOException e) {
+            return null;
+          }
         });
-    }
+  }
 }
