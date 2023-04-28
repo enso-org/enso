c5debe4d7574261144d2d8b92c54ff30ffad5c61
Wojciech Danilo <wojciech.danilo@gmail.com>
2023-04-28 06:53:17 +0200

    Refactoring

--- lib/rust/ensogl/component/list-editor/src/lib.rs
+++ lib/rust/ensogl/component/list-editor/src/lib.rs
@@ -108,7 +108,7 @@ pub mod placeholder;
 /// If set to true, animations will be running slow. This is useful for debugging purposes.
 pub const DEBUG_ANIMATION_SLOWDOWN: bool = false;
 
-pub const DEBUG_PLACEHOLDERS_VIZ: bool = true;
+pub const DEBUG_PLACEHOLDERS_VIZ: bool = false;
 
 /// Spring factor for animations. If [`DEBUG_ANIMATION_SLOWDOWN`] is set to true, this value will be
 /// used for animation simulators.
