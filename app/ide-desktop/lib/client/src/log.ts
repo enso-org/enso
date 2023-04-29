d6c81fc73186aab9a478ffceb677fba863586f6f
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2023-04-29 18:06:13 +0200

    wip

--- app/ide-desktop/lib/client/src/log.ts
+++ app/ide-desktop/lib/client/src/log.ts
@@ -6,24 +6,26 @@ import * as pathModule from 'node:path'
 import * as electron from 'electron'
 
 import * as contentConfig from 'enso-content-config'
-
+import * as paths from 'paths'
 
 import * as linkedDist from '../../../../../target/ensogl-pack/linked-dist'
 
 const logger = linkedDist.logger
+
+/** Adds a new log consumer that writes to a file.
+ *
+ * The path of the log file is {@link generateUniqueLogFileName automatically generated}.
+ *
+ * The log file is created in the {@link paths.LOGS_DIRECTORY logs directory}
+ *
+ * @returns The full path of the log file.
+ */
 export function addFileLog(): string {
-    const logsDirectory = electron.app.getPath('logs')
-    // Create the logs directory if it doesn't exist
-    if (!fsSync.existsSync(logsDirectory)) {
-        fsSync.mkdirSync(logsDirectory, { recursive: true })
-    }
-
+    const dirname = paths.LOGS_DIRECTORY
     const filename = generateUniqueLogFileName()
-    const logFilePath = pathModule.join(logsDirectory, filename)
-
-    contentConfig.logger.addConsumer(
-        new FileConsumer(logFilePath)
-    )
+    const logFilePath = pathModule.join(dirname, filename)
+    const consumer = new FileConsumer(logFilePath)
+    contentConfig.logger.addConsumer(consumer)
     return logFilePath
 }
 
@@ -33,24 +35,32 @@ export function addFileLog(): string {
  * @returns The file name log file.
  */
 export function generateUniqueLogFileName(): string {
-    const timestamp = new Date().toISOString().replace(/[:.-]/g, '')
-    return `ide-${timestamp}.txt`
+    // Replace ':' with '-' because ':' is not allowed in file names.
+    const timestamp = new Date().toISOString().replace(/:/g, '-')
+    const version = contentConfig.VERSION.ide.raw
+    return `${timestamp}-ide-${version}.log`
 }
 
-export function logDirectory(): string {
-    return electron.app.getPath('logs')
-}
+// ================
+// === Consumer ===
+// ================
 
 /** Log consumer that writes to a file. */
 export class FileConsumer extends linkedDist.Consumer {
     private readonly logFilePath: string
     private readonly logFileHandle: number
 
+    /** Create a log consumer that writes to a file.
+     *
+     * @param logPath - The path of the log file. Must be writeable.
+     */
     constructor(logPath: string) {
         super()
+        // Create the directory if it doesn't exist, otherwise fsSync.openSync will fail.
+        const logsDirectory = pathModule.dirname(logPath)
+        fsSync.mkdirSync(logsDirectory, { recursive: true })
         this.logFilePath = logPath
         this.logFileHandle = fsSync.openSync(this.logFilePath, 'a')
-        logger.log(`Log file: ${this.logFilePath}`)
     }
 
     override message(level: linkedDist.LogLevel, ...args: unknown[]): void {
@@ -67,17 +77,21 @@ export class FileConsumer extends linkedDist.Consumer {
                 console.error('Failed to write log:', error)
             }
         } else {
+            // This should never happen, as the log file handle is initialized in the constructor.
             console.error('Log file not initialized.')
         }
     }
+
     override startGroup(...args: unknown[]): void {
         this.message('log', '[GROUP START]', ...args)
     }
+
     override startGroupCollapsed(...args: unknown[]): void {
         // We don't have a way to collapse groups in the file logger, so we just use the same
         // function as startGroup.
         this.message('log', '[GROUP START]', ...args)
     }
+
     override groupEnd(...args: unknown[]): void {
         this.message('log', '[GROUP END]', ...args)
     }
