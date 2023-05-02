ff65ff114997f8e177a493db11fc05d7cd99e28f
Radosław Waśko <radoslaw.wasko@enso.org>
2023-05-02 19:32:01 +0200

    Legal review but Windows breaking newlines

--- engine/runtime/src/main/scala/org/enso/compiler/phase/ImportResolver.scala
+++ engine/runtime/src/main/scala/org/enso/compiler/phase/ImportResolver.scala
@@ -246,6 +246,12 @@ class ImportResolver(compiler: Compiler) {
               case Some(tp) =>
                 (imp, Some(BindingsMap.ResolvedImport(imp, exp, tp)))
               case None =>
+                if (
+                  impName.matches("Standard.AWS.Database.Redshift.[a-zA-Z]+")
+                ) {
+                  System.out.println("A")
+                  System.err.println("B")
+                }
                 (
                   IR.Error.ImportExport(
                     imp,
