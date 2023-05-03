cb22d7cb8de0ee04435e21960d323fe3510893ad
somebody1234 <ehern.lee@gmail.com>
2023-05-04 02:13:02 +1000

    Fix cloud-v2/#416

--- app/ide-desktop/lib/dashboard/watch.ts
+++ app/ide-desktop/lib/dashboard/watch.ts
@@ -13,8 +13,8 @@ export const THIS_PATH = path.resolve(path.dirname(url.fileURLToPath(import.meta
 // === Constants ===
 // =================
 
-/** This must be port `8081` because it is defined as such in AWS. */
-const PORT = 8081
+/** This must be port `8080` because it is defined as such in AWS. */
+const PORT = 8080
 const HTTP_STATUS_OK = 200
 // `outputPath` does not have to be a real directory because `write` is `false`,
 // meaning that files will not be written to the filesystem.
