7885145b6e3caf42b343950ae23646dcd789bb27
somebody1234 <ehern.lee@gmail.com>
2023-05-04 14:00:42 +1000

    Run typecheck and eslint on `./run lint` (#6314)

--- build/cli/src/lib.rs
+++ build/cli/src/lib.rs
@@ -86,6 +86,8 @@ use ide_ci::programs::git;
 use ide_ci::programs::git::clean;
 use ide_ci::programs::rustc;
 use ide_ci::programs::Cargo;
+use ide_ci::programs::Npm;
+use ide_ci::programs::Npx;
 use std::time::Duration;
 use tempfile::tempdir;
 use tokio::process::Child;
@@ -835,6 +837,10 @@ pub async fn main_internal(config: Option<enso_build::config::Config>) -> Result
                 .await?;
 
             prettier::check(&ctx.repo_root).await?;
+            let js_modules_root = ctx.repo_root.join("app/ide-desktop");
+            Npm.cmd()?.current_dir(&js_modules_root).args(["install"]).run_ok().await?;
+            Npm.cmd()?.current_dir(&js_modules_root).args(["run", "typecheck"]).run_ok().await?;
+            Npx.cmd()?.current_dir(&js_modules_root).args(["eslint", "."]).run_ok().await?;
         }
         Target::Fmt => {
             let prettier = prettier::write(&ctx.repo_root);
