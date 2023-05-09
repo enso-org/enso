4e7f757f53f26c8792066e0d1019e5d95e4993a4
somebody1234 <ehern.lee@gmail.com>
2023-05-09 18:33:11 +1000

    Fix dashboard issues (part 2) (#6511)
    * Fix cloud-v2/#411
    
    * Fix cloud-v2/#412
    
    * Fix cloud-v2/#415
    
    * Fix cloud-v2/#414
    
    * Run prettier
    
    * Fix cloud-v2/#417
    
    ---------
    
    Co-authored-by: Paweł Buchowski <pawel.buchowski@enso.org>

--- app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/topBar.tsx
+++ app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/topBar.tsx
@@ -1,4 +1,6 @@
 /** @file The top-bar of dashboard. */
+import * as react from 'react'
+
 import * as dashboard from './dashboard'
 import * as platformModule from '../../platform'
 import * as svg from '../../components/svg'
@@ -28,8 +30,17 @@ interface TopBarProps {
  */
 function TopBar(props: TopBarProps) {
     const { platform, projectName, tab, toggleTab, setBackendPlatform, query, setQuery } = props
-    const { setModal } = modalProvider.useSetModal()
+    const [userMenuVisible, setUserMenuVisible] = react.useState(false)
+    const { setModal, unsetModal } = modalProvider.useSetModal()
     const { backend } = backendProvider.useBackend()
+
+    react.useEffect(() => {
+        if (userMenuVisible) {
+            setModal(() => <UserMenu />)
+        } else {
+            unsetModal()
+        }
+    }, [userMenuVisible])
 
     return (
         <div className="flex mb-2 h-8">
@@ -114,7 +125,7 @@ function TopBar(props: TopBarProps) {
                     className="rounded-full w-8 h-8 bg-cover cursor-pointer"
                     onClick={event => {
                         event.stopPropagation()
-                        setModal(() => <UserMenu />)
+                        setUserMenuVisible(!userMenuVisible)
                     }}
                 />
             </div>
