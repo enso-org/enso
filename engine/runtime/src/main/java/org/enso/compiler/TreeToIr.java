41bb52c9018939e8de4210c993774872a3b85d25
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-05-14 09:23:18 +0200

    Verify ascribed types of parameters really exist (#6584)
    Verify ascribed types `(a : Xyz)` are checked for existence.

--- engine/runtime/src/main/java/org/enso/compiler/TreeToIr.java
+++ engine/runtime/src/main/java/org/enso/compiler/TreeToIr.java
@@ -1141,7 +1141,7 @@ final class TreeToIr {
       }
       case Tree.Ident id when insideTypeAscription -> {
         try {
-          yield buildQualifiedName(id, getIdentifiedLocation(id), false);
+          yield buildNameOrQualifiedName(id, getIdentifiedLocation(id));
         } catch (SyntaxException ex) {
           yield ex.toError();
         }
@@ -1412,11 +1412,14 @@ final class TreeToIr {
     return new IR$Name$Qualified(qualifiedNameSegments(t, generateId), loc, meta(), diag());
   }
   private IR.Name buildNameOrQualifiedName(Tree t) throws SyntaxException {
+    return buildNameOrQualifiedName(t, Option.empty());
+  }
+  private IR.Name buildNameOrQualifiedName(Tree t, Option<IdentifiedLocation> loc) throws SyntaxException {
     var segments = qualifiedNameSegments(t, false);
     if (segments.length() == 1) {
       return segments.head();
     } else {
-      return new IR$Name$Qualified(segments, Option.empty(), meta(), diag());
+      return new IR$Name$Qualified(segments, loc, meta(), diag());
     }
   }
   private java.util.List<Tree> unrollOprRhs(Tree list, String operator) throws SyntaxException {
