c2294506fe64428d436c31caccf86b7080301202
Gregory Travis <greg.travis@enso.org>
2023-05-15 14:12:01 -0400

    still cannot find it

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/number/decimal/IntNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/number/decimal/IntNode.java
@@ -1,36 +1,22 @@
 package org.enso.interpreter.node.expression.builtin.number.decimal;
 
 import com.oracle.truffle.api.CompilerDirectives;
-import com.oracle.truffle.api.dsl.Fallback;
-import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.profiles.ConditionProfile;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import org.enso.interpreter.dsl.BuiltinMethod;
 import org.enso.interpreter.node.expression.builtin.number.utils.BigIntegerOps;
-import org.enso.interpreter.runtime.builtin.Builtins;
-import org.enso.interpreter.runtime.error.DataflowError;
-import org.enso.interpreter.runtime.EnsoContext;
-import org.enso.interpreter.runtime.error.PanicException;
 import org.enso.interpreter.runtime.number.EnsoBigInteger;
 
 @BuiltinMethod(
-        type = "Decmial",
-        name = "int_builtin",
+        type = "Decimal",
+        name = "int",
         description = "Truncate a floating-point number to an integer by dropping the fractional part.")
-public abstract class IntNode extends Node {
+public class IntNode extends Node {
     private final ConditionProfile fitsProfile = ConditionProfile.createCountingProfile();
 
-    abstract Object execute(double self);
-
-    static IntNode build() {
-        return IntNodeGen.create();
-    }
-
-    @Specialization
-    Object doLong(double self) {
-        // Returns integer
+    Object execute(double self) {
         if (fitsProfile.profile(BigIntegerOps.fitsInLong(self))) {
             return (long) self;
         } else {
