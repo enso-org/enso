1daa546c8f653e17d0b78b5b69aa9082da0e8d3a
somebody1234 <ehern.lee@gmail.com>
2023-05-17 21:41:58 +1000

    Fix password pattern

--- app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/validation.ts
+++ app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/validation.ts
@@ -2,7 +2,7 @@
 
 /** Regex pattern for valid AWS Cognito passwords. */
 export const PASSWORD_PATTERN =
-    '(?:.+[0-9])(?:.+[A-Z])(?:.+[a-z])(?:.+[ ^$*.\\[\\]{}()?"!@#%&/,><\':;|_~`=+-]).{6,256}'
+    '(?=.+[0-9])(?=.+[A-Z])(?=.+[a-z])(?=.+[ ^$*.\\[\\]{}()?"!@#%&/,><\':;|_~`=+-]).{6,256}'
 /** Human readable explanation of password requirements. */
 export const PASSWORD_TITLE =
     'Your password must include numbers, letters (both lowercase and uppercase) and symbols, ' +
