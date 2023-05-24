a86fd2272145b18acac8a2884d3d48dda13f6f20
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-05-24 08:40:18 +0200

    First of all check types.hasType and then handle WithoutType values without negative hasType check

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/TypeOfNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/TypeOfNode.java
@@ -1,6 +1,7 @@
 package org.enso.interpreter.node.expression.builtin.meta;
 
 import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.GenerateUncached;
 import com.oracle.truffle.api.dsl.Specialization;
@@ -10,10 +11,10 @@ import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.nodes.Node;
 import org.enso.interpreter.dsl.AcceptsError;
 import org.enso.interpreter.dsl.BuiltinMethod;
-import org.enso.interpreter.epb.runtime.PolyglotProxy;
 import org.enso.interpreter.runtime.EnsoContext;
 import org.enso.interpreter.runtime.builtin.Builtins;
 import org.enso.interpreter.runtime.callable.UnresolvedSymbol;
+import org.enso.interpreter.runtime.data.Type;
 import org.enso.interpreter.runtime.error.DataflowError;
 import org.enso.interpreter.runtime.error.PanicException;
 import org.enso.interpreter.runtime.error.PanicSentinel;
@@ -75,99 +76,13 @@ public abstract class TypeOfNode extends Node {
     return execute(value.getValue());
   }
 
-  @Specialization(
-      guards = {
-        "interop.hasArrayElements(proxy)",
-        "!interop.isString(proxy)", // R string value is an array and a string
-        "!types.hasType(proxy)",
-        "!interop.hasMetaObject(proxy)"
-      })
-  Object doPolyglotArray(
-      PolyglotProxy proxy,
-      @CachedLibrary(limit = "3") InteropLibrary interop,
-      @CachedLibrary(limit = "3") TypesLibrary types) {
-    return EnsoContext.get(this).getBuiltins().array();
-  }
-
-  @Specialization(
-      guards = {
-        "interop.isString(proxy)",
-        "!types.hasType(proxy)",
-        "!interop.hasMetaObject(proxy)"
-      })
-  Object doPolyglotString(
-      PolyglotProxy proxy,
-      @CachedLibrary(limit = "3") InteropLibrary interop,
-      @CachedLibrary(limit = "3") TypesLibrary types) {
-    return EnsoContext.get(this).getBuiltins().text();
-  }
-
-  @Specialization(
-      guards = {
-        "interop.isNumber(proxy)",
-        "!types.hasType(proxy)",
-        "!interop.hasMetaObject(proxy)"
-      })
-  Object doPolyglotNumber(
-      PolyglotProxy proxy,
-      @CachedLibrary(limit = "3") InteropLibrary interop,
-      @CachedLibrary(limit = "3") TypesLibrary types) {
-    Builtins builtins = EnsoContext.get(this).getBuiltins();
-    if (interop.fitsInInt(proxy)) {
-      return builtins.number().getInteger();
-    } else if (interop.fitsInDouble(proxy)) {
-      return builtins.number().getDecimal();
-    } else {
-      return EnsoContext.get(this).getBuiltins().number();
-    }
-  }
-
-  @Specialization(guards = {"findInterop(value, interop).isDateTime()"})
-  Object doDateTime(Object value, @CachedLibrary(limit = "3") InteropLibrary interop) {
-    return EnsoContext.get(this).getBuiltins().dateTime();
-  }
-
-  @Specialization(guards = {"findInterop(value, interop).isTimeZone()"})
-  Object doTimeZone(Object value, @CachedLibrary(limit = "3") InteropLibrary interop) {
-    EnsoContext ctx = EnsoContext.get(this);
-    return ctx.getBuiltins().timeZone();
-  }
-
-  @Specialization(guards = {"findInterop(value, interop).isDate()"})
-  Object doDate(Object value, @CachedLibrary(limit = "3") InteropLibrary interop) {
-    EnsoContext ctx = EnsoContext.get(this);
-    return ctx.getBuiltins().date();
-  }
-
-  @Specialization(guards = {"findInterop(value, interop).isTime()"})
-  Object doTime(Object value, @CachedLibrary(limit = "3") InteropLibrary interop) {
-    EnsoContext ctx = EnsoContext.get(this);
-    return ctx.getBuiltins().timeOfDay();
-  }
-
-  @Specialization(guards = "findInterop(value, interop).isDuration()")
-  Object doDuration(Object value, @CachedLibrary(limit = "3") InteropLibrary interop) {
-    EnsoContext ctx = EnsoContext.get(this);
-    return ctx.getBuiltins().duration();
-  }
-
-  @Specialization(
-      guards = {
-        "interop.hasMetaObject(value)",
-        "!types.hasType(value)",
-        "findInterop(value, interop).isNone()"
-      })
-  Object doMetaObject(
+  @Specialization(guards = {"!types.hasType(value)"})
+  Object withoutType(
       Object value,
       @CachedLibrary(limit = "3") InteropLibrary interop,
-      @CachedLibrary(limit = "3") TypesLibrary types) {
-    try {
-      return interop.getMetaObject(value);
-    } catch (UnsupportedMessageException e) {
-      CompilerDirectives.transferToInterpreter();
-      Builtins builtins = EnsoContext.get(this).getBuiltins();
-      throw new PanicException(builtins.error().makeCompileError("invalid meta object"), this);
-    }
+      @CachedLibrary(limit = "3") TypesLibrary types,
+      @Cached WithoutType delegate) {
+    return delegate.execute(value);
   }
 
   @Specialization(guards = {"types.hasType(value)", "!interop.isNumber(value)"})
@@ -176,61 +91,6 @@ public abstract class TypeOfNode extends Node {
       @CachedLibrary(limit = "3") InteropLibrary interop,
       @CachedLibrary(limit = "3") TypesLibrary types) {
     return types.getType(value);
-  }
-
-  static InteropType findInterop(Object value, InteropLibrary interop) {
-    return InteropType.find(value, interop);
-  }
-
-  enum InteropType {
-    NONE,
-    DATE_TIME,
-    TIME_ZONE,
-    DATE,
-    TIME,
-    DURATION;
-
-    static InteropType find(Object value, InteropLibrary interop) {
-      boolean time = interop.isTime(value);
-      boolean date = interop.isDate(value);
-      if (time) {
-        return date ? DATE_TIME : TIME;
-      }
-      if (date) {
-        return DATE;
-      }
-      if (interop.isTimeZone(value)) {
-        return TIME_ZONE;
-      }
-      if (interop.isDuration(value)) {
-        return DURATION;
-      }
-      return NONE;
-    }
-
-    boolean isDateTime() {
-      return this == DATE_TIME;
-    }
-
-    boolean isTimeZone() {
-      return this == TIME_ZONE;
-    }
-
-    boolean isTime() {
-      return this == TIME;
-    }
-
-    boolean isDate() {
-      return this == DATE;
-    }
-
-    boolean isDuration() {
-      return this == DURATION;
-    }
-
-    boolean isNone() {
-      return this == NONE;
-    }
   }
 
   @Fallback
@@ -242,5 +102,151 @@ public abstract class TypeOfNode extends Node {
             .error()
             .makeCompileError("unknown type_of for " + value),
         this);
+  }
+
+  @GenerateUncached
+  abstract static class WithoutType extends Node {
+    abstract Object execute(Object value);
+
+    @Specialization(guards = {"findInterop(value, interop).isArray()"})
+    Type doPolyglotArray(Object value, @CachedLibrary(limit = "3") InteropLibrary interop) {
+      return EnsoContext.get(this).getBuiltins().array();
+    }
+
+    @Specialization(guards = {"findInterop(value, interop).isString()"})
+    Type doPolyglotString(Object value, @CachedLibrary(limit = "3") InteropLibrary interop) {
+      return EnsoContext.get(this).getBuiltins().text();
+    }
+
+    @Specialization(guards = {"findInterop(value, interop).isNumber()"})
+    Type doPolyglotNumber(Object value, @CachedLibrary(limit = "3") InteropLibrary interop) {
+      Builtins builtins = EnsoContext.get(this).getBuiltins();
+      if (interop.fitsInInt(value)) {
+        return builtins.number().getInteger();
+      } else if (interop.fitsInDouble(value)) {
+        return builtins.number().getDecimal();
+      } else {
+        return EnsoContext.get(this).getBuiltins().number().getNumber();
+      }
+    }
+
+    @Specialization(guards = {"findInterop(value, interop).isDateTime()"})
+    Type doDateTime(Object value, @CachedLibrary(limit = "3") InteropLibrary interop) {
+      return EnsoContext.get(this).getBuiltins().dateTime();
+    }
+
+    @Specialization(guards = {"findInterop(value, interop).isTimeZone()"})
+    Type doTimeZone(Object value, @CachedLibrary(limit = "3") InteropLibrary interop) {
+      EnsoContext ctx = EnsoContext.get(this);
+      return ctx.getBuiltins().timeZone();
+    }
+
+    @Specialization(guards = {"findInterop(value, interop).isDate()"})
+    Type doDate(Object value, @CachedLibrary(limit = "3") InteropLibrary interop) {
+      EnsoContext ctx = EnsoContext.get(this);
+      return ctx.getBuiltins().date();
+    }
+
+    @Specialization(guards = {"findInterop(value, interop).isTime()"})
+    Type doTime(Object value, @CachedLibrary(limit = "3") InteropLibrary interop) {
+      EnsoContext ctx = EnsoContext.get(this);
+      return ctx.getBuiltins().timeOfDay();
+    }
+
+    @Specialization(guards = "findInterop(value, interop).isDuration()")
+    Type doDuration(Object value, @CachedLibrary(limit = "3") InteropLibrary interop) {
+      EnsoContext ctx = EnsoContext.get(this);
+      return ctx.getBuiltins().duration();
+    }
+
+    @Specialization(guards = {"findInterop(value, interop).isNone()"})
+    Object doMetaObject(Object value, @CachedLibrary(limit = "3") InteropLibrary interop) {
+      try {
+        return interop.getMetaObject(value);
+      } catch (UnsupportedMessageException e) {
+        CompilerDirectives.transferToInterpreter();
+        Builtins builtins = EnsoContext.get(this).getBuiltins();
+        throw new PanicException(builtins.error().makeCompileError("invalid meta object"), this);
+      }
+    }
+
+    static InteropType findInterop(Object value, InteropLibrary interop) {
+      return InteropType.find(value, interop);
+    }
+
+    enum InteropType {
+      NONE,
+      STRING,
+      NUMBER,
+      ARRAY,
+      DATE_TIME,
+      TIME_ZONE,
+      DATE,
+      TIME,
+      DURATION;
+
+      static InteropType find(Object value, InteropLibrary interop) {
+        if (interop.isString(value)) {
+          return STRING;
+        }
+        if (interop.isNumber(value)) {
+          return NUMBER;
+        }
+        if (interop.hasArrayElements(value)) {
+          return ARRAY;
+        }
+        boolean time = interop.isTime(value);
+        boolean date = interop.isDate(value);
+        if (time) {
+          return date ? DATE_TIME : TIME;
+        }
+        if (date) {
+          return DATE;
+        }
+        if (interop.isTimeZone(value)) {
+          return TIME_ZONE;
+        }
+        if (interop.isDuration(value)) {
+          return DURATION;
+        }
+        return NONE;
+      }
+
+      boolean isString() {
+        return this == STRING;
+      }
+
+      boolean isNumber() {
+        return this == NUMBER;
+      }
+
+      boolean isArray() {
+        return this == ARRAY;
+      }
+
+      boolean isDateTime() {
+        return this == DATE_TIME;
+      }
+
+      boolean isTimeZone() {
+        return this == TIME_ZONE;
+      }
+
+      boolean isTime() {
+        return this == TIME;
+      }
+
+      boolean isDate() {
+        return this == DATE;
+      }
+
+      boolean isDuration() {
+        return this == DURATION;
+      }
+
+      boolean isNone() {
+        return this == NONE;
+      }
+    }
   }
 }
