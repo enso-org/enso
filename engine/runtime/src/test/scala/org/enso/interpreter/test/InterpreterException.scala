18b5c297924f592d3bb1711aba517c2855b230fe
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-05-25 09:53:05 +0200

    Don't swallow compiler output when compilation fails

--- engine/runtime/src/test/scala/org/enso/interpreter/test/InterpreterException.scala
+++ engine/runtime/src/test/scala/org/enso/interpreter/test/InterpreterException.scala
@@ -3,23 +3,26 @@ package org.enso.interpreter.test
 import org.graalvm.polyglot.PolyglotException
 
 case class InterpreterException(
-  @transient polyglotException: PolyglotException
+  @transient polyglotException: PolyglotException,
+  @transient output: Option[Any]
 ) extends Throwable
     with Serializable {
-  override def getMessage: String = polyglotException.getMessage
+  override def getMessage: String =
+    polyglotException.getMessage + output.map("\n" + _.toString()).getOrElse("")
   override def getStackTrace: Array[StackTraceElement] =
     polyglotException.getStackTrace
   override def fillInStackTrace(): Throwable = this
 }
 
 object InterpreterException {
-  def rethrowPolyglot[T](compute: => T): T =
+  def rethrowPolyglot[T](compute: => T, output: Option[Any] = None): T =
     try {
       compute
-    } catch { case e: PolyglotException => throw InterpreterException(e) }
+    } catch {
+      case e: PolyglotException => throw InterpreterException(e, output)
+    }
 
   implicit def toPolyglotException(
     interpreterException: InterpreterException
   ): PolyglotException = interpreterException.polyglotException
-
 }
