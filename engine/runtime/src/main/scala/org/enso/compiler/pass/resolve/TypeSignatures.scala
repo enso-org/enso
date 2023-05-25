34e0a2ca88cb7b2166b00852ccc1770894bb1333
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-05-25 14:40:40 +0200

    Less of currly braces
    Co-authored-by: Hubert Plociniczak <hubert.plociniczak@gmail.com>

--- engine/runtime/src/main/scala/org/enso/compiler/pass/resolve/TypeSignatures.scala
+++ engine/runtime/src/main/scala/org/enso/compiler/pass/resolve/TypeSignatures.scala
@@ -179,11 +179,9 @@ case object TypeSignatures extends IRPass {
   private def verifyAscribedArguments(
     arguments: List[IR.DefinitionArgument]
   ): Unit = {
-    arguments.foreach(arg => {
-      arg.ascribedType.map(t => {
-        arg.updateMetadata(this -->> Signature(t))
-      })
-    })
+    arguments.foreach(arg =>
+      arg.ascribedType.map(t => arg.updateMetadata(this -->> Signature(t)))
+    )
   }
 
   /** Resolves type signatures in an arbitrary expression.
