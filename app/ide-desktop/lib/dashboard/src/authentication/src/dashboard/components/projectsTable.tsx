f3a863cca43aa5045940c8e2d53dee08d93a3017
somebody1234 <ehern.lee@gmail.com>
2023-07-03 12:13:28 +1000

    Merge `projectActionButton` into `projectsTable`

--- app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/projectsTable.tsx
+++ app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/projectsTable.tsx
@@ -2,6 +2,8 @@
 import * as React from 'react'
 import toast from 'react-hot-toast'
 
+import ArrowUpIcon from 'enso-assets/arrow_up.svg'
+import PlayIcon from 'enso-assets/play.svg'
 import PlusIcon from 'enso-assets/plus.svg'
 
 import * as backendModule from '../backend'
@@ -16,9 +18,9 @@ import * as modalProvider from '../../providers/modal'
 import * as presence from '../presence'
 import * as projectEventModule from '../events/projectEvent'
 import * as projectListEventModule from '../events/projectListEvent'
-import * as projectRowState from '../projectRowState'
 import * as shortcuts from '../shortcuts'
 import * as string from '../../string'
+import * as svg from '../../components/svg'
 import * as uniqueString from '../../uniqueString'
 import * as validation from '../validation'
 
@@ -28,9 +30,35 @@ import ConfirmDeleteModal from './confirmDeleteModal'
 import ContextMenu from './contextMenu'
 import ContextMenuEntry from './contextMenuEntry'
 import EditableSpan from './editableSpan'
-import ProjectActionButton from './projectActionButton'
 import RenameModal from './renameModal'
 import Table from './table'
+
+// =============
+// === Types ===
+// =============
+
+/** The state of the spinner. It should go from initial, to loading, to done. */
+enum SpinnerState {
+    initial = 'initial',
+    loading = 'loading',
+    done = 'done',
+}
+
+/** The state of checking whether a project is ready. It should go from not checking, to checking
+ * status, to checking resources, to done. */
+enum CheckState {
+    /** The project is not open. */
+    notChecking = 'not-checking',
+    /** A local project is being opened. There are no status and resource checks; the state is
+     * set to done when the RPC call finishes. */
+    localProject = 'local-project',
+    /** Status is not yet `ProjectState.opened`. */
+    checkingStatus = 'checking-status',
+    /** `backend.checkResources` calls are still failing. */
+    checkingResources = 'checking-resources',
+    /** The project is open. */
+    done = 'done',
+}
 
 // =================
 // === Constants ===
@@ -49,6 +77,340 @@ const PLACEHOLDER = (
         You have no projects yet. Go ahead and create one using the form above.
     </span>
 )
+
+// === ProjectActionButton constants ===
+
+const LOADING_MESSAGE =
+    'Your environment is being created. It will take some time, please be patient.'
+/** The interval between requests checking whether the IDE is ready. */
+const CHECK_STATUS_INTERVAL_MS = 5000
+/** The interval between requests checking whether the VM is ready. */
+const CHECK_RESOURCES_INTERVAL_MS = 1000
+/** The corresponding {@link SpinnerState} for each {@link backendModule.ProjectState}. */
+const SPINNER_STATE: Record<backendModule.ProjectState, SpinnerState> = {
+    [backendModule.ProjectState.closed]: SpinnerState.initial,
+    [backendModule.ProjectState.created]: SpinnerState.initial,
+    [backendModule.ProjectState.new]: SpinnerState.initial,
+    [backendModule.ProjectState.openInProgress]: SpinnerState.loading,
+    [backendModule.ProjectState.opened]: SpinnerState.done,
+}
+
+const SPINNER_CSS_CLASSES: Record<SpinnerState, string> = {
+    [SpinnerState.initial]: 'dasharray-5 ease-linear',
+    [SpinnerState.loading]: 'dasharray-75 duration-90000 ease-linear',
+    [SpinnerState.done]: 'dasharray-100 duration-1000 ease-in',
+} as const
+
+// ===========================
+// === ProjectActionButton ===
+// ===========================
+
+/** Props for a {@link ProjectActionButton}. */
+export interface ProjectActionButtonProps {
+    project: backendModule.ProjectAsset
+    rowState: ProjectRowState
+    setRowState: React.Dispatch<React.SetStateAction<ProjectRowState>>
+    projectEvent: projectEventModule.ProjectEvent | null
+    /** Called when the project is opened via the {@link ProjectActionButton}. */
+    doOpenManually: (projectId: backendModule.ProjectId) => void
+    onClose: () => void
+    appRunner: AppRunner | null
+    openIde: () => void
+}
+
+/** An interactive button displaying the status of a project. */
+function ProjectActionButton(props: ProjectActionButtonProps) {
+    const {
+        project,
+        rowState,
+        setRowState,
+        projectEvent,
+        appRunner,
+        doOpenManually,
+        onClose,
+        openIde,
+    } = props
+    const { backend } = backendProvider.useBackend()
+    const { unsetModal } = modalProvider.useSetModal()
+
+    const shouldCheckIfActuallyOpen =
+        backend.type === backendModule.BackendType.remote &&
+        (project.projectState.type === backendModule.ProjectState.opened ||
+            project.projectState.type === backendModule.ProjectState.openInProgress)
+
+    const [state, setState] = React.useState(() => {
+        if (shouldCheckIfActuallyOpen) {
+            return backendModule.ProjectState.created
+        } else {
+            return project.projectState.type
+        }
+    })
+    const [checkState, setCheckState] = React.useState(CheckState.notChecking)
+    const [spinnerState, setSpinnerState] = React.useState(SPINNER_STATE[state])
+    const [shouldOpenWhenReady, setShouldOpenWhenReady] = React.useState(false)
+    const [toastId, setToastId] = React.useState<string | null>(null)
+
+    const openProject = React.useCallback(async () => {
+        setState(backendModule.ProjectState.openInProgress)
+        try {
+            switch (backend.type) {
+                case backendModule.BackendType.remote:
+                    setToastId(toast.loading(LOADING_MESSAGE))
+                    await backend.openProject(project.id, null, project.title)
+                    setRowState({ ...rowState, isRunning: true })
+                    setCheckState(CheckState.checkingStatus)
+                    break
+                case backendModule.BackendType.local:
+                    setCheckState(CheckState.localProject)
+                    setRowState({ ...rowState, isRunning: true })
+                    await backend.openProject(project.id, null, project.title)
+                    setCheckState(oldCheckState => {
+                        if (oldCheckState === CheckState.localProject) {
+                            setState(backendModule.ProjectState.opened)
+                        }
+                        return oldCheckState
+                    })
+                    break
+            }
+        } catch (error) {
+            setCheckState(CheckState.notChecking)
+            toast.error(
+                `Error opening project '${project.title}': ${
+                    errorModule.tryGetMessage(error) ?? 'unknown error'
+                }.`
+            )
+            setState(backendModule.ProjectState.closed)
+        }
+    }, [backend, project.id, project.title, rowState, /* should never change */ setRowState])
+
+    React.useEffect(() => {
+        if (toastId != null) {
+            return () => {
+                toast.dismiss(toastId)
+            }
+        } else {
+            return
+        }
+    }, [toastId])
+
+    React.useEffect(() => {
+        // Ensure that the previous spinner state is visible for at least one frame.
+        requestAnimationFrame(() => {
+            setSpinnerState(SPINNER_STATE[state])
+        })
+    }, [state])
+
+    React.useEffect(() => {
+        if (toastId != null && state !== backendModule.ProjectState.openInProgress) {
+            toast.dismiss(toastId)
+        }
+    }, [state, toastId])
+
+    React.useEffect(() => {
+        if (shouldCheckIfActuallyOpen) {
+            setState(backendModule.ProjectState.openInProgress)
+            setCheckState(CheckState.checkingResources)
+        }
+    }, [shouldCheckIfActuallyOpen])
+
+    hooks.useEventHandler(projectEvent, event => {
+        switch (event.type) {
+            default: {
+                // Ignore; all other events are handled by `ProjectRow`.
+                break
+            }
+            case projectEventModule.ProjectEventType.open: {
+                if (event.projectId !== project.id) {
+                    if (backend.type === backendModule.BackendType.local) {
+                        setState(backendModule.ProjectState.closed)
+                        setCheckState(CheckState.notChecking)
+                    }
+                    setShouldOpenWhenReady(false)
+                } else {
+                    setShouldOpenWhenReady(true)
+                    void openProject()
+                }
+                break
+            }
+            case projectEventModule.ProjectEventType.cancelOpeningAll: {
+                if (backend.type === backendModule.BackendType.local) {
+                    setState(backendModule.ProjectState.closed)
+                    setCheckState(CheckState.notChecking)
+                }
+                setShouldOpenWhenReady(false)
+                break
+            }
+            case projectEventModule.ProjectEventType.showAsOpening: {
+                if (event.projectId === project.id) {
+                    setState(backendModule.ProjectState.openInProgress)
+                }
+                break
+            }
+        }
+    })
+
+    React.useEffect(() => {
+        if (shouldOpenWhenReady && state === backendModule.ProjectState.opened) {
+            openIde()
+            setShouldOpenWhenReady(false)
+        }
+    }, [shouldOpenWhenReady, state, openIde])
+
+    React.useEffect(() => {
+        switch (checkState) {
+            case CheckState.notChecking:
+            case CheckState.localProject:
+            case CheckState.done: {
+                return
+            }
+            case CheckState.checkingStatus: {
+                let handle: number | null = null
+                let continuePolling = true
+                let previousTimestamp = 0
+                const checkProjectStatus = async () => {
+                    try {
+                        const response = await backend.getProjectDetails(project.id, project.title)
+                        handle = null
+                        if (
+                            continuePolling &&
+                            response.state.type === backendModule.ProjectState.opened
+                        ) {
+                            continuePolling = false
+                            setCheckState(CheckState.checkingResources)
+                        }
+                    } finally {
+                        if (continuePolling) {
+                            const nowTimestamp = Number(new Date())
+                            const delay =
+                                CHECK_STATUS_INTERVAL_MS - (nowTimestamp - previousTimestamp)
+                            previousTimestamp = nowTimestamp
+                            handle = window.setTimeout(
+                                () => void checkProjectStatus(),
+                                Math.max(0, delay)
+                            )
+                        }
+                    }
+                }
+                void checkProjectStatus()
+                return () => {
+                    continuePolling = false
+                    if (handle != null) {
+                        clearTimeout(handle)
+                    }
+                }
+            }
+            case CheckState.checkingResources: {
+                let handle: number | null = null
+                let continuePolling = true
+                let previousTimestamp = 0
+                const checkProjectResources = async () => {
+                    if (backend.type === backendModule.BackendType.local) {
+                        // This should never happen, but still should be handled.
+                        setState(backendModule.ProjectState.opened)
+                        setCheckState(CheckState.done)
+                    } else {
+                        try {
+                            // This call will error if the VM is not ready yet.
+                            await backend.checkResources(project.id, project.title)
+                            handle = null
+                            if (continuePolling) {
+                                continuePolling = false
+                                setState(backendModule.ProjectState.opened)
+                                setCheckState(CheckState.done)
+                            }
+                        } catch {
+                            if (continuePolling) {
+                                const nowTimestamp = Number(new Date())
+                                const delay =
+                                    CHECK_RESOURCES_INTERVAL_MS - (nowTimestamp - previousTimestamp)
+                                previousTimestamp = nowTimestamp
+                                handle = window.setTimeout(
+                                    () => void checkProjectResources(),
+                                    Math.max(0, delay)
+                                )
+                            }
+                        }
+                    }
+                }
+                void checkProjectResources()
+                return () => {
+                    continuePolling = false
+                    if (handle != null) {
+                        clearTimeout(handle)
+                    }
+                }
+            }
+        }
+    }, [checkState, project.id, project.title, backend])
+
+    const closeProject = async () => {
+        onClose()
+        setShouldOpenWhenReady(false)
+        setState(backendModule.ProjectState.closed)
+        appRunner?.stopApp()
+        setCheckState(CheckState.notChecking)
+        try {
+            await backend.closeProject(project.id, project.title)
+        } finally {
+            // This is not 100% correct, but it is better than never setting `isRunning` to `false`,
+            // which would prevent the project from ever being deleted.
+            setRowState({ ...rowState, isRunning: false })
+        }
+    }
+
+    switch (state) {
+        case null:
+        case backendModule.ProjectState.created:
+        case backendModule.ProjectState.new:
+        case backendModule.ProjectState.closed:
+            return (
+                <button
+                    onClick={clickEvent => {
+                        clickEvent.stopPropagation()
+                        unsetModal()
+                        doOpenManually(project.id)
+                    }}
+                >
+                    <img src={PlayIcon} />
+                </button>
+            )
+        case backendModule.ProjectState.openInProgress:
+            return (
+                <button
+                    onClick={async clickEvent => {
+                        clickEvent.stopPropagation()
+                        unsetModal()
+                        await closeProject()
+                    }}
+                >
+                    <svg.StopIcon className={SPINNER_CSS_CLASSES[spinnerState]} />
+                </button>
+            )
+        case backendModule.ProjectState.opened:
+            return (
+                <>
+                    <button
+                        onClick={async clickEvent => {
+                            clickEvent.stopPropagation()
+                            unsetModal()
+                            await closeProject()
+                        }}
+                    >
+                        <svg.StopIcon className={SPINNER_CSS_CLASSES[spinnerState]} />
+                    </button>
+                    <button
+                        onClick={clickEvent => {
+                            clickEvent.stopPropagation()
+                            unsetModal()
+                            openIde()
+                        }}
+                    >
+                        <img src={ArrowUpIcon} />
+                    </button>
+                </>
+            )
+    }
+}
 
 // ==========================
 // === ProjectNameHeading ===
@@ -87,7 +449,7 @@ interface InternalProjectNameProps
     extends tableColumn.TableColumnProps<
         backendModule.ProjectAsset,
         ProjectsTableState,
-        projectRowState.ProjectRowState
+        ProjectRowState
     > {}
 
 /** The icon and name of a specific project asset. */
@@ -157,7 +519,7 @@ function ProjectName(props: InternalProjectNameProps) {
                 project={item}
                 rowState={rowState}
                 setRowState={setRowState}
-                event={projectEvent}
+                projectEvent={projectEvent}
                 doOpenManually={doOpenManually}
                 appRunner={appRunner}
                 openIde={() => {
@@ -205,7 +567,7 @@ interface InternalProjectRowContextMenuProps {
     innerProps: tableRow.TableRowInnerProps<
         backendModule.ProjectAsset,
         backendModule.ProjectId,
-        projectRowState.ProjectRowState
+        ProjectRowState
     >
     event: React.MouseEvent
     dispatchProjectEvent: (projectListEvent: projectEventModule.ProjectEvent) => void
@@ -300,7 +662,7 @@ function ProjectRow(
         backendModule.ProjectAsset,
         backendModule.ProjectId,
         ProjectsTableState,
-        projectRowState.ProjectRowState
+        ProjectRowState
     >
 ) {
     const {
@@ -398,6 +760,16 @@ interface ProjectsTableState {
     doOpenIde: (project: backendModule.ProjectAsset) => void
     doCloseIde: () => void
 }
+
+/** Data associated with a project, used for rendering. */
+export interface ProjectRowState {
+    isRunning: boolean
+}
+
+/** The default {@link ProjectRowState} associated with a {@link ProjectRow}. */
+export const INITIAL_ROW_STATE: ProjectRowState = Object.freeze({
+    isRunning: false,
+})
 
 /** Props for a {@link ProjectsTable}. */
 export interface ProjectsTableProps {
@@ -601,13 +973,13 @@ function ProjectsTable(props: ProjectsTableProps) {
             backendModule.ProjectAsset,
             backendModule.ProjectId,
             ProjectsTableState,
-            projectRowState.ProjectRowState
+            ProjectRowState
         >
             rowComponent={ProjectRow}
             items={visibleItems}
             isLoading={isLoading}
             state={state}
-            initialRowState={projectRowState.INITIAL_ROW_STATE}
+            initialRowState={INITIAL_ROW_STATE}
             getKey={backendModule.getAssetId}
             placeholder={PLACEHOLDER}
             forceShowPlaceholder={shouldForceShowPlaceholder}
