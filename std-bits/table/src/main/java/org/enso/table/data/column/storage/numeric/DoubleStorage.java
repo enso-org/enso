0a9aa1adfd788795e8d1f5276de5f0e09e8b76fa
James Dunkerley <jdunkerley@users.noreply.github.com>
2023-07-10 13:05:24 +0100

    Add `is_infinite` to in-memory Column. Adjust `round` so only warns on a problem.

--- std-bits/table/src/main/java/org/enso/table/data/column/storage/numeric/DoubleStorage.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/storage/numeric/DoubleStorage.java
@@ -361,6 +361,22 @@ public final class DoubleStorage extends NumericStorage<Double> {
                 return new BoolStorage(nans, new BitSet(), storage.size, false);
               }
             })
+        .add(
+            new UnaryMapOperation<>(Maps.IS_INFINITE) {
+              @Override
+              public BoolStorage run(DoubleStorage storage) {
+                BitSet infintes = new BitSet();
+                Context context = Context.getCurrent();
+                for (int i = 0; i < storage.size; i++) {
+                  if (!storage.isNa(i) && Double.isInfinite(storage.getItem(i))) {
+                    infintes.set(i);
+                  }
+
+                  context.safepoint();
+                }
+                return new BoolStorage(infintes, new BitSet(), storage.size, false);
+              }
+            })
         .add(new DoubleIsInOp());
     return ops;
   }
