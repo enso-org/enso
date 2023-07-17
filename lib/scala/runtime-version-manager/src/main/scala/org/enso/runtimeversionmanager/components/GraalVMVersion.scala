0fc2621e9bbba8d6d3f95b235e62866703ef8194
Pavel Marek <pavel.marek@enso.org>
2023-07-17 16:51:40 +0200

    Add version requirements to GraalVMVersion

--- lib/scala/runtime-version-manager/src/main/scala/org/enso/runtimeversionmanager/components/GraalVMVersion.scala
+++ lib/scala/runtime-version-manager/src/main/scala/org/enso/runtimeversionmanager/components/GraalVMVersion.scala
@@ -1,12 +1,19 @@
 package org.enso.runtimeversionmanager.components
 
+import nl.gn0s1s.bump.SemVer
+
 /** Version information identifying the runtime that can be used with an engine
   * release.
   *
-  * @param graalVersion version of the GraalVM
-  * @param javaVersion Java version of the GraalVM flavour that should be used
+  * @param graalVersion version of the GraalVM. Can be specified as a semantic
+ *                     version.
+  * @param javaVersion Java version of the GraalVM flavour that should be used.
+ *                    Can be specified either as a single integer or as a
+ *                    semantic version
   */
 case class GraalVMVersion(graalVersion: String, javaVersion: String) {
+  require(isCorrectVersionFormat(graalVersion))
+  require(isCorrectVersionFormat(javaVersion))
 
   /** @inheritdoc
     */
@@ -19,6 +26,17 @@ case class GraalVMVersion(graalVersion: String, javaVersion: String) {
       javaVersion.split("\\.").head.toInt
     } else {
       javaVersion.toInt
+    }
+  }
+
+  private def isCorrectVersionFormat(version: String): Boolean = {
+    version.toIntOption match {
+      case Some(_) => true
+      case None =>
+        SemVer(version) match {
+          case Some(_) => true
+          case None => false
+        }
     }
   }
 }
