a9fab5fc0d8088bdec5c5355ea0f8ccb68a44d75
somebody1234 <ehern.lee@gmail.com>
2023-07-19 19:00:14 +1000

    wip

---
+++ app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/button.tsx
@@ -0,0 +1,25 @@
+/** @file A styled button. */
+import * as React from 'react'
+
+/** Props for a {@link Button}. */
+export interface ButtonProps {
+    active: boolean
+    disabled?: boolean
+    image: string
+    onClick: (event: React.MouseEvent) => void
+}
+
+/** A styled button. */
+function Button(props: ButtonProps) {
+    const { active, disabled = false, image, onClick } = props
+
+    return (
+        <img
+            className={`${active ? '' : 'opacity-30'} ${disabled ? '' : 'cursor-pointer'}`}
+            src={image}
+            onClick={onClick}
+        />
+    )
+}
+
+export default Button
