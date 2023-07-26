a389ff1499da805be377cf01278a79ac20df024f
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-07-26 11:03:36 +0200

    Compute the timedelta offset in seconds before constructing the date

--- engine/runtime/src/main/java/org/enso/interpreter/runtime/data/EnsoTimeZone.java
+++ engine/runtime/src/main/java/org/enso/interpreter/runtime/data/EnsoTimeZone.java
@@ -1,19 +1,21 @@
 package org.enso.interpreter.runtime.data;
 
+import java.time.DateTimeException;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.zone.ZoneRulesException;
+
+import org.enso.interpreter.dsl.Builtin;
+import org.enso.interpreter.runtime.EnsoContext;
+import org.enso.interpreter.runtime.data.text.Text;
+import org.enso.interpreter.runtime.library.dispatch.TypesLibrary;
+
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
-import java.time.DateTimeException;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.zone.ZoneRulesException;
-import org.enso.interpreter.dsl.Builtin;
-import org.enso.interpreter.runtime.EnsoContext;
-import org.enso.interpreter.runtime.data.text.Text;
-import org.enso.interpreter.runtime.library.dispatch.TypesLibrary;
 
 @ExportLibrary(InteropLibrary.class)
 @ExportLibrary(TypesLibrary.class)
@@ -101,5 +103,10 @@ public final class EnsoTimeZone implements TruffleObject {
   @ExportMessage
   Type getType(@CachedLibrary("this") TypesLibrary thisLib) {
     return EnsoContext.get(thisLib).getBuiltins().timeZone();
+  }
+
+  @ExportMessage
+  String toDisplayString(boolean ignore) {
+    return zone.toString();
   }
 }
