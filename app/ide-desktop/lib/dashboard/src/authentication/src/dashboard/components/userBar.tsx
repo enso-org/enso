ece18537e449b7e39b6ce5b2e04d28ad5d0ad043
somebody1234 <ehern.lee@gmail.com>
2023-08-01 19:32:32 +1000

    New top bar (#7392)
    * wip
    
    * wip
    
    * New backend switcher
    
    * New user bar (user and chat buttons)
    
    * Page switcher
    
    * New search bar; minor style fixes
    
    * Address QA
    
    * Refactor HTML `button`s into `Button` component
    
    * Add cloud color back to Tailwind
    
    * Fix icons shrinking
    
    * Fix bug
    
    ---------
    
    Co-authored-by: Paweł Buchowski <pawel.buchowski@enso.org>

---
+++ app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/userBar.tsx
@@ -0,0 +1,45 @@
+/** @file A toolbar containing chat and the user menu. */
+import * as React from 'react'
+
+import ChatIcon from 'enso-assets/chat.svg'
+import DefaultUserIcon from 'enso-assets/default_user.svg'
+
+import * as modalProvider from '../../providers/modal'
+
+import Button from './button'
+import UserMenu from './userMenu'
+
+// ===============
+// === UserBar ===
+// ===============
+
+/** Props for a {@link UserBar}. */
+export interface UserBarProps {
+    isHelpChatOpen: boolean
+    setIsHelpChatOpen: (isHelpChatOpen: boolean) => void
+}
+
+/** A toolbar containing chat and the user menu. */
+export default function UserBar(props: UserBarProps) {
+    const { isHelpChatOpen, setIsHelpChatOpen } = props
+    const { updateModal } = modalProvider.useSetModal()
+    return (
+        <div className="flex shrink-0 items-center bg-frame-bg rounded-full gap-3 h-8 pl-2 pr-0.75">
+            <Button
+                active={isHelpChatOpen}
+                image={ChatIcon}
+                onClick={() => {
+                    setIsHelpChatOpen(!isHelpChatOpen)
+                }}
+            />
+            <button
+                onClick={event => {
+                    event.stopPropagation()
+                    updateModal(oldModal => (oldModal?.type === UserMenu ? null : <UserMenu />))
+                }}
+            >
+                <img src={DefaultUserIcon} height={28} width={28} />
+            </button>
+        </div>
+    )
+}
