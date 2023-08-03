dc9f5ce40faa9b8c1f63bc0579167b0aaab97263
Pavel Marek <pavel.marek@enso.org>
2023-08-03 18:05:53 +0200

    Add some tests

---
+++ engine/interpreter-dsl-test/src/test/java/org/enso/interpreter/dsl/test/ThrowBuiltinNode.java
@@ -0,0 +1,17 @@
+package org.enso.interpreter.dsl.test;
+
+import com.oracle.truffle.api.nodes.Node;
+import java.util.List;
+import java.util.function.Supplier;
+import org.enso.interpreter.dsl.BuiltinMethod;
+
+@BuiltinMethod(
+    type = "ThrowBuiltin",
+    name = "throw"
+)
+public class ThrowBuiltinNode extends Node {
+  public Object execute(long exceptionIdx) throws Throwable {
+    Supplier<Throwable> exceptionSupplier = ThrowableCatchTest.exceptionSuppliers.get((int) exceptionIdx);
+    throw exceptionSupplier.get();
+  }
+}
