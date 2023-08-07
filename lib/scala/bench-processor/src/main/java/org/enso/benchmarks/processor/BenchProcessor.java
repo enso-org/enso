3f623bde447797d14dc838b48aaa5236f7706183
Pavel Marek <pavel.marek@enso.org>
2023-08-07 18:27:41 +0200

    Ensure BenchProcessor generates unique Java method names

--- lib/scala/bench-processor/src/main/java/org/enso/benchmarks/processor/BenchProcessor.java
+++ lib/scala/bench-processor/src/main/java/org/enso/benchmarks/processor/BenchProcessor.java
@@ -315,6 +315,8 @@ public class BenchProcessor extends AbstractProcessor {
     for (char c : name.toCharArray()) {
       if (isValidChar(c)) {
         normalizedNameSb.append(c);
+      } else if (c == '-') {
+        normalizedNameSb.append("minus");
       } else if (Character.isWhitespace(c) && (peekLastChar(normalizedNameSb) != '_')) {
         normalizedNameSb.append('_');
       }
