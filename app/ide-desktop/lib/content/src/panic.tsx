83c7d17644cf28cf5fe320568d3af706f8d83386
Paweł Grabarz <frizi09@gmail.com>
2023-08-08 13:59:11 +0200

    prettier

--- app/ide-desktop/lib/content/src/panic.tsx
+++ app/ide-desktop/lib/content/src/panic.tsx
@@ -14,9 +14,10 @@ interface PanicMessageProps {
 
 /** A component displaying panic message inside a toast. */
 export function PanicMessage(props: PanicMessageProps) {
-    let reportUrl = "https://github.com/enso-org/enso/issues/new" + 
-                    "?labels=--bug%2Ctriage" +
-                    "&template=bug-report.yml";
+    let reportUrl =
+        'https://github.com/enso-org/enso/issues/new' +
+        '?labels=--bug%2Ctriage' +
+        '&template=bug-report.yml'
     return (
         <div className="flex flex-col">
             <h1 className="text-xl font-semibold">Enso has crashed.</h1>
@@ -27,18 +28,18 @@ export function PanicMessage(props: PanicMessageProps) {
             <p>Please include following panic message in your report:</p>
             <pre
                 className={
-                    "overflow-auto mt-2 p-2 bg-gray-200 text-gray-800 border rounded " +
-                    "border-gray-400"
+                    'overflow-auto mt-2 p-2 bg-gray-200 text-gray-800 border rounded ' +
+                    'border-gray-400'
                 }
-                style={{ maxHeight: "50vh" }}
+                style={{ maxHeight: '50vh' }}
             >
                 {props.message}
             </pre>
             <div className="flex flex-row mt-2 gap-2 justify-end">
                 <button
                     className={
-                        "text-sm border text-gray-800 bg-gray-100 hover:bg-gray-200 rounded p-2 " +
-                        "transition"
+                        'text-sm border text-gray-800 bg-gray-100 hover:bg-gray-200 rounded p-2 ' +
+                        'transition'
                     }
                     type="submit"
                     onClick={props.restart}
@@ -50,8 +51,8 @@ export function PanicMessage(props: PanicMessageProps) {
                     rel="noreferrer"
                     href={reportUrl}
                     className={
-                        "text-sm border text-white bg-indigo-600 hover:bg-indigo-700 rounded p-2 " +
-                        "transition"
+                        'text-sm border text-white bg-indigo-600 hover:bg-indigo-700 rounded p-2 ' +
+                        'transition'
                     }
                 >
                     Report
@@ -82,7 +83,7 @@ export async function displayPanicMessageToast(message: string, restartApp: () =
             bodyStyle: {
                 alignItems: 'flex-start',
                 overflow: 'hidden',
-            }
+            },
         })
     })
 }
