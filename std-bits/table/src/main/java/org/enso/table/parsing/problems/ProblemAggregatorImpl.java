b656b336c7d239ef95a548f87deb5af2f4801368
Radosław Waśko <radoslaw.wasko@enso.org>
2023-08-08 14:30:44 +0200

    Report `Loss_Of_Integer_Precision` when an integer is not exactly representable as a float during conversion (#7509)
    Closes #7353
    
    I introduce a new type `WithAggregatedProblems`, because `WithProblems` was too simple - it only allowed to hold a `List<Problem>` but `AggregatedProblems` is more than that. Ideally we shouldn't multiply entities like this too much. We should probably unify all to use `WithAggregatedProblems` - but after starting this, I realised it will likely just take too much effort to do for this little PR. So instead, I created a follow-up task for this: #7514

--- std-bits/table/src/main/java/org/enso/table/parsing/problems/ProblemAggregatorImpl.java
+++ std-bits/table/src/main/java/org/enso/table/parsing/problems/ProblemAggregatorImpl.java
@@ -2,12 +2,11 @@ package org.enso.table.parsing.problems;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.enso.table.problems.Problem;
+import org.enso.table.problems.AggregatedProblems;
 
 public class ProblemAggregatorImpl implements ProblemAggregator {
   public final String relatedColumnName;
   private final List<String> invalidFormatCells = new ArrayList<>();
-  private final List<String> leadingZerosCells = new ArrayList<>();
 
   public ProblemAggregatorImpl(String relatedColumnName) {
     this.relatedColumnName = relatedColumnName;
@@ -25,18 +24,16 @@ public class ProblemAggregatorImpl implements ProblemAggregator {
 
   @Override
   public boolean hasProblems() {
-    return !invalidFormatCells.isEmpty() || !leadingZerosCells.isEmpty();
+    return !invalidFormatCells.isEmpty();
   }
 
   @Override
-  public List<Problem> getAggregatedProblems() {
-    List<Problem> problems = new ArrayList<>();
+  public AggregatedProblems getAggregatedProblems() {
+    AggregatedProblems problems = new AggregatedProblems();
 
     if (!invalidFormatCells.isEmpty()) {
       problems.add(new InvalidFormat(relatedColumnName, invalidFormatCells));
     }
-
-    assert problems.isEmpty() == !hasProblems();
 
     return problems;
   }
