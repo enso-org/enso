59329bd59aeacc4f60ccc4ef323dfaf6629c7710
Adam Obuchowicz <adam.obuchowicz@enso.org>
2023-08-08 17:23:41 +0200

    New Top Bar (#7488)
    Fixes #7411
    
    So far, this branch removes window control buttons and go-to dashboard (hamburger icon), and adds option for dashboard to set offset of the rest of top bar panels.

--- app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/topBar.tsx
+++ app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/topBar.tsx
@@ -28,6 +28,7 @@ export interface TopBarProps {
     setIsHelpChatOpen: (isHelpChatOpen: boolean) => void
     query: string
     setQuery: (value: string) => void
+    onSignOut: () => void
 }
 
 /** The {@link TopBarProps.setQuery} parameter is used to communicate with the parent component,
@@ -44,33 +45,44 @@ export default function TopBar(props: TopBarProps) {
         setIsHelpChatOpen,
         query,
         setQuery,
+        onSignOut,
     } = props
 
     return (
-        <div className="relative flex ml-4.75 mr-2.25 mt-2.25 h-8 gap-6">
+        <div className="relative flex ml-4.75 mr-2.25 mt-2.25 h-8 gap-6 z-10">
             <PageSwitcher page={page} setPage={setPage} isEditorDisabled={isEditorDisabled} />
-            {supportsLocalBackend && <BackendSwitcher setBackendType={setBackendType} />}
+            {supportsLocalBackend && page !== pageSwitcher.Page.editor && (
+                <BackendSwitcher setBackendType={setBackendType} />
+            )}
             <div className="grow" />
-            <div className="search-bar absolute flex items-center text-primary bg-frame-bg rounded-full -translate-x-1/2 gap-2.5 left-1/2 h-8 w-98.25 px-2">
-                <label htmlFor="search">
-                    <img src={FindIcon} className="opacity-80" />
-                </label>
-                <input
-                    type="text"
-                    size={1}
-                    id="search"
-                    placeholder="Type to search for projects, data connectors, users, and more."
-                    value={query}
-                    onChange={event => {
-                        setQuery(event.target.value)
-                    }}
-                    className="grow bg-transparent leading-5 h-6 py-px"
-                />
-            </div>
-            <div className="grow" />
+            {page !== pageSwitcher.Page.editor && (
+                <>
+                    <div className="search-bar absolute flex items-center text-primary bg-frame-bg rounded-full -translate-x-1/2 gap-2.5 left-1/2 h-8 w-98.25 px-2">
+                        <label htmlFor="search">
+                            <img src={FindIcon} className="opacity-80" />
+                        </label>
+                        <input
+                            type="text"
+                            size={1}
+                            id="search"
+                            placeholder="Type to search for projects, data connectors, users, and more."
+                            value={query}
+                            onChange={event => {
+                                setQuery(event.target.value)
+                            }}
+                            className="grow bg-transparent leading-5 h-6 py-px"
+                        />
+                    </div>
+                    <div className="grow" />
+                </>
+            )}
             <div className="flex gap-2">
                 <AssetInfoBar asset={asset} />
-                <UserBar isHelpChatOpen={isHelpChatOpen} setIsHelpChatOpen={setIsHelpChatOpen} />
+                <UserBar
+                    isHelpChatOpen={isHelpChatOpen}
+                    setIsHelpChatOpen={setIsHelpChatOpen}
+                    onSignOut={onSignOut}
+                />
             </div>
         </div>
     )
