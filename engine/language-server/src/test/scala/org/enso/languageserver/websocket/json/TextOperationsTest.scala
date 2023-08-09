74c4c74fae188383999d8598458fd1041bb5ab5f
Dmitry Bushev <bushevdv@gmail.com>
2023-08-09 17:41:43 +0300

    test: did change notifiation

--- engine/language-server/src/test/scala/org/enso/languageserver/websocket/json/TextOperationsTest.scala
+++ engine/language-server/src/test/scala/org/enso/languageserver/websocket/json/TextOperationsTest.scala
@@ -5,8 +5,11 @@ import io.circe.literal._
 import org.enso.languageserver.event.{BufferClosed, JsonSessionTerminated}
 import org.enso.languageserver.filemanager.Path
 import org.enso.languageserver.session.JsonSession
+import org.enso.polyglot.runtime.Runtime.Api
 import org.enso.testkit.FlakySpec
+import org.enso.text.editing.model
 
+import java.io.File
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 
@@ -15,6 +18,387 @@ import scala.concurrent.duration._
 class TextOperationsTest extends BaseServerTest with FlakySpec {
 
   override def isFileWatcherEnabled = true
+
+  "BufferRegistry" must {
+    "grant the canEdit capability if no one else holds it" in {
+      // Interaction:
+      // 1. Client 1 creates a file.
+      // 2. Client 1 receives confirmation.
+      // 3. Client 1 opens the created file.
+      // 4. Client 1 receives the file contents and a canEdit capability.
+      // 5. Client 1 releases the canEdit capability.
+      // 6. Client 1 receives a confirmation.
+      // 7. Client 2 opens the file.
+      // 8. Client 2 receives the file contents and a canEdit capability.
+      val client1 = getInitialisedWsClient()
+      val client2 = getInitialisedWsClient()
+      // 1
+      client1.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "file/write",
+            "id": 0,
+            "params": {
+              "path": {
+                "rootId": $testContentRootId,
+                "segments": [ "grant_can_edit.txt" ]
+              },
+              "contents": "123456789"
+            }
+          }
+          """)
+
+      // 2
+      client1.expectJson(json"""
+          { "jsonrpc": "2.0",
+            "id": 0,
+            "result": null
+          }
+          """)
+
+      // 3
+      client1.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "text/openFile",
+            "id": 1,
+            "params": {
+              "path": {
+                "rootId": $testContentRootId,
+                "segments": [ "grant_can_edit.txt" ]
+              }
+            }
+          }
+          """)
+
+      // 4
+      client1.expectJson(json"""
+          { "jsonrpc": "2.0",
+            "id": 1,
+            "result": {
+              "writeCapability": {
+                "method": "text/canEdit",
+                "registerOptions": { "path": {
+                  "rootId": $testContentRootId,
+                  "segments": ["grant_can_edit.txt"]
+                } }
+              },
+              "content": "123456789",
+              "currentVersion": "5795c3d628fd638c9835a4c79a55809f265068c88729a1a3fcdf8522"
+            }
+          }
+          """)
+
+      // 5
+      client1.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "capability/release",
+            "id": 2,
+            "params": {
+              "method": "text/canEdit",
+                "registerOptions": { "path": {
+                  "rootId": $testContentRootId,
+                  "segments": ["grant_can_edit.txt"]
+                } }
+            }
+          }
+          """)
+
+      // 6
+      client1.expectJson(json"""
+          { "jsonrpc": "2.0",
+            "id": 2,
+            "result": null
+          }
+          """)
+
+      // 7
+      client2.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "text/openFile",
+            "id": 3,
+            "params": {
+              "path": {
+                "rootId": $testContentRootId,
+                "segments": [ "grant_can_edit.txt" ]
+              }
+            }
+          }
+          """)
+
+      // 8
+      client2.expectJson(json"""
+          { "jsonrpc": "2.0",
+            "id": 3,
+            "result": {
+              "writeCapability": {
+                "method": "text/canEdit",
+                "registerOptions": { "path": {
+                  "rootId": $testContentRootId,
+                  "segments": ["grant_can_edit.txt"]
+                } }
+              },
+              "content": "123456789",
+              "currentVersion": "5795c3d628fd638c9835a4c79a55809f265068c88729a1a3fcdf8522"
+            }
+          }
+          """)
+    }
+
+    "take canEdit capability away from clients when another client registers for it" in {
+      val client1 = getInitialisedWsClient()
+      val client2 = getInitialisedWsClient()
+      val client3 = getInitialisedWsClient()
+      client1.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "file/write",
+            "id": 0,
+            "params": {
+              "path": {
+                "rootId": $testContentRootId,
+                "segments": [ "take_can_edit.txt" ]
+              },
+              "contents": "123456789"
+            }
+          }
+          """)
+
+      client1.expectJson(json"""
+          { "jsonrpc": "2.0",
+            "id": 0,
+            "result": null
+          }
+          """)
+
+      client1.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "text/openFile",
+            "id": 1,
+            "params": {
+              "path": {
+                "rootId": $testContentRootId,
+                "segments": [ "take_can_edit.txt" ]
+              }
+            }
+          }
+          """)
+
+      client1.expectJson(json"""
+          { "jsonrpc": "2.0",
+            "id": 1,
+            "result": {
+              "writeCapability": {
+                "method": "text/canEdit",
+                "registerOptions": { "path": {
+                  "rootId": $testContentRootId,
+                  "segments": ["take_can_edit.txt"]
+                } }
+              },
+              "content": "123456789",
+              "currentVersion": "5795c3d628fd638c9835a4c79a55809f265068c88729a1a3fcdf8522"
+            }
+          }
+          """)
+      client2.expectNoMessage()
+      client3.expectNoMessage()
+
+      client2.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "capability/acquire",
+            "id": 2,
+            "params": {
+              "method": "text/canEdit",
+              "registerOptions": {
+                "path": {
+                  "rootId": $testContentRootId,
+                  "segments": [ "take_can_edit.txt" ]
+                }
+              }
+            }
+          }
+          """)
+
+      client1.expectJson(json"""
+          { "jsonrpc": "2.0",
+            "method": "capability/forceReleased",
+            "params": {
+              "method": "text/canEdit",
+              "registerOptions": {
+                "path": {
+                  "rootId": $testContentRootId,
+                  "segments": [ "take_can_edit.txt" ]
+                }
+              }
+            }
+          }
+          """)
+      client2.expectJson(json"""
+          { "jsonrpc": "2.0",
+            "id": 2,
+            "result": null
+          }
+          """)
+      client3.expectNoMessage()
+
+      client3.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "capability/acquire",
+            "id": 3,
+            "params": {
+              "method": "text/canEdit",
+              "registerOptions": {
+                "path": {
+                  "rootId": $testContentRootId,
+                  "segments": [ "take_can_edit.txt" ]
+                }
+              }
+            }
+          }
+          """)
+
+      client1.expectNoMessage()
+      client2.expectJson(json"""
+          { "jsonrpc": "2.0",
+            "method": "capability/forceReleased",
+            "params": {
+              "method": "text/canEdit",
+              "registerOptions": {
+                "path": {
+                  "rootId": $testContentRootId,
+                  "segments": [ "take_can_edit.txt" ]
+                }
+              }
+            }
+          }
+          """)
+      client3.expectJson(json"""
+          { "jsonrpc": "2.0",
+            "id": 3,
+            "result": null
+          }
+          """)
+    }
+
+    "apply refactoring changes" in {
+      val client = getInitialisedWsClient()
+      client.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "file/write",
+            "id": 0,
+            "params": {
+              "path": {
+                "rootId": $testContentRootId,
+                "segments": [ "to_refactor.txt" ]
+              },
+              "contents": "123456789"
+            }
+          }
+          """)
+      client.expectJson(json"""
+          { "jsonrpc": "2.0",
+            "id": 0,
+            "result": null
+          }
+          """)
+      client.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "text/openFile",
+            "id": 1,
+            "params": {
+              "path": {
+                "rootId": $testContentRootId,
+                "segments": [ "to_refactor.txt" ]
+              }
+            }
+          }
+          """)
+      client.expectJson(json"""
+          {
+            "jsonrpc" : "2.0",
+            "id" : 1,
+            "result" : {
+              "writeCapability" : {
+                "method" : "text/canEdit",
+                "registerOptions" : {
+                  "path" : {
+                    "rootId" : $testContentRootId,
+                    "segments" : [
+                      "to_refactor.txt"
+                    ]
+                  }
+                }
+              },
+              "content" : "123456789",
+              "currentVersion" : "5795c3d628fd638c9835a4c79a55809f265068c88729a1a3fcdf8522"
+            }
+          }
+          """)
+
+      val textEdits = Vector(
+        model.TextEdit(
+          model.Range(model.Position(0, 0), model.Position(0, 0)),
+          "bar"
+        ),
+        model.TextEdit(
+          model.Range(model.Position(0, 12), model.Position(0, 12)),
+          "foo"
+        )
+      )
+      val fileEdit = Api.FileEdit(
+        new File(testContentRoot.file, "to_refactor.txt"),
+        textEdits,
+        oldVersion = "5795c3d628fd638c9835a4c79a55809f265068c88729a1a3fcdf8522",
+        newVersion = "ebe55342f9c8b86857402797dd723fb4a2174e0b56d6ace0a6929ec3"
+      )
+      system.eventStream.publish(fileEdit)
+
+      client.expectJson(json"""
+          { "jsonrpc" : "2.0",
+            "method" : "text/didChange",
+            "params" : {
+              "edits" : [
+                {
+                  "path" : {
+                    "rootId" : $testContentRootId,
+                    "segments" : [
+                      "to_refactor.txt"
+                    ]
+                  },
+                  "edits" : [
+                    {
+                      "range" : {
+                        "start" : {
+                          "line" : 0,
+                          "character" : 0
+                        },
+                        "end" : {
+                          "line" : 0,
+                          "character" : 0
+                        }
+                      },
+                      "text" : "bar"
+                    },
+                    {
+                      "range" : {
+                        "start" : {
+                          "line" : 0,
+                          "character" : 12
+                        },
+                        "end" : {
+                          "line" : 0,
+                          "character" : 12
+                        }
+                      },
+                      "text" : "foo"
+                    }
+                  ],
+                  "oldVersion" : "5795c3d628fd638c9835a4c79a55809f265068c88729a1a3fcdf8522",
+                  "newVersion" : "ebe55342f9c8b86857402797dd723fb4a2174e0b56d6ace0a6929ec3"
+                }
+              ]
+            }
+          }""")
+    }
+
+  }
 
   "text/openFile" must {
     "fail opening a file if it does not exist" taggedAs Flaky in {
@@ -292,263 +676,6 @@ class TextOperationsTest extends BaseServerTest with FlakySpec {
           }
           """)
     }
-  }
-
-  "grant the canEdit capability if no one else holds it" in {
-    // Interaction:
-    // 1. Client 1 creates a file.
-    // 2. Client 1 receives confirmation.
-    // 3. Client 1 opens the created file.
-    // 4. Client 1 receives the file contents and a canEdit capability.
-    // 5. Client 1 releases the canEdit capability.
-    // 6. Client 1 receives a confirmation.
-    // 7. Client 2 opens the file.
-    // 8. Client 2 receives the file contents and a canEdit capability.
-    val client1 = getInitialisedWsClient()
-    val client2 = getInitialisedWsClient()
-    // 1
-    client1.send(json"""
-          { "jsonrpc": "2.0",
-            "method": "file/write",
-            "id": 0,
-            "params": {
-              "path": {
-                "rootId": $testContentRootId,
-                "segments": [ "foo.txt" ]
-              },
-              "contents": "123456789"
-            }
-          }
-          """)
-
-    // 2
-    client1.expectJson(json"""
-          { "jsonrpc": "2.0",
-            "id": 0,
-            "result": null
-          }
-          """)
-
-    // 3
-    client1.send(json"""
-          { "jsonrpc": "2.0",
-            "method": "text/openFile",
-            "id": 1,
-            "params": {
-              "path": {
-                "rootId": $testContentRootId,
-                "segments": [ "foo.txt" ]
-              }
-            }
-          }
-          """)
-
-    // 4
-    client1.expectJson(json"""
-          { "jsonrpc": "2.0",
-            "id": 1,
-            "result": {
-              "writeCapability": {
-                "method": "text/canEdit",
-                "registerOptions": { "path": {
-                  "rootId": $testContentRootId,
-                  "segments": ["foo.txt"]
-                } }
-              },
-              "content": "123456789",
-              "currentVersion": "5795c3d628fd638c9835a4c79a55809f265068c88729a1a3fcdf8522"
-            }
-          }
-          """)
-
-    // 5
-    client1.send(json"""
-          { "jsonrpc": "2.0",
-            "method": "capability/release",
-            "id": 2,
-            "params": {
-              "method": "text/canEdit",
-                "registerOptions": { "path": {
-                  "rootId": $testContentRootId,
-                  "segments": ["foo.txt"]
-                } }
-            }
-          }
-          """)
-
-    // 6
-    client1.expectJson(json"""
-          { "jsonrpc": "2.0",
-            "id": 2,
-            "result": null
-          }
-          """)
-
-    // 7
-    client2.send(json"""
-          { "jsonrpc": "2.0",
-            "method": "text/openFile",
-            "id": 3,
-            "params": {
-              "path": {
-                "rootId": $testContentRootId,
-                "segments": [ "foo.txt" ]
-              }
-            }
-          }
-          """)
-
-    // 8
-    client2.expectJson(json"""
-          { "jsonrpc": "2.0",
-            "id": 3,
-            "result": {
-              "writeCapability": {
-                "method": "text/canEdit",
-                "registerOptions": { "path": {
-                  "rootId": $testContentRootId,
-                  "segments": ["foo.txt"]
-                } }
-              },
-              "content": "123456789",
-              "currentVersion": "5795c3d628fd638c9835a4c79a55809f265068c88729a1a3fcdf8522"
-            }
-          }
-          """)
-  }
-
-  "take canEdit capability away from clients when another client registers for it" in {
-    val client1 = getInitialisedWsClient()
-    val client2 = getInitialisedWsClient()
-    val client3 = getInitialisedWsClient()
-    client1.send(json"""
-          { "jsonrpc": "2.0",
-            "method": "file/write",
-            "id": 0,
-            "params": {
-              "path": {
-                "rootId": $testContentRootId,
-                "segments": [ "foo.txt" ]
-              },
-              "contents": "123456789"
-            }
-          }
-          """)
-
-    client1.expectJson(json"""
-          { "jsonrpc": "2.0",
-            "id": 0,
-            "result": null
-          }
-          """)
-
-    client1.send(json"""
-          { "jsonrpc": "2.0",
-            "method": "text/openFile",
-            "id": 1,
-            "params": {
-              "path": {
-                "rootId": $testContentRootId,
-                "segments": [ "foo.txt" ]
-              }
-            }
-          }
-          """)
-
-    client1.expectJson(json"""
-          { "jsonrpc": "2.0",
-            "id": 1,
-            "result": {
-              "writeCapability": {
-                "method": "text/canEdit",
-                "registerOptions": { "path": {
-                  "rootId": $testContentRootId,
-                  "segments": ["foo.txt"]
-                } }
-              },
-              "content": "123456789",
-              "currentVersion": "5795c3d628fd638c9835a4c79a55809f265068c88729a1a3fcdf8522"
-            }
-          }
-          """)
-    client2.expectNoMessage()
-    client3.expectNoMessage()
-
-    client2.send(json"""
-          { "jsonrpc": "2.0",
-            "method": "capability/acquire",
-            "id": 2,
-            "params": {
-              "method": "text/canEdit",
-              "registerOptions": {
-                "path": {
-                  "rootId": $testContentRootId,
-                  "segments": [ "foo.txt" ]
-                }
-              }
-            }
-          }
-          """)
-
-    client1.expectJson(json"""
-          { "jsonrpc": "2.0",
-            "method": "capability/forceReleased",
-            "params": {
-              "method": "text/canEdit",
-              "registerOptions": {
-                "path": {
-                  "rootId": $testContentRootId,
-                  "segments": [ "foo.txt" ]
-                }
-              }
-            }
-          }
-          """)
-    client2.expectJson(json"""
-          { "jsonrpc": "2.0",
-            "id": 2,
-            "result": null
-          }
-          """)
-    client3.expectNoMessage()
-
-    client3.send(json"""
-          { "jsonrpc": "2.0",
-            "method": "capability/acquire",
-            "id": 3,
-            "params": {
-              "method": "text/canEdit",
-              "registerOptions": {
-                "path": {
-                  "rootId": $testContentRootId,
-                  "segments": [ "foo.txt" ]
-                }
-              }
-            }
-          }
-          """)
-
-    client1.expectNoMessage()
-    client2.expectJson(json"""
-          { "jsonrpc": "2.0",
-            "method": "capability/forceReleased",
-            "params": {
-              "method": "text/canEdit",
-              "registerOptions": {
-                "path": {
-                  "rootId": $testContentRootId,
-                  "segments": [ "foo.txt" ]
-                }
-              }
-            }
-          }
-          """)
-    client3.expectJson(json"""
-          { "jsonrpc": "2.0",
-            "id": 3,
-            "result": null
-          }
-          """)
   }
 
   "text/closeFile" must {
