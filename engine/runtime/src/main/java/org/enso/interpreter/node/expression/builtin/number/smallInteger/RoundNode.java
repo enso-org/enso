5fbeb1ab76b1c0b47755255db0825d53a104b6aa
Gregory Travis <greg.travis@enso.org>
2023-08-10 17:48:39 -0400

    remove old, restore bench

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/number/smallInteger/RoundNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/number/smallInteger/RoundNode.java
@@ -8,47 +8,47 @@ import org.enso.interpreter.dsl.BuiltinMethod;
 import org.enso.interpreter.node.expression.builtin.number.utils.RoundHelpers;
 
 @BuiltinMethod(
-        type = "Integer",
-        name = "round",
-        description = "Decimal ceiling, converting to a small or big integer depending on size.")
+    type = "Integer",
+    name = "round",
+    description = "Decimal ceiling, converting to a small or big integer depending on size.")
 public class RoundNode extends Node {
-    private final CountingConditionProfile fitsProfile = CountingConditionProfile.create();
+  private final CountingConditionProfile fitsProfile = CountingConditionProfile.create();
 
-    private final PrimitiveValueProfile constantPlacesDecimalPlaces = PrimitiveValueProfile.create();
+  private final PrimitiveValueProfile constantPlacesDecimalPlaces = PrimitiveValueProfile.create();
 
-    private final PrimitiveValueProfile constantPlacesUseBankers = PrimitiveValueProfile.create();
+  private final PrimitiveValueProfile constantPlacesUseBankers = PrimitiveValueProfile.create();
 
-    private final BranchProfile decimalPlacesOutOfRangeProfile = BranchProfile.create();
+  private final BranchProfile decimalPlacesOutOfRangeProfile = BranchProfile.create();
 
-    Object execute(long n, long dp, boolean ub) {
-        var decimalPlaces = constantPlacesDecimalPlaces.profile(dp);
+  Object execute(long n, long dp, boolean ub) {
+    var decimalPlaces = constantPlacesDecimalPlaces.profile(dp);
 
-        // We don't check if `n` is out of range here, since the Enso wrapper does.
-        if (decimalPlaces < RoundHelpers.ROUND_MIN_DECIMAL_PLACES || decimalPlaces > RoundHelpers.ROUND_MAX_DECIMAL_PLACES) {
-            decimalPlacesOutOfRangeProfile.enter();
-            RoundHelpers.decimalPlacesOutOfRangePanic(this, decimalPlaces);
-        }
-
-        if (decimalPlaces >= 0) {
-            return n;
-        }
-
-        var useBankers = constantPlacesUseBankers.profile(ub);
-        long scale = (long) Math.pow(10, -decimalPlaces);
-        long halfway = scale / 2;
-        long remainder = n % scale;
-        long scaledDown = n / scale;
-        long resultUnnudged = scaledDown * scale;
-
-
-        if (n >= 0) {
-            boolean halfGoesUp = useBankers ? (scaledDown % 2) != 0 : true;
-            boolean roundUp = halfGoesUp ? remainder >= halfway : remainder > halfway;
-            return roundUp ? resultUnnudged + scale : resultUnnudged;
-        } else {
-            boolean halfGoesUp = useBankers ? (scaledDown % 2) == 0 : false;
-            boolean roundUp = halfGoesUp ? remainder < -halfway : remainder <= -halfway;
-            return roundUp ? resultUnnudged - scale : resultUnnudged;
-        }
+    // We don't check if `n` is out of range here, since the Enso wrapper does.
+    if (decimalPlaces < RoundHelpers.ROUND_MIN_DECIMAL_PLACES
+        || decimalPlaces > RoundHelpers.ROUND_MAX_DECIMAL_PLACES) {
+      decimalPlacesOutOfRangeProfile.enter();
+      RoundHelpers.decimalPlacesOutOfRangePanic(this, decimalPlaces);
     }
+
+    if (decimalPlaces >= 0) {
+      return n;
+    }
+
+    var useBankers = constantPlacesUseBankers.profile(ub);
+    long scale = (long) Math.pow(10, -decimalPlaces);
+    long halfway = scale / 2;
+    long remainder = n % scale;
+    long scaledDown = n / scale;
+    long resultUnnudged = scaledDown * scale;
+
+    if (n >= 0) {
+      boolean halfGoesUp = useBankers ? (scaledDown % 2) != 0 : true;
+      boolean roundUp = halfGoesUp ? remainder >= halfway : remainder > halfway;
+      return roundUp ? resultUnnudged + scale : resultUnnudged;
+    } else {
+      boolean halfGoesUp = useBankers ? (scaledDown % 2) == 0 : false;
+      boolean roundUp = halfGoesUp ? remainder < -halfway : remainder <= -halfway;
+      return roundUp ? resultUnnudged - scale : resultUnnudged;
+    }
+  }
 }
