685580a60eef7d1a8e0fec9cf7101001a01ad417
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-08-14 14:20:32 +0200

    Turning ArrayBuilder into a builtin

--- engine/runtime/src/main/java/org/enso/interpreter/runtime/data/vector/ArrayLikeHelpers.java
+++ engine/runtime/src/main/java/org/enso/interpreter/runtime/data/vector/ArrayLikeHelpers.java
@@ -82,7 +82,7 @@ public final class ArrayLikeHelpers {
       @CachedLibrary(limit="3") WarningsLibrary warnings
     ) {
     var len = Math.toIntExact(length);
-    var target = new Array_Builder(len);
+    var target = ArrayBuilder.newBuilder(len);
     boolean nonTrivialEnsoValue = false;
     for (int i = 0; i < len; i++) {
       var value = invokeFunctionNode.execute(fun, frame, state, new Long[] {(long) i});
@@ -124,6 +124,13 @@ public final class ArrayLikeHelpers {
     }
   }
 
+  @Builtin.Method(
+      name = "new_vector_builder",
+      description = "Returns new vector builder.")
+  public static Object newVectorBuilder(long capacity) {
+    return ArrayBuilder.newBuilder((int)Math.min(Math.abs(capacity),Integer.MAX_VALUE));
+  }
+
   public static EnsoObject wrapBuffer(ByteBuffer buffer) {
     return ArrayOverBuffer.wrapBuffer(buffer);
   }
@@ -151,136 +158,4 @@ public final class ArrayLikeHelpers {
   public static EnsoObject asVectorFromArray(Object storage) {
     return Vector.fromInteropArray(storage);
   }
-
-  private static final class Array_Builder<T> {
-    private static final Object[] EMPTY_ARRAY = new Object[0];
-    private final int initialCapacity;
-    private int size;
-    private Object primitiveArray;
-    private Object[] objectArray;
-
-    private Array_Builder(int initialCapacity) {
-      this.initialCapacity = Math.max(1, initialCapacity);
-    }
-
-    /**
-     * This function forces the polyglot conversion of an Enso array into a `byte[]`. This allows for
-     * asserting that it is a valid `byte[]`.
-     *
-     * @param input the converted array.
-     * @return the `input` unchanged.
-     */
-    public static byte[] ensureByteArray(byte[] input) {
-      return input;
-    }
-
-    /** Creates new builder */
-    public static <V> Array_Builder<V> newBuilder(int capacity) {
-      return new Array_Builder<>(capacity);
-    }
-
-    /** Is the builder empty? */
-    public boolean isEmpty() {
-      return size == 0;
-    }
-
-    /**
-     * Adds an element to the builder
-     *
-     * @param e the element to add
-     */
-    @CompilerDirectives.TruffleBoundary
-    public void add(T e) {
-      if (objectArray != null) {
-          if (size == objectArray.length) {
-              objectArray = Arrays.copyOf(objectArray, size * 2);
-          }
-          objectArray[size++] = e;
-      } else if (primitiveArray instanceof long[] longArray) {
-          if (e instanceof Long l) {
-              if (size == longArray.length) {
-                  primitiveArray = longArray = Arrays.copyOf(longArray, size * 2);
-              }
-              longArray[size++] = l;
-          } else {
-              objectArray = new Object[longArray.length];
-              for (int i = 0; i < size; i++) {
-                  objectArray[i] = longArray[i];
-              }
-              primitiveArray = null;
-              add(e);
-          }
-      } else if (primitiveArray instanceof double[] doubleArray) {
-          if (e instanceof Double d) {
-              if (size == doubleArray.length) {
-                  primitiveArray = doubleArray = Arrays.copyOf(doubleArray, size * 2);
-              }
-              doubleArray[size++] = d;
-          } else {
-              objectArray = new Object[doubleArray.length];
-              for (int i = 0; i < size; i++) {
-                  objectArray[i] = doubleArray[i];
-              }
-              primitiveArray = null;
-              add(e);
-          }
-      } else {
-          assert objectArray == null;
-          assert primitiveArray == null;
-          assert size == 0;
-          if (e instanceof Long l) {
-            var arr = new long[initialCapacity];
-            arr[0] = l;
-            primitiveArray = arr;
-          } else if (e instanceof Double d) {
-            var arr = new double[initialCapacity];
-            arr[0] = d;
-            primitiveArray = arr;
-          } else {
-            var arr = new Object[initialCapacity];
-            arr[0] = e;
-            objectArray = arr;
-          }
-          size = 1;
-      }
-    }
-
-    /** Obtains an element from the builder */
-    public Object get(int index) {
-      if (objectArray != null) {
-          return objectArray[index];
-      } else if (primitiveArray instanceof long[] longArray) {
-          return longArray[index];
-      } else if (primitiveArray instanceof double[] doubleArray) {
-          return doubleArray[index];
-      } else {
-          throw new ArrayIndexOutOfBoundsException();
-      }
-    }
-
-    /** A temporary workaround to be able to efficiently append an array to `ArrayList`. */
-    public void appendTo(List<T> list) {
-      for (T obj : list) {
-          add(obj);
-      }
-    }
-
-    /** Returns the current array of the builder. */
-    public Object toArray() {
-      if (objectArray != null) {
-          return objectArray.length == size ? objectArray : Arrays.copyOf(objectArray, size);
-      } else if (primitiveArray instanceof long[] longArray) {
-          return longArray.length == size ? longArray : Arrays.copyOf(longArray, size);
-      } else if (primitiveArray instanceof double[] doubleArray) {
-          return doubleArray.length == size ? doubleArray : Arrays.copyOf(doubleArray, size);
-      } else {
-          return EMPTY_ARRAY;
-      }
-    }
-
-    public int getSize() {
-      return size;
-    }
-  }
-
 }
