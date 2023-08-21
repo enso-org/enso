b360f1b690d363cd7193e0a69ce2f6453b4fda40
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-08-21 06:40:06 +0200

    Combine date and time

--- engine/runtime-language-epb/src/main/java/org/enso/interpreter/epb/node/PyForeignNode.java
+++ engine/runtime-language-epb/src/main/java/org/enso/interpreter/epb/node/PyForeignNode.java
@@ -1,6 +1,7 @@
 package org.enso.interpreter.epb.node;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import org.enso.interpreter.epb.EpbContext;
 
@@ -27,17 +28,47 @@ public abstract class PyForeignNode extends ForeignFunctionCallNode {
     var ctx = EpbContext.get(this);
     var src = Source.newBuilder("python", """
     from datetime import date
-
-    def conv(y, m, d):
-        return date(y, m, d)
-
-    conv
-    """, "convert.py").build();
+    date
+    """, "convert_date.py").build();
 
     var conv = ctx.getEnv().parsePublic(src).call();
 
     try {
       return InteropLibrary.getUncached().execute(conv, date.getYear(), date.getMonthValue(), date.getDayOfMonth());
+    } catch (UnsupportedTypeException | ArityException | UnsupportedMessageException ex) {
+      throw new IllegalStateException(ex);
+    }
+  }
+
+  @CompilerDirectives.TruffleBoundary
+  private Object wrapPythonTime(LocalTime time) {
+    var ctx = EpbContext.get(this);
+    var src = Source.newBuilder("python", """
+    from datetime import time
+    time
+    """, "convert_time.py").build();
+
+    var conv = ctx.getEnv().parsePublic(src).call();
+
+    try {
+      return InteropLibrary.getUncached().execute(conv, time.getHour(), time.getMinute(), time.getSecond(), time.getNano() / 1000);
+    } catch (UnsupportedTypeException | ArityException | UnsupportedMessageException ex) {
+      throw new IllegalStateException(ex);
+    }
+  }
+
+  @CompilerDirectives.TruffleBoundary
+  private Object combinePythonDateTime(Object date, Object time) {
+    var ctx = EpbContext.get(this);
+    var src = Source.newBuilder("python", """
+    from datetime import datetime
+    datetime.combine
+    """, "convert_combine.py").build();
+
+    var conv = ctx.getEnv().parsePublic(src).call();
+
+    try {
+      return InteropLibrary.getUncached().execute(conv, date, time);
     } catch (UnsupportedTypeException | ArityException | UnsupportedMessageException ex) {
       throw new IllegalStateException(ex);
     }
@@ -51,8 +82,14 @@ public abstract class PyForeignNode extends ForeignFunctionCallNode {
   ) {
     try {
       for (int i = 0; i < arguments.length; i++) {
-        if (iop.isDate(arguments[i])) {
-          arguments[i] = wrapPythonDate(iop.asDate(arguments[i]));
+        var time = iop.isTime(arguments[i]) ? wrapPythonTime(iop.asTime(arguments[i])) : null;
+        var date = iop.isDate(arguments[i]) ? wrapPythonDate(iop.asDate(arguments[i])) : null;
+        if (date != null && time != null) {
+          arguments[i] = combinePythonDateTime(date, time);
+        } else if (date != null) {
+          arguments[i] = date;
+        } else if (time != null) {
+          arguments[i] = time;
         }
       }
       return coercePrimitiveNode.execute(interopLibrary.execute(getForeignFunction(), arguments));
