221cd4de6fc343b7d2e4a65ebc1d4d1140d499a4
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-08-21 05:41:45 +0200

    Convert isDate to datetime.date

--- engine/runtime-language-epb/src/main/java/org/enso/interpreter/epb/node/PyForeignNode.java
+++ engine/runtime-language-epb/src/main/java/org/enso/interpreter/epb/node/PyForeignNode.java
@@ -1,5 +1,10 @@
 package org.enso.interpreter.epb.node;
 
+import java.time.LocalDate;
+
+import org.enso.interpreter.epb.EpbContext;
+
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.NodeField;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.interop.ArityException;
@@ -7,6 +12,7 @@ import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.interop.UnsupportedTypeException;
 import com.oracle.truffle.api.library.CachedLibrary;
+import com.oracle.truffle.api.source.Source;
 
 @NodeField(name = "foreignFunction", type = Object.class)
 public abstract class PyForeignNode extends ForeignFunctionCallNode {
@@ -15,10 +21,40 @@ public abstract class PyForeignNode extends ForeignFunctionCallNode {
 
   abstract Object getForeignFunction();
 
+  @CompilerDirectives.TruffleBoundary
+  private Object wrapPythonDate(LocalDate date) {
+
+    var ctx = EpbContext.get(this);
+    var src = Source.newBuilder("python", """
+    from datetime import date
+
+    def conv(y, m, d):
+        return date(y, m, d)
+
+    conv
+    """, "convert.py").build();
+
+    var conv = ctx.getEnv().parsePublic(src).call();
+
+    try {
+      return InteropLibrary.getUncached().execute(conv, date.getYear(), date.getMonthValue(), date.getDayOfMonth());
+    } catch (UnsupportedTypeException | ArityException | UnsupportedMessageException ex) {
+      throw new IllegalStateException(ex);
+    }
+  }
+
   @Specialization
   public Object doExecute(
-      Object[] arguments, @CachedLibrary("foreignFunction") InteropLibrary interopLibrary) {
+      Object[] arguments,
+      @CachedLibrary("foreignFunction") InteropLibrary interopLibrary,
+      @CachedLibrary(limit="3") InteropLibrary iop
+  ) {
     try {
+      for (int i = 0; i < arguments.length; i++) {
+        if (iop.isDate(arguments[i])) {
+          arguments[i] = wrapPythonDate(iop.asDate(arguments[i]));
+        }
+      }
       return coercePrimitiveNode.execute(interopLibrary.execute(getForeignFunction(), arguments));
     } catch (UnsupportedMessageException | UnsupportedTypeException | ArityException e) {
       throw new IllegalStateException("Python parser returned a malformed object", e);
