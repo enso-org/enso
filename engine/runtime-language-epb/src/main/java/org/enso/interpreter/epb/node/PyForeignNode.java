e746613defb036d5790a5811869789e8a5c12b8a
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-08-21 08:37:04 +0200

    Moving Zone conversions to PyForeignNode

--- engine/runtime-language-epb/src/main/java/org/enso/interpreter/epb/node/PyForeignNode.java
+++ engine/runtime-language-epb/src/main/java/org/enso/interpreter/epb/node/PyForeignNode.java
@@ -1,7 +1,9 @@
 package org.enso.interpreter.epb.node;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.ZoneId;
 
 import org.enso.interpreter.epb.EpbContext;
 
@@ -58,7 +60,7 @@ public abstract class PyForeignNode extends ForeignFunctionCallNode {
   }
 
   @CompilerDirectives.TruffleBoundary
-  private Object combinePythonDateTime(Object date, Object time) {
+  private Object combinePythonDateTimeZone(Object date, Object time, Object zone) {
     var ctx = EpbContext.get(this);
     var src = Source.newBuilder("python", """
     from datetime import datetime
@@ -68,7 +70,45 @@ public abstract class PyForeignNode extends ForeignFunctionCallNode {
     var conv = ctx.getEnv().parsePublic(src).call();
 
     try {
-      return InteropLibrary.getUncached().execute(conv, date, time);
+      if (zone != null) {
+        return InteropLibrary.getUncached().execute(conv, date, time, zone);
+      } else {
+        return InteropLibrary.getUncached().execute(conv, date, time);
+      }
+    } catch (UnsupportedTypeException | ArityException | UnsupportedMessageException ex) {
+      throw new IllegalStateException(ex);
+    }
+  }
+
+  @CompilerDirectives.TruffleBoundary
+  private Object wrapPythonZone(ZoneId zone, LocalTime time, LocalDate date) {
+    var ctx = EpbContext.get(this);
+    var src = Source.newBuilder("python", """
+    from datetime import timezone, timedelta
+    def conv(sec):
+        d = timedelta(seconds=sec)
+        return timezone(d)
+    conv
+    """, "convert_time_zone.py").build();
+
+    var conv = ctx.getEnv().parsePublic(src).call();
+
+    Instant when;
+    if (time != null) {
+      if (date != null) {
+        when = date.atTime(time).atZone(zone).toInstant();
+      } else {
+        when = Instant.now();
+      }
+    } else {
+      if (date != null) {
+        when = date.atStartOfDay(zone).toInstant();
+      } else {
+        when = Instant.now();
+      }
+    }
+    try {
+      return InteropLibrary.getUncached().execute(conv, zone.getRules().getOffset(when).getTotalSeconds());
     } catch (UnsupportedTypeException | ArityException | UnsupportedMessageException ex) {
       throw new IllegalStateException(ex);
     }
@@ -82,14 +122,19 @@ public abstract class PyForeignNode extends ForeignFunctionCallNode {
   ) {
     try {
       for (int i = 0; i < arguments.length; i++) {
-        var time = iop.isTime(arguments[i]) ? wrapPythonTime(iop.asTime(arguments[i])) : null;
-        var date = iop.isDate(arguments[i]) ? wrapPythonDate(iop.asDate(arguments[i])) : null;
+        var javaTime = iop.isTime(arguments[i]) ? iop.asTime(arguments[i]) : null;
+        var time = javaTime != null ? wrapPythonTime(javaTime) : null;
+        var javaDate = iop.isDate(arguments[i]) ? iop.asDate(arguments[i]) : null;
+        var date = javaDate != null ? wrapPythonDate(javaDate) : null;
+        var zone = iop.isTimeZone(arguments[i]) ? wrapPythonZone(iop.asTimeZone(arguments[i]), javaTime, javaDate) : null;
         if (date != null && time != null) {
-          arguments[i] = combinePythonDateTime(date, time);
+          arguments[i] = combinePythonDateTimeZone(date, time, zone);
         } else if (date != null) {
           arguments[i] = date;
         } else if (time != null) {
           arguments[i] = time;
+        } else if (zone != null) {
+          arguments[i] = zone;
         }
       }
       return coercePrimitiveNode.execute(interopLibrary.execute(getForeignFunction(), arguments));
