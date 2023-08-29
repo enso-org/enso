0f7e13f69951240849a56652db67402d4c9536c0
somebody1234 <ehern.lee@gmail.com>
2023-08-29 21:11:28 +1000

    Fix context menu option visibility; fix "opened by" when opening

--- app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/projectNameColumn.tsx
+++ app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/projectNameColumn.tsx
@@ -62,6 +62,7 @@ export default function ProjectNameColumn(props: ProjectNameColumnProps) {
     const ownPermission =
         asset.permissions?.find(permission => permission.user.user_email === organization?.email) ??
         null
+    const isRunning = backendModule.DOES_PROJECT_STATE_INDICATE_VM_EXISTS[asset.projectState.type]
     const canExecute =
         ownPermission != null &&
         backendModule.PERMISSION_ACTION_CAN_EXECUTE[ownPermission.permission]
@@ -218,11 +219,9 @@ export default function ProjectNameColumn(props: ProjectNameColumnProps) {
                 item.depth
             )}`}
             onClick={event => {
-                if (
-                    !rowState.isEditingName &&
-                    !isOtherUserUsingProject &&
-                    eventModule.isDoubleClick(event)
-                ) {
+                if (isRunning || rowState.isEditingName || isOtherUserUsingProject) {
+                    // The project should not be edited in these cases.
+                } else if (eventModule.isDoubleClick(event)) {
                     // It is a double click; open the project.
                     dispatchAssetEvent({
                         type: assetEventModule.AssetEventType.openProject,
