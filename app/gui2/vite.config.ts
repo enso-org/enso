fa1d12e3c69447c33b4c5651d5e7928652f396ce
somebody1234 <ehern.lee@gmail.com>
2023-09-05 19:25:51 +1000

    Fix aliases for vite server

--- app/gui2/vite.config.ts
+++ app/gui2/vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, parse } from 'node:url'
+import { fileURLToPath } from 'node:url'
 
 import { defineConfig, Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -11,6 +11,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@shared': fileURLToPath(new URL('./shared', import.meta.url)),
     },
   },
   css: {
