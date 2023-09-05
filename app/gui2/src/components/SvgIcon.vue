3cc15311a703d121b89c940f3b2c993ae698f189
Adam Obuchowicz <adam.obuchowicz@enso.org>
2023-09-05 13:28:53 +0200

    Forgotten files

---
+++ app/gui2/src/components/SvgIcon.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import icons from '@/assets/icons.svg'
+
+const props = defineProps<{ name: String }>()
+</script>
+
+<template>
+  <svg>
+    <use :href="`${icons}#${props.name}`"></use>
+  </svg>
+</template>
+
+<style>
+svg {
+  width: 16px;
+  height: 16px;
+}
+</style>
---
+++ app/gui2/src/components/ToggleIcon.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import SvgIcon from '@/components/SvgIcon.vue'
+import { ref } from 'vue'
+
+const props = defineProps<{ icon_name: String; modelValue?: Boolean }>()
+const emit = defineEmits<{
+  (e: 'update:modelValue', toggled_on: Boolean): void
+}>()
+
+const toggled_on = ref(props.modelValue ?? false)
+
+function toggle() {
+  toggled_on.value = !toggled_on.value
+  emit('update:modelValue', toggled_on.value)
+}
+</script>
+
+<template>
+  <SvgIcon :name="props.icon_name" :class="{ toggled_on: toggled_on }" @click="toggle" />
+</template>
