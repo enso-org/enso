f6693a844c38f41e4071972c49f6b19f5b4a89cc
Ilya Bogdanov <fumlead@gmail.com>
2023-09-11 12:18:29 +0400

    Forbid node pasting when editing (#7768)
    Closes #7759. The reason was a missing condition in the shortcut setup.
    
    https://github.com/enso-org/enso/assets/6566674/d4475d48-f459-4dd9-ba9b-42e8110b541c

--- app/gui/view/graph-editor/src/shortcuts.rs
+++ app/gui/view/graph-editor/src/shortcuts.rs
@@ -81,7 +81,7 @@ pub const SHORTCUTS: &[(ensogl::application::shortcut::ActionType, &str, &str, &
     (Release, "!read_only", "cmd left-mouse-button", "edit_mode_off"),
     // === Copy-paste ===
     (Press, "!node_editing", "cmd c", "copy_selected_node"),
-    (Press, "!read_only", "cmd v", "paste_node"),
+    (Press, "!read_only & !node_editing", "cmd v", "paste_node"),
     // === Debug ===
     (Press, "debug_mode", "ctrl d", "debug_set_test_visualization_data_for_selected_node"),
     (Press, "debug_mode", "ctrl n", "add_node_at_cursor"),
