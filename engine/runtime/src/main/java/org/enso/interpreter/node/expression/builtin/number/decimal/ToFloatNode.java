ab65acaaba253e5ccc92cb2859b418314d4fa008
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2023-09-13 16:33:39 +0200

    Remove remaining  mentions

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/number/decimal/ToFloatNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/number/decimal/ToFloatNode.java
@@ -2,7 +2,7 @@ package org.enso.interpreter.node.expression.builtin.number.decimal;
 
 import org.enso.interpreter.dsl.BuiltinMethod;
 
-@BuiltinMethod(type = "Float", name = "to_decimal", description = "Identity on decimals")
+@BuiltinMethod(type = "Float", name = "to_float", description = "Identity on floats")
 public class ToFloatNode extends FloatNode {
   double execute(double self) {
     return self;
