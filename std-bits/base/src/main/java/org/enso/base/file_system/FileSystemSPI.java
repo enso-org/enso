8295b769225febf97642cafa1763c5675901b3a1
James Dunkerley <jdunkerley@users.noreply.github.com>
2023-09-14 13:27:39 +0100

    Linting.

--- std-bits/base/src/main/java/org/enso/base/file_system/FileSystemSPI.java
+++ std-bits/base/src/main/java/org/enso/base/file_system/FileSystemSPI.java
@@ -6,15 +6,22 @@ import org.graalvm.polyglot.Value;
 
 public abstract class FileSystemSPI {
   private static final ServiceLoader<org.enso.base.file_system.FileSystemSPI> loader =
-      ServiceLoader.load(org.enso.base.file_system.FileSystemSPI.class, org.enso.base.file_format.FileFormatSPI.class.getClassLoader());
+      ServiceLoader.load(
+          org.enso.base.file_system.FileSystemSPI.class,
+          org.enso.base.file_format.FileFormatSPI.class.getClassLoader());
 
   public static Value get_type(String protocol, boolean refresh) {
     if (refresh) {
       loader.reload();
     }
 
-    var first = loader.stream().filter(provider -> provider.get().getProtocol().equals(protocol)).findFirst();
-    return first.map(fileSystemSPIProvider -> fileSystemSPIProvider.get().getTypeObject()).orElse(null);
+    var first =
+        loader.stream()
+            .filter(provider -> provider.get().getProtocol().equals(protocol))
+            .findFirst();
+    return first
+        .map(fileSystemSPIProvider -> fileSystemSPIProvider.get().getTypeObject())
+        .orElse(null);
   }
 
   public static Value[] get_types(boolean refresh) {
