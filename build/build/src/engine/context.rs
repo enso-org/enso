816c2453aeb1594ce6255db1b5a397f6c27c7ebb
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2023-09-19 18:17:21 +0200

    Quick hack to not run bench on Windows
    This was requested by libs team to reduce CI build time.
    Don't know much about Rust so :fingers-crossed: this works.

--- build/build/src/engine/context.rs
+++ build/build/src/engine/context.rs
@@ -330,7 +330,14 @@ impl RunContext {
             // We want benchmarks to run only after the other build tasks are done, as they are
             // really CPU-heavy.
             let build_command = (!tasks.is_empty()).then_some(Sbt::concurrent_tasks(tasks));
-            let benchmark_tasks = self.config.execute_benchmarks.iter().flat_map(|b| b.sbt_task());
+
+            let empty_benchmarks = BTreeSet::new();
+            let benchmark_tasks0 = if TARGET_OS == OS::Windows {
+                &empty_benchmarks
+            } else {
+                &self.config.execute_benchmarks
+            };
+            let benchmark_tasks = benchmark_tasks0.iter().flat_map(|b| b.sbt_task());
             let command_sequence = build_command.as_deref().into_iter().chain(benchmark_tasks);
             let final_command = Sbt::sequential_tasks(command_sequence);
             if !final_command.is_empty() {
