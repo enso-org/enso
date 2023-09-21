3802dc4795c8e6e6807dadc0a7cedc5ec18433da
Paweł Grabarz <frizi09@gmail.com>
2023-09-21 16:39:51 +0200

    split npm install and ci-check in builder

--- build/cli/src/lib.rs
+++ build/cli/src/lib.rs
@@ -845,11 +845,13 @@ pub async fn main_internal(config: Option<enso_build::config::Config>) -> Result
                 .run_ok()
                 .await?;
 
-            Npm.cmd()?.install().run("ci-check").run_ok().await?;
+            Npm.cmd()?.install().run_ok().await?;
+            Npm.cmd()?.run("ci-check").run_ok().await?;
             ensogl_pack::build_ts_sources_only().await?;
         }
         Target::Fmt => {
-            let prettier = Npm.cmd()?.install().run("format").run_ok();
+            Npm.cmd()?.install().run_ok().await?;
+            let prettier = Npm.cmd()?.run("format").run_ok();
             let our_formatter =
                 enso_formatter::process_path(&ctx.repo_root, enso_formatter::Action::Format);
             let (r1, r2) = join!(prettier, our_formatter).await;
