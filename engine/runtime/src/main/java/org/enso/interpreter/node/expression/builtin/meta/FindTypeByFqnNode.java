58ae9c8a0682b94bb9e189f4d527906b830c1680
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-09-25 13:21:43 +0200

    Meta.Type.find a type by FQN

---
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/FindTypeByFqnNode.java
@@ -0,0 +1,31 @@
+package org.enso.interpreter.node.expression.builtin.meta;
+
+import com.oracle.truffle.api.nodes.Node;
+import org.enso.interpreter.dsl.BuiltinMethod;
+import org.enso.interpreter.runtime.EnsoContext;
+import org.enso.interpreter.runtime.data.text.Text;
+import org.enso.pkg.QualifiedName;
+
+@BuiltinMethod(
+    type = "Meta",
+    name = "find_type_by_qualified_name",
+    description = "Finds a type by fully qualified name.",
+    autoRegister = false)
+public class FindTypeByFqnNode extends Node {
+  Object execute(Text fqn) {
+    var ctx = EnsoContext.get(this);
+    var fullName = QualifiedName.fromString(fqn.toString());
+    if (fullName.getParent().isDefined()) {
+      var moduleName = fullName.getParent().get();
+      for (var m : ctx.getTopScope().getModules()) {
+        if (m.getName().equals(moduleName)) {
+          var foundType = m.getScope().getType(fullName.item());
+          if (foundType.isPresent()) {
+            return foundType.get();
+          }
+        }
+      }
+    }
+    return ctx.getBuiltins().error().makeModuleDoesNotExistError("");
+  }
+}
