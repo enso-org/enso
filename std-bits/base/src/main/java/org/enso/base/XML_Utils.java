4b4faf7ab9f18e896c98fe65f572a837c2dad9a8
Gregory Travis <greg.travis@enso.org>
2023-09-29 11:22:12 -0400

    inner xml

--- std-bits/base/src/main/java/org/enso/base/XML_Utils.java
+++ std-bits/base/src/main/java/org/enso/base/XML_Utils.java
@@ -10,6 +10,7 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 public class XML_Utils {
     /**
@@ -27,6 +28,27 @@ public class XML_Utils {
         Source source = new DOMSource(element);
         Result target = new StreamResult(out);
         transformer.transform(source, target);
+        return out.toString();
+    }
+
+    /**
+     * Return the string representation of the contents of an XML element, not
+     * including its tag.
+     *
+     * @param element the element to convert to a string
+     * @return the string representation of the element's contents
+     * @throws TransformerException
+     */
+    public static String innerXML(Element element) throws TransformerException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        Result target = new StreamResult(out);
+        NodeList childNodes = element.getChildNodes();
+        for (int i=0; i < childNodes.getLength(); ++i) {
+            Source source = new DOMSource(childNodes.item(i));
+            transformer.transform(source, target);
+        }
         return out.toString();
     }
 }
