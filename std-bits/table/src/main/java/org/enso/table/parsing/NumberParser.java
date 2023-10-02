72067a89d8b2449dfd48a18b18438771889be222
Radosław Waśko <radoslaw.wasko@enso.org>
2023-10-02 16:58:13 +0200

    optimize imports

--- std-bits/table/src/main/java/org/enso/table/parsing/NumberParser.java
+++ std-bits/table/src/main/java/org/enso/table/parsing/NumberParser.java
@@ -11,13 +11,11 @@ import org.enso.table.problems.WithAggregatedProblems;
 import org.graalvm.polyglot.Context;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 /** A parser for numbers.
  *
