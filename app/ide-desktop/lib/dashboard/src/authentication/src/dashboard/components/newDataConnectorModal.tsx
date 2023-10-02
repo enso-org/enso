3a010a00a75f1329d473b9bc028c8a08e54d9b32
somebody1234 <ehern.lee@gmail.com>
2023-10-02 20:52:26 +1000

    Implement connector button and context menu action. (#7891)
    - Closes https://github.com/enso-org/cloud-v2/issues/686
    
    # Important Notes
    For now they use the backend endpoints for secrets.

--- app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/newDataConnectorModal.tsx
+++ app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/newDataConnectorModal.tsx
@@ -8,27 +8,29 @@ import * as modalProvider from '../../providers/modal'
 
 import Modal from './modal'
 
-// ==========================
-// === ConfirmDeleteModal ===
-// ==========================
+// =============================
+// === NewDataConnectorModal ===
+// =============================
 
-/** Props for a {@link ConfirmDeleteModal}. */
-export interface ConfirmDeleteModalProps {
-    /** Must fit in the sentence "Are you sure you want to delete <description>?". */
-    description: string
-    doDelete: () => void
+/** Props for a {@link NewDataConnectorModal}. */
+export interface NewDataConnectorModalProps {
+    doCreate: (name: string, value: string) => void
 }
 
 /** A modal for confirming the deletion of an asset. */
-export default function ConfirmDeleteModal(props: ConfirmDeleteModalProps) {
-    const { description, doDelete } = props
+export default function NewDataConnectorModal(props: NewDataConnectorModalProps) {
+    const { doCreate } = props
     const logger = loggerProvider.useLogger()
     const { unsetModal } = modalProvider.useSetModal()
+
+    const [name, setName] = React.useState('')
+    const [value, setValue] = React.useState('')
+    const canSubmit = Boolean(name && value)
 
     const onSubmit = () => {
         unsetModal()
         try {
-            doDelete()
+            doCreate(name, value)
         } catch (error) {
             const message = errorModule.getMessageOrToString(error)
             toastify.toast.error(message)
@@ -39,9 +41,6 @@ export default function ConfirmDeleteModal(props: ConfirmDeleteModalProps) {
     return (
         <Modal centered className="bg-dim">
             <div
-                ref={element => {
-                    element?.focus()
-                }}
                 tabIndex={-1}
                 className="relative rounded-2xl pointer-events-auto"
                 onKeyDown={event => {
@@ -61,15 +60,37 @@ export default function ConfirmDeleteModal(props: ConfirmDeleteModalProps) {
                         // delete an important asset.
                         onSubmit()
                     }}
-                    className="relative flex flex-col rounded-2xl gap-2 w-96 px-4 p-2"
+                    className="relative flex flex-col rounded-2xl gap-2 w-96 px-4 py-2"
                 >
-                    <div>Are you sure you want to delete {description}?</div>
+                    <h1 className="text-sm font-semibold">New Data Connector</h1>
+                    <div className="flex">
+                        <div className="w-12 h-6 py-1">Name</div>
+                        <input
+                            autoFocus
+                            placeholder="Enter the name of the data connector"
+                            className="grow bg-transparent border border-black-a10 rounded-full leading-170 h-6 px-4 py-px"
+                            onInput={event => {
+                                setName(event.currentTarget.value)
+                            }}
+                        />
+                    </div>
+                    <div className="flex">
+                        <div className="w-12 h-6 py-1">Value</div>
+                        <input
+                            placeholder="Enter the value of the data connector"
+                            className="grow bg-transparent border border-black-a10 rounded-full leading-170 h-6 px-4 py-px"
+                            onInput={event => {
+                                setValue(event.currentTarget.value)
+                            }}
+                        />
+                    </div>
                     <div className="flex gap-2">
                         <button
+                            disabled={!canSubmit}
                             type="submit"
-                            className="hover:cursor-pointer inline-block text-white bg-delete rounded-full px-4 py-1"
+                            className="hover:cursor-pointer inline-block text-white bg-invite rounded-full px-4 py-1 disabled:opacity-50 disabled:cursor-default"
                         >
-                            Delete
+                            Create
                         </button>
                         <button
                             type="button"
