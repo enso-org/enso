enso-org/enso

commit 828d160c565e3ee2f25030a87fee795243e91379

Introduce new focus APIs, and use for CB (#7167)
Introduce new APIs for managing focus and using focus to inform delivery of keyboard events.

Use new APIs to implement the following behavior:

Focus:
- If the component browser is opened, its initial state is *focused*.
- If the node input area's text component is clicked, the component browser's state becomes *blurred*.
- If a click occurs anywhere in the component browser, the component browser's state becomes *focused*.

Event dispatch:
- When the component browser is in the *focused* state, it handles certain keyboard events (chiefly, arrow keys).
- If the component browser handles an event, the event is not received by other components.
- If an event occurs that the component browser doesn't handle, the node input area's text component receives the event.

[vokoscreenNG-2023-06-29_10-55-00.webm](https://github.com/enso-org/enso/assets/1047859/f1d9d07c-8c32-4482-ba32-15b6e4e20ae7)

# Important Notes
Changes to display object interface:
- **`display::Object` can now be derived.**
- Introduce display object *focus receiver* concept. Many components, when receiving focus, should actually be focused indirectly by focusing a descendant.
- For example, when the CB Panel receives focus, its descendant at `self.model().grid.model().grid` should be focused, because that's the underlying Grid View, which has its own event handlers. By allowing each level of the hierarchy to define a `focus_receiver`, focus can reach the right object without the CB panel having to know structural details of its descendants.
- When delegating to a field's `display::Object` implementation, the derived implementation uses the child's `focus_receiver`, which will normally be the correct behavior.

**Changes to `shortcut` API**:

app/gui/view/src/code_editor.rs
@@ -28,7 +28,7 @@ pub const PADDING_LEFT: f32 = 7.0;
 // === Frp ===
 // ===========
 
-ensogl::define_endpoints! {
+ensogl::define_endpoints_2! {
     Input {
         /// Show the Code Editor.
         show(),
@@ -52,18 +52,13 @@ ensogl::define_endpoints! {
 // ============
 
 /// The View of IDE Code Editor.
-#[derive(Clone, CloneRef, Debug)]
+#[derive(Clone, CloneRef, Deref, Debug, display::Object)]
 pub struct View {
+    #[display_object]
     model:  text::Text,
     styles: StyleWatchFrp,
+    #[deref]
     frp:    Frp,
-}
-
-impl Deref for View {
-    type Target = Frp;
-    fn deref(&self) -> &Self::Target {
-        &self.frp
-    }
 }
 
 impl View {
@@ -85,38 +80,35 @@ impl View {
         model.hover();
 
         frp::extend! { network
-            let is_visible     =  frp.output.is_visible.clone_ref();
+            let is_visible = frp.output.is_visible.clone_ref();
             show_after_toggle <- frp.toggle.gate_not(&is_visible);
-            hide_after_toggle <- frp.toggle.gate    (&is_visible);
-            show              <- any(frp.input.show,show_after_toggle);
-            hide              <- any(frp.input.hide,hide_after_toggle);
+            hide_after_toggle <- frp.toggle.gate(&is_visible);
+            show <- any(frp.input.show, show_after_toggle);
+            hide <- any(frp.input.hide, hide_after_toggle);
 
             eval_ show (height_fraction.set_target_value(HEIGHT_FRACTION));
             focus <- show.gate_not(&frp.set_read_only);
-            eval_ focus ({
-                model.deprecated_focus();
-                model.focus();
-            });
+            eval_ focus (model.focus());
             eval_ hide (height_fraction.set_target_value(0.0));
             enable_read_only <- frp.set_read_only.on_true();
             defocus <- any(hide, enable_read_only);
             eval_ defocus ([model] {
                 model.remove_all_cursors();
-                model.deprecated_defocus();
                 model.blur();
             });
 
-            frp.source.is_visible <+ bool(&frp.input.hide,&frp.input.show);
-            frp.source.is_visible <+ frp.toggle.map2(&is_visible, |(),b| !b);
+            frp.private.output.is_visible <+ bool(&frp.input.hide, &frp.input.show);
+            frp.private.output.is_visible <+ frp.toggle.map2(&is_visible, |(), b| !b);
 
             def init = source_();
-            let shape  = app.display.default_scene.shape();
-            position <- all_with3(&height_fraction.value,shape,&init, |height_f,scene_size,_init| {
-                let height = height_f * scene_size.height;
-                let x      = -scene_size.width  / 2.0 + PADDING_LEFT;
-                let y      = -scene_size.height / 2.0 + height;
-                Vector2(x,y)
-            });
+            let shape = app.display.default_scene.shape();
+            position <- all_with3(&height_fraction.value, shape, &init,
+                |height_f, scene_size, _init| {
+                    let height = height_f * scene_size.height;
+                    let x = -scene_size.width / 2.0 + PADDING_LEFT;
+                    let y = -scene_size.height / 2.0 + height;
+                    Vector2(x,y)
+                });
             eval position ((pos) model.set_xy(*pos));
 
             let color = styles.get_color(ensogl_hardcoded_theme::code::syntax::base);
@@ -131,12 +123,6 @@ impl View {
     /// Return the Text Area component inside this editor.
     pub fn text_area(&self) -> &text::Text {
         &self.model
-    }
-}
-
-impl display::Object for View {
-    fn display_object(&self) -> &display::object::Instance {
-        self.model.display_object()
     }
 }
 
@@ -155,7 +141,7 @@ impl application::View for View {
         Self::new(app)
     }
 
-    fn default_shortcuts() -> Vec<shortcut::Shortcut> {
+    fn global_shortcuts() -> Vec<shortcut::Shortcut> {
         use shortcut::ActionType::*;
         [(Press, "ctrl `", "toggle"), (Press, "escape", "hide")]
             .iter()

app/gui/view/src/project.rs
@@ -182,7 +182,7 @@ ensogl::define_endpoints! {
 // === Model ===
 // =============
 
-#[derive(Clone, CloneRef, Debug)]
+#[derive(Clone, CloneRef, Debug, display::Object)]
 struct Model {
     display_object:   display::object::Instance,
     top_bar:          ProjectViewTopBar,
@@ -357,8 +357,9 @@ mod js {
 
 /// The main view of single project opened in IDE.
 #[allow(missing_docs)]
-#[derive(Clone, CloneRef, Debug)]
+#[derive(Clone, CloneRef, Debug, display::Object)]
 pub struct View {
+    #[display_object]
     model:   Model,
     pub frp: Frp,
 }
@@ -818,12 +819,6 @@ impl View {
     }
 }
 
-impl display::Object for View {
-    fn display_object(&self) -> &display::object::Instance {
-        &self.model.display_object
-    }
-}
-
 impl FrpNetworkProvider for View {
     fn network(&self) -> &frp::Network {
         &self.frp.network
@@ -839,7 +834,7 @@ impl application::View for View {
         View::new(app)
     }
 
-    fn default_shortcuts() -> Vec<application::shortcut::Shortcut> {
+    fn global_shortcuts() -> Vec<application::shortcut::Shortcut> {
         use shortcut::ActionType::*;
         [
             (Press, "!is_searcher_opened", "cmd o", "show_project_list"),

lib/rust/ensogl/component/text/src/component/text.rs
@@ -30,13 +30,14 @@ use ensogl_core::application;
 use ensogl_core::application::command::FrpNetworkProvider;
 use ensogl_core::application::shortcut;
 use ensogl_core::application::Application;
+use ensogl_core::control::io::keyboard::event::*;
 use ensogl_core::data::color;
 use ensogl_core::display;
 use ensogl_core::gui::cursor;
 use ensogl_core::system::web::clipboard;
 use ensogl_text_font_family::NonVariableFaceHeader;
 use owned_ttf_parser::AsFaceRef;
-use std::ops::Not;
+
 
 
 // ==============
@@ -163,11 +164,12 @@ impl From<LinesVec> for Lines {
 
 /// The visual text area implementation. It is meant to be a generic rich text component which you
 /// should use everywhere you want to display text.
-#[derive(Clone, CloneRef, Debug, Deref)]
+#[derive(Clone, CloneRef, Debug, Deref, display::Object)]
 #[allow(missing_docs)]
 pub struct Text {
     #[deref]
     pub frp:  Frp,
+    #[display_object]
     pub data: TextModel,
 }
 
@@ -298,6 +300,7 @@ ensogl_core::define_endpoints_2! {
 
         hover(),
         unhover(),
+        focus(),
         set_single_line_mode(bool),
         set_hover(bool),
 
@@ -363,6 +366,7 @@ ensogl_core::define_endpoints_2! {
 impl Text {
     fn init(self) -> Self {
         self.init_hover();
+        self.init_focus();
         self.init_single_line_mode();
         self.init_cursors();
         self.init_selections();
@@ -384,6 +388,16 @@ impl Text {
             hovered <- any(&input.set_hover,&hovered);
             out.hovered <+ hovered;
             out.pointer_style <+ out.hovered.map(|h| h.then_or_default(cursor::Style::cursor));
+        }
+    }
+
+    fn init_focus(&self) {
+        let m = &self.data;
+        let network = self.frp.network();
+        let input = &self.frp.input;
+
+        frp::extend! { network
+            eval_ input.focus (m.focus());
         }
     }
 
@@ -560,11 +574,11 @@ impl Text {
     fn init_edits(&self) {
         let m = &self.data;
         let scene = &m.app.display.default_scene;
-        let keyboard = &scene.keyboard;
         let network = self.frp.network();
         let input = &self.frp.input;
         let out = &self.frp.private.output;
         let after_animations = ensogl_core::animation::on_after_animations();
+        let key_down = scene.on_event::<KeyDown>();
 
         frp::extend! { network
 
@@ -575,9 +589,7 @@ impl Text {
             eval_ input.delete_word_left (m.buffer.frp.delete_word_left());
             eval_ input.delete_word_right (m.buffer.frp.delete_word_right());
 
-            key_down <- keyboard.frp.down.gate_not(&keyboard.frp.is_meta_down);
-            key_down <- key_down.gate_not(&keyboard.frp.is_control_down);
-            key_to_insert <= key_down.map(f!((key) m.key_to_string(key).map(ImString::from)));
+            key_to_insert <= key_down.map2(&out.single_line_mode, TextModel::process_key_event);
             str_to_insert <- any(&input.insert, &key_to_insert);
             eval str_to_insert ((s) m.buffer.frp.insert(s));
             eval input.set_content ((s) {
@@ -703,13 +715,13 @@ impl Text {
 // =================
 
 /// Internal representation of `Text`.
-#[derive(Clone, CloneRef, Debug, Deref)]
+#[derive(Clone, CloneRef, Debug, Deref, display::Object)]
 pub struct TextModel {
     rc: Rc<TextModelData>,
 }
 
 /// Internal representation of `Text`.
-#[derive(Debug, Deref)]
+#[derive(Debug, Deref, display::Object)]
 pub struct TextModelData {
     #[deref]
     buffer:         buffer::Buffer,
@@ -1913,10 +1925,23 @@ impl TextModel {
         *s = s.lines().next().unwrap_or("").to_string();
     }
 
-    fn key_to_string(&self, key: &Key) -> Option<String> {
+    fn process_key_event(
+        event: &ensogl_core::event::Event<KeyDown>,
+        single_line_mode: &bool,
+    ) -> Option<ImString> {
+        let check_modifiers = (!event.ctrl() && !event.meta()).then_some(());
+        let text =
+            check_modifiers.and_then(|_| Self::key_to_string(event.key(), *single_line_mode));
+        if text.is_some() {
+            event.stop_propagation();
+        }
+        text
+    }
+
+    fn key_to_string(key: &Key, single_line_mode: bool) -> Option<ImString> {
         match key {
-            Key::Character(s) => Some(s.clone()),
-            Key::Enter => self.frp.output.single_line_mode.value().not().as_some("\n".into()),
+            Key::Character(s) => Some(s.into()),
+            Key::Enter if !single_line_mode => Some("\n".into()),
             Key::Space => Some(" ".into()),
             _ => None,
         }
@@ -1945,18 +1970,6 @@ where T: for<'t> FromInContextSnapped<&'t TextModel, S>
     }
 }
 
-impl display::Object for TextModel {
-    fn display_object(&self) -> &display::object::Instance {
-        &self.display_object
-    }
-}
-
-impl display::Object for Text {
-    fn display_object(&self) -> &display::object::Instance {
-        self.data.display_object()
-    }
-}
-
 impl FrpNetworkProvider for Text {
     fn network(&self) -> &frp::Network {
         self.frp.network()
@@ -1978,63 +1991,74 @@ impl application::View for Text {
         Text::new(app)
     }
 
-    fn default_shortcuts() -> Vec<shortcut::Shortcut> {
+    fn global_shortcuts() -> Vec<shortcut::Shortcut> {
         use shortcut::ActionType::*;
-        ([
-            (PressAndRepeat, "left", "cursor_move_left"),
-            (PressAndRepeat, "right", "cursor_move_right"),
-            (PressAndRepeat, "up", "cursor_move_up"),
-            (PressAndRepeat, "down", "cursor_move_down"),
-            (PressAndRepeat, "cmd left", "cursor_move_left_word"),
-            (PressAndRepeat, "cmd right", "cursor_move_right_word"),
-            (Press, "alt left", "cursor_move_left_of_line"),
-            (Press, "alt right", "cursor_move_right_of_line"),
-            (Press, "home", "cursor_move_left_of_line"),
-            (Press, "end", "cursor_move_right_of_line"),
-            (Press, "alt shift left", "cursor_select_left_of_line"),
-            (Press, "alt shift right", "cursor_select_right_of_line"),
-            (Press, "shift home", "cursor_select_left_of_line"),
-            (Press, "shift end", "cursor_select_right_of_line"),
-            (Press, "cmd up", "cursor_move_to_text_start"),
-            (Press, "cmd down", "cursor_move_to_text_end"),
-            (Press, "ctrl home", "cursor_move_to_text_start"),
-            (Press, "ctrl end", "cursor_move_to_text_end"),
-            (Press, "cmd shift up", "cursor_select_to_text_start"),
-            (Press, "cmd shift down", "cursor_select_to_text_end"),
-            (Press, "ctrl shift home", "cursor_select_to_text_start"),
-            (Press, "ctrl shift end", "cursor_select_to_text_end"),
-            (PressAndRepeat, "shift left", "cursor_select_left"),
-            (PressAndRepeat, "shift right", "cursor_select_right"),
-            (PressAndRepeat, "cmd shift left", "cursor_select_left_word"),
-            (PressAndRepeat, "cmd shift right", "cursor_select_right_word"),
-            (PressAndRepeat, "shift up", "cursor_select_up"),
-            (PressAndRepeat, "shift down", "cursor_select_down"),
-            (PressAndRepeat, "backspace", "delete_left"),
-            (PressAndRepeat, "delete", "delete_right"),
-            (PressAndRepeat, "cmd backspace", "delete_word_left"),
-            (PressAndRepeat, "cmd delete", "delete_word_right"),
-            (Press, "shift left-mouse-button", "set_newest_selection_end_to_mouse_position"),
-            (DoublePress, "left-mouse-button", "select_word_at_cursor"),
-            (Press, "left-mouse-button", "set_cursor_at_mouse_position"),
-            (Press, "left-mouse-button", "start_newest_selection_end_follow_mouse"),
-            (Release, "left-mouse-button", "stop_newest_selection_end_follow_mouse"),
-            (Press, "cmd left-mouse-button", "add_cursor_at_mouse_position"),
-            (Press, "cmd left-mouse-button", "start_newest_selection_end_follow_mouse"),
-            (Release, "cmd left-mouse-button", "stop_newest_selection_end_follow_mouse"),
-            (Press, "cmd a", "select_all"),
-            (Press, "cmd c", "copy"),
-            (Press, "cmd x", "cut"),
-            (Press, "cmd v", "paste"),
-            (Press, "cmd z", "undo"),
-            (Press, "escape", "keep_oldest_cursor_only"),
-        ])
-        .iter()
-        .map(|(action, rule, command)| {
-            let only_hovered = *action != Release && rule.contains("left-mouse-button");
-            let condition = if only_hovered { "focused & hovered" } else { "focused" };
-            Self::self_shortcut_when(*action, *rule, *command, condition)
-        })
-        .collect()
+        let focus_capturing_shortcuts = [
+            (PressAndRepeat, "left", "cursor_move_left", ""),
+            (PressAndRepeat, "right", "cursor_move_right", ""),
+            (PressAndRepeat, "up", "cursor_move_up", "!single_line_mode"),
+            (PressAndRepeat, "down", "cursor_move_down", "!single_line_mode"),
+            (PressAndRepeat, "cmd left", "cursor_move_left_word", ""),
+            (PressAndRepeat, "cmd right", "cursor_move_right_word", ""),
+            (Press, "alt left", "cursor_move_left_of_line", ""),
+            (Press, "alt right", "cursor_move_right_of_line", ""),
+            (Press, "home", "cursor_move_left_of_line", ""),
+            (Press, "end", "cursor_move_right_of_line", ""),
+            (Press, "alt shift left", "cursor_select_left_of_line", ""),
+            (Press, "alt shift right", "cursor_select_right_of_line", ""),
+            (Press, "shift home", "cursor_select_left_of_line", ""),
+            (Press, "shift end", "cursor_select_right_of_line", ""),
+            (Press, "cmd up", "cursor_move_to_text_start", ""),
+            (Press, "cmd down", "cursor_move_to_text_end", ""),
+            (Press, "ctrl home", "cursor_move_to_text_start", ""),
+            (Press, "ctrl end", "cursor_move_to_text_end", ""),
+            (Press, "cmd shift up", "cursor_select_to_text_start", ""),
+            (Press, "cmd shift down", "cursor_select_to_text_end", ""),
+            (Press, "ctrl shift home", "cursor_select_to_text_start", ""),
+            (Press, "ctrl shift end", "cursor_select_to_text_end", ""),
+            (PressAndRepeat, "shift left", "cursor_select_left", ""),
+            (PressAndRepeat, "shift right", "cursor_select_right", ""),
+            (PressAndRepeat, "cmd shift left", "cursor_select_left_word", ""),
+            (PressAndRepeat, "cmd shift right", "cursor_select_right_word", ""),
+            (PressAndRepeat, "shift up", "cursor_select_up", "!single_line_mode"),
+            (PressAndRepeat, "shift down", "cursor_select_down", "!single_line_mode"),
+            (Press, "shift left-mouse-button", "set_newest_selection_end_to_mouse_position", ""),
+            (DoublePress, "left-mouse-button", "select_word_at_cursor", ""),
+            (Press, "left-mouse-button", "set_cursor_at_mouse_position", ""),
+            (Press, "left-mouse-button", "start_newest_selection_end_follow_mouse", ""),
+            (Press, "cmd left-mouse-button", "add_cursor_at_mouse_position", ""),
+            (Press, "cmd left-mouse-button", "start_newest_selection_end_follow_mouse", ""),
+            (Press, "cmd a", "select_all", ""),
+        ];
+        let non_focus_capturing_shortcuts = [
+            (Press, "cmd c", "copy", ""),
+            (Press, "cmd x", "cut", ""),
+            (Press, "cmd v", "paste", ""),
+            (Press, "cmd z", "undo", ""),
+            (Press, "escape", "keep_oldest_cursor_only", ""),
+            (Release, "left-mouse-button", "stop_newest_selection_end_follow_mouse", ""),
+            (Release, "cmd left-mouse-button", "stop_newest_selection_end_follow_mouse", ""),
+            (PressAndRepeat, "backspace", "delete_left", ""),
+            (PressAndRepeat, "delete", "delete_right", ""),
+            (PressAndRepeat, "cmd backspace", "delete_word_left", ""),
+            (PressAndRepeat, "cmd delete", "delete_word_right", ""),
+        ];
+        non_focus_capturing_shortcuts
+            .iter()
+            .copied()
+            .chain(focus_capturing_shortcuts.iter().copied())
+            .chain(focus_capturing_shortcuts.iter().map(|(a, r, _, c)| (*a, *r, "focus", *c)))
+            .map(|(action, rule, command, condition)| {
+                let only_hovered = action != Release && rule.contains("left-mouse-button");
+                let base_condition = if only_hovered { "focused & hovered" } else { "focused" };
+                let condition = if condition.is_empty() {
+                    Cow::from(base_condition)
+                } else {
+                    Cow::from(format!("{base_condition} & {condition}"))
+                };
+                Self::self_shortcut_when(action, rule, command, condition.as_ref())
+            })
+            .collect()
     }
 }
 

commit 022a5b0d1b65fc55dda65ba1e9c571162498c0a3

Make the code editor editable again (#7844)
Fixes #7474

Again, a quite complicated issue. The code editor was broken by https://github.com/enso-org/enso/pull/7167 for a few reasons:
1. The text component's shortcuts depend on the `focused` FRP endpoint, which is controlled by `deprecated_(de)focus` inputs. `focused` output was not changed by the code editor `model.focus()` call. (we still use `deprecated_focus` for all other components)
2. `focus()` FRP input was added to the text component but never triggered. It's too easy to confuse it with the `focus()` method of the `object::Instance` (Deref is evil!)
3. The graph editor handles clicks on the background by `blur_tree()`, effectively blurring any focused element in the application. But clicking on code editor (to focus it) also causes a click on the background! So we focus the code editor, and then handle the background click, defocusing it from the graph editor's point of view. It allows graph editor to open CB on <kbd>Enter</kbd> while the code editor is still active.

I solved (3) by disallowing CB opening while code editor is visible. Not ideal, but I guess it solves the issue.

Do you think the other solution is possible – to add some overlay for the code editor and avoid triggering `blur_tree` when clicking on it? That won't work because you still can't defocus the code editor using the new API, so it will always be focused.

So we have a situation when half of the app uses the new focus system, another half uses the old one, and none works properly.


https://github.com/enso-org/enso/assets/6566674/3a0d36ed-4d50-4606-9486-272fbd5d9cb7

app/gui/view/src/code_editor.rs
@@ -87,13 +87,13 @@ impl View {
 
             eval_ show (height_fraction.set_target_value(HEIGHT_FRACTION));
             focus <- show.gate_not(&frp.set_read_only);
-            eval_ focus (model.focus());
+            eval_ focus(model.frp.focus());
             eval_ hide (height_fraction.set_target_value(0.0));
             enable_read_only <- frp.set_read_only.on_true();
             defocus <- any(hide, enable_read_only);
             eval_ defocus ([model] {
                 model.remove_all_cursors();
-                model.blur();
+                model.frp.blur();
             });
 
             frp.private.output.is_visible <+ bool(&frp.input.hide, &frp.input.show);

app/gui/view/src/project.rs
@@ -203,7 +203,7 @@ struct Model {
 
 impl Model {
     fn new(app: &Application) -> Self {
-        let display_object = display::object::Instance::new();
+        let display_object = display::object::Instance::new_named("ProjectView");
         let searcher = app.new_view::<component_browser::View>();
         let graph_editor = app.new_view::<GraphEditor>();
         let code_editor = app.new_view::<code_editor::View>();
@@ -426,6 +426,8 @@ impl View {
         let _network = &self.frp.network;
         frp::extend! { _network
             self.model.code_editor.set_read_only <+ self.frp.set_read_only;
+            self.model.code_editor.hide <+ self.model.graph_editor.node_editing_started.constant(());
+            self.model.code_editor.hide <+ self.model.graph_editor.node_selected.constant(());
         }
         self
     }
@@ -712,6 +714,7 @@ impl View {
         let frp = &self.frp;
         let network = &frp.network;
         let graph_editor = &self.model.graph_editor.frp;
+        let code_editor = &self.model.code_editor;
         frp::extend! { network
             // Searcher type to use for node creation.
             // Debounces are needed, because there are shortcut conflits with Component Browser's
@@ -726,6 +729,7 @@ impl View {
 
             should_not_create_node <- graph_editor.node_editing || graph_editor.read_only;
             should_not_create_node <- should_not_create_node || graph_editor.is_fs_visualization_displayed;
+            should_not_create_node <- should_not_create_node || code_editor.is_visible;
             start_node_creation <- searcher_type.gate_not(&should_not_create_node);
             graph_editor.start_node_creation <+ start_node_creation.constant(());
         }

lib/rust/ensogl/component/text/src/component/text.rs
@@ -301,6 +301,7 @@ ensogl_core::define_endpoints_2! {
         hover(),
         unhover(),
         focus(),
+        blur(),
         set_single_line_mode(bool),
         set_hover(bool),
 
@@ -395,9 +396,13 @@ impl Text {
         let m = &self.data;
         let network = self.frp.network();
         let input = &self.frp.input;
+        let out = &self.frp.private.output;
 
         frp::extend! { network
             eval_ input.focus (m.focus());
+            eval_ input.blur (m.blur());
+            out.focused <+ input.focus.constant(true);
+            out.focused <+ input.blur.constant(false);
         }
     }
 
@@ -1965,6 +1970,7 @@ impl application::View for Text {
 
     fn global_shortcuts() -> Vec<shortcut::Shortcut> {
         use shortcut::ActionType::*;
+        // These shortcuts emit `focus` event when triggered.
         let focus_capturing_shortcuts = [
             (PressAndRepeat, "left", "cursor_move_left", ""),
             (PressAndRepeat, "right", "cursor_move_right", ""),