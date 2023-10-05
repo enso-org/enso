842f8dc228c42a6b4579a7fd04b42be6784bc245
somebody1234 <ehern.lee@gmail.com>
2023-10-05 10:33:26 +1000

    Add Toggle Icon variant to SVG Icon demo

--- app/gui2/stories/SvgIcon.story.vue
+++ app/gui2/stories/SvgIcon.story.vue
@@ -3,18 +3,47 @@ import { logEvent } from 'histoire/client'
 import { ref } from 'vue'
 
 import SvgIcon from '@/components/SvgIcon.vue'
+import ToggleIcon from '@/components/ToggleIcon.vue'
 
 import iconList from '@/util/iconList.json'
 
 const name = ref('enso_logo')
+
+const toggledOn = ref(false)
+const icon = ref('enso_logo')
 </script>
 
 <template>
-  <Story title="SVG Icon" group="misc" :layout="{ type: 'grid', width: 500 }" auto-props-disabled>
-    <SvgIcon :name="name" @click="logEvent('click', [])" />
+  <Story title="SVG Icon" group="misc" :layout="{ type: 'grid', width: 200 }" auto-props-disabled>
+    <Variant title="icon">
+      <SvgIcon :name="name" @click="logEvent('click', [])" />
 
-    <template #controls>
-      <HstSelect v-model="name" :options="iconList" />
-    </template>
+      <template #controls>
+        <HstSelect v-model="name" title="name" :options="iconList" />
+      </template>
+    </Variant>
+    <Variant title="toggle-icon">
+      <ToggleIcon
+        v-model="toggledOn"
+        :icon="icon"
+        class="toggle-icon"
+        @click="logEvent('click', [])"
+      />
+
+      <template #controls>
+        <HstCheckbox v-model="toggledOn" title="v-model" :options="iconList" />
+        <HstSelect v-model="icon" title="icon" :options="iconList" />
+      </template>
+    </Variant>
   </Story>
 </template>
+
+<style scoped>
+.toggle-icon {
+  opacity: 30%;
+}
+
+.toggle-icon.toggledOn {
+  opacity: unset;
+}
+</style>
