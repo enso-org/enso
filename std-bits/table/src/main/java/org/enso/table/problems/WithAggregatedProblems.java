05ebb2a100913f8c6d82323bf16c6aa9a1f1dc23
Radosław Waśko <radoslaw.wasko@enso.org>
2023-10-10 18:14:15 +0200

    remove more stuff that is not used anymore

--- std-bits/table/src/main/java/org/enso/table/problems/WithAggregatedProblems.java
+++
@@ -1,6 +0,0 @@
-package org.enso.table.problems;
-
-/**  A value annotated with aggregated problems that occurred when it was being computed. */
-@Deprecated
-public record WithAggregatedProblems<T>(T value, AggregatedProblems problems) {
-}
