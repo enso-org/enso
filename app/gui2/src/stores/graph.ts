b64a7d392ff7a0cf8263416a05b5fbc3b6b56118
somebody1234 <ehern.lee@gmail.com>
2023-10-11 23:25:38 +1000

    Node icons and colors (#7988)
    Adds node icons and colors.
    
    # Important Notes
    None

--- app/gui2/src/stores/graph.ts
+++ app/gui2/src/stores/graph.ts
@@ -1,3 +1,5 @@
+import { useProjectStore } from '@/stores/project'
+import { DEFAULT_VISUALIZATION_IDENTIFIER } from '@/stores/visualization'
 import { assert, assertNever } from '@/util/assert'
 import { Ast, parseEnso } from '@/util/ast'
 import { useObserveYjs } from '@/util/crdt'
@@ -18,8 +20,6 @@ import {
 } from 'shared/yjsModel'
 import { computed, reactive, ref, watch } from 'vue'
 import * as Y from 'yjs'
-import { useProjectStore } from './project'
-import { DEFAULT_VISUALIZATION_IDENTIFIER } from './visualization'
 
 export const useGraphStore = defineStore('graph', () => {
   const proj = useProjectStore()
