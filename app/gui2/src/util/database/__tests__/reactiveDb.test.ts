03d91420d5f622f961a83d855d20e73911c1b1eb
Ilya Bogdanov <fumlead@gmail.com>
2023-10-13 16:13:38 +0400

    wip reactive db

--- app/gui2/src/util/database/__tests__/reactiveDb.test.ts
+++ app/gui2/src/util/database/__tests__/reactiveDb.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from 'vitest'
+import { expect, test, vi } from 'vitest'
 import { ReactiveDb } from '@/util/database/reactiveDb'
 import { ReactiveIndex } from '@/util/database/reactiveDb'
 import { setIfUndefined } from 'lib0/map'
@@ -17,51 +17,86 @@ test('Basic add/remove', () => {
   expect(db.get('Key 1')).toBeUndefined()
 })
 
+test('Indexing is efficient', () => {
+  const db = new ReactiveDb()
+  const index = new ReactiveIndex(db, (id, entry, index) => index(entry.name, id))
+  const adding = vi.spyOn(index, 'writeToIndex')
+  const removing = vi.spyOn(index, 'removeFromIndex')
+  db.set(1, { name: 'abc' })
+  db.set(2, { name: 'xyz' })
+  db.set(3, { name: 'abc' })
+  db.delete(2)
+  expect(adding).toHaveBeenCalledTimes(3)
+  expect(removing).toHaveBeenCalledTimes(1)
+  db.set(1, { name: 'qdr' })
+  expect(adding).toHaveBeenCalledTimes(4)
+  expect(removing).toHaveBeenCalledTimes(2)
+})
+
 test('Name to id index', () => {
   const db = new ReactiveDb()
-  const index = new ReactiveIndex(db,
-								  (key, value, index) => {
-									const set = setIfUndefined(index, value.name, () => new Set())
-									set.add(key)
-  },
-								  (key, value, index) => {
-									index.set(key, value.name)
-  })
+  const index = new ReactiveIndex(db, (id, entry, index) => index(entry.name, id))
   db.set(1, { name: 'abc' })
   db.set(2, { name: 'xyz' })
   db.set(3, { name: 'abc' })
   expect(index.lookup('abc')).toEqual(new Set([1, 3]))
   expect(index.lookup('xyz')).toEqual(new Set([2]))
-  expect(index.reverseLookup(1)).toEqual('abc')
-  expect(index.reverseLookup(2)).toEqual('xyz')
+  expect(index.lookup('qdr')).toEqual(new Set())
+  expect(index.reverseLookup(1)).toEqual(new Set(['abc']))
+  expect(index.reverseLookup(2)).toEqual(new Set(['xyz']))
+  expect(index.reverseLookup(3)).toEqual(new Set(['abc']))
+
+  db.delete(2)
+  expect(index.lookup('xyz')).toEqual(new Set([]))
+  db.delete(1)
+  expect(index.lookup('abc')).toEqual(new Set([3]))
+  db.set(3, { name: 'qdr' })
+  expect(index.lookup('abc')).toEqual(new Set())
+  expect(index.lookup('qdr')).toEqual(new Set([3]))
 })
 
 test('Parent index', async () => {
   const db = new ReactiveDb()
   const qnIndex = reactive(new ReactiveIndex(db,
-								  (key, value, index) => {
-									const set = setIfUndefined(index, value.name, () => new Set())
-									set.add(key)
-  },
-								  (key, value, index) => {
-									index.set(key, value.name)
+											 (id, entry, index) => {
+											   console.log('index qnIndex')
+											   index(entry.name, id)
   }))
-  const children = new ReactiveIndex(db, (key, value, index) => {
-	watch(qnIndex, (qnIndex) => {
-	  if (!qnIndex.lookup(value.definedIn)) return
-	  for (const id of qnIndex.lookup(value.definedIn).values()) {
-		const set = setIfUndefined(index, id, () => new Set())
-		set.add(key)
-	  }
-	}, { immediate: true })
-  },
-									 (key, value, index) => {
-									
+  const children = new ReactiveIndex(db, (id, entry, index, onDelete) => {
+	console.log('index children')
+ 	const stop = watch(qnIndex, (qnIndex) => {
+ 	  qnIndex.lookup(entry.definedIn).forEach((parentId) => index(parentId, id))
+ 	}, { immediate: true })
+ 	onDelete(() => stop())
   })
+  //const lookupQn = vi.spyOn(qnIndex, 'lookup')
+  //const adding = vi.spyOn(children, 'writeToIndex')
+  //const removing = vi.spyOn(children, 'removeFromIndex')
   db.set(1, { name: 'foo', definedIn: 'A' })
   db.set(2, { name: 'A' })
   db.set(3, { name: 'bar', definedIn: 'A' })
   await nextTick()
-  expect(children.lookup(2)).toEqual(new Set([1, 3]))
-  //expect(children.reverseLookup(1)).toStrictEqual(2)
+  expect(qnIndex.lookup('foo')).toEqual(new Set([1]))
+  expect(qnIndex.lookup('A')).toEqual(new Set([2]))
+  expect(qnIndex.lookup('bar')).toEqual(new Set([3]))
+  // expect(children.lookup(1)).toEqual(new Set())
+  // expect(children.lookup(2)).toEqual(new Set([1, 3]))
+  // expect(children.lookup(3)).toEqual(new Set())
+  // expect(children.reverseLookup(1)).toStrictEqual(new Set([2]))
+  // expect(children.reverseLookup(2)).toEqual(new Set())
+  // expect(children.reverseLookup(3)).toStrictEqual(new Set([2]))
+  //expect(adding).toHaveBeenCalledTimes(2)
+  //expect(removing).toHaveBeenCalledTimes(0)
+  //expect(lookupQn).toHaveBeenCalledTimes(5)
+
+  db.delete(3)
+  await nextTick()
+  expect(qnIndex.lookup('foo')).toEqual(new Set([1]))
+  expect(qnIndex.lookup('A')).toEqual(new Set([2]))
+  expect(qnIndex.lookup('bar')).toEqual(new Set([]))
+  // expect(children.lookup(2)).toEqual(new Set([1]))
+  // expect(children.reverseLookup(3)).toEqual(new Set())
+  // expect(adding).toHaveBeenCalledTimes(2)
+  // expect(removing).toHaveBeenCalledTimes(1)
+  // expect(lookupQn).toHaveBeenCalledTimes(5)
 })
--- app/gui2/src/util/database/reactiveDb.ts
+++ app/gui2/src/util/database/reactiveDb.ts
@@ -1,42 +1,91 @@
+/*
+   Reactive-friendly key-value database.
+
+   `ReactiveDb` is a database itself, a thin adapter around ordinary `Map`.
+
+   `ReactiveIndex` is an abstraction for building arbitrary database indexing with reactive updates.
+*/
+
 import { ObservableV2 } from 'lib0/observable'
 import { reactive } from 'vue'
+import { setIfUndefined } from 'lib0/map'
 
-export class ReactiveDb<Key, Value> extends ObservableV2<[Key, Value]> {
+export type CleanupFn = () => void
+export type OnCleanup = (cleanupFn: CleanupFn) => void
+
+export class ReactiveDb<Key, Value> extends ObservableV2<{ entryAdded(key: Key, value: Value, onCleanup: OnCleanup): void }> {
   internal: Map<Key, Value>
+  onCleanup: Map<Key, CleanupFn>
   constructor() {
 	super()
 	this.internal = new Map()
+	this.onCleanup = new Map()
   }
   set(key: Key, value: Value) {
+	if (this.internal.has(key)) {
+	  this.delete(key)
+	}
 	this.internal.set(key, value)
-	this.emit('keyadded', [key, value])
+	const onCleanup = (callback) => {
+	  this.onCleanup.set(key, callback)
+	}
+	this.emit('entryAdded', [key, value, onCleanup])
   }
   get(key: Key): Value | undefined {
 	return this.internal.get(key)
   }
   delete(key: Key): boolean {
+	const callback = this.onCleanup.get(key)
+	if (callback) {
+	  callback()
+	  this.onCleanup.delete(key)
+	}
 	return this.internal.delete(key)
   }
-  get size() {
+  get size(): number {
 	return this.internal.size
   }
 }
 
-export class ReactiveIndex<IndexKey, IndexValue> {
+export type IndexFn<IndexKey, IndexValue> = (key: IndexKey, value: IndexValue)
+export type Indexer<Key, Value, IndexKey, IndexValue> = (key: Key, value: Value, index: IndexFn<IndexKey, IndexValue>, onDelete: OnCleanup): void
+
+export class ReactiveIndex<Db, IndexKey, IndexValue> {
   straight: Map<IndexKey, Set<IndexValue>>
-  reverse: Map<IndexValue, IndexKey>
-  constructor(db: ReactiveDb, indexer, reverseIndexer) {
+  reverse: Map<IndexValue, Set<IndexKey>>
+  constructor(db: ReactiveDb<Key, Value>, indexer: Indexer<Key, Value>) {
 	this.straight = reactive(new Map())
 	this.reverse = reactive(new Map())
-	db.on('keyadded', (key, value) => {
-	  indexer(key, value, this.straight)
-	  reverseIndexer(key, value, this.reverse)
+	db.on('entryAdded', (key, value, onDelete) => {
+	  let indexOnDeleteCb = null
+	  const index = (key, value) => {
+		this.writeToIndex(key, value)
+		onDelete(() => {
+		  if (indexOnDeleteCb) indexOnDeleteCb()
+		  this.removeFromIndex(key, value)
+		})
+	  }
+	  const userOnDelete = (callback) => { indexOnDeleteCb = callback }
+	  indexer(key, value, index, userOnDelete)
 	})
   }
-  lookup(key: IndexKey): Set<IndexValue> | undefined {
-	return this.straight.get(key)
+  writeToIndex(key: IndexKey, value: IndexValue): void {
+	const straight = setIfUndefined(this.straight, key, () => new Set())
+	straight.add(value)
+	const reverse = setIfUndefined(this.reverse, value, () => new Set())
+	reverse.add(key)
   }
-  reverseLookup(value: IndexValue): Set<IndexKey> | undefined {
-	return this.reverse.get(value)
+  removeFromIndex(key: IndexKey, value: IndexValue): void {
+	const remove = (map, key, value) => {
+	  map.get(key)?.delete(value)
+	}
+	remove(this.straight, key, value)
+	remove(this.reverse, value, key)
+  }
+  lookup(key: IndexKey): Set<IndexValue> {
+	return this.straight.get(key) ?? new Set()
+  }
+  reverseLookup(value: IndexValue): Set<IndexKey> {
+	return this.reverse.get(value) ?? new Set()
   }
 }
