63a09d69a20bb54faab6d10457a711a3068b4fc7
Radosław Waśko <radoslaw.wasko@enso.org>
2023-10-17 18:53:22 +0200

    javafmt

--- test/Benchmarks/polyglot-sources/benchmark-java-helpers/src/main/java/org/enso/benchmark_helpers/JavaHashMapWrapper.java
+++ test/Benchmarks/polyglot-sources/benchmark-java-helpers/src/main/java/org/enso/benchmark_helpers/JavaHashMapWrapper.java
@@ -3,9 +3,9 @@ package org.enso.benchmark_helpers;
 import java.util.HashMap;
 
 /**
- * Wraps a Java HashMap into an interface hiding it, to ensure that we are calling the raw HashMap and are not using the
- * Enso conversions that may be applied automatically. This allows us to compare the raw HashMap performance with other
- * variants.
+ * Wraps a Java HashMap into an interface hiding it, to ensure that we are calling the raw HashMap
+ * and are not using the Enso conversions that may be applied automatically. This allows us to
+ * compare the raw HashMap performance with other variants.
  */
 public class JavaHashMapWrapper {
   private final HashMap<Object, Object> map = new HashMap<>();
