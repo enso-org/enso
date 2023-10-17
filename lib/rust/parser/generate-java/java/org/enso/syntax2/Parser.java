8ceca031e4b75c9c06e9c10f1e58dd44b456cf69
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-10-17 16:54:11 +0200

    Mocked parser to re-play output for 'main = 42'

--- lib/rust/parser/generate-java/java/org/enso/syntax2/Parser.java
+++ lib/rust/parser/generate-java/java/org/enso/syntax2/Parser.java
@@ -1,76 +1,55 @@
 package org.enso.syntax2;
 
-import java.io.File;
-import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.nio.charset.StandardCharsets;
 
 public final class Parser implements AutoCloseable {
-  static {
-    String os = System.getProperty("os.name");
-    String name;
-    if (os.startsWith("Mac")) {
-      name = "libenso_parser.dylib";
-    } else if (os.startsWith("Windows")) {
-      name = "enso_parser.dll";
-    } else {
-      name = "libenso_parser.so";
-    }
-
-    File parser = null;
-    try {
-      var whereAmI = Parser.class.getProtectionDomain().getCodeSource().getLocation();
-      File dir = new File(whereAmI.toURI()).getParentFile();
-      parser = new File(dir, name);
-      System.load(parser.getAbsolutePath());
-    } catch (URISyntaxException | LinkageError e) {
-      File root = new File(".").getAbsoluteFile();
-      if (!searchFromDirToTop(e, root, "target", "rust", "debug", name)) {
-        throw new IllegalStateException("Cannot load parser from " + parser, e);
-      }
-    }
-  }
-
-  private static boolean searchFromDirToTop(Throwable chain, File root, String... names) {
-    while (root != null) {
-      var parser = root;
-      for (var e : names) {
-        parser = new File(parser, e);
-      }
-      try {
-        System.load(parser.getAbsolutePath());
-        return true;
-      } catch (LinkageError err) {
-        while (chain.getCause() != null) {
-          chain = chain.getCause();
-        }
-        chain.initCause(err);
-        root = root.getParentFile();
-      }
-    }
-    return false;
-  }
-
   private long state;
 
   private Parser(long stateIn) {
     state = stateIn;
   }
 
-  private static native long allocState();
+  private static long allocState() {
+    return 0;
+  }
 
-  private static native void freeState(long state);
+  private static void freeState(long state) {}
 
-  private static native ByteBuffer parseInput(long state, ByteBuffer input);
+  private static ByteBuffer parseInput(long state, ByteBuffer input) {
+    // fake output representing simple Enso source:
+    // main = 42
+    return ByteBuffer.wrap(
+        new byte[] {
+          0, 0, 0, 0, -95, 68, 55, 103, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 1, 0, 0, 0,
+          1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -42, 67, 55, 103, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          -95, 68, 55, 103, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, -42, 67, 55, 103, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 19, 0, 0, 0, 0, 0, 0, 0, -42, 67, 55, 103, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, -42, 67, 55, 103, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 32, -12, 58, -76, 4, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 36, -12, 58, -76, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 0, 37,
+          -12, 58, -76, 1, 0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 38, -12, 58, -76, 1, 0, 0,
+          0, 6, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 6, 0, 0, 0, 0, 1, 0, 0, 0, 0, -42, 67, 55, 103, 0,
+          0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 39, -12, 58, -76, 2, 0, 0, 0, 7, 0, 0, 0, 2, 0, 0, 0, 0,
+          0
+        });
+  }
 
-  private static native long getLastInputBase(long state);
+  private static long getLastInputBase(long state) {
+    return 139726899835936L;
+  }
 
-  private static native long getMetadata(long state);
+  private static long getMetadata(long state) {
+    return 0L;
+  }
 
-  static native long getUuidHigh(long metadata, long codeOffset, long codeLength);
+  static long getUuidHigh(long metadata, long codeOffset, long codeLength) {
+    return 0L;
+  }
 
-  static native long getUuidLow(long metadata, long codeOffset, long codeLength);
+  static long getUuidLow(long metadata, long codeOffset, long codeLength) {
+    return 0L;
+  }
 
   public static Parser create() {
     var state = allocState();
@@ -78,13 +57,13 @@ public final class Parser implements AutoCloseable {
   }
 
   public Tree parse(CharSequence input) {
-    byte[] inputBytes = input.toString().getBytes(StandardCharsets.UTF_8);
-    ByteBuffer inputBuf = ByteBuffer.allocateDirect(inputBytes.length);
-    inputBuf.put(inputBytes);
-    var serializedTree = parseInput(state, inputBuf);
+    var serializedTree = parseInput(state, null);
     var base = getLastInputBase(state);
     var metadata = getMetadata(state);
-    serializedTree.order(ByteOrder.LITTLE_ENDIAN);
+    try {
+      serializedTree.order(ByteOrder.LITTLE_ENDIAN);
+    } catch (Throwable ex) {
+    }
     var message = new Message(serializedTree, input, base, metadata);
     return Tree.deserialize(message);
   }
