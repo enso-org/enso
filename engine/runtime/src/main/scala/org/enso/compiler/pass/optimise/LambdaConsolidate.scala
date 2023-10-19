db6dd613ddb444899f681914c875103e93be5436
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2023-10-19 18:06:40 +0200

    handle more cases

--- engine/runtime/src/main/scala/org/enso/compiler/pass/optimise/LambdaConsolidate.scala
+++ engine/runtime/src/main/scala/org/enso/compiler/pass/optimise/LambdaConsolidate.scala
@@ -406,7 +406,7 @@ case object LambdaConsolidate extends IRPass {
     * @param argsWithShadowed the args with whether or not they are shadowed
     * @return a set of argument names, with shadowed arguments replaced
     */
-  def generateNewNames(
+  private def generateNewNames(
     argsWithShadowed: List[(DefinitionArgument, Boolean)],
     freshNameSupply: FreshNameSupply
   ): List[DefinitionArgument] = {
@@ -418,13 +418,12 @@ case object LambdaConsolidate extends IRPass {
         val newName =
           if (isShadowed) {
             freshNameSupply
-              .newName()
+              .newName(from = Some(name))
               .copy(
-                location     = name.location,
-                originalName = Some(name),
-                passData     = name.passData,
-                diagnostics  = name.diagnostics,
-                id           = name.getId
+                location    = name.location,
+                passData    = name.passData,
+                diagnostics = name.diagnostics,
+                id          = name.getId
               )
           } else name
 
--- engine/runtime/src/test/scala/org/enso/compiler/test/pass/lint/UnusedBindingsTest.scala
+++ engine/runtime/src/test/scala/org/enso/compiler/test/pass/lint/UnusedBindingsTest.scala
@@ -230,5 +230,35 @@ class UnusedBindingsTest extends CompilerTest with Inside {
         }
       }
     }
+
+    "report human-readable names for shadowed bindings in patterns" in {
+      implicit val ctx: InlineContext = mkInlineContext
+
+      val ir =
+        """
+          |case x of
+          |    Cons a a -> 10
+          |""".stripMargin.preprocessExpression.get.lint
+          .asInstanceOf[Expression.Block]
+          .returnValue
+          .asInstanceOf[Case.Expr]
+
+      val pattern = ir.branches.head.pattern.asInstanceOf[Pattern.Constructor]
+      val field1  = pattern.fields.head.asInstanceOf[Pattern.Name]
+      val field2  = pattern.fields(1).asInstanceOf[Pattern.Name]
+
+      val lintMeta1 = field1.diagnostics.collect { case u: warnings.Unused =>
+        u
+      }
+      val lintMeta2 = field2.diagnostics.collect { case u: warnings.Unused =>
+        u
+      }
+
+      lintMeta2 should not be empty
+      lintMeta2.head shouldBe an[warnings.Unused.PatternBinding]
+      lintMeta2.head.name.name shouldEqual "a"
+
+      lintMeta1 shouldBe empty
+    }
   }
 }
