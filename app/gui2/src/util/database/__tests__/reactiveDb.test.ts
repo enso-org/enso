e68257d563d11c182b18aca0a910c03daed63a5e
somebody1234 <ehern.lee@gmail.com>
2023-10-19 21:34:06 +1000

    Switch from manual monkeypatching to `mockImplementation` + `restoreMocks`

--- app/gui2/src/util/database/__tests__/reactiveDb.test.ts
+++ app/gui2/src/util/database/__tests__/reactiveDb.test.ts
@@ -38,21 +38,16 @@ test('Indexing is efficient', () => {
 
 test('Error reported when indexer implementation returns non-unique pairs', () => {
   const db = new ReactiveDb()
-  const actualConsoleError = console.error
   console.error = () => {}
-  try {
-    const consoleError = vi.spyOn(console, 'error')
-    // Invalid index
-    new ReactiveIndex(db, (_id, _entry) => [[1, 1]])
-    db.set(1, 1)
-    db.set(2, 2)
-    expect(consoleError).toHaveBeenCalledOnce()
-    expect(consoleError).toHaveBeenCalledWith(
-      'Attempt to repeatedly write the same key-value pair (1,1) to the index. Please check your indexer implementation.',
-    )
-  } finally {
-    console.error = actualConsoleError
-  }
+  const consoleError = vi.spyOn(console, 'error').mockImplementation(() => {})
+  // Invalid index
+  new ReactiveIndex(db, (_id, _entry) => [[1, 1]])
+  db.set(1, 1)
+  db.set(2, 2)
+  expect(consoleError).toHaveBeenCalledOnce()
+  expect(consoleError).toHaveBeenCalledWith(
+    'Attempt to repeatedly write the same key-value pair (1,1) to the index. Please check your indexer implementation.',
+  )
 })
 
 test('Name to id index', () => {
