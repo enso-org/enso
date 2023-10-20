cb30a2cce30b086675d2bc5472459c5cb5001d55
somebody1234 <ehern.lee@gmail.com>
2023-10-20 19:09:19 +1000

    Improve tests

--- app/gui2/src/components/ComponentBrowser/__tests__/placement.test.ts
+++ app/gui2/src/components/ComponentBrowser/__tests__/placement.test.ts
@@ -14,79 +14,61 @@ import { expect, test, vi } from 'vitest'
 const defaultScreenBounds = new Rect(new Vec2(100, 200), new Vec2(2000, 1000))
 const defaultNodeSize = new Vec2(100, 20)
 
-function environment(
-  screenBounds = defaultScreenBounds,
-  overrides: Partial<Omit<Environment, 'screenBounds'>> = {},
-): Environment {
-  return {
-    screenBounds,
-    nodeRects: iterable.empty(),
-    selectedNodeRects: iterable.empty(),
-    mousePosition: Vec2.Zero,
-    ...overrides,
-  }
-}
-
 test('Non dictated placement', () => {
+  const getSelectedNodeRects = vi.fn(() => iterable.empty())
+  const getMousePosition = vi.fn(() => Vec2.Zero)
   // Center is at (1100, 700)
   const screenBounds = defaultScreenBounds
   // Half of this is (50, 10)
   const nodeSize = defaultNodeSize
-  expect(nonDictatedPlacement(nodeSize, environment(screenBounds))).toEqual<Placement>({
+  function environment(nodeRects: Iterable<Rect>): Environment {
+    return {
+      screenBounds,
+      nodeRects,
+      get selectedNodeRects() {
+        return getSelectedNodeRects()
+      },
+      get mousePosition() {
+        return getMousePosition()
+      },
+    }
+  }
+  expect(nonDictatedPlacement(nodeSize, environment([]))).toEqual<Placement>({
     position: new Vec2(1050, 690),
   })
   // Single node
   expect(
-    nonDictatedPlacement(
-      nodeSize,
-      environment(screenBounds, { nodeRects: [new Rect(new Vec2(1050, 690), defaultNodeSize)] }),
-    ),
+    nonDictatedPlacement(nodeSize, environment([new Rect(new Vec2(1050, 690), defaultNodeSize)])),
     // 20px existing node height + 24px gap
   ).toEqual<Placement>({ position: new Vec2(1050, 734) })
   // Single node (far enough left that it does not overlap)
   expect(
-    nonDictatedPlacement(
-      nodeSize,
-      environment(screenBounds, { nodeRects: [new Rect(new Vec2(950, 690), defaultNodeSize)] }),
-    ),
+    nonDictatedPlacement(nodeSize, environment([new Rect(new Vec2(950, 690), defaultNodeSize)])),
   ).toEqual<Placement>({ position: new Vec2(1050, 690) })
   // Single node (far enough right that it does not overlap)
   expect(
-    nonDictatedPlacement(
-      nodeSize,
-      environment(screenBounds, { nodeRects: [new Rect(new Vec2(1150, 690), defaultNodeSize)] }),
-    ),
+    nonDictatedPlacement(nodeSize, environment([new Rect(new Vec2(1150, 690), defaultNodeSize)])),
   ).toEqual<Placement>({ position: new Vec2(1050, 690) })
   // Single node (barely overlaps on the left)
   expect(
-    nonDictatedPlacement(
-      nodeSize,
-      environment(screenBounds, { nodeRects: [new Rect(new Vec2(951, 690), defaultNodeSize)] }),
-    ),
+    nonDictatedPlacement(nodeSize, environment([new Rect(new Vec2(951, 690), defaultNodeSize)])),
   ).toEqual<Placement>({ position: new Vec2(1050, 734) })
   // Single node (barely overlaps on the right)
   expect(
-    nonDictatedPlacement(
-      nodeSize,
-      environment(screenBounds, { nodeRects: [new Rect(new Vec2(1149, 690), defaultNodeSize)] }),
-    ),
+    nonDictatedPlacement(nodeSize, environment([new Rect(new Vec2(1149, 690), defaultNodeSize)])),
   ).toEqual<Placement>({ position: new Vec2(1050, 734) })
   // Single node (BIG gap)
   expect(
-    nonDictatedPlacement(
-      nodeSize,
-      environment(screenBounds, { nodeRects: [new Rect(new Vec2(1050, 690), defaultNodeSize)] }),
-      { gap: 1000 },
-    ),
+    nonDictatedPlacement(nodeSize, environment([new Rect(new Vec2(1050, 690), defaultNodeSize)]), {
+      gap: 1000,
+    }),
     // 20px existing node height + 1000px gap
   ).toEqual<Placement>({ position: new Vec2(1050, 1710), pan: new Vec2(0, 1020) })
   // Multiple nodes
   expect(
     nonDictatedPlacement(
       nodeSize,
-      environment(screenBounds, {
-        nodeRects: map(range(0, 1001, 20), (y) => new Rect(new Vec2(1050, y), defaultNodeSize)),
-      }),
+      environment(map(range(0, 1001, 20), (y) => new Rect(new Vec2(1050, y), defaultNodeSize))),
     ),
   ).toEqual<Placement>({
     // The bottom-most node ends at 1020, and there should be a 24px gap.
@@ -96,9 +78,7 @@ test('Non dictated placement', () => {
   expect(
     nonDictatedPlacement(
       nodeSize,
-      environment(screenBounds, {
-        nodeRects: map(range(1000, -1, -20), (y) => new Rect(new Vec2(1050, y), defaultNodeSize)),
-      }),
+      environment(map(range(1000, -1, -20), (y) => new Rect(new Vec2(1050, y), defaultNodeSize))),
     ),
   ).toEqual<Placement>({
     // Same as above, but with node rects reversed
@@ -108,27 +88,47 @@ test('Non dictated placement', () => {
   expect(
     nonDictatedPlacement(
       nodeSize,
-      environment(screenBounds, {
-        nodeRects: chain(
+      environment(
+        chain(
           map(range(500, 901, 20), (y) => new Rect(new Vec2(1050, y), defaultNodeSize)),
           map(range(1000, 1501, 20), (y) => new Rect(new Vec2(1050, y), defaultNodeSize)),
         ),
-      }),
+      ),
     ),
   ).toEqual<Placement>({
     // There is a 100px gap between y = 900 and y = 1000, which is more than large enough for this node.
     position: new Vec2(1050, 944),
   })
+  // Multiple nodes with gap (just big enough)
+  expect(
+    nonDictatedPlacement(
+      nodeSize,
+      environment(map(range(690, 1500, 88), (y) => new Rect(new Vec2(1050, y), defaultNodeSize))),
+    ),
+  ).toEqual<Placement>({
+    position: new Vec2(1050, 734),
+  })
+  // Multiple nodes with gap (slightly too small)
+  expect(
+    nonDictatedPlacement(
+      nodeSize,
+      // 500 + 87 * 4 === 848
+      environment(map(range(500, 849, 87), (y) => new Rect(new Vec2(1050, y), defaultNodeSize))),
+    ),
+  ).toEqual<Placement>({
+    // 848 + 20 + 24
+    position: new Vec2(1050, 892),
+  })
   // Multiple nodes with smallest gap
   expect(
     nonDictatedPlacement(
       nodeSize,
-      environment(screenBounds, {
-        nodeRects: chain(
+      environment(
+        chain(
           map(range(500, 901, 20), (y) => new Rect(new Vec2(1050, y), defaultNodeSize)),
           map(range(988, 1489, 20), (y) => new Rect(new Vec2(1050, y), defaultNodeSize)),
         ),
-      }),
+      ),
     ),
   ).toEqual<Placement>({
     // There is a 68px gap (20px height + 24px gap * 2) between y = 920 (the bottom of the previous node)
@@ -139,12 +139,12 @@ test('Non dictated placement', () => {
   expect(
     nonDictatedPlacement(
       nodeSize,
-      environment(screenBounds, {
-        nodeRects: chain(
+      environment(
+        chain(
           map(range(1488, 987, -20), (y) => new Rect(new Vec2(1050, y), defaultNodeSize)),
           map(range(900, 499, -20), (y) => new Rect(new Vec2(1050, y), defaultNodeSize)),
         ),
-      }),
+      ),
     ),
   ).toEqual<Placement>({
     // There is a 68px gap (20px height + 24px gap * 2) between y = 920 (the bottom of the previous node)
@@ -155,12 +155,12 @@ test('Non dictated placement', () => {
   expect(
     nonDictatedPlacement(
       nodeSize,
-      environment(screenBounds, {
-        nodeRects: chain(
+      environment(
+        chain(
           map(range(500, 901, 20), (y) => new Rect(new Vec2(1050, y), defaultNodeSize)),
           map(range(987, 1488, 20), (y) => new Rect(new Vec2(1050, y), defaultNodeSize)),
         ),
-      }),
+      ),
     ),
   ).toEqual<Placement>({
     // This gap is 1px smaller than the previous test - so, 1px too small.
@@ -172,12 +172,12 @@ test('Non dictated placement', () => {
   expect(
     nonDictatedPlacement(
       nodeSize,
-      environment(screenBounds, {
-        nodeRects: chain(
+      environment(
+        chain(
           map(range(900, 499, -20), (y) => new Rect(new Vec2(1050, y), defaultNodeSize)),
           map(range(1487, 986, -20), (y) => new Rect(new Vec2(1050, y), defaultNodeSize)),
         ),
-      }),
+      ),
     ),
   ).toEqual<Placement>({
     // This gap is 1px smaller than the previous test - so, 1px too small.
@@ -185,6 +185,8 @@ test('Non dictated placement', () => {
     // This position is offscreen (y >= 1000), so we pan so that the new node is centered (1531 - 690).
     pan: new Vec2(0, 841),
   })
+  expect(getSelectedNodeRects, 'Should not depend on `selectedNodeRects`').toBeCalledTimes(0)
+  expect(getMousePosition, 'Should not depend on `mousePosition`').toBeCalledTimes(0)
 })
 
 test('Mouse dictated placement', () => {
