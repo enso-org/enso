32498f10cab4e171951440d763b1973190e746d4
Gregory Travis <greg.travis@enso.org>
2023-10-24 11:56:37 -0400

    add RIH

---
+++ std-bits/base/src/main/java/org/enso/base/random/RandomInstanceHolder.java
@@ -0,0 +1,33 @@
+package org.enso.base.random;
+
+import java.util.Random;
+
+/**
+ * Container for a `Random` object. Setting the seed requires recreating the
+ * `Random` object, so the `Random` should always be obtained through
+ * `getCurrentRandom`.
+ */
+public class RandomInstanceHolder {
+    private RandomInstanceHolder() {
+    }
+
+    public RandomInstanceHolder(long seed) {
+        setSeed(seed);
+    }
+
+    private Random currentRandom = null;
+
+    public Random getCurrentRandom() {
+        if (currentRandom == null) {
+            setSeed(Random_Utils.getDefaultSeed());
+        }
+        return currentRandom;
+    }
+
+    public void setSeed(long seed) {
+        currentRandom = new Random(seed);
+    }
+
+    public static RandomInstanceHolder SINGLETON = new RandomInstanceHolder();
+    //public RandomInstanceHolder getSingleton() { return SINGLETON; }
+}
