0b759f047563988aa336a4f3c49c8f62463990dc
Pavel Marek <pavel.marek@enso.org>
2023-10-24 12:44:08 +0200

    Specify uses slf4j provider in module-info

--- engine/runtime-with-instruments/src/main/java/module-info.java
+++ engine/runtime-with-instruments/src/main/java/module-info.java
@@ -2,6 +2,10 @@ module org.enso.runtime {
   requires java.base;
   requires org.graalvm.polyglot;
   requires org.graalvm.truffle;
+  requires org.slf4j;
+
+  uses org.slf4j.spi.SLF4JServiceProvider;
+
   provides com.oracle.truffle.api.provider.TruffleLanguageProvider with
       org.enso.interpreter.EnsoLanguageProvider,
       org.enso.interpreter.epb.EpbLanguageProvider;
