34fd622844520aa0602d48176464cd285a162764
Pavel Marek <pavel.marek@enso.org>
2023-10-25 10:23:08 +0200

    EnsoBoottakes path to runner.jar optionally from a system property.

--- engine/runtime/src/main/java/org/enso/EnsoBoot.java
+++ engine/runtime/src/main/java/org/enso/EnsoBoot.java
@@ -1,5 +1,6 @@
 package org.enso;
 
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Path;
@@ -7,20 +8,30 @@ import java.nio.file.Path;
 public final class EnsoBoot {
   private EnsoBoot() {}
 
+  private static final String defaultRunnerJar = "runner/runner.jar";
+
   public static void main(String[] args) throws Exception {
-    var runtimeJar = EnsoBoot.class.getProtectionDomain().getCodeSource().getLocation();
-    var runnerJarUri = runtimeJar.toURI().resolve("runner/runner.jar");
-    var runnerJarPath = Path.of(runnerJarUri);
-    var runnerJar = runnerJarPath.toFile();
-    if (!runnerJar.exists()) {
-      throw new IllegalStateException("Cannot find " + runnerJar + ". Tried path: " + runnerJarPath);
+    var ensoRunnerProp = System.getProperty("enso.runner");
+    var runnerJarPath = ensoRunnerProp != null ? Path.of(ensoRunnerProp) : getDefaultRunnerJarPath();
+    if (!runnerJarPath.toFile().exists()) {
+      throw new IllegalStateException("Cannot find runner fat jar at " + runnerJarPath);
     }
-    var url = runnerJar.toURI().toURL();
+    var url = runnerJarPath.toUri().toURL();
 
     var loader = new PrefferingLoader(new URL[] {url});
     var clazz = loader.loadClass("org.enso.runner.Main");
     var main = clazz.getMethod("main", String[].class);
     main.invoke(null, (Object) args);
+  }
+
+  private static Path getDefaultRunnerJarPath() {
+    var runtimeJar = EnsoBoot.class.getProtectionDomain().getCodeSource().getLocation();
+    try {
+      var runnerJarUri = runtimeJar.toURI().resolve(defaultRunnerJar);
+      return Path.of(runnerJarUri);
+    } catch (URISyntaxException e) {
+      throw new IllegalStateException(e);
+    }
   }
 
   private static class PrefferingLoader extends URLClassLoader {
