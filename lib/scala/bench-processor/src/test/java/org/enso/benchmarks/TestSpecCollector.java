87dcc5564f4f10f98e65c9f7a96e0410a1c32831
Pavel Marek <pavel.marek@enso.org>
2023-10-27 16:13:33 +0200

    fmt

--- lib/scala/bench-processor/src/test/java/org/enso/benchmarks/TestSpecCollector.java
+++ lib/scala/bench-processor/src/test/java/org/enso/benchmarks/TestSpecCollector.java
@@ -63,8 +63,8 @@ public class TestSpecCollector {
 
   /**
    * Locates the root of the Enso repository. Heuristic: we just keep going up the directory tree
-   * until we are in a directory containing ".git" subdirectory. Note that we cannot use the
-   * "enso" name, as users are free to name their cloned directories however they like.
+   * until we are in a directory containing ".git" subdirectory. Note that we cannot use the "enso"
+   * name, as users are free to name their cloned directories however they like.
    */
   private File locateRootDirectory() {
     File rootDir = null;
