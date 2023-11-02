b5836973db0cb382b8ea3ad95f20cee87b3a27e1
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-11-02 10:41:46 +0100

    Measure the expected size of the serialized data

--- engine/runtime-parser/src/test/java/org/enso/compiler/core/PersistanceTest.java
+++ engine/runtime-parser/src/test/java/org/enso/compiler/core/PersistanceTest.java
@@ -14,7 +14,7 @@ public class PersistanceTest {
   @Test
   public void locationTest() throws Exception {
     var l = new Location(12, 33);
-    var n = serde(Location.class, l);
+    var n = serde(Location.class, l, 12);
 
     assertEquals(12, n.start());
     assertEquals(33, n.end());
@@ -24,23 +24,27 @@ public class PersistanceTest {
   @Test
   public void identifiedLocation() throws Exception {
     var il = new IdentifiedLocation(new Location(5, 19), Option.empty());
-    var in = serde(IdentifiedLocation.class, il);
+    var in = serde(IdentifiedLocation.class, il, 20);
     assertEquals(il, in);
   }
 
   @Test
   public void identifiedLocationNoUUID() throws Exception {
     var il = new IdentifiedLocation(new Location(5, 19), Option.apply(UUID.randomUUID()));
-    var in = serde(IdentifiedLocation.class, il);
+    var in = serde(IdentifiedLocation.class, il, 20);
     assertEquals(
         "UUIDs aren't serialized", new IdentifiedLocation(il.location(), Option.empty()), in);
   }
 
-  private static <T> T serde(Class<T> clazz, T l) throws IOException {
+  private static <T> T serde(Class<T> clazz, T l, int expectedSize) throws IOException {
     assertEquals(clazz, l.getClass());
     var buf = ByteBuffer.allocate(512);
     var gen = Persistance.newGenerator(buf);
     var ref = gen.writeObject(l);
+    buf.flip();
+    if (expectedSize >= 0) {
+      assertEquals(expectedSize, buf.limit());
+    }
     return ref.get(clazz);
   }
 }
