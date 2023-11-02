af61aeb9b4f227503b0e02fd76557cb7e987a1c8
Radosław Waśko <radoslaw.wasko@enso.org>
2023-11-02 14:32:33 +0100

    fix doc

--- std-bits/table/src/main/java/org/enso/table/data/table/join/PluggableJoinStrategy.java
+++ std-bits/table/src/main/java/org/enso/table/data/table/join/PluggableJoinStrategy.java
@@ -4,7 +4,7 @@ import java.util.List;
 import org.enso.table.problems.ProblemAggregator;
 
 /**
- * A {@link JoinStrategy} that can also be used within another join strategy to perform a join of
+ * A helper join strategy that can be used within another join strategy to perform a join of
  * sub-sets of indices, stemming from already joining on other conditions.
  */
 public interface PluggableJoinStrategy {
