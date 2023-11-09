28bbf41f98a5442f7af28679b77f1f67e7e0e843
Radosław Waśko <radoslaw.wasko@enso.org>
2023-11-09 20:00:12 +0100

    checkpoint

--- std-bits/table/src/main/java/org/enso/table/data/table/join/CrossJoin.java
+++ std-bits/table/src/main/java/org/enso/table/data/table/join/CrossJoin.java
@@ -5,7 +5,8 @@ import org.graalvm.polyglot.Context;
 public class CrossJoin {
   public static JoinResult perform(int leftRowCount, int rightRowCount) {
     Context context = Context.getCurrent();
-    JoinResult.Builder resultBuilder = new JoinResult.Builder(leftRowCount * rightRowCount, true, true, true);
+    JoinResult.BuilderSettings settings = new JoinResult.BuilderSettings(true, true, true);
+    JoinResult.Builder resultBuilder = new JoinResult.Builder(leftRowCount * rightRowCount, settings);
     for (int l = 0; l < leftRowCount; ++l) {
       for (int r = 0; r < rightRowCount; ++r) {
         resultBuilder.addMatchedRowsPair(l, r);
