enso-org/enso

commit 63802544f7a5d3dade4650ad0be0c5b454943d19

Enable log-to-file configuration (#7918)
* Enable log-to-file configuration

PR #7825 enabled parallel logging to a file with a much more
fine-grained log level by default.
However, logging at `TRACE` level on Windows appears to be still
problematic.

This PR reduced the default log level to file from `DEBUG` to `TRACE`
and allows to control it via an environment variable if one wishes to
change the verbosity without making code changes.

* PR comments

engine/language-server/src/main/resources/application.conf
@@ -43,6 +43,10 @@ logging-service {
   ]
   default-appender = socket
   default-appender = ${?ENSO_APPENDER_DEFAULT}
-  always-log-to-file = false
-  always-log-to-file = ${?ENSO_LOG_TO_FILE}
+  log-to-file {
+    enable = false
+    enable = ${?ENSO_LOG_TO_FILE}
+    log-level = debug
+    log-level = ${?ENSO_LOG_TO_FILE_LOG_LEVEL}
+  }
 }

lib/scala/logging-config/src/main/java/org/enso/logger/config/ConsoleAppender.java
@@ -28,8 +28,9 @@ public final class ConsoleAppender extends Appender {
   @Override
   public boolean setupForPath(
       Level logLevel, Path logRoot, String logPrefix, LoggerSetup loggerSetup) {
-    if (loggerSetup.getConfig().logToFile()) {
-      loggerSetup.setupFileAppender(Level.TRACE, logRoot, logPrefix);
+    if (loggerSetup.getConfig().logToFile().enabled()) {
+      loggerSetup.setupFileAppender(
+          loggerSetup.getConfig().logToFile().logLevel(), logRoot, logPrefix);
     }
     return loggerSetup.setupConsoleAppender(logLevel);
   }

lib/scala/logging-service-logback/src/main/java/org/enso/logger/LogbackSetup.java
@@ -55,6 +55,10 @@ public final class LogbackSetup extends LoggerSetup {
         return config;
     }
 
+    private boolean logToFileEnabled() {
+        return config.logToFile().enabled();
+    }
+
     private final LoggingServiceConfig config;
     private final LoggerContext context;
 
@@ -101,12 +105,13 @@ public final class LogbackSetup extends LoggerSetup {
         Level targetLogLevel;
         // Modify log level if we were asked to always log to a file.
         // The receiver needs to get all logs (up to `trace`) so as to be able to log all verbose messages.
-        if (config.logToFile()) {
-            targetLogLevel = Level.TRACE;
+        if (logToFileEnabled()) {
+            int min = Math.min(Level.TRACE.toInt(), config.logToFile().logLevel().toInt());
+            targetLogLevel = Level.intToLevel(min);
         } else {
             targetLogLevel = logLevel;
         }
-        LoggerAndContext env = contextInit(targetLogLevel, config, !config.logToFile());
+        LoggerAndContext env = contextInit(targetLogLevel, config, !logToFileEnabled());
 
         org.enso.logger.config.SocketAppender appenderConfig = config.getSocketAppender();
 
@@ -119,7 +124,7 @@ public final class LogbackSetup extends LoggerSetup {
             socketAppender.setReconnectionDelay(Duration.buildByMilliseconds(appenderConfig.getReconnectionDelay()));
 
 
-        env.finalizeAppender(socketAppender, config.logToFile());
+        env.finalizeAppender(socketAppender);
         return true;
     }
 
@@ -183,7 +188,7 @@ public final class LogbackSetup extends LoggerSetup {
             fileAppender.setEncoder(encoder);
 
 
-            env.finalizeAppender(fileAppender, false);
+            env.finalizeAppender(fileAppender);
         } catch (Throwable e) {
             e.printStackTrace();
             return false;
@@ -193,7 +198,7 @@ public final class LogbackSetup extends LoggerSetup {
 
     @Override
     public boolean setupConsoleAppender(Level logLevel) {
-        LoggerAndContext env = contextInit(logLevel, config, !getConfig().logToFile());
+        LoggerAndContext env = contextInit(logLevel, config, !logToFileEnabled());
         org.enso.logger.config.ConsoleAppender appenderConfig = config.getConsoleAppender();
         final PatternLayoutEncoder encoder = new PatternLayoutEncoder();
         try {
@@ -208,7 +213,7 @@ public final class LogbackSetup extends LoggerSetup {
         consoleAppender.setName("enso-console");
         consoleAppender.setEncoder(encoder);
 
-        env.finalizeAppender(consoleAppender, false);
+        env.finalizeAppender(consoleAppender);
         return true;
     }
 
@@ -217,7 +222,7 @@ public final class LogbackSetup extends LoggerSetup {
         // TODO: handle proxy
         // TODO: shutdown timeout configuration
         try {
-            LoggerAndContext env = contextInit(logLevel, config, !config.logToFile());
+            LoggerAndContext env = contextInit(logLevel, config, !logToFileEnabled());
 
             org.enso.logger.config.SentryAppender appenderConfig = config.getSentryAppender();
             if (appenderConfig == null) {
@@ -242,7 +247,7 @@ public final class LogbackSetup extends LoggerSetup {
             opts.setDsn(appenderConfig.getDsn());
             appender.setOptions(opts);
 
-            env.finalizeAppender(appender, config.logToFile());
+            env.finalizeAppender(appender);
         } catch (Throwable e) {
             e.printStackTrace();
             return false;
@@ -257,7 +262,7 @@ public final class LogbackSetup extends LoggerSetup {
         NOPAppender<ILoggingEvent> appender = new NOPAppender<>();
         appender.setName("enso-noop");
 
-        env.finalizeAppender(appender, false);
+        env.finalizeAppender(appender);
         return true;
     }
 
@@ -290,17 +295,20 @@ public final class LogbackSetup extends LoggerSetup {
             encoder.setContext(ctx);
             encoder.start();
         }
-        void finalizeAppender(ch.qos.logback.core.Appender<ILoggingEvent> appender, boolean isLogToFile) {
+        void finalizeAppender(ch.qos.logback.core.Appender<ILoggingEvent> appender) {
             ThresholdFilter threshold = new ThresholdFilter();
             threshold.setLevel(ch.qos.logback.classic.Level.convertAnSLF4JLevel(level).toString());
             appender.addFilter(threshold);
             threshold.setContext(ctx);
             threshold.start();
 
-            // Root's log level is set to TRACE, meaning we want to log all events.
+            // Root's log level is set to the minimal required log level.
             // Log level is controlled by `ThresholdFilter` instead, allowing is to specify different
             // log levels for different outputs.
-            logger.setLevel(ch.qos.logback.classic.Level.TRACE);
+            var minLevelInt = Math.min(Level.TRACE.toInt(), level.toInt());
+            var minLevel = ch.qos.logback.classic.Level.convertAnSLF4JLevel(Level.intToLevel(minLevelInt));
+
+            logger.setLevel(minLevel);
             if (filter != null) {
                 appender.addFilter(filter);
                 filter.setContext(ctx);

lib/scala/logging-service/src/main/java/org/enso/logging/LoggingServiceManager.java
@@ -26,7 +26,7 @@ public class LoggingServiceManager {
       throw new LoggingServiceAlreadySetup();
     } else {
       if (config.appenders().containsKey(config.appender())) {
-        currentLevel = config.logToFile() ? Level.TRACE : logLevel;
+        currentLevel = config.logToFile().enabled() ? config.logToFile().logLevel() : logLevel;
         return Future.apply(
             () -> {
               var server = LoggingServiceFactory.get().localServerFor(port);

lib/scala/project-manager/src/main/resources/application.conf
@@ -40,15 +40,23 @@ logging-service {
   ]
   default-appender = socket
   default-appender = ${?ENSO_APPENDER_DEFAULT}
-  always-log-to-file = true
-  always-log-to-file = ${?ENSO_LOG_TO_FILE}
+  log-to-file {
+    enable = true
+    enable = ${?ENSO_LOG_TO_FILE}
+    log-level = debug
+    log-level = ${?ENSO_LOG_TO_FILE_LOG_LEVEL}
+  }
   server {
     start = true
     start = ${?ENSO_LOGSERVER_START}
     port = 6000
     port = ${?ENSO_LOGSERVER_PORT}
-    always-log-to-file = true
-    always-log-to-file = ${?ENSO_LOG_TO_FILE}
+    log-to-file {
+      enable = true
+      enable = ${?ENSO_LOG_TO_FILE}
+      log-level = debug
+      log-level = ${?ENSO_LOG_TO_FILE_LOG_LEVEL}
+    }
     appenders = [ # file/console/socket/sentry
         {
           name = "file"

engine/launcher/src/main/resources/application.conf
@@ -32,6 +32,10 @@ logging-service {
   ]
   default-appender = console
   default-appender = ${?ENSO_APPENDER_DEFAULT}
-  always-log-to-file = true
-  always-log-to-file = ${?ENSO_LOG_TO_FILE}
+  log-to-file {
+    enable = true
+    enable = ${?ENSO_LOG_TO_FILE}
+    log-level = debug
+    log-level = ${?ENSO_LOG_TO_FILE_LOG_LEVEL}
+  }
 }

lib/scala/logging-config/src/main/java/org/enso/logger/config/BaseConfig.java
@@ -11,11 +11,8 @@ public interface BaseConfig {
   /** Returns a map of appenders defined in the logger section of the config. */
   Map<String, Appender> getAppenders();
 
-  /**
-   * Returns true, if logging infrastructure should always log in verbose mode, irrespective of the
-   * log target.
-   */
-  boolean logToFile();
+  /** Returns configuration for parallel logging of messages to a log file. */
+  LogToFile logToFile();
 
   /**
    * Returns a list of custom loggers and their levels that need to be taken into account when

lib/scala/logging-config/src/main/java/org/enso/logger/config/LogToFile.java
@@ -1,0 +1,44 @@
+package org.enso.logger.config;
+
+import com.typesafe.config.Config;
+import org.slf4j.event.Level;
+
+/**
+ * If `enabled`, all logs up to `logLevel` level will be persisted in the log file in parallel to the default appender.
+ */
+public record LogToFile(boolean enabled, Level logLevel) {
+
+    public static final String logToFileEnabledKey = "enable";
+    public static final String logToFileLogLevelKey = "log-level";
+
+    /**
+     * Parses `log-to-file` configuration section.
+     *
+     * @param config application configuration section that determines logging to a file
+     * @return parsed `log-to-file` configuration
+     */
+    public static LogToFile fromConfig(Config config) {
+        if (config.hasPath(logToFileEnabledKey)) {
+            Level lvl = config.hasPath(logToFileLogLevelKey) ? fromConfigValue(config.getString(logToFileLogLevelKey)) : Level.DEBUG;
+            return new LogToFile(config.getBoolean(logToFileEnabledKey), lvl);
+        } else {
+            return disabled();
+        }
+    }
+
+    /**
+     * Returns configuration that disables parallel logging to a file.
+     */
+    public static LogToFile disabled() {
+        return new LogToFile(false, Level.ERROR);
+    }
+
+    private static Level fromConfigValue(String logLevel) {
+        try {
+            return Level.valueOf(logLevel.toUpperCase());
+        } catch (IllegalArgumentException iae) {
+            return Level.DEBUG;
+        }
+    }
+}
+

commit 2543c38bd1086a4b478fb4f2c7ec999b4e89d93c

Let logging to file respect log level
This change fixes a regression introduced in #7918, which prevented the
execution from setting the right log level either via env var or
parameter.

Now passing either of the options returns logs of the expected level
in the log file:
- `ENSO_LOG_TO_FILE_LOG_LEVEL = trace`
- ... `-vv` ...

engine/language-server/src/main/resources/application.conf
@@ -37,9 +37,9 @@ logging-service {
     org.eclipse.jgit = error
     io.methvin.watcher = error
     # Log levels to limit during very verbose setting:
-    #org.enso.languageserver.protocol.json.JsonConnectionController = debug
-    #org.enso.jsonrpc.JsonRpcServer = debug
-    #org.enso.languageserver.runtime.RuntimeConnector = debug
+    org.enso.languageserver.protocol.json.JsonConnectionController = debug
+    org.enso.jsonrpc.JsonRpcServer = debug
+    org.enso.languageserver.runtime.RuntimeConnector = debug
   }
   appenders = [
     {

lib/scala/logging-config/src/main/java/org/enso/logger/config/ConsoleAppender.java
@@ -28,9 +28,11 @@ public final class ConsoleAppender extends Appender {
   @Override
   public boolean setupForPath(
       Level logLevel, Path logRoot, String logPrefix, LoggerSetup loggerSetup) {
-    if (loggerSetup.getConfig().logToFile().enabled()) {
-      loggerSetup.setupFileAppender(
-          loggerSetup.getConfig().logToFile().logLevel(), logRoot, logPrefix);
+    LogToFile logToFileOpt = loggerSetup.getConfig().logToFile();
+    if (logToFileOpt.enabled()) {
+      Level minLevel =
+          Level.intToLevel(Math.min(logToFileOpt.logLevel().toInt(), logLevel.toInt()));
+      loggerSetup.setupFileAppender(minLevel, logRoot, logPrefix);
     }
     return loggerSetup.setupConsoleAppender(logLevel);
   }

lib/scala/logging-service-logback/src/main/java/org/enso/logger/LogbackSetup.java
@@ -115,9 +115,9 @@ public final class LogbackSetup extends LoggerSetup {
             int port) {
         Level targetLogLevel;
         // Modify log level if we were asked to always log to a file.
-        // The receiver needs to get all logs (up to `trace`) so as to be able to log all verbose messages.
+        // The receiver needs to get all logs (up to `trace`) to be able to log all verbose messages.
         if (logToFileEnabled()) {
-            int min = Math.min(Level.TRACE.toInt(), config.logToFile().logLevel().toInt());
+            int min = Math.min(logLevel.toInt(), config.logToFile().logLevel().toInt());
             targetLogLevel = Level.intToLevel(min);
         } else {
             targetLogLevel = logLevel;

lib/scala/logging-service/src/main/java/org/enso/logging/LoggingServiceManager.java
@@ -26,7 +26,17 @@ public class LoggingServiceManager {
       throw new LoggingServiceAlreadySetup();
     } else {
       if (config.appenders().containsKey(config.appender())) {
-        currentLevel = config.logToFile().enabled() ? config.logToFile().logLevel() : logLevel;
+        if (config.logToFile().enabled()) {
+          String envSetLogLevel = System.getenv("ENSO_LOG_TO_FILE_LOG_LEVEL");
+          if (envSetLogLevel != null) {
+            currentLevel = config.logToFile().logLevel();
+          } else {
+            int min = Math.min(config.logToFile().logLevel().toInt(), logLevel.toInt());
+            currentLevel = Level.intToLevel(min);
+          }
+        } else {
+          currentLevel = logLevel;
+        }
         return Future.apply(
             () -> {
               var server = LoggingServiceFactory.get().localServerFor(port);

lib/scala/project-manager/src/main/resources/application.conf
@@ -64,7 +64,8 @@ logging-service {
             max-file-size = "100MB"
             max-history = 30
             max-total-size = "2GB"
-          }
+          },
+          immediate-flush = true
         },
         {
           name = "sentry"