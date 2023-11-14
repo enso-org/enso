3d87718c56816ce3235d6f3b9369892539fec6a0
Ilya Bogdanov <fumlead@gmail.com>
2023-11-14 15:33:45 +0400

    wip

--- app/gui2/src/stores/imports.ts
+++ app/gui2/src/stores/imports.ts
@@ -1,6 +1,35 @@
 import { Ast, AstExtended } from "@/util/ast"
+import { GeneralOprApp } from "@/util/ast/opr"
 import { tryIdentifier, type Identifier, type QualifiedName, tryQualifiedName } from "@/util/qualifiedName"
 import { unwrap } from "@/util/result"
+
+function parseIdent(ast: AstExtended): Identifier | null {
+  if (ast.isTree(Ast.Tree.Type.Ident) || ast.isToken(Ast.Token.Type.Ident)) {
+    return unwrap(tryIdentifier(ast.repr()))
+  } else {
+    return null
+  }
+}
+
+function parseIdents(ast: AstExtended): Identifier[] | null {
+  if (ast.isTree(Ast.Tree.Type.Ident) || ast.isToken(Ast.Token.Type.Ident)) {
+    return [unwrap(tryIdentifier(ast.repr()))]
+  } else if (ast.isTree(Ast.Tree.Type.OprApp)) {
+    const opr = new GeneralOprApp(ast)
+    const operands = opr.operandsOfLeftAssocOprChain(',')
+    const x = [...operands].flatMap((operand) => {
+      if (operand && operand.type === 'ast') {
+        const ident = parseIdent(operand.ast)
+        return ident != null ? [ident] : []
+      } else {
+        return []
+      }
+    })
+    return x
+  } else {
+    return null
+  }
+}
 
 function recognizeImport(ast: AstExtended<Ast.Tree.Import>): Import | null {
   const from = ast.tryMap((import_) => import_.from?.body)
@@ -8,16 +37,37 @@ function recognizeImport(ast: AstExtended<Ast.Tree.Import>): Import | null {
   const import_ = ast.tryMap((import_) => import_.import.body)
   const all = ast.tryMap((import_) => import_.all)
   const hiding = ast.tryMap((import_) => import_.hiding?.body)
+  // console.log('from: ', from ? from.repr() : 'undefined')
+  // console.log('as: ', as ? as.repr() : 'undefined')
+  // console.log('import_: ', import_ ? import_.repr() : 'undefined')
+  // console.log('all: ', all ? all.repr() : 'undefined')
+  // console.log('hiding: ', hiding ? hiding.repr() : 'undefined')
   if (all) {
     return {
       from: unwrap(tryQualifiedName('Standard.Base')),
       imported: { kind: 'All', except: [] }
     }
-  } else {
+  } else if (from) {
+    if (import_) {
+      const names = parseIdents(import_) ?? []
+      return {
+        from: unwrap(tryQualifiedName(from.repr())),
+        imported: { kind: 'List', names }
+      }
+    }
     return {
-      from: unwrap(tryQualifiedName('Standard.Base.Table')),
+      from: unwrap(tryQualifiedName(from.repr())),
       imported: { kind: 'List', names: [unwrap(tryIdentifier('Table'))] }
     }
+  } else if (import_) {
+    const alias = as ? parseIdent(as) : null
+    return {
+      from: unwrap(tryQualifiedName(import_.repr())),
+      imported: alias ? { kind: 'Module', alias } : { kind: 'Module' }
+    }
+  } else {
+    console.error('Unrecognized import', ast.debug())
+    return null
   }
 }
 
@@ -66,14 +116,34 @@ if (import.meta.vitest) {
 
   test('Recognizing import expressions', () => {
     expect(parseImport('1 + 1')).toBeNull()
+
     expect(parseImport('from Standard.Base import all')).toStrictEqual({
       from: unwrap(tryQualifiedName('Standard.Base')),
       imported: { kind: 'All', except: [] }
     })
 
-    expect(parseImport('import Table from Standard.Base.Table')).toStrictEqual({
+    expect(parseImport('from Standard.Base.Table import Table')).toStrictEqual({
       from: unwrap(tryQualifiedName('Standard.Base.Table')),
       imported: { kind: 'List', names: [unwrap(tryIdentifier('Table'))] }
+    })
+
+    expect(parseImport('from Standard.Collections import Array, HashMap')).toStrictEqual({
+      from: unwrap(tryQualifiedName('Standard.Collections')),
+      imported: { kind: 'List', names: [unwrap(tryIdentifier('Array')), unwrap(tryIdentifier('HashMap'))] }
+    })
+
+    expect(parseImport('import Standard.Database')).toStrictEqual({
+      from: unwrap(tryQualifiedName('Standard.Database')),
+      imported: { kind: 'Module' }
+    })
+    expect(parseImport('import Standard.Base')).toStrictEqual({
+      from: unwrap(tryQualifiedName('Standard.Base')),
+      imported: { kind: 'Module' }
+    })
+
+    expect(parseImport('import AWS.Connection as Backend')).toStrictEqual({
+      from: unwrap(tryQualifiedName('AWS.Connection')),
+      imported: { kind: 'Module', alias: 'Backend' }
     })
   })
 }
