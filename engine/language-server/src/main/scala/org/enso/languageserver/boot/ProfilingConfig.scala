32702d383ca0073d894e33e965c9f8db0c4eacdc
Dmitry Bushev <bushevdv@gmail.com>
2023-11-20 16:42:00 +0300

    feat: always create events log next when profiling

--- engine/language-server/src/main/scala/org/enso/languageserver/boot/ProfilingConfig.scala
+++ engine/language-server/src/main/scala/org/enso/languageserver/boot/ProfilingConfig.scala
@@ -1,18 +1,41 @@
 package org.enso.languageserver.boot
 
+import org.apache.commons.io.FilenameUtils
+
 import java.nio.file.Path
 
 import scala.concurrent.duration.FiniteDuration
 
 /** Application profiling configuration.
   *
-  * @param runtimeEventsLogPath the path to the runtime events log file
   * @param profilingPath the path to the profiling output file
   * @param profilingTime limit the profiling duration, as an infinite profiling
   * duration may cause out-of-memory errors.
   */
 case class ProfilingConfig(
-  runtimeEventsLogPath: Option[Path]    = None,
   profilingPath: Option[Path]           = None,
   profilingTime: Option[FiniteDuration] = None
-)
+) {
+
+  /** Creates the path to the runtime events log with the same name as
+    * `profilingPath` but with the `.log` extension.
+    *
+    * @return the path to the runtime events log file
+    */
+  def profilingEventsLogPath: Option[Path] =
+    profilingPath.map { path =>
+      val profilingDirectory     = path.getParent
+      val profilingFileName      = path.getFileName.toString
+      val profilingFileExtension = FilenameUtils.getExtension(profilingFileName)
+      val eventsLogFileName =
+        profilingFileName.stripSuffix(
+          profilingFileExtension
+        ) + ProfilingConfig.EventsLogExtension
+
+      profilingDirectory.resolve(eventsLogFileName)
+    }
+}
+object ProfilingConfig {
+
+  private val EventsLogExtension = "log"
+}
