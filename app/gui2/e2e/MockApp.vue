b18324e127d623d0cf67631456ea655df4d85aa1
somebody1234 <ehern.lee@gmail.com>
2023-11-20 21:49:35 +1000

    Simplify `MockApp.vue`

--- app/gui2/e2e/MockApp.vue
+++ app/gui2/e2e/MockApp.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
-import diff from 'fast-diff'
-import { computed, watchEffect } from 'vue'
-import GraphEditor from '../src/components/GraphEditor.vue'
-import { useProjectStore } from '../src/stores/project'
+import { computed } from 'vue'
+import App from '../src/App.vue'
+import MockProjectStoreWrapper from '../stories/MockProjectStoreWrapper.vue'
 import { getMainFile, setMainFile } from './mockEngine'
-
-const projectStore = useProjectStore()
-const mod = projectStore.projectModel.createNewModule('Main.enso')
-mod.doc.ydoc.emit('load', [])
 
 const mainFile = computed({
   get() {
@@ -17,39 +12,10 @@ const mainFile = computed({
     setMainFile(value)
   },
 })
-
-function applyEdits(module: NonNullable<typeof projectStore.module>, newText: string) {
-  const contents = projectStore.module?.doc.contents
-  if (!contents) return
-  const edits = diff(contents.toString(), newText)
-  if (edits.length === 0) return
-
-  module.transact(() => {
-    let i = 0
-    for (const [type, string] of edits) {
-      switch (type) {
-        case -1: {
-          contents.delete(i, string.length)
-          break
-        }
-        case 0: {
-          i += string.length
-          break
-        }
-        case 1: {
-          contents.insert(i, string)
-          break
-        }
-      }
-    }
-  })
-}
-
-watchEffect(() => projectStore.module && applyEdits(projectStore.module, mainFile.value))
 </script>
 
 <template>
-  <GraphEditor />
+  <MockProjectStoreWrapper v-model="mainFile"><App :config="{}" /></MockProjectStoreWrapper>
 </template>
 
 <style scoped>
