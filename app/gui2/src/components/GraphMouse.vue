6eae23087f8ce86936853701b27d7a911d66aa10
somebody1234 <ehern.lee@gmail.com>
2023-11-23 17:00:44 +1000

    Hide `GraphMouse` when natively dragging

--- app/gui2/src/components/GraphMouse.vue
+++ app/gui2/src/components/GraphMouse.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
 import { injectGraphNavigator } from '@/providers/graphNavigator'
 import { injectGraphSelection } from '@/providers/graphSelection'
-import { computed } from 'vue'
+import { useEvent } from '@/util/events'
+import { computed, ref } from 'vue'
 import SelectionBrush from './SelectionBrush.vue'
 
 const navigator = injectGraphNavigator(true)
 const nodeSelection = injectGraphSelection(true)
 const scaledMousePos = computed(() => navigator?.sceneMousePos?.scale(navigator?.scale ?? 1))
 const scaledSelectionAnchor = computed(() => nodeSelection?.anchor?.scale(navigator?.scale ?? 1))
+const isNativeDragging = ref(false)
+
+useEvent(window, 'dragstart', () => (isNativeDragging.value = true))
+useEvent(window, 'dragend', () => (isNativeDragging.value = false))
 </script>
 
 <template>
   <SelectionBrush
-    v-if="scaledMousePos"
+    v-if="scaledMousePos && !isNativeDragging"
     :position="scaledMousePos"
     :anchor="scaledSelectionAnchor"
     :style="{ transform: navigator?.prescaledTransform }"
