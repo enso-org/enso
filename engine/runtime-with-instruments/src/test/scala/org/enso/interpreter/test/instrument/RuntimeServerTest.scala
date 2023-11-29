b5d11cca6d45172a30bca3407a33736d72203420
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2023-11-29 17:17:02 +0100

    Further performance improvements to tests
    Eliminating shutdown hooks so that objects get GC'ed.

--- engine/runtime-with-instruments/src/test/scala/org/enso/interpreter/test/instrument/RuntimeServerTest.scala
+++ engine/runtime-with-instruments/src/test/scala/org/enso/interpreter/test/instrument/RuntimeServerTest.scala
@@ -1,6 +1,6 @@
 package org.enso.interpreter.test.instrument
 
-import org.enso.distribution.FileSystem
+import org.apache.commons.io.FileUtils
 import org.enso.distribution.locking.ThreadSafeFileLockManager
 import org.enso.interpreter.runtime.`type`.{Constants, ConstantsGen, Types}
 import org.enso.interpreter.runtime.EnsoContext
@@ -21,6 +21,9 @@ import java.nio.file.{Files, Path, Paths}
 import java.util.UUID
 import org.apache.commons.io.output.TeeOutputStream
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 @scala.annotation.nowarn("msg=multiarg infix syntax")
 class RuntimeServerTest
     extends AnyFlatSpec
@@ -34,8 +37,7 @@ class RuntimeServerTest
   class TestContext(packageName: String) extends InstrumentTestContext {
 
     val tmpDir: Path = Files.createTempDirectory("enso-test-packages")
-    sys.addShutdownHook(FileSystem.removeDirectoryIfExists(tmpDir))
-    val lockManager = new ThreadSafeFileLockManager(tmpDir.resolve("locks"))
+    val lockManager  = new ThreadSafeFileLockManager(tmpDir.resolve("locks"))
     val runtimeServerEmulator =
       new RuntimeServerEmulator(messageQueue, lockManager)
 
@@ -368,8 +370,15 @@ class RuntimeServerTest
   }
 
   override protected def afterEach(): Unit = {
-    context.executionContext.context.close()
-    context.runtimeServerEmulator.terminate()
+    if (context != null) {
+      context.reset()
+      context.executionContext.context.close()
+      Await.ready(context.runtimeServerEmulator.terminate(), 5.seconds)
+      context.lockManager.reset()
+      context.out.reset()
+      FileUtils.deleteQuietly(context.tmpDir.toFile)
+      context = null
+    }
   }
 
   "RuntimeServer" should "push and pop functions on the stack" in {
