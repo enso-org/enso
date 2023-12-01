a23dddcef08657ab4100c883f91084442cf3ee17
somebody1234 <ehern.lee@gmail.com>
2023-12-01 21:33:28 +1000

    Implement `extractMatches` to extract CST subtrees

--- app/gui2/src/util/ast/__tests__/match.test.ts
+++ app/gui2/src/util/ast/__tests__/match.test.ts
@@ -1,63 +1,71 @@
 import { AstExtended } from '@/util/ast'
-import { isMatch } from '@/util/ast/match'
+import { extractMatches, isMatch } from '@/util/ast/match'
 import { expect, test } from 'vitest'
 
 test.each([
-  { target: '1 + 1', pattern: '1 + 1' },
-  { target: '1 + 1', pattern: '1 + __' },
-  { target: '1 + 1', pattern: '__ + 1' },
-  { target: '1', pattern: '__' },
-  { target: '1', pattern: '(__)', matches: false },
-  { target: '("a")', pattern: '(__)' },
-  { target: '[1, "a", True]', pattern: '[1, "a", True]' },
-  { target: '[1, "a", True]', pattern: '[__, "a", __]' },
-  { target: '[1, "a", True]', pattern: '[1, "a", False]', matches: false },
-  { target: '[1, "a", True]', pattern: '[1, "a", True, 2]', matches: false },
-  { target: '[1, "a", True]', pattern: '[1, "a", True, __]', matches: false },
-  { target: '(1)', pattern: '1', matches: false },
-  { target: '(1)', pattern: '__' }, // True because `__` matches any expression.
-  { target: '1 + 1', pattern: '1 + 2', matches: false },
-  { target: '1 + 1', pattern: '"1" + 1', matches: false },
-  { target: '1 + 1', pattern: '1 + "1"', matches: false },
-  { target: '1 + 1 + 1', pattern: '(1 + 1) + 1', matches: false },
-  { target: '1 + 1 + 1', pattern: '1 + (1 + 1)', matches: false },
-  { target: '(1 + 1) + 1', pattern: '1 + 1 + 1', matches: false },
-  { target: '1 + (1 + 1)', pattern: '1 + 1 + 1', matches: false },
+  { target: '1 + 1', pattern: '1 + 1', extracted: [] },
+  { target: '1 + 2', pattern: '1 + __', extracted: ['2'] },
+  { target: '1 + 2', pattern: '__ + 2', extracted: ['1'] },
+  { target: '1 + 2', pattern: '__ + __', extracted: ['1', '2'] },
+  { target: '1', pattern: '__', extracted: ['1'] },
+  { target: '1', pattern: '(__)' },
+  { target: '("a")', pattern: '(__)', extracted: ['"a"'] },
+  { target: '[1, "a", True]', pattern: '[1, "a", True]', extracted: [] },
+  { target: '[1, "a", True]', pattern: '[__, "a", __]', extracted: ['1', 'True'] },
+  { target: '[1, "a", True]', pattern: '[1, "a", False]' },
+  { target: '[1, "a", True]', pattern: '[1, "a", True, 2]' },
+  { target: '[1, "a", True]', pattern: '[1, "a", True, __]' },
+  { target: '(1)', pattern: '1' },
+  { target: '(1)', pattern: '__', extracted: ['(1)'] }, // True because `__` matches any expression.
+  { target: '1 + 1', pattern: '1 + 2' },
+  { target: '1 + 1', pattern: '"1" + 1' },
+  { target: '1 + 1', pattern: '1 + "1"' },
+  { target: '1 + 1 + 1', pattern: '(1 + 1) + 1' },
+  { target: '1 + 1 + 1', pattern: '1 + (1 + 1)' },
+  { target: '(1 + 1) + 1', pattern: '1 + 1 + 1' },
+  { target: '1 + (1 + 1)', pattern: '1 + 1 + 1' },
   {
     target:
       'Standard.Base.Runtime.with_enabled_context Standard.Base.Runtime.Context.Output "current_context_name" <| node1.fn',
     pattern:
       'Standard.Base.Runtime.with_enabled_context Standard.Base.Runtime.Context.Output "current_context_name" <| __',
+    extracted: ['node1.fn'],
   },
   {
     target:
       'Standard.Base.Runtime.with_enabled_context Standard.Base.Runtime.Context.Output "current_context_name" <| node1.fn',
     pattern:
       'Standard.Base.Runtime.with_enabled_context Standard.Base.Runtime.Context.Output __ <| __',
+    extracted: ['"current_context_name"', 'node1.fn'],
   },
   {
     target:
       'Standard.Base.Runtime.with_enabled_context Standard.Base.Runtime.Context.Output "current_context_name" <| node1.fn',
     pattern: 'Standard.Base.Runtime.with_enabled_context Standard.Base.Runtime.Context.Output __',
-    matches: false,
   },
   {
     target:
       'Standard.Base.Runtime.with_enabled_context Standard.Base.Runtime.Context.Output "current_context_name" <| node1.fn',
     pattern:
       'Standard.Base.Runtime.with_enabled_context Standard.Base.Runtime.Context.Output "current_context#name" <| node1.fn',
-    matches: false,
   },
   {
     target:
       'Standard.Base.Runtime.with_enabled_context Standard.Base.Runtime.Context.Output "current_context_name" <| node1.fn',
     pattern:
       'Standard.Base.Runtime.with_enabled_context Standard.Base.Runtime.Context.Output "current_context_name" <| node2.fn',
-    matches: false,
   },
-])('`isMatch`', ({ target, pattern, matches = true }) => {
+])('`isMatch`', ({ target, pattern, extracted }) => {
+  const targetAst = AstExtended.parse(target)
+  const patternAst = AstExtended.parse(pattern)
   expect(
-    isMatch(AstExtended.parse(target), AstExtended.parse(pattern)),
-    `'${target}' has CST ${matches ? '' : 'not '}matching '${pattern}'`,
-  ).toBe(matches)
+    isMatch(targetAst, patternAst),
+    `'${target}' has CST ${extracted != null ? '' : 'not '}matching '${pattern}'`,
+  ).toBe(extracted != null)
+  expect(
+    extractMatches(targetAst, patternAst)?.map((match) => match.repr()),
+    extracted != null
+      ? `'${target}' matches '${pattern}' with '__' corresponding to '${extracted}'`
+      : `'${target}' does not match '${pattern}'`,
+  ).toStrictEqual(extracted)
 })
--- app/gui2/src/util/ast/match.ts
+++ app/gui2/src/util/ast/match.ts
@@ -4,7 +4,7 @@ import type { AstExtended } from '@/util/ast'
 export function isMatch(
   target: AstExtended<Tree.Tree | Token.Token, boolean>,
   pattern: AstExtended<Tree.Tree | Token.Token, boolean>,
-  // This MUST NOT be `_`, as that would make it impossible to match.
+  // This SHOULD NOT be `_`, as that would make `_` impossible to match.
   patternPlaceholderIdentifier = '__',
 ): boolean {
   if (pattern.isToken()) return target.isToken() && target.repr() === pattern.repr()
@@ -20,5 +20,33 @@ export function isMatch(
         isMatch(child, patternChildren[i]!, patternPlaceholderIdentifier),
       )
     )
+  }
+}
+
+export function extractMatches<HasIdMap extends boolean = true>(
+  target: AstExtended<Tree.Tree | Token.Token, HasIdMap>,
+  pattern: AstExtended<Tree.Tree | Token.Token, HasIdMap>,
+  // This SHOULD NOT be `_`, as that would make `_` impossible to match.
+  patternPlaceholderIdentifier = '__',
+): AstExtended<Tree.Tree | Token.Token, HasIdMap>[] | undefined {
+  if (pattern.isToken())
+    return target.isToken() && target.repr() === pattern.repr() ? [] : undefined
+  else if (pattern.isTree(Tree.Type.Ident) && pattern.repr() === patternPlaceholderIdentifier)
+    return [target]
+  else if (target.treeTypeName() !== pattern.treeTypeName()) return undefined
+  else {
+    const children = target.children()
+    const patternChildren = pattern.children()
+    const matches: AstExtended<Tree.Tree | Token.Token, HasIdMap>[] = []
+    for (let i = 0; i < children.length; i += 1) {
+      const childMatches = extractMatches(
+        children[i]!,
+        patternChildren[i]!,
+        patternPlaceholderIdentifier,
+      )
+      if (!childMatches) return undefined
+      matches.push(...childMatches)
+    }
+    return matches
   }
 }
