ad362707ece2de3c955f2bcede6b89973ce9d71e
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2023-12-04 11:41:35 +0100

    nit

--- project/Platform.scala
+++ project/Platform.scala
@@ -15,8 +15,10 @@ object Platform {
   def isMacOS: Boolean =
     sys.props("os.name").toLowerCase().contains("mac")
 
-  def isAmd64: Boolean =
-    sys.props("os.arch").toLowerCase().contains("amd64")
+  def isAmd64: Boolean = {
+    val arch = sys.props("os.arch").toLowerCase()
+    arch.contains("amd64") || arch.contains("x86_64")
+  }
 
   def isArm64: Boolean =
     sys.props("os.arch").toLowerCase().contains("aarch64")
