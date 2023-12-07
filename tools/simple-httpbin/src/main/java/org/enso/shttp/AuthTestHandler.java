4d93f9d94e96feb17205d69662fdb45d0c463bfe
Radosław Waśko <radoslaw.wasko@enso.org>
2023-12-07 16:54:48 +0100

    checkpoint

---
+++ tools/simple-httpbin/src/main/java/org/enso/shttp/AuthTestHandler.java
@@ -0,0 +1,13 @@
+package org.enso.shttp;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class AuthTestHandler implements HttpHandler {
+  @Override
+  public void handle(HttpExchange exchange) throws IOException {
+
+  }
+}
