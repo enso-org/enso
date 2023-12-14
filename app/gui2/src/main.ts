16361214bff008631bf5fd3de38697b934ade9c4
somebody1234 <ehern.lee@gmail.com>
2023-12-15 09:53:34 +1000

    Add scam warning

--- app/gui2/src/main.ts
+++ app/gui2/src/main.ts
@@ -13,12 +13,46 @@ import { decodeQueryParams } from 'lib0/url'
 const vueAppEntry = import('./createApp')
 
 const INITIAL_URL_KEY = `Enso-initial-url`
+const SCAM_WARNING_TIMEOUT = 1000
 
 const params = decodeQueryParams(location.href)
 const initialProjectName = params.project ?? null
 
 let unmount: null | (() => void) = null
 let runRequested = false
+
+function printScamWarning() {
+  const headerCss = `
+    color: white;
+    background: crimson;
+    display: block;
+    border-radius: 8px;
+    font-weight: bold;
+    padding: 10px 20px 10px 20px;
+  `
+    .trim()
+    .replace(/\n\s+/, ' ')
+  const headerCss1 = headerCss + ' font-size: 46px;'
+  const headerCss2 = headerCss + ' font-size: 20px;'
+  const msgCSS = 'font-size: 16px;'
+
+  const msg1 =
+    'This is a browser feature intended for developers. If someone told you to ' +
+    'copy-paste something here, it is a scam and will give them access to your ' +
+    'account and data.'
+  const msg2 = 'See https://enso.org/selfxss for more information.'
+  console.log('%cStop!', headerCss1)
+  console.log('%cYou may be the victim of a scam!', headerCss2)
+  console.log('%c' + msg1, msgCSS)
+  console.log('%c' + msg2, msgCSS)
+}
+
+printScamWarning()
+let scamWarningHandle = 0
+window.addEventListener('resize', () => {
+  window.clearTimeout(scamWarningHandle)
+  scamWarningHandle = window.setTimeout(printScamWarning, SCAM_WARNING_TIMEOUT)
+})
 
 export interface StringConfig {
   [key: string]: StringConfig | string
