42844b0ba86e8fce5d8c2d5343664eb92d153f34
Ilya Bogdanov <fumlead@gmail.com>
2023-12-14 16:06:08 +0400

    Fix component browser story (again)

--- app/gui2/stories/GroupColorsWrapper.vue
+++ app/gui2/stories/GroupColorsWrapper.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useSuggestionDbStore } from '@/stores/suggestionDatabase'
+import { groupColorVar, useSuggestionDbStore } from '@/stores/suggestionDatabase'
 import { colorFromString } from '@/util/colors'
 import { computed } from 'vue'
 
@@ -8,9 +8,7 @@ const suggestionDb = useSuggestionDbStore()
 const groupColors = computed(() => {
   const styles: { [key: string]: string } = {}
   for (let group of suggestionDb.groups) {
-    const name = group.name.replace(/\s/g, '-')
-    let color = group.color ?? colorFromString(name)
-    styles[`--group-color-${name}`] = color
+    styles[groupColorVar(group)] = group.color ?? colorFromString(group.name)
   }
   styles[`--group-color-fallback`] = '#006b8a'
   return styles
