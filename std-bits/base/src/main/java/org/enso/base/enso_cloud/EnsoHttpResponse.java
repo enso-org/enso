321db41339e56c7ec941a5d11672910bae6c681f
Radosław Waśko <radoslaw.wasko@enso.org>
2023-12-15 17:16:48 +0100

    javafmt

--- std-bits/base/src/main/java/org/enso/base/enso_cloud/EnsoHttpResponse.java
+++ std-bits/base/src/main/java/org/enso/base/enso_cloud/EnsoHttpResponse.java
@@ -3,14 +3,6 @@ package org.enso.base.enso_cloud;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpHeaders;
-import java.util.List;
 
-/**
- * A subset of the HttpResponse to avoid leaking the decrypted Enso secrets.
- */
-public record EnsoHttpResponse(
-    URI uri,
-    HttpHeaders headers,
-    InputStream body,
-    int statusCode) {
-}
+/** A subset of the HttpResponse to avoid leaking the decrypted Enso secrets. */
+public record EnsoHttpResponse(URI uri, HttpHeaders headers, InputStream body, int statusCode) {}
