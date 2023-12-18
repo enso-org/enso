1efb0f8894d40f66e7e322de63251cea82c2a159
Radosław Waśko <radoslaw.wasko@enso.org>
2023-12-18 14:43:59 +0100

    updating how metadata is fetched to be more robust

--- std-bits/base/src/main/java/org/enso/base/enso_cloud/HideableValue.java
+++ std-bits/base/src/main/java/org/enso/base/enso_cloud/HideableValue.java
@@ -1,21 +1,35 @@
 package org.enso.base.enso_cloud;
 
-/** Represents a value that is input of various operation that may contain a Secret. */
+/**
+ * Represents a value that is input of various operation that may contain a Secret.
+ */
 public sealed interface HideableValue permits HideableValue.SecretValue, HideableValue.PlainValue {
 
-    record SecretValue(String secretId) implements HideableValue {
-        @Override
-        public String render() {
-            return "__SECRET__";
-        }
+  record SecretValue(String secretId) implements HideableValue {
+    @Override
+    public String render() {
+      return "__SECRET__";
     }
 
-    record PlainValue(String value) implements HideableValue {
-        @Override
-        public String render() {
-            return value;
-        }
+    @Override
+    public String safeResolve() throws EnsoSecretAccessDenied {
+      throw new EnsoSecretAccessDenied();
+    }
+  }
+
+  record PlainValue(String value) implements HideableValue {
+    @Override
+    public String render() {
+      return value;
     }
 
-    String render();
+    @Override
+    public String safeResolve() throws EnsoSecretAccessDenied {
+      return value;
+    }
+  }
+
+  String render();
+
+  String safeResolve() throws EnsoSecretAccessDenied;
 }
--- std-bits/base/src/main/java/org/enso/base/net/URIWithSecrets.java
+++ std-bits/base/src/main/java/org/enso/base/net/URIWithSecrets.java
@@ -24,7 +24,91 @@ public record URIWithSecrets(
     return new URISchematic(baseUri, renderedParameters, renderedUserInfo);
   }
 
-  public URI render() throws URISyntaxException {
-    return makeSchematicForRender().build();
+  public URI render() {
+    try {
+      return makeSchematicForRender().build();
+    } catch (URISyntaxException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  /** Resolves to a proper URI if it does not contain any secrets. If there was a secret, it throws an exception. */
+  public URI safeResolve() {
+    try {
+      return makeSchematicForSafeResolve().build();
+    } catch (URISyntaxException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  private URISchematic makeSchematicForSafeResolve() {
+    List<Pair<String, String>> resolvedParameters = queryParameters.stream()
+        .map(p -> Pair.create(p.getLeft(), p.getRight().safeResolve()))
+        .toList();
+    Pair<String, String> resolvedUserInfo = userInfo == null ? null : Pair.create(userInfo.username().safeResolve(), userInfo.password().safeResolve());
+    return new URISchematic(baseUri, resolvedParameters, resolvedUserInfo);
+  }
+
+  public String getScheme() {
+    return baseUri.getScheme();
+  }
+
+  private URIWithSecrets forAuthorityPart() {
+    // We can ignore secrets in the query part, because they are not used for resolving the authority.
+    return new URIWithSecrets(baseUri, List.of(), userInfo);
+  }
+
+  public String getUserInfo() {
+    return this.forAuthorityPart().safeResolve().getUserInfo();
+  }
+
+  public String getRawUserInfo() {
+    return this.forAuthorityPart().safeResolve().getRawUserInfo();
+  }
+
+  public String getHost() {
+    // This is not affected by secrets at all, so we can rely on the baseUri.
+    return this.baseUri.getHost();
+  }
+
+  public int getPort() {
+    return this.baseUri.getPort();
+  }
+
+  public String getAuthority() {
+    return this.forAuthorityPart().safeResolve().getAuthority();
+  }
+
+  public String getRawAuthority() {
+    return this.forAuthorityPart().safeResolve().getRawAuthority();
+  }
+
+  public String getPath() {
+    return baseUri.getPath();
+  }
+
+  public String getRawPath() {
+    return baseUri.getRawPath();
+  }
+
+  private URIWithSecrets forQueryPart() {
+    // We can ignore secrets in the authority part, because they are not used for resolving the query.
+    return new URIWithSecrets(baseUri, queryParameters, null);
+  }
+
+  public String getQuery() {
+    return this.forQueryPart().safeResolve().getQuery();
+  }
+
+  public String getRawQuery() {
+    return this.forQueryPart().safeResolve().getRawQuery();
+  }
+
+  public String getFragment() {
+    return baseUri.getFragment();
+  }
+
+  public String getRawFragment() {
+    return baseUri.getRawFragment();
   }
 }
