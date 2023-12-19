a7ac4648e9601669d092f028e45837e81ce88a90
Pavel Marek <pavel.marek@enso.org>
2023-12-19 19:03:29 +0100

    Ensure that TestLogProvider is in unnamed module

--- engine/runtime/src/test/java/org/enso/interpreter/test/TestLogProviderOnClasspath.java
+++ engine/runtime/src/test/java/org/enso/interpreter/test/TestLogProviderOnClasspath.java
@@ -1,7 +1,7 @@
 package org.enso.interpreter.test;
 
-import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +11,7 @@ import org.junit.Test;
 import org.slf4j.spi.SLF4JServiceProvider;
 
 /**
- * In the `runtime/Test` testing suite, {@link }org.enso.logger.TestLogProvider} should be among the
+ * In the `runtime/Test` testing suite, {@link org.enso.logger.TestLogProvider} should be among the
  * logging providers, because it is explicitly chosen as the logging provider for the tests.
  */
 public class TestLogProviderOnClasspath {
@@ -26,5 +26,13 @@ public class TestLogProviderOnClasspath {
     List<String> providerNames =
         providers.stream().map(elem -> elem.getClass().getName()).collect(Collectors.toList());
     assertThat(providerNames, hasItem("org.enso.logger.TestLogProvider"));
+  }
+
+  @Test
+  public void testLogProviderIsInUnnamedModule() throws Exception {
+    var testLogProviderClass = Class.forName("org.enso.logger.TestLogProvider");
+    var mod = testLogProviderClass.getModule();
+    assertThat(mod, notNullValue());
+    assertThat("Should be an unnamed module - with null name", mod.getName(), nullValue());
   }
 }
