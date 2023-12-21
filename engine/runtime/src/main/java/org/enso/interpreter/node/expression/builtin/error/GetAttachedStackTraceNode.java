97c5b3bc342914d3b9cf9eb7866ebc1df583b9f3
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-12-21 08:34:17 +0100

    Concentrate stack trace handling into GetStackTraceNode

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/error/GetAttachedStackTraceNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/error/GetAttachedStackTraceNode.java
@@ -1,16 +1,12 @@
 package org.enso.interpreter.node.expression.builtin.error;
 
 import com.oracle.truffle.api.interop.InteropLibrary;
-import com.oracle.truffle.api.interop.InvalidArrayIndexException;
-import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.nodes.Node;
 import org.enso.interpreter.dsl.AcceptsError;
 import org.enso.interpreter.dsl.BuiltinMethod;
 import org.enso.interpreter.node.expression.builtin.runtime.GetStackTraceNode;
 import org.enso.interpreter.runtime.EnsoContext;
 import org.enso.interpreter.runtime.builtin.Builtins;
-import org.enso.interpreter.runtime.data.vector.ArrayLikeHelpers;
-import org.enso.interpreter.runtime.error.DataflowError;
 import org.enso.interpreter.runtime.error.PanicException;
 
 @BuiltinMethod(
@@ -21,36 +17,8 @@ public class GetAttachedStackTraceNode extends Node {
   @Child private InteropLibrary iop = InteropLibrary.getFactory().createDispatched(3);
 
   Object execute(@AcceptsError Object error) {
-    try {
-      if (error instanceof DataflowError) {
-        var arr = iop.getExceptionStackTrace(error);
-        assert InteropLibrary.getUncached().hasArrayElements(arr);
-        var len = iop.getArraySize(arr);
-        var count = 0;
-        for (long i = 0; i < len; i++) {
-          try {
-            var elem = iop.readArrayElement(arr, i);
-            if (includeFrame(iop, elem)) {
-              count++;
-            }
-          } catch (UnsupportedMessageException | InvalidArrayIndexException e) {
-          }
-        }
-        var res = new Object[count];
-        var at = 0;
-        for (long i = 0; i < len; i++) {
-          try {
-            var elem = iop.readArrayElement(arr, i);
-            if (includeFrame(iop, elem)) {
-              res[at++] = elem;
-            }
-          } catch (UnsupportedMessageException | InvalidArrayIndexException e) {
-          }
-        }
-        return ArrayLikeHelpers.wrapObjectsWithCheckAt(res);
-      }
-    } catch (UnsupportedMessageException ex) {
-      // OK, go on
+    if (iop.hasExceptionStackTrace(error)) {
+      return GetStackTraceNode.stackTraceToArray(iop, error);
     }
     if (error instanceof Throwable) {
       return GetStackTraceNode.stackTraceToArray((Throwable) error);
@@ -59,17 +27,5 @@ public class GetAttachedStackTraceNode extends Node {
       throw new PanicException(
           builtins.error().makeTypeError("Throwable", error, "throwable"), this);
     }
-  }
-
-  private boolean includeFrame(InteropLibrary iop, Object elem) throws UnsupportedMessageException {
-    if (!iop.hasSourceLocation(elem)) {
-      return false;
-    }
-    var ss = iop.getSourceLocation(elem);
-    if (ss == null) {
-      return false;
-    }
-    var src = ss.getSource();
-    return src != null && !src.isInternal();
   }
 }
