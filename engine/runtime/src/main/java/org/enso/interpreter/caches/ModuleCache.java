e0b27a240eb7d761bb26daefe7c9fed2e15223af
Jaroslav Tulach <jaroslav.tulach@enso.org>
2023-12-27 05:57:42 +0100

    Histogram of loaded types

--- engine/runtime/src/main/java/org/enso/interpreter/caches/ModuleCache.java
+++ engine/runtime/src/main/java/org/enso/interpreter/caches/ModuleCache.java
@@ -9,6 +9,7 @@ import com.oracle.truffle.api.source.Source;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Optional;
 import java.util.logging.Level;
 import org.apache.commons.lang3.StringUtils;
@@ -45,23 +46,50 @@ public final class ModuleCache extends Cache<ModuleCache.CachedModule, ModuleCac
   protected CachedModule deserialize(
       EnsoContext context, byte[] data, Metadata meta, TruffleLogger logger)
       throws ClassNotFoundException, IOException, ClassNotFoundException {
+    var counts = new HashMap<Class, int[]>();
     var ref =
         Persistance.read(
             data,
-            (obj) ->
-                switch (obj) {
-                  case ProcessingPass.Metadata metadata -> {
-                    var option = metadata.restoreFromSerialization(context.getCompiler().context());
-                    if (option.nonEmpty()) {
-                      yield option.get();
-                    } else {
-                      throw raise(
-                          RuntimeException.class, new IOException("Cannot convert " + metadata));
-                    }
+            (obj) -> {
+              var c = counts.get(obj.getClass());
+              if (c == null) {
+                c = new int[1];
+                counts.put(obj.getClass(), c);
+              }
+              c[0]++;
+              return switch (obj) {
+                case ProcessingPass.Metadata metadata -> {
+                  var option = metadata.restoreFromSerialization(context.getCompiler().context());
+                  if (option.nonEmpty()) {
+                    yield option.get();
+                  } else {
+                    throw raise(
+                        RuntimeException.class, new IOException("Cannot convert " + metadata));
                   }
-                  default -> obj;
-                });
+                }
+                default -> obj;
+              };
+            });
     var mod = ref.get(Module.class);
+
+    var all = mod.preorder().size();
+
+    var arr = new ArrayList<>(counts.entrySet());
+    arr.sort(
+        (a, b) -> {
+          return a.getValue()[0] - b.getValue()[0];
+        });
+
+    logger.log(Level.WARNING, "Loaded " + module.getName() + " with " + all + " IR elements");
+    for (var i = 0; i < arr.size(); i++) {
+      if (i == 30) {
+        break;
+      }
+      var elem = arr.get(arr.size() - 1 - i);
+      logger.log(
+          Level.WARNING, "  {0} {1}", new Object[] {elem.getValue()[0], elem.getKey().getName()});
+    }
+
     return new CachedModule(
         mod, CompilationStage.valueOf(meta.compilationStage()), module.getSource());
   }
