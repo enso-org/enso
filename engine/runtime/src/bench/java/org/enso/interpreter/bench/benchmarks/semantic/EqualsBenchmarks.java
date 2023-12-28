3782a4697c655d9dc53923e598281e64a3bf677e
Pavel Marek <pavel.marek@enso.org>
2023-12-28 11:29:39 +0100

    Increase warmup for `EqualsBenchmarks`.
    Also increate measurement so that JMH provides some reasonable statistics.

--- engine/runtime/src/bench/java/org/enso/interpreter/bench/benchmarks/semantic/EqualsBenchmarks.java
+++ engine/runtime/src/bench/java/org/enso/interpreter/bench/benchmarks/semantic/EqualsBenchmarks.java
@@ -35,8 +35,8 @@ import org.openjdk.jmh.infra.Blackhole;
  */
 @BenchmarkMode(Mode.AverageTime)
 @Fork(1)
-@Warmup(iterations = 3)
-@Measurement(iterations = 2)
+@Warmup(iterations = 5)
+@Measurement(iterations = 3)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Benchmark)
 public class EqualsBenchmarks {
