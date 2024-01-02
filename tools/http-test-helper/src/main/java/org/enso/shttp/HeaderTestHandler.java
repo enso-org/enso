8d8abbe5fcf452b577a4c0eca814d637aa5fc214
Radosław Waśko <radoslaw.wasko@enso.org>
2024-01-02 14:15:48 +0100

    add tests for Content-Disposition

--- tools/http-test-helper/src/main/java/org/enso/shttp/HeaderTestHandler.java
+++ tools/http-test-helper/src/main/java/org/enso/shttp/HeaderTestHandler.java
@@ -2,7 +2,10 @@ package org.enso.shttp;
 
 import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
 import org.apache.http.client.utils.URIBuilder;
 
 public class HeaderTestHandler extends SimpleHttpHandler {
@@ -10,15 +13,31 @@ public class HeaderTestHandler extends SimpleHttpHandler {
   protected void doHandle(HttpExchange exchange) throws IOException {
     URI uri = exchange.getRequestURI();
     URIBuilder builder = new URIBuilder(uri);
+
+    String content = null;
+
     try {
       for (var queryPair : builder.getQueryParams()) {
-        exchange.getResponseHeaders().add(queryPair.getName(), queryPair.getValue());
+        if (queryPair.getName().equals("content")) {
+          content = queryPair.getValue();
+        } else {
+          exchange.getResponseHeaders().add(queryPair.getName(), queryPair.getValue());
+        }
       }
     } catch (Exception e) {
       e.printStackTrace();
       exchange.sendResponseHeaders(500, -1);
     }
 
-    exchange.sendResponseHeaders(200, -1);
+    if (content != null) {
+      byte[] response = content.getBytes(StandardCharsets.UTF_8);
+      exchange.sendResponseHeaders(200, response.length);
+      try (OutputStream os = exchange.getResponseBody()) {
+        os.write(response);
+      }
+      exchange.close();
+    } else {
+      exchange.sendResponseHeaders(200, -1);
+    }
   }
 }
