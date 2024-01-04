86ebf062c425b2b3cda55d4a6a0fe11f3b9aa81c
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-01-04 14:48:21 +0100

    I/O exception with stacktrace shalln't be visible by default

--- engine/runtime/src/main/java/org/enso/interpreter/caches/Cache.java
+++ engine/runtime/src/main/java/org/enso/interpreter/caches/Cache.java
@@ -220,8 +220,6 @@ public abstract class Cache<T, M extends Cache.Metadata> {
                   logger.log(logLevel, "Unable to load a cache [" + logName + "]");
                 } catch (IOException e) {
                   logger.log(
-                      Level.WARNING, "Unable to load a cache [" + logName + "]: " + e.getMessage());
-                  logger.log(
                       Level.FINE, "Unable to load a cache [" + logName + "]: " + e.getMessage(), e);
                 } catch (Exception e) {
                   logger.log(
