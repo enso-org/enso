10fd2eaa1dfe290ab142686d1596f859a2ec5419
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-01-05 11:34:08 +0100

    Providing more Enso Snippets and Values

--- engine/runtime-test-instruments/src/main/java/org/enso/interpreter/test/instruments/tck/EnsoTckLanguageProvider.java
+++ engine/runtime-test-instruments/src/main/java/org/enso/interpreter/test/instruments/tck/EnsoTckLanguageProvider.java
@@ -1,12 +1,14 @@
 package org.enso.interpreter.test.instruments.tck;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 import org.graalvm.polyglot.tck.LanguageProvider;
 import org.graalvm.polyglot.tck.Snippet;
+import org.graalvm.polyglot.tck.TypeDescriptor;
 
 public class EnsoTckLanguageProvider implements LanguageProvider {
     public EnsoTckLanguageProvider() {
@@ -19,31 +21,51 @@ public class EnsoTckLanguageProvider implements LanguageProvider {
 
     @Override
     public Value createIdentityFunction(Context context) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        var id = context.eval("enso", """
+        id a = a
+        """).invokeMember("eval_expression", "id");
+        return id;
     }
 
     @Override
     public Collection<? extends Snippet> createValueConstructors(Context context) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return List.of();
     }
 
     @Override
     public Collection<? extends Snippet> createExpressions(Context context) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        var plus = context.eval("enso", """
+        plus a b = a + b
+        """).invokeMember("eval_expression", "plus");
+
+        return List.of(
+            Snippet.newBuilder("plus", plus, TypeDescriptor.executable(TypeDescriptor.NUMBER, TypeDescriptor.NUMBER, TypeDescriptor.NUMBER)).build()
+        );
     }
 
     @Override
     public Collection<? extends Snippet> createStatements(Context context) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        var when = context.eval("enso", """
+        when c = if c then 1 else -1
+        """).invokeMember("eval_expression", "when");;
+        return List.of(
+            Snippet.newBuilder("if", when, TypeDescriptor.NUMBER)
+            .parameterTypes(TypeDescriptor.BOOLEAN)
+            .build()
+        );
     }
 
     @Override
     public Collection<? extends Snippet> createScripts(Context context) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return List.of();
     }
 
     @Override
     public Collection<? extends Source> createInvalidSyntaxScripts(Context context) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return List.of(
+            Source.newBuilder("enso", """
+            main = x + 2
+            """, "unknown_x.enso").buildLiteral()
+        );
     }
 }
