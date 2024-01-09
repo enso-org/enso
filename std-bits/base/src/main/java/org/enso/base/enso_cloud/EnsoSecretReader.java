efd76073329a01ce0daa9130cba8de0c0d24b75f
Radosław Waśko <radoslaw.wasko@enso.org>
2024-01-09 01:53:24 +0100

    fix URI

--- std-bits/base/src/main/java/org/enso/base/enso_cloud/EnsoSecretReader.java
+++ std-bits/base/src/main/java/org/enso/base/enso_cloud/EnsoSecretReader.java
@@ -27,7 +27,7 @@ class EnsoSecretReader {
       return secrets.get(secretId);
     }
 
-    var apiUri = AuthenticationProvider.getAPIRootURI() + "/s3cr3tz/" + secretId;
+    var apiUri = AuthenticationProvider.getAPIRootURI() + "s3cr3tz/" + secretId;
     var client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.ALWAYS).build();
     var request =
         HttpRequest.newBuilder()
