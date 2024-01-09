4b3a4ef9674277a1ac4b863d9191d0a1c6817a58
Adam Riley <riley.ad@gmail.com>
2024-01-09 12:27:07 +0000

    Further simplification

--- std-bits/table/src/main/java/org/enso/table/data/column/storage/StringStorage.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/storage/StringStorage.java
@@ -13,9 +13,7 @@ import org.enso.table.data.column.operation.map.text.StringIsInOp;
 import org.enso.table.data.column.operation.map.text.StringStringOp;
 import org.enso.table.data.column.storage.type.StorageType;
 import org.enso.table.data.column.storage.type.TextType;
-import org.enso.table.problems.ProblemAggregator;
 import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.Value;
 
 /** A column storing strings. */
 public final class StringStorage extends SpecializedStorage<String> {
@@ -45,12 +43,6 @@ public final class StringStorage extends SpecializedStorage<String> {
   @Override
   public TextType getType() {
     return type;
-  }
-
-  @Override
-  public Storage<?> fillMissing(
-      Value arg, StorageType commonType, ProblemAggregator problemAggregator) {
-    return super.fillMissing(arg, commonType, problemAggregator);
   }
 
   private static MapOperationStorage<String, SpecializedStorage<String>> buildOps() {
