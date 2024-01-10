28fc63e5acbad16c04b22ad22cfaa219d1034fad
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-01-10 09:44:29 +0100

    Don't convert BigInteger on the boundary otherwise TCK identity check fails

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/interop/syntax/HostValueToEnsoNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/interop/syntax/HostValueToEnsoNode.java
@@ -1,22 +1,16 @@
 package org.enso.interpreter.node.expression.builtin.interop.syntax;
 
-import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.Cached;
-import com.oracle.truffle.api.dsl.Cached.Shared;
 import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.GenerateUncached;
 import com.oracle.truffle.api.dsl.NeverDefault;
 import com.oracle.truffle.api.dsl.ReportPolymorphism;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.interop.InteropLibrary;
-import com.oracle.truffle.api.interop.TruffleObject;
-import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.nodes.Node;
-import org.enso.interpreter.node.expression.builtin.number.utils.ToEnsoNumberNode;
 import org.enso.interpreter.node.expression.foreign.CoerceNothing;
 import org.enso.interpreter.runtime.data.text.Text;
-import org.enso.interpreter.runtime.error.WarningsLibrary;
 
 /**
  * Converts a value returned by a polyglot call back to a value that can be further used within Enso
@@ -67,19 +61,6 @@ public abstract class HostValueToEnsoNode extends Node {
     return Text.create(Character.toString(i));
   }
 
-  @Specialization(guards = {"n != null", "iop.fitsInBigInteger(n)", "!warnings.hasWarnings(n)"})
-  Object doBigIntegerConversion(
-      TruffleObject n,
-      @Shared("iop") @CachedLibrary(limit = "3") InteropLibrary iop,
-      @CachedLibrary(limit = "3") WarningsLibrary warnings,
-      @Cached ToEnsoNumberNode to) {
-    try {
-      return to.execute(iop.asBigInteger(n));
-    } catch (UnsupportedMessageException e) {
-      throw CompilerDirectives.shouldNotReachHere(e);
-    }
-  }
-
   @Specialization
   Text doString(String txt) {
     return Text.create(txt);
@@ -88,7 +69,7 @@ public abstract class HostValueToEnsoNode extends Node {
   @Specialization(guards = {"o != null", "iop.isNull(o)"})
   Object doNull(
       Object o,
-      @Shared("iop") @CachedLibrary(limit = "3") InteropLibrary iop,
+      @CachedLibrary(limit = "3") InteropLibrary iop,
       @Cached CoerceNothing coerceNothing) {
     return coerceNothing.execute(o);
   }
