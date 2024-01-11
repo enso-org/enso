ca08bf82c20cbf49765f87145bbeb55121c2c96a
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-01-11 13:35:15 +0100

    Avoid running TCK syntax test for now

--- engine/runtime/src/test/java/org/enso/interpreter/test/tck/EnsoTckInvalidSyntaxTest.java
+++
@@ -1,9 +0,0 @@
-package org.enso.interpreter.test.tck;
-
-import org.graalvm.polyglot.Source;
-
-public class EnsoTckInvalidSyntaxTest extends com.oracle.truffle.tck.tests.InvalidSyntaxTest {
-  public EnsoTckInvalidSyntaxTest(String n, Source s) {
-    super(n, s);
-  }
-}
