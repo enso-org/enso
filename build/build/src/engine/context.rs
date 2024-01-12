4776a8344d2de6344a4c6ec92041bf8f462958c9
Michał Wawrzyniec Urbańczyk <mwu-tow@gazeta.pl>
2024-01-12 04:59:07 +0100

    CI: Enable sanity test for native Runner (#8724)
    Co-authored-by: Pavel Marek <pavel.marek@enso.org>

--- build/build/src/engine/context.rs
+++ build/build/src/engine/context.rs
@@ -18,6 +18,8 @@ use crate::enso::IrCaches;
 use crate::paths::cache_directory;
 use crate::paths::Paths;
 use crate::paths::TargetTriple;
+use crate::paths::ENSO_DATA_DIRECTORY;
+use crate::paths::ENSO_JAVA;
 use crate::paths::ENSO_TEST_JUNIT_DIR;
 use crate::project::ProcessWrapper;
 
@@ -523,24 +525,18 @@ impl RunContext {
         }
 
 
-        // if build_native_runner {
-        //     let factorial_input = "6";
-        //     let factorial_expected_output = "720";
-        //     let output = Command::new(&self.repo_root.runner)
-        //         .args([
-        //             "--run",
-        //
-        // self.repo_root.engine.runner_native.src.test.resources.factorial_enso.as_str(),
-        //             factorial_input,
-        //         ])
-        //         .env(ENSO_DATA_DIRECTORY.name(), &self.paths.engine.dir)
-        //         .run_stdout()
-        //         .await?;
-        //     ensure!(
-        //         output.contains(factorial_expected_output),
-        //         "Native runner output does not contain expected result."
-        //     );
-        // }
+        if build_native_runner {
+            debug!("Building and testing native engine runners");
+            runner_sanity_test(&self.repo_root, None).await?;
+            ide_ci::fs::remove_file_if_exists(&self.repo_root.runner)?;
+            let enso_java = "espresso";
+            sbt.command()?
+                .env(ENSO_JAVA, enso_java)
+                .arg("engine-runner/buildNativeImage")
+                .run_ok()
+                .await?;
+            runner_sanity_test(&self.repo_root, Some(enso_java)).await?;
+        }
 
 
         // Verify License Packages in Distributions
@@ -670,4 +666,31 @@ pub async fn upload_test_results(test_results_dir: PathBuf) -> Result {
         );
     }
     upload_result
+}
+
+/// Run the native runner and check if it produces the expected output on a simple test.
+pub async fn runner_sanity_test(
+    repo_root: &crate::paths::generated::RepoRoot,
+    enso_java: Option<&str>,
+) -> Result {
+    let factorial_input = "6";
+    let factorial_expected_output = "720";
+    let output = Command::new(&repo_root.runner)
+        .args([
+            "--run",
+            repo_root.engine.runner.src.test.resources.factorial_enso.as_str(),
+            factorial_input,
+        ])
+        .set_env_opt(ENSO_JAVA, enso_java)?
+        .set_env(
+            ENSO_DATA_DIRECTORY,
+            repo_root.built_distribution.enso_engine_triple.engine_package.as_path(),
+        )?
+        .run_stdout()
+        .await?;
+    ensure!(
+        output.contains(factorial_expected_output),
+        "Native runner output does not contain expected result '{factorial_expected_output}'. Output:\n{output}",
+    );
+    Ok(())
 }
