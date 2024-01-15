ede9e9a7517433dc0e22bad081272c5066a7f542
somebody1234 <ehern.lee@gmail.com>
2024-01-15 23:08:25 +1000

    Remove references to screenshots

--- app/ide-desktop/lib/dashboard/e2e/labelsPanel.spec.ts
+++ app/ide-desktop/lib/dashboard/e2e/labelsPanel.spec.ts
@@ -35,13 +35,11 @@ test.test('labels', async ({ page }) => {
     await actions.locateNewLabelModalNameInput(page).fill('New Label')
     await test.expect(actions.locateNewLabelModal(page)).toHaveText(/^New Label/)
 
-    // Screenshot (flaky, omitted): Labels panel with one entry
+    // Labels panel with one entry
     await actions.locateCreateButton(actions.locateNewLabelModal(page)).click()
     await test.expect(actions.locateLabelsPanel(page)).toBeVisible()
 
     // Empty labels panel again, after deleting the only entry
-    // This uses a screenshot instead of `toHaveCount(count)` because it is less prone to breakage
-    // and easier to maintain.
     await actions.locateLabelsPanelLabels(page).first().hover()
     await actions.locateDeleteIcon(actions.locateLabelsPanel(page)).first().click()
     await actions.locateDeleteButton(page).click()
