101b9623d582b9727de013206c2c8700290da787
Adam Riley <riley.ad@gmail.com>
2024-01-15 22:49:01 +0000

    In memory implementation

--- std-bits/table/src/main/java/org/enso/table/data/column/storage/StringStorage.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/storage/StringStorage.java
@@ -10,6 +10,7 @@ import org.enso.table.data.column.operation.map.numeric.UnaryIntegerOp;
 import org.enso.table.data.column.operation.map.text.LikeOp;
 import org.enso.table.data.column.operation.map.text.StringBooleanOp;
 import org.enso.table.data.column.operation.map.text.StringIsInOp;
+import org.enso.table.data.column.operation.map.text.StringStringLongOp;
 import org.enso.table.data.column.operation.map.text.StringStringOp;
 import org.enso.table.data.column.storage.type.StorageType;
 import org.enso.table.data.column.storage.type.TextType;
@@ -127,6 +128,20 @@ public final class StringStorage extends SpecializedStorage<String> {
           @Override
           protected long doOperation(String a) {
             return Text_Utils.grapheme_length(a);
+          }
+        });
+    t.add(
+        new StringStringLongOp(Maps.TEXT_LEFT) {
+          @Override
+          protected String doString(String a, Long b) {
+            return Text_Utils.take_prefix(a, b);
+          }
+        });
+    t.add(
+        new StringStringLongOp(Maps.TEXT_RIGHT) {
+          @Override
+          protected String doString(String a, Long b) {
+            return Text_Utils.take_suffix(a, b);
           }
         });
     t.add(
