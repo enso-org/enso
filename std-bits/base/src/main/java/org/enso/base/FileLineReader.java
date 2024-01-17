98512d4e9cb0a781ca53ce622add4bd6e923b934
James Dunkerley <jdunkerley@users.noreply.github.com>
2024-01-17 18:26:26 +0000

    Remove some defect simplify Java code.

--- std-bits/base/src/main/java/org/enso/base/FileLineReader.java
+++ std-bits/base/src/main/java/org/enso/base/FileLineReader.java
@@ -12,9 +12,8 @@ import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.enso.base.arrays.LongArrayList;
-import org.graalvm.polyglot.Context;
 
-public class File_Line_Reader {
+public class FileLineReader {
   private static final Logger LOGGER = Logger.getLogger("enso-file-line-reader");
 
   // ** Amount of data to read at a time for a single line (4KB). */
@@ -37,14 +36,17 @@ public class File_Line_Reader {
     };
   }
 
+  private static int readByte(MappedByteBuffer buffer) {
+    return buffer.hasRemaining() ? buffer.get() : -1;
+  }
+
   // ** Reads a line into an OutputStream.
   //    Returns true if the end of the line was found, false if the buffer finished. */
   private static boolean readLine(MappedByteBuffer buffer, ByteArrayOutputStream result) {
-    int c = buffer.get();
+    int c = readByte(buffer);
     while (moreToRead(c, buffer)) {
       result.write(c);
-      c = buffer.hasRemaining() ? buffer.get() : -1;
-      Context.getCurrent().safepoint();
+      c = readByte(buffer);
     }
     return c != -1;
   }
@@ -52,16 +54,15 @@ public class File_Line_Reader {
   // ** Scans forward one line.
   //    Returns true if the end of the line was found, false if the buffer finished. */
   private static boolean scanLine(MappedByteBuffer buffer) {
-    int c = buffer.get();
+    int c = readByte(buffer);
     while (moreToRead(c, buffer)) {
-      c = buffer.hasRemaining() ? buffer.get() : -1;
-      Context.getCurrent().safepoint();
+      c = readByte(buffer);
     }
     return c != -1;
   }
 
   // ** Reads a line from a file at the given index using the existing rowMap. */
-  public static String readLineByIndex(File file, LongArrayList rowMap, int index, Charset charset)
+  private static String readLineByIndex(File file, LongArrayList rowMap, int index, Charset charset)
       throws IOException {
     if (index >= rowMap.getSize()) {
       throw new IndexOutOfBoundsException(index);
@@ -72,7 +73,8 @@ public class File_Line_Reader {
     if (position >= length) {
       return null;
     }
-    long toRead = rowMap.getSize() > index + 1 ? rowMap.get(index + 1) - position : length - position;
+    long toRead =
+        rowMap.getSize() > index + 1 ? rowMap.get(index + 1) - position : length - position;
 
     // Output buffer
     var outputStream = new ByteArrayOutputStream(128);
@@ -91,7 +93,6 @@ public class File_Line_Reader {
         buffer = channel.map(FileChannel.MapMode.READ_ONLY, position, bufferSize);
         result = readLine(buffer, outputStream);
       }
-
     }
 
     return outputStream.toString(charset);
@@ -129,7 +130,7 @@ public class File_Line_Reader {
       Function<String, Boolean> filter,
       BiFunction<Integer, String, Boolean> action)
       throws IOException {
-    LOGGER.log(Level.INFO, "readLines: {0} {1}", new Object[] { startAt, endAt });
+    LOGGER.log(Level.INFO, "readLines: {0} {1}", new Object[] {startAt, endAt});
 
     if (startAt >= rowMap.getSize()) {
       throw new IndexOutOfBoundsException(startAt);
@@ -143,81 +144,65 @@ public class File_Line_Reader {
     }
 
     boolean readAll = filter != null || action != null || endAt == -1;
+    var outputStream = new ByteArrayOutputStream(128);
     String output = null;
 
     try (var stream = new FileInputStream(file)) {
       var channel = stream.getChannel();
 
-      var bufferSize =  (int)Math.min(BUFFER_SIZE, (length - position));
+      var bufferSize = (int) Math.min(BUFFER_SIZE, (length - position));
       var truncated = bufferSize != (length - position);
       var buffer = channel.map(FileChannel.MapMode.READ_ONLY, position, bufferSize);
 
-      // If we have an action then apply it to the lines we already know about.
-      if (action != null) {
-        while (index < Math.min(endAt, rowMap.getSize() - 1)) {
-          // Do we need to read more data?
-          if (truncated && rowMap.get(index + 1) > position + buffer.position()) {
-            position = rowMap.get(index);
-            bufferSize = (int)Math.min(BUFFER_SIZE, (length - position));
+      // Loop until we either reach the required record or run out of data.
+      while ((endAt == -1 || index <= endAt) && (truncated || buffer.hasRemaining())) {
+        var linePosition = buffer.position() + position;
+
+        // Read a line.
+        outputStream.reset();
+        boolean success =
+            (readAll || index == endAt) ? readLine(buffer, outputStream) : scanLine(buffer);
+
+        if (success || !truncated) {
+          String line = null;
+          if (filter == null || filter.apply(line = outputStream.toString(charset))) {
+            if (index >= rowMap.getSize()) {
+              rowMap.add(linePosition);
+            }
+
+            if (action != null) {
+              line = line == null ? outputStream.toString(charset) : line;
+              action.apply(index, line);
+            }
+
+            if (index == endAt) {
+              output = line == null ? outputStream.toString(charset) : line;
+            }
+
+            if (index % 100000 == 0) {
+              LOGGER.log(Level.INFO, "Scanned Lines: {0}", index);
+            }
+            index++;
+
+            // If no filter we can record the start of the next line.
+            if (filter == null && index == rowMap.getSize()) {
+              rowMap.add(buffer.position() + position);
+            }
+          }
+
+          // Fast-forward if needed
+          if (filter != null && index < rowMap.getSize()) {
+            int newPosition = Math.min(bufferSize, (int) (rowMap.get(index) - position));
+            buffer.position(newPosition);
+          }
+        } else {
+          // Read more if we need to
+          if (!buffer.hasRemaining()) {
+            position = linePosition;
+            bufferSize = (int) Math.min(BUFFER_SIZE, (length - position));
             truncated = bufferSize != (length - position);
             buffer = channel.map(FileChannel.MapMode.READ_ONLY, position, bufferSize);
-          } else if (filter != null) {
-            buffer.position((int) (rowMap.get(index) - position));
           }
-
-          output = readLine(buffer, charset);
-          if (action.apply(index, output)) {
-            return output;
-          }
-
-          index++;
-        }
-      }
-
-      // Scan forward until we reach the end or the endAt index.
-      while (endAt == -1 || index <= endAt) {
-        var linePosition = buffer.position() + position;
-        if (linePosition >= length) {
-          // Reached end of file.
-          break;
-        }
-
-        if (readAll || index == endAt) {
-          output = readLine(buffer, charset);
-        } else {
-          scanLine(buffer);
-        }
-
-        if ((buffer.hasRemaining() || !truncated) && (filter == null || filter.apply(output))) {
-          if (index % 100000 == 0) {
-            LOGGER.log(Level.INFO, "Scanned Lines: {0}", index);
-          }
-          if (index >= rowMap.getSize()) {
-            rowMap.add(linePosition);
-          }
-
-          index++;
-          if (filter == null && index >= rowMap.getSize()) {
-            rowMap.add(buffer.position() + position);
-          }
-
-          // Callback on action and stop if requested.
-          if (action != null && !action.apply(index - 1, output)) {
-            return output;
-          }
-        }
-
-        if (!buffer.hasRemaining()) {
-          if (!truncated) {
-            // Reached end of file.
-            break;
-          }
-
-          // Need to read more data.
-          position = rowMap.get(index);
-          bufferSize = (int) Math.min(BUFFER_SIZE, (length - position));
-          truncated = bufferSize != (length - position);
-          buffer = channel.map(FileChannel.MapMode.READ_ONLY, position, bufferSize);
         }
       }
 
