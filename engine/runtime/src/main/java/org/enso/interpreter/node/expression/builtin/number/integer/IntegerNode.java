ccc3d99c80fd3d8e64d404bfe9ee04f6f0d9cb4f
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-01-18 11:06:51 +0100

    Introducing DoThatConversionNode

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/number/integer/IntegerNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/number/integer/IntegerNode.java
@@ -1,7 +1,10 @@
 package org.enso.interpreter.node.expression.builtin.number.integer;
 
-import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.GenerateUncached;
+import com.oracle.truffle.api.dsl.NeverDefault;
+import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.interop.ArityException;
 import com.oracle.truffle.api.interop.InteropLibrary;
@@ -9,15 +12,12 @@ import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.nodes.Node;
 import org.enso.interpreter.node.callable.InteropConversionCallNode;
-import org.enso.interpreter.node.callable.InvokeCallableNode.ArgumentsExecutionMode;
-import org.enso.interpreter.node.callable.InvokeCallableNode.DefaultsExecutionMode;
 import org.enso.interpreter.node.callable.dispatch.InvokeFunctionNode;
 import org.enso.interpreter.node.expression.builtin.meta.TypeOfNode;
 import org.enso.interpreter.node.expression.builtin.number.utils.ToEnsoNumberNode;
 import org.enso.interpreter.runtime.EnsoContext;
 import org.enso.interpreter.runtime.callable.UnresolvedConversion;
 import org.enso.interpreter.runtime.callable.UnresolvedSymbol;
-import org.enso.interpreter.runtime.callable.argument.CallArgumentInfo;
 import org.enso.interpreter.runtime.callable.function.Function;
 import org.enso.interpreter.runtime.data.Type;
 import org.enso.interpreter.runtime.error.PanicException;
@@ -28,7 +28,6 @@ import org.enso.interpreter.runtime.state.State;
 abstract class IntegerNode extends Node {
   private final String symbol;
   @Child ToEnsoNumberNode toEnsoNumberNode = ToEnsoNumberNode.create();
-  @Child private InteropConversionCallNode convertNode;
 
   IntegerNode(String symbol) {
     this.symbol = symbol;
@@ -83,55 +82,104 @@ abstract class IntegerNode extends Node {
     throw new AbstractMethodError();
   }
 
-  final Object doThatConversion(VirtualFrame frame, Object self, Object that) {
-    var ctx = EnsoContext.get(this);
-    var typeOfNode = TypeOfNode.getUncached();
-    var rawSelfType = typeOfNode.execute(self);
-    var rawThatType = typeOfNode.execute(that);
-    UnresolvedConversion convert = null;
-    Function symbolFn = null;
-    InvokeFunctionNode invokeNode = null;
-    if (rawSelfType == ctx.getBuiltins().number().getInteger()
-        && rawThatType instanceof Type thatType) {
-      CompilerDirectives.transferToInterpreter();
-      convert = UnresolvedConversion.build(thatType.getDefinitionScope());
-      var unresolved = UnresolvedSymbol.build(this.symbol, thatType.getDefinitionScope());
-      var found = unresolved.resolveFor(this, thatType);
-      if (found != null) {
-        symbolFn = found.getLeft();
-        var conversionFn =
-            convert.resolveFor(ctx, thatType, ctx.getBuiltins().number().getInteger());
-        if (conversionFn != null) {
-          convertNode = insert(InteropConversionCallNode.build());
-          var schema =
-              new CallArgumentInfo[] {new CallArgumentInfo("self"), new CallArgumentInfo("that")};
-          invokeNode =
-              InvokeFunctionNode.build(
-                  schema, DefaultsExecutionMode.EXECUTE, ArgumentsExecutionMode.EXECUTE);
-        }
-      }
-    }
-
-    if (convertNode != null && rawThatType instanceof Type type) {
-      try {
-        var state = State.create(ctx);
-        var convertedValue = convertNode.execute(convert, state, new Object[] {rawThatType, self});
-        var result =
-            invokeNode.execute(symbolFn, frame, state, new Object[] {convertedValue, that});
-        return result;
-      } catch (ArityException ex) {
-        return null;
-      }
-    } else {
-      return null;
-    }
-  }
-
   Object doOther(VirtualFrame frame, Object self, Object that) {
     if (doThatConversion(frame, self, that) instanceof Object result) {
       return result;
     } else {
       throw throwTypeErrorIfNotInt(self, that);
+    }
+  }
+
+  final Object doThatConversion(VirtualFrame frame, Object self, Object that) {
+    var node = DoThatConversionNode.getUncached();
+    return node.executeThatConversion(frame, symbol, self, that);
+  }
+
+  /**
+   * A node that checks the type of {@code that} argument and performs conversion of {@code self} to
+   * {@code that} type, if it contains the requested symbol implementation.
+   */
+  @GenerateUncached
+  abstract static class DoThatConversionNode extends Node {
+    static DoThatConversionNode getUncached() {
+      return IntegerNodeFactory.DoThatConversionNodeGen.getUncached();
+    }
+
+    static DoThatConversionNode create() {
+      return IntegerNodeFactory.DoThatConversionNodeGen.create();
+    }
+
+    /**
+     * @return {code null} if the conversion makes no sense or result of doing the conversion an
+     *     executing the {@link #symbol}
+     */
+    abstract Object executeThatConversion(
+        VirtualFrame frame, String symbol, Object self, Object that);
+
+    static Type findType(TypeOfNode typeOfNode, Object obj) {
+      var rawType = typeOfNode.execute(obj);
+      return rawType instanceof Type type ? type : null;
+    }
+
+    static Type findTypeUncached(Object obj) {
+      return findType(TypeOfNode.getUncached(), obj);
+    }
+
+    final Function findSymbol(String symbol, Type type) {
+      var unresolved = UnresolvedSymbol.build(symbol, type.getDefinitionScope());
+      var found = unresolved.resolveFor(this, type);
+      if (found != null) {
+        return found.getLeft();
+      } else {
+        return null;
+      }
+    }
+
+    @NeverDefault
+    static UnresolvedConversion buildConversion(Type type) {
+      return UnresolvedConversion.build(type.getDefinitionScope());
+    }
+
+    @Specialization(
+        limit = "3",
+        guards = {
+          "cachedSymbol.equals(symbol)",
+          "selfType != null",
+          "thatType != null",
+          "symbolFn != null",
+          "thatType == findType(typeOfNode, that)"
+        })
+    final Object doThatConversion(
+        VirtualFrame frame,
+        String symbol,
+        Object self,
+        Object that,
+        @Cached("symbol") String cachedSymbol,
+        @Cached TypeOfNode typeOfNode,
+        @Cached(value = "findType(typeOfNode, self)", uncached = "findTypeUncached(self)")
+            Type selfType,
+        @Cached(value = "findType(typeOfNode, that)", uncached = "findTypeUncached(that)")
+            Type thatType,
+        @Cached(allowUncached = true, value = "findSymbol(cachedSymbol, thatType)")
+            Function symbolFn,
+        @Cached(allowUncached = true, value = "buildConversion(thatType)")
+            UnresolvedConversion convert,
+        @Cached InteropConversionCallNode convertNode,
+        @Cached(allowUncached = true, value = "buildWithArity(2)") InvokeFunctionNode invokeNode) {
+      var ctx = EnsoContext.get(this);
+      var state = State.create(ctx);
+      try {
+        var selfAsThat = convertNode.execute(convert, state, new Object[] {thatType, self});
+        var result = invokeNode.execute(symbolFn, frame, state, new Object[] {selfAsThat, that});
+        return result;
+      } catch (ArityException ex) {
+        return null;
+      }
+    }
+
+    @Specialization
+    final Object doThatConversionSlow(VirtualFrame frame, String symbol, Object self, Object that) {
+      return null;
     }
   }
 }
