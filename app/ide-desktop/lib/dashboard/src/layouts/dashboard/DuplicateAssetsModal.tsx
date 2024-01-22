d7eeb251ac3d3876bc9eb7ec8dbb76fa282b6bd7
somebody1234 <ehern.lee@gmail.com>
2024-01-23 01:17:05 +1000

    Improve text

--- app/ide-desktop/lib/dashboard/src/layouts/dashboard/DuplicateAssetsModal.tsx
+++ app/ide-desktop/lib/dashboard/src/layouts/dashboard/DuplicateAssetsModal.tsx
@@ -23,6 +23,12 @@ const pluralizeFile = string.makePluralize('file', 'files')
 // This is a function, even though it does not look like one.
 // eslint-disable-next-line no-restricted-syntax
 const pluralizeProject = string.makePluralize('project', 'projects')
+// This is a function, even though it does not look like one.
+// eslint-disable-next-line no-restricted-syntax
+const pluralizeFileUppercase = string.makePluralize('File', 'Files')
+// This is a function, even though it does not look like one.
+// eslint-disable-next-line no-restricted-syntax
+const pluralizeProjectUppercase = string.makePluralize('Project', 'Projects')
 
 // =============
 // === Types ===
@@ -73,6 +79,22 @@ export default function DuplicateAssetsModal(props: DuplicateAssetsModalProps) {
     const siblingProjectNames = React.useRef(new Set<string>())
     const count = conflictingFiles.length + conflictingProjects.length
     const firstConflict = conflictingFiles[0] ?? conflictingProjects[0]
+    let firstConflictTypeName: string
+    switch (firstConflict?.new.type) {
+        case backendModule.AssetType.file: {
+            firstConflictTypeName = 'File'
+            break
+        }
+        case backendModule.AssetType.project: {
+            firstConflictTypeName = 'Project'
+            break
+        }
+        // eslint-disable-next-line no-restricted-syntax
+        case undefined: {
+            // This variable does not matter as it should not be used.
+            firstConflictTypeName = 'Unknown Asset'
+        }
+    }
     const otherFilesCount = Math.max(0, conflictingFiles.length - 1)
     const otherFilesText =
         otherFilesCount === 0
@@ -85,6 +107,8 @@ export default function DuplicateAssetsModal(props: DuplicateAssetsModalProps) {
             : `and ${otherProjectsCount}${
                   conflictingFiles.length > 0 ? '' : ' other'
               } ${pluralizeProject(otherProjectsCount)}`
+    const filesTextUppercase = pluralizeFileUppercase(conflictingFiles.length)
+    const projectsTextUppercase = pluralizeProjectUppercase(conflictingProjects.length)
 
     React.useEffect(() => {
         for (const name of siblingFileNamesRaw) {
@@ -181,7 +205,13 @@ export default function DuplicateAssetsModal(props: DuplicateAssetsModalProps) {
                 }}
             >
                 <h1 className="relative text-sm font-semibold">
-                    Duplicate Files and Projects Found
+                    Duplicate{' '}
+                    {conflictingFiles.length > 0
+                        ? conflictingProjects.length > 0
+                            ? `${filesTextUppercase} and ${projectsTextUppercase}`
+                            : filesTextUppercase
+                        : projectsTextUppercase}{' '}
+                    Found
                 </h1>
                 {nonConflictingCount > 0 && (
                     <div className="relative flex flex-col gap-0.5">
@@ -261,7 +291,7 @@ export default function DuplicateAssetsModal(props: DuplicateAssetsModalProps) {
                                         }
                                     }}
                                 >
-                                    Rename New File
+                                    Rename New {firstConflictTypeName}
                                 </button>
                             </div>
                         )}
@@ -293,7 +323,9 @@ export default function DuplicateAssetsModal(props: DuplicateAssetsModalProps) {
                             doRename([...conflictingFiles, ...conflictingProjects])
                         }}
                     >
-                        {count === 1 ? 'Rename New File' : 'Rename New Files'}
+                        {count === 1
+                            ? `Rename New ${firstConflictTypeName}`
+                            : `Rename New ${firstConflictTypeName}s`}
                     </button>
                     <button
                         type="button"
