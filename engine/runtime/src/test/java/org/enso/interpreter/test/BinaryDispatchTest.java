f1478eb55d7ba7cfa21aa845422d67270e1f8ab0
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-01-23 20:01:44 +0100

    Static invocations exactly defines the function to run

--- engine/runtime/src/test/java/org/enso/interpreter/test/BinaryDispatchTest.java
+++ engine/runtime/src/test/java/org/enso/interpreter/test/BinaryDispatchTest.java
@@ -1,13 +1,17 @@
 package org.enso.interpreter.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.enso.polyglot.MethodNames;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.PolyglotException;
 import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class BinaryDispatchTest extends TestBase {
@@ -24,6 +28,8 @@ public class BinaryDispatchTest extends TestBase {
         Source.newBuilder(
                 "enso",
                 """
+                import Standard.Base.Data.Numbers
+
                 type Z
                     Number n
 
@@ -84,5 +90,92 @@ public class BinaryDispatchTest extends TestBase {
 
     var diff1 = one.invokeMember("---", threeHalfs);
     assertEquals(diff1.asDouble(), 1.0 / 2, 0.01);
+  }
+
+  @Test
+  public void staticVerifyZ() {
+    var zOperator = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "Z.---");
+    assertTrue("It's executable", zOperator.canExecute());
+
+    var six = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "Z.Number 6");
+    var ten = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "Z.Number 10");
+
+    var diff1 = zOperator.execute(six, ten);
+    assertEquals(diff1.asInt(), 4);
+  }
+
+  @Test
+  public void staticVerifyR() {
+    var rOperator = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "R.---");
+    assertTrue("It's executable", rOperator.canExecute());
+
+    var half = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "R.Fraction 1 2");
+    var oneEight = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "R.Fraction 1 8");
+
+    var diff1 = rOperator.execute(half, oneEight);
+    assertEquals(diff1.asDouble(), 3.0 / 8, 0.01);
+  }
+
+  @Test
+  @Ignore // PENDING: #8805
+  public void staticWithRFirstArgumentIsConverted() {
+    var rOperator = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "R.---");
+
+    var two = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "Z.Number 2");
+    var half = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "R.Fraction 1 2");
+
+    var diff1 = rOperator.execute(two, half);
+    assertEquals(diff1.asDouble(), 3.0 / 2, 0.01);
+  }
+
+  @Test
+  public void staticWithRSecondArgumentIsConverted() {
+    var rOperator = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "R.---");
+
+    var half = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "R.Fraction 1 2");
+    var two = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "Z.Number 2");
+
+    var diff1 = rOperator.execute(half, two);
+    assertEquals(diff1.asDouble(), 3.0 / 2, 0.01);
+  }
+
+  @Test
+  public void staticWithZFirstAndRSecondNoConversionHappens() {
+    var zOperator = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "Z.---");
+
+    var two = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "Z.Number 2");
+    var half = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "R.Fraction 1 2");
+
+    try {
+      var diff1 = zOperator.execute(two, half);
+      fail("Shouldn't return a value: " + diff1);
+    } catch (PolyglotException ex) {
+      assertContains("Type error", ex.getMessage());
+      assertContains("`that` to be Z", ex.getMessage());
+    }
+  }
+
+  @Test
+  @Ignore // PENDING #8805
+  public void staticWithRFirstAndZSecondNoConversionHappens() {
+    var zOperator = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "Z.---");
+
+    var half = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "R.Fraction 1 2");
+    var two = module.invokeMember(MethodNames.Module.EVAL_EXPRESSION, "Z.Number 2");
+
+    try {
+      var diff1 = zOperator.execute(half, two);
+      fail("Shouldn't return a value: " + diff1);
+    } catch (PolyglotException ex) {
+      assertContains("Type error", ex.getMessage());
+      assertContains("`self` to be Z", ex.getMessage());
+    }
+  }
+
+  private static void assertContains(String expected, String actual) {
+    if (actual.contains(expected)) {
+      return;
+    }
+    fail("Expecting " + expected + " in " + actual);
   }
 }
