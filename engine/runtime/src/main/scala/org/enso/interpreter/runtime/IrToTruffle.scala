738dba7f1c493d38f9004cb0733a312d70cabcab
Radosław Waśko <radoslaw.wasko@enso.org>
2024-01-25 13:56:35 +0100

    probable fix for https://github.com/enso-org/enso/issues/8706

--- engine/runtime/src/main/scala/org/enso/interpreter/runtime/IrToTruffle.scala
+++ engine/runtime/src/main/scala/org/enso/interpreter/runtime/IrToTruffle.scala
@@ -2279,7 +2279,7 @@ class IrToTruffle(
               moduleScope,
               defaultExpression,
               null,
-              s"<default::$scopeName::${arg.name}>",
+              s"<default::$scopeName::${arg.name.showCode()}>",
               false,
               false
             )
