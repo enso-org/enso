586de79ae00ec50060e9f999a59c578d07ed2559
Adam Obuchowicz <adam.obuchowicz@enso.org>
2024-01-26 08:52:09 +0100

    Fix gui2 typecheck

--- app/gui2/src/components/widgets/PlaceholderWidget.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import SvgIcon from '@/components/SvgIcon.vue'
-</script>
-
-<template>
-  <div class="Placeholder">
-    <SvgIcon :width="24" :height="16" name="underscore" />
-  </div>
-</template>
