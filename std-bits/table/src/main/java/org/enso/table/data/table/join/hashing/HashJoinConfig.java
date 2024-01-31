c4b9d83c1a877bb5c8d477e507dd09af8b27e0af
Adam Riley <riley.ad@gmail.com>
2024-01-31 09:12:32 +0000

    Green

---
+++ std-bits/table/src/main/java/org/enso/table/data/table/join/hashing/HashJoinConfig.java
@@ -0,0 +1,60 @@
+package org.enso.table.data.table.join.hashing;
+
+import java.util.List;
+
+import org.enso.base.text.TextFoldingStrategy;
+import org.enso.table.data.table.Column;
+import org.enso.table.data.table.join.conditions.Equals;
+import org.enso.table.data.table.join.conditions.EqualsIgnoreCase;
+import org.enso.table.data.table.join.conditions.HashableCondition;
+
+public class HashJoinConfig {
+
+    private Column[] leftEquals;
+    private Column[] rightEquals;
+    private List<TextFoldingStrategy> textFoldingStrategies;
+
+    public HashJoinConfig(List<HashableCondition> conditions) {
+        List<HashEqualityCondition> equalConditions
+                = conditions.stream().map(HashJoinConfig::makeHashEqualityCondition).toList();
+
+        if (equalConditions.isEmpty()) {
+            throw new IllegalArgumentException(
+                    "EqualityHashJoin is applicable if there is at least one equality condition.");
+        }
+
+        this.leftEquals = equalConditions.stream().map(HashEqualityCondition::left).toArray(Column[]::new);
+        this.rightEquals = equalConditions.stream().map(HashEqualityCondition::right).toArray(Column[]::new);
+        this.textFoldingStrategies = equalConditions.stream().map(HashEqualityCondition::textFoldingStrategy).toList();
+    }
+
+    public Column[] getLeftEquals() {
+        return leftEquals;
+    }
+
+    public Column[] getRightEquals() {
+        return rightEquals;
+    }
+
+    public List<TextFoldingStrategy> getTextFoldingStrategies() {
+        return textFoldingStrategies;
+    }
+
+    private static HashEqualityCondition makeHashEqualityCondition(HashableCondition eq) {
+        switch (eq) {
+            case Equals e -> {
+                return new HashEqualityCondition(
+                        e.left(), e.right(), TextFoldingStrategy.unicodeNormalizedFold);
+            }
+            case EqualsIgnoreCase e -> {
+                return new HashEqualityCondition(
+                        e.left(), e.right(), TextFoldingStrategy.caseInsensitiveFold(e.locale()));
+            }
+        }
+    }
+
+    private record HashEqualityCondition(
+            Column left, Column right, TextFoldingStrategy textFoldingStrategy) {
+
+    }
+}
--- std-bits/table/src/main/java/org/enso/table/data/table/join/hashing/SimpleHashJoin.java
+++ std-bits/table/src/main/java/org/enso/table/data/table/join/hashing/SimpleHashJoin.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import org.enso.base.text.TextFoldingStrategy;
+
 import org.enso.table.data.column.storage.Storage;
 import org.enso.table.data.index.MultiValueIndex;
 import org.enso.table.data.index.UnorderedMultiValueKey;
@@ -15,8 +15,6 @@ import org.enso.table.data.table.join.JoinStrategy;
 import org.enso.table.data.table.join.MatchAllStrategy;
 import org.enso.table.data.table.join.NoOpStrategy;
 import org.enso.table.data.table.join.PluggableJoinStrategy;
-import org.enso.table.data.table.join.conditions.Equals;
-import org.enso.table.data.table.join.conditions.EqualsIgnoreCase;
 import org.enso.table.data.table.join.conditions.HashableCondition;
 import org.enso.table.problems.ColumnAggregatedProblemAggregator;
 import org.enso.table.problems.ProblemAggregator;
@@ -25,100 +23,77 @@ import org.graalvm.polyglot.Context;
 /**
  * A strategy that uses a hash-map to perform join on the equality conditions.
  *
- * <p>It then delegates to {@code remainingMatcher} to perform the remaining conditions on the
- * matching pairs of row subsets.
+ * <p>
+ * It then delegates to {@code remainingMatcher} to perform the remaining
+ * conditions on the matching pairs of row subsets.
  */
 public class SimpleHashJoin implements JoinStrategy {
-  public SimpleHashJoin(List<HashableCondition> conditions, JoinKind joinKind) {
-    JoinStrategy.ensureConditionsNotEmpty(conditions);
-    this.remainingMatcher = joinKind.wantsCommon ? new MatchAllStrategy() : new NoOpStrategy();
-    this.joinKind = joinKind;
 
-    List<HashEqualityCondition> equalConditions =
-        conditions.stream().map(SimpleHashJoin::makeHashEqualityCondition).toList();
-
-    if (equalConditions.isEmpty()) {
-      throw new IllegalArgumentException(
-          "EqualityHashJoin is applicable if there is at least one equality condition.");
+    public SimpleHashJoin(List<HashableCondition> conditions, JoinKind joinKind) {
+        JoinStrategy.ensureConditionsNotEmpty(conditions);
+        this.remainingMatcher = joinKind.wantsCommon ? new MatchAllStrategy() : new NoOpStrategy();
+        this.joinKind = joinKind;
+        this.hashJoinConfig = new HashJoinConfig(conditions);
     }
 
-    leftEquals = equalConditions.stream().map(HashEqualityCondition::left).toArray(Column[]::new);
-    rightEquals = equalConditions.stream().map(HashEqualityCondition::right).toArray(Column[]::new);
-    textFoldingStrategies =
-        equalConditions.stream().map(HashEqualityCondition::textFoldingStrategy).toList();
-  }
+    private final HashJoinConfig hashJoinConfig;
+    private final PluggableJoinStrategy remainingMatcher;
+    private final JoinKind joinKind;
 
-  private final Column[] leftEquals, rightEquals;
-  private final List<TextFoldingStrategy> textFoldingStrategies;
-  private final PluggableJoinStrategy remainingMatcher;
-  private final JoinKind joinKind;
+    @Override
+    public JoinResult join(ProblemAggregator problemAggregator) {
+        Context context = Context.getCurrent();
 
-  @Override
-  public JoinResult join(ProblemAggregator problemAggregator) {
-    Context context = Context.getCurrent();
+        var rightEquals = hashJoinConfig.getRightEquals();
+        var leftEquals = hashJoinConfig.getLeftEquals();
+        var textFoldingStrategies = hashJoinConfig.getTextFoldingStrategies();
 
-    var rightIndex =
-        MultiValueIndex.makeUnorderedIndex(
-            rightEquals, rightEquals[0].getSize(), textFoldingStrategies, problemAggregator);
+        var rightIndex
+                = MultiValueIndex.makeUnorderedIndex(
+                        rightEquals, rightEquals[0].getSize(), textFoldingStrategies, problemAggregator);
 
-    JoinResult.Builder resultBuilder = new JoinResult.Builder();
-    final Storage<?>[] storage =
-        Arrays.stream(leftEquals).map(Column::getStorage).toArray(Storage[]::new);
+        JoinResult.Builder resultBuilder = new JoinResult.Builder();
+        final Storage<?>[] storage
+                = Arrays.stream(leftEquals).map(Column::getStorage).toArray(Storage[]::new);
 
-    Set<UnorderedMultiValueKey> matchedRightKeys = new HashSet<>();
-    ColumnAggregatedProblemAggregator groupingProblemAggregator =
-        new ColumnAggregatedProblemAggregator(problemAggregator);
-    for (int leftRow = 0; leftRow < leftEquals[0].getSize(); leftRow++) {
-      var leftKey = new UnorderedMultiValueKey(storage, leftRow, textFoldingStrategies);
-      leftKey.checkAndReportFloatingEquality(
-          groupingProblemAggregator, columnIx -> leftEquals[columnIx].getName());
+        Set<UnorderedMultiValueKey> matchedRightKeys = new HashSet<>();
+        ColumnAggregatedProblemAggregator groupingProblemAggregator
+                = new ColumnAggregatedProblemAggregator(problemAggregator);
+        for (int leftRow = 0; leftRow < leftEquals[0].getSize(); leftRow++) {
+            var leftKey = new UnorderedMultiValueKey(storage, leftRow, textFoldingStrategies);
+            leftKey.checkAndReportFloatingEquality(
+                    groupingProblemAggregator, columnIx -> leftEquals[columnIx].getName());
 
-      // If any field of the key is null, it cannot match anything.
-      List<Integer> rightRows = leftKey.hasAnyNulls() ? null : rightIndex.get(leftKey);
-      if (rightRows != null) {
-        List<Integer> leftRows = List.of(leftRow);
-        remainingMatcher.joinSubsets(leftRows, rightRows, resultBuilder, problemAggregator);
-        if (joinKind.wantsRightUnmatched) {
-          matchedRightKeys.add(leftKey);
-        }
-      } else {
-        if (joinKind.wantsLeftUnmatched) {
-          resultBuilder.addUnmatchedLeftRow(leftRow);
-          context.safepoint();
-        }
-      }
-      context.safepoint();
-    }
-
-    if (joinKind.wantsRightUnmatched) {
-      for (var rightEntry : rightIndex.mapping().entrySet()) {
-        UnorderedMultiValueKey rightKey = rightEntry.getKey();
-        boolean wasCompletelyUnmatched = !matchedRightKeys.contains(rightKey);
-        if (wasCompletelyUnmatched) {
-          for (int rightRow : rightEntry.getValue()) {
-            resultBuilder.addUnmatchedRightRow(rightRow);
+            // If any field of the key is null, it cannot match anything.
+            List<Integer> rightRows = leftKey.hasAnyNulls() ? null : rightIndex.get(leftKey);
+            if (rightRows != null) {
+                List<Integer> leftRows = List.of(leftRow);
+                remainingMatcher.joinSubsets(leftRows, rightRows, resultBuilder, problemAggregator);
+                if (joinKind.wantsRightUnmatched) {
+                    matchedRightKeys.add(leftKey);
+                }
+            } else {
+                if (joinKind.wantsLeftUnmatched) {
+                    resultBuilder.addUnmatchedLeftRow(leftRow);
+                    context.safepoint();
+                }
+            }
             context.safepoint();
-          }
         }
-      }
+
+        if (joinKind.wantsRightUnmatched) {
+            for (var rightEntry : rightIndex.mapping().entrySet()) {
+                UnorderedMultiValueKey rightKey = rightEntry.getKey();
+                boolean wasCompletelyUnmatched = !matchedRightKeys.contains(rightKey);
+                if (wasCompletelyUnmatched) {
+                    for (int rightRow : rightEntry.getValue()) {
+                        resultBuilder.addUnmatchedRightRow(rightRow);
+                        context.safepoint();
+                    }
+                }
+            }
+        }
+
+        return resultBuilder.buildAndInvalidate();
     }
-
-    return resultBuilder.buildAndInvalidate();
-  }
-
-  private static HashEqualityCondition makeHashEqualityCondition(HashableCondition eq) {
-    switch (eq) {
-      case Equals e -> {
-        return new HashEqualityCondition(
-            e.left(), e.right(), TextFoldingStrategy.unicodeNormalizedFold);
-      }
-      case EqualsIgnoreCase e -> {
-        return new HashEqualityCondition(
-            e.left(), e.right(), TextFoldingStrategy.caseInsensitiveFold(e.locale()));
-      }
-    }
-  }
-
-  private record HashEqualityCondition(
-      Column left, Column right, TextFoldingStrategy textFoldingStrategy) {}
 }
