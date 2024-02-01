e7eeaac01da80ebd562842cf218bc5d28b793514
Adam Obuchowicz <adam.obuchowicz@enso.org>
2024-02-01 12:19:00 +0100

    Revert "Bump electron + fixes"
    This reverts commit e833dfe912d471847ed94b4fb66d2513828ddb45.

--- app/ide-desktop/lib/client/src/bin/server.ts
+++ app/ide-desktop/lib/client/src/bin/server.ts
@@ -176,11 +176,11 @@ export class Server {
         } else {
             const url = requestUrl.split('?')[0]
             const resource = url === '/' ? '/index.html' : requestUrl
-            // `preload.js` must be specialcased here as it is loaded by electron from the root,
+            // `preload.cjs` must be specialcased here as it is loaded by electron from the root,
             // in contrast to all assets loaded by the window, which are loaded from `assets/` via
             // this server.
             const resourceFile =
-                resource === '/preload.js.map'
+                resource === '/preload.cjs.map'
                     ? `${paths.APP_PATH}${resource}`
                     : `${this.config.dir}${resource}`
             for (const [header, value] of common.COOP_COEP_CORP_HEADERS) {
