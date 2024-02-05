d5c6ac85d8c0430818ab8d3191c67e5b82da3abb
Kaz <kaz@lambdaverse.org>
2024-02-05 08:02:35 -0800

    Fix merge conflict

--- app/gui2/src/providers/widgetTree.ts
+++ app/gui2/src/providers/widgetTree.ts
@@ -1,10 +1,6 @@
 import { createContextStore } from '@/providers'
-<<<<<<< HEAD
 import { useGraphStore } from '@/stores/graph'
-import { asNodeId } from '@/stores/graph/graphDatabase'
-=======
 import { type NodeId } from '@/stores/graph/graphDatabase'
->>>>>>> origin/develop
 import { Ast } from '@/util/ast'
 import { computed, proxyRefs, type Ref } from 'vue'
 
@@ -13,7 +9,6 @@ const { provideFn, injectFn } = createContextStore(
   'Widget tree',
   (astRoot: Ref<Ast.Ast>, nodeId: Ref<NodeId>, hasActiveAnimations: Ref<boolean>) => {
     const graph = useGraphStore()
-    const nodeId = computed(() => asNodeId(astRoot.value.id))
     const nodeSpanStart = computed(() => graph.moduleSource.getSpan(astRoot.value.id)![0])
     return proxyRefs({ astRoot, nodeId, nodeSpanStart, hasActiveAnimations })
   },
