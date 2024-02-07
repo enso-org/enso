f4105f9dcb69e3c7a9ef9c745a37ee3f10af6082
Adam Riley <riley.ad@gmail.com>
2024-02-07 20:01:25 +0000

    Refactor

--- std-bits/table/src/main/java/org/enso/table/data/table/TableToXml.java
+++ std-bits/table/src/main/java/org/enso/table/data/table/TableToXml.java
@@ -1,12 +1,12 @@
 package org.enso.table.data.table;
 
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.apache.xmlbeans.XmlException;
 import org.apache.xmlbeans.XmlObject;
 import org.apache.xmlbeans.XmlOptions;
 import org.graalvm.polyglot.Context;
+import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -20,46 +20,38 @@ public class TableToXml {
       String root_name,
       String row_name)
       throws XmlException, ParserConfigurationException {
-    DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-    DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-    Document doc = docBuilder.newDocument();
-    Element rootElement =
+    var docFactory = DocumentBuilderFactory.newInstance();
+    var docBuilder = docFactory.newDocumentBuilder();
+    var doc = docBuilder.newDocument();
+    var rootElement =
         doc.createElement(root_name.isEmpty() ? "root" : makeXmlTagNameLegal(root_name));
     doc.appendChild(rootElement);
 
-    Context context = Context.getCurrent();
+    var context = Context.getCurrent();
     for (int row = 0; row < rowCount; row++) {
-      Element rowElement =
+      var rowElement =
           doc.createElement(row_name.isEmpty() ? "row" : makeXmlTagNameLegal(row_name));
       if (value_Column != null) {
-        var item = value_Column.getStorage().getItemBoxed(row);
-        if (item != null) {
-          rowElement.setTextContent(item.toString());
-        }
+        get_set_value(value_Column, row, rowElement);
       }
-      for (Column element_column : element_columns) {
-        var item = element_column.getStorage().getItemBoxed(row);
-        if (item != null) {
-          Element columnElement = doc.createElement(makeXmlTagNameLegal(element_column.getName()));
-          columnElement.setTextContent(item.toString());
-          rowElement.appendChild(columnElement);
-        }
+      for (var element_column : element_columns) {
+        get_append_element(element_column, row, doc, rowElement);
         context.safepoint();
       }
-      for (Column attribute_column : attribute_columns) {
-        var item = attribute_column.getStorage().getItemBoxed(row);
-        if (item != null) {
-          rowElement.setAttribute(makeXmlTagNameLegal(attribute_column.getName()), item.toString());
-        }
+      for (var attribute_column : attribute_columns) {
+        get_set_attribute(attribute_column, row, rowElement);
         context.safepoint();
       }
       rootElement.appendChild(rowElement);
       context.safepoint();
     }
 
-    XmlObject xmlObject = XmlObject.Factory.parse(doc);
+    return convert_to_string(doc);
+  }
 
-    XmlOptions options = new XmlOptions();
+  private static String convert_to_string(Document doc) throws XmlException {
+    var xmlObject = XmlObject.Factory.parse(doc);
+    var options = new XmlOptions();
     options.setSavePrettyPrint();
     options.setUseDefaultNamespace();
     options.setSaveAggressiveNamespaces();
@@ -67,6 +59,32 @@ public class TableToXml {
     String xmlString = xmlObject.xmlText(options);
 
     return xmlString;
+  }
+
+  private static void get_set_attribute(Column attribute_column, int row, Element rowElement)
+      throws DOMException {
+    var item = attribute_column.getStorage().getItemBoxed(row);
+    if (item != null) {
+      rowElement.setAttribute(makeXmlTagNameLegal(attribute_column.getName()), item.toString());
+    }
+  }
+
+  private static void get_append_element(
+      Column element_column, int row, Document doc, Element rowElement) throws DOMException {
+    var item = element_column.getStorage().getItemBoxed(row);
+    if (item != null) {
+      var columnElement = doc.createElement(makeXmlTagNameLegal(element_column.getName()));
+      columnElement.setTextContent(item.toString());
+      rowElement.appendChild(columnElement);
+    }
+  }
+
+  private static void get_set_value(Column value_Column, int row, Element rowElement)
+      throws DOMException {
+    var item = value_Column.getStorage().getItemBoxed(row);
+    if (item != null) {
+      rowElement.setTextContent(item.toString());
+    }
   }
 
   public static String makeXmlTagNameLegal(String input) {
