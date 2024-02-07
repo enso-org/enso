dc6535706f060bd2b27fdd84ef7a11a62d9e13d9
Adam Riley <riley.ad@gmail.com>
2024-02-07 20:01:26 +0000

    Refactor

--- std-bits/table/src/main/java/org/enso/table/data/table/TableToXml.java
+++ std-bits/table/src/main/java/org/enso/table/data/table/TableToXml.java
@@ -87,11 +87,13 @@ public class TableToXml {
     }
 
     public static String makeXmlTagNameLegal(String input) {
+        String nameStartChar = "[^:A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\ud800\udc00-\udbff\udfff]";
+        String nameChar = "[^-.0-9:A-Z_a-z\u00B7\u0300-\u036F\u203F-\u2040\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\ud800\udc00-\udbff\udfff]";
         // XML tag names cannot start with a number or punctuation character, and cannot contain spaces
-        String cleaned = input.replaceAll("^[^a-zA-Z0-9_]+|[^a-zA-Z0-9-_:.]", "_");
+        String cleaned = input.replaceAll(nameChar, "_");
         // If the cleaned name is empty or doesn't start with a valid character, prefix it with an
         // underscore
-        if (cleaned.isEmpty() || !cleaned.substring(0, 1).matches("[a-zA-Z_]")) {
+        if (cleaned.isEmpty() || cleaned.substring(0, 1).matches(nameStartChar)) {
             cleaned = "_" + cleaned;
         }
         return cleaned;
