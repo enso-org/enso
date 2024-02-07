b109eada1b375bb3d58b93c0718b949b49427d5b
Pavel Marek <pavel.marek@enso.org>
2024-02-07 16:05:39 +0100

    Merge BenchmarksRunner from bench-processor

--- lib/java/benchmarks-common/src/main/java/org/enso/interpreter/bench/BenchmarksRunner.java
+++ lib/java/benchmarks-common/src/main/java/org/enso/interpreter/bench/BenchmarksRunner.java
@@ -2,7 +2,9 @@ package org.enso.interpreter.bench;
 
 import jakarta.xml.bind.JAXBException;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.openjdk.jmh.results.RunResult;
@@ -11,6 +13,8 @@ import org.openjdk.jmh.runner.BenchmarkListEntry;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
+import org.openjdk.jmh.runner.options.CommandLineOptionException;
+import org.openjdk.jmh.runner.options.CommandLineOptions;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.runner.options.TimeValue;
@@ -28,29 +32,80 @@ public class BenchmarksRunner {
         .collect(Collectors.toList());
   }
 
-  /**
-   * Runs and persists a new report for a given benchmark.
-   *
-   * @param label a qualified (including method) name of the benchmark to run.
-   * @return a {@link BenchmarkItem} containing current run result and historical results.
-   */
-  public BenchmarkItem run(String label) throws RunnerException, JAXBException {
-    ChainedOptionsBuilder builder =
-        new OptionsBuilder()
-            .jvmArgsAppend("-Xss16M", "-Dpolyglot.engine.MultiTier=false")
-            .include("^" + label + "$");
-
-    if (Boolean.getBoolean("bench.compileOnly")) {
-      builder
-          .measurementIterations(1)
-          .warmupIterations(0)
-          .measurementTime(TimeValue.seconds(1))
-          .forks(0);
+  public static void run(String[] args) throws RunnerException {
+    CommandLineOptions cmdOpts = null;
+    try {
+      cmdOpts = new CommandLineOptions(args);
+    } catch (CommandLineOptionException e) {
+      System.err.println("Error parsing command line args:");
+      System.err.println("  " + e.getMessage());
+      System.exit(1);
     }
 
-    Options benchmarkOptions = builder.build();
-    RunResult benchmarksResult = new Runner(benchmarkOptions).runSingle();
+    if (Boolean.getBoolean("bench.compileOnly")) {
+      // Do not report results from `compileOnly` mode
+      runCompileOnly(cmdOpts.getIncludes());
+    } else {
+      Runner jmhRunner = new Runner(cmdOpts);
 
+      if (cmdOpts.shouldHelp()) {
+        System.err.println("Enso benchmark runner: A modified JMH runner for Enso benchmarks.");
+        try {
+          cmdOpts.showHelp();
+        } catch (IOException e) {
+          throw new IllegalStateException("Unreachable", e);
+        }
+        System.exit(0);
+      }
+
+      if (cmdOpts.shouldList()) {
+        jmhRunner.list();
+        System.exit(0);
+      }
+
+      Collection<RunResult> results;
+      results = jmhRunner.run();
+
+      for (RunResult result : results) {
+        try {
+          reportResult(result.getParams().getBenchmark(), result);
+        } catch (JAXBException e) {
+          throw new IllegalStateException("Benchmark result report writing failed", e);
+        }
+      }
+      System.out.println("Benchmark results reported into " + REPORT_FILE.getAbsolutePath());
+    }
+  }
+
+
+  private static Collection<RunResult> runCompileOnly(List<String> includes)
+      throws RunnerException {
+    System.out.println("Running benchmarks " + includes + " in compileOnly mode");
+    var optsBuilder = new OptionsBuilder().measurementIterations(1).warmupIterations(0).forks(0);
+    includes.forEach(optsBuilder::include);
+    var opts = optsBuilder.build();
+    var runner = new Runner(opts);
+    return runner.run();
+  }
+
+  public static BenchmarkItem runSingle(String label) throws RunnerException, JAXBException {
+    String includeRegex = "^" + label + "$";
+    if (Boolean.getBoolean("bench.compileOnly")) {
+      var results = runCompileOnly(List.of(includeRegex));
+      var firstResult = results.iterator().next();
+      return reportResult(label, firstResult);
+    } else {
+      var opts =
+          new OptionsBuilder()
+              .jvmArgsAppend("-Xss16M", "-Dpolyglot.engine.MultiTier=false")
+              .include(includeRegex)
+              .build();
+      RunResult benchmarksResult = new Runner(opts).runSingle();
+      return reportResult(label, benchmarksResult);
+    }
+  }
+
+  private static BenchmarkItem reportResult(String label, RunResult result) throws JAXBException {
     Report report;
     if (REPORT_FILE.exists()) {
       report = Report.readFromFile(REPORT_FILE);
@@ -58,8 +113,7 @@ public class BenchmarksRunner {
       report = new Report();
     }
 
-    BenchmarkItem benchItem =
-        new BenchmarkResultProcessor().processResult(label, report, benchmarksResult);
+    BenchmarkItem benchItem = new BenchmarkResultProcessor().processResult(label, report, result);
 
     Report.writeToFile(report, REPORT_FILE);
     return benchItem;
