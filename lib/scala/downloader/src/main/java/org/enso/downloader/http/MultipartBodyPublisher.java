b17f86b5db5d01fd427a6e0a9edcf38c30b030c3
Pavel Marek <pavel.marek@enso.org>
2024-02-09 14:47:12 +0100

    fmt

--- lib/scala/downloader/src/main/java/org/enso/downloader/http/MultipartBodyPublisher.java
+++ lib/scala/downloader/src/main/java/org/enso/downloader/http/MultipartBodyPublisher.java
@@ -12,37 +12,48 @@ import java.util.Map;
 import java.util.Random;
 
 /**
- * This utility class adds support for {@code multipart/form-data} content-type as there is no
- * such support in {@code java.net.http} JDK.
+ * This utility class adds support for {@code multipart/form-data} content-type as there is no such
+ * support in {@code java.net.http} JDK.
  *
- * Copied from <a href="https://stackoverflow.com/a/56482187/4816269">SO</a>.
+ * <p>Copied from <a href="https://stackoverflow.com/a/56482187/4816269">SO</a>.
  */
 public class MultipartBodyPublisher {
-  private static final String BOUNDARY =  new BigInteger(256, new Random()).toString();
+  private static final String BOUNDARY = new BigInteger(256, new Random()).toString();
 
-  public static HttpRequest.BodyPublisher ofMimeMultipartData(Map<Object, Object> data) throws IOException {
+  public static HttpRequest.BodyPublisher ofMimeMultipartData(Map<Object, Object> data)
+      throws IOException {
     // Result request body
     List<byte[]> byteArrays = new ArrayList<>();
 
     // Separator with boundary
-    byte[] separator = ("--" + BOUNDARY + "\r\nContent-Disposition: form-data; name=").getBytes(
-        StandardCharsets.UTF_8);
+    byte[] separator =
+        ("--" + BOUNDARY + "\r\nContent-Disposition: form-data; name=")
+            .getBytes(StandardCharsets.UTF_8);
 
     for (Map.Entry<Object, Object> entry : data.entrySet()) {
 
       // Opening boundary
       byteArrays.add(separator);
 
-      // If value is type of Path (file) append content type with file name and file binaries, otherwise simply append key=value
+      // If value is type of Path (file) append content type with file name and file binaries,
+      // otherwise simply append key=value
       if (entry.getValue() instanceof Path path) {
         String mimeType = Files.probeContentType(path);
-        byteArrays.add(("\"" + entry.getKey() + "\"; filename=\"" + path.getFileName()
-            + "\"\r\nContent-Type: " + mimeType + "\r\n\r\n").getBytes(StandardCharsets.UTF_8));
+        byteArrays.add(
+            ("\""
+                    + entry.getKey()
+                    + "\"; filename=\""
+                    + path.getFileName()
+                    + "\"\r\nContent-Type: "
+                    + mimeType
+                    + "\r\n\r\n")
+                .getBytes(StandardCharsets.UTF_8));
         byteArrays.add(Files.readAllBytes(path));
         byteArrays.add("\r\n".getBytes(StandardCharsets.UTF_8));
       } else {
-        byteArrays.add(("\"" + entry.getKey() + "\"\r\n\r\n" + entry.getValue() + "\r\n")
-            .getBytes(StandardCharsets.UTF_8));
+        byteArrays.add(
+            ("\"" + entry.getKey() + "\"\r\n\r\n" + entry.getValue() + "\r\n")
+                .getBytes(StandardCharsets.UTF_8));
       }
     }
 
