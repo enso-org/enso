413bf8dc048d6d3e9f6e0149367fac6ede71ea06
somebody1234 <ehern.lee@gmail.com>
2024-02-12 21:16:55 +1000

    Fix file path for `.env` file

--- app/ide-desktop/lib/dashboard/env.ts
+++ app/ide-desktop/lib/dashboard/env.ts
@@ -1,5 +1,7 @@
 /** @file Read environment variables from a file. */
 import * as fs from 'node:fs/promises'
+import * as path from 'node:path'
+import * as url from 'node:url'
 
 // ===============
 // === loadEnv ===
@@ -8,8 +10,12 @@ import * as fs from 'node:fs/promises'
 /** Read environment variables from a file. */
 export async function loadEnv() {
   const environment = process.env.ENSO_CLOUD_ENV ?? null
-  const path = environment == null || environment === 'production' ? '.env' : `.${environment}.env`
-  const file = await fs.readFile(path, { encoding: 'utf-8' })
+  const fileName =
+    environment == null || environment === 'production' ? '.env' : `.${environment}.env`
+  const filePath = path.join(url.fileURLToPath(new URL('.', import.meta.url)), fileName)
+  const file = await fs.readFile(filePath, {
+    encoding: 'utf-8',
+  })
   const variables = Object.fromEntries(
     file.split('\n').flatMap(line => {
       if (/^\s*$|^.s*#/.test(line)) {
