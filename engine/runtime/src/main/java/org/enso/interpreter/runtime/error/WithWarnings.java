7441df7ae41635f00fbc889d35dd313586fa7bc3
Gregory Travis <greg.travis@enso.org>
2024-02-12 14:57:02 -0500

    make getWarningsNoCache private

--- engine/runtime/src/main/java/org/enso/interpreter/runtime/error/WithWarnings.java
+++ engine/runtime/src/main/java/org/enso/interpreter/runtime/error/WithWarnings.java
@@ -108,7 +108,7 @@ public final class WithWarnings implements EnsoObject {
 
   // Ignore the warnings cache in .value and re-fetch them using the WarningsLibrary.
   // This is only used for shouldWrap=true.
-  public Warning[] getWarningsNoCache(WarningsLibrary warningsLibrary) {
+  private Warning[] getWarningsNoCache(WarningsLibrary warningsLibrary) {
     if (warningsLibrary != null && warningsLibrary.hasWarnings(value)) {
       try {
         return warningsLibrary.getWarnings(value, null, true);
