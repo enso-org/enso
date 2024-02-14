134e1dcf548f4361e790b314d5982a21ef127604
Gregory Travis <greg.travis@enso.org>
2024-02-14 16:57:35 -0500

    wip

--- std-bits/base/src/main/java/org/enso/base/numeric/Decimal_Utils.java
+++ std-bits/base/src/main/java/org/enso/base/numeric/Decimal_Utils.java
@@ -6,6 +6,8 @@ import java.math.MathContext;
 
 /** Utils for the Enso Decmial type. */
 public class Decimal_Utils {
+  public static record ConversionResult(BigDecimal newValue, bool hasPrecisionLoss) {}
+
   public static BigDecimal fromEnsoInteger(Object o) {
     if (o instanceof Long l) {
       // According to the BigInteger Javadocs, valueOf is preferred "because it
@@ -18,18 +20,23 @@ public class Decimal_Utils {
     }
   }
 
-  public static BigDecimal fromEnsoInteger(Object o, MathContext mc) {
+  public static ConversionResult fromEnsoInteger(Object o, MathContext mc) {
     if (o instanceof Long l) {
-      return new BigDecimal(l, mc);
+      BigDecimal bd = new BigDecimal(l, mc);
+      long backToLong = bd.longValue();
+      System.out.println("AAA l " + o + " " + (l == backToLong) + " " + l + " " + backToLong + " " + (l-backToLong) );
+      return new ConversionResult(bd, l != backToLong);
     } else if (o instanceof BigInteger bi) {
-      return new BigDecimal(bi, mc);
+      BigDecimal bd = new BigDecimal(bi, mc);
+      BigInteger backToBigInteger = bd.toBigInteger();
+      System.out.println("AAA bi " + o + " " + (bi.compareTo(backToBigInteger) == 0) + " " + bi + " " + backToBigInteger + " " + (bi.subtract(backToBigInteger)));
+      return new ConversionResult(bd, bi.compareTo(backToBigInteger) != 0);
     } else {
       throw new IllegalArgumentException("Input must be Long or BigInteger");
     }
   }
 
   public static BigDecimal fromEnsoFloat(Double d) {
-    System.out.println("AAA " + d + " " + d.getClass());
     // According to the BigInteger Javadocs, valueOf is preferred because "the
     // value returned is equal to that resulting from constructing a BigDecimal
     // from the result of using Double.toString(double)."
@@ -37,7 +44,9 @@ public class Decimal_Utils {
   }
 
   public static BigDecimal fromEnsoFloat(Double d, MathContext mc) {
-    System.out.println("AAAmc " + d + " " + d.getClass() + " " + mc);
-    return new BigDecimal(d, mc);
+    BigDecimal bd = new BigDecimal(d, mc);
+    double backToDouble = bd.doubleValue();
+    System.out.println("AAA d " + d + " " + (d == backToDouble) + " " + d + " " + backToDouble + " " + (d-backToDouble));
+    return new ConversionResult(bd, d != backToDouble);
   }
 }
