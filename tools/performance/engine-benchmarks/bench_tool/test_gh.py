94db4796555eec3637ef46ac88a70ff685af2db8
Pavel Marek <pavel.marek@enso.org>
2024-02-16 17:42:43 +0100

    Add test case to test_gh

--- tools/performance/engine-benchmarks/bench_tool/test_gh.py
+++ tools/performance/engine-benchmarks/bench_tool/test_gh.py
@@ -1,20 +1,24 @@
 import unittest
 
 from bench_tool import ENSO_REPO
-from .gh import ensure_gh_installed, fetch_file, invoke_gh_api
+from . import gh
 
 
 # Create a unit test
 class TestGH(unittest.IsolatedAsyncioTestCase):
     async def test_ensure_gh_installed(self):
-        self.assertIsNone(ensure_gh_installed())
+        self.assertIsNone(gh.ensure_gh_installed())
 
     async def test_file_fetch(self):
-        content = await fetch_file(ENSO_REPO, "README.md")
+        content = await gh.fetch_file(ENSO_REPO, "README.md")
         self.assertIsNotNone(content)
         self.assertIsInstance(content, str)
         self.assertGreater(len(content), 0)
 
+    async def test_fetch_non_existing_file(self):
+        content = await gh.fetch_file(ENSO_REPO, "non_existing_file")
+        self.assertIsNone(content)
+
     async def test_wrong_gh_query_should_not_fail(self):
-        res = await invoke_gh_api("non_existing_repo", "/non_existing_endpoint")
+        res = await gh.invoke_gh_api("non_existing_repo", "/non_existing_endpoint")
         self.assertIsNone(res)
