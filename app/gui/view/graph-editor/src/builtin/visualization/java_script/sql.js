b6b51f5faeb128b33c9de463099fc291d1e37451
Adam Riley <riley.ad@gmail.com>
2024-02-19 18:07:50 +0000

    Remove TODO

--- app/gui/view/graph-editor/src/builtin/visualization/java_script/sql.js
+++ app/gui/view/graph-editor/src/builtin/visualization/java_script/sql.js
@@ -80,8 +80,6 @@ loadScript('https://cdnjs.cloudflare.com/ajax/libs/sql-formatter/4.0.2/sql-forma
  * interpolated query parameters.
  */
 class SqlVisualization extends Visualization {
-    // TODO Change the type below once #837 is done:
-    // 'Standard.Database.Data.DB_Table.DB_Table | Standard.Database.Data.DB_Column.DB_Column'
     static inputType =
         'Standard.Database.Data.DB_Table.DB_Table | Standard.Database.Data.DB_Column.DB_Column'
     static label = 'SQL Query'
