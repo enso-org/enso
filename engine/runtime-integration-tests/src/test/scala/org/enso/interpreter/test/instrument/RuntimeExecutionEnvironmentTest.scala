62c138543b40dc722a683982698c0c9da5d16494
Radosław Waśko <radoslaw.wasko@enso.org>
2024-02-19 10:03:18 +0100

    update tests

--- engine/runtime-integration-tests/src/test/scala/org/enso/interpreter/test/instrument/RuntimeExecutionEnvironmentTest.scala
+++ engine/runtime-integration-tests/src/test/scala/org/enso/interpreter/test/instrument/RuntimeExecutionEnvironmentTest.scala
@@ -184,7 +184,10 @@ class RuntimeExecutionEnvironmentTest
         idRes,
         IF_ENABLED_METH_CALL,
         Api.ExpressionUpdate.Payload
-          .Panic("Forbidden operation: Output.", Seq(idRes)),
+          .Panic(
+            "Forbidden operation: The Output context is disabled.",
+            Seq(idRes)
+          ),
         false
       ),
       context.executionComplete(contextId)
@@ -285,7 +288,10 @@ class RuntimeExecutionEnvironmentTest
         idRes,
         IF_ENABLED_METH_CALL,
         Api.ExpressionUpdate.Payload
-          .Panic("Forbidden operation: Input.", Seq(idRes)),
+          .Panic(
+            "Forbidden operation: The Input context is disabled..",
+            Seq(idRes)
+          ),
         false
       ),
       context.executionComplete(contextId)
