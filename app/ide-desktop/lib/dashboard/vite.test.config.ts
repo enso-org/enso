3ee0b3b0fd9662d8ee8c6258379ece9896def50d
somebody1234 <ehern.lee@gmail.com>
2024-02-22 20:41:58 +1000

    Add missing environment variables to E2E build

--- app/ide-desktop/lib/dashboard/vite.test.config.ts
+++ app/ide-desktop/lib/dashboard/vite.test.config.ts
@@ -9,6 +9,8 @@ import config from './vite.config'
 
 /* eslint-disable @typescript-eslint/naming-convention */
 
+const SERVER_PORT = 8080
+
 export default vite.defineConfig(env =>
   vite.mergeConfig(
     config(env),
@@ -30,6 +32,19 @@ export default vite.defineConfig(env =>
           '.tsx',
           '.json',
         ],
+      },
+      define: {
+        'process.env.NODE_ENV': JSON.stringify('production'),
+        'process.env.ENSO_CLOUD_REDIRECT': JSON.stringify(`http://localhost:${SERVER_PORT}`),
+        'process.env.ENSO_CLOUD_ENVIRONMENT': JSON.stringify('production'),
+        'process.env.ENSO_CLOUD_API_URL': JSON.stringify(''),
+        'process.env.ENSO_CLOUD_SENTRY_DSN': JSON.stringify(''),
+        'process.env.ENSO_CLOUD_STRIPE_KEY': JSON.stringify(''),
+        'process.env.ENSO_CLOUD_CHAT_URL': JSON.stringify(''),
+        'process.env.ENSO_CLOUD_AMPLIFY_USER_POOL_ID': JSON.stringify(''),
+        'process.env.ENSO_CLOUD_AMPLIFY_USER_POOL_WEB_CLIENT_ID': JSON.stringify(''),
+        'process.env.ENSO_CLOUD_AMPLIFY_DOMAIN': JSON.stringify(''),
+        'process.env.ENSO_CLOUD_AMPLIFY_REGION': JSON.stringify(''),
       },
     })
   )
