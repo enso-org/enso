ccf3e75bcc02d953bb34ccf656102c2531b5542d
Radosław Waśko <radoslaw.wasko@enso.org>
2024-02-26 17:06:41 +0100

    try actual test

--- app/ide-desktop/lib/dashboard/src/data/__tests__/dataLinkSchema.test.ts
+++ app/ide-desktop/lib/dashboard/src/data/__tests__/dataLinkSchema.test.ts
@@ -1,9 +1,32 @@
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+
 import * as v from 'vitest'
 
 import SCHEMA from '#/data/dataLinkSchema.json' assert { type: 'json' }
+
 import * as jsonSchema from '#/utilities/jsonSchema'
 
-v.test('adds 1 + 2 to equal 3', () => {
-    // jsonSchema.isMatch()
-    v.expect(1+2).toBe(4)
+const DEFS = SCHEMA.$defs
+const DATALINK_SCHEMA = DEFS.DataLink
+
+v.test('correctly rejects invalid values as not matching the schema', () => {
+  v.expect(jsonSchema.isMatch(DEFS, DATALINK_SCHEMA, {})).toBe(false)
+  v.expect(jsonSchema.isMatch(DEFS, DATALINK_SCHEMA, 'foobar')).toBe(false)
+  v.expect(jsonSchema.isMatch(DEFS, DATALINK_SCHEMA, { foo: 'BAR' })).toBe(false)
+})
+
+function loadDataLinkFile(path: string): object {
+  const text: string = fs.readFileSync(path, { encoding: 'utf-8' })
+  return JSON.parse(text)
+}
+
+// We need to go up from `app/ide-desktop/lib/dashboard/` to the root of the repo
+const repoRoot = '../../../../'
+v.test('correctly validates example S3 .datalink files with the schema', () => {
+  const s3datalinksRoot = path.resolve(repoRoot, 'test/AWS_Tests/data/')
+
+  const simple = loadDataLinkFile(path.resolve(s3datalinksRoot, 'simple.datalink'))
+  console.log(simple)
+  v.expect(jsonSchema.isMatch(DEFS, DATALINK_SCHEMA, simple)).toBe(true)
 })
