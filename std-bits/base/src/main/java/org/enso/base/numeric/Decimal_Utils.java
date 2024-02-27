b14aa95d975813ed3d6dac834a39f2db7e293cdf
Gregory Travis <greg.travis@enso.org>
2024-02-27 12:17:30 -0500

    wip

--- std-bits/base/src/main/java/org/enso/base/numeric/Decimal_Utils.java
+++ std-bits/base/src/main/java/org/enso/base/numeric/Decimal_Utils.java
@@ -1,14 +1,9 @@
 package org.enso.base.numeric;
 
-import java.lang.ArithmeticException;
-import java.math.BigInteger;
-import java.math.BigDecimal;
-import java.math.MathContext;
-
 /** Utils for the Enso Decmial type. */
 public class Decimal_Utils {
   public static Double floatId(Double d) {
-    System.out.println("Decimal_Utils.floatId " + d);
+    // System.out.println("Decimal_Utils.floatId " + d);
     return d;
   }
   }
