9a3215c7223c712cb4f6af914e59ede0d38f4655
Gregory Travis <greg.travis@enso.org>
2024-02-29 15:30:45 -0500

    unused

--- engine/polyglot-api/src/main/java/org/enso/polyglot/HostAccessFactory.java
+++ engine/polyglot-api/src/main/java/org/enso/polyglot/HostAccessFactory.java
@@ -4,11 +4,6 @@ import org.graalvm.polyglot.HostAccess;
 
 /** Utility class for creating HostAccess object. */
 public class HostAccessFactory {
-
-  // Copied from com.oracle.truffle.api.interop.NumberUtils
-  // since the latter is inaccessible
-  private static final long LONG_MAX_SAFE_DOUBLE = 9007199254740991L; // 2 ** 53 - 1
-
   public HostAccess allWithTypeMapping() {
     return HostAccess.newBuilder()
         .allowPublicAccess(true)
