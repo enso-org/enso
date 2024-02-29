6cb11b241b5bd51aa5b906836320f1a595f29cf4
Dmitry Bushev <bushevdv@gmail.com>
2024-02-29 12:50:52 +0000

    test: FileSystemServiceSuite

---
+++ lib/scala/project-manager/src/test/scala/org/enso/projectmanager/service/filesystem/FileSystemServiceSpec.scala
@@ -0,0 +1,89 @@
+package org.enso.projectmanager.service.filesystem
+
+import org.enso.projectmanager.{BaseServerSpec, ProjectManagementOps}
+import org.enso.projectmanager.control.effect.Effects
+import org.enso.projectmanager.data.ProjectMetadata
+import org.enso.projectmanager.infrastructure.repository.{
+  MetadataFileStorage,
+  ProjectFileRepositoryFactory
+}
+import org.enso.runtimeversionmanager.test.OverrideTestVersionSuite
+import org.enso.semver.SemVer
+import org.scalatest.EitherValues
+import zio.{ZAny, ZIO}
+
+import java.io.File
+import java.nio.file.Files
+
+class FileSystemServiceSpec
+    extends BaseServerSpec
+    with ProjectManagementOps
+    with OverrideTestVersionSuite
+    with Effects
+    with EitherValues {
+
+  override val testVersion: SemVer = SemVer.of(0, 0, 1)
+
+  override val engineToInstall: Option[SemVer] = Some(SemVer.of(0, 0, 1))
+
+  lazy val fileSystemService: FileSystemService[ZIO[ZAny, +*, +*]] = {
+    val projectRepositoryFactory =
+      new ProjectFileRepositoryFactory[ZIO[ZAny, +*, +*]](
+        config.storage,
+        testClock,
+        fileSystem,
+        gen
+      )
+    new FileSystemService[ZIO[ZAny, +*, +*]](
+      fileSystem,
+      projectRepositoryFactory
+    )
+  }
+
+  def metadataFileStorage(directory: File) =
+    new MetadataFileStorage[ZIO[ZAny, +*, +*]](
+      directory,
+      config.storage,
+      testClock,
+      fileSystem,
+      gen
+    )
+
+  "FileSystemService" should {
+
+    "list file system entries" in {
+      implicit val client: WsTestClient = new WsTestClient(address)
+
+      val testDir = testStorageConfig.userProjectsPath
+
+      val projectName = "New_Project_1"
+      val projectId   = createProject(projectName)
+      val projectPath = new File(testDir, projectName)
+
+      val testFile = new File(testDir, "foo.txt")
+
+      Files.createFile(testFile.toPath)
+
+      val result = fileSystemService
+        .list(testDir)
+        .unsafeRunSync()
+
+      result.value should contain theSameElementsAs Seq(
+        FileSystemEntry.ProjectEntry(
+          projectPath,
+          ProjectMetadata(
+            projectName,
+            "local",
+            projectId,
+            testClock.currentTime,
+            None
+          )
+        ),
+        FileSystemEntry.FileEntry(testFile)
+      )
+
+      // cleanup
+      deleteProject(projectId)
+    }
+  }
+}
