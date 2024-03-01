8a6ba9c3f5de830a226ecc1674b84399cd51ff28
Dmitry Bushev <bushevdv@gmail.com>
2024-03-01 16:45:12 +0000

    misc: add docs

--- lib/scala/project-manager/src/main/scala/org/enso/projectmanager/service/filesystem/FileSystemEntry.scala
+++ lib/scala/project-manager/src/main/scala/org/enso/projectmanager/service/filesystem/FileSystemEntry.scala
@@ -4,17 +4,32 @@ import org.enso.projectmanager.data.ProjectMetadata
 
 import java.io.File
 
+/** Base trait for the file system entries. */
 sealed trait FileSystemEntry {
 
+  /** A path to the file system entry. */
   def path: File
 }
 
 object FileSystemEntry {
 
+  /** A file.
+    *
+    * @param path the path to the file
+    */
   case class FileEntry(path: File) extends FileSystemEntry
 
+  /** A directory.
+    *
+    * @param path the path to the directory
+    */
   case class DirectoryEntry(path: File) extends FileSystemEntry
 
+  /** A directory containing a project.
+    *
+    * @param path the path to the directory
+    * @param metadata the project metadata
+    */
   case class ProjectEntry(path: File, metadata: ProjectMetadata)
       extends FileSystemEntry
 }
