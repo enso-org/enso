96bfcb3e63aaaff7dfe69dc48325f4cefb547b48
Pavel Marek <pavel.marek@enso.org>
2024-03-01 14:37:18 +0100

    Add compiler benchmarks (#9158)
    Add compiler benchmarks to `engine/runtime-benchmarks`. All the benchmarks generate source code on the fly into `engine/runtime-benchmarks/target/bench-data` directory. Random data generators are set with the same seed. For the convenience of reviewers, I am attaching the benchmark sources in [bench-data.zip](https://github.com/enso-org/enso/files/14423372/bench-data.zip).
    
    I have created benchmarks that measure the performance of a whole module compilation, and benchmarks that measure the performance of inline compilation. They directly call `run` and `runInline` methods on `org.enso.compiler.Compiler`.
    
    # Important Notes
    - The results will be available in https://enso-org.github.io/engine-benchmark-results/engine-benchs.html in a few days after the merge of this PR.
    - The benchmark parameters are tweaked so that an average iteration takes less than 400 ms and more than 30 ms.
    - Ensured that the benchmarks measure performance of the compiler, for example:
    ![image](https://github.com/enso-org/enso/assets/14013887/c870f4ad-1418-4812-85f2-ca9664711163)

---
+++ engine/runtime-benchmarks/src/main/java/org/enso/compiler/benchmarks/inline/InlineSource.java
@@ -0,0 +1,11 @@
+package org.enso.compiler.benchmarks.inline;
+
+import java.util.Set;
+import org.enso.compiler.context.InlineContext;
+
+record InlineSource(
+    String source,
+    // InlineContext for the main method
+    InlineContext mainInlineContext,
+    // Local variables in main method
+    Set<String> localVarNames) {}
