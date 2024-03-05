52ab7297330c1724cd17448dc1e89752e7731436
Gregory Travis <greg.travis@enso.org>
2024-03-05 15:17:31 -0500

    cleanup

--- std-bits/base/src/main/java/org/enso/base/numeric/Conversion_Result.java
+++ std-bits/base/src/main/java/org/enso/base/numeric/Conversion_Result.java
@@ -2,4 +2,5 @@ package org.enso.base.numeric;
 
 import java.math.BigDecimal;
 
+// Wraps the result of a conversion to BigDecimal with a precision loss flag.
 public record Conversion_Result(BigDecimal newValue, boolean hasPrecisionLoss) {}
