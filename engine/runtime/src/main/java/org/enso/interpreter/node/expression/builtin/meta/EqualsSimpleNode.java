enso-org/enso

commit a664dd9d56226bab96290a186f3a053ec0af4497

Equality with conversions (#9070)

engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/EqualsSimpleNode.java
@@ -1,0 +1,386 @@
+package org.enso.interpreter.node.expression.builtin.meta;
+
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.Cached.Shared;
+import com.oracle.truffle.api.dsl.GenerateUncached;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
+import com.oracle.truffle.api.library.CachedLibrary;
+import com.oracle.truffle.api.nodes.Node;
+import java.math.BigInteger;
+import org.enso.interpreter.node.expression.builtin.number.utils.BigIntegerOps;
+import org.enso.interpreter.runtime.EnsoContext;
+import org.enso.interpreter.runtime.data.EnsoMultiValue;
+import org.enso.interpreter.runtime.data.atom.Atom;
+import org.enso.interpreter.runtime.data.atom.AtomConstructor;
+import org.enso.interpreter.runtime.data.text.Text;
+import org.enso.interpreter.runtime.error.WarningsLibrary;
+import org.enso.interpreter.runtime.number.EnsoBigInteger;
+import org.enso.polyglot.common_utils.Core_Text_Utils;
+
+@GenerateUncached
+public abstract class EqualsSimpleNode extends Node {
+
+  public static EqualsSimpleNode build() {
+    return EqualsSimpleNodeGen.create();
+  }
+
+  public abstract boolean execute(VirtualFrame frame, Object self, Object right);
+
+  @Specialization
+  boolean equalsBoolBool(boolean self, boolean other) {
+    return self == other;
+  }
+
+  @Specialization
+  boolean equalsBoolDouble(boolean self, double other) {
+    return false;
+  }
+
+  @Specialization
+  boolean equalsBoolLong(boolean self, long other) {
+    return false;
+  }
+
+  @Specialization
+  boolean equalsBoolBigInt(boolean self, EnsoBigInteger other) {
+    return false;
+  }
+
+  @Specialization
+  boolean equalsBoolText(boolean self, Text other) {
+    return false;
+  }
+
+  @Specialization
+  boolean equalsBoolInterop(
+      boolean self,
+      Object other,
+      @Shared("interop") @CachedLibrary(limit = "10") InteropLibrary interop) {
+    try {
+      return self == interop.asBoolean(other);
+    } catch (UnsupportedMessageException ex) {
+      return false;
+    }
+  }
+
+  @Specialization
+  boolean equalsInteropBool(
+      TruffleObject self,
+      boolean other,
+      @Shared("interop") @CachedLibrary(limit = "10") InteropLibrary interop) {
+    try {
+      return other == interop.asBoolean(self);
+    } catch (UnsupportedMessageException ex) {
+      return false;
+    }
+  }
+
+  @Specialization
+  boolean equalsLongLong(long self, long other) {
+    return self == other;
+  }
+
+  @Specialization
+  boolean equalsLongBool(long self, boolean other) {
+    return false;
+  }
+
+  @Specialization
+  boolean equalsLongDouble(long self, double other) {
+    return (double) self == other;
+  }
+
+  @Specialization
+  boolean equalsLongText(long self, Text other) {
+    return false;
+  }
+
+  @Specialization
+  @TruffleBoundary
+  boolean equalsLongBigInt(long self, EnsoBigInteger other) {
+    if (BigIntegerOps.fitsInLong(other.getValue())) {
+      return BigInteger.valueOf(self).compareTo(other.getValue()) == 0;
+    } else {
+      return false;
+    }
+  }
+
+  @Specialization
+  boolean equalsLongInterop(
+      long self,
+      Object other,
+      @Shared("interop") @CachedLibrary(limit = "10") InteropLibrary interop) {
+    try {
+      return self == interop.asLong(other);
+    } catch (UnsupportedMessageException ex) {
+      return false;
+    }
+  }
+
+  @Specialization
+  boolean equalsDoubleDouble(double self, double other) {
+    if (Double.isNaN(self) || Double.isNaN(other)) {
+      return false;
+    } else {
+      return self == other;
+    }
+  }
+
+  @Specialization
+  boolean equalsDoubleLong(double self, long other) {
+    return self == (double) other;
+  }
+
+  @Specialization
+  boolean equalsDoubleBool(double self, boolean other) {
+    return false;
+  }
+
+  @Specialization
+  boolean equalsDoubleText(double self, Text other) {
+    return false;
+  }
+
+  @Specialization
+  boolean equalsDoubleInterop(
+      double self,
+      Object other,
+      @Shared("interop") @CachedLibrary(limit = "10") InteropLibrary interop) {
+    try {
+      if (interop.fitsInDouble(other)) {
+        return self == interop.asDouble(other);
+      }
+      var otherBig = interop.asBigInteger(other);
+      return self == asDouble(otherBig);
+    } catch (UnsupportedMessageException ex) {
+      return false;
+    }
+  }
+
+  @TruffleBoundary
+  private static double asDouble(BigInteger big) {
+    return big.doubleValue();
+  }
+
+  @Specialization(guards = {"isBigInteger(iop, self)", "isBigInteger(iop, other)"})
+  @TruffleBoundary
+  boolean other(
+      Object self,
+      Object other,
+      @Shared("interop") @CachedLibrary(limit = "10") InteropLibrary iop) {
+    return asBigInteger(iop, self).equals(asBigInteger(iop, other));
+  }
+
+  @Specialization(guards = "isBigInteger(iop, self)")
+  @TruffleBoundary
+  boolean equalsBigIntDouble(
+      Object self,
+      double other,
+      @Shared("interop") @CachedLibrary(limit = "10") InteropLibrary iop) {
+    return asBigInteger(iop, self).doubleValue() == other;
+  }
+
+  @Specialization(guards = "isBigInteger(iop, self)")
+  @TruffleBoundary
+  boolean equalsBigIntLong(
+      Object self, long other, @Shared("interop") @CachedLibrary(limit = "10") InteropLibrary iop) {
+    var v = asBigInteger(iop, self);
+    if (BigIntegerOps.fitsInLong(v)) {
+      return v.compareTo(BigInteger.valueOf(other)) == 0;
+    } else {
+      return false;
+    }
+  }
+
+  @Specialization
+  boolean equalsBigIntText(EnsoBigInteger self, Text other) {
+    return false;
+  }
+
+  @TruffleBoundary
+  @Specialization(guards = {"isBigInteger(iop, self)", "!isPrimitiveValue(other)"})
+  boolean equalsBigIntInterop(
+      Object self,
+      Object other,
+      @Shared("interop") @CachedLibrary(limit = "10") InteropLibrary iop) {
+    try {
+      var otherBigInteger = InteropLibrary.getUncached().asBigInteger(other);
+      return asBigInteger(iop, self).equals(otherBigInteger);
+    } catch (UnsupportedMessageException ex) {
+      return false;
+    }
+  }
+
+  @Specialization(guards = {"selfText.is_normalized()", "otherText.is_normalized()"})
+  boolean equalsTextText(Text selfText, Text otherText) {
+    return selfText.toString().compareTo(otherText.toString()) == 0;
+  }
+
+  @Specialization
+  boolean equalsTextLong(Text selfText, long otherLong) {
+    return false;
+  }
+
+  @Specialization
+  boolean equalsTextDouble(Text selfText, double otherDouble) {
+    return false;
+  }
+
+  @Specialization
+  boolean equalsTextBigInt(Text self, EnsoBigInteger other) {
+    return false;
+  }
+
+  /**
+   * Compares interop string with other object. If the other object doesn't support conversion to
+   * String, it is not equal. Otherwise the two strings are compared according to the
+   * lexicographical order, handling Unicode normalization. See {@code Text_Utils.compare_to}.
+   */
+  @TruffleBoundary
+  @Specialization(
+      guards = {"selfInterop.isString(selfString)"},
+      limit = "3")
+  boolean equalsStrings(
+      Object selfString,
+      Object otherString,
+      @CachedLibrary("selfString") InteropLibrary selfInterop,
+      @CachedLibrary("otherString") InteropLibrary otherInterop) {
+    String selfJavaString;
+    String otherJavaString;
+    try {
+      selfJavaString = selfInterop.asString(selfString);
+      otherJavaString = otherInterop.asString(otherString);
+    } catch (UnsupportedMessageException e) {
+      return false;
+    }
+    return Core_Text_Utils.equals(selfJavaString, otherJavaString);
+  }
+
+  @Specialization(guards = "isPrimitive(self, interop) != isPrimitive(other, interop)")
+  boolean equalsDifferent(
+      Object self,
+      Object other,
+      @Shared("interop") @CachedLibrary(limit = "10") InteropLibrary interop) {
+    return false;
+  }
+
+  /** Equals for Atoms and AtomConstructors */
+  @Specialization
+  boolean equalsAtomConstructors(AtomConstructor self, AtomConstructor other) {
+    return self == other;
+  }
+
+  @Specialization
+  boolean equalsAtoms(
+      VirtualFrame frame,
+      Atom self,
+      Atom other,
+      @Cached EqualsAtomNode equalsAtomNode,
+      @Shared("isSameObjectNode") @Cached IsSameObjectNode isSameObjectNode) {
+    return isSameObjectNode.execute(self, other) || equalsAtomNode.execute(frame, self, other);
+  }
+
+  @Specialization
+  boolean equalsReverseLong(
+      VirtualFrame frame,
+      TruffleObject self,
+      long other,
+      @Shared("interop") @CachedLibrary(limit = "10") InteropLibrary interop,
+      @Shared("reverse") @Cached EqualsSimpleNode reverse) {
+    return reverse.execute(frame, other, self);
+  }
+
+  @Specialization
+  boolean equalsReverseDouble(
+      VirtualFrame frame,
+      TruffleObject self,
+      double other,
+      @Shared("interop") @CachedLibrary(limit = "10") InteropLibrary interop,
+      @Shared("reverse") @Cached EqualsSimpleNode reverse) {
+    return reverse.execute(frame, other, self);
+  }
+
+  @Specialization
+  boolean equalsReverseBigInt(
+      VirtualFrame frame,
+      TruffleObject self,
+      EnsoBigInteger other,
+      @Shared("interop") @CachedLibrary(limit = "10") InteropLibrary interop,
+      @Shared("reverse") @Cached EqualsSimpleNode reverse) {
+    return reverse.execute(frame, other, self);
+  }
+
+  @Specialization(guards = "isNotPrimitive(self, other, interop, warnings)")
+  boolean equalsComplex(
+      VirtualFrame frame,
+      Object self,
+      Object other,
+      @Cached EqualsComplexNode equalsComplex,
+      @Shared("isSameObjectNode") @Cached IsSameObjectNode isSameObjectNode,
+      @Shared("interop") @CachedLibrary(limit = "10") InteropLibrary interop,
+      @CachedLibrary(limit = "5") WarningsLibrary warnings) {
+    return isSameObjectNode.execute(self, other) || equalsComplex.execute(frame, self, other);
+  }
+
+  static boolean isNotPrimitive(
+      Object a, Object b, InteropLibrary interop, WarningsLibrary warnings) {
+    if (a instanceof AtomConstructor && b instanceof AtomConstructor) {
+      return false;
+    }
+    if (a instanceof Atom && b instanceof Atom) {
+      return false;
+    }
+    if (warnings.hasWarnings(a) || warnings.hasWarnings(b)) {
+      return true;
+    }
+    if (a instanceof EnsoMultiValue || b instanceof EnsoMultiValue) {
+      return true;
+    }
+    return !isPrimitive(a, interop) && !isPrimitive(b, interop);
+  }
+
+  /**
+   * Return true iff object is a primitive value used in some specializations guard. By primitive
+   * value we mean any value that can be present in Enso, so, for example, not Integer, as that
+   * cannot be present in Enso. All the primitive types should be handled in their corresponding
+   * specializations. See {@link
+   * org.enso.interpreter.node.expression.builtin.interop.syntax.HostValueToEnsoNode}.
+   */
+  static boolean isPrimitive(Object object, InteropLibrary interop) {
+    return isPrimitiveValue(object)
+        || object instanceof EnsoBigInteger
+        || object instanceof Text
+        || interop.isString(object)
+        || interop.isNumber(object)
+        || interop.isBoolean(object);
+  }
+
+  static boolean isPrimitiveValue(Object object) {
+    return object instanceof Boolean || object instanceof Long || object instanceof Double;
+  }
+
+  static boolean isBigInteger(InteropLibrary iop, Object v) {
+    if (v instanceof EnsoBigInteger) {
+      return true;
+    } else {
+      return !iop.fitsInDouble(v) && !iop.fitsInLong(v) && iop.fitsInBigInteger(v);
+    }
+  }
+
+  BigInteger asBigInteger(InteropLibrary iop, Object v) {
+    if (v instanceof EnsoBigInteger big) {
+      return big.getValue();
+    } else {
+      try {
+        return iop.asBigInteger(v);
+      } catch (UnsupportedMessageException ex) {
+        throw EnsoContext.get(this).raiseAssertionPanic(this, "Expecting BigInteger", ex);
+      }
+    }
+  }
+}

distribution/lib/Standard/Test/0.0.0-dev/src/Extensions.enso
@@ -296,7 +296,7 @@ Number.should_equal : Float -> Float -> Integer -> Spec_Result
 Number.should_equal self that epsilon=0 frames_to_skip=0 =
     matches = case that of
         _ : Number -> self.equals that epsilon
-        _ -> False
+        _ -> self==that
     case matches of
         True -> Spec_Result.Success
         False ->
@@ -519,7 +519,7 @@ Error.should_contain_the_same_elements_as self that frames_to_skip=0 =
 
    It checks that all elements from `self` are also present in `that`. It does
    not require that all elements of `that` are contained in `self`. Arities of
-   elements are not checked, so `self` may still contain more elements than 
+   elements are not checked, so `self` may still contain more elements than
    `that` by containing duplicates.
 
    It will work on any collection which supports the methods

engine/runtime-benchmarks/src/main/java/org/enso/interpreter/bench/benchmarks/semantic/EqualsBenchmarks.java
@@ -75,6 +75,21 @@ public class EqualsBenchmarks {
         new StringBuilder(
             """
         import Standard.Base.Data.Range.Extensions
+        import Standard.Base.Data.Numbers.Number
+        import Standard.Base.Data.Ordering.Ordering
+        import Standard.Base.Data.Ordering.Comparable
+        import Standard.Base.Data.Ordering.Default_Comparator
+
+        type Num
+            Value n
+
+        Num.from (that:Number) = Num.Value that
+        Comparable.from (_:Number) = Num_Comparator
+        Comparable.from (_:Num) = Num_Comparator
+
+        type Num_Comparator
+            compare x:Num y:Num = Ordering.compare x.n y.n
+            hash x:Num = Default_Comparator.hash x.n
 
         type Node
             C1 f1
@@ -87,7 +102,9 @@ public class EqualsBenchmarks {
 
         eq_vec vec1 vec2 =
             (0.up_to vec1.length).map idx->
-                (vec1.at idx) == (vec2.at idx)
+                v1 = vec1.at idx
+                v2 = vec2.at idx
+                v1 == v2
 
         eq x y = x == y
         """);
@@ -108,10 +125,37 @@ public class EqualsBenchmarks {
                 primitiveVectorSize / 64);
         codeBuilder
             .append(
-                generateVectorOfPrimitives(primitiveVectorSize, "vec1", 42, trueExpectedAt, random))
+                generateVectorOfPrimitives(
+                    primitiveVectorSize, "vec1", 42, trueExpectedAt, random, "%d", "%f"))
             .append("\n")
             .append(
-                generateVectorOfPrimitives(primitiveVectorSize, "vec2", 42, trueExpectedAt, random))
+                generateVectorOfPrimitives(
+                    primitiveVectorSize, "vec2", 42, trueExpectedAt, random, "%d", "%f"))
+            .append("\n");
+      }
+      case "equalsWithConversion" -> {
+        trueExpectedAt =
+            Set.of(
+                primitiveVectorSize / 2,
+                primitiveVectorSize / 4,
+                primitiveVectorSize / 8,
+                primitiveVectorSize / 16,
+                primitiveVectorSize / 32,
+                primitiveVectorSize / 64);
+        codeBuilder
+            .append(
+                generateVectorOfPrimitives(
+                    primitiveVectorSize, "vec1", 42, trueExpectedAt, random, "%d", "%f"))
+            .append("\n")
+            .append(
+                generateVectorOfPrimitives(
+                    primitiveVectorSize,
+                    "vec2",
+                    42,
+                    trueExpectedAt,
+                    random,
+                    "Num.Value %d",
+                    "Num.Value %f"))
             .append("\n");
       }
       case "equalsStrings" -> {
@@ -141,7 +185,7 @@ public class EqualsBenchmarks {
     }
 
     codeBuilder.append("""
-        bench x = eq_vec vec1 vec2
+        bench _ = eq_vec vec1 vec2
         """);
 
     module = ctx.eval(SrcUtil.source(benchmarkName, codeBuilder.toString()));
@@ -168,6 +212,11 @@ public class EqualsBenchmarks {
    */
   @Benchmark
   public void equalsPrimitives(Blackhole blackHole) {
+    performBenchmark(blackHole);
+  }
+
+  @Benchmark
+  public void equalsWithConversion(Blackhole blackHole) {
     performBenchmark(blackHole);
   }
 
@@ -207,7 +256,9 @@ public class EqualsBenchmarks {
       String vecName,
       Object identityElem,
       Collection<Integer> constantIdxs,
-      Random random) {
+      Random random,
+      String intFormat,
+      String doubleFormat) {
     var partSize = totalSize / 2;
     List<Object> primitiveValues = new ArrayList<>();
     random.ints(partSize).forEach(primitiveValues::add);
@@ -221,9 +272,9 @@ public class EqualsBenchmarks {
     sb.append(vecName).append(" = [");
     for (Object primitiveValue : primitiveValues) {
       if (primitiveValue instanceof Double dbl) {
-        sb.append(String.format("%f", dbl)).append(",");
+        sb.append(String.format(doubleFormat, dbl)).append(",");
       } else {
-        sb.append(primitiveValue).append(",");
+        sb.append(String.format(intFormat, primitiveValue)).append(",");
       }
     }
     // Replace last comma

engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/EqualsConversionsTest.java
@@ -1,0 +1,174 @@
+package org.enso.interpreter.test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.PolyglotException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class EqualsConversionsTest extends TestBase {
+  private static Context context;
+
+  @BeforeClass
+  public static void initContextAndData() {
+    context = createDefaultContext();
+  }
+
+  @AfterClass
+  public static void disposeContext() {
+    context.close();
+  }
+
+  @Test
+  public void testBasicInequalities() {
+    var results =
+        TestBase.evalModule(
+                context,
+                """
+    from Standard.Base import all
+
+    Text.from (that:Number) = that.to_text
+
+    main =
+        r0 = "4"+"2" == "42"
+        r1 = 42 == "42"
+        r2 = "42" == 42
+        [ r0, r1, r2 ]
+    """)
+            .as(List.class);
+
+    assertTrue("strings are equal: " + results, (boolean) results.get(0));
+    assertFalse("string is not equal to number: " + results, (boolean) results.get(1));
+    assertFalse("number is not equal to string: " + results, (boolean) results.get(2));
+  }
+
+  @Test
+  public void testNumWrapperAroundIntegerIsEqualToInteger() {
+    var gen = new DefineComparableWrapper();
+    gen.intNumConversion = true;
+    gen.intComparator = true;
+    gen.numComparator = true;
+    assertTrue("Num.Value equal to Integer: ", gen.evaluate());
+  }
+
+  @Test
+  public void testMissingIntegerNumConversion() {
+    var gen = new DefineComparableWrapper();
+    gen.intNumConversion = false;
+    gen.intComparator = true;
+    gen.numComparator = true;
+    assertFalse("Num.Value not equal to Integer: ", gen.evaluate());
+  }
+
+  @Test
+  public void testMissingIntComparator() {
+    var gen = new DefineComparableWrapper();
+    gen.intNumConversion = true;
+    gen.intComparator = false;
+    gen.numComparator = true;
+    assertFalse("Num.Value not equal to Integer: ", gen.evaluate());
+  }
+
+  @Test
+  public void testMissingNumComparator() {
+    var gen = new DefineComparableWrapper();
+    gen.intNumConversion = true;
+    gen.intComparator = true;
+    gen.numComparator = false;
+    assertFalse("Num.Value not equal to Integer: ", gen.evaluate());
+  }
+
+  @Test
+  public void testDifferentComparators() {
+    var gen = new DefineComparableWrapper();
+    gen.intNumConversion = true;
+    gen.intComparator = true;
+    gen.numComparator = false;
+    gen.extraBlock =
+        """
+    type Second_Comparator
+        compare a:Num b:Num = Num_Comparator.compare a b
+        hash a:Num = Num_Comparator.hash a
+
+    Comparable.from (_:Num) = Second_Comparator
+    """;
+    assertFalse("Num.Value not equal to Integer: ", gen.evaluate());
+  }
+
+  @Test
+  public void testInconsistentHashFunction() {
+    var gen = new DefineComparableWrapper();
+    gen.intNumConversion = true;
+    gen.intComparator = true;
+    gen.numComparator = true;
+    gen.hashFn = "x.n*31";
+
+    boolean r;
+    try {
+      r = gen.evaluate();
+    } catch (PolyglotException ex) {
+      assertTrue(ex.getMessage(), ex.getMessage().contains("Different hash code!"));
+      return;
+    }
+    fail("Expecting assertion error, not: " + r);
+  }
+
+  private static final class DefineComparableWrapper {
+    boolean intNumConversion;
+    boolean numComparator;
+    boolean intComparator;
+    String hashFn = "Default_Comparator.hash x.n";
+    String extraBlock = "";
+
+    boolean evaluate() {
+      var block0 =
+          """
+          from Standard.Base import all
+
+          type Num
+              Value n:Integer
+
+          type Num_Comparator
+              compare x:Num y:Num = Ordering.compare x.n y.n
+              hash x:Num = {hashFn}
+          """
+              .replace("{hashFn}", hashFn);
+
+      var block1 =
+          !intNumConversion
+              ? ""
+              : """
+          Num.from (that:Integer) = Num.Value that
+          """;
+
+      var block2 =
+          !numComparator
+              ? ""
+              : """
+          Comparable.from (_:Num) = Num_Comparator
+          """;
+
+      var block3 =
+          !intComparator ? "" : """
+      Comparable.from (_:Integer) = Num_Comparator
+      """;
+
+      var mainBlock =
+          """
+      main =
+          num42 = Num.Value 42
+
+          r0 = 42 == num42
+          r0
+      """;
+      var res =
+          TestBase.evalModule(context, block0 + block1 + block2 + block3 + mainBlock + extraBlock);
+      return res.asBoolean();
+    }
+  }
+}

commit f02213ae2bd47123f4619aba1147cd24e3c1fd3b

Enable splitting for EqualsSimpleNode (#9268)
Fixes the regression introduced by #9070 in `org.enso.benchmarks.generated.Collections.list_meta_fold` benchmark.

# Important Notes
As can be seen on the graph in IGV:
![image](https://github.com/enso-org/enso/assets/14013887/31b6ceca-4909-4a8f-987f-b456b3fb0a1b)
For some reason, `EqualsSimpleNode` is POLYMORPHIC. That seems to be the most visible performance problem.

First, I tried to introduce `ConditionProfile` with:
```diff
diff --git a/engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/EqualsNode.java b/engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/EqualsNode.java
index b368fb7fe..57274b37e 100644
--- a/engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/EqualsNode.java
+++ b/engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/EqualsNode.java
@@ -9,6 +9,7 @@ import com.oracle.truffle.api.dsl.Specialization;
import com.oracle.truffle.api.frame.VirtualFrame;
import com.oracle.truffle.api.interop.ArityException;
import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.profiles.ConditionProfile;
import org.enso.interpreter.dsl.AcceptsError;
import org.enso.interpreter.dsl.BuiltinMethod;
import org.enso.interpreter.node.EnsoRootNode;
@@ -46,6 +47,7 @@ public final class EqualsNode extends Node {
@Child private EqualsSimpleNode node;
@Child private TypeOfNode types;
@Child private WithConversionNode convert;
+  private final ConditionProfile equalsProfile = ConditionProfile.create();

private static final EqualsNode UNCACHED =
new EqualsNode(EqualsSimpleNodeGen.getUncached(), TypeOfNode.getUncached(), true);
@@ -85,7 +87,7 @@ public final class EqualsNode extends Node {
public boolean execute(
VirtualFrame frame, @AcceptsError Object self, @AcceptsError Object other) {
var areEqual = node.execute(frame, self, other);
-    if (!areEqual) {
+    if (!equalsProfile.profile(areEqual)) {

engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/EqualsSimpleNode.java
@@ -4,6 +4,7 @@ import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Cached.Shared;
 import com.oracle.truffle.api.dsl.GenerateUncached;
+import com.oracle.truffle.api.dsl.ReportPolymorphism;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.interop.InteropLibrary;
@@ -23,6 +24,7 @@ import org.enso.interpreter.runtime.number.EnsoBigInteger;
 import org.enso.polyglot.common_utils.Core_Text_Utils;
 
 @GenerateUncached
+@ReportPolymorphism
 public abstract class EqualsSimpleNode extends Node {
 
   public static EqualsSimpleNode build() {