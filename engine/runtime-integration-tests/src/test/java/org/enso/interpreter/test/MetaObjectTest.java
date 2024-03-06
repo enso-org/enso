cf2a64f49c3b14c0a7ee5a0937f55f3f24da24da
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2024-03-06 12:44:59 +0100

    nit

--- engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/MetaObjectTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/MetaObjectTest.java
@@ -108,7 +108,7 @@ public class MetaObjectTest extends TestBase {
       }
     }
     expecting.remove(ConstantsGen.ARRAY_LIKE_HELPERS);
-    expecting.remove(ConstantsGen.ARROW_ARRAY_BUILDER);
+    expecting.remove(ConstantsGen.POLYGLOT_ARRAY_BUILDER);
     var w = new StringBuilder();
     var f = new StringWriter();
     var err = new PrintWriter(f);
