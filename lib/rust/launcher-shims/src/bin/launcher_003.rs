f1d719d602c87858cf8cce4353206dd9c456a88b
Michael Mauderer <michael.mauderer@enso.org>
2024-03-06 12:30:32 +0000

    Revert "Remove unused crates."
    This reverts commit 4da8cb6dad76f1854d288e37bac54a5265583182.

---
+++ lib/rust/launcher-shims/src/bin/launcher_003.rs
@@ -0,0 +1,18 @@
+// === Standard Linter Configuration ===
+#![deny(non_ascii_idents)]
+#![warn(unsafe_code)]
+#![allow(clippy::bool_to_int_with_if)]
+#![allow(clippy::let_and_return)]
+
+use launcher_shims::wrap_launcher;
+
+
+
+// =======================
+// === EntryPoint0.0.3 ===
+// =======================
+
+/// Runs the launcher wrapper overriding the version to 0.0.3.
+fn main() {
+    wrap_launcher("0.0.3")
+}
---
+++ lib/rust/launcher-shims/src/lib.rs
@@ -0,0 +1,86 @@
+// === Standard Linter Configuration ===
+#![deny(non_ascii_idents)]
+#![warn(unsafe_code)]
+#![allow(clippy::bool_to_int_with_if)]
+#![allow(clippy::let_and_return)]
+
+use std::io::prelude::*;
+
+use std::env;
+use std::fs::OpenOptions;
+use std::io;
+use std::path::PathBuf;
+use std::process::exit;
+use std::process::Command;
+
+
+
+// ====================
+// === WrapLauncher ===
+// ====================
+
+/// Run the wrapped launcher overriding its reported version to the provided version.
+///
+/// The launcher's executable location is also overridden to point to this executable. The launcher
+/// is passed all the original arguments plus the arguments that handle the version and location
+/// override. The location of the original launcher executable that is wrapped is determined by the
+/// environment variable `ENSO_LAUNCHER_LOCATION` that should be set at build-time.
+///
+/// Additionally, the wrapper appends to a log file called `.launcher_version_log` a line containing
+/// the version string that was launched (creating the file if necessary). This can be used by tests
+/// to verify the order of launched versions.
+pub fn wrap_launcher(version: impl AsRef<str>) {
+    let args: Vec<String> = env::args().collect();
+
+    let missing_location_message = "`ENSO_LAUNCHER_LOCATION` is not defined.";
+    let launcher_location = env::var("ENSO_LAUNCHER_LOCATION").expect(missing_location_message);
+
+    let current_exe_path = env::current_exe().expect("Cannot get current executable path.");
+    let exe_location = match current_exe_path.to_str() {
+        Some(str) => str,
+        None => {
+            eprintln!("Path {} is invalid.", current_exe_path.to_string_lossy());
+            exit(1)
+        }
+    };
+
+    let missing_directory_message = "Executable path should have a parent directory.";
+    let parent_directory = current_exe_path.parent().expect(missing_directory_message);
+    let log_name = ".launcher_version_log";
+    let log_path = parent_directory.join(log_name);
+    append_to_log(log_path, version.as_ref()).expect("Cannot write to log.");
+
+    let override_args = [
+        String::from("--internal-emulate-version"),
+        version.as_ref().to_string(),
+        String::from("--internal-emulate-location"),
+        String::from(exe_location),
+    ];
+    let modified_args = [&override_args[..], &args[1..]].concat();
+
+    let exit_status = Command::new(launcher_location).args(modified_args).status();
+    let exit_code = match exit_status {
+        Ok(status) =>
+            if let Some(code) = status.code() {
+                code
+            } else {
+                eprintln!("Process terminated by signal.");
+                exit(1)
+            },
+        Err(error) => {
+            eprintln!("{error}");
+            exit(1)
+        }
+    };
+    exit(exit_code)
+}
+
+
+// === Log ===
+
+/// Appends a line to the file located at the provided path.
+pub fn append_to_log(path: PathBuf, line: impl AsRef<str>) -> io::Result<()> {
+    let mut log_file = OpenOptions::new().create(true).write(true).append(true).open(path)?;
+    writeln!(log_file, "{}", line.as_ref())?;
+    Ok(())
+}
