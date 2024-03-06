0f873743e59c516502a5cfc990750d1af310d99a
somebody1234 <ehern.lee@gmail.com>
2024-03-06 23:04:03 +1000

    Fix `*.config.ts`

--- app/ide-desktop/lib/dashboard/vite.test.config.ts
+++ app/ide-desktop/lib/dashboard/vite.test.config.ts
@@ -11,41 +11,39 @@ import config from './vite.config'
 
 const SERVER_PORT = 8080
 
-export default vite.defineConfig(env =>
-  vite.mergeConfig(
-    config(env),
-    vite.defineConfig({
-      resolve: {
-        extensions: [
-          '.mock.mjs',
-          '.mock.js',
-          '.mock.mts',
-          '.mock.ts',
-          '.mock.jsx',
-          '.mock.tsx',
-          '.mock.json',
-          '.mjs',
-          '.js',
-          '.mts',
-          '.ts',
-          '.jsx',
-          '.tsx',
-          '.json',
-        ],
-      },
-      define: {
-        'process.env.NODE_ENV': JSON.stringify('production'),
-        'process.env.ENSO_CLOUD_REDIRECT': JSON.stringify(`http://localhost:${SERVER_PORT}`),
-        'process.env.ENSO_CLOUD_ENVIRONMENT': JSON.stringify('production'),
-        'process.env.ENSO_CLOUD_API_URL': JSON.stringify('https://mock'),
-        'process.env.ENSO_CLOUD_SENTRY_DSN': JSON.stringify(''),
-        'process.env.ENSO_CLOUD_STRIPE_KEY': JSON.stringify(''),
-        'process.env.ENSO_CLOUD_CHAT_URL': JSON.stringify(''),
-        'process.env.ENSO_CLOUD_COGNITO_USER_POOL_ID': JSON.stringify(''),
-        'process.env.ENSO_CLOUD_COGNITO_USER_POOL_WEB_CLIENT_ID': JSON.stringify(''),
-        'process.env.ENSO_CLOUD_COGNITO_DOMAIN': JSON.stringify(''),
-        'process.env.ENSO_CLOUD_COGNITO_REGION': JSON.stringify(''),
-      },
-    })
-  )
+export default vite.mergeConfig(
+  config,
+  vite.defineConfig({
+    resolve: {
+      extensions: [
+        '.mock.mjs',
+        '.mock.js',
+        '.mock.mts',
+        '.mock.ts',
+        '.mock.jsx',
+        '.mock.tsx',
+        '.mock.json',
+        '.mjs',
+        '.js',
+        '.mts',
+        '.ts',
+        '.jsx',
+        '.tsx',
+        '.json',
+      ],
+    },
+    define: {
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      'process.env.ENSO_CLOUD_REDIRECT': JSON.stringify(`http://localhost:${SERVER_PORT}`),
+      'process.env.ENSO_CLOUD_ENVIRONMENT': JSON.stringify('production'),
+      'process.env.ENSO_CLOUD_API_URL': JSON.stringify('https://mock'),
+      'process.env.ENSO_CLOUD_SENTRY_DSN': JSON.stringify(''),
+      'process.env.ENSO_CLOUD_STRIPE_KEY': JSON.stringify(''),
+      'process.env.ENSO_CLOUD_CHAT_URL': JSON.stringify(''),
+      'process.env.ENSO_CLOUD_COGNITO_USER_POOL_ID': JSON.stringify(''),
+      'process.env.ENSO_CLOUD_COGNITO_USER_POOL_WEB_CLIENT_ID': JSON.stringify(''),
+      'process.env.ENSO_CLOUD_COGNITO_DOMAIN': JSON.stringify(''),
+      'process.env.ENSO_CLOUD_COGNITO_REGION': JSON.stringify(''),
+    },
+  })
 )
--- app/ide-desktop/lib/dashboard/vitest.config.ts
+++ app/ide-desktop/lib/dashboard/vitest.config.ts
@@ -5,16 +5,14 @@ import * as vitestConfig from 'vitest/config'
 
 import viteConfig from './vite.config'
 
-export default vitestConfig.defineConfig(env =>
-  vitestConfig.mergeConfig(
-    viteConfig(env),
-    vitestConfig.defineConfig({
-      test: {
-        environment: 'jsdom',
-        exclude: ['**/*.spec.{ts,tsx}'],
-        root: url.fileURLToPath(new URL('./', import.meta.url)),
-        restoreMocks: true,
-      },
-    })
-  )
+export default vitestConfig.mergeConfig(
+  viteConfig,
+  vitestConfig.defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: ['**/*.spec.{ts,tsx}'],
+      root: url.fileURLToPath(new URL('./', import.meta.url)),
+      restoreMocks: true,
+    },
+  })
 )
