7c68bf170d754fcfb17eecbf724ebe8b872a433c
Michael Mauderer <michael.mauderer@enso.org>
2024-03-07 02:20:21 +0000

    Remove gui1 codebase (#9242)
    Removes the old GUI1 code base and reduces the Rust code footprint by removing unused code.
    
    # Important Notes
    Updates build scripts and reformats part of the codebase with the autoformatter.

--- build/deprecated/build-utils/src/lib.rs
+++ build/deprecated/build-utils/src/lib.rs
@@ -3,11 +3,13 @@
 
 // === Features ===
 #![feature(trait_alias)]
+
 // === Standard Linter Configuration ===
 #![deny(non_ascii_idents)]
 #![warn(unsafe_code)]
 #![allow(clippy::bool_to_int_with_if)]
 #![allow(clippy::let_and_return)]
+
 // === Non-Standard Linter Configuration ===
 #![allow(clippy::option_map_unit_fn)]
 #![allow(clippy::precedence)]
