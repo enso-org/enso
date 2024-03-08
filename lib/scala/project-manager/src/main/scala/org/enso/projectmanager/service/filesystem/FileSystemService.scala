ee2dc57d68ed3005a00731d0ca5545d2ce2a5f03
Dmitry Bushev <bushevdv@gmail.com>
2024-03-08 15:44:17 +0000

    Project manager API for basic file system operations (#9245)
    close #9173
    
    Changelog:
    - add: `--filesystem-list {path}` cli parameter that list files, directories and projects
    - add: `--filesystem-create-directory {path}` cli parameter that creates a specified directory
    - add: `--filesystem-delete-directory {path}` cli parameter that deletes a specified directory
    - add: `--filesystem-move-from {path}` ` --filesystem-move-to {path}` cli parameters to move specified filesystem object

---
+++ lib/scala/project-manager/src/main/scala/org/enso/projectmanager/service/filesystem/FileSystemService.scala
@@ -0,0 +1,97 @@
+package org.enso.projectmanager.service.filesystem
+
+import org.enso.projectmanager.control.core.{
+  Applicative,
+  CovariantFlatMap,
+  Traverse
+}
+import org.enso.projectmanager.control.effect.ErrorChannel
+import org.enso.projectmanager.infrastructure.file.FileSystem
+import org.enso.projectmanager.control.core.syntax._
+import org.enso.projectmanager.control.effect.syntax._
+import org.enso.projectmanager.infrastructure.repository.ProjectRepositoryFactory
+import org.enso.projectmanager.service.ProjectService
+
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.attribute.BasicFileAttributes
+
+class FileSystemService[F[+_, +_]: Applicative: CovariantFlatMap: ErrorChannel](
+  fileSystem: FileSystem[F],
+  projectRepositoryFactory: ProjectRepositoryFactory[F]
+) extends FileSystemServiceApi[F] {
+
+  /** @inheritdoc */
+  override def list(
+    path: File
+  ): F[FileSystemServiceFailure, Seq[FileSystemEntry]] =
+    fileSystem
+      .list(path)
+      .mapError(_ =>
+        FileSystemServiceFailure.FileSystem("Failed to list directories")
+      )
+      .flatMap { files =>
+        Traverse[List].traverse(files)(toFileSystemEntry).map(_.flatten)
+      }
+
+  /** @inheritdoc */
+  override def createDirectory(path: File): F[FileSystemServiceFailure, Unit] =
+    fileSystem
+      .createDir(path)
+      .mapError(_ =>
+        FileSystemServiceFailure.FileSystem("Failed to create directory")
+      )
+
+  /** @inheritdoc */
+  override def deleteDirectory(path: File): F[FileSystemServiceFailure, Unit] =
+    fileSystem
+      .removeDir(path)
+      .mapError(_ =>
+        FileSystemServiceFailure.FileSystem("Failed to delete directory")
+      )
+
+  /** @inheritdoc */
+  override def move(from: File, to: File): F[FileSystemServiceFailure, Unit] =
+    fileSystem
+      .move(from, to)
+      .mapError(_ => FileSystemServiceFailure.FileSystem("Failed to move path"))
+
+  private def toFileSystemEntry(
+    path: File
+  ): F[FileSystemServiceFailure, Option[FileSystemEntry]] = {
+    val file = normalize(path)
+    val projectRepository =
+      projectRepositoryFactory.getProjectRepository(Some(file))
+    val basicFileAttributes =
+      Files.readAttributes(file.toPath, classOf[BasicFileAttributes])
+    val attributes = Attributes(basicFileAttributes)
+    if (file.isFile)
+      CovariantFlatMap[F].pure(
+        Some(FileSystemEntry.FileEntry(file, attributes))
+      )
+    else if (file.isDirectory) {
+      projectRepository
+        .tryLoadProject(file)
+        .map(
+          _.fold[FileSystemEntry](
+            FileSystemEntry.DirectoryEntry(file, attributes)
+          )(project =>
+            FileSystemEntry
+              .ProjectEntry(
+                file,
+                attributes,
+                ProjectService.toProjectMetadata(project)
+              )
+          )
+        )
+        .map(Some(_))
+        .mapError(e =>
+          FileSystemServiceFailure
+            .ProjectRepository("Failed to load the project", e.message)
+        )
+    } else CovariantFlatMap[F].pure(None)
+  }
+
+  private def normalize(file: File): File =
+    file.toPath.toAbsolutePath.normalize().toFile
+}
---
+++ lib/scala/project-manager/src/test/scala/org/enso/projectmanager/service/filesystem/FileSystemServiceSpec.scala
@@ -0,0 +1,173 @@
+package org.enso.projectmanager.service.filesystem
+
+import org.apache.commons.io.FileUtils
+import org.enso.projectmanager.{BaseServerSpec, ProjectManagementOps}
+import org.enso.projectmanager.control.effect.Effects
+import org.enso.projectmanager.data.ProjectMetadata
+import org.enso.projectmanager.infrastructure.repository.{
+  MetadataFileStorage,
+  ProjectFileRepositoryFactory
+}
+import org.enso.runtimeversionmanager.test.OverrideTestVersionSuite
+import org.enso.semver.SemVer
+import org.scalatest.EitherValues
+import zio.{ZAny, ZIO}
+
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.attribute.BasicFileAttributes
+
+class FileSystemServiceSpec
+    extends BaseServerSpec
+    with ProjectManagementOps
+    with OverrideTestVersionSuite
+    with Effects
+    with EitherValues {
+
+  override val testVersion: SemVer = SemVer.of(0, 0, 1)
+
+  override val engineToInstall: Option[SemVer] = Some(SemVer.of(0, 0, 1))
+
+  lazy val fileSystemService: FileSystemService[ZIO[ZAny, +*, +*]] = {
+    val projectRepositoryFactory =
+      new ProjectFileRepositoryFactory[ZIO[ZAny, +*, +*]](
+        config.storage,
+        testClock,
+        fileSystem,
+        gen
+      )
+    new FileSystemService[ZIO[ZAny, +*, +*]](
+      fileSystem,
+      projectRepositoryFactory
+    )
+  }
+
+  def metadataFileStorage(directory: File) =
+    new MetadataFileStorage[ZIO[ZAny, +*, +*]](
+      directory,
+      config.storage,
+      testClock,
+      fileSystem,
+      gen
+    )
+
+  def attributes(path: File): Attributes =
+    Attributes(Files.readAttributes(path.toPath, classOf[BasicFileAttributes]))
+
+  "FileSystemService" should {
+
+    "list directory" in {
+      implicit val client: WsTestClient = new WsTestClient(address)
+
+      val testDir = testStorageConfig.userProjectsPath
+
+      val projectName = "New_Project_1"
+      val projectId   = createProject(projectName)
+      val projectPath = new File(testDir, projectName)
+
+      val testFile = new File(testDir, "foo.txt")
+
+      Files.createFile(testFile.toPath)
+
+      val result = fileSystemService
+        .list(testDir)
+        .unsafeRunSync()
+
+      result.value should contain theSameElementsAs Seq(
+        FileSystemEntry.ProjectEntry(
+          projectPath,
+          attributes(projectPath),
+          ProjectMetadata(
+            projectName,
+            "local",
+            projectId,
+            testClock.currentTime,
+            None
+          )
+        ),
+        FileSystemEntry.FileEntry(testFile, attributes(testFile))
+      )
+
+      // cleanup
+      deleteProject(projectId)
+    }
+
+    "create directory" in {
+      val testDir = testStorageConfig.userProjectsPath
+
+      val directoryName = "filesystem_test_create_dir"
+      val directoryPath = new File(testDir, directoryName)
+
+      fileSystemService
+        .createDirectory(directoryPath)
+        .unsafeRunSync()
+
+      Files.isDirectory(directoryPath.toPath) shouldEqual true
+
+      // cleanup
+      FileUtils.deleteQuietly(directoryPath)
+    }
+
+    "delete directory" in {
+      implicit val client: WsTestClient = new WsTestClient(address)
+
+      val testDir = testStorageConfig.userProjectsPath
+
+      val projectName = "New_Project_2"
+      createProject(projectName)
+
+      val directoryPath = new File(testDir, projectName)
+
+      fileSystemService
+        .deleteDirectory(directoryPath)
+        .unsafeRunSync()
+
+      Files.exists(directoryPath.toPath) shouldEqual false
+    }
+
+    "move file" in {
+      val testDir = testStorageConfig.userProjectsPath
+
+      val targetFileName      = "target_move_file.txt"
+      val destinationFileName = "destination_move_file.txt"
+      val targetFilePath      = new File(testDir, targetFileName)
+      val destinationFilePath = new File(testDir, destinationFileName)
+
+      FileUtils.forceMkdirParent(targetFilePath)
+      FileUtils.touch(targetFilePath)
+
+      fileSystemService
+        .move(targetFilePath, destinationFilePath)
+        .unsafeRunSync()
+
+      Files.exists(targetFilePath.toPath) shouldEqual false
+      Files.exists(destinationFilePath.toPath) shouldEqual true
+
+      // cleanup
+      FileUtils.deleteQuietly(destinationFilePath)
+    }
+
+    "move directory" in {
+      implicit val client: WsTestClient = new WsTestClient(address)
+
+      val testDir = testStorageConfig.userProjectsPath
+
+      val projectName = "New_Project_3"
+      createProject(projectName)
+
+      val directoryPath = new File(testDir, projectName)
+      val targetPath    = new File(testDir, "Target_Move_Directory")
+
+      fileSystemService
+        .move(directoryPath, targetPath)
+        .unsafeRunSync()
+
+      Files.exists(directoryPath.toPath) shouldEqual false
+      Files.isDirectory(targetPath.toPath) shouldEqual true
+
+      // cleanup
+      FileUtils.deleteQuietly(targetPath)
+    }
+
+  }
+}
