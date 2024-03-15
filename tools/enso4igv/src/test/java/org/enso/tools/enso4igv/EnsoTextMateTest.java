43ef037d0cdf3684184b6c046e398edfc72fc82c
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-03-15 09:32:15 +0100

    Test of lexing foreign js function

--- tools/enso4igv/src/test/java/org/enso/tools/enso4igv/EnsoTextMateTest.java
+++ tools/enso4igv/src/test/java/org/enso/tools/enso4igv/EnsoTextMateTest.java
@@ -2,6 +2,7 @@ package org.enso.tools.enso4igv;
 
 import java.util.List;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.assertNotEquals;
@@ -37,7 +38,40 @@ public class EnsoTextMateTest {
         assertNextToken(seq, "42", "constant.character.numeric");
     }
 
+    @Test
+    public void testForeignJavaScriptFunction() {
+        var code = """
+                  foreign js dbg = '''
+                      debugger;
+                  """;
+        var hier = org.netbeans.api.lexer.TokenHierarchy.create(code, ensoLanguage);
+        assertNotNull(hier);
+        var seq = hier.tokenSequence();
+        assertEquals(4, seq.tokenCount());
+        assertNextToken(seq, "foreign js dbg = ", null);
+        assertNextToken(seq, "'''", "string.quoted.triple.begin");
+        assertNextToken(seq, "\n", null);
+        assertNextToken(seq, "    debugger;\n", null);
+        assertFalse("EOF", seq.moveNext());
+    }
+
     private static void assertNextToken(TokenSequence<?> seq, String expectedText, String expectedCategory) {
+        try {
+            assertNextTokenImpl(seq, expectedText, expectedCategory);
+        } catch (AssertionError err) {
+            var sb = new StringBuilder();
+            sb.append(err.getMessage()).append("\n");
+            sb.append("Remaining tokens:\n");
+            seq.movePrevious();
+            while (seq.moveNext()) {
+                final Object categories = seq.token().getProperty("categories");
+                sb.append(seq.token().text()).append(" categories: ").append(categories).append("\n");
+            }
+            throw new AssertionError(sb.toString(), err);
+        }
+    }
+
+    private static void assertNextTokenImpl(TokenSequence<?> seq, String expectedText, String expectedCategory) {
         assertTrue("Can move next", seq.moveNext());
         var tok = seq.token();
         assertNotNull("Token found", tok);
@@ -46,6 +80,7 @@ public class EnsoTextMateTest {
         }
         if (expectedCategory != null) {
             var categories = (List<String>) tok.getProperty("categories");
+            assertNotNull("Categories found", categories);
             var at = categories.indexOf(expectedCategory);
             assertNotEquals("Category " + expectedCategory + " found in " + categories, -1, at);
         }
