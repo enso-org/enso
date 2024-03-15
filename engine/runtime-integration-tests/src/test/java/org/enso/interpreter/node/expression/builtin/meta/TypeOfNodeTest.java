55877b98c15320c10b55dbc4848163d49c7320c9
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-03-15 11:42:06 +0100

    Moving the TypeOfNodeTest into runtime-integration-tests project

--- engine/runtime-integration-tests/src/test/java/org/enso/interpreter/node/expression/builtin/meta/TypeOfNodeTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/node/expression/builtin/meta/TypeOfNodeTest.java
@@ -4,22 +4,16 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import com.oracle.truffle.api.interop.TruffleObject;
-import java.lang.System.Logger.Level;
-import java.nio.file.Paths;
-import java.util.concurrent.Callable;
 import org.enso.interpreter.runtime.callable.UnresolvedConstructor;
 import org.enso.interpreter.runtime.callable.UnresolvedSymbol;
 import org.enso.interpreter.runtime.error.DataflowError;
-import org.enso.polyglot.RuntimeOptions;
+import org.enso.interpreter.test.TestBase;
 import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.Value;
-import org.graalvm.polyglot.io.IOAccess;
-import org.graalvm.polyglot.proxy.ProxyExecutable;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class TypeOfNodeTest {
+public class TypeOfNodeTest extends TestBase {
 
   private static Context ctx;
 
@@ -71,47 +65,5 @@ public class TypeOfNodeTest {
           assertEquals(expectedTypeName, symbolTypeValue.getMetaSimpleName());
           return null;
         });
-  }
-
-  private static Context.Builder defaultContextBuilder(String... languages) {
-    return Context.newBuilder(languages)
-        .allowExperimentalOptions(true)
-        .allowIO(IOAccess.ALL)
-        .allowAllAccess(true)
-        .option(RuntimeOptions.LOG_LEVEL, Level.WARNING.getName())
-        .option(RuntimeOptions.DISABLE_IR_CACHES, "true")
-        .logHandler(System.err)
-        .option(RuntimeOptions.STRICT_ERRORS, "true")
-        .option(
-            RuntimeOptions.LANGUAGE_HOME_OVERRIDE,
-            Paths.get("../../distribution/component").toFile().getAbsolutePath());
-  }
-
-  private static Value executeInContext(Context ctx, Callable<Object> callable) {
-    // Force initialization of the context
-    ctx.eval("enso", "42");
-    var err = new Exception[1];
-    ctx.getPolyglotBindings()
-        .putMember(
-            "testSymbol",
-            (ProxyExecutable)
-                (Value... args) -> {
-                  try {
-                    return callable.call();
-                  } catch (Exception e) {
-                    err[0] = e;
-                    return null;
-                  }
-                });
-    var res = ctx.getPolyglotBindings().getMember("testSymbol").execute();
-    if (err[0] != null) {
-      throw raise(RuntimeException.class, err[0]);
-    }
-    return res;
-  }
-
-  @SuppressWarnings("unchecked")
-  private static <E extends Throwable> E raise(Class<E> clazz, Throwable t) throws E {
-    throw (E) t;
   }
 }
