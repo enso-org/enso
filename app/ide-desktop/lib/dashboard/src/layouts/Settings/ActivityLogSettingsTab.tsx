2b16ccc8a55fbc513f0f959c6b6bb2a761525ea5
somebody1234 <ehern.lee@gmail.com>
2024-03-19 15:23:06 +1000

    Add sorting to Activity Log settings page

--- app/ide-desktop/lib/dashboard/src/layouts/Settings/ActivityLogSettingsTab.tsx
+++ app/ide-desktop/lib/dashboard/src/layouts/Settings/ActivityLogSettingsTab.tsx
@@ -4,6 +4,7 @@ import * as React from 'react'
 import DataUploadIcon from 'enso-assets/data_upload.svg'
 import KeyIcon from 'enso-assets/key.svg'
 import Play2Icon from 'enso-assets/play2.svg'
+import SortAscendingIcon from 'enso-assets/sort_ascending.svg'
 import TrashIcon from 'enso-assets/trash.svg'
 
 import * as asyncEffectHooks from '#/hooks/asyncEffectHooks'
@@ -18,6 +19,18 @@ import SvgMask from '#/components/SvgMask'
 import * as backendModule from '#/services/Backend'
 
 import * as dateTime from '#/utilities/dateTime'
+import * as sorting from '#/utilities/sorting'
+
+// =========================
+// === ActivityLogColumn ===
+// =========================
+
+/** Sortable columns in an activity log table. */
+enum ActivityLogSortableColumn {
+  type = 'type',
+  email = 'email',
+  timestamp = 'timestamp',
+}
 
 // ==============================
 // === ActivityLogSettingsTab ===
@@ -48,6 +61,8 @@ export default function ActivityLogSettingsTab() {
   const [typeIndices, setTypeIndices] = React.useState<readonly number[]>(() => [])
   const [emails, setEmails] = React.useState<readonly string[]>([])
   const [emailIndices, setEmailIndices] = React.useState<readonly number[]>(() => [])
+  const [sortInfo, setSortInfo] =
+    React.useState<sorting.SortInfo<ActivityLogSortableColumn> | null>(null)
   const users = asyncEffectHooks.useAsyncEffect([], () => backend.listUsers(), [backend])
   const allEmails = React.useMemo(() => users.map(user => user.email), [users])
   const logs = asyncEffectHooks.useAsyncEffect(null, () => backend.getLogEvents(), [backend])
@@ -66,7 +81,38 @@ export default function ActivityLogSettingsTab() {
           )
         })
   }, [logs, types, emails, startDate, endDate, allEmails])
-  const isLoading = filteredLogs == null
+  const sortedLogs = React.useMemo(() => {
+    if (sortInfo == null || filteredLogs == null) {
+      return filteredLogs
+    } else {
+      let compare: (a: backendModule.Event, b: backendModule.Event) => number
+      const multiplier = sortInfo.direction === sorting.SortDirection.ascending ? 1 : -1
+      switch (sortInfo.field) {
+        case ActivityLogSortableColumn.type: {
+          compare = (a, b) =>
+            multiplier *
+            (a.metadata.type < b.metadata.type ? -1 : a.metadata.type > b.metadata.type ? 1 : 0)
+          break
+        }
+        case ActivityLogSortableColumn.email: {
+          compare = (a, b) =>
+            multiplier * (a.userEmail < b.userEmail ? -1 : a.userEmail > b.userEmail ? 1 : 0)
+          break
+        }
+        case ActivityLogSortableColumn.timestamp: {
+          compare = (a, b) => {
+            const aTime = a.timestamp == null ? 0 : Number(new Date(a.timestamp))
+            const bTime = b.timestamp == null ? 0 : Number(new Date(b.timestamp))
+            return multiplier * aTime - bTime
+          }
+          break
+        }
+      }
+      return [...filteredLogs].sort(compare)
+    }
+  }, [filteredLogs, sortInfo])
+  const isDescending = sortInfo?.direction === sorting.SortDirection.descending
+  const isLoading = sortedLogs == null
 
   return (
     <div className="flex flex-col gap-settings-subsection">
@@ -125,20 +171,137 @@ export default function ActivityLogSettingsTab() {
             <tr className="h-row">
               <th className="w-activity-log-icon-column border-x-2 border-transparent bg-clip-padding pl-cell-x pr-icon-column-r text-left text-sm font-semibold last:border-r-0" />
               <th className="w-activity-log-type-column border-x-2 border-transparent bg-clip-padding px-cell-x text-left text-sm font-semibold last:border-r-0">
-                Type
+                <button
+                  title={
+                    sortInfo?.field !== ActivityLogSortableColumn.type
+                      ? 'Sort by name'
+                      : isDescending
+                        ? 'Stop sorting by name'
+                        : 'Sort by name descending'
+                  }
+                  className="group flex h-drive-table-heading w-full items-center gap-icon-with-text px-name-column-x"
+                  onClick={event => {
+                    event.stopPropagation()
+                    const nextDirection =
+                      sortInfo?.field === ActivityLogSortableColumn.type
+                        ? sorting.nextSortDirection(sortInfo.direction)
+                        : sorting.SortDirection.ascending
+                    if (nextDirection == null) {
+                      setSortInfo(null)
+                    } else {
+                      setSortInfo({
+                        field: ActivityLogSortableColumn.type,
+                        direction: nextDirection,
+                      })
+                    }
+                  }}
+                >
+                  <span className="text-header">Type</span>
+                  <img
+                    alt={
+                      sortInfo?.field === ActivityLogSortableColumn.type && isDescending
+                        ? 'Sort Descending'
+                        : 'Sort Ascending'
+                    }
+                    src={SortAscendingIcon}
+                    className={`transition-all duration-arrow ${
+                      sortInfo?.field === ActivityLogSortableColumn.type
+                        ? 'selectable active'
+                        : 'transparent group-hover:selectable'
+                    } ${isDescending ? 'rotate-180' : ''}`}
+                  />
+                </button>
               </th>
               <th className="w-activity-log-email-column border-x-2 border-transparent bg-clip-padding px-cell-x text-left text-sm font-semibold last:border-r-0">
-                Email
+                <button
+                  title={
+                    sortInfo?.field !== ActivityLogSortableColumn.email
+                      ? 'Sort by email'
+                      : isDescending
+                        ? 'Stop sorting by email'
+                        : 'Sort by email descending'
+                  }
+                  className="group flex h-drive-table-heading w-full items-center gap-icon-with-text px-name-column-x"
+                  onClick={event => {
+                    event.stopPropagation()
+                    const nextDirection =
+                      sortInfo?.field === ActivityLogSortableColumn.email
+                        ? sorting.nextSortDirection(sortInfo.direction)
+                        : sorting.SortDirection.ascending
+                    if (nextDirection == null) {
+                      setSortInfo(null)
+                    } else {
+                      setSortInfo({
+                        field: ActivityLogSortableColumn.email,
+                        direction: nextDirection,
+                      })
+                    }
+                  }}
+                >
+                  <span className="text-header">Email</span>
+                  <img
+                    alt={
+                      sortInfo?.field === ActivityLogSortableColumn.email && isDescending
+                        ? 'Sort Descending'
+                        : 'Sort Ascending'
+                    }
+                    src={SortAscendingIcon}
+                    className={`transition-all duration-arrow ${
+                      sortInfo?.field === ActivityLogSortableColumn.email
+                        ? 'selectable active'
+                        : 'transparent group-hover:selectable'
+                    } ${isDescending ? 'rotate-180' : ''}`}
+                  />
+                </button>
               </th>
               <th className="w-activity-log-timestamp-column border-x-2 border-transparent bg-clip-padding px-cell-x text-left text-sm font-semibold last:border-r-0">
-                Timestamp
+                <button
+                  title={
+                    sortInfo?.field !== ActivityLogSortableColumn.timestamp
+                      ? 'Sort by timestamp'
+                      : isDescending
+                        ? 'Stop sorting by timestamp'
+                        : 'Sort by timestamp descending'
+                  }
+                  className="group flex h-drive-table-heading w-full items-center gap-icon-with-text px-name-column-x"
+                  onClick={event => {
+                    event.stopPropagation()
+                    const nextDirection =
+                      sortInfo?.field === ActivityLogSortableColumn.timestamp
+                        ? sorting.nextSortDirection(sortInfo.direction)
+                        : sorting.SortDirection.ascending
+                    if (nextDirection == null) {
+                      setSortInfo(null)
+                    } else {
+                      setSortInfo({
+                        field: ActivityLogSortableColumn.timestamp,
+                        direction: nextDirection,
+                      })
+                    }
+                  }}
+                >
+                  <span className="text-header">Timestamp</span>
+                  <img
+                    alt={
+                      sortInfo?.field === ActivityLogSortableColumn.timestamp && isDescending
+                        ? 'Sort Descending'
+                        : 'Sort Ascending'
+                    }
+                    src={SortAscendingIcon}
+                    className={`transition-all duration-arrow ${
+                      sortInfo?.field === ActivityLogSortableColumn.timestamp
+                        ? 'selectable active'
+                        : 'transparent group-hover:selectable'
+                    } ${isDescending ? 'rotate-180' : ''}`}
+                  />
+                </button>
               </th>
             </tr>
           </thead>
           <tbody className="select-text">
             {isLoading ? (
               <tr className="h-row">
-                <td colSpan={4} className="rounded-full">
+                <td colSpan={4} className="rounded-full bg-transparent">
                   <div className="flex justify-center">
                     <StatelessSpinner
                       size={32}
@@ -148,7 +311,7 @@ export default function ActivityLogSettingsTab() {
                 </td>
               </tr>
             ) : (
-              filteredLogs.map((log, i) => (
+              sortedLogs.map((log, i) => (
                 <tr key={i} className="h-row">
                   <td className="border-x-2 border-transparent bg-clip-padding pl-cell-x pr-icon-column-r first:rounded-l-full last:rounded-r-full last:border-r-0">
                     <div className="flex items-center">
