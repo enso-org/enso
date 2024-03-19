7d5bce00ce714ef1f08a8da1867406bfaf769520
somebody1234 <ehern.lee@gmail.com>
2024-03-19 17:08:27 +1000

    Remove obsolete GUI arguments

--- app/ide-desktop/lib/client/esbuild-config.ts
+++ app/ide-desktop/lib/client/esbuild-config.ts
@@ -23,23 +23,19 @@ await appConfig.readEnvironmentFromFile()
  * - `ENSO_BUILD_IDE` - output directory for bundled client files;
  * - `ENSO_BUILD_PROJECT_MANAGER_IN_BUNDLE_PATH` - path to the project manager executable relative
  * to the PM bundle root;
- * - `ENSO_BUILD_IDE_BUNDLED_ENGINE_VERSION` - version of the Engine (backend) that is bundled
- * along with this client build.
  * @see bundlerOptions
  */
 export function bundlerOptionsFromEnv(): esbuild.BuildOptions {
     return bundlerOptions(
         path.join(paths.getIdeDirectory(), 'client'),
-        paths.getProjectManagerInBundlePath(),
-        paths.getBundledEngineVersion()
+        paths.getProjectManagerInBundlePath()
     )
 }
 
 /** Get options without relying on the environment. */
 export function bundlerOptions(
     outdir: string,
-    projectManagerInBundlePath: string,
-    bundledEngineVersion: string
+    projectManagerInBundlePath: string
 ): esbuild.BuildOptions {
     return {
         bundle: true,
@@ -53,7 +49,6 @@ export function bundlerOptions(
         /* eslint-disable @typescript-eslint/naming-convention */
         outExtension: { '.js': '.cjs' },
         define: {
-            BUNDLED_ENGINE_VERSION: JSON.stringify(bundledEngineVersion),
             PROJECT_MANAGER_IN_BUNDLE_PATH: JSON.stringify(projectManagerInBundlePath),
         },
         /* eslint-enable @typescript-eslint/naming-convention */
