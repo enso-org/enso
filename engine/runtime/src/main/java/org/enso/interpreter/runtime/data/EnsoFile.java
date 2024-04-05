a9db79f69be1da53c01cecb3568c078c608e6f99
Radosław Waśko <radoslaw.wasko@enso.org>
2024-04-05 19:09:43 +0200

    javafmt

--- engine/runtime/src/main/java/org/enso/interpreter/runtime/data/EnsoFile.java
+++ engine/runtime/src/main/java/org/enso/interpreter/runtime/data/EnsoFile.java
@@ -159,7 +159,8 @@ public final class EnsoFile implements EnsoObject {
     // Normalization is needed to correctly handle paths containing `..` and `.`.
     var parentOrNull = this.normalize().truffleFile.getParent();
 
-    // If the path has no parent because it is relative and there are no more segments, try again after making it absolute:
+    // If the path has no parent because it is relative and there are no more segments, try again
+    // after making it absolute:
     if (parentOrNull == null && !this.truffleFile.isAbsolute()) {
       parentOrNull = this.truffleFile.getAbsoluteFile().normalize().getParent();
     }
