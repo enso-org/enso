039a67ad78821699843f7b37f718c72d5a7acc2c
Radosław Waśko <radoslaw.wasko@enso.org>
2024-04-08 18:37:40 +0200

    simple req cache - for now caching just /users/me endpoint (which is safe to cache for a long time since it is not expected to be changing), made Enso_File_Spec run in 35s compared to 53s before caching

---
+++ std-bits/base/src/main/java/org/enso/base/enso_cloud/CloudRequestCache.java
@@ -0,0 +1,26 @@
+package org.enso.base.enso_cloud;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.function.Function;
+
+public final class CloudRequestCache {
+  private record CacheEntry(Object value, LocalDateTime expiresAt) {}
+  private final static HashMap<String, CacheEntry> cache = new HashMap<>();
+
+  public static void clear() {
+    cache.clear();
+  }
+
+  public static Object getOrCompute(String key, Function<String, Object> compute, Duration ttl) {
+    var entry = cache.get(key);
+    if (entry != null && entry.expiresAt.isAfter(LocalDateTime.now())) {
+      return entry.value;
+    } else {
+      var value = compute.apply(key);
+      cache.put(key, new CacheEntry(value, LocalDateTime.now().plus(ttl)));
+      return value;
+    }
+  }
+}
