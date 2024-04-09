f791a94e428761ac88d6d35a055a7e80f8f7c045
somebody1234 <ehern.lee@gmail.com>
2024-04-10 09:53:12 +1000

    Switch table to React Aria omponents

--- app/ide-desktop/lib/dashboard/src/layouts/Settings/MembersTable.tsx
+++ app/ide-desktop/lib/dashboard/src/layouts/Settings/MembersTable.tsx
@@ -4,6 +4,7 @@ import * as asyncEffectHooks from '#/hooks/asyncEffectHooks'
 import * as backendProvider from '#/providers/BackendProvider'
 import * as textProvider from '#/providers/TextProvider'
 
+import * as aria from '#/components/aria'
 import StatelessSpinner, * as statelessSpinner from '#/components/StatelessSpinner'
 
 /** A list of members in the organization. */
@@ -14,39 +15,51 @@ export default function MembersTable() {
   const isLoading = members == null
 
   return (
-    <table className="table-fixed self-start rounded-rows">
-      <thead>
-        <tr className="h-row">
-          <th className="w-members-name-column border-x-2 border-transparent bg-clip-padding px-cell-x text-left text-sm font-semibold last:border-r-0">
-            {getText('name')}
-          </th>
-          <th className="w-members-email-column border-x-2 border-transparent bg-clip-padding px-cell-x text-left text-sm font-semibold last:border-r-0">
-            {getText('email')}
-          </th>
-        </tr>
-      </thead>
-      <tbody className="select-text">
+    <aria.Table
+      aria-label={getText('users')}
+      selectionMode="multiple"
+      className="table-fixed self-start rounded-rows"
+    >
+      <aria.TableHeader className="h-row">
+        <aria.Column
+          isRowHeader
+          className="w-members-name-column border-x-2 border-transparent bg-clip-padding px-cell-x text-left text-sm font-semibold last:border-r-0"
+        >
+          {getText('name')}
+        </aria.Column>
+        <aria.Column className="w-members-email-column border-x-2 border-transparent bg-clip-padding px-cell-x text-left text-sm font-semibold last:border-r-0">
+          {getText('email')}
+        </aria.Column>
+      </aria.TableHeader>
+      <aria.TableBody className="select-text">
         {isLoading ? (
-          <tr className="h-row">
-            <td colSpan={2} className="rounded-full bg-transparent">
+          <aria.Row className="h-row">
+            <aria.Cell
+              ref={element => {
+                if (element != null) {
+                  element.colSpan = 2
+                }
+              }}
+              className="rounded-full bg-transparent"
+            >
               <div className="flex justify-center">
                 <StatelessSpinner size={32} state={statelessSpinner.SpinnerState.loadingMedium} />
               </div>
-            </td>
-          </tr>
+            </aria.Cell>
+          </aria.Row>
         ) : (
           members.map(member => (
-            <tr key={member.userId} className="h-row">
-              <td className="text border-x-2 border-transparent bg-clip-padding px-cell-x first:rounded-l-full last:rounded-r-full last:border-r-0">
+            <aria.Row key={member.userId} className="h-row">
+              <aria.Cell className="text border-x-2 border-transparent bg-clip-padding px-cell-x first:rounded-l-full last:rounded-r-full last:border-r-0">
                 {member.name}
-              </td>
-              <td className="text border-x-2 border-transparent bg-clip-padding px-cell-x first:rounded-l-full last:rounded-r-full last:border-r-0">
+              </aria.Cell>
+              <aria.Cell className="text border-x-2 border-transparent bg-clip-padding px-cell-x first:rounded-l-full last:rounded-r-full last:border-r-0">
                 {member.email}
-              </td>
-            </tr>
+              </aria.Cell>
+            </aria.Row>
           ))
         )}
-      </tbody>
-    </table>
+      </aria.TableBody>
+    </aria.Table>
   )
 }
--- app/ide-desktop/lib/dashboard/src/layouts/Settings/UserGroupsSettingsTab.tsx
+++ app/ide-desktop/lib/dashboard/src/layouts/Settings/UserGroupsSettingsTab.tsx
@@ -8,10 +8,11 @@ import * as backendProvider from '#/providers/BackendProvider'
 import * as modalProvider from '#/providers/ModalProvider'
 import * as textProvider from '#/providers/TextProvider'
 
+import MembersTable from '#/layouts/Settings/MembersTable'
+
 import * as aria from '#/components/aria'
 import StatelessSpinner, * as statelessSpinner from '#/components/StatelessSpinner'
 import HorizontalMenuBar from '#/components/styled/HorizontalMenuBar'
-import SettingsPage from '#/components/styled/settings/SettingsPage'
 import SettingsSection from '#/components/styled/settings/SettingsSection'
 import UnstyledButton from '#/components/UnstyledButton'
 
@@ -37,54 +38,59 @@ export default function MemberRolesSettingsTab() {
   const isLoading = userGroups == null || users == null
 
   return (
-    <SettingsPage>
-      <SettingsSection noFocusArea title={getText('userGroups')}>
-        <HorizontalMenuBar>
-          <UnstyledButton
-            className="flex h-row items-center rounded-full bg-frame px-new-project-button-x"
-            onPress={() => {
-              setModal(<NewUserGroupModal onSubmit={doRefresh} />)
-            }}
-          >
-            <aria.Text className="text whitespace-nowrap font-semibold">
-              {getText('newUserGroup')}
-            </aria.Text>
-          </UnstyledButton>
-        </HorizontalMenuBar>
-        <table className="table-fixed self-start rounded-rows">
-          <thead>
-            <tr className="h-row">
-              <th className="w-members-name-column border-x-2 border-transparent bg-clip-padding px-cell-x text-left text-sm font-semibold last:border-r-0">
-                {getText('userGroup')}
-              </th>
-            </tr>
-          </thead>
-          <tbody className="select-text">
-            {isLoading ? (
+    <div className="flex h flex-col gap-settings-section lg:h-auto lg:flex-row">
+      <div className="flex w-settings-main-section flex-col gap-settings-subsection">
+        <SettingsSection noFocusArea title={getText('userGroups')}>
+          <HorizontalMenuBar>
+            <UnstyledButton
+              className="flex h-row items-center rounded-full bg-frame px-new-project-button-x"
+              onPress={() => {
+                setModal(<NewUserGroupModal onSubmit={doRefresh} />)
+              }}
+            >
+              <aria.Text className="text whitespace-nowrap font-semibold">
+                {getText('newUserGroup')}
+              </aria.Text>
+            </UnstyledButton>
+          </HorizontalMenuBar>
+          <table className="table-fixed self-start rounded-rows">
+            <thead>
               <tr className="h-row">
-                <td colSpan={3} className="bg-transparent">
-                  <div className="flex justify-center">
-                    <StatelessSpinner
-                      size={32}
-                      state={statelessSpinner.SpinnerState.loadingMedium}
-                    />
-                  </div>
-                </td>
+                <th className="w-members-name-column border-x-2 border-transparent bg-clip-padding px-cell-x text-left text-sm font-semibold last:border-r-0">
+                  {getText('userGroup')}
+                </th>
               </tr>
-            ) : (
-              userGroups.map(userGroup => {
-                return (
-                  <tr key={userGroup.groupName} className="h-row">
-                    <td className="text border-x-2 border-transparent bg-clip-padding px-cell-x first:rounded-l-full last:rounded-r-full last:border-r-0">
-                      {userGroup.groupName}
-                    </td>
-                  </tr>
-                )
-              })
-            )}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="select-text">
+              {isLoading ? (
+                <tr className="h-row">
+                  <td colSpan={3} className="bg-transparent">
+                    <div className="flex justify-center">
+                      <StatelessSpinner
+                        size={32}
+                        state={statelessSpinner.SpinnerState.loadingMedium}
+                      />
+                    </div>
+                  </td>
+                </tr>
+              ) : (
+                userGroups.map(userGroup => {
+                  return (
+                    <tr key={userGroup.groupName} className="h-row">
+                      <td className="text border-x-2 border-transparent bg-clip-padding px-cell-x first:rounded-l-full last:rounded-r-full last:border-r-0">
+                        {userGroup.groupName}
+                      </td>
+                    </tr>
+                  )
+                })
+              )}
+            </tbody>
+          </table>
+        </SettingsSection>
+      </div>
+      <SettingsSection noFocusArea title={getText('users')}>
+        <MembersTable />
       </SettingsSection>
-    </SettingsPage>
+    </div>
   )
 }
