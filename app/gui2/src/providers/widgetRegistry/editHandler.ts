7b643f5f44dd83c93dd570535ac48510effd58da
Kaz <kaz@lambdaverse.org>
2024-04-10 05:29:25 -0700

    Review 2/2: rearrange and document

--- app/gui2/src/providers/widgetRegistry/editHandler.ts
+++ app/gui2/src/providers/widgetRegistry/editHandler.ts
@@ -9,164 +9,8 @@ import { ArgumentInfoKey } from '@/util/callTree'
 import { computed, markRaw, onBeforeUnmount, ref, shallowRef, type Ref } from 'vue'
 
 declare const brandWidgetId: unique symbol
+/** Uniquely identifies a widget type. */
 export type WidgetId = string & { [brandWidgetId]: true }
-
-type ResumeCallback = (() => void) | undefined
-type PortEditResumeData = Map<WidgetId, ResumeCallback>
-
-class SuspendedPortEdit implements Interaction {
-  constructor(
-    public readonly argId: string,
-    public readonly resumable: PortEditResumeData,
-  ) {}
-
-  pointerdown() {
-    return false
-  }
-
-  cancel() {}
-}
-
-class PortEditInteraction implements Interaction {
-  readonly portId: PortId
-  readonly argId: string | undefined
-  readonly active: Ref<boolean>
-  private readonly resumable: PortEditResumeData
-  private readonly interactions = new Array<PortEditSubinteraction>()
-
-  private constructor(
-    portId: PortId,
-    argId: string | undefined,
-    resumable: PortEditResumeData | undefined,
-    active: boolean,
-    private readonly interactionHandler = injectInteractionHandler(),
-  ) {
-    this.portId = portId
-    this.argId = argId
-    this.resumable = resumable ?? new Map()
-    this.active = ref(active)
-    this.interactionHandler = interactionHandler
-  }
-
-  static Start(
-    portId: PortId,
-    argId: string | undefined,
-    interactionHandler = injectInteractionHandler(),
-    resumable?: Map<WidgetId, ResumeCallback> | undefined,
-  ) {
-    const interaction = new PortEditInteraction(portId, argId, resumable, true, interactionHandler)
-    interactionHandler.setCurrent(interaction)
-    return interaction
-  }
-
-  pointerdown(event: PointerEvent, navigator: GraphNavigator): boolean | void {
-    for (const interaction of this.interactions) {
-      if (!interaction.pointerdown) continue
-      if (interaction.pointerdown(event, navigator) !== false) break
-    }
-    return false
-  }
-
-  cancel() {
-    for (const interaction of this.interactions) interaction.cancel()
-    this.interactions.length = 0
-    this.active.value = false
-    this.interactionHandler.end(this)
-  }
-
-  end(origin: WidgetId) {
-    for (const interaction of this.interactions) interaction.end?.(origin)
-    this.interactions.length = 0
-    this.active.value = false
-    this.interactionHandler.end(this)
-  }
-
-  register(interaction: PortEditSubinteraction) {
-    this.interactions.push(interaction)
-    this.resumable?.delete(interaction.widgetId)
-  }
-
-  suspend() {
-    if (this.argId === undefined) return
-    const suspendedWidgets = new Map(
-      this.interactions.map((interaction) => [
-        interaction.widgetId,
-        interaction.suspend?.().resume,
-      ]),
-    )
-    this.interactionHandler.setCurrent(new SuspendedPortEdit(this.argId, suspendedWidgets))
-  }
-
-  getResumeHook(widgetId: WidgetId) {
-    return this.resumable.has(widgetId) ? this.resumable.get(widgetId) ?? (() => {}) : undefined
-  }
-}
-
-interface Endable {
-  end?(origin: WidgetId): void
-}
-/** A child interaction of a @{link PortEditInteraction} */
-interface PortEditSubinteraction extends Interaction, Endable {
-  widgetId: WidgetId
-
-  suspend?: () => { resume: () => void }
-}
-
-export interface WidgetEditHooks extends Interaction, Endable {
-  start?(origin: WidgetId): void
-  /**
-   * Hook call when a child widget, or this widget itself, provides an updated value.
-   */
-  edit?(origin: WidgetId, value: Ast.Owned | string): void
-  /**
-   * Hook enabling a widget to provide a handler for the add-item intent of a child widget. The parent can return true
-   * to indicate that creating the new item has been handled and the child should not perform its action in this case.
-   */
-  addItem?(): boolean
-  /** Hook called when the edit is aborted because the component instance is about to be unmounted due to a change in
-   *  the graph.
-   *
-   *  In this case, if a successor is identified in the graph, the interaction will be restarted. If this hook is
-   *  implemented, the returned `resume` function will be called after starting an interaction for this reason. It may
-   *  use information captured at suspension time; however note that `resume` will be called in a different component
-   *  instance from `suspend`.
-   */
-  suspend?(): { resume: () => void }
-}
-
-/** @internal Public for unit testing.
- *  Obtains a top-level interaction to edit a port (see {@link PortEditInteraction}), which may be a pre-existing
- *  ongoing interaction, or a newly-started interaction. */
-export class PortEditInitiatorOrResumer {
-  constructor(
-    private readonly portId: PortId,
-    private readonly argId: string | undefined,
-    private readonly interactionHandler: InteractionHandler,
-  ) {}
-
-  start() {
-    const current = this.interactionHandler.getCurrent()
-    if (current instanceof PortEditInteraction && current.portId === this.portId) {
-      return current
-    } else {
-      return this.newEdit()
-    }
-  }
-
-  tryResume() {
-    if (this.argId == null) return
-    const current = this.interactionHandler.getCurrent()
-    if (current instanceof SuspendedPortEdit && this.argId === current.argId) {
-      return this.newEdit(current.resumable)
-    } else {
-      return
-    }
-  }
-
-  private newEdit(resumable?: PortEditResumeData) {
-    return PortEditInteraction.Start(this.portId, this.argId, this.interactionHandler, resumable)
-  }
-}
 
 /**
  * Widget edit handler.
@@ -302,5 +146,174 @@ export class WidgetEditHandler {
 
   addItem(): boolean {
     return (this.parent?.addItem() || this.hooks.addItem?.()) ?? false
+  }
+}
+
+/** Callbacks for {@link WidgetEditHandler} events */
+export interface WidgetEditHooks extends Interaction {
+  /**
+   * Hook called when this widget enters its active state, either due to the `start` method of its own
+   * {@link WidgetEditHandler} being called, or because a child is to be started.
+   */
+  start?(origin: WidgetId): void
+  end?(origin: WidgetId): void
+  /**
+   * Hook called when a child widget, or this widget itself, provides an updated value.
+   */
+  edit?(origin: WidgetId, value: Ast.Owned | string): void
+  /**
+   * Hook enabling a widget to provide a handler for the add-item intent of a child widget. The parent can return true
+   * to indicate that creating the new item has been handled and the child should not perform its action in this case.
+   */
+  addItem?(): boolean
+  /** Hook called when the edit is aborted because the component instance is about to be unmounted due to a change in
+   *  the graph.
+   *
+   *  In this case, if a successor is identified in the graph, the interaction will be restarted. If this hook is
+   *  implemented, the returned `resume` function will be called after starting an interaction for this reason. It may
+   *  use information captured at suspension time; however note that `resume` will be called in a different component
+   *  instance from `suspend`.
+   */
+  suspend?(): { resume: () => void }
+}
+
+type ResumeCallback = (() => void) | undefined
+type PortEditResumeData = Map<WidgetId, ResumeCallback>
+
+/** Top-level interaction active when a port is being edited by associated widgets.
+ *
+ *  `PortEditInteraction` contains sub-interactions for all active widgets, and distributes interaction events to them.
+ *
+ *  It manages *active state*, which can be started, stopped, suspended and resumed; this is separate from the static
+ *  parent/child chain of edit-handlers on a port, which is used for events such as `add-item` that may occur
+ *  independently of the active-states of widgets and follow a strict parent-child relationship.
+ */
+class PortEditInteraction implements Interaction {
+  readonly active: Ref<boolean>
+  private readonly resumable: PortEditResumeData
+  private readonly interactions = new Array<PortEditSubinteraction>()
+
+  private constructor(
+    public readonly portId: PortId,
+    public readonly argId: string | undefined,
+    resumable: PortEditResumeData | undefined,
+    active: boolean,
+    private readonly interactionHandler = injectInteractionHandler(),
+  ) {
+    this.resumable = resumable ?? new Map()
+    this.active = ref(active)
+  }
+
+  static Start(
+    portId: PortId,
+    argId: string | undefined,
+    interactionHandler = injectInteractionHandler(),
+    resumable?: Map<WidgetId, ResumeCallback> | undefined,
+  ) {
+    const interaction = new PortEditInteraction(portId, argId, resumable, true, interactionHandler)
+    interactionHandler.setCurrent(interaction)
+    return interaction
+  }
+
+  pointerdown(event: PointerEvent, navigator: GraphNavigator): boolean | void {
+    for (const interaction of this.interactions) {
+      if (!interaction.pointerdown) continue
+      if (interaction.pointerdown(event, navigator) !== false) break
+    }
+    return false
+  }
+
+  cancel() {
+    for (const interaction of this.interactions) interaction.cancel()
+    this.shutdown()
+  }
+
+  end(origin: WidgetId) {
+    for (const interaction of this.interactions) interaction.end?.(origin)
+    this.shutdown()
+  }
+
+  private shutdown() {
+    this.interactions.length = 0
+    this.active.value = false
+    this.interactionHandler.end(this)
+  }
+
+  register(interaction: PortEditSubinteraction) {
+    this.interactions.push(interaction)
+    this.resumable?.delete(interaction.widgetId)
+  }
+
+  suspend() {
+    if (this.argId === undefined) return
+    const suspendedWidgets = new Map(
+      this.interactions.map((interaction) => [
+        interaction.widgetId,
+        interaction.suspend?.().resume,
+      ]),
+    )
+    this.interactionHandler.setCurrent(new SuspendedPortEdit(this.argId, suspendedWidgets))
+  }
+
+  getResumeHook(widgetId: WidgetId) {
+    return this.resumable.has(widgetId) ? this.resumable.get(widgetId) ?? (() => {}) : undefined
+  }
+}
+
+/** A top-level interaction containing suspended state for {@PortEditSubinteractions} that were aborted when a
+ *  component was unmounted. This state may be used (when a successor for the original port is found) or discarded (if
+ *  the user starts a new top-level interaction, replacing this one). */
+class SuspendedPortEdit implements Interaction {
+  constructor(
+    public readonly argId: string,
+    public readonly resumable: PortEditResumeData,
+  ) {}
+
+  pointerdown() {
+    return false
+  }
+
+  cancel() {}
+}
+
+/** A sub-interaction of a @{link PortEditInteraction} */
+interface PortEditSubinteraction extends Interaction {
+  widgetId: WidgetId
+
+  suspend?: () => { resume: () => void }
+  end?(origin: WidgetId): void
+}
+
+/** @internal Public for unit testing.
+ *  Obtains a top-level interaction to edit a port (see {@link PortEditInteraction}), which may be a pre-existing
+ *  ongoing interaction, or a newly-started interaction. */
+export class PortEditInitiatorOrResumer {
+  constructor(
+    private readonly portId: PortId,
+    private readonly argId: string | undefined,
+    private readonly interactionHandler: InteractionHandler,
+  ) {}
+
+  start() {
+    const current = this.interactionHandler.getCurrent()
+    if (current instanceof PortEditInteraction && current.portId === this.portId) {
+      return current
+    } else {
+      return this.newEdit()
+    }
+  }
+
+  tryResume() {
+    if (this.argId == null) return
+    const current = this.interactionHandler.getCurrent()
+    if (current instanceof SuspendedPortEdit && this.argId === current.argId) {
+      return this.newEdit(current.resumable)
+    } else {
+      return
+    }
+  }
+
+  private newEdit(resumable?: PortEditResumeData) {
+    return PortEditInteraction.Start(this.portId, this.argId, this.interactionHandler, resumable)
   }
 }
