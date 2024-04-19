aef6e55e28bf50de02562b22c38fd2e3b3210037
Pavel Marek <pavel.marek@enso.org>
2024-04-19 12:33:23 +0200

    Fix PassesTest

--- engine/runtime-integration-tests/src/test/scala/org/enso/compiler/test/PassesTest.scala
+++ engine/runtime-integration-tests/src/test/scala/org/enso/compiler/test/PassesTest.scala
@@ -5,14 +5,7 @@ import org.enso.compiler.context.{InlineContext, ModuleContext}
 import org.enso.compiler.core.ir.Expression
 import org.enso.compiler.core.ir.Module
 import org.enso.compiler.pass.IRPass
-import org.enso.compiler.pass.analyse.{
-  AliasAnalysis,
-  AmbiguousImportsAnalysis,
-  BindingAnalysis,
-  ExportSymbolAnalysis,
-  ImportSymbolAnalysis,
-  PrivateModuleAnalysis
-}
+import org.enso.compiler.pass.analyse.{AliasAnalysis, AmbiguousImportsAnalysis, BindingAnalysis, ExportSymbolAnalysis, ImportSymbolAnalysis, PrivateConstructorAnalysis, PrivateModuleAnalysis}
 import org.enso.compiler.pass.desugar._
 import org.enso.compiler.pass.lint.{ModuleNameConflicts, ShadowedPatternFields}
 import org.enso.compiler.pass.optimise.UnreachableMatchBranches
@@ -63,6 +56,7 @@ class PassesTest extends CompilerTest {
           ImportSymbolAnalysis,
           AmbiguousImportsAnalysis,
           PrivateModuleAnalysis.INSTANCE,
+          PrivateConstructorAnalysis.INSTANCE,
           ExportSymbolAnalysis.INSTANCE,
           ShadowedPatternFields,
           UnreachableMatchBranches,
