ab24af3f2567a2ead2caa0d457d4c77bbb87c567
Dmitry Bushev <bushevdv@gmail.com>
2024-04-23 15:04:20 +0100

    misc: fmt

--- lib/java/ydoc-server/src/main/java/org/enso/ydoc/polyfill/web/EventEmitter.java
+++ lib/java/ydoc-server/src/main/java/org/enso/ydoc/polyfill/web/EventEmitter.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-
 import org.enso.ydoc.Polyfill;
 import org.enso.ydoc.polyfill.Arguments;
 import org.graalvm.polyglot.Context;
