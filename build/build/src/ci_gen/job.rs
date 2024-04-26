36fed13b14d4b1ed90be636105167e2fe60ad78a
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2024-04-26 19:01:29 +0200

    try to have things as they were before

--- build/build/src/ci_gen/job.rs
+++ build/build/src/ci_gen/job.rs
@@ -66,6 +66,7 @@ impl RunsOn for RunnerLabel {
             RunnerLabel::MacOS => Some("MacOS".to_string()),
             RunnerLabel::Linux => Some("Linux".to_string()),
             RunnerLabel::Windows => Some("Windows".to_string()),
+            RunnerLabel::MacOS12 => Some("MacOS12".to_string()),
             RunnerLabel::MacOS13 => Some("MacOS13".to_string()),
             RunnerLabel::MacOSLatest => Some("MacOSLatest".to_string()),
             RunnerLabel::LinuxLatest => Some("LinuxLatest".to_string()),
@@ -94,9 +95,13 @@ impl RunsOn for OS {
 impl RunsOn for (OS, Arch) {
     fn runs_on(&self) -> Vec<RunnerLabel> {
         match self {
-            (OS::MacOS, Arch::X86_64) => vec![RunnerLabel::MacOS13],
+            (OS::MacOS, Arch::X86_64) => vec![RunnerLabel::MacOS12],
             (os, Arch::X86_64) => runs_on(*os, RunnerType::SelfHosted),
-            (OS::MacOS, Arch::AArch64) => vec![RunnerLabel::MacOSLatest],
+            (OS::MacOS, Arch::AArch64) => {
+                let mut ret = runs_on(OS::MacOS, RunnerType::SelfHosted);
+                ret.push(RunnerLabel::Arm64);
+                ret
+            }
             _ => panic!("Unsupported OS/arch combination: {self:?}"),
         }
     }
--- build/ci_utils/src/actions/workflow/definition.rs
+++ build/ci_utils/src/actions/workflow/definition.rs
@@ -1014,6 +1014,8 @@ pub enum RunnerLabel {
     Windows,
     #[serde(rename = "engine")]
     Engine,
+    #[serde(rename = "macos-12")]
+    MacOS12,
     /// macos-13 is the last x64 version of the GitHub-hosted macOS runner.
     #[serde(rename = "macos-13")]
     MacOS13,
