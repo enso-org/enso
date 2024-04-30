280ac09731fc2ba53f34e6981195a87139a1ac84
Adam Riley <riley.ad@gmail.com>
2024-04-30 16:53:02 +0300

    Refactor

--- std-bits/table/src/main/java/org/enso/table/operations/AddRowNumber.java
+++ std-bits/table/src/main/java/org/enso/table/operations/AddRowNumber.java
@@ -20,212 +20,144 @@ import org.enso.table.util.ConstantList;
 
 public class AddRowNumber {
 
-  public static Storage<?> create_numbering(
-      long start,
-      long step,
-      Column[] groupingColumns,
-      Column[] orderingColumns,
-      int[] directions,
-      ProblemAggregator problemAggregator) {
-    if (orderingColumns.length != directions.length) {
-      throw new IllegalArgumentException(
-          "The number of ordering columns and directions must be the same.");
-    }
-    if (groupingColumns.length == 0) {
-      throw new IllegalArgumentException("At least one grouping column is required.");
-    }
-    var sourceColumn = groupingColumns[0];
-    var numberingStatistic = new NumberingStatistic(start, step, sourceColumn, problemAggregator);
-    RunningLooper.loop(
-        groupingColumns,
-        orderingColumns,
-        directions,
-        problemAggregator,
-        numberingStatistic,
-        sourceColumn.getSize());
-    return numberingStatistic.getResult();
-  }
-
-  private static class NumberingStatistic implements RunningStatistic<Long> {
-
-    private final long start;
-    private final long step;
-    long[] numbers;
-
-    NumberingStatistic(
-        long start, long step, Column sourceColumn, ProblemAggregator problemAggregator) {
-      this.start = start;
-      this.step = step;
-      int n = sourceColumn.getSize();
-      numbers = new long[n];
+    public static Storage<?> create_numbering(
+            long start,
+            long step,
+            Column[] groupingColumns,
+            Column[] orderingColumns,
+            int[] directions,
+            ProblemAggregator problemAggregator) {
+        if (orderingColumns.length != directions.length) {
+            throw new IllegalArgumentException(
+                    "The number of ordering columns and directions must be the same.");
+        }
+        if (groupingColumns.length == 0 && orderingColumns.length == 0) {
+            throw new IllegalArgumentException("At least one grouping or ordering column is required.");
+        }
+        var sourceColumn = groupingColumns.length > 0 ? groupingColumns[0] : orderingColumns[0];
+        var numberingStatistic = new NumberingStatistic(start, step, sourceColumn, problemAggregator);
+        RunningLooper.loop(
+                groupingColumns,
+                orderingColumns,
+                directions,
+                problemAggregator,
+                numberingStatistic,
+                sourceColumn.getSize());
+        return numberingStatistic.getResult();
     }
 
-    @Override
-    public RunningIterator<Long> getNewIterator() {
-      return new RangeIterator(start, step);
-    }
+    private static class NumberingStatistic implements RunningStatistic<Long> {
 
-    @Override
-    public void calculateNextValue(int i, RunningIterator<Long> it) {
-      numbers[i] = it.next(0l);
-    }
+        private final long start;
+        private final long step;
+        long[] numbers;
 
-    @Override
-    public Storage<Long> getResult() {
-      return new LongStorage(numbers, IntegerType.INT_64);
-    }
-
-    private static class RangeIterator implements RunningIterator<Long> {
-
-      private final long start;
-      private final long step;
-      private long current;
-      private boolean isFirst = true;
-
-      RangeIterator(long start, long step) {
-        this.start = start;
-        this.step = step;
-      }
-
-      @Override
-      public Long next(Long value) throws ArithmeticException {
-        if (isFirst) {
-          isFirst = false;
-          current = start;
-        } else {
-          current = Math.addExact(current, step);
+        NumberingStatistic(
+                long start, long step, Column sourceColumn, ProblemAggregator problemAggregator) {
+            this.start = start;
+            this.step = step;
+            int n = sourceColumn.getSize();
+            numbers = new long[n];
         }
 
-        return current;
-      }
+        @Override
+        public RunningIterator<Long> getNewIterator() {
+            return new RangeIterator(start, step);
+        }
 
-      @Override
-      public Long currentValue() {
-        return current;
-      }
-    }
-  }
+        @Override
+        public void calculateNextValue(int i, RunningIterator<Long> it) {
+            numbers[i] = it.next(0l);
+        }
 
-  public static LongStorage create_grouped_numbering(
-      long start, long step, Column[] groupingColumns, ProblemAggregator problemAggregator) {
-    if (groupingColumns.length == 0) {
-      throw new IllegalArgumentException("At least one grouping column is required.");
-    }
+        @Override
+        public Storage<Long> getResult() {
+            return new LongStorage(numbers, IntegerType.INT_64);
+        }
 
-    int n = groupingColumns[0].getSize();
-    long[] numbers = new long[n];
-    Storage<?>[] groupingStorages =
-        Arrays.stream(groupingColumns).map(Column::getStorage).toArray(Storage[]::new);
-    ColumnAggregatedProblemAggregator groupingProblemAggregator =
-        new ColumnAggregatedProblemAggregator(problemAggregator);
-    List<TextFoldingStrategy> textFoldingStrategy =
-        ConstantList.make(TextFoldingStrategy.unicodeNormalizedFold, groupingStorages.length);
-    Map<UnorderedMultiValueKey, RangeIterator> groups = new HashMap<>();
-    for (int i = 0; i < n; i++) {
-      UnorderedMultiValueKey key =
-          new UnorderedMultiValueKey(groupingStorages, i, textFoldingStrategy);
-      key.checkAndReportFloatingEquality(
-          groupingProblemAggregator, columnIx -> groupingColumns[columnIx].getName());
-      RangeIterator it = groups.computeIfAbsent(key, k -> new RangeIterator(start, step));
-      numbers[i] = it.next();
-    }
-    return new LongStorage(numbers, IntegerType.INT_64);
-  }
+        private static class RangeIterator implements RunningIterator<Long> {
 
-  public static LongStorage create_ordered_numbering(
-      long start, long step, Column[] orderingColumns, int[] directions) {
-    if (orderingColumns.length == 0) {
-      throw new IllegalArgumentException("At least one ordering column is required.");
-    }
-    if (orderingColumns.length != directions.length) {
-      throw new IllegalArgumentException(
-          "The number of ordering columns and directions must be the same.");
-    }
+            private final long start;
+            private final long step;
+            private long current;
+            private boolean isFirst = true;
 
-    int n = orderingColumns[0].getSize();
-    Storage<?>[] orderingStorages =
-        Arrays.stream(orderingColumns).map(Column::getStorage).toArray(Storage[]::new);
-    long[] numbers = new long[n];
-    List<OrderedMultiValueKey> keys =
-        new ArrayList<>(
-            IntStream.range(0, n)
-                .mapToObj(i -> new OrderedMultiValueKey(orderingStorages, i, directions))
-                .toList());
+            RangeIterator(long start, long step) {
+                this.start = start;
+                this.step = step;
+            }
 
-    keys.sort(null);
+            @Override
+            public Long next(Long value) throws ArithmeticException {
+                if (isFirst) {
+                    isFirst = false;
+                    current = start;
+                } else {
+                    current = Math.addExact(current, step);
+                }
 
-    RangeIterator it = new RangeIterator(start, step);
-    for (var key : keys) {
-      numbers[key.getRowIndex()] = it.next();
-    }
-    return new LongStorage(numbers, IntegerType.INT_64);
-  }
+                return current;
+            }
 
-  public static LongStorage create_grouped_ordered_numbering(
-      long start,
-      long step,
-      Column[] orderingColumns,
-      int[] directions,
-      Column[] groupingColumns,
-      ProblemAggregator problemAggregator) {
-    if (orderingColumns.length == 0) {
-      throw new IllegalArgumentException("At least one ordering column is required.");
-    }
-    if (orderingColumns.length != directions.length) {
-      throw new IllegalArgumentException(
-          "The number of ordering columns and directions must be the same.");
+            @Override
+            public Long currentValue() {
+                return current;
+            }
+        }
     }
 
-    int n = orderingColumns[0].getSize();
-    Storage<?>[] orderingStorages =
-        Arrays.stream(orderingColumns).map(Column::getStorage).toArray(Storage[]::new);
-    long[] numbers = new long[n];
-    MultiValueIndex<UnorderedMultiValueKey> groupIndex =
-        MultiValueIndex.makeUnorderedIndex(
-            groupingColumns, n, TextFoldingStrategy.unicodeNormalizedFold, problemAggregator);
+    public static LongStorage create_grouped_numbering(
+            long start, long step, Column[] groupingColumns, ProblemAggregator problemAggregator) {
+        if (groupingColumns.length == 0) {
+            throw new IllegalArgumentException("At least one grouping column is required.");
+        }
 
-    for (var entry : groupIndex.mapping().entrySet()) {
-      List<Integer> indices = entry.getValue();
-      List<OrderedMultiValueKey> orderingKeys =
-          new ArrayList<>(
-              indices.stream()
-                  .map(i -> new OrderedMultiValueKey(orderingStorages, i, directions))
-                  .toList());
-      orderingKeys.sort(null);
-      RangeIterator it = new RangeIterator(start, step);
-      for (OrderedMultiValueKey key : orderingKeys) {
-        numbers[key.getRowIndex()] = it.next();
-      }
+        int n = groupingColumns[0].getSize();
+        long[] numbers = new long[n];
+        Storage<?>[] groupingStorages
+                = Arrays.stream(groupingColumns).map(Column::getStorage).toArray(Storage[]::new);
+        ColumnAggregatedProblemAggregator groupingProblemAggregator
+                = new ColumnAggregatedProblemAggregator(problemAggregator);
+        List<TextFoldingStrategy> textFoldingStrategy
+                = ConstantList.make(TextFoldingStrategy.unicodeNormalizedFold, groupingStorages.length);
+        Map<UnorderedMultiValueKey, RangeIterator> groups = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            UnorderedMultiValueKey key
+                    = new UnorderedMultiValueKey(groupingStorages, i, textFoldingStrategy);
+            key.checkAndReportFloatingEquality(
+                    groupingProblemAggregator, columnIx -> groupingColumns[columnIx].getName());
+            RangeIterator it = groups.computeIfAbsent(key, k -> new RangeIterator(start, step));
+            numbers[i] = it.next();
+        }
+        return new LongStorage(numbers, IntegerType.INT_64);
     }
 
-    return new LongStorage(numbers, IntegerType.INT_64);
-  }
+    /**
+     * A helper for computing consecutive numbers based on a start and step. It
+     * will throw an {@link
+     * java.lang.ArithmeticException} if the next number overflows.
+     */
+    private static class RangeIterator {
 
-  /**
-   * A helper for computing consecutive numbers based on a start and step. It will throw an {@link
-   * java.lang.ArithmeticException} if the next number overflows.
-   */
-  private static class RangeIterator {
+        private final long start;
+        private final long step;
+        private long current;
+        private boolean isFirst = true;
 
-    private final long start;
-    private final long step;
-    private long current;
-    private boolean isFirst = true;
+        RangeIterator(long start, long step) {
+            this.start = start;
+            this.step = step;
+        }
 
-    RangeIterator(long start, long step) {
-      this.start = start;
-      this.step = step;
+        long next() throws ArithmeticException {
+            if (isFirst) {
+                isFirst = false;
+                current = start;
+            } else {
+                current = Math.addExact(current, step);
+            }
+
+            return current;
+        }
     }
-
-    long next() throws ArithmeticException {
-      if (isFirst) {
-        isFirst = false;
-        current = start;
-      } else {
-        current = Math.addExact(current, step);
-      }
-
-      return current;
-    }
-  }
 }
