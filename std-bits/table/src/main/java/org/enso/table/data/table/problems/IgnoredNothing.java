2e532204904f87a86ce11030257b0722343d40c5
Adam Riley <riley.ad@gmail.com>
2024-04-30 11:08:59 +0300

    Update

--- std-bits/table/src/main/java/org/enso/table/data/table/problems/IgnoredNothing.java
+++ std-bits/table/src/main/java/org/enso/table/data/table/problems/IgnoredNothing.java
@@ -2,18 +2,23 @@ package org.enso.table.data.table.problems;
 
 public class IgnoredNothing extends ColumnAggregatedProblem {
 
-  public IgnoredNothing(String locationName, Integer row) {
-    super(locationName, row);
-  }
-
-  @Override
-  public boolean merge(ColumnAggregatedProblem another) {
-    if (another instanceof IgnoredNothing
-        && this.getLocationName().equals(another.getLocationName())) {
-      this.rows.addAll(another.rows);
-      return true;
+    public IgnoredNothing(String locationName, Integer row) {
+        super(locationName, row);
     }
 
-    return false;
-  }
+    @Override
+    public boolean merge(ColumnAggregatedProblem another) {
+        if (another instanceof IgnoredNothing
+                && this.getLocationName().equals(another.getLocationName())) {
+            this.rows.addAll(another.rows);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String getMessage() {
+        return "The column " + getLocationName() + " contained Nothing values in rows " + makeTruncatedRowsString() + " which were ignored.";
+    }
 }
