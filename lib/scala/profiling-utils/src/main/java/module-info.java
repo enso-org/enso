e208a5e444def99967d2d9320ba6704b87ba1399
Pavel Marek <pavel.marek@enso.org>
2024-04-30 11:22:00 +0200

    Fix profiling-utils/compile

--- lib/scala/profiling-utils/src/main/java/module-info.java
+++ lib/scala/profiling-utils/src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module org.enso.profiling {
+  requires org.netbeans.modules.sampler.RELEASE180;
+  requires jdk.management;
   exports org.enso.profiling.sampler;
   exports org.enso.profiling.events;
 }
