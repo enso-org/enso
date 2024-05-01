30679815df490e420775e893166efb72f720939f
James Dunkerley <jdunkerley@users.noreply.github.com>
2024-05-01 16:13:33 +0100

    Remove `In_Memory` type and restructure `SQLite_Details`.

--- std-bits/database/src/main/java/org/enso/database/sqlite/SQLiteInMemoryDetailsSPI.java
+++ std-bits/database/src/main/java/org/enso/database/sqlite/SQLiteInMemoryDetailsSPI.java
@@ -3,7 +3,7 @@ package org.enso.database.sqlite;
 import org.enso.database.DatabaseConnectionDetailsSPI;
 
 @org.openide.util.lookup.ServiceProvider(service = DatabaseConnectionDetailsSPI.class)
-public class SQLiteConnectionDetailsSPI extends DatabaseConnectionDetailsSPI {
+public class SQLiteInMemoryDetailsSPI extends DatabaseConnectionDetailsSPI {
   @Override
   protected String getModuleName() {
     return "Standard.Database.Connection.SQLite";
@@ -16,7 +16,7 @@ public class SQLiteConnectionDetailsSPI extends DatabaseConnectionDetailsSPI {
 
   @Override
   protected String getCodeForDefaultConstructor() {
-    return "(SQLite.From_File location=_)";
+    return "SQLite.In_Memory";
   }
 
   @Override
