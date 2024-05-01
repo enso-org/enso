7d6d6a7729f636e161c7ca9b5b673f90eb7fcd26
Dmitry Bushev <bushevdv@gmail.com>
2024-05-01 12:41:35 +0100

    feat: sampling

--- lib/java/ydoc-server/src/main/java/org/enso/ydoc/Main.java
+++ lib/java/ydoc-server/src/main/java/org/enso/ydoc/Main.java
@@ -1,10 +1,7 @@
 package org.enso.ydoc;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
-import org.enso.profiling.sampler.OutputStreamSampler;
 import org.enso.ydoc.polyfill.ParserPolyfill;
 import org.enso.ydoc.polyfill.web.WebEnvironment;
 import org.graalvm.polyglot.Source;
@@ -13,25 +10,14 @@ import org.graalvm.polyglot.io.IOAccess;
 public class Main {
 
   private static final String YDOC_SERVER_PATH = "/dist/assets/ydocServer.js";
-  private static final String SAMPLES_PATH = "/tmp/ydoc-server.out";
 
   private Main() {}
 
   public static void main(String[] args) throws Exception {
     var ydoc = Main.class.getResource(YDOC_SERVER_PATH);
     var contextBuilder = WebEnvironment.createContext().allowIO(IOAccess.ALL);
-    var sampler = OutputStreamSampler.ofFile(new File(SAMPLES_PATH));
-    sampler.start();
-    Runtime.getRuntime()
-        .addShutdownHook(
-            new Thread(
-                () -> {
-                  try {
-                    sampler.stop();
-                  } catch (IOException e) {
-                    throw new RuntimeException(e);
-                  }
-                }));
+
+    Sampling.init();
 
     try (var executor = Executors.newSingleThreadExecutor();
         var parser = new ParserPolyfill()) {
