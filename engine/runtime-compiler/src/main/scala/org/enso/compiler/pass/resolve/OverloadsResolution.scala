89bb7d6b3870380c8650e0961f96fbbc640dddad
Pavel Marek <pavel.marek@enso.org>
2024-05-03 11:46:32 +0200

    OverloadResolution compiler pass checks redefinition of static methods from imported modules.

--- engine/runtime-compiler/src/main/scala/org/enso/compiler/pass/resolve/OverloadsResolution.scala
+++ engine/runtime-compiler/src/main/scala/org/enso/compiler/pass/resolve/OverloadsResolution.scala
@@ -2,10 +2,11 @@ package org.enso.compiler.pass.resolve
 
 import org.enso.compiler.context.{InlineContext, ModuleContext}
 import org.enso.compiler.core.ir.{Diagnostic, Expression, Module, Name, Type}
-import org.enso.compiler.core.ir.expression.{errors, Comment}
+import org.enso.compiler.core.ir.expression.{Comment, errors}
 import org.enso.compiler.core.ir.module.scope.Definition
 import org.enso.compiler.core.ir.module.scope.definition
 import org.enso.compiler.core.CompilerError
+import org.enso.compiler.data.BindingsMap.ModuleReference
 import org.enso.compiler.pass.IRPass
 import org.enso.compiler.pass.desugar.{ComplexType, GenerateMethodBodies}
 
@@ -21,8 +22,8 @@ import scala.collection.mutable
   * 2. Extension methods in the current scope are resolved next.
   * 3. Extension methods in imported scopes are resolved last.
   *
-  * This means that it is possible to shadow an imported extension method with
-  * one defined locally, and this does not count as an overload.
+  * It is not possible to define two extension methods with same name in different
+  * location - that results in an ambiguity error.
   *
   * This pass requires the context to provide:
   *
@@ -51,6 +52,8 @@ case object OverloadsResolution extends IRPass {
     moduleContext: ModuleContext
   ): Module = {
     var seenTypes: Set[String]                                   = Set()
+    // Key is an optional name of a type, values are set of tuples (method name, isStatic)
+    // seenMethods are only the methods defined in the current module.
     var seenMethods: Map[Option[String], Set[(String, Boolean)]] = Map()
 
     val types = ir.bindings.collect { case tp: Definition.Type =>
@@ -65,10 +68,11 @@ case object OverloadsResolution extends IRPass {
 
     val newBindings = ir.bindings.map {
       case tp: Definition.Type =>
-        if (seenTypes.contains(tp.name.name)) {
+        val typeName = tp.name.name
+        if (seenTypes.contains(typeName)) {
           errors.Redefined.Type(tp.name, tp.location)
         } else {
-          seenTypes += tp.name.name
+          seenTypes += typeName
           tp
         }
 
@@ -88,11 +92,20 @@ case object OverloadsResolution extends IRPass {
                 method.location
               )
             case _ =>
-              val currentMethods: Set[(String, Boolean)] =
-                seenMethods(method.typeName.map(_.name))
-              seenMethods += (method.typeName.map(_.name) ->
-              (currentMethods + ((method.methodName.name, method.isStatic))))
-              method
+              // Also check if some of the imported modules define the same method.
+              // Note that we are not checking instance methods here on purpose. If `method`
+              // is an instance method, it means that it is inside a type definition. And
+              // types can be shadowed. Therefore, instance methods cannot be ambiguous.
+              if (isExtensionOrStatic(method) && isExtensionMethodDefinedInImportedModule(method, moduleContext)) {
+                errors.Redefined
+                  .Method(method.typeName, method.methodName, method.location)
+              } else {
+                val currentMethods: Set[(String, Boolean)] =
+                  seenMethods(method.typeName.map(_.name))
+                seenMethods += (method.typeName.map(_.name) ->
+                  (currentMethods + ((method.methodName.name, method.isStatic))))
+                method
+              }
           }
         }
 
@@ -154,6 +167,58 @@ case object OverloadsResolution extends IRPass {
     inlineContext: InlineContext
   ): Expression = ir
 
-  /** @inheritdoc */
+  /**
+   * Returns true if the given extension or static `method` is already defined in one of
+   * the imported modules. That should result in an ambiguity error.
+   * @param method Static or extension method to check for redefinition.
+   * @param moduleContext Current module context. Imported modules are taken from this context.
+   * @return True if the method is already defined in one of the imported modules.
+   */
+  private def isExtensionMethodDefinedInImportedModule(
+    method: definition.Method.Explicit,
+    moduleContext: ModuleContext
+  ): Boolean = {
+    assert (method.isStatic)
+    assert (method.typeName.isDefined)
+    val typeName = method.typeName.get.name
+    val methodName = method.methodName.name
+    val extMethod = StaticMethod(typeName, methodName)
+    val bindingMap = moduleContext.bindingsAnalysis()
+    val impTargets = bindingMap.resolvedImports.map(_.target)
+    impTargets.exists { impTarget =>
+      impTarget.module match {
+        case ModuleReference.Concrete(impModule) =>
+          val importedExtMethods = collectStaticMethods(impModule.getIr.bindings)
+          importedExtMethods.contains(extMethod)
+        case ModuleReference.Abstract(_) =>
+          // ModuleReference might not be resolved. In that case, we just return false
+          false
+      }
+    }
+  }
 
+  private def isExtensionOrStatic(
+    methodDef: definition.Method.Explicit
+  ): Boolean = {
+    methodDef.isStatic && methodDef.typeName.isDefined
+  }
+
+  private def collectStaticMethods(
+    bindings: List[Definition]
+  ): List[StaticMethod] = {
+    bindings.flatMap { binding =>
+      binding match {
+        case method: definition.Method.Explicit if method.isStatic =>
+          method.typeName.map { typeName =>
+            StaticMethod(typeName.name, method.methodName.name)
+          }
+        case _ => None
+      }
+    }
+  }
+
+  private case class StaticMethod(
+    typeName: String,
+    methodName: String
+  )
 }
