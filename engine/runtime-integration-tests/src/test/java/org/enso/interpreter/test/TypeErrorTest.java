343a2f116d96209148f919e7ec313a786a8a4203
Pavel Marek <pavel.marek@enso.org>
2024-05-03 18:34:51 +0200

    Add TypeErrorTest for testing typeErrorMessage

---
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/TypeErrorTest.java
@@ -0,0 +1,61 @@
+package org.enso.interpreter.test;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.IOException;
+import java.util.Set;
+import org.enso.pkg.QualifiedName;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class TypeErrorTest extends TestBase {
+  @Rule public TemporaryFolder tempFolder = new TemporaryFolder();
+
+  @Test
+  public void typeErrorMessageContainsQualifiedNames() throws IOException {
+    var modSrc = """
+        type T
+            Ctor
+
+        make_t = T.Ctor
+        """;
+    var mainSrc =
+        """
+        from Standard.Base import all
+        from project.Data.Mod import T
+        from project.Data.Mod import make_t
+
+        type T
+
+        method (t:T) = t.baz
+
+        main =
+            t = make_t
+            p = Panic.catch Any handler=(_.payload) <| method t
+            p.to_text
+        """;
+    var projDir =
+        createProject(
+            "Proj",
+            Set.of(
+                new SourceModule(QualifiedName.fromString("Main"), mainSrc),
+                new SourceModule(QualifiedName.fromString("Data.Mod"), modSrc)),
+            tempFolder);
+    testProjectRun(
+        projDir,
+        res -> {
+          assertThat(res.isString(), is(true));
+          assertThat(
+              res.asString(),
+              allOf(
+                  containsString("Type error"),
+                  containsString("expected `t` to be"),
+                  containsString("Data.Mod.T"),
+                  containsString("Main.T")));
+        });
+  }
+}
