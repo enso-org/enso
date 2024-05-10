3fd58ecfccadbfba4d819c2d75a15191680bde17
Sergey Garin <sergey.garin@cube.dev>
2024-05-10 12:03:18 +0300

    add transitions to tooltips

--- app/ide-desktop/lib/dashboard/src/components/AriaComponents/Tooltip/Tooltip.tsx
+++ app/ide-desktop/lib/dashboard/src/components/AriaComponents/Tooltip/Tooltip.tsx
@@ -9,9 +9,16 @@ import * as portal from '#/components/Portal'
 // =================
 
 const DEFAULT_CLASSES =
-  'flex bg-frame outline outline-2 outline-primary backdrop-blur-default text-primary px-2 leading-cozy min-h-6 rounded-default shadow-soft text-xs'
+  'group flex bg-frame justify-center outline outline-2 outline-primary/70 backdrop-blur-default text-primary px-2 leading-cozy min-h-6 rounded-default shadow-soft text-xs opacity-1'
 const DEFAULT_CONTAINER_PADDING = 4
 const DEFAULT_OFFSET = 4
+
+const TRANSITIONS_BY_STATE: Record<'entering' | 'exiting', string> = {
+  entering:
+    'animate-in fade-in placement-bottom:slide-in-from-top-0.5 placement-top:slide-in-from-bottom-0.5 placement-left:slide-in-from-right-0.5 placement-right:slide-in-from-left-0.5 ease-out duration-200',
+  exiting:
+    'animate-out fade-out placement-bottom:slide-out-to-top-0.5 placement-top:slide-out-to-bottom-0.5 placement-left:slide-out-to-right-0.5 placement-right:slide-out-to-left-0.5 ease-in duration-150',
+}
 
 // ===============
 // === Tooltip ===
@@ -25,19 +32,20 @@ export interface TooltipProps
 export function Tooltip(props: TooltipProps) {
   const { className, containerPadding = DEFAULT_CONTAINER_PADDING, ...ariaTooltipProps } = props
   const root = portal.useStrictPortalContext()
-  const classes = tailwindMerge.twJoin(DEFAULT_CLASSES)
 
   return (
     <aria.Tooltip
       offset={DEFAULT_OFFSET}
       containerPadding={containerPadding}
       UNSTABLE_portalContainer={root.current}
-      className={values =>
-        tailwindMerge.twMerge(
-          classes,
+      className={values => {
+        return tailwindMerge.twMerge(
+          DEFAULT_CLASSES,
+          values.isEntering && TRANSITIONS_BY_STATE.entering,
+          values.isExiting && TRANSITIONS_BY_STATE.exiting,
           typeof className === 'function' ? className(values) : className
         )
-      }
+      }}
       {...ariaTooltipProps}
     />
   )
