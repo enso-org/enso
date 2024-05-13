24d5502cbd18363470bc14e70be5c8c143b36bf7
Dmitry Bushev <bushevdv@gmail.com>
2024-05-13 18:09:35 +0100

    fix: fmt

--- engine/runtime/src/main/java/org/enso/ClassLoaderConstants.java
+++ engine/runtime/src/main/java/org/enso/ClassLoaderConstants.java
@@ -15,7 +15,8 @@ public class ClassLoaderConstants {
   public static final List<String> CLASS_DELEGATION_PATTERNS =
       List.of("org.graalvm", "java", "org.slf4j", "ch.qos", "io.helidon");
 
-  public static final List<String> RESOURCE_DELEGATION_PATTERNS = List.of("org.slf4j", "ch.qos", "io.helidon");
+  public static final List<String> RESOURCE_DELEGATION_PATTERNS =
+      List.of("org.slf4j", "ch.qos", "io.helidon");
 
   /**
    * Path to the {@code runner.jar} fat jar. This must not be on the system's module-path, because
