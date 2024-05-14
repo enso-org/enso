794784b0a6ffc26350cef6140b0662ddfba4eb4f
Paweł Grabarz <frizi09@gmail.com>
2024-05-14 15:54:52 +0200

    revert accidental import rename

--- app/ide-desktop/lib/dashboard/src/providers/LoggerProvider.tsx
+++ app/ide-desktop/lib/dashboard/src/providers/LoggerProvider.tsx
@@ -1,6 +1,6 @@
 /** @file The React provider for the {@link Logger} interface, along with a hook to use the
  * provider via the shared React context. */
-import * as loggerProvider from 'react'
+import * as React from 'react'
 
 // ==============
 // === Logger ===
@@ -23,7 +23,7 @@ export interface Logger {
 
 /** See `AuthContext` for safety details. */
 // eslint-disable-next-line no-restricted-syntax
-const LoggerContext = loggerProvider.createContext<Logger>({} as Logger)
+const LoggerContext = React.createContext<Logger>({} as Logger)
 
 // ======================
 // === LoggerProvider ===
@@ -31,7 +31,7 @@ const LoggerContext = loggerProvider.createContext<Logger>({} as Logger)
 
 /** Props for a {@link LoggerProvider}. */
 export interface LoggerProviderProps {
-  readonly children: loggerProvider.ReactNode
+  readonly children: React.ReactNode
   readonly logger: Logger
 }
 
@@ -47,5 +47,5 @@ export default function LoggerProvider(props: LoggerProviderProps) {
 
 /** A React context hook exposing the diagnostic logger. */
 export function useLogger() {
-  return loggerProvider.useContext(LoggerContext)
+  return React.useContext(LoggerContext)
 }
