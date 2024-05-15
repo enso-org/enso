32bad7ed54bcf9b7abd67d6593fc2bc46f8c19b1
Dmitry Bushev <bushevdv@gmail.com>
2024-05-15 17:38:34 +0100

    DRAFT: workflow install-command

--- build/ci_utils/src/actions/workflow/definition.rs
+++ build/ci_utils/src/actions/workflow/definition.rs
@@ -99,7 +99,10 @@ pub fn npm_install_step() -> Step {
     Step {
         name: Some("Run npm install".into()),
         uses: Some("bahmutov/npm-install@v1".into()),
-        with: Some(step::Argument::new_other("working-directory", "app/gui2")),
+        with: Some(step::Argument::new_other(
+            "install-command",
+            "npm --workspace=enso-gui2 install",
+        )),
         ..default()
     }
 }
