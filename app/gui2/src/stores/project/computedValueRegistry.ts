5a23e990d4500d3e2da5c0939cd3a9e0b56fa93c
Ilya Bogdanov <fumlead@gmail.com>
2024-05-15 23:40:40 +0400

    Restore changes to computedValueRegistry

--- app/gui2/src/stores/project/computedValueRegistry.ts
+++ app/gui2/src/stores/project/computedValueRegistry.ts
@@ -44,7 +44,8 @@ export class ComputedValueRegistry {
   processUpdates(updates: ExpressionUpdate[]) {
     for (const update of updates) {
       const info = this.db.get(update.expressionId)
-      this.db.set(update.expressionId, combineInfo(info, update))
+      if (info) updateInfo(info, update)
+      else this.db.set(update.expressionId, combineInfo(undefined, update))
     }
   }
 
@@ -55,6 +56,14 @@ export class ComputedValueRegistry {
   dispose() {
     this.executionContext?.off('expressionUpdates', this._updateHandler)
   }
+}
+
+function updateInfo(info: ExpressionInfo, update: ExpressionUpdate) {
+  const newInfo = combineInfo(info, update)
+  if (newInfo.typename !== info.typename) info.typename = newInfo.typename
+  if (newInfo.methodCall !== info.methodCall) info.methodCall = newInfo.methodCall
+  if (newInfo.payload !== info.payload) info.payload = newInfo.payload
+  if (newInfo.profilingInfo !== info.profilingInfo) info.profilingInfo = update.profilingInfo
 }
 
 function combineInfo(info: ExpressionInfo | undefined, update: ExpressionUpdate): ExpressionInfo {
