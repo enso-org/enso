92041a1983082785b371aec2fd5d9570793ccf01
somebody1234 <ehern.lee@gmail.com>
2024-05-15 19:13:09 +1000

    Address code review

--- app/ide-desktop/lib/dashboard/src/modals/LogsModal.tsx
+++ app/ide-desktop/lib/dashboard/src/modals/LogsModal.tsx
@@ -1,10 +1,14 @@
 /** @file A modal for showing logs for a project. */
 import * as React from 'react'
 
+import * as reactQuery from '@tanstack/react-query'
+
 import * as textProvider from '#/providers/TextProvider'
 
 import * as aria from '#/components/aria'
-import Modal from '#/components/Modal'
+import * as ariaComponents from '#/components/AriaComponents'
+
+import * as uniqueString from '#/utilities/uniqueString'
 
 // =================
 // === LogsModal ===
@@ -18,33 +22,28 @@ export interface LogsModalProps {
 /** A modal for showing logs for a project. */
 export default function LogsModal(props: LogsModalProps) {
   const { logs: logsRaw } = props
-  const [logs, setLogs] = React.useState(() => (logsRaw instanceof Promise ? null : logsRaw))
   const { getText } = textProvider.useText()
-
-  React.useEffect(() => {
-    if (logsRaw instanceof Promise) {
-      void logsRaw.then(newLogs => {
-        setLogs(newLogs)
-      })
-    }
-  }, [logsRaw])
+  const logsQuery = reactQuery.useQuery({
+    queryKey: ['logs', uniqueString.uniqueString()],
+    queryFn: () => logsRaw,
+  })
 
   return (
-    <Modal centered className="bg-dim">
+    <ariaComponents.Dialog type="fullscreen" className="bg-dim">
       <div
         tabIndex={-1}
         className="pointer-events-auto relative flex h-4/5 w-4/5 flex-col gap-4 overflow-auto rounded-default p-4 before:absolute before:inset before:h-full before:w-full before:rounded-default before:bg-selected-frame before:backdrop-blur-3xl"
-        onClick={() => {
-          event?.stopPropagation()
+        onClick={event => {
+          event.stopPropagation()
         }}
       >
         <aria.Heading level={2} className="relative text-lg font-bold">
           {getText('logs')}
         </aria.Heading>
         <pre className="relative overflow-auto whitespace-pre-wrap">
-          <code>{logs}</code>
+          <code>{logsQuery.data ?? ''}</code>
         </pre>
       </div>
-    </Modal>
+    </ariaComponents.Dialog>
   )
 }
