e70f53adf49bc3964e13997318f210c0ec251e3e
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-05-19 15:25:41 +0200

    Print out summary in EnsoLibraryFeature

--- engine/runner/src/main/java/org/enso/runner/EnsoLibraryFeature.java
+++ engine/runner/src/main/java/org/enso/runner/EnsoLibraryFeature.java
@@ -5,13 +5,13 @@ import static scala.jdk.javaapi.CollectionConverters.asJava;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.LinkedHashSet;
+import java.util.TreeSet;
 import org.enso.compiler.core.ir.module.scope.imports.Polyglot;
 import org.enso.pkg.PackageManager$;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeReflection;
 
 public final class EnsoLibraryFeature implements Feature {
-
   @Override
   public void beforeAnalysis(BeforeAnalysisAccess access) {
     var libs = new LinkedHashSet<Path>();
@@ -27,6 +27,7 @@ public final class EnsoLibraryFeature implements Feature {
       }
     }
 
+    var classes = new TreeSet<String>();
     try (var parser = new org.enso.compiler.core.EnsoParser()) {
       for (var p : libs) {
         var result = PackageManager$.MODULE$.Default().loadPackage(p.toFile());
@@ -50,7 +51,11 @@ public final class EnsoLibraryFeature implements Feature {
                   }
                   name.setCharAt(at, '$');
                 }
-
+                classes.add(clazz.getName());
+                RuntimeReflection.register(clazz);
+                RuntimeReflection.register(clazz.getConstructors());
+                RuntimeReflection.register(clazz.getMethods());
+                RuntimeReflection.register(clazz.getFields());
                 RuntimeReflection.registerAllConstructors(clazz);
                 RuntimeReflection.registerAllFields(clazz);
                 RuntimeReflection.registerAllMethods(clazz);
@@ -63,5 +68,10 @@ public final class EnsoLibraryFeature implements Feature {
       ex.printStackTrace();
       throw new IllegalStateException(ex);
     }
+    System.err.println("Summary for polyglot import java:");
+    for (var className : classes) {
+      System.err.println("  " + className);
+    }
+    System.err.println("Registered " + classes.size() + " classes for reflection");
   }
 }
