77149060d9634b96d47e626ea773ee755d088840
Sergey Garin <sergey.garin@cube.dev>
2024-05-19 21:51:16 +0300

    fix npm problems

--- app/ide-desktop/lib/dashboard/src/ReactQueryDevtools.tsx
+++ app/ide-desktop/lib/dashboard/src/ReactQueryDevtools.tsx
@@ -5,6 +5,7 @@
  */
 import * as React from 'react'
 
+import * as reactQuery from '@tanstack/react-query'
 import * as reactQueryDevtools from '@tanstack/react-query-devtools'
 
 const ReactQueryDevtoolsProduction = React.lazy(() =>
@@ -19,6 +20,11 @@ const ReactQueryDevtoolsProduction = React.lazy(() =>
  */
 export function ReactQueryDevtools() {
   const [showDevtools, setShowDevtools] = React.useState(false)
+  // It's safitier to pass the client directly to the devtools
+  // Since there might be a chance that we might have multiple versions if react-query
+  // in case if we forgot to update the devtools or if npm messed up with the versions
+  // or hoising issues.
+  const client = reactQuery.useQueryClient()
 
   React.useEffect(() => {
     window.toggleDevtools = () => {
@@ -28,11 +34,11 @@ export function ReactQueryDevtools() {
 
   return (
     <>
-      <reactQueryDevtools.ReactQueryDevtools />
+      <reactQueryDevtools.ReactQueryDevtools client={client} />
 
       {showDevtools && (
         <React.Suspense fallback={null}>
-          <ReactQueryDevtoolsProduction />
+          <ReactQueryDevtoolsProduction client={client} />
         </React.Suspense>
       )}
     </>
