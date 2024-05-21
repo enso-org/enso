23c95f6677adc489329aa01ff9bfef2a5b265964
Kaz <kaz@lambdaverse.org>
2024-05-21 09:30:59 -0700

    More tests

--- app/gui2/src/composables/__tests__/nodeCreation.test.ts
+++ app/gui2/src/composables/__tests__/nodeCreation.test.ts
@@ -1,5 +1,6 @@
 import { insertNodeStatements } from '@/composables/nodeCreation'
 import { Ast } from '@/util/ast'
+import { identifier } from 'shared/ast'
 import { initializeFFI } from 'shared/ast/ffi'
 import { expect, test } from 'vitest'
 
@@ -13,6 +14,7 @@ test.each([
   ['', '*', 'node1'],
   ['*', '## Return value', 'node1'],
   ['*', '## Return value', '', 'node1'],
+  ['*', '## Block ends in documentation?!'],
 ])('New node location in block', (...linesWithInsertionPoint: string[]) => {
   const inputLines = linesWithInsertionPoint.filter((line) => line !== '*')
   const bodyBlock = Ast.parseBlock(inputLines.join('\n'))
@@ -22,4 +24,20 @@ test.each([
     .split('\n')
     .map((line) => (line === 'newNodePositionMarker' ? '*' : line))
   expect(lines).toEqual(linesWithInsertionPoint)
+})
+
+// This is a special case because when a block is empty, adding a line requires adding *two* linebreaks.
+test('Adding node to empty block', () => {
+  const module = Ast.MutableModule.Transient()
+  const func = Ast.Function.fromStatements(module, identifier('f')!, [], [])
+  const rootBlock = Ast.BodyBlock.new([], module)
+  rootBlock.push(func)
+  expect(rootBlock.code().trimEnd()).toBe('f =')
+  insertNodeStatements(func.bodyAsBlock(), [Ast.parse('newNode')])
+  expect(
+    rootBlock
+      .code()
+      .split('\n')
+      .map((line) => line.trimEnd()),
+  ).toEqual(['f =', '    newNode'])
 })
