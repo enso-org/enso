6f494ccfa85d8a741b93e59af7f0fb7c2d3df004
Martha King <martha_king@hotmail.co.uk>
2024-05-22 15:04:44 +0100

    right align numbers and group when large numbers are in dataset

--- app/gui2/src/components/visualizations/TableVisualization.vue
+++ app/gui2/src/components/visualizations/TableVisualization.vue
@@ -3,7 +3,7 @@ import { useAutoBlur } from '@/util/autoBlur'
 import { VisualizationContainer } from '@/util/visualizationBuiltins'
 import '@ag-grid-community/styles/ag-grid.css'
 import '@ag-grid-community/styles/ag-theme-alpine.css'
-import type { ColumnResizedEvent, ICellRendererParams } from 'ag-grid-community'
+import type { ColumnResizedEvent, ICellRendererParams, CellClassParams } from 'ag-grid-community'
 import type { ColDef, GridOptions, HeaderValueGetterParams } from 'ag-grid-enterprise'
 import { computed, onMounted, onUnmounted, reactive, ref, watchEffect, type Ref } from 'vue'
 
@@ -105,6 +105,7 @@ const defaultColDef = {
   minWidth: 25,
   headerValueGetter: (params: HeaderValueGetterParams) => params.colDef.field,
   cellRenderer: cellRenderer,
+  cellClass: cellClass
 }
 const agGridOptions: Ref<GridOptions & Required<Pick<GridOptions, 'defaultColDef'>>> = ref({
   headerHeight: 26,
@@ -135,11 +136,25 @@ const selectableRowLimits = computed(() => {
 })
 const wasAutomaticallyAutosized = ref(false)
 
-const numberFormat = new Intl.NumberFormat(undefined, {
-  style: 'decimal',
-  maximumFractionDigits: 12,
-  useGrouping: 'min2' as any,
-})
+function isGrouingRequired(params: ICellRendererParams){
+  const field = params.colDef?.field;
+  const allDataForCol = params.node.parent?.allLeafChildren.map(row => {
+    const data = row.data[field || '']
+    return data?.type === "BigInt" ? data.value : data
+  })
+  return allDataForCol?.find(num => num > 9999) != null;
+}
+
+function formatNumber(params: ICellRendererParams) {
+  const isGrouingRequiredForData = isGrouingRequired(params);
+  const valueType = params.value?.type
+  const value = valueType === 'BigInt' ? BigInt(params.value?.value) :  params.value;
+  return new Intl.NumberFormat(undefined, {
+    style: 'decimal',
+    maximumFractionDigits: 12,
+    useGrouping: isGrouingRequiredForData,
+  }).format(value)
+}
 
 function setRowLimit(newRowLimit: number) {
   if (newRowLimit !== rowLimit.value) {
@@ -164,12 +179,22 @@ function escapeHTML(str: string) {
   return str.replace(/[&<>"']/g, (m) => mapping[m]!)
 }
 
+function cellClass(params: CellClassParams) {
+  if(params.colDef.field != '#') return null;
+  if(typeof params.value === 'number' ||params.value === null) return 'ag-right-aligned-cell' 
+  if (typeof params.value === 'object') {
+    const valueType = params.value?.type
+    if (valueType === 'BigInt' || valueType === 'Float') return 'ag-right-aligned-cell'
+  }
+  return null;
+}
+
 function cellRenderer(params: ICellRendererParams) {
   // Convert's the value into a display string.
   if (params.value === null) return '<span style="color:grey; font-style: italic;">Nothing</span>'
   else if (params.value === undefined) return ''
   else if (params.value === '') return '<span style="color:grey; font-style: italic;">Empty</span>'
-  else if (typeof params.value === 'number') return numberFormat.format(params.value)
+  else if (typeof params.value === 'number') return formatNumber(params)
   else if (Array.isArray(params.value)) {
     const content = params.value
     if (isMatrix({ json: content })) {
@@ -181,8 +206,8 @@ function cellRenderer(params: ICellRendererParams) {
     }
   } else if (typeof params.value === 'object') {
     const valueType = params.value?.type
-    if (valueType === 'BigInt') return numberFormat.format(BigInt(params.value?.value))
-    else if (valueType === 'Float')
+  if (valueType === 'BigInt') return formatNumber(params)
+  else if (valueType === 'Float')
       return `<span style="color:grey; font-style: italic;">${params.value?.value ?? 'Unknown'}</span>`
     else if ('_display_text_' in params.value && params.value['_display_text_'])
       return String(params.value['_display_text_'])
