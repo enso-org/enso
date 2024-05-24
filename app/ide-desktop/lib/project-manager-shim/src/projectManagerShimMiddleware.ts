b07e6a200c6962d752722c907927fa1b1984e119
Dmitry Bushev <bushevdv@gmail.com>
2024-05-24 15:08:44 +0100

    misc: lint

--- app/ide-desktop/lib/project-manager-shim/src/projectManagerShimMiddleware.ts
+++ app/ide-desktop/lib/project-manager-shim/src/projectManagerShimMiddleware.ts
@@ -6,7 +6,7 @@ import * as http from 'node:http'
 import * as os from 'node:os'
 import * as path from 'node:path'
 
-import { isHiddenFile } from 'is-hidden-file'
+import * as isHiddenFile from 'is-hidden-file'
 import * as tar from 'tar'
 import * as yaml from 'yaml'
 
@@ -222,7 +222,7 @@ export default function projectManagerShimMiddleware(
                                         const entries: FileSystemEntry[] = []
                                         for (const entryName of entryNames) {
                                             const entryPath = path.join(directoryPath, entryName)
-                                            if (isHiddenFile(entryPath)) continue
+                                            if (isHiddenFile.isHiddenFile(entryPath)) continue
                                             const stat = await fs.stat(entryPath)
                                             const attributes: Attributes = {
                                                 byteSize: stat.size,
