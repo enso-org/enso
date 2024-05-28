9572d67670105cfa87eb17f8f4e15afa6712164e
Radosław Waśko <radoslaw.wasko@enso.org>
2024-05-28 17:20:06 +0200

    update test

--- std-bits/base/src/main/java/org/enso/base/encoding/DecodingProblem.java
+++ std-bits/base/src/main/java/org/enso/base/encoding/DecodingProblem.java
@@ -7,6 +7,6 @@ public record DecodingProblem(int count, List<Integer> examplePositions) {
   public String getMessage() {
     String positions = examplePositions.stream().map(Object::toString).collect(Collectors.joining(", "));
     String suffix = count > examplePositions.size() ? ", ..." : "";
-    return "Failed to decode " + count + " code units (at positions " + positions + suffix + ").";
+    return "Failed to decode " + count + " code units (at positions: " + positions + suffix + ").";
   }
 }
