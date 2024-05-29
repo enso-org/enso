928c46a0bda71ce63264f293a50f1f3dd550816b
Adam Riley <riley.ad@gmail.com>
2024-05-29 16:46:17 +0300

    Red

--- std-bits/base/src/main/java/org/enso/base/statistics/Statistic.java
+++ std-bits/base/src/main/java/org/enso/base/statistics/Statistic.java
@@ -1,20 +1,21 @@
 package org.enso.base.statistics;
 
 public enum Statistic {
-  Count,
-  Minimum,
-  Maximum,
-  Sum,
-  Mean,
-  VariancePopulation,
-  VarianceSample,
-  StandardDeviationPopulation,
-  StandardDeviationSample,
-  SkewPopulation,
-  SkewSample,
-  Kurtosis,
-  Covariance,
-  Pearson,
-  Spearman,
-  R_Squared
+    Count,
+    Minimum,
+    Maximum,
+    Sum,
+    Product,
+    Mean,
+    VariancePopulation,
+    VarianceSample,
+    StandardDeviationPopulation,
+    StandardDeviationSample,
+    SkewPopulation,
+    SkewSample,
+    Kurtosis,
+    Covariance,
+    Pearson,
+    Spearman,
+    R_Squared
 }
