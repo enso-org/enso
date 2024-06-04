8e2ce8c829e7b0062df30de24bde2632fb00c5ca
Dmitry Bushev <bushevdv@gmail.com>
2024-06-04 17:55:40 +0100

    misc: cleanup

--- lib/java/ydoc-server/src/main/java/org/enso/ydoc/Main.java
+++ lib/java/ydoc-server/src/main/java/org/enso/ydoc/Main.java
@@ -4,8 +4,8 @@ import java.util.concurrent.Semaphore;
 
 public class Main {
 
-  private static final String YDOC_HOST = "ENSO_YDOC_HOST";
-  private static final String YDOC_PORT = "ENSO_YDOC_PORT";
+  private static final String ENSO_YDOC_HOST = "ENSO_YDOC_HOST";
+  private static final String ENSO_YDOC_PORT = "ENSO_YDOC_PORT";
 
   private static final Semaphore lock = new Semaphore(0);
 
@@ -18,8 +18,8 @@ public class Main {
 
     Sampling.init();
 
-    var ydocHost = System.getenv(YDOC_HOST);
-    var ydocPort = System.getenv(YDOC_PORT);
+    var ydocHost = System.getenv(ENSO_YDOC_HOST);
+    var ydocPort = System.getenv(ENSO_YDOC_PORT);
 
     var builder = Ydoc.builder();
     if (ydocHost != null) {
