8b08e046d84deb78b35327f3be6fca0863fdbe01
Paweł Grabarz <frizi09@gmail.com>
2024-06-05 14:36:30 +0200

    remove log

--- app/gui2/src/asyncApp.ts
+++ app/gui2/src/asyncApp.ts
@@ -1,7 +1,6 @@
 import '@/assets/main.css'
 
 export async function AsyncApp() {
-  console.log('AsyncApp')
   const [_, app] = await Promise.all([
     import('shared/ast/ffi').then((mod) => mod.initializeFFI()),
     import('@/App.vue'),
