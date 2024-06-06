e699a1dc030682e2446554d52a1f21f688a789bc
Ilya Bogdanov <fumlead@gmail.com>
2024-06-06 11:57:09 +0400

    test: cover new behavior in e2e test

--- app/gui2/e2e/nodeClipboard.spec.ts
+++ app/gui2/e2e/nodeClipboard.spec.ts
@@ -1,8 +1,13 @@
 import test from 'playwright/test'
+import { edgesFromNodeWithBinding, edgesToNodeWithBinding } from './locate'
 import * as actions from './actions'
 import { expect } from './customExpect'
 import { CONTROL_KEY } from './keyboard'
 import * as locate from './locate'
+
+/** Every edge consists of multiple parts.
+  * See e2e/edgeRendering.spec.ts for explanation. */
+const EDGE_PARTS = 2
 
 test.beforeEach(async ({ page }) => {
   await page.addInitScript(() => {
@@ -55,7 +60,7 @@ test('Copy multiple nodes', async ({ page }) => {
   // Select some nodes.
   const node1 = locate.graphNodeByBinding(page, 'final')
   await node1.click()
-  const node2 = locate.graphNodeByBinding(page, 'data')
+  const node2 = locate.graphNodeByBinding(page, 'prod')
   await node2.click({ modifiers: ['Shift'] })
   await expect(node1).toBeSelected()
   await expect(node2).toBeSelected()
@@ -68,5 +73,16 @@ test('Copy multiple nodes', async ({ page }) => {
 
   // Nodes and comment have been copied.
   await expect(locate.graphNode(page)).toHaveCount(originalNodes + 2)
+  // `final` node has a comment.
   await expect(page.locator('.GraphNodeComment')).toHaveCount(originalNodeComments + 1)
+  // Check that two copied nodes are isolated, i.e. connected to each other, not original nodes.
+  await expect(locate.graphNodeByBinding(page, 'prod1')).toBeVisible()
+  await expect(locate.graphNodeByBinding(page, 'final1')).toBeVisible()
+  await expect(await edgesFromNodeWithBinding(page, 'sum')).toHaveCount(2 * EDGE_PARTS)
+  await expect(await edgesFromNodeWithBinding(page, 'prod')).toHaveCount(1 * EDGE_PARTS)
+
+  await expect(await edgesToNodeWithBinding(page, 'prod')).toHaveCount(1 * EDGE_PARTS)
+  await expect(await edgesToNodeWithBinding(page, 'final')).toHaveCount(1 * EDGE_PARTS)
+  await expect(await edgesToNodeWithBinding(page, 'prod1')).toHaveCount(1 * EDGE_PARTS)
+  await expect(await edgesToNodeWithBinding(page, 'final1')).toHaveCount(1 * EDGE_PARTS)
 })
