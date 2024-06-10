45fe434bf01ecd16427b01aa004cda8652ae3a8d
Pavel Marek <pavel.marek@enso.org>
2024-06-10 18:31:27 +0200

    Add benchmarks-data cmdline app skeleton

---
+++ tools/performance/benchmarks-data/README.md
@@ -0,0 +1,2 @@
+Gets GH Action ID as argument and fetches the bench-results artifact from it.
+Then, it stores it in the `engine-benchmark-results/cache` directory.
--- tools/performance/benchmarks-data/src/index.ts
+++ tools/performance/benchmarks-data/src/index.ts
@@ -1,8 +1,42 @@
 
 import { Octokit, App } from "octokit";
+import { program } from "commander";
+import { readFileSync, existsSync } from "fs";
 
-const ghToken = process.env.GITHUB_TOKEN;
 
-const octoKit = new Octokit({
-    auth: ghToken
-});
+function findGHToken() {
+    let ghToken = process.env.GITHUB_TOKEN;
+    if (!ghToken) {
+      // Read $HOME/.github-token
+      const ghTokenPath = `${process.env.HOME}/GITHUB_TOKEN`;
+      if (existsSync(ghTokenPath)) {
+        ghToken = readFileSync(ghTokenPath, "utf-8")
+      }
+    }
+    if (!ghToken) {
+        console.error("GITHUB_TOKEN not found in env vars neither in ~/GITHUB_TOKEN file");
+        process.exit(1);
+    }
+    return ghToken;
+}
+
+function main() {
+  program
+    .name("bench-data")
+    .description("Utility to fetch benchmarking data from GH Actions")
+    .option("-v", "--verbose", "Enable verbose logging")
+
+  program.parse();
+
+  const options = program.opts();
+  console.log("options", options);
+
+  const ghToken = findGHToken();
+  console.log("ghToken", ghToken);
+  const octoKit = new Octokit({
+      auth: ghToken
+  });
+  console.log("octoKit", octoKit);
+}
+
+main();
