aaaebcabf87214b61adc1894d0d74592566d9796
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-06-11 14:50:59 +0200

    Implement and benchmark `ArrowOperationPlus` node (#10150)
    Prototype of #10056 showing `+` operation implemented in the _Arrow language_.

--- engine/runtime-language-arrow/src/main/java/org/enso/interpreter/arrow/runtime/ValueToNumberNode.java
+++ engine/runtime-language-arrow/src/main/java/org/enso/interpreter/arrow/runtime/ValueToNumberNode.java
@@ -1,7 +1,13 @@
 package org.enso.interpreter.arrow.runtime;
 
 import com.oracle.truffle.api.CompilerDirectives;
-import com.oracle.truffle.api.dsl.*;
+import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.Fallback;
+import com.oracle.truffle.api.dsl.GenerateInline;
+import com.oracle.truffle.api.dsl.GenerateUncached;
+import com.oracle.truffle.api.dsl.ImportStatic;
+import com.oracle.truffle.api.dsl.NeverDefault;
+import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.interop.UnsupportedTypeException;
@@ -17,58 +23,56 @@ import org.enso.interpreter.arrow.LogicalLayout;
 @ImportStatic(LogicalLayout.class)
 @GenerateUncached
 @GenerateInline(value = false)
-abstract class WriteToBuilderNode extends Node {
-
-  public abstract void executeWrite(ArrowFixedSizeArrayBuilder receiver, long index, Object value)
-      throws UnsupportedTypeException;
+abstract class ValueToNumberNode extends Node {
+  /**
+   * Converts {@code value} to a suitable representation to be stored in an appropriate
+   * DirectBuffer.
+   *
+   * @param unit type of layout
+   * @param value a value to convert
+   * @return byte, short, int or long
+   * @throws UnsupportedTypeException if the conversion isn't possible
+   */
+  abstract Number executeAdjust(LogicalLayout unit, Object value) throws UnsupportedTypeException;
 
   @NeverDefault
-  static WriteToBuilderNode build() {
-    return WriteToBuilderNodeGen.create();
+  static ValueToNumberNode build() {
+    return ValueToNumberNodeGen.create();
   }
 
-  @Specialization(guards = "receiver.getUnit() == Date32")
-  void doWriteDay(
-      ArrowFixedSizeArrayBuilder receiver,
-      long index,
+  @NeverDefault
+  static ValueToNumberNode getUncached() {
+    return ValueToNumberNodeGen.getUncached();
+  }
+
+  @Specialization(guards = "unit == Date32")
+  Integer doDay(
+      LogicalLayout unit,
       Object value,
       @Cached.Shared("interop") @CachedLibrary(limit = "1") InteropLibrary iop)
       throws UnsupportedTypeException {
-    validAccess(receiver, index);
-    if (iop.isNull(value)) {
-      receiver.getBuffer().setNull((int) index);
-      return;
-    }
     if (!iop.isDate(value)) {
       throw UnsupportedTypeException.create(new Object[] {value}, "value is not a date");
     }
-    var at = ArrowFixedArrayDate.typeAdjustedIndex(index, 4);
     long time;
     try {
       time = iop.asDate(value).toEpochDay();
     } catch (UnsupportedMessageException e) {
       throw UnsupportedTypeException.create(new Object[] {value}, "value is not a date");
     }
-    receiver.getBuffer().putInt(at, Math.toIntExact(time));
+    return Math.toIntExact(time);
   }
 
-  @Specialization(guards = {"receiver.getUnit() == Date64"})
-  void doWriteMilliseconds(
-      ArrowFixedSizeArrayBuilder receiver,
-      long index,
+  @Specialization(guards = {"unit == Date64"})
+  Long doMilliseconds(
+      LogicalLayout unit,
       Object value,
       @Cached.Shared("interop") @CachedLibrary(limit = "1") InteropLibrary iop)
       throws UnsupportedTypeException {
-    validAccess(receiver, index);
-    if (iop.isNull(value)) {
-      receiver.getBuffer().setNull((int) index);
-      return;
-    }
     if (!iop.isDate(value) || !iop.isTime(value)) {
       throw UnsupportedTypeException.create(new Object[] {value}, "value is not a date and a time");
     }
 
-    var at = ArrowFixedArrayDate.typeAdjustedIndex(index, 8);
     if (iop.isTimeZone(value)) {
       Instant zoneDateTimeInstant;
       try {
@@ -84,7 +88,7 @@ abstract class WriteToBuilderNode extends Node {
       var secondsPlusNano =
           zoneDateTimeInstant.getEpochSecond() * ArrowFixedArrayDate.NANO_DIV
               + zoneDateTimeInstant.getNano();
-      receiver.getBuffer().putLong(at, secondsPlusNano);
+      return secondsPlusNano;
     } else {
       Instant dateTime;
       try {
@@ -94,7 +98,7 @@ abstract class WriteToBuilderNode extends Node {
       }
       var secondsPlusNano =
           dateTime.getEpochSecond() * ArrowFixedArrayDate.NANO_DIV + dateTime.getNano();
-      receiver.getBuffer().putLong(at, secondsPlusNano);
+      return secondsPlusNano;
     }
   }
 
@@ -109,109 +113,75 @@ abstract class WriteToBuilderNode extends Node {
     return date.atTime(time).toInstant(offset);
   }
 
-  @Specialization(guards = "receiver.getUnit() == Int8")
-  void doWriteByte(
-      ArrowFixedSizeArrayBuilder receiver,
-      long index,
+  @Specialization(guards = "unit == Int8")
+  Byte doByte(
+      LogicalLayout unit,
       Object value,
       @Cached.Shared("interop") @CachedLibrary(limit = "1") InteropLibrary iop)
       throws UnsupportedTypeException {
-    validAccess(receiver, index);
-    if (iop.isNull(value)) {
-      receiver.getBuffer().setNull((int) index);
-      return;
-    }
     if (!iop.fitsInByte(value)) {
       throw UnsupportedTypeException.create(new Object[] {value}, "value does not fit a byte");
     }
     try {
-      receiver.getBuffer().put(typeAdjustedIndex(index, receiver.getUnit()), (iop.asByte(value)));
+      return iop.asByte(value);
     } catch (UnsupportedMessageException e) {
       throw UnsupportedTypeException.create(new Object[] {value}, "value is not a byte");
     }
   }
 
-  @Specialization(guards = "receiver.getUnit() == Int16")
-  void doWriteShort(
-      ArrowFixedSizeArrayBuilder receiver,
-      long index,
+  @Specialization(guards = "unit == Int16")
+  Short doShort(
+      LogicalLayout unit,
       Object value,
       @Cached.Shared("interop") @CachedLibrary(limit = "1") InteropLibrary iop)
       throws UnsupportedTypeException {
-    validAccess(receiver, index);
-    if (iop.isNull(value)) {
-      receiver.getBuffer().setNull((int) index);
-      return;
-    }
     if (!iop.fitsInShort(value)) {
       throw UnsupportedTypeException.create(
           new Object[] {value}, "value does not fit a 2 byte short");
     }
     try {
-      receiver
-          .getBuffer()
-          .putShort(typeAdjustedIndex(index, receiver.getUnit()), (iop.asShort(value)));
+      return iop.asShort(value);
     } catch (UnsupportedMessageException e) {
       throw UnsupportedTypeException.create(new Object[] {value}, "value is not a short");
     }
   }
 
-  @Specialization(guards = "receiver.getUnit() == Int32")
-  void doWriteInt(
-      ArrowFixedSizeArrayBuilder receiver,
-      long index,
+  @Specialization(guards = "unit == Int32")
+  Integer doInt(
+      LogicalLayout unit,
       int value,
       @Cached.Shared("interop") @CachedLibrary(limit = "1") InteropLibrary iop)
       throws UnsupportedTypeException {
-    validAccess(receiver, index);
-    if (iop.isNull(value)) {
-      receiver.getBuffer().setNull((int) index);
-      return;
-    }
     if (!iop.fitsInInt(value)) {
       throw UnsupportedTypeException.create(
           new Object[] {value}, "value does not fit a 4 byte int");
     }
     try {
-      receiver.getBuffer().putInt(typeAdjustedIndex(index, receiver.getUnit()), (iop.asInt(value)));
+      return iop.asInt(value);
     } catch (UnsupportedMessageException e) {
       throw UnsupportedTypeException.create(new Object[] {value}, "value is not an int");
     }
   }
 
-  @Specialization(guards = "receiver.getUnit() == Int64")
-  public static void doWriteLong(
-      ArrowFixedSizeArrayBuilder receiver,
-      long index,
-      long value,
+  @Specialization(guards = "unit == Int64")
+  static Long doLong(
+      LogicalLayout unit,
+      Object value,
       @Cached.Shared("interop") @CachedLibrary(limit = "1") InteropLibrary iop)
       throws UnsupportedTypeException {
-    validAccess(receiver, index);
-    if (iop.isNull(value)) {
-      receiver.getBuffer().setNull((int) index);
-      return;
+    if (!iop.fitsInLong(value)) {
+      throw UnsupportedTypeException.create(
+          new Object[] {value}, "value does not fit a 8 byte int");
     }
-    receiver.getBuffer().putLong(typeAdjustedIndex(index, receiver.getUnit()), value);
+    try {
+      return iop.asLong(value);
+    } catch (UnsupportedMessageException e) {
+      throw UnsupportedTypeException.create(new Object[] {value}, "value is not a long");
+    }
   }
 
   @Fallback
-  void doWriteOther(ArrowFixedSizeArrayBuilder receiver, long index, Object value)
-      throws UnsupportedTypeException {
-    throw UnsupportedTypeException.create(new Object[] {index, value}, "unknown type of receiver");
-  }
-
-  private static void validAccess(ArrowFixedSizeArrayBuilder receiver, long index)
-      throws UnsupportedTypeException {
-    if (receiver.isSealed()) {
-      throw UnsupportedTypeException.create(
-          new Object[] {receiver}, "receiver is not an unsealed buffer");
-    }
-    if (index >= receiver.getSize() || index < 0) {
-      throw UnsupportedTypeException.create(new Object[] {index}, "index is out of range");
-    }
-  }
-
-  private static int typeAdjustedIndex(long index, SizeInBytes unit) {
-    return ArrowFixedArrayDate.typeAdjustedIndex(index, unit.sizeInBytes());
+  Number doOther(LogicalLayout unit, Object value) throws UnsupportedTypeException {
+    throw UnsupportedTypeException.create(new Object[] {unit, value}, "unknown type");
   }
 }
