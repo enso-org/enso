0fa146ce6dc7ba4010565c36b00ec7b29e275add
Radosław Waśko <radoslaw.wasko@enso.org>
2024-06-11 13:28:43 +0200

    formatting

--- engine/runtime-integration-tests/src/test/scala/org/enso/compiler/test/pass/resolve/TypeSignaturesTest.scala
+++ engine/runtime-integration-tests/src/test/scala/org/enso/compiler/test/pass/resolve/TypeSignaturesTest.scala
@@ -270,13 +270,17 @@ class TypeSignaturesTest extends CompilerTest {
     }
 
     "work recursively" in {
-      val arg2Value = ir.asInstanceOf[Application.Prefix].arguments(1).value
+      val arg2Value     = ir.asInstanceOf[Application.Prefix].arguments(1).value
       val arg2Signature = arg2Value.getMetadata(TypeSignatures)
       arg2Signature shouldBe defined
       arg2Signature.get.signature.showCode() shouldEqual "Int"
 
       // But arg1 has no signature:
-      val arg1Signature = ir.asInstanceOf[Application.Prefix].arguments(0).value.getMetadata(TypeSignatures)
+      val arg1Signature = ir
+        .asInstanceOf[Application.Prefix]
+        .arguments(0)
+        .value
+        .getMetadata(TypeSignatures)
       arg1Signature shouldBe empty
     }
   }
