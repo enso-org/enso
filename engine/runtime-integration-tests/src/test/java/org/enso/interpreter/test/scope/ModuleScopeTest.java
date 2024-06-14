4eabff29a5449309a5b1f566e59929fe141e92ba
Pavel Marek <pavel.marek@enso.org>
2024-06-14 20:09:31 +0200

    Add another ModuleScopeTest

--- engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/scope/ModuleScopeTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/scope/ModuleScopeTest.java
@@ -5,9 +5,15 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
 import java.io.IOException;
+import java.util.Set;
 import org.enso.common.LanguageInfo;
 import org.enso.interpreter.runtime.Module;
+import org.enso.pkg.QualifiedName;
+import org.enso.polyglot.PolyglotContext;
+import org.enso.polyglot.RuntimeOptions;
 import org.enso.test.utils.ContextUtils;
+import org.enso.test.utils.ProjectUtils;
+import org.enso.test.utils.SourceModule;
 import org.graalvm.polyglot.Source;
 import org.junit.Rule;
 import org.junit.Test;
@@ -17,14 +23,35 @@ public class ModuleScopeTest {
   @Rule public TemporaryFolder tempFolder = new TemporaryFolder();
 
   @Test
-  public void staticMethodIsRegisteredInModuleScope() throws IOException {
+  public void extensionMethodIsRegisteredInModuleScope() throws IOException {
     var mainSrc =
         Source.newBuilder(
                 LanguageInfo.ID,
                 """
         type My_Type
             Value x
-        My_Type.static_method self = self.x
+        My_Type.extension_method self = self.x
+        """,
+                "test.enso")
+            .build();
+    try (var ctx = ContextUtils.createDefaultContext()) {
+      var mainMod = ctx.eval(mainSrc);
+      var mainRuntimeMod = (Module) ContextUtils.unwrapValue(ctx, mainMod);
+      var myType = mainRuntimeMod.getScope().getType("My_Type", true);
+      assertThat(myType, is(notNullValue()));
+      var extensionMethod = mainRuntimeMod.getScope().getMethodForType(myType, "extension_method");
+      assertThat(extensionMethod, is(notNullValue()));
+    }
+  }
+
+  @Test
+  public void staticMethodIsRegisteredInModuleScope() throws IOException {
+    var mainSrc =
+        Source.newBuilder(
+                LanguageInfo.ID,
+                """
+        type My_Type
+            static_method _ = 42
         """,
                 "test.enso")
             .build();
@@ -55,6 +82,44 @@ public class ModuleScopeTest {
       assertThat(assocType, is(notNullValue()));
       var moduleMethod = mainRuntimeMod.getScope().getMethodForType(assocType, "module_method");
       assertThat(moduleMethod, is(notNullValue()));
+    }
+  }
+
+  @Test
+  public void importedStaticMethodIsRegisteredInModuleScope() throws IOException {
+    var mod =
+        new SourceModule(
+            QualifiedName.fromString("Mod"),
+            """
+        type My_Type
+            static_method _ = 1
+        """);
+    var mainMod =
+        new SourceModule(
+            QualifiedName.fromString("Main"),
+            """
+        from project.Mod import My_Type
+        main = 2
+        """);
+    var projDir = tempFolder.newFolder().toPath();
+    ProjectUtils.createProject("Proj", Set.of(mod, mainMod), projDir);
+    var mainSrcPath = projDir.resolve("src").resolve("Main.enso");
+    try (var ctx =
+        ContextUtils.defaultContextBuilder()
+            .option(RuntimeOptions.PROJECT_ROOT, projDir.toAbsolutePath().toString())
+            .build()) {
+      var polyCtx = new PolyglotContext(ctx);
+      var mainRuntimeMod = polyCtx.evalModule(mainSrcPath.toFile());
+      var mainMethod = mainRuntimeMod.getMethod(mainRuntimeMod.getAssociatedType(), "main").get();
+      var mainRes = mainMethod.execute();
+      assertThat(mainRes.asInt(), is(2));
+      var ensoCtx = ContextUtils.leakContext(ctx);
+      var runtimeAbstractMod =
+          ensoCtx.getPackageRepository().getLoadedModule("local.Proj.Mod").get();
+      var runtimeConcreteMod = Module.fromCompilerModule(runtimeAbstractMod);
+      var myType = runtimeConcreteMod.getScope().getType("My_Type", true);
+      var staticMethod = runtimeConcreteMod.getScope().getMethodForType(myType, "static_method");
+      assertThat(staticMethod, is(notNullValue()));
     }
   }
 }
