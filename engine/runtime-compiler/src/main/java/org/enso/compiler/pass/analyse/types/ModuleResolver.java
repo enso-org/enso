730e3b9cb9092820e7cbeeae4411b70b88fb7621
Radosław Waśko <radoslaw.wasko@enso.org>
2024-06-15 11:52:26 +0200

    WIP

---
+++ engine/runtime-compiler/src/main/java/org/enso/compiler/pass/analyse/types/ModuleResolver.java
@@ -0,0 +1,17 @@
+package org.enso.compiler.pass.analyse.types;
+
+import org.enso.compiler.context.CompilerContext;
+import org.enso.compiler.core.ir.Module;
+import org.enso.pkg.QualifiedName;
+
+public class ModuleResolver {
+  private final CompilerContext context;
+
+  public ModuleResolver(CompilerContext context) {
+    this.context = context;
+  }
+
+  Module findModule(QualifiedName name) {
+    return context.findTopScopeModule(name.toString()).getIr();
+  }
+}
