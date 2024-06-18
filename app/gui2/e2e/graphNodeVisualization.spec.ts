d93e9f309ad8635328a9bdb81ce77c85dfa5e3bf
Adam Obuchowicz <adam.obuchowicz@enso.org>
2024-06-18 16:32:33 +0200

    Add e2e test

--- app/gui2/e2e/graphNodeVisualization.spec.ts
+++ app/gui2/e2e/graphNodeVisualization.spec.ts
@@ -5,7 +5,7 @@ import { computedContent } from './css'
 import { expect } from './customExpect'
 import * as locate from './locate'
 
-test('node can open and load visualization', async ({ page }) => {
+test('Node can open and load visualization', async ({ page }) => {
   await actions.goToGraph(page)
   const node = locate.graphNode(page).last()
   await node.click({ position: { x: 8, y: 8 } })
@@ -23,6 +23,29 @@ test('node can open and load visualization', async ({ page }) => {
   const textContent = await computedContent(element)
   const jsonContent = JSON.parse(textContent)
   expect(typeof jsonContent).toBe('object')
+})
+
+test('Previewing visualization', async ({ page }) => {
+  await actions.goToGraph(page)
+  const node = locate.graphNode(page).last()
+  const port = await locate.outputPortCoordinates(node)
+  await page.keyboard.down('Meta')
+  await page.keyboard.down('Control')
+  await expect(locate.anyVisualization(page)).not.toBeVisible()
+  await page.mouse.move(port.x, port.y)
+  await expect(locate.anyVisualization(node)).toBeVisible()
+  await page.keyboard.up('Meta')
+  await page.keyboard.up('Control')
+  await expect(locate.anyVisualization(page)).not.toBeVisible()
+  await page.keyboard.down('Meta')
+  await page.keyboard.down('Control')
+  await expect(locate.anyVisualization(node)).toBeVisible()
+  await page.mouse.move(1, 1)
+  await expect(locate.anyVisualization(page)).not.toBeVisible()
+  await page.keyboard.up('Meta')
+  await page.keyboard.up('Control')
+  await page.mouse.move(port.x, port.y)
+  await expect(locate.anyVisualization(page)).not.toBeVisible()
 })
 
 test('Warnings visualization', async ({ page }) => {
