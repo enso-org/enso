34424ae67cf74e5b2cb363d8337f50f475c116f0
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-06-20 12:40:54 +0200

    Have to absolutize the file path

--- lib/rust/parser/generate-java/java/org/enso/syntax2/Parser.java
+++ lib/rust/parser/generate-java/java/org/enso/syntax2/Parser.java
@@ -45,7 +45,8 @@ public final class Parser implements AutoCloseable {
       System.load(path.getAbsolutePath());
     } catch (NullPointerException | IllegalArgumentException | LinkageError e) {
       if (!searchFromDirToTop(e, root, "target", "rust", "debug", name)) {
-        if (!searchFromDirToTop(e, new File("."), "target", "rust", "debug", name)) {
+        if (!searchFromDirToTop(
+            e, new File(".").getAbsoluteFile(), "target", "rust", "debug", name)) {
           throw new IllegalStateException("Cannot load parser from " + root, e);
         }
       }
