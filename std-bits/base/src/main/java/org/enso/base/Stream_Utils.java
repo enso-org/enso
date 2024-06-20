b9d221f883cde16377c8b39a8091483675a2de2f
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-06-20 07:30:26 +0200

    asOutputStream wrapper

--- std-bits/base/src/main/java/org/enso/base/Stream_Utils.java
+++ std-bits/base/src/main/java/org/enso/base/Stream_Utils.java
@@ -2,6 +2,7 @@ package org.enso.base;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Arrays;
 
 public final class Stream_Utils {
@@ -39,6 +40,40 @@ public final class Stream_Utils {
    */
   public static InputStream asInputStream(InputStreamLike inputStreamLike) {
     return new GuestInputStream(inputStreamLike);
+  }
+
+  /**
+   * Conversion interface. Any Enso/Truffle object with invocable {@write} member that takes three
+   * arguments is eligible for being treated as host Java {@link OutputStream}. There are two
+   * overloaded {@link #asOutputStream conversion methods}. The <em>hosted Java interop</em> system
+   * of Truffle will pick the more suitable one depending on the type of argument.
+   *
+   * @see #asOutputStream
+   */
+  public static interface OutputStreamLike {
+    public void write(byte[] arr, int off, int len) throws IOException;
+  }
+
+  /**
+   * No conversion conversion. When the argument is already {@link OutputStream} there is no need
+   * for doing any further conversions.
+   *
+   * @param os
+   * @return the {@code is} itself
+   */
+  public static OutputStream asOutputStream(OutputStream os) {
+    return os;
+  }
+
+  /**
+   * Conversion to {@link OutputStream}. When the argument <em>looks like</em> an output stream,
+   * let's wrap it.
+   *
+   * @param outputStreamLike any guest object with {@code write} method
+   * @return proper
+   */
+  public static OutputStream asOutputStream(OutputStreamLike outputStreamLike) {
+    return new GuestOutputStream(outputStreamLike);
   }
 
   public static byte[] peek(InputStream stream, int n) throws IOException {
@@ -90,6 +125,26 @@ public final class Stream_Utils {
     @Override
     public int read(byte[] b) throws IOException {
       return read(b, 0, b.length);
+    }
+  }
+
+  private static final class GuestOutputStream extends OutputStream {
+
+    private final OutputStreamLike outputStreamLike;
+
+    private GuestOutputStream(OutputStreamLike os) {
+      this.outputStreamLike = os;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+      byte[] arr = new byte[] {(byte) b};
+      write(arr, 0, 1);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+      outputStreamLike.write(b, off, len);
     }
   }
 }
