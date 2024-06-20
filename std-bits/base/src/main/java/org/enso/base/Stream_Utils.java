3c52ea7e03c121aeee705cb2aea4d7e552fb0657
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-06-20 06:27:15 +0200

    Let the Stream_Utils handle asInputStream conversion

--- std-bits/base/src/main/java/org/enso/base/Stream_Utils.java
+++ std-bits/base/src/main/java/org/enso/base/Stream_Utils.java
@@ -4,7 +4,43 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 
-public class Stream_Utils {
+public final class Stream_Utils {
+  private Stream_Utils() {}
+
+  /**
+   * Conversion interface. Any Enso/Truffle object with invocable {@read} member that takes three
+   * arguments is eligible for being treated as host Java {@link InputStream}. There are two
+   * overloaded {@link #asInputStream conversion methods}. The <em>hosted Java interop</em> system
+   * of Truffle will pick the more suitable one depending on the type of argument.
+   *
+   * @see #asInputStream
+   */
+  public static interface InputStreamLike {
+    public int read(byte[] arr, int off, int len) throws IOException;
+  }
+
+  /**
+   * No conversion conversion. When the argument is already {@link InputStream} there is no need for
+   * doing any further conversions.
+   *
+   * @param is
+   * @return the {@code is} itself
+   */
+  public static InputStream asInputStream(InputStream is) {
+    return is;
+  }
+
+  /**
+   * Conversion to {@link InputStream}. When the argument <em>looks like</em> an input stream, let's
+   * wrap it.
+   *
+   * @param inputStreamLike any guest object with {@code read} method
+   * @return proper
+   */
+  public static InputStream asInputStream(InputStreamLike inputStreamLike) {
+    return new GuestInputStream(inputStreamLike);
+  }
+
   public static byte[] peek(InputStream stream, int n) throws IOException {
     assert n >= 0;
     assert stream.markSupported();
@@ -24,5 +60,36 @@ public class Stream_Utils {
       buffer = Arrays.copyOf(buffer, offset);
     }
     return buffer;
+  }
+
+  private static class GuestInputStream extends InputStream {
+    private final InputStreamLike inputStreamLike;
+
+    private GuestInputStream(InputStreamLike inputStreamLike) {
+      this.inputStreamLike = inputStreamLike;
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+      return inputStreamLike.read(b, off, len);
+    }
+
+    @Override
+    public int read() throws IOException {
+      byte[] arr = new byte[1];
+      int read = read(arr, 0, 1);
+      if (read == -1) {
+        return -1;
+      }
+      if (read != 1) {
+        throw new IOException();
+      }
+      return arr[0];
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+      return read(b, 0, b.length);
+    }
   }
 }
--- std-bits/base/src/main/java/org/enso/base/XML_Utils.java
+++ std-bits/base/src/main/java/org/enso/base/XML_Utils.java
@@ -69,33 +69,8 @@ public class XML_Utils {
     return out.toString();
   }
 
-  public static Document parseStream(InputStreamLike inputStreamLike)
+  public static Document parseStream(InputStream is)
       throws ParserConfigurationException, SAXException, IOException {
-    var is =
-        new InputStream() {
-          @Override
-          public int read(byte[] b, int off, int len) throws IOException {
-            return inputStreamLike.read(b, off, len);
-          }
-
-          @Override
-          public int read() throws IOException {
-            byte[] arr = new byte[1];
-            int read = read(arr, 0, 1);
-            if (read == -1) {
-              return -1;
-            }
-            if (read != 1) {
-              throw new IOException();
-            }
-            return arr[0];
-          }
-
-          @Override
-          public int read(byte[] b) throws IOException {
-            return read(b, 0, b.length);
-          }
-        };
     return doParse(new InputSource(is));
   }
 
@@ -128,9 +103,5 @@ public class XML_Utils {
             throw e;
           }
         });
-  }
-
-  public static interface InputStreamLike {
-    public int read(byte[] arr, int off, int len) throws IOException;
   }
 }
