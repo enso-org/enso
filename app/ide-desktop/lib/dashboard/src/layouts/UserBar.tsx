0954cbb0f87fd6510be87f0fc178d5aea2bb5764
somebody1234 <ehern.lee@gmail.com>
2024-07-02 23:24:40 +1000

    Remove "Chat" button

--- app/ide-desktop/lib/dashboard/src/layouts/UserBar.tsx
+++ app/ide-desktop/lib/dashboard/src/layouts/UserBar.tsx
@@ -75,17 +75,6 @@ export default function UserBar(props: UserBarProps) {
             className="flex h-[46px] shrink-0 cursor-default items-center gap-user-bar pl-icons-x pr-3"
             {...innerProps}
           >
-            <ariaComponents.Button
-              variant="icon"
-              size="custom"
-              className="mr-1"
-              icon={ChatIcon}
-              aria-label={getText('openHelpChat')}
-              onPress={() => {
-                setIsHelpChatOpen(true)
-              }}
-            />
-
             {shouldShowUpgradeButton && (
               <paywall.PaywallDialogButton feature={'inviteUser'} size="medium" variant="tertiary">
                 {getText('invite')}
