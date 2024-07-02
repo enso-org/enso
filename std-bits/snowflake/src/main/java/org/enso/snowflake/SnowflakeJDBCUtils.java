ed16ea945dc0229a565caaf3d4a4d9fa2a1a277d
Radosław Waśko <radoslaw.wasko@enso.org>
2024-07-02 10:31:53 +0200

    support setting Date column

---
+++ std-bits/snowflake/src/main/java/org/enso/snowflake/SnowflakeJDBCUtils.java
@@ -0,0 +1,35 @@
+package org.enso.snowflake;
+
+import net.snowflake.client.jdbc.SnowflakeUtil;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+
+public class SnowflakeJDBCUtils {
+  public static void setDateTime(PreparedStatement stmt, int columnIndex, ZonedDateTime dateTime, boolean keepOffset) throws SQLException {
+    if (keepOffset) {
+      Timestamp timestamp = Timestamp.from(dateTime.toInstant());
+      stmt.setObject(columnIndex, timestamp, SnowflakeUtil.EXTRA_TYPES_TIMESTAMP_TZ);
+    } else {
+      LocalDateTime localDateTime = dateTime.toLocalDateTime();
+      Timestamp timestamp = Timestamp.valueOf(localDateTime);
+      stmt.setObject(columnIndex, timestamp, SnowflakeUtil.EXTRA_TYPES_TIMESTAMP_NTZ);
+    }
+  }
+
+  public static void setTimeOfDay(PreparedStatement stmt, int columnIndex, LocalTime timeOfDay) throws SQLException {
+    // We use setString instead of setTime, because setTime was losing milliseconds,
+    // or with some tricks maybe could have milliseconds but not nanoseconds.
+    // With setting as text we can keep the precision.
+    stmt.setString(columnIndex, timeOfDay.toString());
+  }
+
+  public static void setDate(PreparedStatement stmt, int columnIndex, LocalDate date) throws SQLException {
+    stmt.setDate(columnIndex, java.sql.Date.valueOf(date));
+  }
+}
