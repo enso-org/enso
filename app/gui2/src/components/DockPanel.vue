836da9504169dfa26cca9c8527b1240743db8dce
Kaz <kaz@lambdaverse.org>
2024-07-04 08:52:15 -0700

    Use existing SizeTransition component

--- app/gui2/src/components/DockPanel.vue
+++ app/gui2/src/components/DockPanel.vue
@@ -1,23 +1,22 @@
 <script setup lang="ts">
 import { documentationEditorBindings } from '@/bindings'
 import ResizeHandles from '@/components/ResizeHandles.vue'
-import SlideIn from '@/components/SlideIn.vue'
+import SizeTransition from '@/components/SizeTransition.vue'
 import ToggleIcon from '@/components/ToggleIcon.vue'
 import { useResizeObserver } from '@/composables/events'
 import { Rect } from '@/util/data/rect'
 import { Vec2 } from '@/util/data/vec2'
-import { computed, ref, type ComponentInstance } from 'vue'
+import { computed, ref } from 'vue'
 
 const MIN_DOCK_SIZE_PX = 200
 
 const toolbarElement = ref<HTMLElement>()
-const slideInPanel = ref<ComponentInstance<typeof SlideIn>>()
-const slideInPanelRoot = computed(() => slideInPanel.value?.rootElement)
+const slideInPanel = ref<HTMLElement>()
 
 const show = defineModel<boolean>('show', { required: true })
 const size = defineModel<number | undefined>('size')
 
-const computedSize = useResizeObserver(slideInPanelRoot)
+const computedSize = useResizeObserver(slideInPanel)
 const computedBounds = computed(() => new Rect(Vec2.Zero, computedSize.value))
 
 function clampSize(size: number) {
@@ -25,7 +24,7 @@ function clampSize(size: number) {
 }
 
 const style = computed(() => ({
-  '--slide-in-panel-size': size.value != null ? `${clampSize(size.value)}px` : undefined,
+  width: size.value != null ? `${clampSize(size.value)}px` : 'var(--right-dock-default-width)',
 }))
 </script>
 
@@ -36,18 +35,19 @@ const style = computed(() => ({
     icon="right_panel"
     class="toggleDock"
   />
-  <SlideIn ref="slideInPanel" :show="show" :style="style" class="DockPanel" data-testid="rightDock">
-    <div ref="toolbarElement" class="toolbar"></div>
-    <div class="scrollArea">
-      <slot :toolbar="toolbarElement" />
+  <SizeTransition width :duration="100">
+    <div v-if="show" ref="slideInPanel" :style="style" class="DockPanel" data-testid="rightDock">
+      <div ref="toolbarElement" class="toolbar"></div>
+      <div class="scrollArea">
+        <slot :toolbar="toolbarElement" />
+      </div>
+      <ResizeHandles left :modelValue="computedBounds" @update:modelValue="size = $event.width" />
     </div>
-    <ResizeHandles left :modelValue="computedBounds" @update:modelValue="size = $event.width" />
-  </SlideIn>
+  </SizeTransition>
 </template>
 
 <style scoped>
 .DockPanel {
-  --slide-in-panel-size: var(--right-dock-default-width);
   background-color: rgb(255, 255, 255);
 }
 
