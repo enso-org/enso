a038bd264698eb3f2c4899198fca09bf4bf6d978
Radosław Waśko <radoslaw.wasko@enso.org>
2024-07-05 12:08:47 +0200

    misc

--- std-bits/snowflake/src/main/java/org/enso/snowflake/SnowflakeJDBCUtils.java
+++ std-bits/snowflake/src/main/java/org/enso/snowflake/SnowflakeJDBCUtils.java
@@ -1,11 +1,7 @@
 package org.enso.snowflake;
 
-import net.snowflake.client.jdbc.SnowflakeType;
-import net.snowflake.client.jdbc.SnowflakeUtil;
-
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -13,28 +9,33 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class SnowflakeJDBCUtils {
-  private static final DateTimeFormatter dateTimeWithOffsetFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS XXX");
+  private static final DateTimeFormatter dateTimeWithOffsetFormatter =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS XXX");
 
-  public static void setDateTime(PreparedStatement stmt, int columnIndex, ZonedDateTime dateTime, boolean keepOffset) throws SQLException {
+  public static void setDateTime(
+      PreparedStatement stmt, int columnIndex, ZonedDateTime dateTime, boolean keepOffset)
+      throws SQLException {
     if (keepOffset) {
       String formatted = dateTime.format(dateTimeWithOffsetFormatter);
       stmt.setString(columnIndex, formatted);
     } else {
       LocalDateTime localDateTime = dateTime.toLocalDateTime();
-      //Timestamp timestamp = Timestamp.valueOf(localDateTime);
-      //stmt.setObject(columnIndex, timestamp, SnowflakeUtil.EXTRA_TYPES_TIMESTAMP_NTZ);
+      // Timestamp timestamp = Timestamp.valueOf(localDateTime);
+      // stmt.setObject(columnIndex, timestamp, SnowflakeUtil.EXTRA_TYPES_TIMESTAMP_NTZ);
       stmt.setString(columnIndex, localDateTime.toString());
     }
   }
 
-  public static void setTimeOfDay(PreparedStatement stmt, int columnIndex, LocalTime timeOfDay) throws SQLException {
+  public static void setTimeOfDay(PreparedStatement stmt, int columnIndex, LocalTime timeOfDay)
+      throws SQLException {
     // We use setString instead of setTime, because setTime was losing milliseconds,
     // or with some tricks maybe could have milliseconds but not nanoseconds.
     // With setting as text we can keep the precision.
     stmt.setString(columnIndex, timeOfDay.toString());
   }
 
-  public static void setDate(PreparedStatement stmt, int columnIndex, LocalDate date) throws SQLException {
+  public static void setDate(PreparedStatement stmt, int columnIndex, LocalDate date)
+      throws SQLException {
     stmt.setDate(columnIndex, java.sql.Date.valueOf(date));
   }
 }
