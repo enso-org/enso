39fa2b063e2f4f27f6d94f3cfbe69c11931e7625
Sergey Garin <sergey.garin@cube.dev>
2024-07-10 19:54:47 +0300

    Fix infinite refetch

--- app/ide-desktop/lib/dashboard/src/pages/dashboard/Dashboard.tsx
+++ app/ide-desktop/lib/dashboard/src/pages/dashboard/Dashboard.tsx
@@ -153,6 +153,7 @@ export function createGetProjectDetailsQuery(options: CreateOpenedProjectQueryOp
   return reactQuery.queryOptions({
     queryKey: createGetProjectDetailsQuery.getQueryKey(assetId),
     meta: { persist: false },
+    gcTime: 0,
     refetchInterval: ({ state }) => {
       /**
        * Default interval for refetching project status when the project is opened.
@@ -169,22 +170,30 @@ export function createGetProjectDetailsQuery(options: CreateOpenedProjectQueryOp
       const activeSyncIntervalMS = 100
       const states = [backendModule.ProjectState.opened, backendModule.ProjectState.closed]
 
+      if (state.status === 'error') {
+        // eslint-disable-next-line no-restricted-syntax
+        return false
+      }
+
       if (isLocal) {
         if (state.data?.state.type === backendModule.ProjectState.opened) {
           return openedIntervalMS
         } else {
           return activeSyncIntervalMS
         }
-      } else if (state.data == null) {
-        return activeSyncIntervalMS
-      } else if (states.includes(state.data.state.type)) {
-        return openedIntervalMS
       } else {
-        return cloudOpeningIntervalMS
+        if (state.data == null) {
+          return activeSyncIntervalMS
+        } else if (states.includes(state.data.state.type)) {
+          return openedIntervalMS
+        } else {
+          return cloudOpeningIntervalMS
+        }
       }
     },
     refetchIntervalInBackground: true,
     refetchOnWindowFocus: true,
+    refetchOnMount: true,
     queryFn: () => {
       invariant(backend != null, 'Backend is null')
 
