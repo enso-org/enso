aaa349554637cf1aabce21e18d8d16391ad8e3b4
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-07-12 17:17:07 +0200

    `engine-runner` shall not depend on `language-server` (#10536)

---
+++ engine/runner-common/src/main/java/org/enso/runner/common/LanguageServerApi.java
@@ -0,0 +1,29 @@
+package org.enso.runner.common;
+
+import java.util.ServiceLoader;
+import org.apache.commons.cli.CommandLine;
+import org.slf4j.event.Level;
+
+public abstract class LanguageServerApi {
+  public static final String DAEMONIZE_OPTION = "daemon";
+  public static final String ROOT_ID_OPTION = "root-id";
+  public static final String ROOT_PATH_OPTION = "path";
+  public static final String INTERFACE_OPTION = "interface";
+  public static final String RPC_PORT_OPTION = "rpc-port";
+  public static final String DATA_PORT_OPTION = "data-port";
+  public static final String SECURE_RPC_PORT_OPTION = "secure-rpc-port";
+  public static final String SECURE_DATA_PORT_OPTION = "secure-data-port";
+  public static final String SKIP_GRAALVM_UPDATER = "skip-graalvm-updater";
+
+  public static void launchLanguageServer(CommandLine line, ProfilingConfig config, Level logLevel)
+      throws WrongOption {
+    var it =
+        ServiceLoader.load(LanguageServerApi.class, LanguageServerApi.class.getClassLoader())
+            .iterator();
+    var impl = it.next();
+    impl.runLanguageServer(line, config, logLevel);
+  }
+
+  protected abstract void runLanguageServer(
+      CommandLine line, ProfilingConfig config, Level logLevel) throws WrongOption;
+}
