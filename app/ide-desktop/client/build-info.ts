61b3d99bf927141c53fe97478906e87b1b636898
somebody1234 <ehern.lee@gmail.com>
2024-07-17 16:44:39 +1000

    Fix import path

--- app/ide-desktop/client/build-info.ts
+++ app/ide-desktop/client/build-info.ts
@@ -1,4 +1,4 @@
 /** @file A re-export of `build.json` to avoid breakage when moving the path of this module. */
 
-import BUILD_INFO from '../../build.json' assert { type: 'json' }
+import BUILD_INFO from '../../../build.json' assert { type: 'json' }
 export default BUILD_INFO
