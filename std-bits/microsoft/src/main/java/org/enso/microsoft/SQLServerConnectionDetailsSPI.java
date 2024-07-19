e3ca70be1366843147b10c03df2e6b294e3edc93
Adam Riley <riley.ad@gmail.com>
2024-07-19 11:20:58 +0100

    Make time type work

--- std-bits/microsoft/src/main/java/org/enso/microsoft/SQLServerConnectionDetailsSPI.java
+++ std-bits/microsoft/src/main/java/org/enso/microsoft/SQLServerConnectionDetailsSPI.java
@@ -5,23 +5,23 @@ import org.enso.database.DatabaseConnectionDetailsSPI;
 @org.openide.util.lookup.ServiceProvider(service = DatabaseConnectionDetailsSPI.class)
 public class SQLServerConnectionDetailsSPI extends DatabaseConnectionDetailsSPI {
 
-    @Override
-    protected String getModuleName() {
-        return "Standard.Mircosoft.SQLServer_Details";
-    }
+  @Override
+  protected String getModuleName() {
+    return "Standard.Mircosoft.SQLServer_Details";
+  }
 
-    @Override
-    protected String getTypeName() {
-        return "SQLServer_Details";
-    }
+  @Override
+  protected String getTypeName() {
+    return "SQLServer_Details";
+  }
 
-    @Override
-    protected String getCodeForDefaultConstructor() {
-        return "(SQLServer_Details.SQLServer)";
-    }
+  @Override
+  protected String getCodeForDefaultConstructor() {
+    return "(SQLServer_Details.SQLServer)";
+  }
 
-    @Override
-    protected String getUserFacingConnectionName() {
-        return "Microsoft SQL Server";
-    }
+  @Override
+  protected String getUserFacingConnectionName() {
+    return "Microsoft SQL Server";
+  }
 }
