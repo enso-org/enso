887d739b03ee8adf072d1ee050e5d05de1c70863
Paweł Grabarz <frizi09@gmail.com>
2024-07-19 14:15:09 +0200

    remove log

--- app/gui2/src/components/GraphEditor/GraphNode.vue
+++ app/gui2/src/components/GraphEditor/GraphNode.vue
@@ -345,7 +345,6 @@ function startEditingNode(position?: Vec2 | undefined) {
 function getRelatedSpanOffset(domNode: globalThis.Node, domOffset: number): number {
   if (domNode instanceof HTMLElement && domOffset == 1) {
     const offsetData = domNode.dataset.spanStart
-    console.log('domNode', domNode)
     const offset = (offsetData != null && parseInt(offsetData)) || 0
     const length = domNode.textContent?.length ?? 0
     return offset + length
