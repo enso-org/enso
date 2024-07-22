992f715ba7fd0f78012f84ecbd123acabe274cba
Radosław Waśko <radoslaw.wasko@enso.org>
2024-07-22 21:17:36 +0200

    only run parser test if JVM tests are being run - it was unnecessarily ran always

--- build/cli/src/lib.rs
+++ build/cli/src/lib.rs
@@ -414,12 +414,15 @@ impl Processor {
                 let mut config = enso_build::engine::BuildConfigurationFlags::default();
                 for arg in which {
                     match arg {
-                        Tests::Jvm => config.test_jvm = true,
+                        Tests::Jvm => {
+                            config.test_jvm = true;
+                            // We also test the Java parser integration when running the JVM tests.
+                            config.test_java_generated_from_rust = true;
+                        },
                         Tests::StandardLibrary => config.add_standard_library_test_selection(StandardLibraryTestsSelection::All),
                         Tests::StdSnowflake => config.add_standard_library_test_selection(StandardLibraryTestsSelection::Selected(vec!["Snowflake_Tests".to_string()])),
                     }
                 }
-                config.test_java_generated_from_rust = true;
                 let context = self.prepare_backend_context(config);
                 async move { context.await?.build().void_ok().await }.boxed()
             }
