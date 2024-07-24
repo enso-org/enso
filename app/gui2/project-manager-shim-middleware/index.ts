7b811730a14936925a35efa47908ea6b44159d84
somebody1234 <ehern.lee@gmail.com>
2024-07-24 18:34:45 +1000

    Show error in the future when shim finds unexpected arguments

--- app/gui2/project-manager-shim-middleware/index.ts
+++ app/gui2/project-manager-shim-middleware/index.ts
@@ -322,7 +322,10 @@ export default function projectManagerShimMiddleware(
                   break
                 }
                 default: {
-                  // Ignored. `result` retains its original value indicating an error.
+                  const message = `Error in Project Manager shim: unknown command ${JSON.stringify(cliArguments)}`
+                  console.error(message)
+                  result = toJSONRPCError(message)
+                  break
                 }
               }
             } catch {
