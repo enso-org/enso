f31c084f43c33e0d988d1bf2f02d5c93bd1f5db9
GregoryTravis <greg.m.travis@gmail.com>
2024-07-25 17:27:19 -0400

    Implement in-memory and database mixed decimal column comparisons (#10614)

--- std-bits/table/src/main/java/org/enso/table/data/column/operation/map/numeric/comparisons/EqualsComparison.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/operation/map/numeric/comparisons/EqualsComparison.java
@@ -1,5 +1,6 @@
 package org.enso.table.data.column.operation.map.numeric.comparisons;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import org.enso.table.data.column.operation.map.MapOperationProblemAggregator;
 import org.enso.table.data.column.operation.map.numeric.helpers.DoubleArrayAdapter;
@@ -41,6 +42,11 @@ public class EqualsComparison<T extends Number, I extends Storage<? super T>>
   @Override
   protected boolean doBigInteger(BigInteger a, BigInteger b) {
     return a.equals(b);
+  }
+
+  @Override
+  protected boolean doBigDecimal(BigDecimal a, BigDecimal b) {
+    return a.compareTo(b) == 0;
   }
 
   @Override
