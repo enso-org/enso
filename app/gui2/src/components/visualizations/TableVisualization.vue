adbef819b994ca08140768143b0192b57cae627e
Martha King <martha_king@hotmail.co.uk>
2024-07-26 15:44:39 +0100

    remove property

--- app/gui2/src/components/visualizations/TableVisualization.vue
+++ app/gui2/src/components/visualizations/TableVisualization.vue
@@ -161,7 +161,6 @@ const agGridOptions: Ref<GridOptions & Required<Pick<GridOptions, 'defaultColDef
   onFirstDataRendered: updateColumnWidths,
   onRowDataUpdated: updateColumnWidths,
   onColumnResized: lockColumnSize,
-  copyHeadersToClipboard: false,
   sendToClipboard: ({ data }: { data: string }) => sendToClipboard(data),
   suppressFieldDotNotation: true,
   enableRangeSelection: true,
