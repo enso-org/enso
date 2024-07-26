7fda8138d853ebd4509c3fb50d899ff83896dc37
Dmitry Bushev <bushevdv@gmail.com>
2024-07-26 13:21:33 +0100

    feat: disable console logging for project-manager cli

--- lib/scala/project-manager/src/main/scala/org/enso/projectmanager/boot/command/CommandHandler.scala
+++ lib/scala/project-manager/src/main/scala/org/enso/projectmanager/boot/command/CommandHandler.scala
@@ -1,6 +1,7 @@
 package org.enso.projectmanager.boot.command
 
 import org.enso.jsonrpc.{Id, JsonProtocol, Protocol}
+import org.enso.logger.LoggerSetup
 import org.enso.projectmanager.boot.Globals.SuccessExitCode
 import org.enso.projectmanager.requesthandler.FailureMapper
 import zio.{Console, ExitCode, ZAny, ZIO}
@@ -9,7 +10,8 @@ final class CommandHandler(protocol: Protocol) {
 
   def printJson[E: FailureMapper](
     result: ZIO[ZAny, E, Any]
-  ): ZIO[ZAny, Throwable, ExitCode] =
+  ): ZIO[ZAny, Throwable, ExitCode] = {
+    consoleLoggingOff *>
     result
       .foldZIO(
         e => {
@@ -29,5 +31,14 @@ final class CommandHandler(protocol: Protocol) {
         }
       )
       .map(_ => SuccessExitCode)
+  }
+
+  private def consoleLoggingOff: ZIO[ZAny, Throwable, Unit] =
+    ZIO.attempt {
+      val loggerSetup = LoggerSetup.get()
+      loggerSetup.teardown()
+      // sets up only the default (file) logger
+      loggerSetup.setup()
+    }
 
 }
