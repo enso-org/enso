3148976a7af4fbd9b15f8ff2f8e45a6cfba272e8
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-08-01 14:55:29 +0200

    language-server/test compiles again

--- engine/language-server/src/test/scala/org/enso/languageserver/libraries/ComponentGroupsValidatorSpec.scala
+++ engine/language-server/src/test/scala/org/enso/languageserver/libraries/ComponentGroupsValidatorSpec.scala
@@ -188,6 +188,7 @@ object ComponentGroupsValidatorSpec {
       authors              = Nil,
       maintainers          = Nil,
       edition              = None,
+      requires             = Array(),
       preferLocalLibraries = true,
       componentGroups = Some(
         ComponentGroups(
