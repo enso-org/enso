2adffe1fa72b22a28824f12aba8040660a191798
James Dunkerley <jdunkerley@users.noreply.github.com>
2024-08-01 16:49:47 +0100

    Add File_Format for Tableau.

--- std-bits/tableau/src/main/java/org/enso/tableau/TableauFormatSPI.java
+++ std-bits/tableau/src/main/java/org/enso/tableau/TableauFormatSPI.java
@@ -1,3 +1,21 @@
-package src.main.java.org.enso.tableau;
+package org.enso.tableau;
 
-public class TableauFormatSPI {}
+import org.enso.base.file_format.FileFormatSPI;
+
+@org.openide.util.lookup.ServiceProvider(service = FileFormatSPI.class)
+public class TableauFormatSPI extends FileFormatSPI {
+  @Override
+  protected String getModuleName() {
+    return "Standard.Tableau.Tableau_Format";
+  }
+
+  @Override
+  protected String getTypeName() {
+    return "Tableau_Format";
+  }
+
+  @Override
+  protected String getDataLinkFormatName() {
+    return "tableau";
+  }
+}
