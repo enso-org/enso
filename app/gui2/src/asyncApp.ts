604b9d071fb266d66e0ec77a47d96d1abd64a339
Paweł Grabarz <frizi09@gmail.com>
2024-08-02 15:41:08 +0200

    address review

--- app/gui2/src/asyncApp.ts
+++ app/gui2/src/asyncApp.ts
@@ -1,8 +1,6 @@
 import '@/assets/base.css'
 
 export async function AsyncApp() {
-  console.log('Loading app...')
   const app = await import('@/App.vue')
-  console.log('App loaded')
   return app
 }
