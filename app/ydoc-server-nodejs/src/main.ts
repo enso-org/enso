0cba327065689888ad04b4950e6db04cfcb78042
Paweł Grabarz <frizi09@gmail.com>
2024-08-02 14:06:50 +0200

    Add healthcheck endpoint to ydoc server

--- app/ydoc-server-nodejs/src/main.ts
+++ app/ydoc-server-nodejs/src/main.ts
@@ -15,7 +15,14 @@ await runServer()
 /** Start http server that handles ydoc websocket connections. */
 async function runServer() {
   const server = http.createServer()
+  server.on('request', (request, response) => {
+    if (request.method === 'GET' && request.url === '/_health') {
+      response.writeHead(200, { 'Content-Type': 'text/plain; charset=UTF-8' }).end('OK')
+    }
+  })
+
   await createGatewayServer(server, LANGUAGE_SERVER_URL)
+
   server.listen(PORT, HOSTNAME, () => {
     console.log(`Ydoc server listening on ${HOSTNAME}:${PORT}`)
   })
