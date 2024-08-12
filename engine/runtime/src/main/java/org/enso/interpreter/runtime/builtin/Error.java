658c7c2d3427b605cdccd5b5d1c4a424af2203b7
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2024-08-12 19:27:08 +0200

    more info

--- engine/runtime/src/main/java/org/enso/interpreter/runtime/builtin/Error.java
+++ engine/runtime/src/main/java/org/enso/interpreter/runtime/builtin/Error.java
@@ -159,7 +159,7 @@ public final class Error {
    */
   @CompilerDirectives.TruffleBoundary
   public Atom makeNoSuchMethod(Object target, UnresolvedSymbol symbol) {
-    new RuntimeException("#10750 " + symbol.getName()).printStackTrace();
+    new RuntimeException("#10750 " + symbol.getName() + " at " + target.toString()).printStackTrace();
     return noSuchMethod.newInstance(target, symbol);
   }
 
