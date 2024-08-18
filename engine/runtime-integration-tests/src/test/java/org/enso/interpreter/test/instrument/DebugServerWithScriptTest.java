aa7c9a7621c26a4275d960f5b7cbc8e3e3640460
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-08-18 08:48:56 +0200

    More normalized JUnit test

--- engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/instrument/DebugServerWithScriptTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/instrument/DebugServerWithScriptTest.java
@@ -11,59 +11,64 @@ import org.enso.polyglot.debugger.DebugServerInfo;
 import org.enso.test.utils.ContextUtils;
 import org.graalvm.polyglot.Context;
 import org.hamcrest.core.AllOf;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class DebugServerWithScriptTest {
-  private interface WithContext {
-    void action(Context ctx, Object out, Object err) throws Exception;
+  private static Context ctx;
+  private static ByteArrayOutputStream out = new ByteArrayOutputStream();
+  private static ByteArrayOutputStream err = new ByteArrayOutputStream();
+
+  @BeforeClass
+  public static void initContext() throws Exception {
+    var b = ContextUtils.defaultContextBuilder().out(out).err(err);
+    b.option(DebugServerInfo.FN_OPTION, "ScriptTest.inspect");
+    ctx = b.build();
   }
 
-  private void withContext(WithContext action) throws Exception {
-    var out = new ByteArrayOutputStream();
-    var err = new ByteArrayOutputStream();
+  @AfterClass
+  public static void closeContext() throws Exception {
+    ctx.close();
+    ctx = null;
+    out = null;
+    err = null;
+  }
 
-    var b = ContextUtils.defaultContextBuilder().out(out).err(err);
-
-    b.option(DebugServerInfo.FN_OPTION, "ScriptTest.inspect");
-
-    try (var ctx = b.build()) {
-      action.action(ctx, out, err);
-    }
+  @Before
+  public void cleanSteams() {
+    out.reset();
+    err.reset();
   }
 
   @Test
   public void propertyListingVariables() throws Exception {
-    withContext(
-        (ctx, out, err) -> {
-          var r =
-              ContextUtils.evalModule(
-                  ctx,
-                  """
-            from Standard.Base import all
+    var code =
+        """
+        from Standard.Base import all
 
-            inspect =
-                j = 1
-                d = Warning.attach "doubled value" 2
-                t = j + d
-                v = [j, d, t]
-                v
-            """,
-                  "ScriptTest",
-                  "inspect");
-          assertTrue("Got array back: " + r, r.hasArrayElements());
-          assertEquals("Got three elements", 3, r.getArraySize());
-          assertEquals("One", 1, r.getArrayElement(0).asInt());
-          assertEquals("Two", 2, r.getArrayElement(1).asInt());
-          assertEquals("Three", 3, r.getArrayElement(2).asInt());
-          assertEquals("No output printed", "", out.toString());
-          assertThat(
-              "Error contains some warnings",
-              err.toString(),
-              AllOf.allOf(
-                  containsString("d = 2"),
-                  containsString("t = 3"),
-                  containsString("doubled value"),
-                  not(containsString("j = 1"))));
-        });
+        inspect =
+            j = 1
+            d = Warning.attach "doubled value" 2
+            t = j + d
+            v = [j, d, t]
+            v
+        """;
+    var r = ContextUtils.evalModule(ctx, code, "ScriptTest.enso", "inspect");
+    assertTrue("Got array back: " + r, r.hasArrayElements());
+    assertEquals("Got three elements", 3, r.getArraySize());
+    assertEquals("One", 1, r.getArrayElement(0).asInt());
+    assertEquals("Two", 2, r.getArrayElement(1).asInt());
+    assertEquals("Three", 3, r.getArrayElement(2).asInt());
+    assertEquals("No output printed", "", out.toString());
+    assertThat(
+        "Error contains some warnings",
+        err.toString(),
+        AllOf.allOf(
+            containsString("d = 2"),
+            containsString("t = 3"),
+            containsString("doubled value"),
+            not(containsString("j = 1"))));
   }
 }
