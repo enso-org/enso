140a5d6ce02efd0cafd9b5c87dda7b420106bba0
Dmitry Bushev <bushevdv@gmail.com>
2024-08-21 17:03:58 +0100

    misc: scalafmt

--- engine/runtime-instrument-common/src/main/scala/org/enso/interpreter/instrument/job/EnsureCompiledJob.scala
+++ engine/runtime-instrument-common/src/main/scala/org/enso/interpreter/instrument/job/EnsureCompiledJob.scala
@@ -424,7 +424,7 @@ final class EnsureCompiledJob(
                 _,
                 expression.errors.Resolution
                   .ResolverError(BindingsMap.ResolutionNotFound),
-                _,
+                _
               ) =>
             DataflowAnalysis.DependencyInfo.Type.Static(
               err.getId,
