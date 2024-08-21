18c23c198ae1a0d6f7b233d77883180276af83f7
Ilya Bogdanov <fumlead@gmail.com>
2024-08-21 17:32:27 +0400

    wip

---
+++ app/gui2/src/stores/settings.ts
@@ -0,0 +1,36 @@
+import { createContextStore } from '@/providers'
+import { useSyncLocalStorage } from '@/composables/syncLocalStorage'
+import { ref } from 'vue'
+
+/** Global application settings. */
+export interface UserSettings {
+  /** Whether to show Help panel when Component Browser is opened or not. */
+  showHelpForCB: boolean
+}
+
+const defaultUserSettings = {
+  showHelpForCB: true,
+}
+
+export const { injectFn: useSettings, provideFn: provideSettings } = createContextStore(
+  'settings',
+  () => {
+    const user = ref<UserSettings>(defaultUserSettings)
+
+    useSyncLocalStorage<UserSettings>({
+      storageKey: 'enso-user-settings',
+      mapKeyEncoder: () => {},
+      debounce: 200,
+      captureState() {
+        return user.value ?? defaultUserSettings
+      },
+      async restoreState(restored) {
+        if (restored) {
+          user.value = { ...defaultUserSettings, ...restored }
+        }
+      }
+    })
+
+    return { user }
+  }
+)
