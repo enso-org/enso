d8be0fb6cf847d8a3d62fe08adb955b760857ad9
somebody1234 <ehern.lee@gmail.com>
2024-08-23 17:51:35 +1000

    Add label to "remove" button for local directories

--- app/dashboard/src/layouts/CategorySwitcher.tsx
+++ app/dashboard/src/layouts/CategorySwitcher.tsx
@@ -448,6 +448,7 @@ export default function CategorySwitcher(props: CategorySwitcherProps) {
                     size="medium"
                     variant="icon"
                     icon={Trash2Icon}
+                    aria-label={getText('removeDirectoryFromFavorites')}
                     className="hidden text-delete group-hover:block"
                     onPress={() => {
                       setModal(
