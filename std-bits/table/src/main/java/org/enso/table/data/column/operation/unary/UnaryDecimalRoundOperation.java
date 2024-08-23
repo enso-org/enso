dab4d549ceba386b418f3ac1c7df945b7648e654
Gregory Travis <greg.travis@enso.org>
2024-08-23 12:14:56 -0400

    wip

--- std-bits/table/src/main/java/org/enso/table/data/column/operation/unary/UnaryDecimalRoundOperation.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/operation/unary/UnaryDecimalRoundOperation.java
@@ -1,67 +1,53 @@
 package org.enso.table.data.column.operation.unary;
 
-import java.util.function.DoubleToLongFunction;
-import org.enso.table.data.column.builder.LongBuilder;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.function.Function;
+import org.enso.base.numeric.Decimal_Utils;
+import org.enso.table.data.column.builder.Builder;
+import org.enso.table.data.column.builder.InferredBuilder;
 import org.enso.table.data.column.operation.UnaryOperation;
 import org.enso.table.data.column.operation.map.MapOperationProblemAggregator;
-import org.enso.table.data.column.storage.ColumnLongStorage;
 import org.enso.table.data.column.storage.ColumnStorage;
-import org.enso.table.data.column.storage.type.IntegerType;
+import org.enso.table.data.column.storage.type.BigDecimalType;
 
 public class UnaryDecimalRoundOperation extends AbstractUnaryOperation {
   public static final String CEIL = "ceil";
   public static final UnaryOperation CEIL_INSTANCE =
-      new UnaryDecimalRoundOperation(CEIL, d -> (long) Math.ceil(d));
+      new UnaryDecimalRoundOperation(CEIL, d -> Decimal_Utils.ceil(d));
 
   public static final String FLOOR = "floor";
   public static final UnaryOperation FLOOR_INSTANCE =
-      new UnaryDecimalRoundOperation(FLOOR, d -> (long) Math.floor(d));
+      new UnaryDecimalRoundOperation(CEIL, d -> Decimal_Utils.floor(d));
 
   public static String TRUNCATE = "truncate";
   public static final UnaryOperation TRUNCATE_INSTANCE =
-      new UnaryDecimalRoundOperation(TRUNCATE, d -> (long) d);
+      new UnaryDecimalRoundOperation(CEIL, d -> Decimal_Utils.truncate(d));
 
-  private final DoubleToLongFunction function;
+  private final Function<BigDecimal, BigInteger> function;
 
-  private UnaryDecimalRoundOperation(String name, DoubleToLongFunction function) {
-    super(name, true, IntegerType.INT_64);
+  private UnaryDecimalRoundOperation(String name, Function<BigDecimal, BigInteger> function) {
+    super(name, true);
     this.function = function;
   }
 
   @Override
   public boolean canApply(ColumnStorage storage) {
-    return storage.getType().isNumeric();
+    return storage.getType() instanceof BigDecimalType;
   }
 
   @Override
-  public ColumnStorage apply(
-      ColumnStorage storage, MapOperationProblemAggregator problemAggregator) {
-    if (storage instanceof ColumnLongStorage longStorage) {
-      // For a long storage, the operation is an identity operation.
-      return longStorage;
-    }
-
-    return super.apply(storage, problemAggregator);
+  protected final void applyObjectRow(
+      Object value, Builder builder, MapOperationProblemAggregator problemAggregator) {
+    applyObjectRow(value, (InferredBuilder) builder, problemAggregator);
   }
 
-  @Override
   protected void applyObjectRow(
-      Object value, LongBuilder builder, MapOperationProblemAggregator problemAggregator) {
-    // Null handled by base class
+      Object value, InferredBuilder builder, MapOperationProblemAggregator problemAggregator) {
     switch (value) {
-      case Double d -> {
-        if (Double.isNaN(d) || Double.isInfinite(d)) {
-          String msg = "Value is " + d;
-          problemAggregator.reportArithmeticError(msg, builder.getCurrentSize());
-          builder.appendNulls(1);
-        } else {
-          builder.appendLong(function.applyAsLong(d));
-        }
-      }
-      case Float f -> applyObjectRow((double) f, builder, problemAggregator);
-      case Number n -> applyObjectRow(n.doubleValue(), builder, problemAggregator);
+      case BigDecimal d -> builder.append(function.apply(d));
       default -> throw new IllegalArgumentException(
-          "Unsupported type: " + value.getClass() + " (expected numeric type).");
+          "Unsupported type: " + value.getClass() + " (expected decimal).");
     }
   }
 }
