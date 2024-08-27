4a0e1411f23576613e01c1b6adf125247dda0057
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-08-27 09:12:20 +0200

    Less verbose output. Only use stderr when they may be troubles.

--- engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/instrument/RuntimeManagementCleanupTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/instrument/RuntimeManagementCleanupTest.java
@@ -2,6 +2,8 @@ package org.enso.interpreter.test.instrument;
 
 import static org.junit.Assert.fail;
 
+import java.io.OutputStream;
+import java.io.PrintStream;
 import org.junit.Test;
 
 public final class RuntimeManagementCleanupTest {
@@ -9,18 +11,19 @@ public final class RuntimeManagementCleanupTest {
   @Test
   public void cleanUp() throws InterruptedException {
     var cnt = 0;
-    for (var i = 0; i < 100; i++) {
+    for (var i = 1; i < 100; i++) {
       cnt = 0;
+      var err = i % 10 == 0 ? System.err : new PrintStream(OutputStream.nullOutputStream());
       for (var threadStack : Thread.getAllStackTraces().entrySet()) {
         if (threadStack.getKey().getName().startsWith("Thread-")) {
-          System.err.println(threadStack.getKey().getName());
+          err.println(threadStack.getKey().getName());
           for (var frame : threadStack.getValue()) {
-            System.err.println("  " + frame);
+            err.println("  " + frame);
           }
           cnt++;
         }
       }
-      System.err.println("Found " + cnt + " suspicious threads");
+      err.println("Found " + cnt + " suspicious threads");
       if (cnt == 0) {
         return;
       }
