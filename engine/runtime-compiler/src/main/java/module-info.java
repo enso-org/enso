b9f100ecf0317bd023c78a50e0ae66c7f7a406af
Pavel Marek <pavel.marek@enso.org>
2024-08-29 19:51:50 +0200

    Add some exports to org.enso.runtime.compiler

--- engine/runtime-compiler/src/main/java/module-info.java
+++ engine/runtime-compiler/src/main/java/module-info.java
@@ -17,11 +17,16 @@ module org.enso.runtime.compiler {
   exports org.enso.compiler.data;
   exports org.enso.compiler.dump;
   exports org.enso.compiler.exception;
-  exports org.enso.compiler.phase;
   exports org.enso.compiler.pass;
   exports org.enso.compiler.pass.analyse;
   exports org.enso.compiler.pass.analyse.alias;
   exports org.enso.compiler.pass.analyse.alias.graph;
   exports org.enso.compiler.pass.analyse.types;
+  exports org.enso.compiler.pass.desugar;
+  exports org.enso.compiler.pass.lint;
+  exports org.enso.compiler.pass.optimise;
   exports org.enso.compiler.pass.resolve;
+  exports org.enso.compiler.phase;
+  exports org.enso.compiler.phase.exports;
+  exports org.enso.compiler.refactoring;
 }
