c55cd50e7646bc5ec5d66ae74bcd818e83790bdd
Pavel Marek <pavel.marek@enso.org>
2024-08-29 19:57:58 +0200

    Replace TestLogProviderOnClassPath with TestLogProviderOnModulePath

---
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/TestLogProviderOnModulePath.java
@@ -0,0 +1,20 @@
+package org.enso.interpreter.test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.enso.logging.service.logback.test.provider.TestLogProvider;
+import org.junit.Test;
+
+/**
+ * In the `runtime/Test` testing suite, {@link TestLogProvider} should be among the
+ * logging providers, because it is explicitly chosen as the logging provider for the tests.
+ */
+public class TestLogProviderOnModulePath {
+  @Test
+  public void testLogProviderOnClasspath() {
+    var modName = "org.enso.logging.service.logback.test.provider";
+    var logProviderMod = ModuleLayer.boot().findModule(modName);
+    assertThat(logProviderMod.isPresent(), is(true));
+  }
+}
