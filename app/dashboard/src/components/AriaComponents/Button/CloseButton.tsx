05f18f18f71e3724035c70ebffa53c8842b7c4b5
somebody1234 <ehern.lee@gmail.com>
2024-08-30 18:48:20 +1000

    Show "x" instead of circle for close button on platforms other than macOS

--- app/dashboard/src/components/AriaComponents/Button/CloseButton.tsx
+++ app/dashboard/src/components/AriaComponents/Button/CloseButton.tsx
@@ -3,6 +3,7 @@ import DismissIcon from '#/assets/dismiss.svg'
 import { Button, type ButtonProps } from '#/components/AriaComponents/Button'
 import { useText } from '#/providers/TextProvider'
 import { twMerge } from '#/utilities/tailwindMerge'
+import { isOnMacOS } from 'enso-common/src/detect'
 
 // ===================
 // === CloseButton ===
@@ -27,14 +28,17 @@ export function CloseButton(props: CloseButtonProps) {
       variant="icon"
       className={(values) =>
         twMerge(
-          'bg-primary/30 hover:bg-red-500/80 focus-visible:bg-red-500/80 focus-visible:outline-offset-1',
+          'hover:bg-red-500/80 focus-visible:bg-red-500/80 focus-visible:outline-offset-1',
+          isOnMacOS() ? 'bg-primary/30' : (
+            'text-primary/90 hover:text-primary focus-visible:text-primary'
+          ),
           // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
           // @ts-expect-error ts fails to infer the type of the className prop
           typeof className === 'function' ? className(values) : className,
         )
       }
       tooltip={tooltip}
-      showIconOnHover
+      showIconOnHover={isOnMacOS()}
       size="xsmall"
       rounded="full"
       extraClickZone="medium"
