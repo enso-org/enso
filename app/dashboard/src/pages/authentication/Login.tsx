7b4e1146a115ede4606c23d41b336341a27f2977
Sergei Garin <sergei.garin@enso.org>
2024-09-02 18:11:40 +0300

    Fix login

--- app/dashboard/src/pages/authentication/Login.tsx
+++ app/dashboard/src/pages/authentication/Login.tsx
@@ -86,6 +86,7 @@ export default function Login() {
           })
         }
         gap="medium"
+        defaultValues={{ email: initialEmail ?? '' }}
         onSubmit={({ email, password }) => signInWithPassword(email, password)}
       >
         <Input
