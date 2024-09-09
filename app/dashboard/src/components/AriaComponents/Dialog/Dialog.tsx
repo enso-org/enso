enso-org/enso

commit 3420a05a84ed5314b6cf50895eaa8c766694bfa0

Dashboard improvements 08/26/2024 (#10946)
- Address part of https://github.com/enso-org/cloud-v2/issues/1453
- Many of the other issues are addressed in other PRs.
- Username in "Set username" dialog defaults to user email
- Hide "Share with" column for Free and Solo plans
- Reorder categories in left sidebar
- Previous: Cloud, My Files, Recent, Trash, ...Users, ...Teams
- New: Cloud, Me (formerly My Files), ...Users, Teams, Recent, Trash

- Fix #10968
- Show column toggles on Local category too (previously was explicitly specialcased to only be visible on Cloud categories as a remnant of the old design with the backend switcher at the top)
- Added to this PR as this PR already touches the column toggles

# Important Notes
None

app/dashboard/src/pages/authentication/Setup/Setup.tsx
@@ -16,7 +16,7 @@ import { DASHBOARD_PATH, LOGIN_PATH } from '#/appUtils'
 
 import { useIsFirstRender } from '#/hooks/mountHooks'
 
-import { useAuth, UserSessionType } from '#/providers/AuthProvider'
+import { useAuth, UserSessionType, useUserSession } from '#/providers/AuthProvider'
 import { useRemoteBackendStrict } from '#/providers/BackendProvider'
 import * as textProvider from '#/providers/TextProvider'
 
@@ -65,9 +65,11 @@ const BASE_STEPS: Step[] = [
      */
     component: function SetUsernameStep({ session, goToNextStep }) {
       const { setUsername } = useAuth()
+      const userSession = useUserSession()
       const { getText } = textProvider.useText()
 
-      const defaultName = session && 'user' in session ? session.user.name : ''
+      const defaultName =
+        session && 'user' in session ? session.user.name : userSession?.email ?? ''
 
       return (
         <ariaComponents.Form

app/dashboard/src/components/dashboard/column/columnUtils.ts
@@ -40,21 +40,6 @@ export const DEFAULT_ENABLED_COLUMNS: ReadonlySet<Column> = new Set([
   Column.labels,
 ])
 
-/** The list of all possible columns for the local backend, in order. */
-export const LOCAL_COLUMNS = Object.freeze([Column.name, Column.modified] as const)
-
-/** The list of all possible columns for the cloud backend, in order. */
-// This MUST be `as const`, to generate the `ExtraColumn` type above.
-export const CLOUD_COLUMNS = Object.freeze([
-  Column.name,
-  Column.modified,
-  Column.sharedWith,
-  Column.labels,
-  Column.accessedByProjects,
-  Column.accessedData,
-  Column.docs,
-] as const)
-
 export const COLUMN_ICONS: Readonly<Record<Column, string>> = {
   /* The file column does not have an icon, however this does not matter as it is not
    * collapsible. */
@@ -97,20 +82,17 @@ export const COLUMN_CSS_CLASS: Readonly<Record<Column, string>> = {
 // =====================
 
 /** Return the full list of columns given the relevant current state. */
-export function getColumnList(
-  backendType: backend.BackendType,
-  enabledColumns: ReadonlySet<Column>,
-) {
-  let columns: readonly Column[]
-  switch (backendType) {
-    case backend.BackendType.local: {
-      columns = LOCAL_COLUMNS
-      break
-    }
-    case backend.BackendType.remote: {
-      columns = CLOUD_COLUMNS
-      break
-    }
-  }
-  return columns.filter((column) => enabledColumns.has(column))
+export function getColumnList(user: backend.User, backendType: backend.BackendType) {
+  const isCloud = backendType === backend.BackendType.remote
+  const isEnterprise = user.plan === backend.Plan.enterprise
+  const columns = [
+    Column.name,
+    Column.modified,
+    isCloud && isEnterprise && Column.sharedWith,
+    isCloud && Column.labels,
+    isCloud && Column.accessedByProjects,
+    isCloud && Column.accessedData,
+    isCloud && Column.docs,
+  ]
+  return columns.flatMap((column) => (column !== false ? [column] : []))
 }

app/dashboard/src/layouts/AssetPanel.tsx
@@ -39,11 +39,13 @@ declare module '#/utilities/LocalStorage' {
   /** */
   interface LocalStorageData {
     readonly assetPanelTab: AssetPanelTab
+    readonly assetPanelWidth: number
   }
 }
 
-LocalStorage.registerKey('assetPanelTab', {
-  schema: z.nativeEnum(AssetPanelTab),
+LocalStorage.register({
+  assetPanelTab: { schema: z.nativeEnum(AssetPanelTab) },
+  assetPanelWidth: { schema: z.number().int() },
 })
 
 // ==================

app/dashboard/src/layouts/AssetsTable.tsx
@@ -110,7 +110,7 @@ declare module '#/utilities/LocalStorage' {
 }
 
 LocalStorage.registerKey('enabledColumns', {
-  schema: z.enum(columnUtils.CLOUD_COLUMNS).array().readonly(),
+  schema: z.nativeEnum(columnUtils.Column).array().readonly(),
 })
 
 // =================
@@ -335,7 +335,9 @@ export default function AssetsTable(props: AssetsTableProps) {
   const didLoadingProjectManagerFail = backendProvider.useDidLoadingProjectManagerFail()
   const reconnectToProjectManager = backendProvider.useReconnectToProjectManager()
   const [enabledColumns, setEnabledColumns] = React.useState(columnUtils.DEFAULT_ENABLED_COLUMNS)
-
+  const hiddenColumns = columnUtils
+    .getColumnList(user, backend.type)
+    .filter((column) => !enabledColumns.has(column))
   const [sortInfo, setSortInfo] =
     React.useState<sorting.SortInfo<columnUtils.SortableColumn> | null>(null)
   const driveStore = useDriveStore()
@@ -2282,13 +2284,12 @@ export default function AssetsTable(props: AssetsTableProps) {
       rootRef.current != null &&
       headerRowRef.current != null
     ) {
-      const hiddenColumnsCount = columnUtils.CLOUD_COLUMNS.length - enabledColumns.size
       const shrinkBy =
-        COLUMNS_SELECTOR_BASE_WIDTH_PX + COLUMNS_SELECTOR_ICON_WIDTH_PX * hiddenColumnsCount
+        COLUMNS_SELECTOR_BASE_WIDTH_PX + COLUMNS_SELECTOR_ICON_WIDTH_PX * hiddenColumns.length
       const rightOffset = rootRef.current.clientWidth + rootRef.current.scrollLeft - shrinkBy
       headerRowRef.current.style.clipPath = `polygon(0 0, ${rightOffset}px 0, ${rightOffset}px 100%, 0 100%)`
     }
-  }, [backend.type, enabledColumns.size])
+  }, [backend.type, hiddenColumns.length])
 
   const updateClipPathObserver = React.useMemo(
     () => new ResizeObserver(updateClipPath),
@@ -2507,7 +2508,9 @@ export default function AssetsTable(props: AssetsTableProps) {
     setAsset,
   }))
 
-  const columns = columnUtils.getColumnList(backend.type, enabledColumns)
+  const columns = columnUtils
+    .getColumnList(user, backend.type)
+    .filter((column) => enabledColumns.has(column))
 
   const headerRow = (
     <tr ref={headerRowRef} className="sticky top-[1px] text-sm font-semibold">
@@ -2895,47 +2898,43 @@ export default function AssetsTable(props: AssetsTableProps) {
                 />
               )}
               <div className="flex h-max min-h-full w-max min-w-full flex-col">
-                {isCloud && (
-                  <div className="flex-0 sticky top-0 flex h-0 flex-col">
-                    <div
-                      data-testid="extra-columns"
-                      className="sticky right-0 flex self-end px-2 py-3"
-                    >
-                      <FocusArea direction="horizontal">
-                        {(columnsBarProps) => (
-                          <div
-                            {...aria.mergeProps<JSX.IntrinsicElements['div']>()(columnsBarProps, {
-                              className: 'inline-flex gap-icons',
-                              onFocus: () => {
-                                setKeyboardSelectedIndex(null)
-                              },
-                            })}
-                          >
-                            {columnUtils.CLOUD_COLUMNS.filter(
-                              (column) => !enabledColumns.has(column),
-                            ).map((column) => (
-                              <Button
-                                key={column}
-                                light
-                                image={columnUtils.COLUMN_ICONS[column]}
-                                alt={getText(columnUtils.COLUMN_SHOW_TEXT_ID[column])}
-                                onPress={() => {
-                                  const newExtraColumns = new Set(enabledColumns)
-                                  if (enabledColumns.has(column)) {
-                                    newExtraColumns.delete(column)
-                                  } else {
-                                    newExtraColumns.add(column)
-                                  }
-                                  setEnabledColumns(newExtraColumns)
-                                }}
-                              />
-                            ))}
-                          </div>
-                        )}
-                      </FocusArea>
-                    </div>
+                <div className="flex-0 sticky top-0 flex h-0 flex-col">
+                  <div
+                    data-testid="extra-columns"
+                    className="sticky right-0 flex self-end px-2 py-3"
+                  >
+                    <FocusArea direction="horizontal">
+                      {(columnsBarProps) => (
+                        <div
+                          {...aria.mergeProps<JSX.IntrinsicElements['div']>()(columnsBarProps, {
+                            className: 'inline-flex gap-icons',
+                            onFocus: () => {
+                              setKeyboardSelectedIndex(null)
+                            },
+                          })}
+                        >
+                          {hiddenColumns.map((column) => (
+                            <Button
+                              key={column}
+                              light
+                              image={columnUtils.COLUMN_ICONS[column]}
+                              alt={getText(columnUtils.COLUMN_SHOW_TEXT_ID[column])}
+                              onPress={() => {
+                                const newExtraColumns = new Set(enabledColumns)
+                                if (enabledColumns.has(column)) {
+                                  newExtraColumns.delete(column)
+                                } else {
+                                  newExtraColumns.add(column)
+                                }
+                                setEnabledColumns(newExtraColumns)
+                              }}
+                            />
+                          ))}
+                        </div>
+                      )}
+                    </FocusArea>
                   </div>
-                )}
+                </div>
                 <div className="flex h-full w-min min-w-full grow flex-col">{table}</div>
               </div>
             </div>

app/dashboard/src/layouts/CategorySwitcher.tsx
@@ -394,25 +394,6 @@ export default function CategorySwitcher(props: CategorySwitcherProps) {
             dropZoneLabel={getText('myFilesCategoryDropZoneLabel')}
           />
         )}
-        <CategorySwitcherItem
-          {...itemProps}
-          isNested
-          category={{ type: 'recent' }}
-          icon={RecentIcon}
-          label={getText('recentCategory')}
-          buttonLabel={getText('recentCategoryButtonLabel')}
-          dropZoneLabel={getText('recentCategoryDropZoneLabel')}
-          iconClassName="-ml-0.5"
-        />
-        <CategorySwitcherItem
-          {...itemProps}
-          isNested
-          category={{ type: 'trash' }}
-          icon={Trash2Icon}
-          label={getText('trashCategory')}
-          buttonLabel={getText('trashCategoryButtonLabel')}
-          dropZoneLabel={getText('trashCategoryDropZoneLabel')}
-        />
         {usersDirectoryQuery.data?.map((userDirectory) => {
           if (userDirectory.type !== backend.AssetType.directory) {
             return null
@@ -460,6 +441,25 @@ export default function CategorySwitcher(props: CategorySwitcherProps) {
               )
           }
         })}
+        <CategorySwitcherItem
+          {...itemProps}
+          isNested
+          category={{ type: 'recent' }}
+          icon={RecentIcon}
+          label={getText('recentCategory')}
+          buttonLabel={getText('recentCategoryButtonLabel')}
+          dropZoneLabel={getText('recentCategoryDropZoneLabel')}
+          iconClassName="-ml-0.5"
+        />
+        <CategorySwitcherItem
+          {...itemProps}
+          isNested
+          category={{ type: 'trash' }}
+          icon={Trash2Icon}
+          label={getText('trashCategory')}
+          buttonLabel={getText('trashCategoryButtonLabel')}
+          dropZoneLabel={getText('trashCategoryDropZoneLabel')}
+        />
         {localBackend && (
           <div className="group flex items-center justify-between self-stretch">
             <CategorySwitcherItem

app/dashboard/src/utilities/LocalStorage.ts
@@ -4,6 +4,19 @@ import type * as z from 'zod'
 import * as common from 'enso-common'
 
 import * as object from '#/utilities/object'
+import { IS_DEV_MODE } from 'enso-common/src/detect'
+import invariant from 'tiny-invariant'
+
+const KEY_DEFINITION_STACK_TRACES = new Map<string, string>()
+
+/** Whether the source location for `LocalStorage.register(key)` is different to the previous
+ * known source location. */
+function isSourceChanged(key: string) {
+  const stack = (new Error().stack ?? '').replace(/[?]t=\d+:\d+:\d+/g, '')
+  const isChanged = stack !== KEY_DEFINITION_STACK_TRACES.get(key)
+  KEY_DEFINITION_STACK_TRACES.set(key, stack)
+  return isChanged
+}
 
 // ===============================
 // === LocalStorageKeyMetadata ===
@@ -74,7 +87,26 @@ export default class LocalStorage {
 
   /** Register runtime behavior associated with a {@link LocalStorageKey}. */
   static registerKey<K extends LocalStorageKey>(key: K, metadata: LocalStorageKeyMetadata<K>) {
+    if (IS_DEV_MODE ? isSourceChanged(key) : true) {
+      invariant(
+        !(key in LocalStorage.keyMetadata),
+        `Local storage key '${key}' has already been registered.`,
+      )
+    }
     LocalStorage.keyMetadata[key] = metadata
+  }
+
+  /** Register runtime behavior associated with a {@link LocalStorageKey}. */
+  static register<K extends LocalStorageKey>(metadata: { [K_ in K]: LocalStorageKeyMetadata<K_> }) {
+    for (const key in metadata) {
+      if (IS_DEV_MODE ? isSourceChanged(key) : true) {
+        invariant(
+          !(key in LocalStorage.keyMetadata),
+          `Local storage key '${key}' has already been registered.`,
+        )
+      }
+    }
+    Object.assign(LocalStorage.keyMetadata, metadata)
   }
 
   /** Retrieve an entry from the stored data. */

commit 1fdaf37addffe021b8377d4bbc1e000d5cd2d94f

Fix skipping creating a user if we don't change the default name (#11013)
Closes: enso-org/cloud-v2#1473

This PR fixes a bug introduced in #10946

app/dashboard/src/components/AriaComponents/Dialog/Dialog.tsx
@@ -9,13 +9,14 @@ import * as portal from '#/components/Portal'
 import * as suspense from '#/components/Suspense'
 
 import * as mergeRefs from '#/utilities/mergeRefs'
-import * as twv from '#/utilities/tailwindVariants'
 
+import type { VariantProps } from '#/utilities/tailwindVariants'
+import { tv } from '#/utilities/tailwindVariants'
 import * as dialogProvider from './DialogProvider'
 import * as dialogStackProvider from './DialogStackProvider'
 import type * as types from './types'
 import * as utlities from './utilities'
-import * as variants from './variants'
+import { DIALOG_BACKGROUND } from './variants'
 
 // =================
 // === Constants ===
@@ -25,10 +26,10 @@ import * as variants from './variants'
  */
 export interface DialogProps
   extends types.DialogProps,
-    Omit<twv.VariantProps<typeof DIALOG_STYLES>, 'scrolledToTop'> {}
+    Omit<VariantProps<typeof DIALOG_STYLES>, 'scrolledToTop'> {}
 
-const OVERLAY_STYLES = twv.tv({
-  base: 'fixed inset-0 isolate flex items-center justify-center bg-black/[25%]',
+const OVERLAY_STYLES = tv({
+  base: 'fixed inset-0 isolate flex items-center justify-center bg-primary/20',
   variants: {
     isEntering: { true: 'animate-in fade-in duration-200 ease-out' },
     isExiting: { true: 'animate-out fade-out duration-200 ease-in' },
@@ -36,17 +37,23 @@ const OVERLAY_STYLES = twv.tv({
   },
 })
 
-const MODAL_STYLES = twv.tv({
-  base: 'fixed inset-0 flex items-center justify-center text-center text-xs text-primary p-4',
+const MODAL_STYLES = tv({
+  base: 'fixed inset-0 flex items-center justify-center text-xs text-primary',
   variants: {
-    isEntering: { true: 'animate-in slide-in-from-top-1 ease-out duration-200' },
-    isExiting: { true: 'animate-out slide-out-to-top-1 ease-in duration-200' },
+    isEntering: { true: 'animate-in ease-out duration-200' },
+    isExiting: { true: 'animate-out ease-in duration-200' },
+    type: { modal: '', fullscreen: 'p-3.5' },
   },
+  compoundVariants: [
+    { type: 'modal', isEntering: true, class: 'slide-in-from-top-1' },
+    { type: 'modal', isExiting: true, class: 'slide-out-to-top-1' },
+    { type: 'fullscreen', isEntering: true, class: 'zoom-in-[1.015]' },
+    { type: 'fullscreen', isExiting: true, class: 'zoom-out-[1.015]' },
+  ],
 })
 
-const DIALOG_STYLES = twv.tv({
-  extend: variants.DIALOG_STYLES,
-  base: 'w-full overflow-y-auto',
+const DIALOG_STYLES = tv({
+  base: DIALOG_BACKGROUND({ className: 'w-full flex flex-col text-left align-middle shadow-xl' }),
   variants: {
     type: {
       modal: {
@@ -73,6 +80,16 @@ const DIALOG_STYLES = twv.tv({
         header: 'p-0 max-h-0 min-h-0 h-0 border-0 z-1',
         content: 'isolate',
       },
+    },
+    rounded: {
+      none: { base: '' },
+      small: { base: 'rounded-sm' },
+      medium: { base: 'rounded-md' },
+      large: { base: 'rounded-lg' },
+      xlarge: { base: 'rounded-xl' },
+      xxlarge: { base: 'rounded-2xl', content: 'scroll-offset-edge-2xl' },
+      xxxlarge: { base: 'rounded-3xl', content: 'scroll-offset-edge-4xl' },
+      xxxxlarge: { base: 'rounded-4xl', content: 'scroll-offset-edge-6xl' },
     },
     /**
      * The size of the dialog.
@@ -120,6 +137,7 @@ const DIALOG_STYLES = twv.tv({
     hideCloseButton: false,
     size: 'medium',
     padding: 'medium',
+    rounded: 'xxlarge',
   },
 })
 
@@ -146,6 +164,7 @@ export function Dialog(props: DialogProps) {
     rounded,
     padding = type === 'modal' ? 'medium' : 'xlarge',
     fitContent,
+    variants = DIALOG_STYLES,
     ...ariaDialogProps
   } = props
 
@@ -155,11 +174,13 @@ export function Dialog(props: DialogProps) {
    * Handles the scroll event on the dialog content.
    */
   const handleScroll = (scrollTop: number) => {
-    if (scrollTop > 0) {
-      setIsScrolledToTop(false)
-    } else {
-      setIsScrolledToTop(true)
-    }
+    React.startTransition(() => {
+      if (scrollTop > 0) {
+        setIsScrolledToTop(false)
+      } else {
+        setIsScrolledToTop(true)
+      }
+    })
   }
 
   const dialogId = aria.useId()
@@ -169,7 +190,7 @@ export function Dialog(props: DialogProps) {
   const overlayState = React.useRef<aria.OverlayTriggerState | null>(null)
   const root = portal.useStrictPortalContext()
 
-  const styles = DIALOG_STYLES({
+  const styles = variants({
     className,
     type,
     rounded,
@@ -200,11 +221,7 @@ export function Dialog(props: DialogProps) {
   return (
     <aria.ModalOverlay
       className={({ isEntering, isExiting }) =>
-        OVERLAY_STYLES({
-          isEntering,
-          isExiting,
-          blockInteractions: !isDismissable,
-        })
+        OVERLAY_STYLES({ isEntering, isExiting, blockInteractions: !isDismissable })
       }
       isDismissable={isDismissable}
       isKeyboardDismissDisabled={isKeyboardDismissDisabled}
@@ -218,7 +235,7 @@ export function Dialog(props: DialogProps) {
 
         return (
           <aria.Modal
-            className={MODAL_STYLES}
+            className={({ isEntering, isExiting }) => MODAL_STYLES({ type, isEntering, isExiting })}
             isDismissable={isDismissable}
             isKeyboardDismissDisabled={isKeyboardDismissDisabled}
             UNSTABLE_portalContainer={root}
@@ -252,25 +269,23 @@ export function Dialog(props: DialogProps) {
                 {(opts) => {
                   return (
                     <dialogProvider.DialogProvider value={{ close: opts.close, dialogId }}>
-                      {((!hideCloseButton && closeButton !== 'floating') || title != null) && (
-                        <aria.Header className={styles.header({ scrolledToTop: isScrolledToTop })}>
-                          <ariaComponents.CloseButton
-                            className={styles.closeButton()}
-                            onPress={opts.close}
-                          />
+                      <aria.Header className={styles.header({ scrolledToTop: isScrolledToTop })}>
+                        <ariaComponents.CloseButton
+                          className={styles.closeButton()}
+                          onPress={opts.close}
+                        />
 
-                          {title != null && (
-                            <ariaComponents.Text.Heading
-                              id={titleId}
-                              level={2}
-                              className={styles.heading()}
-                              weight="semibold"
-                            >
-                              {title}
-                            </ariaComponents.Text.Heading>
-                          )}
-                        </aria.Header>
-                      )}
+                        {title != null && (
+                          <ariaComponents.Text.Heading
+                            id={titleId}
+                            level={2}
+                            className={styles.heading()}
+                            weight="semibold"
+                          >
+                            {title}
+                          </ariaComponents.Text.Heading>
+                        )}
+                      </aria.Header>
 
                       <div
                         ref={(ref) => {

app/dashboard/src/components/AriaComponents/Dialog/variants.ts
@@ -14,19 +14,4 @@ export const DIALOG_BACKGROUND = twv.tv({
 export const DIALOG_STYLES = twv.tv({
   extend: DIALOG_BACKGROUND,
   base: 'flex flex-col text-left align-middle shadow-xl',
-  variants: {
-    rounded: {
-      none: '',
-      small: 'rounded-sm',
-      medium: 'rounded-md',
-      large: 'rounded-lg',
-      xlarge: 'rounded-xl',
-      xxlarge: 'rounded-2xl',
-      xxxlarge: 'rounded-3xl',
-      xxxxlarge: 'rounded-4xl',
-    },
-  },
-  defaultVariants: {
-    rounded: 'xxlarge',
-  },
 })

app/dashboard/src/modules/payments/components/PlanSelector/PlanSelector.tsx
@@ -7,6 +7,8 @@ import { useAuth } from '#/providers/AuthProvider'
 import { useRemoteBackendStrict } from '#/providers/BackendProvider'
 import { useText } from '#/providers/TextProvider'
 import { Plan, PLANS } from '#/services/Backend'
+import type { VariantProps } from '#/utilities/tailwindVariants'
+import { tv } from '#/utilities/tailwindVariants'
 import { Card } from './components'
 import { getComponentPerPlan } from './getComponentForPlan'
 
@@ -26,7 +28,7 @@ interface CreateCheckoutSessionMutation {
 /**
  * Props for {@link PlanSelector}
  */
-export interface PlanSelectorProps {
+export interface PlanSelectorProps extends VariantProps<typeof PLAN_SELECTOR_STYLES> {
   readonly showFreePlan?: boolean
   readonly hasTrial?: boolean
   readonly userPlan?: Plan | undefined
@@ -35,6 +37,26 @@ export interface PlanSelectorProps {
   readonly onSubscribeSuccess?: (plan: Plan, paymentMethodId: string) => void
   readonly onSubscribeError?: (error: Error) => void
 }
+
+const PLAN_SELECTOR_STYLES = tv({
+  base: DIALOG_BACKGROUND({
+    className: 'w-full snap-x overflow-auto rounded-4xl scroll-hidden',
+  }),
+  variants: {
+    showFreePlan: {
+      true: {
+        grid: 'grid-cols-1fr md:grid-cols-2 xl:grid-cols-4',
+      },
+      false: {
+        grid: 'grid-cols-1fr md:grid-cols-3 justify-center',
+      },
+    },
+  },
+  slots: {
+    grid: 'inline-grid min-w-full gap-6 p-6',
+    card: 'min-w-64 snap-center',
+  },
+})
 
 /**
  * Plan selector component.
@@ -49,6 +71,7 @@ export function PlanSelector(props: PlanSelectorProps) {
     showFreePlan = true,
     hasTrial = true,
     isOrganizationAdmin = false,
+    variants = PLAN_SELECTOR_STYLES,
   } = props
 
   const { getText } = useText()
@@ -77,13 +100,11 @@ export function PlanSelector(props: PlanSelectorProps) {
     onError: (error) => onSubscribeError?.(error),
   })
 
+  const classes = variants({ showFreePlan })
+
   return (
-    <div
-      className={DIALOG_BACKGROUND({
-        className: 'w-full snap-x overflow-auto rounded-4xl scroll-hidden',
-      })}
-    >
-      <div className="inline-grid min-w-full grid-cols-1fr gap-6 p-6 md:grid-cols-2 xl:grid-cols-4">
+    <div className={classes.base()}>
+      <div className={classes.grid()}>
         {PLANS.map((newPlan) => {
           const paywallLevel = getPaywallLevel(newPlan)
           const userPaywallLevel = getPaywallLevel(userPlan)
@@ -96,7 +117,7 @@ export function PlanSelector(props: PlanSelectorProps) {
             return (
               <Card
                 key={newPlan}
-                className="min-w-64 snap-center"
+                className={classes.card()}
                 features={planProps.features}
                 subtitle={planProps.subtitle}
                 title={planProps.title}
@@ -112,6 +133,7 @@ export function PlanSelector(props: PlanSelectorProps) {
                       })
 
                       const startEpochMs = Number(new Date())
+
                       while (true) {
                         const { data: session } = await refetchSession()
                         if (session && 'user' in session && session.user.plan === newPlan) {

app/dashboard/src/pages/authentication/Setup/Setup.tsx
@@ -68,6 +68,7 @@ const BASE_STEPS: Step[] = [
       const userSession = useUserSession()
       const { getText } = textProvider.useText()
 
+      const isUserCreated = userSession?.type === UserSessionType.full
       const defaultName =
         session && 'user' in session ? session.user.name : userSession?.email ?? ''
 
@@ -85,7 +86,8 @@ const BASE_STEPS: Step[] = [
           }
           defaultValues={{ username: defaultName }}
           onSubmit={({ username }) => {
-            if (username === defaultName) {
+            // If user is already created we shouldn't call `setUsername` if value wasn't changed
+            if (username === defaultName && isUserCreated) {
               goToNextStep()
               return
             } else {