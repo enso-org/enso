3f158f799e31e1f9afc4e8ee1584ced351602fbc
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2024-09-10 17:58:01 +0200

    Workaround for Electron sanbox issues on Ubuntu
    Disabling sanbox, while a non-secure option, appears to be currently the
    only option to workaround Electron issues on latest Ubuntu (24.04).
    When passed to Enso it is treated as a regular argument, preventing the
    start of the product.
    This change treats `--no-sandbox` specially by ignoring all arguments
    that precede it (and including itself).

--- app/ide-desktop/client/src/fileAssociations.ts
+++ app/ide-desktop/client/src/fileAssociations.ts
@@ -80,8 +80,13 @@ function getClientArguments(args = process.argv): readonly string[] {
       return args.slice(separatorIndex + 1)
     }
   } else {
-    // Drop the leading executable name.
-    return args.slice(1)
+    const noSandbox = args.indexOf('--no-sandbox')
+    if (noSandbox !== NOT_FOUND) {
+      return args.slice(noSandbox + 1)
+    } else {
+      // Drop the leading executable name.
+      return args.slice(1)
+    }
   }
 }
 
