53e101566a9c8bd860fe37835eb2507b57253549
Adam Obuchowicz <adam.obuchowicz@enso.org>
2024-09-16 16:46:58 +0200

    Fix Numeric Input cooperation with Drop Down (#11091)
    Fixes #11063
    
    The blur on Numeric Input was removed in https://github.com/enso-org/enso/pull/10337 and then reintroduced in https://github.com/enso-org/enso/pull/10512. I guess it was to handle `tab` properly, so I added another way of handling `tab` and removed blur again (with explanation).

--- app/gui2/src/components/GraphEditor/widgets/WidgetText.vue
+++ app/gui2/src/components/GraphEditor/widgets/WidgetText.vue
@@ -98,6 +98,12 @@ export const widgetDefinition = defineWidget(
 <template>
   <label ref="widgetRoot" class="WidgetText widgetRounded">
     <NodeWidget v-if="shownLiteral.open" :input="WidgetInput.FromAst(shownLiteral.open)" />
+    <!-- Do not finish edit on blur here!
+  
+    It is tempting, but it breaks the cooperation with possible drop-down widget. Blur may be done on
+    pointerdown, and if it would end the interaction, the drop down would also be hidden, making 
+    any `click` event on it impossible.
+    -->
     <AutoSizedInput
       ref="input"
       v-model="editedContents"
