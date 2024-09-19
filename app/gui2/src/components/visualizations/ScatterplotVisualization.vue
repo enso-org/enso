f0103ad1227983f74ce07a85c6734e7dbb6e2442
Martha King <martha_king@hotmail.co.uk>
2024-09-19 13:53:03 +0100

    remove time support for now

--- app/gui2/src/components/visualizations/ScatterplotVisualization.vue
+++ app/gui2/src/components/visualizations/ScatterplotVisualization.vue
@@ -540,51 +540,15 @@ const filterPattern = computed(() => Pattern.parse('__ (..Between __ __)'))
 const makeFilterPattern = (
   module: Ast.MutableModule,
   columnName: string,
-  minVal: number | Date,
-  maxVal: number | Date,
+  min: number,
+  max: number,
 ) => {
-  const min =
-    typeof minVal === 'number' ?
-      Ast.tryNumberToEnso(minVal, module)!
-    : makeTimePattern(minVal, module)
-  const max =
-    typeof maxVal === 'number' ?
-      Ast.tryNumberToEnso(maxVal, module)!
-    : makeTimePattern(maxVal, module)
-  return filterPattern.value.instantiateCopied([Ast.TextLiteral.new(columnName), min, max])
+  return filterPattern.value.instantiateCopied([
+    Ast.TextLiteral.new(columnName),
+    Ast.tryNumberToEnso(min, module)!,
+    Ast.tryNumberToEnso(max, module)!,
+  ])
 }
-
-const makeTimePattern = (val: Date, module: Ast.MutableModule) => {
-  switch (data.value.x_value_type) {
-    case 'Time':
-      return timePattern.value.instantiateCopied([
-        Ast.tryNumberToEnso(val.getHours(), module)!,
-        Ast.tryNumberToEnso(val.getMinutes(), module)!,
-        Ast.tryNumberToEnso(val.getSeconds(), module)!,
-      ])
-    case 'Date_Time':
-      return dateTimePattern.value.instantiateCopied([
-        Ast.tryNumberToEnso(val.getFullYear(), module)!,
-        Ast.tryNumberToEnso(val.getMonth(), module)!,
-        Ast.tryNumberToEnso(val.getDate(), module)!,
-        Ast.tryNumberToEnso(val.getHours(), module)!,
-        Ast.tryNumberToEnso(val.getMinutes(), module)!,
-        Ast.tryNumberToEnso(val.getSeconds(), module)!,
-      ])
-    default:
-      return datePattern.value.instantiateCopied([
-        Ast.tryNumberToEnso(val.getFullYear(), module)!,
-        Ast.tryNumberToEnso(val.getMonth(), module)!,
-        Ast.tryNumberToEnso(val.getDate(), module)!,
-      ])
-  }
-}
-
-const datePattern = computed(() => Pattern.parse('(Date.new __ __ __)'))
-
-const dateTimePattern = computed(() => Pattern.parse('(Date_Time.new __ __ __ __ __ __)'))
-
-const timePattern = computed(() => Pattern.parse('(Time_Of_Day.new __ __ __'))
 
 function getAstPatternFilter(columnName: string, min: number, max: number) {
   return Pattern.new((ast) =>
@@ -622,7 +586,6 @@ function createNode(rowNumber: number) {
 // === Update contents ===
 
 watchPostEffect(() => {
-  console.log(isBrushing.value)
   const xScale_ = xScale.value
   const yScale_ = yScale.value
   const plotData = getPlotData(data.value) as Point[]
