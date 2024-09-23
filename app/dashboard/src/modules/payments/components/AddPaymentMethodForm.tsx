3d5b7f88127da25b4a27134e6c39065c1b866df3
somebody1234 <ehern.lee@gmail.com>
2024-09-23 12:36:19 +1000

    Fix test for solo plan

--- app/dashboard/src/modules/payments/components/AddPaymentMethodForm.tsx
+++ app/dashboard/src/modules/payments/components/AddPaymentMethodForm.tsx
@@ -2,13 +2,15 @@
 import { CardElement } from '@stripe/react-stripe-js'
 import type { PaymentMethod, Stripe, StripeCardElement, StripeElements } from '@stripe/stripe-js'
 
-import { Form, type FormInstance, useDialogContext } from '#/components/AriaComponents'
-import { useText } from '#/providers/TextProvider'
+import { Form, useDialogContext, type FormInstance, type schema } from '#/components/AriaComponents'
+import { useText, type GetText } from '#/providers/TextProvider'
 import { useCreatePaymentMethodMutation } from '../api/createPaymentMethod'
 
 /** Props for an {@link AddPaymentMethodForm}. */
 export interface AddPaymentMethodFormProps<
-  Schema extends typeof ADD_PAYMENT_METHOD_FORM_SCHEMA = typeof ADD_PAYMENT_METHOD_FORM_SCHEMA,
+  Schema extends ReturnType<typeof createAddPaymentMethodFormSchema> = ReturnType<
+    typeof createAddPaymentMethodFormSchema
+  >,
 > {
   readonly stripeInstance: Stripe
   readonly elements: StripeElements
@@ -17,54 +19,59 @@ export interface AddPaymentMethodFormProps<
   readonly form?: FormInstance<Schema>
 }
 
-export const ADD_PAYMENT_METHOD_FORM_SCHEMA = Form.schema.object({
-  card: Form.schema
-    .object(
-      {
-        complete: Form.schema.boolean(),
-        error: Form.schema.object({ message: Form.schema.string() }).nullish(),
-      },
-      { message: 'This field is required' },
-    )
-    .nullable()
-    .refine(
-      (data) => data?.error == null,
-      (data) => ({ message: data?.error?.message ?? 'This field is required' }),
-    ),
-  cardElement: Form.schema.custom<StripeCardElement | null>(),
-  stripeInstance: Form.schema.custom<Stripe>(),
-})
+/** The validation schema for this form. */
+export function createAddPaymentMethodFormSchema(z: typeof schema, getText: GetText) {
+  return z.object({
+    card: z
+      .object(
+        {
+          complete: z.boolean(),
+          error: z.object({ message: z.string() }).nullish(),
+        },
+        { message: getText('arbitraryFieldRequired') },
+      )
+      .nullable()
+      .refine(
+        (data) => data?.error == null,
+        (data) => ({ message: data?.error?.message ?? getText('arbitraryFieldRequired') }),
+      ),
+    cardElement: z.custom<StripeCardElement | null | undefined>(),
+    stripeInstance: z.custom<Stripe>(),
+  })
+}
 
 /**
  * A form for adding a payment method.
  */
 export function AddPaymentMethodForm<
-  Schema extends typeof ADD_PAYMENT_METHOD_FORM_SCHEMA = typeof ADD_PAYMENT_METHOD_FORM_SCHEMA,
+  Schema extends ReturnType<typeof createAddPaymentMethodFormSchema> = ReturnType<
+    typeof createAddPaymentMethodFormSchema
+  >,
 >(props: AddPaymentMethodFormProps<Schema>) {
-  const { stripeInstance, onSubmit, submitText, form } = props
-
+  const { stripeInstance, onSubmit, submitText, form: formRaw } = props
   const { getText } = useText()
-
   const dialogContext = useDialogContext()
-
   const createPaymentMethodMutation = useCreatePaymentMethodMutation()
 
-  // No idea if it's safe or not, but outside of the function everything is fine
-  // but for some reason TypeScript fails to infer the `card` field from the schema (it should always be there)
-  // eslint-disable-next-line no-restricted-syntax
-  const formInstance = Form.useForm(
-    form ?? {
-      schema: ADD_PAYMENT_METHOD_FORM_SCHEMA,
+  const form = Form.useForm(
+    formRaw ?? {
+      mode: 'onChange',
+      schema: (z) => createAddPaymentMethodFormSchema(z, getText),
       onSubmit: ({ cardElement }) =>
         createPaymentMethodMutation.mutateAsync({ stripeInstance, cardElement }),
       onSubmitSuccess: ({ paymentMethod }) => onSubmit?.(paymentMethod.id),
     },
-  ) as unknown as FormInstance<typeof ADD_PAYMENT_METHOD_FORM_SCHEMA>
+  )
 
-  const cardElement = Form.useWatch({ control: formInstance.control, name: 'cardElement' })
+  const cardElement =
+    // FIXME[sb]: I do not understand why `useWatch` is not sufficient for Playwright.
+    // (The value is always `undefined` with `useWatch` alone)
+    // It is worth noting that E2E tests previously worked without requiring this change - as of:
+    // 1500849c32f70f5f4d95240b7e31377c649dc25b
+    Form.useWatch({ control: form.control, name: 'cardElement' }) ?? form.getValues().cardElement
 
   return (
-    <Form method="dialog" form={formInstance}>
+    <Form method="dialog" form={form}>
       <Form.Field name="card" fullWidth label={getText('bankCardLabel')}>
         <CardElement
           options={{
@@ -77,19 +84,19 @@ export function AddPaymentMethodForm<
           }}
           onEscape={() => dialogContext?.close()}
           onReady={(element) => {
-            formInstance.setValue('cardElement', element)
-            formInstance.setValue('stripeInstance', stripeInstance)
+            form.setValue('cardElement', element)
+            form.setValue('stripeInstance', stripeInstance)
           }}
           onChange={(event) => {
             if (event.error?.message != null) {
-              formInstance.setError('card', { message: event.error.message })
+              form.setError('card', { message: event.error.message })
               cardElement?.focus()
             } else {
-              formInstance.clearErrors('card')
+              form.clearErrors('card')
             }
-            formInstance.setValue('card', event)
+            form.setValue('card', event)
           }}
-          onBlur={() => formInstance.trigger('card')}
+          onBlur={() => form.trigger('card')}
         />
       </Form.Field>
 
