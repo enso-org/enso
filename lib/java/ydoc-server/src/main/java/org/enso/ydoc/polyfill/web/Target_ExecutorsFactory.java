546ac9c6edd67f8a343f98bbdb8ae569854f8a63
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-09-24 06:47:22 +0200

    Making the ExecutorsFactory substitutions conditional

--- lib/java/ydoc-server/src/main/java/org/enso/ydoc/polyfill/web/Target_ExecutorsFactory.java
+++ lib/java/ydoc-server/src/main/java/org/enso/ydoc/polyfill/web/Target_ExecutorsFactory.java
@@ -5,9 +5,14 @@ import com.oracle.svm.core.annotate.TargetClass;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
+import java.util.function.Predicate;
 
-@TargetClass(className = "io.helidon.webserver.ExecutorsFactory")
+@TargetClass(
+    className = Target_ExecutorsFactory.CLASS_NAME,
+    onlyWith = Target_ExecutorsFactory.Available.class)
 final class Target_ExecutorsFactory {
+  static final String CLASS_NAME = "io.helidon.webserver.ExecutorsFactory";
+
   @Substitute
   private static ThreadFactory virtualThreadFactory() {
     return Thread.ofPlatform().name("Emulating loom", 0).factory();
@@ -16,5 +21,17 @@ final class Target_ExecutorsFactory {
   @Substitute
   public static ExecutorService newLoomServerVirtualThreadPerTaskExecutor() {
     return Executors.newThreadPerTaskExecutor(virtualThreadFactory());
+  }
+
+  static final class Available implements Predicate {
+    @Override
+    public boolean test(Object t) {
+      try {
+        Class.forName(CLASS_NAME);
+        return true;
+      } catch (ClassNotFoundException e) {
+        return false;
+      }
+    }
   }
 }
