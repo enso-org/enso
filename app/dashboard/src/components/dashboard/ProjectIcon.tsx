enso-org/enso

commit 609d5abbc07c95b774644edd10c629586c2f9ab5

Only allow creating single project at a time (#10586)
- Close https://github.com/enso-org/cloud-v2/issues/1390
- Show loading spinner and disable "New Empty Project" if the new project is still being created or opened.

# Important Notes
None

app/dashboard/src/components/dashboard/ProjectIcon.tsx
@@ -74,16 +74,17 @@ export default function ProjectIcon(props: ProjectIconProps) {
   const { user } = authProvider.useNonPartialUserSession()
   const { getText } = textProvider.useText()
 
-  const isRunningInBackground = item.projectState.executeAsync ?? false
   const {
-    data: status,
+    data: projectState,
     isLoading,
     isError,
   } = reactQuery.useQuery({
     ...projectHooks.createGetProjectDetailsQuery.createPassiveListener(item.id),
-    select: data => data.state.type,
+    select: data => data.state,
     enabled: isOpened,
   })
+  const status = projectState?.type
+  const isRunningInBackground = projectState?.executeAsync ?? false
 
   const isCloud = backend.type === backendModule.BackendType.remote
 
@@ -137,7 +138,6 @@ export default function ProjectIcon(props: ProjectIconProps) {
 
   switch (state) {
     case null:
-    case backendModule.ProjectState.created:
     case backendModule.ProjectState.new:
     case backendModule.ProjectState.closing:
     case backendModule.ProjectState.closed:
@@ -149,9 +149,11 @@ export default function ProjectIcon(props: ProjectIconProps) {
           aria-label={getText('openInEditor')}
           tooltipPlacement="left"
           extraClickZone="xsmall"
+          isDisabled={projectState?.type === backendModule.ProjectState.closing}
           onPress={doOpenProject}
         />
       )
+    case backendModule.ProjectState.created:
     case backendModule.ProjectState.openInProgress:
     case backendModule.ProjectState.scheduled:
     case backendModule.ProjectState.provisioned:

app/dashboard/src/App.tsx
@@ -48,7 +48,7 @@ import * as inputBindingsModule from '#/configurations/inputBindings'
 import * as backendHooks from '#/hooks/backendHooks'
 
 import AuthProvider, * as authProvider from '#/providers/AuthProvider'
-import BackendProvider from '#/providers/BackendProvider'
+import BackendProvider, { useLocalBackend, useRemoteBackend } from '#/providers/BackendProvider'
 import DevtoolsProvider from '#/providers/EnsoDevtoolsProvider'
 import * as httpClientProvider from '#/providers/HttpClientProvider'
 import InputBindingsProvider from '#/providers/InputBindingsProvider'
@@ -284,9 +284,6 @@ function AppRouter(props: AppRouterProps) {
     [httpClient, logger, getText]
   )
 
-  backendHooks.useObserveBackend(remoteBackend)
-  backendHooks.useObserveBackend(localBackend)
-
   if (detect.IS_DEV_MODE) {
     // @ts-expect-error This is used exclusively for debugging.
     window.navigate = navigate
@@ -512,6 +509,7 @@ function AppRouter(props: AppRouterProps) {
 
   let result = (
     <>
+      <MutationListener />
       <VersionChecker />
       {routes}
     </>
@@ -566,4 +564,19 @@ function AppRouter(props: AppRouterProps) {
   result = <DevtoolsProvider>{result}</DevtoolsProvider>
 
   return result
+}
+
+// ========================
+// === MutationListener ===
+// ========================
+
+/** A component that applies state updates for successful mutations. */
+function MutationListener() {
+  const remoteBackend = useRemoteBackend()
+  const localBackend = useLocalBackend()
+
+  backendHooks.useObserveBackend(remoteBackend)
+  backendHooks.useObserveBackend(localBackend)
+
+  return null
 }

app/dashboard/src/components/dashboard/AssetRow.tsx
@@ -427,8 +427,6 @@ export default function AssetRow(props: AssetRowProps) {
         case AssetEventType.newDatalink:
         case AssetEventType.newSecret:
         case AssetEventType.updateFiles:
-        case AssetEventType.openProject:
-        case AssetEventType.closeProject:
         case AssetEventType.projectClosed: {
           break
         }

app/dashboard/src/components/dashboard/DatalinkNameColumn.tsx
@@ -70,9 +70,7 @@ export default function DatalinkNameColumn(props: DatalinkNameColumnProps) {
         case AssetEventType.newFolder:
         case AssetEventType.uploadFiles:
         case AssetEventType.newSecret:
-        case AssetEventType.openProject:
         case AssetEventType.updateFiles:
-        case AssetEventType.closeProject:
         case AssetEventType.copy:
         case AssetEventType.cut:
         case AssetEventType.cancelCut:

app/dashboard/src/components/dashboard/DirectoryNameColumn.tsx
@@ -96,9 +96,7 @@ export default function DirectoryNameColumn(props: DirectoryNameColumnProps) {
         case AssetEventType.uploadFiles:
         case AssetEventType.newDatalink:
         case AssetEventType.newSecret:
-        case AssetEventType.openProject:
         case AssetEventType.updateFiles:
-        case AssetEventType.closeProject:
         case AssetEventType.copy:
         case AssetEventType.cut:
         case AssetEventType.cancelCut:

app/dashboard/src/components/dashboard/FileNameColumn.tsx
@@ -92,8 +92,6 @@ export default function FileNameColumn(props: FileNameColumnProps) {
         case AssetEventType.newFolder:
         case AssetEventType.newDatalink:
         case AssetEventType.newSecret:
-        case AssetEventType.openProject:
-        case AssetEventType.closeProject:
         case AssetEventType.copy:
         case AssetEventType.cut:
         case AssetEventType.cancelCut:

app/dashboard/src/components/dashboard/ProjectNameColumn.tsx
@@ -141,8 +141,6 @@ export default function ProjectNameColumn(props: ProjectNameColumnProps) {
         case AssetEventType.newFolder:
         case AssetEventType.newDatalink:
         case AssetEventType.newSecret:
-        case AssetEventType.openProject:
-        case AssetEventType.closeProject:
         case AssetEventType.copy:
         case AssetEventType.cut:
         case AssetEventType.cancelCut:
@@ -189,6 +187,7 @@ export default function ProjectNameColumn(props: ProjectNameColumnProps) {
                       event.versionId,
                       asset.title,
                     ])
+              event.onCreated?.(createdProject)
               rowState.setVisibility(Visibility.visible)
               setAsset(
                 object.merge(asset, {
@@ -205,6 +204,7 @@ export default function ProjectNameColumn(props: ProjectNameColumnProps) {
                 title: asset.title,
               })
             } catch (error) {
+              event.onError?.()
               dispatchAssetListEvent({ type: AssetListEventType.delete, key: item.key })
               toastAndLog('createProjectError', error)
             }

app/dashboard/src/components/dashboard/SecretNameColumn.tsx
@@ -70,9 +70,7 @@ export default function SecretNameColumn(props: SecretNameColumnProps) {
         case AssetEventType.newFolder:
         case AssetEventType.uploadFiles:
         case AssetEventType.newDatalink:
-        case AssetEventType.openProject:
         case AssetEventType.updateFiles:
-        case AssetEventType.closeProject:
         case AssetEventType.copy:
         case AssetEventType.cut:
         case AssetEventType.cancelCut:

commit f6c262c0628df0d39b039804ad2252aed736a812

Do not show loading spinner for created projects (#11164)
- Fix https://github.com/enso-org/cloud-v2/issues/1501
- The `Created` project state does not mean the project is opening, so the loading spinner should not appear.
- Regression accidentally introduced by myself in https://github.com/enso-org/enso/commit/609d5abbc07c95b774644edd10c629586c2f9ab5#diff-79e1f8800be680c64a41e2c428927c1f176b728a6a24d5f8c4d02cfc2a3922f9

# Important Notes
None

app/dashboard/src/components/dashboard/ProjectIcon.tsx
@@ -134,6 +134,7 @@ export default function ProjectIcon(props: ProjectIconProps) {
     case backendModule.ProjectState.new:
     case backendModule.ProjectState.closing:
     case backendModule.ProjectState.closed:
+    case backendModule.ProjectState.created:
       return (
         <ariaComponents.Button
           size="custom"
@@ -146,7 +147,6 @@ export default function ProjectIcon(props: ProjectIconProps) {
           onPress={doOpenProject}
         />
       )
-    case backendModule.ProjectState.created:
     case backendModule.ProjectState.openInProgress:
     case backendModule.ProjectState.scheduled:
     case backendModule.ProjectState.provisioned: