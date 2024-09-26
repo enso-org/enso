12d08c8cc22eab80212c9d7d04e625ef9cfca7a5
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-09-26 07:48:50 +0200

    Avoid concurrency in native code by synchronizing parser methods

--- engine/runtime-parser/src/main/java/org/enso/compiler/core/EnsoParser.java
+++ engine/runtime-parser/src/main/java/org/enso/compiler/core/EnsoParser.java
@@ -23,31 +23,31 @@ public final class EnsoParser implements AutoCloseable {
   }
 
   @Override
-  public void close() throws Exception {
+  public synchronized void close() throws Exception {
     if (parser != null) {
       parser.close();
     }
   }
 
-  public Module compile(CharSequence src) {
+  public synchronized Module compile(CharSequence src) {
     var tree = parser.parse(src);
     return generateIR(tree);
   }
 
-  public Tree parse(CharSequence src) {
+  public synchronized Tree parse(CharSequence src) {
     return parser.parse(src);
   }
 
-  public Module generateIR(Tree t) {
+  public synchronized Module generateIR(Tree t) {
     return TreeToIr.MODULE.translate(t);
   }
 
-  public Module generateModuleIr(Tree t, Map<Location, UUID> idMap) {
+  public synchronized Module generateModuleIr(Tree t, Map<Location, UUID> idMap) {
     var treeToIr = new TreeToIr(idMap);
     return treeToIr.translate(t);
   }
 
-  public scala.Option<Expression> generateIRInline(Tree t) {
+  public synchronized scala.Option<Expression> generateIRInline(Tree t) {
     return TreeToIr.MODULE.translateInline(t);
   }
 }
