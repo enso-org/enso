0d23eeb1de0340586a6905b82acb3810ac02367f
Radosław Waśko <radoslaw.wasko@enso.org>
2024-10-02 14:39:50 +0200

    handle escape codes

--- build/ci_utils/src/program/command.rs
+++ build/ci_utils/src/program/command.rs
@@ -496,26 +496,23 @@ pub fn spawn_log_processor(
 /// Checks if the line contains a GitHub command and extracts it from the line if it does.
 /// Currently only error and group commands are supported. All commands are documented at https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions
 fn extract_github_command(line: &str) -> Option<String> {
-    if line.contains("group") {
-        let bytes = line.as_bytes();
-        info!("GROUP? line='{line}', bytes={bytes:?}");
-    }
-
     // We remove a possible [info] prefix that is added by sbt.
-    let trimmed = line.trim_start().strip_prefix("[info]").unwrap_or(line).trim_start();
-    let tmp = trimmed.strip_prefix("::");
-    if let Some(remaining) = tmp {
-        if remaining.starts_with("error")
-            || remaining.starts_with("group")
-            || remaining.starts_with("endgroup")
+    // We need to be careful as the [info] text can contain ANSI escape codes, so simple text
+    // matching for it won't work. Instead we locate `::`. If the line starts with `::` and the
+    // following text is `error`, `group`, or `endgroup`, we return the line as a special command.
+    let command_prefix = "::";
+    if let Some(start_of_command) = line.find(command_prefix) {
+        let command_part = &line[start_of_command + command_prefix.len()..];
+        if command_part.starts_with("error")
+            || command_part.starts_with("group")
+            || command_part.starts_with("endgroup")
         {
+            // We now remove the stripped [info] prefix and return the rest of the line.
+            let trimmed = &line[start_of_command..];
             return Some(trimmed.to_string());
         }
-    };
-
-    if line.contains("group") {
-        info!("GROUP??? line='{line}', trimmed={trimmed}, tmp={tmp:?}")
     }
+
     None
 }
 
