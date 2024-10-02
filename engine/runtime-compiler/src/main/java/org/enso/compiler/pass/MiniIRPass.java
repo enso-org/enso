a4aaf8e0e57b7ffa5ede2910d0cf21bbb1d5ddfe
Pavel Marek <pavel.marek@enso.org>
2024-10-02 19:18:52 +0200

    Add docs to MiniIRPass

--- engine/runtime-compiler/src/main/java/org/enso/compiler/pass/MiniIRPass.java
+++ engine/runtime-compiler/src/main/java/org/enso/compiler/pass/MiniIRPass.java
@@ -5,13 +5,27 @@ import org.enso.compiler.core.ir.ProcessingPass;
 
 /**
  * Mini IR pass operates on a single IR element at a time. The {@link org.enso.compiler.Compiler}
- * traverses the whole IR tree bottom-up from leaves to the root. For each IR element:
+ * traverses the whole IR tree in DFS. It works in two phases.
+ *
+ * <p>In the first, <b>prepare</b> phase, the compiler traverses from the root to the leaves and
+ * calls the {@link #prepare(IR)} method on the mini pass. During this phase, the mini pass can
+ * gather information about the current IR element, but not modify it.
+ *
+ * <p>In the second, <b>transform</b> phase, the compiler returns from the leaves to the root and
+ * calls the {@link #transformIr(IR)} method on the mini pass. During this phase, the mini pass is
+ * free to transform the current IR element. The children of the current IR element are already
+ * transformed.
+ *
+ * <p>For each IR element:
  *
  * <ol>
- *   <li>The {@link #prepare(IR)} method is called to prepare the pass for the current IR
- *       element. This method is called when the {@link org.enso.compiler.Compiler} traverses the
- *       element from top to bottom. This is useful for mini passes that need to build some
- *       information about the current IR element before transforming it.
+ *   <li>The {@link #prepare(IR)} method is called to prepare the pass for the current IR element.
+ *       This method is called when the {@link org.enso.compiler.Compiler} traverses the IR tree
+ *       from top to bottom. This is useful for mini passes that need to build some information
+ *       about the current IR element before transforming it. The mini pass must not modify the IR
+ *       element neither attach any metadata to it in this method. By returning {@code null} from
+ *       this method, the mini pass signals to the compiler that it wishes to not process the
+ *       subtree of the current IR element.
  *   <li>The {@link #transformIr(IR)} method is called to transform the current IR element. This
  *       method is called when the {@link org.enso.compiler.Compiler} traverses the element from
  *       bottom to top. All the children of the current IR element are already transformed when this
@@ -24,8 +38,16 @@ import org.enso.compiler.core.ir.ProcessingPass;
  */
 public abstract class MiniIRPass implements ProcessingPass {
   /**
-   * Prepare the pass for the provided IR element. This method is called when the
-   * {@link org.enso.compiler.Compiler} traverses the IR element from top to bottom.
+   * Prepare the pass for the provided IR element. This method is called when the {@link
+   * org.enso.compiler.Compiler} traverses the IR element from top to bottom.
+   *
+   * <p>The mini pass is free to gather any information about the elements it encounters (via this
+   * method) and use it in the {@link #transformIr(IR)} method. Note however, that it is not wise to
+   * store the references to the IR or their children for later comparison in the {@link
+   * #transformIr(IR) transform phase}, as the IR tree will most likely be transformed during the
+   * compilation process.
+   *
+   * <p>TL;DR; Do no store references to the IR elements or their children in this method.
    *
    * @param current IR element to be prepared for transformation.
    * @return May be null if the pass does not want to traverse anything anymore.
