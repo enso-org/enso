2bd96d4e46aade0ca44bc08d6dab0a918502dc75
Pavel Marek <pavel.marek@enso.org>
2024-10-07 19:04:32 +0200

    FileSystemSPI uses Lookup instead of ServiceLoader

--- std-bits/base/src/main/java/org/enso/base/file_system/FileSystemSPI.java
+++ std-bits/base/src/main/java/org/enso/base/file_system/FileSystemSPI.java
@@ -1,14 +1,13 @@
 package org.enso.base.file_system;
 
 import java.util.ServiceLoader;
+import org.enso.base.lookup.Lookup;
 import org.enso.base.polyglot.EnsoMeta;
 import org.graalvm.polyglot.Value;
 
 public abstract class FileSystemSPI {
-  private static final ServiceLoader<org.enso.base.file_system.FileSystemSPI> loader =
-      ServiceLoader.load(
-          org.enso.base.file_system.FileSystemSPI.class,
-          org.enso.base.file_format.FileFormatSPI.class.getClassLoader());
+  private static final Lookup<org.enso.base.file_system.FileSystemSPI> loader =
+      Lookup.lookup((l) -> ServiceLoader.load(l, org.enso.base.file_system.FileSystemSPI.class));
 
   public static Value get_type(String protocol, boolean refresh) {
     if (refresh) {
