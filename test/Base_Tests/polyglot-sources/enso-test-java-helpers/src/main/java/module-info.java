f0c2ae450d1b27aa4779f14fa34f57553cbcc992
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-10-08 08:16:48 +0200

    std.table module isn't present in NI enso build

--- test/Base_Tests/polyglot-sources/enso-test-java-helpers/src/main/java/module-info.java
+++ test/Base_Tests/polyglot-sources/enso-test-java-helpers/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module org.enso.test.java_helpers {
   requires static org.graalvm.polyglot;
-  requires org.enso.std.table;
+  requires static org.enso.std.table;
 
   exports org.enso.base_test_helpers;
   exports org.enso.table_test_helpers;
