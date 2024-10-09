08b0571164d0aeb42c9442373de7cba8698510b9
Pavel Marek <pavel.marek@enso.org>
2024-10-09 20:38:18 +0200

    IRProcessor does some sanity checks

--- engine/runtime-parser-processor/src/main/java/org/enso/runtime/parser/processor/IRProcessor.java
+++ engine/runtime-parser-processor/src/main/java/org/enso/runtime/parser/processor/IRProcessor.java
@@ -5,7 +5,12 @@ import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
+import javax.tools.Diagnostic.Kind;
+import org.enso.runtime.parser.dsl.IRNode;
 
 @SupportedAnnotationTypes({
   "org.enso.runtime.parser.dsl.IRNode",
@@ -20,6 +25,29 @@ public class IRProcessor extends AbstractProcessor {
 
   @Override
   public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-    throw new UnsupportedOperationException("unimplemented");
+    var irNodeElems = roundEnv.getElementsAnnotatedWith(IRNode.class);
+    for (var irNodeElem : irNodeElems) {
+      processIrNode(irNodeElem);
+    }
+    return true;
+  }
+
+  private void processIrNode(Element irNodeElem) {
+    if (irNodeElem.getKind() != ElementKind.INTERFACE) {
+      printError("IRNode annotation can only be applied to interfaces", irNodeElem);
+    }
+    if (!isSubtypeOfIR(irNodeElem.asType())) {
+      printError("Interface annotated with @IRNode must be a subtype of IR interface", irNodeElem);
+    }
+  }
+
+  private boolean isSubtypeOfIR(TypeMirror type) {
+    var irType =
+        processingEnv.getElementUtils().getTypeElement("org.enso.compiler.core.IR").asType();
+    return processingEnv.getTypeUtils().isAssignable(type, irType);
+  }
+
+  private void printError(String msg, Element elem) {
+    processingEnv.getMessager().printMessage(Kind.ERROR, msg, elem);
   }
 }
--- engine/runtime-parser-processor/src/test/java/org/enso/runtime/parser/processor/test/TestIRProcessor.java
+++ engine/runtime-parser-processor/src/test/java/org/enso/runtime/parser/processor/test/TestIRProcessor.java
@@ -1,34 +1,72 @@
 package org.enso.runtime.parser.processor.test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
-
-import com.google.testing.compile.Compilation;
+import com.google.testing.compile.CompilationSubject;
 import com.google.testing.compile.Compiler;
 import com.google.testing.compile.JavaFileObjects;
 import org.enso.runtime.parser.processor.IRProcessor;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class TestIRProcessor {
   @Test
-  public void testIRProcessorFailsWithUnimplemented() {
+  public void simpleIRNodeWithoutChildren_CompilationSucceeds() {
     var src =
-        JavaFileObjects.forSourceLines(
-            "HelloWorld",
-            "import org.enso.runtime.parser.dsl.IRNode;",
-            "@IRNode(name = \"HelloWorld\")",
-            "final class HelloWorld {",
-            "}");
+        JavaFileObjects.forSourceString(
+            "JName",
+            """
+        import org.enso.runtime.parser.dsl.IRNode;
+        import org.enso.compiler.core.IR;
+        @IRNode
+        public interface JName extends IR {}
+        """);
     var compiler = Compiler.javac().withProcessors(new IRProcessor());
-    Compilation compilation = null;
-    try {
-      compilation = compiler.compile(src);
-      Assert.fail("Expected compilation to fail with UnimplementedException");
-    } catch (Exception e) {
-      // nop
-      assertThat(compilation, is(nullValue()));
-    }
+    var compilation = compiler.compile(src);
+    CompilationSubject.assertThat(compilation).succeeded();
+  }
+
+  @Test
+  public void annotatedInterfaceMustExtendIR() {
+    var src =
+        JavaFileObjects.forSourceString(
+            "Hello",
+            """
+        import org.enso.runtime.parser.dsl.IRNode;
+        @IRNode
+        public interface Hello {}
+        """);
+    var compiler = Compiler.javac().withProcessors(new IRProcessor());
+    var compilation = compiler.compile(src);
+    CompilationSubject.assertThat(compilation).failed();
+  }
+
+  @Test
+  public void annotationCanOnlyBeAppliedToInterface() {
+    var src =
+        JavaFileObjects.forSourceString(
+            "Hello",
+            """
+        import org.enso.runtime.parser.dsl.IRNode;
+        @IRNode
+        public class Hello {}
+        """);
+    var compiler = Compiler.javac().withProcessors(new IRProcessor());
+    var compilation = compiler.compile(src);
+    CompilationSubject.assertThat(compilation).failed();
+  }
+
+  @Test
+  public void simpleIRNodeWithoutChildren_GeneratesSource() {
+    var src =
+        JavaFileObjects.forSourceString(
+            "JName",
+            """
+        import org.enso.runtime.parser.dsl.IRNode;
+        import org.enso.compiler.core.IR;
+        @IRNode
+        public interface JName extends IR {}
+        """);
+    var compiler = Compiler.javac().withProcessors(new IRProcessor());
+    var compilation = compiler.compile(src);
+    CompilationSubject.assertThat(compilation).succeeded();
+    CompilationSubject.assertThat(compilation).generatedSourceFile("JNameGen").isNotNull();
   }
 }
