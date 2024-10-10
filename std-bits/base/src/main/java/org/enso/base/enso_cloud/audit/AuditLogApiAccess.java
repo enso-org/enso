a0fe3c3769a1aa1a2094a40d9bac0bc5601078f8
Radosław Waśko <radoslaw.wasko@enso.org>
2024-10-10 13:32:43 +0200

    debug logging

--- std-bits/base/src/main/java/org/enso/base/enso_cloud/audit/AuditLogApiAccess.java
+++ std-bits/base/src/main/java/org/enso/base/enso_cloud/audit/AuditLogApiAccess.java
@@ -144,6 +144,7 @@ class AuditLogApiAccess {
       if (job.completionNotification() != null) {
         job.completionNotification().complete(null);
       }
+      System.out.println("Log sent successfully: " + job.message().payload() + " to " + job.requestConfig().apiUri);
     }
   }
 
@@ -152,6 +153,7 @@ class AuditLogApiAccess {
       if (job.completionNotification() != null) {
         job.completionNotification().completeExceptionally(e);
       }
+      System.out.println("Failed to send log: " + job.message().payload() + " to " + job.requestConfig().apiUri + " because of " + e + " - " + e.getMessage());
     }
   }
 
