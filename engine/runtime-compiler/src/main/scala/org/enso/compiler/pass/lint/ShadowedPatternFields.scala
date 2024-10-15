b36fd1c01bb3a2489f43e7276a6e18ef728085db
Pavel Marek <pavel.marek@enso.org>
2024-10-15 21:26:08 +0200

    Migrate some passes to Mini passes (#11191)
    Gets ready for avoiding IR traversal by introducing _mini passes_ as proposed by #10981:
    - creates [MiniPassFactory](https://github.com/enso-org/enso/pull/11191/commits/762045a35745ccb08421ab08135882bac3479de3) (that extends common `IRProcessingPass`) to transform an `IR` element to another `IR` element
    - modifies `PassManager` to recognize such _mini passes_ and treat them in a special way - by using `MiniIRPass.compile`
    - `MiniIRPass.compile` is using `IR.mapExpressions` to traverse the `IR` - alternative approach [withNewChildren](https://github.com/enso-org/enso/pull/11191/commits/1abc70d33c47ca1700157de3649041dbac01067d) rejected for now, see _future work_ for details
    - unlike _mega passes_ `IRMiniPass.compile` **does not  recursively** traverse, but with 0964711ba944fccee20e7f2f0d5a3e8c07127355 it invokes each _mini pass_ at constant stack depth - way better for profiling
    - `MiniIRPass.prepare` _works on edges_ since ffd27dfe9b6fbbd59cccb35633cb802be097815c - there is `IRMiniPass prepare(parent, child)` to collect information while pre-order traversing from a particular `IR` parent to a particular `IR` child
    - `PassManager` rewritten to group _subsequent mini passes_ together by `MiniIRPass.combine` and really and traverse the `IR` just once - done in 2736a76
    - converted to _mini pass_: `LambdaShorthandToLambda`, `OperatorToFunction`, `SectionsToBinOp` and `TailCall`
    - tested for 1:1 compatibility by [converting original code to test code](https://github.com/enso-org/enso/pull/11191/commits/f54ba6d1623b7742048360baa12b7d5e776022cc) and _comparing `IR` produced by old and new_ implementations

--- engine/runtime-compiler/src/main/scala/org/enso/compiler/pass/lint/ShadowedPatternFields.scala
+++ engine/runtime-compiler/src/main/scala/org/enso/compiler/pass/lint/ShadowedPatternFields.scala
@@ -7,6 +7,7 @@ import org.enso.compiler.core.ir.{Expression, Module, Name, Pattern}
 import org.enso.compiler.core.ir.expression.{errors, warnings, Case}
 import org.enso.compiler.core.CompilerError
 import org.enso.compiler.pass.IRPass
+import org.enso.compiler.pass.IRProcessingPass
 import org.enso.compiler.pass.analyse.{
   AliasAnalysis,
   DataflowAnalysis,
@@ -34,16 +35,16 @@ case object ShadowedPatternFields extends IRPass {
   override type Metadata = IRPass.Metadata.Empty
   override type Config   = IRPass.Configuration.Default
 
-  override lazy val precursorPasses: Seq[IRPass] = List(
+  override lazy val precursorPasses: Seq[IRProcessingPass] = List(
     GenerateMethodBodies
   )
-  override lazy val invalidatedPasses: Seq[IRPass] = List(
+  override lazy val invalidatedPasses: Seq[IRProcessingPass] = List(
     AliasAnalysis,
     DataflowAnalysis,
     DemandAnalysis,
     IgnoredBindings,
     NestedPatternMatch,
-    TailCall
+    TailCall.INSTANCE
   )
 
   /** Lints for shadowed pattern fields.
