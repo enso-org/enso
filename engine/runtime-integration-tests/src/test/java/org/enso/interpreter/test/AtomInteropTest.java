4a2e522935495297cbaec8455e975b6d3c0d5d4a
Pavel Marek <pavel.marek@enso.org>
2024-10-15 12:04:07 +0200

    Show only fields from current atom constructor in the debugger (#11217)
    Debugger shows only fields of the current atom constructor: (internal members shown in gray, "public" members shown in bold purple)
    ![image](https://github.com/user-attachments/assets/21815296-c8aa-4ea2-ae7b-6feac78a221f)
    
    (Note that `static_method` is not displayed as a member of `My_Type` - not in scope of this PR)
    
    # Important Notes
    The *interop* contract for `Atom` is changed as follows:
    - Members are all methods and fields of the current constructor.
    - All methods are internal members.
    - If the constructor is project-private, fields are internal members.
    - All the members are both readable, and invocable.
    - Fields are field getters, that is, they are just methods.
    - Fields are not invocable
    - Constructors and static methods are **not** members of an atom. They should be members of the type.
    - Note that methods used to be atom members before #9692

--- engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/AtomInteropTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/AtomInteropTest.java
@@ -2,14 +2,19 @@ package org.enso.interpreter.test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.InvalidArrayIndexException;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import org.enso.test.utils.ContextUtils;
 import org.graalvm.polyglot.Context;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -90,7 +95,6 @@ public class AtomInteropTest {
     assertThat(anyType.getMetaSimpleName(), is("Any"));
   }
 
-  @Ignore("https://github.com/enso-org/enso/issues/10675")
   @Test
   public void atomMembersAreConstructorFields_ManyConstructors() {
     var myTypeAtom =
@@ -108,6 +112,242 @@ public class AtomInteropTest {
         "Member names correspond to constructor field names for a single constructor",
         myTypeAtom.getMemberKeys(),
         containsInAnyOrder("g1", "g2", "g3"));
+  }
+
+  @Test
+  public void methodIsAtomMember() {
+    var myTypeAtom =
+        ContextUtils.evalModule(
+            ctx,
+            """
+        type My_Type
+            Cons a b
+            method self = 42
+
+        main = My_Type.Cons "a" "b"
+        """);
+    assertThat("Method is a member of the atom", myTypeAtom.getMemberKeys(), hasItem("method"));
+    assertThat("method is an invokable member", myTypeAtom.canInvokeMember("method"), is(true));
+  }
+
+  @Test
+  public void methodIsAtomMember_InteropLibrary() {
+    var myTypeAtom =
+        ContextUtils.evalModule(
+            ctx,
+            """
+        type My_Type
+            Cons a b
+            method self = 42
+
+        main = My_Type.Cons "a" "b"
+        """);
+    var atom = ContextUtils.unwrapValue(ctx, myTypeAtom);
+    var interop = InteropLibrary.getUncached();
+    assertThat("Atom has members", interop.hasMembers(atom), is(true));
+    assertThat("Method is readable", interop.isMemberReadable(atom, "method"), is(true));
+    assertThat("Method is invocable", interop.isMemberInvocable(atom, "method"), is(true));
+    assertThat("Field is readable", interop.isMemberReadable(atom, "a"), is(true));
+  }
+
+  @Test
+  public void fieldsFromPrivateConstructorAreInternalMembers() {
+    var myTypeAtom =
+        ContextUtils.evalModule(
+            ctx,
+            """
+        type My_Type
+            private Cons a
+
+        main = My_Type.Cons "a"
+        """);
+    var atom = ContextUtils.unwrapValue(ctx, myTypeAtom);
+    var interop = InteropLibrary.getUncached();
+    assertThat("field a is internal", interop.isMemberInternal(atom, "a"), is(true));
+  }
+
+  @Test
+  public void fieldFromPrivateConstructorIsReadable() {
+    var myTypeAtom =
+        ContextUtils.evalModule(
+            ctx,
+            """
+        type My_Type
+            private Cons a
+
+        main = My_Type.Cons "a"
+        """);
+    ContextUtils.executeInContext(
+        ctx,
+        () -> {
+          var atom = ContextUtils.unwrapValue(ctx, myTypeAtom);
+          var interop = InteropLibrary.getUncached();
+          assertThat(
+              "Field from private constructor is readable",
+              interop.isMemberReadable(atom, "a"),
+              is(true));
+          assertThat(
+              "Field from private constructor is invocable",
+              interop.isMemberInvocable(atom, "a"),
+              is(true));
+          assertThat(
+              "Field from private constructor can be read",
+              interop.asString(interop.readMember(atom, "a")),
+              is("a"));
+          assertThat(
+              "Field from private constructor can be invoked",
+              interop.asString(interop.invokeMember(atom, "a")),
+              is("a"));
+          return null;
+        });
+  }
+
+  @Test
+  public void allMethodsAreInternalMembers() {
+    var myTypeAtom =
+        ContextUtils.evalModule(
+            ctx,
+            """
+        type My_Type
+            Cons a
+            pub_method self = 42
+            private priv_method self = 42
+
+        main = My_Type.Cons "a"
+        """);
+    var atom = ContextUtils.unwrapValue(ctx, myTypeAtom);
+    var interop = InteropLibrary.getUncached();
+    assertThat(
+        "public method is internal member", interop.isMemberInternal(atom, "pub_method"), is(true));
+    assertThat(
+        "private method is internal member",
+        interop.isMemberInternal(atom, "priv_method"),
+        is(true));
+  }
+
+  @Test
+  public void allMembersAreReadableAndInvocable()
+      throws UnsupportedMessageException, InvalidArrayIndexException {
+    var myTypeAtom =
+        ContextUtils.evalModule(
+            ctx,
+            """
+        type My_Type
+            Cons a
+            pub_method self = 42
+            private priv_method self = 42
+
+        main = My_Type.Cons "a"
+        """);
+    var atom = ContextUtils.unwrapValue(ctx, myTypeAtom);
+    var interop = InteropLibrary.getUncached();
+    var members = interop.getMembers(atom, true);
+    for (long i = 0; i < interop.getArraySize(members); i++) {
+      var memberName = interop.asString(interop.readArrayElement(members, i));
+      assertThat(
+          "Member " + memberName + " should be readable",
+          interop.isMemberReadable(atom, memberName),
+          is(true));
+      assertThat(
+          "Member " + memberName + " should be invocable",
+          interop.isMemberInvocable(atom, memberName),
+          is(true));
+    }
+  }
+
+  @Test
+  public void constructorIsNotAtomMember() {
+    var myTypeAtom =
+        ContextUtils.evalModule(
+            ctx,
+            """
+        type My_Type
+            Cons a b
+            method self = 42
+
+        main = My_Type.Cons "a" "b"
+        """);
+    assertThat("Cons is not atom member", myTypeAtom.getMemberKeys(), not(hasItem("Cons")));
+  }
+
+  @Test
+  public void fieldIsInvocable() {
+    var myTypeAtom =
+        ContextUtils.evalModule(
+            ctx,
+            """
+        type My_Type
+            Cons a b
+
+        main = My_Type.Cons 1 2
+        """);
+    ContextUtils.executeInContext(
+        ctx,
+        () -> {
+          var atom = ContextUtils.unwrapValue(ctx, myTypeAtom);
+          var interop = InteropLibrary.getUncached();
+          assertThat("Field a is invocable", interop.isMemberInvocable(atom, "a"), is(true));
+          var aField = interop.invokeMember(atom, "a");
+          assertThat("Field is a number", interop.asInt(aField), is(1));
+          assertThat("Field b is invocable", interop.isMemberInvocable(atom, "b"), is(true));
+          return null;
+        });
+  }
+
+  @Test
+  public void fieldIsReadable() {
+    var myTypeAtom =
+        ContextUtils.evalModule(
+            ctx,
+            """
+        type My_Type
+            Cons a
+
+        main = My_Type.Cons 1
+        """);
+    ContextUtils.executeInContext(
+        ctx,
+        () -> {
+          var atom = ContextUtils.unwrapValue(ctx, myTypeAtom);
+          var interop = InteropLibrary.getUncached();
+          assertThat("Field a is readable", interop.isMemberReadable(atom, "a"), is(true));
+          return null;
+        });
+  }
+
+  @Test
+  public void staticMethodIsNotAtomMember() {
+    var myTypeAtom =
+        ContextUtils.evalModule(
+            ctx,
+            """
+        type My_Type
+            Cons
+            static_method = 42
+
+        main = My_Type.Cons
+        """);
+    assertThat(
+        "Static method is not atom member",
+        myTypeAtom.getMemberKeys(),
+        not(hasItem(containsString("static_method"))));
+  }
+
+  @Test
+  public void constructorIsNotAtomMember_InteropLibrary() {
+    var myTypeAtom =
+        ContextUtils.evalModule(
+            ctx,
+            """
+        type My_Type
+            Cons a b
+            method self = 42
+
+        main = My_Type.Cons "a" "b"
+        """);
+    var atom = ContextUtils.unwrapValue(ctx, myTypeAtom);
+    var interop = InteropLibrary.getUncached();
+    assertThat("Cons is not atom member", interop.isMemberExisting(atom, "Cons"), is(false));
   }
 
   @Test
