6a506161cb86e896ae02e420d9908ba20a8adfae
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-10-18 17:13:02 +0200

    Make sure unrecognized CLI command message is printed (#11357)

--- engine/runner/src/test/java/org/enso/runner/EngineMainTest.java
+++ engine/runner/src/test/java/org/enso/runner/EngineMainTest.java
@@ -3,6 +3,7 @@ package org.enso.runner;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,6 +17,21 @@ public class EngineMainTest {
   @Rule public TemporaryFolder tempDir = new TemporaryFolder();
 
   private final List<String> linesOut = new ArrayList<>();
+
+  @Test
+  public void unknownCommandBecauseTwoAreConcatenated() throws Exception {
+    var m = new MainMock();
+    try {
+      var file = tempDir.newFile("some.enso");
+      var line = m.preprocessArguments("--repl --inspect", "--run", file.getAbsolutePath());
+      m.mainEntry(line, Level.INFO, false);
+    } catch (ExitCode ex) {
+      assertEquals("Execution fails", 1, ex.exitCode);
+      assertEquals("One line printed", 1, linesOut.size());
+      assertEquals("Unrecognized option: --repl --inspect", linesOut.get(0));
+      assertTrue("Also help was printed", m.helpPrinted);
+    }
+  }
 
   @Test
   public void cannotUseReplAndInspectAtOnce() throws Exception {
@@ -76,6 +92,8 @@ public class EngineMainTest {
   }
 
   private final class MainMock extends Main {
+    boolean helpPrinted;
+
     @Override
     RuntimeException doExit(int exitCode) {
       throw new ExitCode(exitCode);
@@ -84,6 +102,11 @@ public class EngineMainTest {
     @Override
     void println(String msg) {
       linesOut.add(msg);
+    }
+
+    @Override
+    void printHelp() {
+      helpPrinted = true;
     }
   }
 
