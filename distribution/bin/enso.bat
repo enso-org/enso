9c3d18fb3b8bceeb66662c603e0e9c1b90ecdd42
Pavel Marek <pavel.marek@enso.org>
2024-10-24 14:21:36 +0200

    Revert "Set stack size to 16M (#11327)"
    This reverts commit 07d0015f2f11c38c7b386337de3b701e0832fc7a.

--- distribution/bin/enso.bat
+++ distribution/bin/enso.bat
@@ -1,5 +1,5 @@
 @echo off
 set comp-dir=%~dp0\..\component
-set JAVA_OPTS=%JAVA_OPTS% --add-opens=java.base/java.nio=ALL-UNNAMED -Xss16M
+set JAVA_OPTS=%JAVA_OPTS% --add-opens=java.base/java.nio=ALL-UNNAMED
 java --module-path %comp-dir% -Dpolyglot.compiler.IterativePartialEscape=true %JAVA_OPTS% -m org.enso.runner/org.enso.runner.Main %*
 exit /B %errorlevel%
