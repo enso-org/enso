ee45c9287e3ae4e5eb6514a8aab44e9f29465c83
Adam Riley <riley.ad@gmail.com>
2025-01-10 12:18:16 +0000

    More plumbing

--- std-bits/table/src/main/java/org/enso/table/operations/Offset.java
+++ std-bits/table/src/main/java/org/enso/table/operations/Offset.java
@@ -4,6 +4,11 @@ import org.enso.table.data.column.storage.Storage;
 import org.enso.table.data.table.Column;
 import org.enso.table.problems.ProblemAggregator;
 
+import java.util.BitSet;
+
+import org.enso.table.data.column.storage.numeric.DoubleStorage;
+import org.enso.table.problems.ColumnAggregatedProblemAggregator;
+
 public class Offset {
     public static Storage<?> offset(
       Column sourceColumn,
@@ -11,6 +16,59 @@ public class Offset {
       Column[] orderingColumns,
       int[] directions,
       ProblemAggregator problemAggregator) {
-        return sourceColumn.getStorage();
+        var offsetRunningStatistic = new OffsetRunningStatistic<Double>(sourceColumn, problemAggregator);
+        RunningLooper.loop(
+            groupingColumns,
+            orderingColumns,
+            directions,
+            problemAggregator,
+            offsetRunningStatistic,
+            sourceColumn.getSize());
+        return offsetRunningStatistic.getResult();
       }
+
+    private static class OffsetRunningStatistic<T> implements RunningStatistic<Double> {
+
+        long[] result;
+        BitSet isNothing;
+        ColumnAggregatedProblemAggregator columnAggregatedProblemAggregator;
+        Column sourceColumn;
+
+        OffsetRunningStatistic(Column sourceColumn, ProblemAggregator problemAggregator) {
+            result = new long[sourceColumn.getSize()];
+            isNothing = new BitSet();
+            columnAggregatedProblemAggregator = new ColumnAggregatedProblemAggregator(problemAggregator);
+            this.sourceColumn = sourceColumn;
+        }
+
+        @Override
+        public void calculateNextValue(int i, RunningIterator<Double> it) {
+        
+        }
+
+        @Override
+        public Storage<Double> getResult() {
+            return new DoubleStorage(result, sourceColumn.getSize(), isNothing);
+        }
+
+        @Override
+        public RunningIterator<Double> getNewIterator() {
+            return new OffsetRunning();
+        }
+    }
+
+
+  private static class OffsetRunning implements RunningIterator<Double> {
+
+        @Override
+        public Double next(Double value) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public Double currentValue() {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+  }
 }
