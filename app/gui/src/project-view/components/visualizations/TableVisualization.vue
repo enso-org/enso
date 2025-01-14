1f7ca480d72d6822d042231fba44358fa3233748
Martha King <martha.king@enso.org>
2025-01-14 17:06:10 +0000

    use darker shade

--- app/gui/src/project-view/components/visualizations/TableVisualization.vue
+++ app/gui/src/project-view/components/visualizations/TableVisualization.vue
@@ -134,7 +134,7 @@ const defaultColDef: Ref<ColDef> = ref({
   minWidth: 25,
   cellRenderer: cellRenderer,
   cellClass: cellClass,
-  cellStyle: {'padding-left': 0 ,'border-right': '1px solid #D3D3D3'},
+  cellStyle: {'padding-left': 0 ,'border-right': '1px solid #C0C0C0'},
   contextMenuItems: [
     commonContextMenuActions.copy,
     commonContextMenuActions.copyWithHeaders,
