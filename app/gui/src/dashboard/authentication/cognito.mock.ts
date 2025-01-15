21ddca8b2a6f09966e6bd885644f15bcd337a54e
Sergei Garin <sergei.garin@enso.org>
2025-01-15 18:48:35 +0400

    Fix login

--- app/gui/src/dashboard/authentication/cognito.mock.ts
+++ app/gui/src/dashboard/authentication/cognito.mock.ts
@@ -229,7 +229,7 @@ export class Cognito {
     localStorage.setItem(MOCK_EMAIL_KEY, username)
     const result = await results.Result.wrapAsync(async () => {
       listen.authEventListener?.(listen.AuthEvent.signIn)
-      return await this.userSession()
+      return Promise.resolve(await this.userSession())
     })
     return result
       .mapErr(original.intoAmplifyErrorOrThrow)
