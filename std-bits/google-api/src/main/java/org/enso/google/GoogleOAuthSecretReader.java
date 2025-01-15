de70523b0ad2d111e32226d26ddf495f0b5b6863
Radosław Waśko <radoslaw.wasko@enso.org>
2025-01-15 19:41:47 +0100

    using non deprecated Credentials API

--- std-bits/google-api/src/main/java/org/enso/google/GoogleOAuthSecretReader.java
+++ std-bits/google-api/src/main/java/org/enso/google/GoogleOAuthSecretReader.java
@@ -1,6 +1,6 @@
 package org.enso.google;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.auth.oauth2.GoogleCredentials;
 import org.enso.base.enso_cloud.ExternalLibrarySecretHelper;
 import org.enso.base.enso_cloud.HideableValue;
 
@@ -8,11 +8,11 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 public class GoogleOAuthSecretReader {
-  public static GoogleCredential createCredentialFromSecretValue(HideableValue secretValue) {
+  public static GoogleCredentials createCredentialFromSecretValue(HideableValue secretValue) {
     String payload = ExternalLibrarySecretHelper.resolveValue(secretValue);
     ByteArrayInputStream stream = new ByteArrayInputStream(payload.getBytes());
     try {
-      return GoogleCredential.fromStream(stream);
+      return GoogleCredentials.fromStream(stream);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
--- std-bits/google-api/src/main/java/org/enso/google/GoogleSheetsHelpers.java
+++ std-bits/google-api/src/main/java/org/enso/google/GoogleSheetsHelpers.java
@@ -1,8 +1,13 @@
 package org.enso.google;
 
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.sheets.v4.Sheets;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
 
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.List;
 
 public class GoogleSheetsHelpers {
@@ -15,5 +20,15 @@ public class GoogleSheetsHelpers {
         .setValueRenderOption("UNFORMATTED_VALUE")
         .execute()
         .getValues();
+  }
+
+  public static Sheets createService(GoogleCredentials credentials) throws GeneralSecurityException, IOException {
+    var credentialsAdapter = new HttpCredentialsAdapter(credentials);
+    Sheets.Builder builder = new Sheets.Builder(
+        GoogleNetHttpTransport.newTrustedTransport(),
+        GsonFactory.getDefaultInstance(),
+        credentialsAdapter
+    ).setApplicationName("Enso");
+    return builder.build();
   }
 }
