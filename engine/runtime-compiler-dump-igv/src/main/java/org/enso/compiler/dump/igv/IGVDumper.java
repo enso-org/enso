d7943f693a1b98c1d27b68284966d04cf3f8875b
Pavel Marek <pavel.marek@enso.org>
2025-01-20 14:23:30 +0100

    Create separate IGVDumper for each module

--- engine/runtime-compiler-dump-igv/src/main/java/org/enso/compiler/dump/igv/IGVDumper.java
+++ engine/runtime-compiler-dump-igv/src/main/java/org/enso/compiler/dump/igv/IGVDumper.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
 import org.enso.compiler.core.ir.Module;
 import org.enso.compiler.dump.service.IRDumper;
 import org.slf4j.Logger;
@@ -15,39 +13,36 @@ public final class IGVDumper implements IRDumper {
 
   private static final String DEFAULT_DUMP_DIR = "ir-dumps";
   private static final Logger LOGGER = LoggerFactory.getLogger(IGVDumper.class);
-  private final Map<String, ModuleGraph> moduleGraphs = new HashMap<>();
+  private final ModuleGraph moduleGraph;
+
+  IGVDumper(String moduleName) {
+    this.moduleGraph = new ModuleGraph(moduleName);
+  }
 
   @Override
   public void dump(Module ir, String moduleName, File srcFile, String afterPass) {
-    var moduleGraph = ensureGraphOpened(moduleName);
+    assert moduleName.equals(moduleName());
     var nodesCnt = moduleGraph.getAllNodes().size() + 1;
-    LOGGER.trace("Creating EnsoModuleAST for module {}, nodeId = {}", moduleName, nodesCnt);
+    LOGGER.trace("[{}] Creating EnsoModuleAST for module {}, nodeId = {}", moduleName, moduleName, nodesCnt);
     var moduleAst = EnsoModuleAST.fromIR(ir, srcFile, moduleName, nodesCnt);
     moduleGraph.addSubGraphForPass(afterPass, moduleAst);
   }
 
-  private ModuleGraph ensureGraphOpened(String moduleName) {
-    if (!moduleGraphs.containsKey(moduleName)) {
-      LOGGER.trace("Opening graph for module {}", moduleName);
-      var graph = new ModuleGraph(moduleName);
-      moduleGraphs.put(moduleName, graph);
-    }
-    return moduleGraphs.get(moduleName);
-  }
-
   @Override
   public void close() {
-    for (var entry : moduleGraphs.entrySet()) {
-      var modName = entry.getKey();
-      var moduleGraph = entry.getValue();
-      var outPath = outputForModule(modName);
-      LOGGER.trace("Dumping graph for module {} to {}", modName, outPath);
-      try {
-        moduleGraph.dump(outPath);
-      } catch (IOException e) {
-        LOGGER.error("Failed to dump graph for module {}", modName, e);
-      }
+    var modName = moduleGraph.getModuleName();
+    var outPath = outputForModule(modName);;
+    LOGGER.trace("[{}] Dumping graph to {}", modName, outPath);
+    try {
+      moduleGraph.dump(outPath);
+    } catch (IOException e) {
+      LOGGER.error("[{}] Failed to dump graph: {}", modName, e);
     }
+    LOGGER.trace("[{}] Graph dumped", modName);
+  }
+
+  private String moduleName() {
+    return moduleGraph.getModuleName();
   }
 
   private static Path outputForModule(String moduleName) {
--- engine/runtime-compiler-dump-igv/src/main/java/org/enso/compiler/dump/igv/IGVDumperFactory.java
+++ engine/runtime-compiler-dump-igv/src/main/java/org/enso/compiler/dump/igv/IGVDumperFactory.java
@@ -12,6 +12,6 @@ public class IGVDumperFactory implements IRDumpFactoryService {
   @Override
   public IRDumper create(String moduleName) {
     LOGGER.trace("Creating IGV dumper for module {}", moduleName);
-    return null;
+    return new IGVDumper(moduleName);
   }
 }
