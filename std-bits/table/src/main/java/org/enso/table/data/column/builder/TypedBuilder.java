c073786774b21b0752c64698ec3baee28001ae13
Radosław Waśko <radoslaw.wasko@enso.org>
2025-01-21 22:27:26 +0100

    add a test for better coverage

--- std-bits/table/src/main/java/org/enso/table/data/column/builder/TypedBuilder.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/builder/TypedBuilder.java
@@ -61,7 +61,8 @@ public abstract class TypedBuilder<T> implements BuilderWithRetyping, BuilderFor
                 + ". This is a bug in the Table library.");
       }
     } else if (storage.getType() instanceof NullType) {
-      appendNulls(storage.size());
+      //appendNulls(storage.size());
+      throw new RuntimeException("Hello!");
     } else {
       throw new StorageTypeMismatchException(getType(), storage.getType());
     }
