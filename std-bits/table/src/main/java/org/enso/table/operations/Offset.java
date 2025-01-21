5de39ee98111beedeefc96f2e1a3f9619e11465f
Adam Riley <riley.ad@gmail.com>
2025-01-21 11:55:30 +0000

    Final tweaks

--- std-bits/table/src/main/java/org/enso/table/operations/Offset.java
+++ std-bits/table/src/main/java/org/enso/table/operations/Offset.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.stream.IntStream;
-
 import org.enso.table.data.column.storage.Storage;
 import org.enso.table.data.mask.OrderMask;
 import org.enso.table.data.table.Column;
