36619e5260158d7f13208492abce1217cbc5b388
Sergei Garin <sergei.garin@enso.org>
2025-01-21 20:11:08 +0400

    Return dialog animation

--- app/gui/src/dashboard/components/AriaComponents/Dialog/Dialog.tsx
+++ app/gui/src/dashboard/components/AriaComponents/Dialog/Dialog.tsx
@@ -12,12 +12,14 @@ import * as suspense from '#/components/Suspense'
 
 import * as mergeRefs from '#/utilities/mergeRefs'
 
-import { DialogDismiss } from '#/components/AriaComponents'
+import { DialogDismiss, ResetButtonGroupContext } from '#/components/AriaComponents'
 import { useEventCallback } from '#/hooks/eventCallbackHooks'
 import { useMeasure } from '#/hooks/measureHooks'
 import { motion, type Spring } from '#/utilities/motion'
 import type { VariantProps } from '#/utilities/tailwindVariants'
 import { tv } from '#/utilities/tailwindVariants'
+import { unsafeWriteValue } from '#/utilities/write'
+import { useRootContext } from '../../UIProviders'
 import { Close } from './Close'
 import * as dialogProvider from './DialogProvider'
 import * as dialogStackProvider from './DialogStackProvider'
@@ -25,6 +27,7 @@ import { DialogTrigger } from './DialogTrigger'
 import type * as types from './types'
 import * as utlities from './utilities'
 import { DIALOG_BACKGROUND } from './variants'
+
 // eslint-disable-next-line no-restricted-syntax
 const MotionDialog = motion(aria.Dialog)
 
@@ -254,6 +257,8 @@ function DialogContent(props: DialogContentProps) {
   const scrollerRef = React.useRef<HTMLDivElement | null>(null)
   const dialogId = aria.useId()
 
+  const { appRoot } = useRootContext()
+
   const titleId = `${dialogId}-title`
   const padding = paddingRaw ?? (type === 'modal' ? 'medium' : 'xlarge')
   const isFullscreen = type === 'fullscreen'
@@ -297,6 +302,20 @@ function DialogContent(props: DialogContentProps) {
     }
   }, [isFullscreen])
 
+  React.useEffect(() => {
+    if (isFullscreen && modalState.isOpen) {
+      unsafeWriteValue(appRoot.style, 'scale', '0.99')
+      unsafeWriteValue(appRoot.style, 'filter', 'blur(8px)')
+      unsafeWriteValue(appRoot.style, 'willChange', 'scale, filter')
+
+      return () => {
+        unsafeWriteValue(appRoot.style, 'scale', '')
+        unsafeWriteValue(appRoot.style, 'filter', '')
+        unsafeWriteValue(appRoot.style, 'willChange', '')
+      }
+    }
+  }, [isFullscreen, modalState, appRoot])
+
   const styles = variants({
     className,
     type,
@@ -322,7 +341,7 @@ function DialogContent(props: DialogContentProps) {
   }
 
   return (
-    <>
+    <ResetButtonGroupContext>
       <MotionDialog
         layout
         transition={TRANSITION}
@@ -401,7 +420,7 @@ function DialogContent(props: DialogContentProps) {
       </MotionDialog>
 
       <dialogStackProvider.DialogStackRegistrar id={dialogId} type={TYPE_TO_DIALOG_TYPE[type]} />
-    </>
+    </ResetButtonGroupContext>
   )
 }
 
