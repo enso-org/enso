fa0ca1647e54280c47ca4e40b29e614297e6f262
Pavel Marek <pavel.marek@enso.org>
2025-01-22 20:00:41 +0100

    Ensure that compiler module exports graphio package

--- engine/runtime-compiler-dump-igv/src/main/java/org/enso/compiler/dump/igv/IGVDumperFactory.java
+++ engine/runtime-compiler-dump-igv/src/main/java/org/enso/compiler/dump/igv/IGVDumperFactory.java
@@ -7,8 +7,29 @@ import org.slf4j.LoggerFactory;
 
 public class IGVDumperFactory implements IRDumpFactoryService {
   private static final Logger LOGGER = LoggerFactory.getLogger(IGVDumperFactory.class);
+  private static final String GRAPHIO_PKG = "org.graalvm.graphio";
+  private static final String COMPILER_MOD = "jdk.internal.vm.compiler";
 
-  public IGVDumperFactory() {}
+  public IGVDumperFactory() {
+    ensureInternalModuleIsExported();
+  }
+
+  private static void ensureInternalModuleIsExported() {
+    var internalCompilerMod = ModuleLayer.boot().findModule(COMPILER_MOD);
+    if (internalCompilerMod.isEmpty()) {
+      throw new IllegalStateException(
+          "Module " + COMPILER_MOD + " is not present. Is this a GraalVM JDK?");
+    }
+    var thisModule = IGVDumperFactory.class.getModule();
+    if (!internalCompilerMod.get().isExported(GRAPHIO_PKG, thisModule)) {
+      throw new IllegalStateException(
+          "Package "
+              + GRAPHIO_PKG
+              + " is not exported to this module. You must explicitly provide "
+              + "--add-exports %s/%s=%s"
+                  .formatted(COMPILER_MOD, GRAPHIO_PKG, thisModule.getName()));
+    }
+  }
 
   @Override
   public IRDumper create(String moduleName) {
