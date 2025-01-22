8ced35718c1c7baa0b53f94ceaf980c26237349d
Pavel Marek <pavel.marek@enso.org>
2025-01-22 13:46:18 +0100

    GetTypeMethodsNode delegates to Type.getMethods

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/GetTypeMethodsNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/GetTypeMethodsNode.java
@@ -27,10 +27,9 @@ public abstract class GetTypeMethodsNode extends Node {
   @Specialization
   @CompilerDirectives.TruffleBoundary
   EnsoObject allMethods(Type type) {
-    var methods = type.getDefinitionScope().getMethodNamesForType(type);
-    return methods == null
-        ? ArrayLikeHelpers.empty()
-        : ArrayLikeHelpers.wrapStrings(methods.toArray(new String[0]));
+    var methods = type.getMethods(true);
+    var methodNames = methods.keySet().toArray(String[]::new);
+    return ArrayLikeHelpers.wrapStrings(methodNames);
   }
 
   @Fallback
