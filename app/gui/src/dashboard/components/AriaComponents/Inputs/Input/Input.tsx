2ca5ce4a4669a2c95cb564d6e8f2f5d4b37e5bb8
Sergei Garin <sergei.garin@enso.org>
2025-01-24 14:01:44 +0400

    Fixes

--- app/gui/src/dashboard/components/AriaComponents/Inputs/Input/Input.tsx
+++ app/gui/src/dashboard/components/AriaComponents/Inputs/Input/Input.tsx
@@ -78,6 +78,7 @@ export const Input = forwardRef(function Input<
     autoFocus = false,
     className,
     testId: testIdRaw,
+    label,
     ...inputProps
   } = props
   const form = Form.useFormContext(formRaw)
@@ -140,6 +141,7 @@ export const Input = forwardRef(function Input<
         variants: fieldVariants,
         form: formInstance,
       })}
+      label={label}
       ref={ref}
       name={props.name}
       data-testid={testId}
