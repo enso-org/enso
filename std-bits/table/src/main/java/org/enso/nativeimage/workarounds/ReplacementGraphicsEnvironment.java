94c8f83be8b771e4e8e43744194997a01bf75ff2
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2025-01-26 14:45:14 +0100

    fmt

--- std-bits/table/src/main/java/org/enso/nativeimage/workarounds/ReplacementGraphicsEnvironment.java
+++ std-bits/table/src/main/java/org/enso/nativeimage/workarounds/ReplacementGraphicsEnvironment.java
@@ -1,10 +1,12 @@
 package org.enso.nativeimage.workarounds;
 
-import com.oracle.svm.core.annotate.TargetClass;
 import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
 
 @TargetClass(java.awt.GraphicsEnvironment.class)
 final class ReplacementGraphicsEnvironment {
-    @Substitute
-    public static boolean isHeadless() { return true; }
+  @Substitute
+  public static boolean isHeadless() {
+    return true;
+  }
 }
--- std-bits/table/src/main/java/org/enso/nativeimage/workarounds/ReplacementToolkit.java
+++ std-bits/table/src/main/java/org/enso/nativeimage/workarounds/ReplacementToolkit.java
@@ -1,17 +1,19 @@
 package org.enso.nativeimage.workarounds;
 
 import com.oracle.svm.core.annotate.Alias;
-import com.oracle.svm.core.annotate.TargetClass;
 import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
 
 @TargetClass(java.awt.Toolkit.class)
 final class ReplacementToolkit {
 
-    @Alias private static boolean loaded;
+  @Alias private static boolean loaded;
 
-    @Substitute
-    static void loadLibraries() { loaded = true; }
+  @Substitute
+  static void loadLibraries() {
+    loaded = true;
+  }
 
-    @Substitute
-    private static  void initStatic() { }
+  @Substitute
+  private static void initStatic() {}
 }
