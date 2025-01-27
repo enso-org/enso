15af6bce2e71c8d00e8b3210501cf1bf38622e01
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2025-01-27 15:01:19 +0100

    Always attempt to resolve to a default edition
    It appears that we still fail when some old edition is requested and
    ignore the default editions.
    This changes adds (a last one?) fallback to the default edition.

--- lib/scala/editions/src/main/scala/org/enso/editions/EditionResolver.scala
+++ lib/scala/editions/src/main/scala/org/enso/editions/EditionResolver.scala
@@ -6,6 +6,7 @@ import org.enso.editions.EditionResolutionError.{
 }
 import org.enso.editions.Editions.{RawEdition, ResolvedEdition}
 import org.enso.editions.provider.EditionProvider
+import org.slf4j.LoggerFactory
 
 import scala.annotation.tailrec
 
@@ -26,7 +27,15 @@ case class EditionResolver(provider: EditionProvider) {
   def resolve(
     edition: RawEdition
   ): Either[EditionResolutionError, ResolvedEdition] =
-    resolveEdition(edition, Nil)
+    resolveEdition(edition, Nil).left.flatMap { err =>
+      LoggerFactory
+        .getLogger(classOf[EditionResolver])
+        .warn(
+          "Failed to resolve original edition. Trying fallback to the default one",
+          err
+        )
+      resolveEdition(DefaultEdition.getDefaultEdition, Nil).left.map(_ => err)
+    }
 
   /** A helper method that resolves an edition and keeps a list of already
     * visited edition names to avoid cycles.
