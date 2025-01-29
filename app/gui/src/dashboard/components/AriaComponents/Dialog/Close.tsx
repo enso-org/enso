4b4bf6fc99389815acede7e04a672fce369b495e
Sergei Garin <sergei.garin@enso.org>
2025-01-29 16:37:31 +0400

    Fix prettier

--- app/gui/src/dashboard/components/AriaComponents/Dialog/Close.tsx
+++ app/gui/src/dashboard/components/AriaComponents/Dialog/Close.tsx
@@ -4,8 +4,6 @@
  * Close button for a dialog.
  */
 
-import invariant from 'tiny-invariant'
-
 import { useEventCallback } from '#/hooks/eventCallbackHooks'
 import { type ButtonProps, Button } from '../Button'
 import * as dialogProvider from './DialogProvider'
@@ -15,14 +13,14 @@ export type CloseProps<IconType extends string> = ButtonProps<IconType>
 
 /** Close button for a dialog. */
 export function Close<IconType extends string>(props: CloseProps<IconType>) {
-  const dialogContext = dialogProvider.useDialogContext()
+  const dialogContext = dialogProvider.useDialogStrictContext()
 
-  invariant(dialogContext, 'Close must be used inside a DialogProvider')
-
-  const onPressCallback = useEventCallback<NonNullable<ButtonProps<IconType>['onPress']>>((event) => {
-    dialogContext.close()
-    return props.onPress?.(event)
-  })
+  const onPressCallback = useEventCallback<NonNullable<ButtonProps<IconType>['onPress']>>(
+    (event) => {
+      dialogContext.close()
+      return props.onPress?.(event)
+    },
+  )
 
   return <Button {...props} onPress={onPressCallback} />
 }
--- app/gui/src/dashboard/components/Paywall/PaywallDialogButton.tsx
+++ app/gui/src/dashboard/components/Paywall/PaywallDialogButton.tsx
@@ -12,10 +12,11 @@ import * as components from './components'
 import * as paywallDialog from './PaywallDialog'
 
 /** Props for a {@link PaywallDialogButton}. */
-export type PaywallDialogButtonProps<IconType extends string> = components.PaywallButtonProps<IconType> & {
-  readonly dialogProps?: paywallDialog.PaywallDialogProps
-  readonly dialogTriggerProps?: ariaComponents.DialogTriggerProps
-}
+export type PaywallDialogButtonProps<IconType extends string> =
+  components.PaywallButtonProps<IconType> & {
+    readonly dialogProps?: paywallDialog.PaywallDialogProps
+    readonly dialogTriggerProps?: ariaComponents.DialogTriggerProps
+  }
 
 /** A button that opens a paywall dialog when clicked */
 export function PaywallDialogButton<IconType extends string>(
