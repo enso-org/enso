b694ccf419fc98fc3e8d1f4d761f15d325536d98
Dmitry Bushev <bushevdv@gmail.com>
2025-02-02 16:54:44 +0000

    misc: cleanup test artifacts

--- build_tools/build/src/ci_gen/job.rs
+++ build_tools/build/src/ci_gen/job.rs
@@ -757,18 +757,7 @@ pub struct CiCheckBackend {
 impl JobArchetype for CiCheckBackend {
     fn job(&self, target: Target) -> Job {
         let job_name = format!("Engine ({})", self.graal_edition);
-        let mut job_builder = RunStepsBuilder::new("backend ci-check");
-        if target.0 == OS::Linux && self.graal_edition == graalvm::Edition::Community {
-            let upload_edition_file = step::upload_artifact("Upload Edition File")
-                .with_custom_argument("name", paths::EDITION_FILE_ARTIFACT_NAME)
-                .with_custom_argument("path", "distribution/editions/*.yaml");
-            let upload_fbs_schema = step::upload_artifact("Upload fbs-schema")
-                .with_custom_argument("name", "fbs-schema")
-                .with_custom_argument("path", "engine/language-server/src/main/schema/");
-            job_builder = job_builder
-                .customize(move |step| vec![step, upload_edition_file, upload_fbs_schema]);
-        }
-        let mut job = job_builder.build_job(job_name, target);
+        let mut job = RunStepsBuilder::new("backend ci-check").build_job(job_name, target);
         match self.graal_edition {
             graalvm::Edition::Community =>
                 job.env(engine_env::GRAAL_EDITION, graalvm::Edition::Community),
