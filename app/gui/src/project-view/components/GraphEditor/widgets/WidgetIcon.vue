b771d24ac52b6a2f1d20cb370f42e3884a767890
Adam Obuchowicz <adam.obuchowicz@enso.org>
2025-02-03 12:13:26 +0100

    Fix loading spinner

--- app/gui/src/project-view/components/GraphEditor/widgets/WidgetIcon.vue
+++ app/gui/src/project-view/components/GraphEditor/widgets/WidgetIcon.vue
@@ -66,17 +66,6 @@ export const widgetDefinition = defineWidget(
 .nodeCategoryIcon {
   position: absolute;
 }
-.LoadingSpinner {
-  border: 4px solid;
-  border-radius: 100%;
-  border-color: rgba(255, 255, 255, 90%) #0000;
-  animation: s1 0.8s infinite;
-}
-@keyframes s1 {
-  to {
-    transform: rotate(0.5turn);
-  }
-}
 .v-enter-active,
 .v-leave-active {
   transition: opacity 0.1s ease;
