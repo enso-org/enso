471c0b50b3d3c5bb4c1df625d50c4fb4e203e1f4
Pavel Marek <pavel.marek@enso.org>
2025-02-06 16:59:08 +0100

    Git tests are run sequentially

--- build_tools/ci_utils/src/programs/git.rs
+++ build_tools/ci_utils/src/programs/git.rs
@@ -377,6 +377,7 @@ impl FromStr for RemoteLsEntry {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serial_test::serial;
 
     #[tokio::test]
     async fn github_wrapping_non_directory() -> Result {
@@ -425,6 +426,7 @@ mod tests {
     }
 
     #[tokio::test]
+    #[serial]
     async fn diff_same_files_test() -> Result {
         let git = Context::new_current().await?;
         let file_1 = tempfile::NamedTempFile::new()?;
@@ -439,6 +441,7 @@ mod tests {
     }
 
     #[tokio::test]
+    #[serial]
     async fn diff_different_files_test() -> Result {
         let git = Context::new_current().await?;
         let file_1 = tempfile::NamedTempFile::new()?;
