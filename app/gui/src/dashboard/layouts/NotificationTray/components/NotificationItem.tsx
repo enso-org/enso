14f4490132068bfd828438d0c57186f007957db1
somebody1234 <ehern.lee@gmail.com>
2025-02-06 19:40:01 +1000

    Switch `NotificationItem` to use `tailwind-variants`

--- app/gui/src/dashboard/layouts/NotificationTray/components/NotificationItem.tsx
+++ app/gui/src/dashboard/layouts/NotificationTray/components/NotificationItem.tsx
@@ -3,10 +3,22 @@ import { Button, CloseButton, Text } from '#/components/AriaComponents'
 import { GridListItem, ProgressBar } from '#/components/aria'
 import type { NotificationInfo } from '#/layouts/NotificationTray/types'
 import { useText } from '#/providers/TextProvider'
+import { tv } from '#/utilities/tailwindVariants'
+
+const NOTIFICATION_ITEM_STYLES = tv({
+  base: 'relative flex flex-col px-2',
+  slots: {
+    closeButton: 'absolute right-0 top-0',
+    content: 'flex min-h-8 items-center gap-2 text-primary',
+    contentPadding: 'grow',
+    progressBarContainer: 'h-2 rounded-full bg-primary/10',
+    progressBar: 'h-full rounded-full bg-accent transition-width duration-1000',
+  },
+})
 
 /** Props for a {@link NotificationItem}. */
 export interface NotificationItemProps extends NotificationInfo {
-  readonly remove: () => Promise<void> | void
+  readonly remove?: () => Promise<void> | void
 }
 
 /** An item in the notification tray. */
@@ -15,14 +27,16 @@ export function NotificationItem(props: NotificationItemProps) {
   const { locale } = useText()
   const dateTime = timestamp != null ? new Date(timestamp) : undefined
 
+  const styles = NOTIFICATION_ITEM_STYLES()
+
   return (
     <GridListItem>
-      <div className="relative flex flex-col px-2">
-        <CloseButton className="absolute right-0 top-0" onPress={remove} />
-        <div className="flex min-h-8 items-center gap-2 text-primary">
+      <div className={styles.base()}>
+        {remove && <CloseButton className={styles.closeButton()} onPress={remove} />}
+        <div className={styles.content()}>
           <Button isDisabled isActive variant="icon" color={color} icon={icon} />
           <Text>{message}</Text>
-          <div className="grow" />
+          <div className={styles.contentPadding()} />
           {dateTime != null && (
             <Text color="disabled">
               {dateTime.toLocaleString(locale, {
@@ -41,11 +55,8 @@ export function NotificationItem(props: NotificationItemProps) {
             maxValue={1}
           >
             {({ percentage }) => (
-              <div className="h-2 rounded-full bg-primary/10">
-                <div
-                  className="h-full rounded-full bg-accent transition-width duration-1000"
-                  style={{ width: percentage + '%' }}
-                />
+              <div className={styles.progressBarContainer()}>
+                <div className={styles.progressBar()} style={{ width: percentage + '%' }} />
               </div>
             )}
           </ProgressBar>
