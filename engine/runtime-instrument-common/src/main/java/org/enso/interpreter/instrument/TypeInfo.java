6de087fd76bba21ea505473234dea5b8938444de
Dmitry Bushev <bushevdv@gmail.com>
2025-02-06 17:07:42 +0000

    misc: assert

--- engine/runtime-instrument-common/src/main/java/org/enso/interpreter/instrument/TypeInfo.java
+++ engine/runtime-instrument-common/src/main/java/org/enso/interpreter/instrument/TypeInfo.java
@@ -23,10 +23,6 @@ public record TypeInfo(String[] visibleType, String[] hiddenType) {
 
   @Override
   public String toString() {
-    return "TypeInfo("
-        + Arrays.toString(visibleType)
-        + ","
-        + Arrays.toString(hiddenType)
-        + ")";
+    return "TypeInfo(" + Arrays.toString(visibleType) + "," + Arrays.toString(hiddenType) + ")";
   }
 }
--- engine/runtime-instrument-common/src/main/java/org/enso/interpreter/service/ExecutionCallbacks.java
+++ engine/runtime-instrument-common/src/main/java/org/enso/interpreter/service/ExecutionCallbacks.java
@@ -4,6 +4,7 @@ import com.oracle.truffle.api.CompilerDirectives;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.function.Consumer;
 import org.enso.interpreter.instrument.ExpressionExecutionState;
@@ -228,6 +229,9 @@ final class ExecutionCallbacks implements IdExecutionService.Callbacks {
       if (publicTypes != null) {
         final Type[] allTypes = typeOfNode.findAllTypesOrNull(value, true);
         // Relies on the fact that allTypes appends extra types to the end of publicTypes.
+        assert (publicTypes.length == 0
+            || Objects.equals(
+                publicTypes[publicTypes.length - 1], allTypes[publicTypes.length - 1]));
         final Type[] conversionTypes =
             Arrays.copyOfRange(allTypes, publicTypes.length, allTypes.length);
 
