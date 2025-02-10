85061575fa071f674a97c0cd5ccaa15c87e7b023
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-02-10 07:40:51 +0100

    More tests on Integer type

--- engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/IntegerTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/IntegerTest.java
@@ -7,9 +7,12 @@ import static org.junit.Assert.assertTrue;
 import java.math.BigInteger;
 import org.enso.interpreter.node.expression.builtin.number.integer.AbsNode;
 import org.enso.interpreter.node.expression.builtin.number.integer.AddNode;
+import org.enso.interpreter.runtime.data.EnsoMultiValue;
+import org.enso.interpreter.runtime.data.Type;
 import org.enso.interpreter.runtime.error.PanicException;
 import org.enso.interpreter.runtime.number.EnsoBigInteger;
 import org.enso.test.utils.ContextUtils;
+import org.enso.test.utils.TestRootNode;
 import org.graalvm.polyglot.Context;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -20,6 +23,7 @@ public class IntegerTest {
 
   private static AbsNode absNode;
   private static AddNode addNode;
+  private static TestRootNode root;
   private static Context ctx;
 
   @BeforeClass
@@ -30,6 +34,9 @@ public class IntegerTest {
         () -> {
           absNode = AbsNode.build();
           addNode = AddNode.build();
+
+          root = new TestRootNode();
+          root.insertChildren(absNode, addNode);
           return null;
         });
   }
@@ -46,15 +53,33 @@ public class IntegerTest {
       new EnsoBigInteger(new BigInteger("-1000000000000000000000000000000000000"));
 
   @Test
-  public void testAbs() {
+  public void testAbs23() {
     ContextUtils.executeInContext(
         ctx,
         () -> {
           assertEquals(23L, absNode.execute(23L));
           assertEquals(23L, absNode.execute(-23L));
+          return null;
+        });
+  }
+
+  @Test
+  public void testAbsBig() {
+    ContextUtils.executeInContext(
+        ctx,
+        () -> {
           assertTrue(absNode.execute(Long.MIN_VALUE) instanceof EnsoBigInteger);
           assertEquals(bigInt, absNode.execute(bigInt));
           assertEquals(bigInt, absNode.execute(bigIntNegative));
+          return null;
+        });
+  }
+
+  @Test
+  public void testAbsPanic() {
+    ContextUtils.executeInContext(
+        ctx,
+        () -> {
           assertThrows(
               "Decimals are not supported", PanicException.class, () -> absNode.execute(23.0));
           assertThrows(
@@ -64,11 +89,47 @@ public class IntegerTest {
   }
 
   @Test
-  public void testAdd() {
+  public void testAdd21And1() {
     ContextUtils.executeInContext(
         ctx,
         () -> {
           assertEquals(23L, addNode.execute(22L, 1L));
+          return null;
+        });
+  }
+
+  @Test
+  public void testAddMulti21And1() {
+    ContextUtils.executeInContext(
+        ctx,
+        () -> {
+          var nn = EnsoMultiValue.NewNode.getUncached();
+          var leak = ContextUtils.leakContext(ctx);
+          var intType = leak.getBuiltins().number().getInteger();
+          var textType = leak.getBuiltins().text();
+          var both = new Type[] {intType, textType};
+          var twentyTwoHello = nn.newValue(both, 2, 0, new Object[] {22L, "Hello"});
+          assertEquals(23L, addNode.execute(twentyTwoHello, 1L));
+          return null;
+        });
+  }
+
+  @Test
+  public void testAddInterop21And1() {
+    ContextUtils.executeInContext(
+        ctx,
+        () -> {
+          var twentyOne = new WrappedPrimitive(21L);
+          assertEquals(23L, addNode.execute(twentyOne, 2L));
+          return null;
+        });
+  }
+
+  @Test
+  public void testAddLongAndText() {
+    ContextUtils.executeInContext(
+        ctx,
+        () -> {
           assertThrows(PanicException.class, () -> addNode.execute(23L, "Hello"));
           return null;
         });
