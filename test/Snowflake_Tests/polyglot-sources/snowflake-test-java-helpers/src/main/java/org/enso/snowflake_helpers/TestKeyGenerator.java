a26eb0ddda0528bb379e72da4bbc9b9dcf19b543
Radosław Waśko <radoslaw.wasko@enso.org>
2025-02-11 11:30:40 +0100

    test for encrypted key

--- test/Snowflake_Tests/polyglot-sources/snowflake-test-java-helpers/src/main/java/org/enso/snowflake_helpers/TestKeyGenerator.java
+++ test/Snowflake_Tests/polyglot-sources/snowflake-test-java-helpers/src/main/java/org/enso/snowflake_helpers/TestKeyGenerator.java
@@ -9,12 +9,20 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.Security;
+
+import net.snowflake.client.jdbc.internal.org.bouncycastle.jce.provider.BouncyCastleProvider;
+import net.snowflake.client.jdbc.internal.org.bouncycastle.openssl.PKCS8Generator;
 import net.snowflake.client.jdbc.internal.org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import net.snowflake.client.jdbc.internal.org.bouncycastle.openssl.jcajce.JcaPKCS8Generator;
+import net.snowflake.client.jdbc.internal.org.bouncycastle.openssl.jcajce.JceOpenSSLPKCS8EncryptorBuilder;
+import net.snowflake.client.jdbc.internal.org.bouncycastle.operator.OperatorCreationException;
+import net.snowflake.client.jdbc.internal.org.bouncycastle.operator.OutputEncryptor;
 
 public class TestKeyGenerator {
   public static void generateKeyPairForTest(
       String privateKeyPath, String publicKeyPath, String passphrase)
-      throws NoSuchAlgorithmException, IOException {
+      throws NoSuchAlgorithmException, IOException, OperatorCreationException {
 
     File privateKeyFile = new File(privateKeyPath);
     File publicKeyFile = new File(publicKeyPath);
@@ -50,7 +58,19 @@ public class TestKeyGenerator {
     }
   }
 
-  private static void savePrivateKeyEncrypted(PrivateKey key, File destination, String passphrase) {
-    throw new UnsupportedOperationException("TODO");
+  private static void savePrivateKeyEncrypted(PrivateKey key, File destination, String passphrase)
+      throws IOException, OperatorCreationException {
+    Security.addProvider(new BouncyCastleProvider());
+    var encryptorBuilder = new JceOpenSSLPKCS8EncryptorBuilder(PKCS8Generator.AES_256_CBC);
+    encryptorBuilder.setPassword(passphrase.toCharArray().clone());
+    OutputEncryptor encryptor = encryptorBuilder.build();
+    JcaPKCS8Generator pkcs8Generator = new JcaPKCS8Generator(key, encryptor);
+
+    try (FileOutputStream fileOutputStream = new FileOutputStream(destination);
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+        JcaPEMWriter pemWriter = new JcaPEMWriter(outputStreamWriter)) {
+      pemWriter.writeObject(pkcs8Generator.generate());
+      pemWriter.flush();
+    }
   }
 }
