20d7fde1f6a35d3bd707706f6daeb886ac885317
Adam Obuchowicz <adam.obuchowicz@enso.org>
2025-02-12 14:59:52 +0100

    Do not fetch submodules on CI

--- build_tools/ci_utils/src/actions/workflow/definition.rs
+++ build_tools/ci_utils/src/actions/workflow/definition.rs
@@ -1127,7 +1127,6 @@ pub fn checkout_repo_step() -> Step {
         with: Some(step::Argument::Checkout {
             repository: None,
             clean:      Some(false),
-            submodules: Some(CheckoutArgumentSubmodules::Recursive),
         }),
         ..default()
     }
