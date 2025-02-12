5040b023b11f11d88c42c5fe375e7b27a680ccb8
Pavel Marek <pavel.marek@enso.org>
2025-02-12 17:44:22 +0100

    Ignore consistency tests

--- engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/interop/MethodInvocationOnTypeConsistencyTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/interop/MethodInvocationOnTypeConsistencyTest.java
@@ -21,14 +21,16 @@ import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Value;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 /**
- * Tests consistency between invocation of methods on types via pure enso, and invocation of methods
- * on {@link org.enso.interpreter.runtime.data.Type} via {@link
+ * TODO[pm]: Ignored - https://github.com/enso-org/enso/pull/12099#issuecomment-2654281345 Tests
+ * consistency between invocation of methods on types via pure enso, and invocation of methods on
+ * {@link org.enso.interpreter.runtime.data.Type} via {@link
  * com.oracle.truffle.api.interop.InteropLibrary#invokeMember(Object, String, Object...) interop
  * protocol}.
  */
@@ -132,6 +134,7 @@ public final class MethodInvocationOnTypeConsistencyTest {
   }
 
   @Test
+  @Ignore("https://github.com/enso-org/enso/pull/12099#issuecomment-2654281345")
   public void methodInvocationViaInterop_IsConsistentWithPureEnso() {
     executeInContext(
         ctx,
