9fd4edd61dcf64b04c20e3c1be4422141c235199
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2025-02-12 15:01:47 +0100

    fmt

--- lib/scala/logging-service-logback/src/main/java/org/enso/logging/service/logback/DeferredProcessingSocketAppender.java
+++ lib/scala/logging-service-logback/src/main/java/org/enso/logging/service/logback/DeferredProcessingSocketAppender.java
@@ -3,9 +3,7 @@ package org.enso.logging.service.logback;
 import ch.qos.logback.classic.net.SocketAppender;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 
-/**
- * SocketAppender that ensures that MDC info is not lost during serialization.
- */
+/** SocketAppender that ensures that MDC info is not lost during serialization. */
 public class DeferredProcessingSocketAppender extends SocketAppender {
   @Override
   protected void append(ILoggingEvent event) {
