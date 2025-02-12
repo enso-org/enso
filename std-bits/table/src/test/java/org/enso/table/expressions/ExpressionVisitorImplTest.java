5645dbbd11aafb6a6b05bd280365715c54fb997c
Adam Riley <riley.ad@gmail.com>
2025-02-12 11:42:57 +0000

    Checkpoint

--- std-bits/table/src/test/java/org/enso/table/expressions/ExpressionVisitorImplTest.java
+++ std-bits/table/src/test/java/org/enso/table/expressions/ExpressionVisitorImplTest.java
@@ -23,6 +23,7 @@ public class ExpressionVisitorImplTest {
     @SuppressWarnings("unchecked")
     @Before
     public void setUp() {
+        Mockito.framework().clearInlineMocks();
         getColumn = (Function<String, Value>) mock(Function.class);
         makeConstantColumn = (Function<Object, Value>) mock(Function.class);
         getMethod = (Function<String, Value>) mock(Function.class);
@@ -34,9 +35,34 @@ public class ExpressionVisitorImplTest {
             expr, getColumn, makeConstantColumn, getMethod, makeConstructor, new String[]{});
     }
 
+    private Value mockValue(int value) {
+        Value val = mock(Value.class);
+        when(val.asInt()).thenReturn(value);
+        when(val.isNumber()).thenReturn(true);
+        return val;
+    }
+
     @Test
     public void testIntegerConstant() {
         Value result = evaluate("1");
         assertEquals(1, result.asInt());
+    }
+
+    @Test
+    public void testSimpleExpressionOnColumn() {
+        Value mockedColumn = mock(Value.class);
+        Value mockedMethod = mock(Value.class);
+        Value mockedResult = mockValue(5);
+
+        when(getColumn.apply("Column 1")).thenReturn(mockedColumn);
+        when(getMethod.apply("text_length")).thenReturn(mockedMethod);
+        when(mockedMethod.canExecute()).thenReturn(true);
+        when(mockedMethod.execute(mockedColumn)).thenReturn(mockedResult);
+        when(makeConstantColumn.apply(mockedResult)).thenReturn(mockedResult);
+
+        Value result = ExpressionVisitorImpl.evaluateImpl(
+            "text_length([Column 1])", getColumn, makeConstantColumn, getMethod, makeConstructor, new String[]{});
+
+        assertEquals(5, result.asInt());
     }
 }
