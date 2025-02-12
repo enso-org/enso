4ba025fa5306d6843f09eed6d84daeedc99c8b78
Adam Riley <riley.ad@gmail.com>
2025-02-12 13:32:06 +0000

    Fix

--- std-bits/table/src/test/java/org/enso/table/expressions/ExpressionVisitorImplTest.java
+++ std-bits/table/src/test/java/org/enso/table/expressions/ExpressionVisitorImplTest.java
@@ -1,21 +1,19 @@
-import static org.mockito.Mockito.*;
+package org.enso.table.expressions;
 
 import java.util.function.Function;
 
 import org.graalvm.polyglot.Value;
-import org.junit.Before;
+import static org.junit.Assert.assertEquals;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.quality.Strictness;
-
-import org.enso.table.expressions.ExpressionVisitorImpl;
-
-import static org.junit.Assert.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 public class ExpressionVisitorImplTest {
@@ -40,15 +38,15 @@ public class ExpressionVisitorImplTest {
     }
 
     @Test
-    public void testSimpleExpressionOnColumn() {
-        Value mockedColumn = mock(Value.class);
-        Value mockedMethod = mock(Value.class);
+    public void testSimpleMethodOnColumn() {
+        Value mockedColumn1 = mock(Value.class);
+        Value mockedMethodTextLength = mock(Value.class);
         Value mockedResult = mock(Value.class);
 
-        when(getColumn.apply("Column 2")).thenReturn(mockedColumn);
-        when(getMethod.apply("text_length")).thenReturn(mockedMethod);
-        when(mockedMethod.canExecute()).thenReturn(true);
-        when(mockedMethod.execute(mockedColumn)).thenReturn(mockedResult);
+        when(getColumn.apply("Column 1")).thenReturn(mockedColumn1);
+        when(getMethod.apply("text_length")).thenReturn(mockedMethodTextLength);
+        when(mockedMethodTextLength.canExecute()).thenReturn(true);
+        when(mockedMethodTextLength.execute(mockedColumn1)).thenReturn(mockedResult);
         when(makeConstantColumn.apply(mockedResult)).thenReturn(mockedResult);
 
         Value result = evaluate("text_length([Column 1])");
