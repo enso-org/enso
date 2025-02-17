955f63cae8ab8d68930f43a5d7dee84d33e0acf9
Dmitry Bushev <bushevdv@gmail.com>
2025-02-17 17:24:00 +0000

    misc: cleanup project-manager-shim

--- app/gui/project-manager-shim-middleware/projectManagement.ts
+++ app/gui/project-manager-shim-middleware/projectManagement.ts
@@ -13,7 +13,7 @@ import * as crypto from 'node:crypto'
 import * as fs from 'node:fs'
 import * as os from 'node:os'
 import * as pathModule from 'node:path'
-import * as stream from 'node:stream'
+import type * as stream from 'node:stream'
 
 import * as tar from 'tar'
 
