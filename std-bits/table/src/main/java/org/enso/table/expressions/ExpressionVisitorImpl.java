1142b3e2fb8c5e814a8d19ec6c370b8f485dbd6a
Adam Riley <riley.ad@gmail.com>
2025-02-18 11:15:40 +0000

    Remove previous -1 fix

--- std-bits/table/src/main/java/org/enso/table/expressions/ExpressionVisitorImpl.java
+++ std-bits/table/src/main/java/org/enso/table/expressions/ExpressionVisitorImpl.java
@@ -284,11 +284,7 @@ public class ExpressionVisitorImpl extends ExpressionBaseVisitor<Value> {
 
   @Override
   public Value visitUnaryMinus(ExpressionParser.UnaryMinusContext ctx) {
-    var v = visit(ctx.expr());
-    if (v.isNumber() && v.fitsInLong()) {
-      return Value.asValue(Math.negateExact(v.asLong()));
-    }
-    return executeMethod("*", v, Value.asValue(-1));
+    return executeMethod("*", visit(ctx.expr()), Value.asValue(-1));
   }
 
   @Override
