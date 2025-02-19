15411fbf14952c6e04d3b5fb4b4563dbb9f96bb2
James Dunkerley <jdunkerley@users.noreply.github.com>
2025-02-19 17:29:44 +0000

    Java Format.

--- std-bits/table/src/main/java/org/enso/table/data/column/operation/comparators/StringComparators.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/operation/comparators/StringComparators.java
@@ -1,19 +1,23 @@
 package org.enso.table.data.column.operation.comparators;
 
+import java.util.function.BiPredicate;
 import org.enso.base.Text_Utils;
 import org.enso.table.data.column.storage.ColumnStorage;
 import org.enso.table.data.column.storage.type.AnyObjectType;
 import org.enso.table.data.column.storage.type.TextType;
 
-import java.util.function.BiPredicate;
-
 public final class StringComparators extends GenericComparators<String> {
   public static final StringComparators EQ = new StringComparators(Text_Utils::equals, false);
-  public static final StringComparators NEQ = new StringComparators((a, b) -> !Text_Utils.equals(a, b), false);
-  public static final StringComparators LT = new StringComparators((a, b) -> Text_Utils.compare_normalized(a, b) < 0);
-  public static final StringComparators LTE = new StringComparators((a, b) -> Text_Utils.compare_normalized(a, b) <= 0);
-  public static final StringComparators GT = new StringComparators((a, b) -> Text_Utils.compare_normalized(a, b) > 0);
-  public static final StringComparators GTE = new StringComparators((a, b) -> Text_Utils.compare_normalized(a, b) >= 0);
+  public static final StringComparators NEQ =
+      new StringComparators((a, b) -> !Text_Utils.equals(a, b), false);
+  public static final StringComparators LT =
+      new StringComparators((a, b) -> Text_Utils.compare_normalized(a, b) < 0);
+  public static final StringComparators LTE =
+      new StringComparators((a, b) -> Text_Utils.compare_normalized(a, b) <= 0);
+  public static final StringComparators GT =
+      new StringComparators((a, b) -> Text_Utils.compare_normalized(a, b) > 0);
+  public static final StringComparators GTE =
+      new StringComparators((a, b) -> Text_Utils.compare_normalized(a, b) >= 0);
 
   private StringComparators(BiPredicate<String, String> comparator) {
     this(comparator, true);
