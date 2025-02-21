4b5852f8c15f3df05257dcdae390223dc4f4bf52
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-02-21 15:12:56 +0100

    Default timeout is 60 seconds

--- engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/instrument/RuntimeServerTesting.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/instrument/RuntimeServerTesting.java
@@ -83,7 +83,7 @@ final class RuntimeServerTesting {
                     new Runtime$Api$MethodPointer(moduleName, moduleName, "main"),
                     Option.empty(),
                     ScalaConversions.<String>nil().toVector()))));
-    var reply = context.receiveN(5, 3);
+    var reply = context.receiveN(5, 60);
     assertEquals("Five messages", 5, reply.size());
     assertEquals(
         "Output should be correct for " + reply + " messages",
