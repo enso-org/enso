e25b0afd6adde6db0fe59deb6fdbd34844497f25
Pavel Marek <pavel.marek@enso.org>
2025-02-21 17:42:49 +0100

    Update schema URI

--- lib/java/benchmarks-common/src/main/java/org/enso/interpreter/bench/BenchmarksRunner.java
+++ lib/java/benchmarks-common/src/main/java/org/enso/interpreter/bench/BenchmarksRunner.java
@@ -33,7 +33,7 @@ public class BenchmarksRunner {
   private final File resultsFile;
   private static final URI SCHEMA_URI =
       URI.create(
-          "https://raw.githubusercontent.com/enso-org/enso/d9a6478419472ce7c1fd58cc0108e8b904346fda/lib/java/benchmarks-common/src/main/resources/results_schema.json");
+          "https://raw.githubusercontent.com/enso-org/enso/c5c1f6031af37d1e1d7211a4e388b2f080ee4622/lib/java/benchmarks-common/src/main/resources/results_schema.json");
 
   public BenchmarksRunner() {
     this.resultsFile = new File("./bench-report.json");
