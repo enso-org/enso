a2e241baac3cd4566e0e243ae72ce015848744c4
Pavel Marek <pavel.marek@enso.org>
2025-02-24 18:00:30 +0100

    Use JSON schema from engine-benchmark-results repo

--- lib/java/benchmarks-common/src/main/java/org/enso/interpreter/bench/BenchmarksRunner.java
+++ lib/java/benchmarks-common/src/main/java/org/enso/interpreter/bench/BenchmarksRunner.java
@@ -33,7 +33,7 @@ public class BenchmarksRunner {
   private final File resultsFile;
   private static final URI SCHEMA_URI =
       URI.create(
-          "https://raw.githubusercontent.com/enso-org/enso/90760411d896d6e83b9a1054b49d4ef622046fc0/lib/java/benchmarks-common/src/main/resources/results_schema.json");
+          "https://raw.githubusercontent.com/enso-org/engine-benchmark-results/2b29af9dee5d4caea75a67fe2b1ef721dc09aaad/schema/cache-v2.json");
 
   public BenchmarksRunner() {
     this.resultsFile = new File("./bench-report.json");
