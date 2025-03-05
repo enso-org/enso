c540239727c9f4e779b23ea4e32c68c116c388b0
Pavel Marek <pavel.marek@enso.org>
2025-03-05 14:32:54 +0100

    Revert "[WIP] Add more logging"
    This reverts commit 51db71e1fc1e0d26e5f881d4e46a4f606d4d8094.

--- std-bits/base/src/main/java/org/enso/base/enso_cloud/logging/LogApiAccess.java
+++ std-bits/base/src/main/java/org/enso/base/enso_cloud/logging/LogApiAccess.java
@@ -162,7 +162,6 @@ public final class LogApiAccess {
     assert requestConfig != null
         : "The request configuration must be set before building a request.";
     var payload = buildPayload(messages);
-    LOGGER.warning(() -> "Constructed HttpRequest with payload " + payload + ". Requestconfig: " + requestConfig);
     return HttpRequest.newBuilder()
         .uri(requestConfig.apiUri())
         .header("Authorization", "Bearer " + requestConfig.accessToken())
@@ -220,7 +219,7 @@ public final class LogApiAccess {
       }
     } catch (RequestFailureException e) {
       if (retryCount < 0) {
-        LOGGER.severe("Failed to send log messages after retrying: " + e.getMessage() + ". The request was: " + request);
+        LOGGER.severe("Failed to send log messages after retrying: " + e.getMessage());
         throw e;
       } else {
         LOGGER.warning("Exception when sending log messages: " + e.getMessage() + ". Retrying...");
