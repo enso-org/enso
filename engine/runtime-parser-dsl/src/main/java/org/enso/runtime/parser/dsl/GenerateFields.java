5c4dfc5abc65192eb1403d378d4f3f0273252622
Pavel Marek <pavel.marek@enso.org>
2025-03-07 13:50:15 +0100

    Add docs

--- engine/runtime-parser-dsl/src/main/java/org/enso/runtime/parser/dsl/GenerateFields.java
+++ engine/runtime-parser-dsl/src/main/java/org/enso/runtime/parser/dsl/GenerateFields.java
@@ -6,9 +6,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Parameters of the constructor annotated with this annotation will be scanned by the IR processor
- * and <b>fields</b>will be generated for them. There can be only a single constructor with this
- * annotation in a class. The enclosing class must be annotated with {@link GenerateIR}.
+ * An annotation for a constructor. Parameters of the constructor annotated with this annotation
+ * will be scanned by the IR processor and <b>fields</b> will be generated for them. There can be
+ * only a single constructor with this annotation in a class. The enclosing class must be annotated
+ * with {@link GenerateIR}.
  *
  * <h2>Fields</h2>
  *
@@ -32,19 +33,43 @@ import java.lang.annotation.Target;
  *   <li>One of the <emph>meta</emph> types mentioned above
  * </ul>
  *
+ * <p>For every user-defined field, there will be a getter generated in the superclass. The getter
+ * has the same name as the field, and will be public. The body of the getter depends on the
+ * parameter type. See {@link IRChild}.
+ *
  * <p>A user-defined field generated out of constructor parameter annotated with {@link IRChild} is
  * a child element of this IR element. That means that it will be included in generated
  * implementation of IR methods that iterate over the IR tree. For example {@code mapExpressions} or
  * {@code children}.
  *
  * <p>A user-defined field generated out of constructor parameter annotated with {@link IRField}
- * will be a field with generated getters. Such field however, will not be part of the IR tree
- * traversal methods.
+ * will not be part of the IR tree traversal methods.
  *
  * <p>For a constructor parameter of a meta type, there will be no user-defined field generated, as
  * the meta fields are always generated.
  *
  * <p>Other types of constructor parameters are forbidden.
+ *
+ * <h2>Examples</h2>
+ *
+ * {@snippet : import scala.Option; import scala.collection.immutable.List; @GenerateIR // NameGen
+ * is the name of the class that will be generated. // This can be an arbitrary name. public final
+ * class Name extends NameGen { @GenerateFields public Name( // name will not be part of "tree
+ * elements" traversal methods. // But it will still be a field in the generated super
+ * class. @IRField String name, // Allowing Option as parameter type is mostly for backward
+ * compatibility @IRChild Option<IR> expressionOpt, @IRChild List<IR> annotations, // This is pretty
+ * much the same as `expressionOpt`, but can be null @IRChild(required = false) IR expression, //
+ * IdentifiedLocation is a special "meta" field that is always generated. // Therefore, it does not
+ * have to be annotated. IdentifiedLocation identifiedLocation ) { // Annotation processor generates
+ * a constructor with the same signature as // this constructor. super(name, expressionOpt,
+ * annotations, expression, identifiedLocation); }
+ *
+ * <p>// Need to override showCode, as the annotation processor cannot generate this // for us. It
+ * can, however, generate toString method. @Override public String showCode(int indent) { return "";
+ * } } }
+ *
+ * <p>For other examples, see the tests in {@code
+ * org.enso.runtime.parser.processor.test.TestIRProcessorInline}.
  */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.CONSTRUCTOR)
