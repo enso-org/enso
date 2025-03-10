40488c03b868db3b762f1c996cf2badd88fb605c
Pavel Marek <pavel.marek@enso.org>
2025-03-10 19:18:35 +0100

    Add test for FQN resolution in Pattern.Type

--- engine/runtime-integration-tests/src/test/java/org/enso/compiler/test/passes/resolve/FullyQualifiedNamesTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/compiler/test/passes/resolve/FullyQualifiedNamesTest.java
@@ -9,15 +9,25 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import org.enso.common.RuntimeOptions;
 import org.enso.compiler.MetadataInteropHelpers;
 import org.enso.compiler.core.IR;
+import org.enso.compiler.core.ir.Literal;
 import org.enso.compiler.core.ir.Location;
 import org.enso.compiler.core.ir.Module;
+import org.enso.compiler.core.ir.Pattern;
+import org.enso.compiler.core.ir.expression.Case;
+import org.enso.compiler.data.BindingsMap;
+import org.enso.compiler.data.BindingsMap.ResolvedType;
+import org.enso.compiler.pass.IRProcessingPass;
 import org.enso.compiler.pass.resolve.FullyQualifiedNames;
 import org.enso.compiler.pass.resolve.FullyQualifiedNames$;
+import org.enso.compiler.pass.resolve.Patterns;
+import org.enso.compiler.pass.resolve.Patterns$;
 import org.enso.pkg.QualifiedName;
 import org.enso.polyglot.PolyglotContext;
+import org.enso.scala.wrapper.ScalaConversions;
 import org.enso.test.utils.ContextUtils;
 import org.enso.test.utils.ProjectUtils;
 import org.enso.test.utils.SourceModule;
@@ -41,13 +51,68 @@ public final class FullyQualifiedNamesTest {
     try (var ctx = createCtx(projDir)) {
       compileAllModules(ctx);
       var modIr = getModuleIr(ctx, "local.Proj.Main");
-      var location = getLocationOf(mainSrc, "Proj");
+      var location = getLastLocationOf(mainSrc, "Proj");
       var ir = findIrByLocation(modIr, location);
       assertHasFQNMetadata(
           ir,
           FullyQualifiedNames.ResolvedModule.class,
           meta -> {
             assertThat(meta.moduleRef().getName().toString(), is("local.Proj.Main"));
+          });
+    }
+  }
+
+  /**
+   * Fully qualified name used in {@link Pattern.Type} is resolved in {@link Patterns} compiler
+   * pass. Moreover, this pass resolves it to {@link ResolvedType} - which means full resolution.
+   */
+  @Test
+  public void libNameIsResolved_InCaseBranch() throws Exception {
+    var myMod = srcModule("My_Module", """
+        type My_Type
+        """);
+    var mainMod =
+        srcModule(
+            "Main",
+            """
+        import project.My_Module.My_Type
+
+        main =
+            x = 42
+            case x of
+                _ : local.Proj.My_Module.My_Type -> 21  # Case.Branch
+                _ -> 22
+        """);
+    var projDir = TMP_DIR.newFolder("Proj").toPath();
+    ProjectUtils.createProject("Proj", Set.of(myMod, mainMod), projDir);
+    try (var ctx = createCtx(projDir)) {
+      compileAllModules(ctx);
+      var modIr = getModuleIr(ctx, "local.Proj.Main");
+      var caseBranch =
+          findIR(
+              modIr,
+              Case.Branch.class,
+              branch -> {
+                if (branch.expression() instanceof Literal.Number num) {
+                  return num.value().equals("21");
+                }
+                return false;
+              });
+      assertThat(caseBranch, is(notNullValue()));
+      var patternType = (Pattern.Type) caseBranch.pattern();
+      var tpeName = patternType.tpe();
+      assertHasMetadata(
+          tpeName,
+          Patterns$.MODULE$,
+          BindingsMap.Resolution.class,
+          resolution -> {
+            assertThat(
+                "Resolution target: " + resolution.target() + " should be ResolvedType",
+                resolution.target() instanceof ResolvedType,
+                is(true));
+            assertThat(
+                resolution.target().qualifiedName().toString(),
+                containsString("My_Module.My_Type"));
           });
     }
   }
@@ -78,7 +143,7 @@ public final class FullyQualifiedNamesTest {
     try (var ctx = createCtx(projDir)) {
       compileAllModules(ctx);
       var modIr = getModuleIr(ctx, "local.Proj.Main");
-      var location = getLocationOf(mainSrc, "Lib");
+      var location = getLastLocationOf(mainSrc, "Lib");
       var ir = findIrByLocation(modIr, location);
       assertHasFQNMetadata(
           ir,
@@ -87,6 +152,18 @@ public final class FullyQualifiedNamesTest {
             assertThat(meta.moduleRef().getName().toString(), is("local.Lib.Main"));
           });
     }
+  }
+
+  private static <T extends IR> T findIR(IR root, Class<T> expectedIRType, Predicate<T> predicate) {
+    for (var ir : ScalaConversions.asJava(root.preorder())) {
+      if (expectedIRType.isAssignableFrom(ir.getClass())) {
+        T castedIr = expectedIRType.cast(ir);
+        if (predicate.test(castedIr)) {
+          return castedIr;
+        }
+      }
+    }
+    return null;
   }
 
   private <T> void assertHasFQNMetadata(IR ir, Class<T> metaTargetType, Consumer<T> callback) {
@@ -101,6 +178,16 @@ public final class FullyQualifiedNamesTest {
         is(true));
     var target = metaTargetType.cast(meta.target());
     callback.accept(target);
+  }
+
+  private <T> void assertHasMetadata(
+      IR ir, IRProcessingPass pass, Class<T> expectedMetaType, Consumer<T> callback) {
+    var meta = MetadataInteropHelpers.getMetadataOrNull(ir, pass, expectedMetaType);
+    assertThat(
+        "Should have metadata from pass " + pass + " with type " + expectedMetaType.getName(),
+        meta,
+        is(notNullValue()));
+    callback.accept(meta);
   }
 
   private Context createCtx(Path projectRoot) {
@@ -127,9 +214,9 @@ public final class FullyQualifiedNamesTest {
     return modIr;
   }
 
-  private static Location getLocationOf(String moduleSrc, String expr) {
+  private static Location getLastLocationOf(String moduleSrc, String expr) {
     assertThat("moduleSrc must contain expr", moduleSrc, containsString(expr));
-    var start = moduleSrc.indexOf(expr);
+    var start = moduleSrc.lastIndexOf(expr);
     assert start != -1;
     var end = start + expr.length();
     return new Location(start, end);
