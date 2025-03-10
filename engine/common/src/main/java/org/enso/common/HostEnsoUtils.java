b14bb641613afc3b1dc79e1d7ce800fa6b928c80
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-03-10 13:39:52 +0100

    Properly formatted by human

--- engine/common/src/main/java/org/enso/common/HostEnsoUtils.java
+++ engine/common/src/main/java/org/enso/common/HostEnsoUtils.java
@@ -23,8 +23,9 @@ public final class HostEnsoUtils {
   }
 
   /**
-   * Checks the AOT mode.Delegates to {@link ImageInfo#is}
-     * @return {@code true} if running inside of native executable, {@code false} otherwise
+   * Checks state of the AOT mode. Delegates to {@link ImageInfo#inImageRuntimeCode}.
+   *
+   * @return {@code true} if running inside of native executable, {@code false} otherwise
    */
   public static boolean isAot() {
     return ImageInfo.inImageRuntimeCode();
