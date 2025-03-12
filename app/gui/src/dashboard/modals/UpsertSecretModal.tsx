39127b8a2723973f2df5e413a3647dbe7bfdc75e
somebody1234 <ehern.lee@gmail.com>
2025-03-12 21:22:11 +1000

    [dashboard/UpsertSecretModal] Reset form values when changing between secrets

--- app/gui/src/dashboard/modals/UpsertSecretModal.tsx
+++ app/gui/src/dashboard/modals/UpsertSecretModal.tsx
@@ -1,11 +1,9 @@
 /** @file Modal for confirming delete of any type of asset. */
 import { ButtonGroup, Dialog, DialogDismiss, Form, Input } from '#/components/AriaComponents'
+import { useSyncRef } from '#/hooks/syncRefHooks'
 import { useText } from '#/providers/TextProvider'
 import type { SecretId } from '#/services/Backend'
-
-// =========================
-// === UpsertSecretModal ===
-// =========================
+import { useEffect } from 'react'
 
 /** Props for a {@link UpsertSecretModal}. */
 export interface UpsertSecretModalProps {
@@ -26,20 +24,26 @@ export default function UpsertSecretModal(props: UpsertSecretModalProps) {
   const { canCancel = true, canReset = false } = props
   const { getText } = useText()
 
+  const form = Form.useForm({
+    schema: (z) => z.object({ title: z.string().min(1), value: z.string() }),
+    defaultValues: { title: nameRaw ?? '', value: '' },
+    onSubmit: async ({ title, value }) => {
+      await doCreate(title, value)
+      form.reset({ title, value })
+    },
+  })
+
+  const resetFormDeps = useSyncRef({ form })
+
+  useEffect(() => {
+    const deps = resetFormDeps.current
+    deps.form.reset({ title: nameRaw ?? '' })
+  }, [nameRaw, resetFormDeps])
+
   const isCreatingSecret = id == null
 
   const content = (
-    <Form
-      method="dialog"
-      schema={(z) => z.object({ title: z.string().min(1), value: z.string() })}
-      defaultValues={{ title: nameRaw ?? '', value: '' }}
-      onSubmit={async ({ title, value }, form) => {
-        await doCreate(title, value)
-        form.reset({ title, value })
-      }}
-      testId="upsert-secret-modal"
-      className="w-full"
-    >
+    <Form form={form} method="dialog" testId="upsert-secret-modal" className="w-full">
       <Input
         name="title"
         autoFocus
