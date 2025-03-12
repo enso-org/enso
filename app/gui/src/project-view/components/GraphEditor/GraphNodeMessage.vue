c4a3422f7e923b3de1ae1ce2670602b91b2b06c2
Adam Obuchowicz <adam.obuchowicz@enso.org>
2025-03-12 12:06:19 +0100

    Make preview over the warn/err message

--- app/gui/src/project-view/components/GraphEditor/GraphNodeMessage.vue
+++ app/gui/src/project-view/components/GraphEditor/GraphNodeMessage.vue
@@ -98,7 +98,7 @@ export const colorForMessageType: Record<MessageType, string> = {
   color: var(--color-text-inversed);
   background-color: var(--background-color);
   line-height: 20px;
-  z-index: 20;
+  z-index: -1;
 }
 
 .icon {
