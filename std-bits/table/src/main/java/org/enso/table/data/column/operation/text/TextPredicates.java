c0435f94ed007a0300f7a1bf723af1bc52a33de4
Adam Riley <riley.ad@gmail.com>
2025-03-13 11:28:02 +0000

    Green

--- std-bits/table/src/main/java/org/enso/table/data/column/operation/text/TextPredicates.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/operation/text/TextPredicates.java
@@ -1,10 +1,10 @@
 package org.enso.table.data.column.operation.text;
 
-import com.ibm.icu.impl.UnicodeRegex;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiPredicate;
 import java.util.regex.Pattern;
+
 import org.enso.base.Regex_Utils;
 import org.enso.base.Text_Utils;
 import org.enso.table.data.column.operation.comparators.GenericComparators;
@@ -13,11 +13,14 @@ import org.enso.table.data.column.storage.type.NullType;
 import org.enso.table.data.column.storage.type.TextType;
 import org.enso.table.error.UnexpectedTypeException;
 
+import com.ibm.icu.impl.UnicodeRegex;
+
 public final class TextPredicates extends GenericComparators<String> {
   public static final TextPredicates STARTS_WITH = new TextPredicates(Text_Utils::starts_with);
   public static final TextPredicates ENDS_WITH = new TextPredicates(Text_Utils::ends_with);
   public static final TextPredicates CONTAINS = new TextPredicates(Text_Utils::contains);
   public static final TextPredicates LIKE = new TextPredicates(TextPredicates::LikePredicate);
+  public static final TextPredicates REGEX_MATCH = new TextPredicates(TextPredicates::RegexMatchPredicate);
 
   private TextPredicates(BiPredicate<String, String> predicate) {
     super(predicate, true);
@@ -56,6 +59,10 @@ public final class TextPredicates extends GenericComparators<String> {
 
   private static Pattern createRegexPatternFromSql(String sqlPattern) {
     String regex = Regex_Utils.sql_like_pattern_to_regex(sqlPattern);
+    return createRegexPattern(regex);
+  }
+
+  private static Pattern createRegexPattern(String regex) {
     String unicodeTransformed = UnicodeRegex.fix(regex);
     /*
      * There is <a href="https://bugs.java.com/bugdatabase/view_bug.do?bug_id=8032926">a bug with Java
@@ -68,6 +75,13 @@ public final class TextPredicates extends GenericComparators<String> {
   private static boolean LikePredicate(String left, String right) {
     return regexCache
         .computeIfAbsent(right, TextPredicates::createRegexPatternFromSql)
+        .matcher(left)
+        .matches();
+  }
+
+  private static boolean RegexMatchPredicate(String left, String right) {
+    return regexCache
+        .computeIfAbsent(right, TextPredicates::createRegexPattern)
         .matcher(left)
         .matches();
   }
