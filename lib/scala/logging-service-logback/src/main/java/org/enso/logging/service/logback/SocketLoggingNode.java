a8ded99456761fb0b79b7c9f7b9609a66f3f0894
Pavel Marek <pavel.marek@enso.org>
2025-03-13 17:09:00 +0100

    Rollback SocketLoggingNode to develop

--- lib/scala/logging-service-logback/src/main/java/org/enso/logging/service/logback/SocketLoggingNode.java
+++ lib/scala/logging-service-logback/src/main/java/org/enso/logging/service/logback/SocketLoggingNode.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.util.UUID;
-import org.enso.logging.service.logback.telemetry.TelemetryAppender;
 
 // Contributors: Moses Hohman <mmhohman@rainbow.uchicago.edu>
 
@@ -31,8 +30,6 @@ public class SocketLoggingNode implements Runnable {
   LoggerContext context;
   HardenedLoggingEventInputStream hardenedLoggingEventInputStream;
   SocketAddress remoteSocketAddress;
-  private static final String TELEMETRY_LOGGER_NAME = "org.enso.telemetry";
-  private static final String TELEMETRY_APPENDER_NAME = "telemetry";
 
   Logger logger;
 
@@ -87,17 +84,13 @@ public class SocketLoggingNode implements Runnable {
             // ignore
           }
         }
-        if (event.getLoggerName().startsWith(TELEMETRY_LOGGER_NAME)) {
-          logTelemetryEvent(event);
-        } else {
-          // get a logger from the hierarchy. The name of the logger is taken to
-          // be the name contained in the event.
-          remoteLogger = context.getLogger(event.getLoggerName());
-          // apply the logger-level filter
-          if (remoteLogger.isEnabledFor(event.getLevel())) {
-            // finally log the event as if was generated locally
-            remoteLogger.callAppenders(event);
-          }
+        // get a logger from the hierarchy. The name of the logger is taken to
+        // be the name contained in the event.
+        remoteLogger = context.getLogger(event.getLoggerName());
+        // apply the logger-level filter
+        if (remoteLogger.isEnabledFor(event.getLevel())) {
+          // finally log the event as if was generated locally
+          remoteLogger.callAppenders(event);
         }
       }
     } catch (java.io.EOFException e) {
@@ -144,19 +137,6 @@ public class SocketLoggingNode implements Runnable {
         hardenedLoggingEventInputStream = null;
       }
     }
-  }
-
-  private void logTelemetryEvent(ILoggingEvent event) {
-    var telemetryLogger = context.getLogger(TELEMETRY_LOGGER_NAME);
-    var telemetryAppender = telemetryLogger.getAppender(TELEMETRY_APPENDER_NAME);
-    if (telemetryAppender == null) {
-      telemetryAppender = TelemetryAppender.create();
-      telemetryAppender.setContext(context);
-      telemetryAppender.setName(TELEMETRY_APPENDER_NAME);
-      telemetryAppender.start();
-      telemetryLogger.addAppender(telemetryAppender);
-    }
-    telemetryLogger.callAppenders(event);
   }
 
   @Override
