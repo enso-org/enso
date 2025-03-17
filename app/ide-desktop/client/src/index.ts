8caf5ecf49c3aa59c1d2b895ce1d7da5fcd8911c
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2025-03-17 16:32:32 +0100

    linter

--- app/ide-desktop/client/src/index.ts
+++ app/ide-desktop/client/src/index.ts
@@ -349,8 +349,7 @@ class App {
         this.args.groups.debug.options.profile.value ?
           ['--profiling-path', 'profiling.npss', ...backendProfileTime]
         : []
-      const backendJvmOpts =
-        this.args.options.jvm.value ? ['--jvm'] : []
+      const backendJvmOpts = this.args.options.jvm.value ? ['--jvm'] : []
       const backendOpts = [...backendVerboseOpts, ...backendProfileOpts, ...backendJvmOpts]
       const backendEnv = Object.assign({}, process.env, {
         SERVER_HOST: this.projectManagerHost,
