245f90a543bb155b942488b801d8cdf6766370f8
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2025-03-19 12:06:13 +0100

    unit test

--- lib/scala/pkg/src/test/scala/org/enso/pkg/ConfigSpec.scala
+++ lib/scala/pkg/src/test/scala/org/enso/pkg/ConfigSpec.scala
@@ -39,6 +39,44 @@ class ConfigSpec
       deserialized shouldEqual config
     }
 
+    "should be parseable and have the right values" in {
+      val config =
+        """name: placeholder
+          |vresion: dev
+          |namespace: local
+          |edition:
+          |  engine-version: 4.5.6
+          |license: none
+          |maintainers:
+          |  - name: A
+          |    email: a@example.com
+          |  - name: B
+          |  - email: c@example.com
+          |prefer-local-libraries: true
+          |jvm: true
+          |""".stripMargin
+      val parsed = Config.fromYaml(config).get
+      val expectedConfig = Config(
+        name           = "placeholder",
+        normalizedName = None,
+        version        = "dev",
+        namespace      = "local",
+        edition =
+          Some(Config.makeCompatibilityEditionFromVersion(SemVer.of(4, 5, 6))),
+        license = "none",
+        authors = List(),
+        maintainers = List(
+          Contact(Some("A"), Some("a@example.com")),
+          Contact(Some("B"), None),
+          Contact(None, Some("c@example.com"))
+        ),
+        preferLocalLibraries = true,
+        componentGroups      = None,
+        jvm                  = Some(true)
+      )
+      parsed shouldEqual expectedConfig
+    }
+
     "only require the name and use defaults for everything else" in {
       val parsed = Config.fromYaml("name: fooBar").get
       parsed.name shouldEqual "fooBar"
