9c7f2b4cd0298afa57cf649f0dc77e248740c13b
Pavel Marek <pavel.marek@enso.org>
2025-03-19 16:30:33 +0100

    Fix tests

--- engine/runtime-integration-tests/src/test/java/org/enso/logging/service/logback/telemetry/test/TestLogFormatter.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/logging/service/logback/telemetry/test/TestLogFormatter.java
@@ -5,14 +5,17 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.util.Map;
 import org.enso.logging.service.logback.telemetry.LogFormatter;
 import org.enso.logging.service.logback.telemetry.LogMessage;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestLogFormatter {
   @Test
   public void shouldNotTransformLog_WithIncorrectMessage() {
-    var logMessage = createLogMessage("Message - arg=1", 1);
+    var logMessage = createLogMessage("Message - arg={}", 1);
     var json = LogFormatter.transform(logMessage);
     assertThat("was not transformed", json, is(nullValue()));
   }
@@ -26,7 +29,7 @@ public class TestLogFormatter {
 
   @Test
   public void shouldNotTransformLog_WithRestrictedMetadata() {
-    var logMessage = createLogMessage("Message: type=1", 1);
+    var logMessage = createLogMessage("Message: type={}", 1);
     var json = LogFormatter.transform(logMessage);
     assertThat("was not transformed", json, is(nullValue()));
   }
@@ -40,7 +43,7 @@ public class TestLogFormatter {
 
   @Test
   public void shouldNotTransformLog_WithIncorrectArgumentDelimiter() {
-    var logMessage = createLogMessage("Message: arg1={}; arg2=2", {}, 2);
+    var logMessage = createLogMessage("Message: arg1={}; arg2={}", 2);
     var json = LogFormatter.transform(logMessage);
     assertThat("was not transformed", json, is(nullValue()));
   }
@@ -48,77 +51,80 @@ public class TestLogFormatter {
   @Test
   public void shouldFillLoggerName() {
     var loggerName = "org.enso.telemetry.MyLogger";
-    var logMessage = new LogMessage(loggerName, "msg: arg=1", new Object[] {1});
+    var logMessage = new LogMessage(loggerName, "msg: arg={}", new Object[] {1});
     var json = LogFormatter.transform(logMessage);
     assertThat(json.get("metadata").get("loggerName").asText(), is(loggerName));
   }
 
   @Test
   public void shouldFillMessage() {
-    var logMessage = createLogMessage("Message: arg=1", 1);
+    var logMessage = createLogMessage("Message: arg={}", 1);
     var json = LogFormatter.transform(logMessage);
-    assertThat(json.get("message").asText(), is("Message"));
+    assertMessage(json, "Message", Map.of("arg", 1));
   }
 
   @Test
   public void shouldFillMessage_WithMoreWords() {
     var logMessage = createLogMessage("This message has more words: arg={}", 1);
     var json = LogFormatter.transform(logMessage);
-    assertThat(json.get("message").asText(), is("This message has more words"));
+    assertMessage(json, "This message has more words", Map.of("arg", 1));
   }
 
   @Test
   public void shouldFillArgumentToMetadata() {
-    var logMessage = createLogMessage("Message: arg=1", 1);
+    var logMessage = createLogMessage("Message: arg={}", 1);
     var json = LogFormatter.transform(logMessage);
-    assertThat(json.get("metadata").get("arg").asInt(), is(1));
+    assertMessage(json, "Message", Map.of("arg", 1));
   }
 
   @Test
   public void shouldFillMoreArgumentsToMetadata() {
     var logMessage = createLogMessage("Message: arg1={}, arg2={}", 1, 2);
     var json = LogFormatter.transform(logMessage);
-    assertThat(json.get("metadata").get("arg1").asInt(), is(1));
-    assertThat(json.get("metadata").get("arg2").asInt(), is(2));
+    assertMessage(json, "Message", Map.of("arg1", 1, "arg2", 2));
   }
 
   @Test
   public void shouldFillMoreArgumentsToMetadata_WithWhiteSpacesAroundArguments() {
-    var logMessage = createLogMessage("Message: arg1 = 1 , arg2  =  2", 1, 2);
+    var logMessage = createLogMessage("Message: arg1 = {} , arg2  =  {}", 1, 2);
     var json = LogFormatter.transform(logMessage);
-    assertThat(json.get("metadata").get("arg1").asInt(), is(1));
-    assertThat(json.get("metadata").get("arg2").asInt(), is(2));
+    assertMessage(json, "Message", Map.of("arg1", 1, "arg2", 2));
   }
 
   @Test
   public void shouldFillBooleanArgumentToMetadata() {
     var logMessage = createLogMessage("Message: arg={}", true);
     var json = LogFormatter.transform(logMessage);
-    assertThat("was transformed", json, is(notNullValue()));
-    assertThat(json.get("metadata").get("arg").asBoolean(), is(true));
+    assertMessage(json, "Message", Map.of("arg", true));
   }
 
   @Test
   public void shouldFillStringArgumentToMetadata() {
     var logMessage = createLogMessage("Message: arg={}", "some string");
     var json = LogFormatter.transform(logMessage);
-    assertThat("was transformed", json, is(notNullValue()));
-    assertThat(json.get("metadata").get("arg").asText(), is("some string"));
-  }
-
-  /**
-   * When there is a clash between argument value inside message and inside {@link
-   * LogMessage#arguments}, the argument from log message has precedence.
-   */
-  @Test
-  public void shouldFillArgument_FromLogEventArgArray() {
-    var logMessage = createLogMessage("Message: arg=42", 1);
-    var json = LogFormatter.transform(logMessage);
-    assertThat("was transformed", json, is(notNullValue()));
-    assertThat(json.get("metadata").get("arg").asInt(), is(1));
+    assertMessage(json, "Message", Map.of("arg", "some string"));
   }
 
   private static LogMessage createLogMessage(String message, Object... args) {
     return new LogMessage("org.enso.telemetry.MyLogger", message, args);
+  }
+
+  private static void assertMessage(
+      ObjectNode json, String message, Map<String, Object> arguments) {
+    assertThat(json, is(notNullValue()));
+    assertThat("Has message property", json.has("message"), is(notNullValue()));
+    assertThat("Has message property", json.get("message").asText(), is(message));
+    var meta = json.get("metadata");
+    for (var arg : arguments.entrySet()) {
+      var argName = arg.getKey();
+      assertThat("Has argument " + argName, meta.has(argName), is(true));
+      var argValFromJson = meta.get(argName);
+      switch (arg.getValue()) {
+        case Integer i -> assertThat(argValFromJson.asInt(), is(i));
+        case Boolean b -> assertThat(argValFromJson.asBoolean(), is(b));
+        case String s -> assertThat(argValFromJson.asText(), is(s));
+        default -> Assert.fail("Unsupported argument type: " + arg.getValue());
+      }
+    }
   }
 }
