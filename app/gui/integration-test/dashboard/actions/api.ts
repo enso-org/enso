47f4bc4fe1279ce4800d0ce7e194fdb8bce71697
somebody1234 <ehern.lee@gmail.com>
2025-03-25 03:10:55 +1000

    [dashboard] Remove debug log from API mocks for integration tests

--- app/gui/integration-test/dashboard/actions/api.ts
+++ app/gui/integration-test/dashboard/actions/api.ts
@@ -232,8 +232,6 @@ async function mockApiInternal({ page, setupAPI }: MockParams) {
 
   usersMap.set(defaultUser.userId, defaultUser)
 
-  console.log(backend.ParentsPath, backend.VirtualParentsPath, backend.EnsoPath)
-
   function getParentPath(parentId: backend.DirectoryId, acc: string[] = []) {
     const parent = assetMap.get(parentId)
 
