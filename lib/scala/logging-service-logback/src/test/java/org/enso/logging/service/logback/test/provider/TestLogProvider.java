84a082cd05785e88ab1ede7fbc5b80e0ef532dd3
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-03-25 08:32:51 +0100

    Revert console logging

--- lib/scala/logging-service-logback/src/test/java/org/enso/logging/service/logback/test/provider/TestLogProvider.java
+++ lib/scala/logging-service-logback/src/test/java/org/enso/logging/service/logback/test/provider/TestLogProvider.java
@@ -6,7 +6,6 @@ import org.enso.logging.config.MissingConfigurationField;
 import org.enso.logging.service.logback.LogbackSetup;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.IMarkerFactory;
-import org.slf4j.event.Level;
 import org.slf4j.spi.MDCAdapter;
 import org.slf4j.spi.SLF4JServiceProvider;
 
@@ -23,9 +22,7 @@ public class TestLogProvider implements SLF4JServiceProvider {
     assert factory instanceof LoggerContext;
     if (!initialized) {
       try {
-        var setup = new LogbackSetup((LoggerContext) factory);
-        setup.setup();
-        setup.setupConsoleAppender(Level.WARN);
+        new LogbackSetup((LoggerContext) factory).setup();
         initialized = true;
       } catch (MissingConfigurationField e) {
         throw new RuntimeException(e);
