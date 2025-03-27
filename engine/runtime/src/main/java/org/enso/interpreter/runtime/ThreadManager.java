7fc45e97e029b4c4b405ba7219e1fce4f12012b9
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-03-27 14:39:44 +0100

    Simplify internal state

--- engine/runtime/src/main/java/org/enso/interpreter/runtime/ThreadManager.java
+++ engine/runtime/src/main/java/org/enso/interpreter/runtime/ThreadManager.java
@@ -6,19 +6,14 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Supplier;
 import org.enso.interpreter.runtime.control.ThreadInterruptedException;
 
 /** Manages threads running guest code, exposing a safepoint-like functionality. */
 public final class ThreadManager {
-  private final ReentrantLock lock = new ReentrantLock();
   private final ThreadExecutors threads;
   private final ExecutorService questCode;
   private final ConcurrentHashMap<Thread, Boolean> interruptFlags = new ConcurrentHashMap<>();
-
-  private static final Object ENTERED = new Object();
-  private static final Object NO_OP = new Object();
 
   ThreadManager(ThreadExecutors th, int throughput, Env env) {
     this.threads = th;
@@ -33,14 +28,15 @@ public final class ThreadManager {
    * @return observable future filled with the computed value
    */
   public final <T> CompletableFuture<T> submit(Supplier<T> action) {
-    if (isNotEnteredYet()) {
+    var notYetEntered = interruptFlags.get(Thread.currentThread()) == null;
+    if (notYetEntered) {
       Supplier<T> wrap =
           () -> {
-            var tok = enter();
+            interruptFlags.put(Thread.currentThread(), false);
             try {
               return action.get();
             } finally {
-              leave(tok);
+              interruptFlags.remove(Thread.currentThread());
             }
           };
       return CompletableFuture.supplyAsync(wrap, questCode);
@@ -63,39 +59,33 @@ public final class ThreadManager {
    * <p>This method may not be called from a thread that is itself managed by this system, as doing
    * so may result in a deadlock.
    */
-  public void interruptThreads() {
-    lock.lock();
-    try {
-      interruptFlags.replaceAll((t, b) -> true);
-      Object p = enter();
-      try {
-        submitThreadLocal(
-            null,
-            new ThreadLocalAction(true, false) {
-              @Override
-              protected void perform(ThreadLocalAction.Access access) {
-                Boolean interrupt = interruptFlags.get(access.getThread());
-                if (Boolean.TRUE.equals(interrupt)) {
-                  throw new ThreadInterruptedException();
-                }
-              }
-            });
-      } finally {
-        leave(p);
-      }
-    } finally {
-      lock.unlock();
-    }
+  public final void interruptThreads() {
+    interruptFlags.replaceAll((t, b) -> true);
+    submitThreadLocal(
+        null,
+        new ThreadLocalAction(true, false) {
+          @Override
+          protected void perform(ThreadLocalAction.Access access) {
+            Boolean interrupt = interruptFlags.get(access.getThread());
+            if (Boolean.TRUE.equals(interrupt)) {
+              throw new ThreadInterruptedException();
+            }
+          }
+        });
   }
 
   /** Requests that all threads are shutdown. */
-  public void shutdown() {
+  public final void shutdown() {
+    var hasBeenInterrupted = Thread.interrupted();
     for (var t : interruptFlags.keySet()) {
       try {
         t.join();
       } catch (InterruptedException e) {
-        e.printStackTrace();
+        hasBeenInterrupted = true;
       }
+    }
+    if (hasBeenInterrupted) {
+      Thread.currentThread().interrupt();
     }
   }
 
@@ -106,48 +96,11 @@ public final class ThreadManager {
    * @param action the action to execute at given threads
    * @return future to check whether action has been executed
    */
-  public Future<Void> submitThreadLocal(Thread[] threads, ThreadLocalAction action) {
+  public final Future<Void> submitThreadLocal(Thread[] threads, ThreadLocalAction action) {
     return this.threads.submitThreadLocal(threads, action);
   }
 
   final Thread createThread(boolean systemThread, Runnable run) {
     return this.threads.createThread(systemThread, run);
-  }
-
-  /**
-   * Registers the current thread as running guest code.
-   *
-   * <p>From this point on, the thread is assumed to be controlled by the Enso runtime and e.g. will
-   * be waited on in safepoints.
-   *
-   * <p>{@link #leave(Object)} must be called immediately after guest execution is finished in the
-   * given thread, otherwise a deadlock may occur.
-   *
-   * @return a token object that must be passed to {@link #leave(Object)}. This object is opaque,
-   *     with no guarantees on its structure.
-   */
-  private Object enter() {
-    if (isNotEnteredYet()) {
-      interruptFlags.put(Thread.currentThread(), false);
-      return ENTERED;
-    }
-    return NO_OP;
-  }
-
-  private boolean isNotEnteredYet() {
-    return interruptFlags.get(Thread.currentThread()) == null;
-  }
-
-  /**
-   * Deregisters the current thread from the control of the Enso runtime.
-   *
-   * <p>The thread may no longer execute Enso code, until {@link #enter()} is called again.
-   *
-   * @param token the token returned by the corresponding call to {@link #enter()}.
-   */
-  private void leave(Object token) {
-    if (token != NO_OP) {
-      interruptFlags.remove(Thread.currentThread());
-    }
   }
 }
