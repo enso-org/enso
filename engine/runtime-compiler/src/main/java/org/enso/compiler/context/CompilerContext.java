28e2717abe3df8884c4b7d7b2812803fffa78d3c
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-03-27 15:16:04 +0100

    CompilerContext.newParsingPool() rather than generic method for creating threads

--- engine/runtime-compiler/src/main/java/org/enso/compiler/context/CompilerContext.java
+++ engine/runtime-compiler/src/main/java/org/enso/compiler/context/CompilerContext.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.URI;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.function.Consumer;
 import java.util.logging.Level;
@@ -68,7 +69,7 @@ public interface CompilerContext extends CompilerStub {
   // threads
   boolean isCreateThreadAllowed();
 
-  Thread createSystemThread(Runnable r);
+  ExecutorService newParsingPool();
 
   // Truffle related
 
