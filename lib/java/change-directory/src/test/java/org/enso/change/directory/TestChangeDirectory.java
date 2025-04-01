982cff0ed1dcf7fe03607eb2574aac1d10fd09c4
Pavel Marek <pavel.marek@enso.org>
2025-04-01 10:51:39 +0200

    Test change dir

--- lib/java/change-directory/src/test/java/org/enso/change/directory/TestChangeDirectory.java
+++ lib/java/change-directory/src/test/java/org/enso/change/directory/TestChangeDirectory.java
@@ -1,7 +1,11 @@
 package org.enso.change.directory;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import org.graalvm.nativeimage.ImageInfo;
 import org.junit.Test;
 
@@ -10,7 +14,9 @@ public class TestChangeDirectory {
 
   private static void ensureInNativeImage() {
     var inNativeImage = ImageInfo.inImageRuntimeCode();
-    assert inNativeImage;
+    assumeTrue(
+        "This test should only be run in a native image. Please run it in a native image.",
+        inNativeImage);
   }
 
   @Test
@@ -20,5 +26,17 @@ public class TestChangeDirectory {
     var curDir = nativeApi.currentWorkingDir();
     var expectedDir = System.getProperty("user.dir");
     assertEquals(expectedDir, curDir);
+  }
+
+  @Test
+  public void changeDir() throws IOException {
+    ensureInNativeImage();
+    var tmpDir = Files.createTempDirectory("TestChangeDirectory");
+    var tmpDirAbs = tmpDir.toAbsolutePath().toString();
+    var nativeApi = WorkingDirectory.getForCurrentPlatform();
+    var succeeded = nativeApi.changeWorkingDir(tmpDirAbs);
+    assertTrue(succeeded);
+    var curDir = nativeApi.currentWorkingDir();
+    assertEquals(tmpDirAbs, curDir);
   }
 }
