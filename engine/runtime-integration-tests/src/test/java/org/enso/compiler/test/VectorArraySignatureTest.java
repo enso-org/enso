5ab4c7a4732e46d42bd9d076baa598c6c0a63f74
Gregory Travis <greg.travis@enso.org>
2025-04-02 12:48:00 -0400

    doc test

--- engine/runtime-integration-tests/src/test/java/org/enso/compiler/test/VectorArraySignatureTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/compiler/test/VectorArraySignatureTest.java
@@ -150,8 +150,7 @@ public class VectorArraySignatureTest {
                   d.doc()
                       .replace("]", "].to_array")
                       .replace("a vector", "an array")
-                      .replace("vector", "array")
-                      .replace("Vector", "Array");
+                      .replace("vector", "array");
               assertEquals("Bad documentation for Array." + name, exp, ad.doc());
             }
           }
