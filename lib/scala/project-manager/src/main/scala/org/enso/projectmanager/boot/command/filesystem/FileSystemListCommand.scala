072d4171d69ceaf50cb8d9468f2cca7141277524
Dmitry Bushev <bushevdv@gmail.com>
2025-04-07 11:19:00 +0100

    Revert "Hide temporary directories of projects running in Hybrid mode (#12735)"
    This reverts commit 7c5bcb46ddb97b10a6d610d52317baf766e73907.

--- lib/scala/project-manager/src/main/scala/org/enso/projectmanager/boot/command/filesystem/FileSystemListCommand.scala
+++ lib/scala/project-manager/src/main/scala/org/enso/projectmanager/boot/command/filesystem/FileSystemListCommand.scala
@@ -55,9 +55,6 @@ object FileSystemListCommand {
     new FileSystemListCommand[F](service, path)
   }
 
-  /** Prefix for the temporary directory of a cloud project running in hybrid mode. */
-  private val CLOUD_PROJECT_PREFIX = "cloud-project-"
-
   /** Filters the files system entries that are not hidden.
     *
     * @param entries the file system entries
@@ -66,7 +63,7 @@ object FileSystemListCommand {
   private def filterNotHidden(
     entries: Seq[FileSystemEntry]
   ): Seq[FileSystemEntry] =
-    entries.filterNot(entry => isHidden(entry) || isTempCloudProject(entry))
+    entries.filterNot(isHidden)
 
   /** Checks whether the provided entry is hidden.
     *
@@ -78,16 +75,5 @@ object FileSystemListCommand {
     */
   private def isHidden(entry: FileSystemEntry): Boolean = {
     entry.path.isHidden || entry.path.getName.startsWith(".")
-  }
-
-  /** Check whether the provided entry is a temporary directory for a cloud
-    * project running in hybrid mode.
-    *
-    * @param entry the file system entry
-    * @return `true` if the entry is a temporary cloud project directory
-    */
-  private def isTempCloudProject(entry: FileSystemEntry): Boolean = {
-    entry.path.isDirectory &&
-    entry.path.getName.startsWith(CLOUD_PROJECT_PREFIX)
   }
 }
