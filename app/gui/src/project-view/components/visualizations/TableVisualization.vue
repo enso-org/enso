8983ed9a08da80de7f88cc56c0ae77726308607b
Martha King <martha.king@enso.org>
2025-04-07 15:42:25 +0100

    correct shift for db tables

--- app/gui/src/project-view/components/visualizations/TableVisualization.vue
+++ app/gui/src/project-view/components/visualizations/TableVisualization.vue
@@ -770,7 +770,7 @@ watchEffect(() => {
       : dataHeader
     if (!data_.is_using_server_sort_and_filter) {
       const hasIndexRow =
-        config.nodeType === TABLE_NODE_TYPE || config.nodeType === COLUMN_NODE_TYPE
+        config.nodeType === TABLE_NODE_TYPE || config.nodeType === COLUMN_NODE_TYPE || config.nodeType === DB_TABLE_NODE_TYPE
       const shift = hasIndexRow ? 1 : 0
       rowData.value =
         data_.data ?
