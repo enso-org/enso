9ced9cbf8ee0930e0cbba3f598ef30965184f26a
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2025-04-09 10:39:53 +0200

    snwoflake-jdbc-thin forced update of signed archives
    We no longer use Snowflake's fat jar, meaning that native libraries are
    in their respective dependencies.

--- app/ide-desktop/client/tasks/signArchivesMacOs.ts
+++ app/ide-desktop/client/tasks/signArchivesMacOs.ts
@@ -118,9 +118,14 @@ async function ensoPackageSignables(resourcesDir: string): Promise<Signable[]> {
       ],
     ],
     [
-      'lib/Standard/Snowflake/*/polyglot/java/snowflake-jdbc-*.jar',
+      'lib/Standard/Snowflake/*/polyglot/java/conscript-openjdk-uber-*.jar',
       [
         'META-INF/native/libconscrypt_openjdk_jni-osx-*.dylib',
+      ],
+    ],
+    [
+      'lib/Standard/Snowflake/*/polyglot/java/grpc-netty-shaded-*.jar',
+      [
         'META-INF/native/libio_grpc_netty_shaded_netty_tcnative_osx_*.jnilib',
       ],
     ],
