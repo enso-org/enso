bf828f6d65f855edab0c9920ed6d703e110cc043
somebody1234 <ehern.lee@gmail.com>
2025-04-09 17:43:56 +1000

    [dashboard/DriveBarToolbar] Improve copied/cut visual state

--- app/gui/src/dashboard/pages/dashboard/Drive/DriveBar/DriveBarToolbar.tsx
+++ app/gui/src/dashboard/pages/dashboard/Drive/DriveBar/DriveBarToolbar.tsx
@@ -15,8 +15,9 @@ import {
   Button,
   ButtonGroup,
   DialogTrigger,
-  Text,
+  IconDisplay,
   useVisualTooltip,
+  VisualTooltip,
 } from '#/components/AriaComponents'
 import { ErrorBoundary, InlineErrorDisplay } from '#/components/ErrorBoundary'
 import {
@@ -157,11 +158,18 @@ export function DriveBarToolbar(props: DriveBarToolbarProps) {
 
   const pasteDataStatus = effectivePasteData && (
     <div className="flex items-center">
-      <Text>
-        {effectivePasteData.type === 'copy' ?
-          getText('xItemsCopied', effectivePasteData.data.ids.size)
-        : getText('xItemsCut', effectivePasteData.data.ids.size)}
-      </Text>
+      <VisualTooltip
+        tooltip={
+          effectivePasteData.type === 'copy' ?
+            getText('xItemsCopied', effectivePasteData.data.ids.size)
+          : getText('xItemsCut', effectivePasteData.data.ids.size)
+        }
+        tooltipPlacement="top"
+      >
+        <IconDisplay icon={effectivePasteData.type === 'copy' ? 'copy' : 'scissors'}>
+          {String(effectivePasteData.data.ids.size)}
+        </IconDisplay>
+      </VisualTooltip>
     </div>
   )
 
