371784c701f290b760edf4048699f6e35a16bbcc
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2025-04-11 15:04:23 +0200

    fmt

--- std-bits/snowflake/src/main/java/org/enso/snowflake/BouncyCastleInitializer.java
+++ std-bits/snowflake/src/main/java/org/enso/snowflake/BouncyCastleInitializer.java
@@ -5,6 +5,7 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public final class BouncyCastleInitializer {
   private BouncyCastleInitializer() {}
+
   static {
     Security.addProvider(new BouncyCastleProvider());
   }
