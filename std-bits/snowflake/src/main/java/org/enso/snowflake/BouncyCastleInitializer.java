be4cfc084007489036d17b623777f76295c668fe
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2025-04-11 09:31:29 +0200

    nit

--- std-bits/snowflake/src/main/java/org/enso/snowflake/BouncyCastleInitializer.java
+++ std-bits/snowflake/src/main/java/org/enso/snowflake/BouncyCastleInitializer.java
@@ -5,7 +5,6 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class BouncyCastleInitializer {
   static {
-    System.out.println("TEST KEY GENERATOR IS RUN AT BUILD TIME!");
     Security.addProvider(new BouncyCastleProvider());
   }
 }
