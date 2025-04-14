b3f5894da3c4c568fefa5772777a6342495a9a36
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-04-14 08:32:34 +0200

    Invoke Short.parseShort method

--- lib/java/os-environment/src/test/java/org/enso/os/environment/jni/LoadClassTest.java
+++ lib/java/os-environment/src/test/java/org/enso/os/environment/jni/LoadClassTest.java
@@ -1,7 +1,9 @@
 package org.enso.os.environment.jni;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.graalvm.nativeimage.StackValue;
 import org.graalvm.nativeimage.c.type.CTypeConversion;
 import org.junit.Test;
 
@@ -9,16 +11,31 @@ public class LoadClassTest {
   private static final String PATH = System.getProperty("java.home");
 
   @Test
-  public void loadJavaLangShortClass() {
+  public void invokeParseShortMethod() {
     var env = JVM.create(PATH).env();
     assertTrue("JNI created", env.isNonNull());
 
     var findClassFn = env.getFunctions().getFindClass();
+    var getStaticMethodIDFn = env.getFunctions().getGetStaticMethodID();
+    var newStringFn = env.getFunctions().getNewStringUTF();
+    var callStaticMethodFn = env.getFunctions().getCallStaticIntMethodA();
 
-    try (var nameHolder = CTypeConversion.toCString("java/lang/Short")) {
-      var clazz = findClassFn.call(env, nameHolder.get());
+    try (var shortName = CTypeConversion.toCString("java/lang/Short");
+        var valueOfName = CTypeConversion.toCString("parseShort");
+        var valueOfSig = CTypeConversion.toCString("(Ljava/lang/String;)S");
+        var toParse = CTypeConversion.toCString("345"); ) {
+      var Short = findClassFn.call(env, shortName.get());
 
-      assertTrue("Short class is loaded", clazz.isNonNull());
+      assertTrue("Short class is loaded", Short.isNonNull());
+
+      var valueOf = getStaticMethodIDFn.call(env, Short, valueOfName.get(), valueOfSig.get());
+      assertTrue("valueOf method found", valueOf.isNonNull());
+
+      var args = StackValue.get(JNI.JValue.class);
+      var str = newStringFn.call(env, toParse.get());
+      args.setJObject(str);
+      var res = callStaticMethodFn.call(env, Short, valueOf, args);
+      assertEquals(345, res);
     }
   }
 }
