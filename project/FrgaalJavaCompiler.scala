400be7093c6f4273e21b22ae3d9b36195eca3167
Pavel Marek <pavel.marek@enso.org>
2025-04-15 19:20:21 +0200

    Frgaal has access to jdk.unsupported module

--- project/FrgaalJavaCompiler.scala
+++ project/FrgaalJavaCompiler.scala
@@ -303,7 +303,9 @@ object FrgaalJavaCompiler {
         "java.desktop",
         "java.net.http",
         "java.sql",
-        "jdk.jfr"
+        "jdk.jfr",
+        // sun.misc.Unsafe
+        "jdk.unsupported"
       )
       val limitModulesArgs =
         if (shouldNotLimitModules) Seq()
