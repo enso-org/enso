f45bed5312d3d92bfdeea5d00a2c65ea63a2035e
Paweł Grabarz <frizi09@gmail.com>
2025-04-17 00:17:01 +0200

    add playwright flag

--- build_tools/build/src/ci_gen/job.rs
+++ build_tools/build/src/ci_gen/job.rs
@@ -851,6 +851,7 @@ rm dist/backend/project-manager.tar"
                             "projectToken".into(),
                             "${{ secrets.ELECTRON_IDE_CHROMATIC_PROJECT_TOKEN }}".into(),
                         ),
+                        ("playwright".into(), true.into()),
                         ("storybookBuildDir".into(), "app/ide-desktop/client/test-results".into()),
                     ]))),
                     ..Default::default()
