e691ef3414b033848deb9977bd703a6d53fc9076
Paweł Grabarz <frizi09@gmail.com>
2025-04-17 14:21:16 +0200

    adjust test traces path to not include characters that CI doesn't like

--- app/ide-desktop/client/tests/electronTest.ts
+++ app/ide-desktop/client/tests/electronTest.ts
@@ -39,7 +39,7 @@ export const test = base.extend<{
   page: Page
 }>({
   testRunId: async function ({}, use, testInfo) {
-    await use(`${testInfo.testId}-${new Date().toISOString()}`)
+    await use(`${testInfo.testId}-${Date.now()}`)
   },
   projectsDir: async function ({ testRunId }, use) {
     const projectsDir = path.join(os.tmpdir(), 'enso-test-projects', testRunId)
