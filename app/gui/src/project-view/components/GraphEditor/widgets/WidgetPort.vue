41725162736380435333e1c5cdfe46e21b64faf9
Ilya Bogdanov <fumlead@gmail.com>
2025-04-21 23:50:38 +0400

    Avoid using string comparison for portId

--- app/gui/src/project-view/components/GraphEditor/widgets/WidgetPort.vue
+++ app/gui/src/project-view/components/GraphEditor/widgets/WidgetPort.vue
@@ -147,7 +147,10 @@ export const widgetDefinition = defineWidget(
       // in argument positions. A port needs to be created with portId override, and it
       // will be handled by `WidgetTypeCastPort`. Without this check, the port will be
       // created with invalid portId because of `ArgumentInfoKey` being set on the input.
-      if (props.input.portId.includes('TypeAnnotated')) {
+      if (
+        props.input.value instanceof Ast.TypeAnnotated &&
+        props.input.value.id === props.input.portId
+      ) {
         return Score.Mismatch
       }
       if (portInfo != null && value instanceof Ast.Ast && portInfo.portId === value.id) {
