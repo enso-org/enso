53b8f3e216cf0d2c931005d5640127f5e00767ed
Adam Riley <riley.ad@gmail.com>
2025-04-22 16:50:53 +0100

    Green

--- std-bits/tableau/src/main/java/org/enso/tableau/HyperReader.java
+++ std-bits/tableau/src/main/java/org/enso/tableau/HyperReader.java
@@ -351,7 +351,7 @@ public class HyperReader {
           case DateType x -> tableDef.addColumn(columnName, SqlType.date());
           case TimeOfDayType x -> tableDef.addColumn(columnName, SqlType.time());
           case DateTimeType x -> tableDef.addColumn(columnName, SqlType.timestampTz());
-          default -> throw new IllegalArgumentException("Unknown type");
+          default -> throw new HyperUnsupportedTypeError(storage.getType().toString());
         }
       }
 
@@ -379,7 +379,7 @@ public class HyperReader {
               case LocalDate ld -> inserter.add(ld);
               case LocalTime lt -> inserter.add(lt);
               case ZonedDateTime zdt -> inserter.add(zdt);
-              default -> throw new IllegalArgumentException("Unknown type");
+              default -> throw new HyperUnsupportedTypeError(value.toString());
             }
           }
         }
