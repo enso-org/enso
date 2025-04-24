24a57b535fb0a57251960548d5a83e45ef3e84c5
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2025-04-24 11:04:39 +0200

    Add unit test for visualization attached to param
    Also adjusted parser to PR comments.

--- engine/runtime-parser/src/main/java/org/enso/compiler/core/TreeToIr.java
+++ engine/runtime-parser/src/main/java/org/enso/compiler/core/TreeToIr.java
@@ -1518,7 +1518,7 @@ final class TreeToIr {
         ascribedType,
         defaultValue,
         isSuspended,
-        getIdentifiedLocation(def),
+        getIdentifiedLocation(def.getPattern()),
         meta()
     );
   }
@@ -1985,6 +1985,8 @@ final class TreeToIr {
     long end;
     if (ast.getClose() != null) {
       end = ast.getClose().getEndCode();
+    } else if (ast.getDefault() != null) {
+      end = ast.getDefault().getEquals().getEndCode();
     } else if (ast.getClose2() != null) {
       end = ast.getClose2().getEndCode();
     } else if (ast.getType() != null) {
