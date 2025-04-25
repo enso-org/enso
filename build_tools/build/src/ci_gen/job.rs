40b8c3dd464163b45fd8a0c068687c9e2013b2de
Dmitry Bushev <bushevdv@gmail.com>
2025-04-25 21:59:07 +0100

    debug: stdlib check 1

--- build_tools/build/src/ci_gen/job.rs
+++ build_tools/build/src/ci_gen/job.rs
@@ -449,13 +449,14 @@ impl JobArchetype for StandardLibraryApiCheck {
 
                 let unpack_engine_distribution = Step {
                     run: Some(
-                        "echo 'Unpack Engine Distribution'
-rm -rf built-distribution
-echo 'built-distribution removed'
-tar -xvf built-distribution.tar
-echo 'built-distribution unpacked'
-rm built-distribution.tar
-echo 'built-distribution.tar removed'"
+                        "echo 'Unpack Engine Distribution';
+rm -rf built-distribution;
+echo 'built-distribution removed';
+tar -xvf built-distribution.tar;
+echo 'built-distribution unpacked';
+rm built-distribution.tar;
+echo 'built-distribution.tar removed';
+"
                             .into(),
                     ),
                     shell: Some(Shell::Bash),
