6b4d0dade1f038852c1619f76021c795ffd38bb5
Dmitry Bushev <bushevdv@gmail.com>
2025-04-26 21:21:30 +0100

    update: verify generated packages

--- build_tools/build/src/ci_gen/job.rs
+++ build_tools/build/src/ci_gen/job.rs
@@ -992,49 +992,70 @@ pub struct BuildEngineDistribution {
 
 impl JobArchetype for BuildEngineDistribution {
     fn job(&self, target: Target) -> Job {
-        let args_separator = if target.0 == OS::Windows { "'--'" } else { "--" };
         let engine_launcher = self.engine_launcher;
         let job_name =
             format!("Build Engine Distribution ({}) ({})", self.graal_edition, engine_launcher);
-        let mut job =
-            RunStepsBuilder::new(format!("backend sbt {} buildEngineDistribution", args_separator))
-                .customize(move |step| {
-                    let cleanup_built_distribution = Step {
-                        name: Some("Cleanup built-distribution".into()),
-                        run: Some("rm -rf built-distribution".into()),
-                        shell: Some(Shell::Bash),
-                        ..Default::default()
-                    };
+        let mut job = RunStepsBuilder::new("backend sbt '--' buildEngineDistribution")
+            .customize(move |step| {
+                let cleanup_built_distribution = Step {
+                    name: Some("Cleanup built-distribution".into()),
+                    run: Some("rm -rf built-distribution".into()),
+                    shell: Some(Shell::Bash),
+                    ..Default::default()
+                };
 
-                    let archive_engine_distribution = Step {
-                        name: Some("Archive Engine Distribution".into()),
-                        run: Some("tar -cvf built-distribution.tar built-distribution".into()),
-                        ..Default::default()
-                    };
+                let check_engine_distribution = Step {
+                    name: Some("Prepare Check Engine Distribution".into()),
+                    id: Some("check-engine-distribution".into()),
+                    run: Some(r#"third_party=$(find $(pwd)/built-distribution/ -type d -name 'THIRD-PARTY')
 
-                    let upload_engine_distribution =
-                        step::upload_artifact("Upload Engine Distribution")
-                            .with_custom_argument(
-                                "name",
-                                format!("engine-distribution-{}-{}", target.0, engine_launcher),
-                            )
-                            .with_custom_argument("path", "built-distribution.tar");
+tasks=''
+for path in $third_party; do
+  libname=$(basename $(dirname $(dirname $path)))
+  if [[ $libname == *"engine"* ]]; then
+    libname='engine'
+  fi
+  task="enso/verifyGeneratedPackage $libname $path"
+  echo $task
+  tasks="$tasks $task;"
+done
 
-                    let cleanup_archive = Step {
-                        name: Some("Cleanup".into()),
-                        run: Some("rm built-distribution.tar".into()),
-                        ..Default::default()
-                    };
+./run backend sbt '--' "$tasks"
+"#.into()),
+                    shell: Some(Shell::Bash),
+                    ..Default::default()
+                };
 
-                    vec![
-                        cleanup_built_distribution,
-                        step,
-                        archive_engine_distribution,
-                        upload_engine_distribution,
-                        cleanup_archive,
-                    ]
-                })
-                .build_job(job_name, target);
+                let archive_engine_distribution = Step {
+                    name: Some("Archive Engine Distribution".into()),
+                    run: Some("tar -cvf built-distribution.tar built-distribution".into()),
+                    ..Default::default()
+                };
+
+                let upload_engine_distribution =
+                    step::upload_artifact("Upload Engine Distribution")
+                        .with_custom_argument(
+                            "name",
+                            format!("engine-distribution-{}-{}", target.0, engine_launcher),
+                        )
+                        .with_custom_argument("path", "built-distribution.tar");
+
+                let cleanup_archive = Step {
+                    name: Some("Cleanup".into()),
+                    run: Some("rm built-distribution.tar".into()),
+                    ..Default::default()
+                };
+
+                vec![
+                    cleanup_built_distribution,
+                    step,
+                    check_engine_distribution,
+                    archive_engine_distribution,
+                    upload_engine_distribution,
+                    cleanup_archive,
+                ]
+            })
+            .build_job(job_name, target);
         job.env(engine_env::ENSO_LAUNCHER, self.engine_launcher);
         match self.graal_edition {
             graalvm::Edition::Community =>
