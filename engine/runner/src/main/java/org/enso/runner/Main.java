9ae6b3edeef374f3e70b4087151e8c7fd098054f
Radosław Waśko <radoslaw.wasko@enso.org>
2025-04-28 12:25:24 +0200

    Revert "introduce 'type coverage' tool"
    This reverts commit ea302aa2a03bb7335940662dc443f76ac7918758.

--- engine/runner/src/main/java/org/enso/runner/Main.java
+++ engine/runner/src/main/java/org/enso/runner/Main.java
@@ -865,7 +865,6 @@ public class Main {
    */
   private void generateDocsFrom(
       String docsFormat, String path, Level logLevel, boolean logMasking, boolean enableIrCaches) {
-    boolean enableStaticAnalysis = docsFormat.equals("type-coverage");
     var executionContext =
         new PolyglotContext(
             ContextFactory.create()
@@ -875,7 +874,6 @@ public class Main {
                 .logLevel(logLevel)
                 .logMasking(logMasking)
                 .enableIrCaches(enableIrCaches)
-                .enableStaticAnalysis(enableStaticAnalysis)
                 .build());
 
     var file = new File(path);
