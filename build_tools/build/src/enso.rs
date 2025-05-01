60d80eafac2fd34f0e87e7341161b96577d09b4f
Dmitry Bushev <bushevdv@gmail.com>
2025-05-01 08:56:11 +0100

    update: enso data directory

--- build_tools/build/src/enso.rs
+++ build_tools/build/src/enso.rs
@@ -3,7 +3,6 @@ use crate::prelude::*;
 use crate::cloud_tests;
 use crate::engine::StandardLibraryTestsSelection;
 use crate::paths::Paths;
-use crate::paths::ENSO_DATA_DIRECTORY;
 use crate::paths::ENSO_ENABLE_ASSERTIONS;
 use crate::paths::ENSO_META_TEST_ARGS;
 use crate::paths::ENSO_META_TEST_COMMAND;
@@ -270,12 +269,13 @@ impl BuiltEnso {
                 "1".to_string(),
             ));
         };
-        if extra_runner_args.is_none() {
-            let engine_package =
-                self.paths.repo_root.built_distribution.enso_engine_triple.engine_package.as_str();
-            environment_overrides
-                .push((ENSO_DATA_DIRECTORY.name().to_string(), engine_package.to_string()));
-        }
+        /*         if extra_runner_args.is_none() {
+                   let engine_package =
+                       self.paths.repo_root.built_distribution.enso_engine_triple.engine_package.as_str();
+                   environment_overrides
+                       .push((ENSO_DATA_DIRECTORY.name().to_string(), engine_package.to_string()));
+               }
+        */
         let graal_path = ide_ci::cache::goodie::graalvm::locate_graal()?;
         environment_overrides
             .push((JAVA_HOME.name().to_string(), graal_path.to_string_lossy().into_owned()));
