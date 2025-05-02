6877e2857da9e1b79dc7ae6a85e4ac4e07319217
Dmitry Bushev <bushevdv@gmail.com>
2025-05-02 19:43:39 +0100

    update: run self tests

--- build_tools/build/src/engine/context.rs
+++ build_tools/build/src/engine/context.rs
@@ -277,6 +277,7 @@ impl RunContext {
             ide_ci::future::perhaps(self.config.test_java_generated_from_rust, || {
                 crate::rust::parser::run_self_tests(&self.paths.repo_root)
             });
+        perhaps_test_java_generated_from_rust_job.await.transpose()?;
 
         // If we have much memory, we can try building everything in a single batch. Reducing number
         // of SBT invocations significantly helps build time. However, it is more memory heavy, so
@@ -368,8 +369,6 @@ impl RunContext {
             }
             None => {}
         }
-
-        perhaps_test_java_generated_from_rust_job.await.transpose()?;
 
         // === Stdlib API check ===
         if self.config.stdlib_api_check {
