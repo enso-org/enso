73d1d2c32f65c272db0a0b02a00455e854eed62c
Sergei Garin <sergei.garin@enso.org>
2025-05-05 10:42:44 +0300

    Implement more memory-efficient LRU

--- app/gui/src/dashboard/utilities/LruCache.ts
+++ app/gui/src/dashboard/utilities/LruCache.ts
@@ -1,42 +1,52 @@
 /** @file A simple LRU cache. */
 /**
  * A simple LRU cache.
+ *
+ * Implementation based on https://github.com/dominictarr/hashlru#algorithm
  */
 export class LRUCache<K, V> {
-  private readonly cache: Map<K, V>
-  private readonly maxSize: number
+  private oldCache: Map<K, V>
+  private cache: Map<K, V>
 
   /**
    * Create a new LRU cache.
    */
-  constructor(maxSize: number) {
+  constructor(private readonly maxSize: number) {
     this.cache = new Map()
-    this.maxSize = maxSize
+    this.oldCache = new Map()
   }
 
   /**
    * Get a value from the cache.
    */
   get(key: K): V | undefined {
-    const value = this.cache.get(key)
-    if (value != null) {
-      this.cache.delete(key)
-      this.cache.set(key, value)
+    const newCacheValue = this.cache.get(key)
+
+    if (newCacheValue != null) {
+      return newCacheValue
     }
-    return value
+
+    const oldCacheValue = this.oldCache.get(key)
+
+    if (oldCacheValue != null) {
+      this.cache.set(key, oldCacheValue)
+      this.evictIfNecessary()
+    }
+
+    return oldCacheValue
   }
 
   /**
    * Set a value in the cache.
    */
   set(key: K, value: V) {
-    if (this.cache.size >= this.maxSize) {
-      const oldestKey = this.cache.keys().next().value
-      if (oldestKey != null) {
-        this.cache.delete(oldestKey)
-      }
-    }
+    const isValueInNewCache = this.cache.has(key)
+
     this.cache.set(key, value)
+
+    if (isValueInNewCache) {
+      this.evictIfNecessary()
+    }
   }
 
   /**
@@ -44,5 +54,16 @@ export class LRUCache<K, V> {
    */
   clear() {
     this.cache.clear()
+    this.oldCache.clear()
+  }
+
+  /**
+   * Evict the oldest value from the cache.
+   */
+  private evictIfNecessary() {
+    if (this.cache.size > this.maxSize) {
+      this.oldCache = this.cache
+      this.cache = new Map()
+    }
   }
 }
