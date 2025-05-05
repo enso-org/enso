822aab7feb317f51aca1c18ecf0ed51fbd9aeae9
Martha King <martha.king@enso.org>
2025-05-05 17:12:35 +0100

    original set up

---
+++ app/gui/src/project-view/components/visualizations/TableVisualization/tableVizFilterSetUpUtils.ts
@@ -0,0 +1,93 @@
+import { SetFilterValuesFuncParams } from 'ag-grid-community'
+import { ValueType } from '../TableVisualization.vue'
+import { isNumericType } from './tableVizUtils'
+
+export const getFilterParams = (
+  isSSRM: boolean,
+  valueType: ValueType | null | undefined,
+  filterType: string | null,
+  getFilterValues: (params: SetFilterValuesFuncParams<any, string>) => Promise<void>,
+) => {
+  const filterOptions = valueType ? getFilterOptions(valueType.constructor) : null
+  const filterButtons = valueType ? getFilterButtons(valueType.constructor) : null
+
+  return filterType != 'agMultiColumnFilter' ?
+      {
+        maxNumConditions: 1,
+        values: isSSRM ? getFilterValues : null,
+        filterOptions: filterOptions,
+        buttons: filterButtons,
+      }
+    : {
+        filters: [
+          {
+            filter: 'agTextColumnFilter',
+            filterParams: {
+              buttons: filterButtons,
+            },
+          },
+          {
+            filter: 'agSetColumnFilter',
+            filterParams: {
+              values: isSSRM ? getFilterValues : null,
+              buttons: filterButtons,
+            },
+          },
+        ],
+      }
+}
+
+export const getFilterType = (valueType: string, usingMultiFilter: boolean) => {
+  if (valueType === 'Date') {
+    return 'agDateColumnFilter'
+  } else if (isNumericType(valueType)) {
+    return 'agNumberColumnFilter'
+  } else if (valueType === 'Char') {
+    return usingMultiFilter ? 'agMultiColumnFilter' : 'agTextColumnFilter'
+  } else {
+    return 'agSetColumnFilter'
+  }
+}
+
+export const getCellDataType = (valueType: string) => {
+  if (valueType === 'Date') {
+    return 'date'
+  } else if (isNumericType(valueType)) {
+    return 'number'
+  } else if (valueType === 'Char') {
+    return 'text'
+  } else if (valueType === 'Boolean') {
+    return 'boolean'
+  } else {
+    return false
+  }
+}
+
+function getFilterOptions(valueType: string) {
+  if (valueType === 'Date') {
+    return ['equals', 'notEqual', 'greaterThan', 'lessThan', 'inRange', 'blank', 'notBlank']
+  } else if (isNumericType(valueType)) {
+    return [
+      'equals',
+      'notEqual',
+      'greaterThan',
+      'greaterThanOrEqual',
+      'lessThan',
+      'lessThanOrEqual',
+      'inRange',
+      'blank',
+      'notBlank',
+    ]
+  } else if (valueType === 'Char') {
+    return ['equals', 'notEqual', 'contains', 'startsWith', 'endsWith', 'blank', 'notBlank']
+  } else {
+    return null
+  }
+}
+function getFilterButtons(valueType: string) {
+  if (valueType === 'Date') {
+    return ['apply', 'clear']
+  } else {
+    return ['clear']
+  }
+}
