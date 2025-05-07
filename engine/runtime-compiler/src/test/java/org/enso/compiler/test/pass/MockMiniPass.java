c972fdba24ae122203b15c928a312187fb748c6c
Pavel Marek <pavel.marek@enso.org>
2025-05-07 19:46:13 +0200

    Test traversal over Case.Expr

--- engine/runtime-compiler/src/test/java/org/enso/compiler/test/pass/MockMiniPass.java
+++ engine/runtime-compiler/src/test/java/org/enso/compiler/test/pass/MockMiniPass.java
@@ -3,12 +3,15 @@ package org.enso.compiler.test.pass;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.enso.compiler.core.IR;
 import org.enso.compiler.core.ir.Expression;
 import org.enso.compiler.pass.MiniIRPass;
 
 final class MockMiniPass extends MiniIRPass {
   private final MockExpression stopExpr;
+  private final List<Expression> transformedExpressions = new ArrayList<>();
 
   /**
    * @param stopExpr When encountered this expression, {@code prepare} method will return null to
@@ -35,6 +38,7 @@ final class MockMiniPass extends MiniIRPass {
           "Transform is called just once by one pass", mockExpr.isTransformedBy(this), is(false));
       mockExpr.setTransformedByPass(this);
     }
+    transformedExpressions.add(expr);
     return expr;
   }
 
@@ -48,6 +52,14 @@ final class MockMiniPass extends MiniIRPass {
       mockExpr.setPreparedBy(this);
     }
     return this;
+  }
+
+  /**
+   * Returns list of all the expressions that were transformed in {@link
+   * #transformExpression(Expression)} method.
+   */
+  public List<Expression> getTransformedExpressions() {
+    return transformedExpressions;
   }
 
   static final class Builder {
