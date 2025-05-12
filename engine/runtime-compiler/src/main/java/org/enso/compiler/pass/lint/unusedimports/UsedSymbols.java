66937b46c171005d671018ecc0de2d89805bbf81
Pavel Marek <pavel.marek@enso.org>
2025-05-12 20:15:25 +0200

    UsedSymbolsBuilder expect symbol to be directly contained in imports.

--- engine/runtime-compiler/src/main/java/org/enso/compiler/pass/lint/unusedimports/UsedSymbols.java
+++ engine/runtime-compiler/src/main/java/org/enso/compiler/pass/lint/unusedimports/UsedSymbols.java
@@ -1,5 +1,7 @@
 package org.enso.compiler.pass.lint.unusedimports;
 
+import static org.enso.scala.wrapper.ScalaConversions.asJava;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -46,9 +48,39 @@ final class UsedSymbols {
 
   static final class Builder {
 
-    private final Map<Import, Set<QualifiedName>> symbols = new HashMap<>();
+    private final Map<Import.Module, Set<QualifiedName>> symbols = new HashMap<>();
 
-    void addUsedSymbol(Import importIr, QualifiedName symbol) {
+    /**
+     * Records the {@code symbol} as used by the {@code importIr}.
+     *
+     * <p>The symbol must be directly cotained in the import, for example symbol {@code
+     * local.Proj.Module.T.Cons} is not contained in {@code from local.Proj.Module import T}, but it
+     * is contained in {@code from local.Proj.Module.T import Cons}. If this is not true, {@link
+     * AssertionError} is thrown.
+     */
+    void addUsedSymbol(Import.Module importIr, QualifiedName symbol) {
+      if (importIr.onlyNames().isDefined()) {
+        var onlyNames = importIr.onlyNames().get();
+        var baseName = QualifiedName.fromString(importIr.name().name());
+        var someNameMatches = false;
+        for (var onlyName : asJava(onlyNames)) {
+          var fqn = baseName.createChild(onlyName.name());
+          if (fqn.equals(symbol)) {
+            someNameMatches = true;
+          }
+        }
+        if (!someNameMatches) {
+          throw new AssertionError(
+              "Attempting to add symbol '"
+                  + symbol
+                  + "' to import '"
+                  + importIr.showCode()
+                  + "'. But the symbol is not in the list of only names.");
+        }
+      } else {
+        var fqn = QualifiedName.fromString(importIr.name().name());
+        assert fqn.equals(symbol);
+      }
       var usedSymbols = symbols.computeIfAbsent(importIr, k -> new HashSet<>());
       usedSymbols.add(symbol);
     }
