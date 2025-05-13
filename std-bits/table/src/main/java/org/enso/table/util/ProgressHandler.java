f84ab7d48d6af39b6ef14a47d3cece459ee1a985
James Dunkerley <jdunkerley@users.noreply.github.com>
2025-05-13 17:36:29 +0100

    Fix ProgressHandler.

--- std-bits/table/src/main/java/org/enso/table/util/ProgressHandler.java
+++ std-bits/table/src/main/java/org/enso/table/util/ProgressHandler.java
@@ -8,6 +8,7 @@ public final class ProgressHandler implements AutoCloseable {
   private static final Logger log = LoggerFactory.getLogger("Standard.Base.Logging.Progress");
   private static final long PROGRESS_STEP = 50000;
 
+  private final Object handle;
   private final String name;
   private final long count;
   private final Context context;
@@ -15,6 +16,13 @@ public final class ProgressHandler implements AutoCloseable {
 
   private ProgressHandler(String name, long count) {
     this.name = name;
+    this.handle =
+        new Object() {
+          @Override
+          public String toString() {
+            return name;
+          }
+        };
     this.count = count;
     this.context = Context.getCurrent();
     this.step = PROGRESS_STEP;
@@ -29,19 +37,19 @@ public final class ProgressHandler implements AutoCloseable {
     step--;
     if (step == 0) {
       context.safepoint();
-      log.trace("ADVANCE {}+{}", this, PROGRESS_STEP);
+      log.trace("ADVANCE {}+{}", handle, PROGRESS_STEP);
       step = PROGRESS_STEP;
     }
   }
 
   @Override
   public void close() {
-    log.trace("ADVANCE {}+{}", this, count);
+    log.trace("ADVANCE {}+{}", handle, count);
   }
 
   public static ProgressHandler init(String name, long count) {
     var result = new ProgressHandler(name, count);
-    log.trace("INIT {}:{}@{}", name, result, count);
+    log.trace("INIT {}:{}@{}", result.handle, "Process started", count);
     return result;
   }
 }
