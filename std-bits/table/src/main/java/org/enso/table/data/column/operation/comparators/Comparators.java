9a999a0e08cf562f7c1094620b9cd590efce3d95
James Dunkerley <jdunkerley@users.noreply.github.com>
2025-05-14 09:12:26 +0100

    Add the BooleanComparators.

--- std-bits/table/src/main/java/org/enso/table/data/column/operation/comparators/Comparators.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/operation/comparators/Comparators.java
@@ -1,11 +1,7 @@
 package org.enso.table.data.column.operation.comparators;
 
 import org.enso.table.data.column.operation.BinaryOperation;
-import org.enso.table.data.column.storage.type.DateTimeType;
-import org.enso.table.data.column.storage.type.DateType;
-import org.enso.table.data.column.storage.type.NullType;
-import org.enso.table.data.column.storage.type.TextType;
-import org.enso.table.data.column.storage.type.TimeOfDayType;
+import org.enso.table.data.column.storage.type.*;
 import org.enso.table.data.table.Column;
 
 /**
@@ -24,7 +20,7 @@ public interface Comparators extends BinaryOperation<Boolean> {
         || storageType instanceof NullType;
   }
 
-  static BinaryOperation eq(Column left) {
+  static BinaryOperation<Boolean> eq(Column left) {
     var leftStorage = BinaryOperation.getInferredStorage(left);
     return switch (leftStorage.getType()) {
       case NullType nt -> NullComparators.INSTANCE;
@@ -32,11 +28,12 @@ public interface Comparators extends BinaryOperation<Boolean> {
       case DateTimeType dt -> DateTimeComparators.EQ;
       case TimeOfDayType tm -> TimeOfDayComparators.EQ;
       case TextType tt -> StringComparators.EQ;
+      case BooleanType bt -> BooleanComparators.EQ;
       default -> throw new IllegalArgumentException("Unsupported StorageType");
     };
   }
 
-  static BinaryOperation notEq(Column left) {
+  static BinaryOperation<Boolean> notEq(Column left) {
     var leftStorage = BinaryOperation.getInferredStorage(left);
     return switch (leftStorage.getType()) {
       case NullType nt -> NullComparators.INSTANCE;
@@ -44,11 +41,12 @@ public interface Comparators extends BinaryOperation<Boolean> {
       case DateTimeType dt -> DateTimeComparators.NEQ;
       case TimeOfDayType tm -> TimeOfDayComparators.NEQ;
       case TextType tt -> StringComparators.NEQ;
+      case BooleanType bt -> BooleanComparators.NEQ;
       default -> throw new IllegalArgumentException("Unsupported StorageType");
     };
   }
 
-  static BinaryOperation lessThan(Column left) {
+  static BinaryOperation<Boolean> lessThan(Column left) {
     var leftStorage = BinaryOperation.getInferredStorage(left);
     return switch (leftStorage.getType()) {
       case NullType nt -> NullComparators.INSTANCE;
@@ -56,11 +54,12 @@ public interface Comparators extends BinaryOperation<Boolean> {
       case DateTimeType dt -> DateTimeComparators.LT;
       case TimeOfDayType tm -> TimeOfDayComparators.LT;
       case TextType tt -> StringComparators.LT;
+      case BooleanType bt -> BooleanComparators.LT;
       default -> throw new IllegalArgumentException("Unsupported StorageType");
     };
   }
 
-  static BinaryOperation lessThanEq(Column left) {
+  static BinaryOperation<Boolean> lessThanEq(Column left) {
     var leftStorage = BinaryOperation.getInferredStorage(left);
     return switch (leftStorage.getType()) {
       case NullType nt -> NullComparators.INSTANCE;
@@ -68,11 +67,12 @@ public interface Comparators extends BinaryOperation<Boolean> {
       case DateTimeType dt -> DateTimeComparators.LTE;
       case TimeOfDayType tm -> TimeOfDayComparators.LTE;
       case TextType tt -> StringComparators.LTE;
+      case BooleanType bt -> BooleanComparators.LTE;
       default -> throw new IllegalArgumentException("Unsupported StorageType");
     };
   }
 
-  static BinaryOperation greaterThan(Column left) {
+  static BinaryOperation<Boolean> greaterThan(Column left) {
     var leftStorage = BinaryOperation.getInferredStorage(left);
     return switch (leftStorage.getType()) {
       case NullType nt -> NullComparators.INSTANCE;
@@ -80,6 +80,7 @@ public interface Comparators extends BinaryOperation<Boolean> {
       case DateTimeType dt -> DateTimeComparators.GT;
       case TimeOfDayType tm -> TimeOfDayComparators.GT;
       case TextType tt -> StringComparators.GT;
+      case BooleanType bt -> BooleanComparators.GT;
       default -> throw new IllegalArgumentException("Unsupported StorageType");
     };
   }
@@ -92,6 +93,7 @@ public interface Comparators extends BinaryOperation<Boolean> {
       case DateTimeType dt -> DateTimeComparators.GTE;
       case TimeOfDayType tm -> TimeOfDayComparators.GTE;
       case TextType tt -> StringComparators.GTE;
+      case BooleanType bt -> BooleanComparators.GTE;
       default -> throw new IllegalArgumentException("Unsupported StorageType");
     };
   }
--- std-bits/table/src/main/java/org/enso/table/data/column/storage/type/BooleanType.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/storage/type/BooleanType.java
@@ -1,7 +1,7 @@
 package org.enso.table.data.column.storage.type;
 
 import org.enso.table.data.column.builder.Builder;
-import org.enso.table.data.column.builder.BuilderForType;
+import org.enso.table.data.column.builder.BuilderForBoolean;
 import org.enso.table.data.column.storage.ColumnBooleanStorage;
 import org.enso.table.data.column.storage.ColumnStorage;
 import org.enso.table.problems.ProblemAggregator;
@@ -35,7 +35,7 @@ public record BooleanType() implements StorageType<Boolean> {
   }
 
   @Override
-  public BuilderForType<Boolean> makeBuilder(
+  public BuilderForBoolean makeBuilder(
       long initialCapacity, ProblemAggregator problemAggregator) {
     return Builder.getForBoolean(initialCapacity);
   }
