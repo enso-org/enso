35051ee8e9ce2ec4f6ce1bb22e65ddb768a4e0e1
somebody1234 <ehern.lee@gmail.com>
2025-05-20 17:12:15 +1000

    [dashboard] Address CR

--- app/gui/src/dashboard/typings.d.ts
+++ app/gui/src/dashboard/typings.d.ts
@@ -3,14 +3,14 @@ import type { ReactNode, Ref, RefAttributes } from 'react'
 
 declare module 'react' {
   function forwardRef<T, P = object>(
-    render: (props: P, ref: Ref<T>) => ReactNode | null,
-  ): (props: P & RefAttributes<T>) => ReactNode | null
+    render: (props: P, ref: Ref<T>) => ReactNode,
+  ): (props: P & RefAttributes<T>) => ReactNode
 
   // Technically incorrect, as internally it is a `MemoExoticComponent`,
   // however in practice it should be indistinguishable from the original component.
   function memo<P = object>(
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    Component: (props: P) => ReactNode | null,
+    Component: (props: P) => ReactNode,
     propsAreEqual?: (prevProps: Readonly<P>, nextProps: Readonly<P>) => boolean,
-  ): (props: P) => ReactNode | null
+  ): (props: P) => ReactNode
 }
