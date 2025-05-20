e6ea3dc4798b87fe5e5dc8deeb9e19e89fc1d00e
Adam Obuchowicz <adam.obuchowicz@enso.org>
2025-05-20 13:19:58 +0200

    Fix import cyclic dependency

--- app/gui/src/dashboard/components/AriaComponents/Text/Text.tsx
+++ app/gui/src/dashboard/components/AriaComponents/Text/Text.tsx
@@ -8,6 +8,7 @@ import * as React from 'react'
 import { memo } from 'react'
 import type { TestIdProps } from '../types'
 import * as visualTooltip from '../VisualTooltip'
+import { TEXT_STYLE } from './style'
 import * as textProvider from './TextProvider'
 import { TEXT_STYLE } from './variant'
 
--- app/gui/src/dashboard/components/AriaComponents/Text/index.ts
+++ app/gui/src/dashboard/components/AriaComponents/Text/index.ts
@@ -3,5 +3,6 @@
  *
  * Barrel import file for Text component.
  */
+export * from './style'
 export * from './Text'
 export * from './variant'
