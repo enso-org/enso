0cb3516e1351ec3a101a217a9001994560a7fdb2
Adam Riley <riley.ad@gmail.com>
2025-05-21 11:46:28 +0100

    Remove illegal blank lines

--- app/gui/src/project-view/components/visualizations/TableVisualization.vue
+++ app/gui/src/project-view/components/visualizations/TableVisualization.vue
@@ -743,10 +743,8 @@ function getAstPattern(params: CellDoubleClickedEvent, action: string, defaultSe
  *
  * The action string should be of the format `at {{#fieldname}}` which will generate a Node `at 2`
  * or `at {{@fieldname}}` which will generate a Node `at "2"`
- *
  * If the action contains no placeholders then the defaultSelector is used like so
  * `action {{@defaultSelector}}`
- *
  * @param params - The grid cell event containing the clicked row's data.
  * @param defaultSelector - A fallback key used when the template contains no placeholders.
  * @param action - A template string with placeholders (e.g., `at {{@name}}`, `at {{#value}}`) used to generate the AST.
