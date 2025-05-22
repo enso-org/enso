2ab892ef9b9ee872f456c560a925dec466bcb350
Gregory Travis <greg.travis@enso.org>
2025-05-22 10:44:18 -0400

    just use int

--- std-bits/base/src/main/java/org/enso/base/encoding/SimpleDecodingProblemAggregator.java
+++ std-bits/base/src/main/java/org/enso/base/encoding/SimpleDecodingProblemAggregator.java
@@ -1,16 +1,9 @@
 package org.enso.base.encoding;
 
 public class SimpleDecodingProblemAggregator
-    extends DecodingProblemAggregator<SimpleDecodingProblemAggregator.Location> {
+    extends DecodingProblemAggregator<Integer> {
   @Override
-  protected Location toLocation(int position) {
-    return new Location(position);
-  }
-
-  public record Location(int position) {
-    @Override
-    public String toString() {
-      return Integer.toString(position);
-    }
+  protected Integer toLocation(int position) {
+    return position;
   }
 }
