3d384ef6c46837d36c820df54bd73aff67d9ca45
Gregory Travis <greg.travis@enso.org>
2025-05-22 12:26:07 -0400

    fmt

--- std-bits/base/src/main/java/org/enso/base/encoding/SimpleDecodingProblemAggregator.java
+++ std-bits/base/src/main/java/org/enso/base/encoding/SimpleDecodingProblemAggregator.java
@@ -1,7 +1,6 @@
 package org.enso.base.encoding;
 
-public class SimpleDecodingProblemAggregator
-    extends DecodingProblemAggregator<Integer> {
+public class SimpleDecodingProblemAggregator extends DecodingProblemAggregator<Integer> {
   @Override
   protected Integer toLocation(int position) {
     return position;
