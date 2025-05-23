9fb89718795a2004d00cd00f0546e1f3e8178519
Adam Obuchowicz <adam.obuchowicz@enso.org>
2025-05-23 15:40:14 +0200

    Remove debug logs

--- app/gui/src/project-view/stores/graph/index.ts
+++ app/gui/src/project-view/stores/graph/index.ts
@@ -674,7 +674,6 @@ export function createGraphStore(
   const viewModule = computed((): Ast.Module => syncModule.value!)
 
   // expose testing hook
-  console.log('Overriding mockExpressionUpdate')
   ;(window as any)._mockExpressionUpdate = mockExpressionUpdate
 
   function mockExpressionUpdate(
@@ -712,7 +711,6 @@ export function createGraphStore(
       hiddenType: update.hiddenType ?? [],
       ...(update.methodCall ? { methodCall: update.methodCall } : {}),
     }
-    console.log('Processing update')
     proj.computedValueRegistry.processUpdates([update_])
   }
 
