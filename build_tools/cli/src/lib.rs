39dcc8a8b6b51364871ae8e73e52d14444a1caeb
Pavel Marek <pavel.marek@enso.org>
2025-05-26 16:34:38 +0200

    Ensure --jvm arg always receives small JDK

--- build_tools/cli/src/lib.rs
+++ build_tools/cli/src/lib.rs
@@ -411,11 +411,18 @@ impl Processor {
                             config.check_enso_benchmarks = TARGET_OS == OS::Linux;
                         }
                         Tests::StandardLibrary => {
+                            config.build_small_jdk = true;
+                            let small_jdk_dir =
+                                self.context.repo_root.target.small_jdk.path.clone();
+                            config.small_jdk_dir = Some(small_jdk_dir.clone());
                             config.test_standard_library =
                                 Some(StandardLibraryTestsSelection::blacklist(vec![
                                     "Examples_Tests".to_string(),
                                 ]));
                             config.add_engine_runner_arg("--jvm");
+                            config.add_engine_runner_arg(
+                                small_jdk_dir.to_string_lossy().to_string().as_str(),
+                            );
                             config.use_native_runner = true;
                         }
                         Tests::StandardLibraryInNative => {
