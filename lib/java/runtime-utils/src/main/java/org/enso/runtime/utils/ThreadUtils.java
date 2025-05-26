18dcdea97907fa2c118405452511ac4852e5113b
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2025-05-26 18:10:16 +0200

    fmt

--- lib/java/runtime-utils/src/main/java/org/enso/runtime/utils/ThreadUtils.java
+++ lib/java/runtime-utils/src/main/java/org/enso/runtime/utils/ThreadUtils.java
@@ -4,27 +4,30 @@ import java.util.Arrays;
 
 public class ThreadUtils {
 
-    private ThreadUtils() {}
+  private ThreadUtils() {}
 
-    public static String dumpAllStacktraces(String prefix) {
-        var sb = new StringBuilder(prefix);
-        sb.append(System.lineSeparator());
-        Thread.getAllStackTraces().entrySet().forEach(entry -> {
-            sb.append(entry.getKey().getName()).append(System.lineSeparator());
-            Arrays.stream(entry.getValue()).forEach(e ->
-                sb.append("    ")
-                        .append(e.getClassName())
-                        .append(".")
-                        .append(e.getMethodName())
-                        .append("(")
-                        .append(e.getFileName())
-                        .append(":")
-                        .append(e.getLineNumber())
-                        .append(")")
-                        .append(System.lineSeparator())
-            );
-        });
-        return sb.toString();
-    }
-
+  public static String dumpAllStacktraces(String prefix) {
+    var sb = new StringBuilder(prefix);
+    sb.append(System.lineSeparator());
+    Thread.getAllStackTraces()
+        .entrySet()
+        .forEach(
+            entry -> {
+              sb.append(entry.getKey().getName()).append(System.lineSeparator());
+              Arrays.stream(entry.getValue())
+                  .forEach(
+                      e ->
+                          sb.append("    ")
+                              .append(e.getClassName())
+                              .append(".")
+                              .append(e.getMethodName())
+                              .append("(")
+                              .append(e.getFileName())
+                              .append(":")
+                              .append(e.getLineNumber())
+                              .append(")")
+                              .append(System.lineSeparator()));
+            });
+    return sb.toString();
+  }
 }
