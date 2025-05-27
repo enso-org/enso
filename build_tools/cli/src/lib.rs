e2654f7d1a5da59da16b7e385b4dfdd2513753be
Pavel Marek <pavel.marek@enso.org>
2025-05-27 17:28:25 +0200

    Build small JDK in backend upload command

--- build_tools/cli/src/lib.rs
+++ build_tools/cli/src/lib.rs
@@ -354,10 +354,13 @@ impl Processor {
                             command: enso_build::engine::ReleaseCommand::Upload,
                         },
                     );
+                    let small_jdk_dir = self.context.repo_root.target.small_jdk.path.clone();
                     let config = enso_build::engine::BuildConfigurationFlags {
                         build_engine_package: true,
                         build_launcher_bundle: true,
                         build_project_manager_bundle: true,
+                        build_small_jdk: true,
+                        small_jdk_dir: Some(small_jdk_dir.clone()),
                         verify_packages: true,
                         ..default()
                     };
