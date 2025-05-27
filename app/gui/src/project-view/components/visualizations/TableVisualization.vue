6c19508507ce8f8fbea8a8a3797d90fe816bfe29
Martha King <martha.king@enso.org>
2025-05-27 12:06:41 +0100

    fix conflict

--- app/gui/src/project-view/components/visualizations/TableVisualization.vue
+++ app/gui/src/project-view/components/visualizations/TableVisualization.vue
@@ -39,7 +39,6 @@ import { TableVisualisationTooltip } from './TableVisualization/TableVisualisati
 import {
   Error,
   GenericGrid,
-  SingleColumnOfActions,
   isError,
   isGenericGrid,
   isSingleColumnOfActions,
