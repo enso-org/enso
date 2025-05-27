27c851bea897e69d4384ec2b06fc96b64c225064
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2025-05-27 10:40:10 +0200

    Bump one more time
    Previously it passed with 560. Now it needs one more MB. :shrug:

--- project/GraalVM.scala
+++ project/GraalVM.scala
@@ -103,7 +103,7 @@ object GraalVM {
     private val linuxX64Release   = NativeImageSize(200, 465)
     private val macX64Release     = NativeImageSize(200, 426)
     private val macARM64Release   = NativeImageSize(200, 423)
-    private val testNISize        = NativeImageSize(100, 560)
+    private val testNISize        = NativeImageSize(100, 562)
   }
 
   /** Has the user requested to use Espresso for Java interop? */
