7033120da4c8999fc5a8a24a380d30029c1a72a7
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-05-29 11:26:38 +0200

    No need for ydoc-server-nodejs project

--- app/ydoc-server-nodejs/src/main.ts
+++
@@ -1,8 +0,0 @@
-/** @file entrypoint for standalone ydoc-server running in nodejs environment. */
-
-import './cjs-shim' // must be imported first
-
-const DEFAULT_PORT = 5976
-const PORT = (process.env.PORT != null && parseInt(process.env.PORT, 10)) || DEFAULT_PORT
-const HOSTNAME = process.env.GUI_HOSTNAME ?? 'localhost'
-const LANGUAGE_SERVER_URL = process.env.LANGUAGE_SERVER_URL
