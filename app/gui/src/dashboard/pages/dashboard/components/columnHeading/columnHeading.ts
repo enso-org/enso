74f5a449deec3c2aaeff1db90139524efbfdecf2
somebody1234 <ehern.lee@gmail.com>
2025-05-31 00:09:27 +1000

    [dashboard] Avoid module name conflict for `column` and `columnHeading`

--- app/gui/src/dashboard/pages/dashboard/components/columnHeading/columnHeading.ts
+++ app/gui/src/dashboard/pages/dashboard/components/columnHeading/columnHeading.ts
@@ -1,14 +1,14 @@
 /** @file A lookup containing a component for the corresponding heading for each column type. */
 import { memo } from 'react'
-import type * as column from './column'
-import { Column } from './column/columnUtils'
-import AccessedByProjectsColumnHeading from './columnHeading/AccessedByProjectsColumnHeading'
-import AccessedDataColumnHeading from './columnHeading/AccessedDataColumnHeading'
-import LabelsColumnHeading from './columnHeading/LabelsColumnHeading'
-import ModifiedColumnHeading from './columnHeading/ModifiedColumnHeading'
-import NameColumnHeading from './columnHeading/NameColumnHeading'
-import PathColumnHeading from './columnHeading/PathColumnHeading'
-import SharedWithColumnHeading from './columnHeading/SharedWithColumnHeading'
+import type * as column from '../column'
+import { Column } from '../column/columnUtils'
+import AccessedByProjectsColumnHeading from './AccessedByProjectsColumnHeading'
+import AccessedDataColumnHeading from './AccessedDataColumnHeading'
+import LabelsColumnHeading from './LabelsColumnHeading'
+import ModifiedColumnHeading from './ModifiedColumnHeading'
+import NameColumnHeading from './NameColumnHeading'
+import PathColumnHeading from './PathColumnHeading'
+import SharedWithColumnHeading from './SharedWithColumnHeading'
 
 export const COLUMN_HEADING: Readonly<
   Record<
