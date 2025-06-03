e8e518ee76c3a9cc610cb0656c51e29a4018151a
James Dunkerley <jdunkerley@users.noreply.github.com>
2025-06-03 17:21:28 +0100

    A little tidying.

--- std-bits/table/src/main/java/org/enso/table/data/column/storage/Storage.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/storage/Storage.java
@@ -57,7 +57,6 @@ public abstract class Storage<T> implements ColumnStorage<T> {
     public static final String DIV = "/";
     public static final String MOD = "%";
     public static final String POWER = "^";
-    public static final String ROUND = "round";
     public static final String IS_IN = "is_in";
   }
 
