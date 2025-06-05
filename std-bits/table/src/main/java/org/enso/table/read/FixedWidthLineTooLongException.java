457ef4a09a9c0038cfb322b826016f4d9d8643b1
Gregory Travis <greg.travis@enso.org>
2025-06-05 14:44:51 -0400

    fixed too-long test

--- std-bits/table/src/main/java/org/enso/table/read/FixedWidthLineTooLongException.java
+++ std-bits/table/src/main/java/org/enso/table/read/FixedWidthLineTooLongException.java
@@ -2,8 +2,8 @@ package org.enso.table.read;
 
 /** An exception thrown when a problem occured when reading a fixed-width file. */
 public class FixedWidthLineTooLongException extends RuntimeException {
-  private final long sourceLineNumber;
-  private final long maxLineLength;
+  public final long sourceLineNumber;
+  public final long maxLineLength;
 
   public FixedWidthLineTooLongException(long sourceLineNumber, long maxLineLength) {
     this.sourceLineNumber = sourceLineNumber;
