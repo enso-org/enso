f0cff3dc9572d01534e0efd66a004340d9c16b1f
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-06-06 19:18:41 +0200

    Unit test using Channel functionality in a single JVM

---
+++ engine/runtime-language-epb/src/test/java/org/enso/interpreter/epb/ChannelInSingleJvmTest.java
@@ -0,0 +1,43 @@
+package org.enso.interpreter.epb;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.function.Function;
+import org.enso.jvm.channel.Channel;
+import org.enso.persist.Persistable;
+import org.junit.Test;
+
+public class ChannelInSingleJvmTest {
+  @Test
+  public void exchangeMessages() {
+    var ch = Channel.create(null, Persistables.class);
+
+    var msg = new Increment(10);
+
+    var newMsg = ch.execute(Increment.class, msg);
+
+    assertNotNull("Got a value", newMsg);
+    assertEquals("10 + 1", 11, newMsg.valueToIncrement());
+    assertEquals("Original value remains", 10, msg.valueToIncrement());
+  }
+
+  @Persistable(id = 8341)
+  static final class Increment implements Function<Channel, Increment> {
+    int valueToIncrement;
+
+    Increment(int valueToIncrement) {
+      this.valueToIncrement = valueToIncrement;
+    }
+
+    int valueToIncrement() {
+      return valueToIncrement;
+    }
+
+    @Override
+    public Increment apply(Channel t) {
+      valueToIncrement++;
+      return this;
+    }
+  }
+}
--- lib/java/persistance-dsl/src/main/java/org/enso/persist/impl/PersistableProcessor.java
+++ lib/java/persistance-dsl/src/main/java/org/enso/persist/impl/PersistableProcessor.java
@@ -92,9 +92,12 @@ public class PersistableProcessor extends AbstractProcessor {
 
             w.append("package " + entry.getKey() + ";\n");
             w.append("import org.enso.persist.Persistance;\n");
-            w.append("public final class Persistables extends Persistance.Pool {\n");
+            w.append("import java.util.function.Supplier;\n");
+            w.append(
+                "public final class Persistables extends Persistance.Pool implements"
+                    + " Supplier<Persistance.Pool> {\n");
             w.append("  public static final Persistance.Pool POOL = new Persistables();\n");
-            w.append("  private Persistables() {\n");
+            w.append("  public Persistables() {\n");
             w.append("    super(\"").append(entry.getKey()).append("\",");
             var lineEnding = "\n";
             for (var idName : props.entrySet()) {
@@ -103,6 +106,9 @@ public class PersistableProcessor extends AbstractProcessor {
               lineEnding = ",\n";
             }
             w.append("\n    );\n");
+            w.append("  }\n");
+            w.append("  public Persistance.Pool get() {\n");
+            w.append("    return this;\n");
             w.append("  }\n");
             w.append("}\n");
           }
