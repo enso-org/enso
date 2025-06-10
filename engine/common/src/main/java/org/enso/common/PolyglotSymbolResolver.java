cec892c34a4fb6b0feabaa30a038ea81f6a9a578
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-06-10 11:32:57 +0200

    Provide support for additional PolyglotSymbolResolver extensions

---
+++ engine/common/src/main/java/org/enso/common/PolyglotSymbolResolver.java
@@ -0,0 +1,49 @@
+package org.enso.common;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.ServiceLoader;
+
+/** Generic support for loading Java polyglot symbols. */
+public abstract class PolyglotSymbolResolver {
+  private static final Collection<PolyglotSymbolResolver> ALL;
+
+  static {
+    var arr = new ArrayList<PolyglotSymbolResolver>();
+    for (var l : ServiceLoader.load(PolyglotSymbolResolver.class)) {
+      arr.add(l);
+    }
+    ALL = Collections.unmodifiableList(arr);
+  }
+
+  /**
+   * Search all providers for given name.
+   *
+   * @param name dot separated name to search for
+   * @return non-null object representing the name
+   * @throws java.lang.ClassNotFoundException if no name was found
+   */
+  public static Object loadClass(String name) throws ClassNotFoundException {
+    var ex = new ClassNotFoundException();
+    for (var p : ALL) {
+      try {
+        var found = p.handleLoadClass(name);
+        assert found != null;
+        return found;
+      } catch (ClassNotFoundException cnfe) {
+        ex = cnfe;
+      }
+    }
+    throw ex;
+  }
+
+  /**
+   * Subclasses implement this method to seach for provided name
+   *
+   * @param name dot separated name to search for
+   * @return non-null object representing the name
+   * @throws java.lang.ClassNotFoundException if no name was found
+   */
+  protected abstract Object handleLoadClass(String name) throws ClassNotFoundException;
+}
