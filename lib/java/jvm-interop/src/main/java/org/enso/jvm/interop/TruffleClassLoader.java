31eff03733e5d29bcaa70b66e647365d93265197
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-06-10 16:46:16 +0200

    Using Context with HostAccess.ALL

--- lib/java/jvm-interop/src/main/java/org/enso/jvm/interop/TruffleClassLoader.java
+++ lib/java/jvm-interop/src/main/java/org/enso/jvm/interop/TruffleClassLoader.java
@@ -4,21 +4,36 @@ import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
-import org.graalvm.polyglot.Value;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.HostAccess;
 
 @ExportLibrary(value = InteropLibrary.class)
 final class TruffleClassLoader implements TruffleObject {
+  private static Context ctx;
 
   private Object value;
 
   private TruffleClassLoader() {}
 
+  private static synchronized Context ctx() {
+    if (ctx == null) {
+      ctx =
+          Context.newBuilder() // no dynamic languages needed
+              .allowHostAccess(HostAccess.ALL) // all public members
+              .build();
+    }
+    return ctx;
+  }
+
   static TruffleObject loadClass(String className) throws ClassNotFoundException {
-    java.lang.Class<?> clazz = Class.forName(className);
-    org.graalvm.polyglot.Value clazzValue = Value.asValue(clazz).getMember("static");
-    org.enso.jvm.interop.TruffleClassLoader holderRaw = new TruffleClassLoader();
-    org.graalvm.polyglot.Value holderValue = Value.asValue(holderRaw);
-    holderValue.putMember("any", clazzValue);
+    var context = ctx();
+
+    var clazz = Class.forName(className);
+    var clazzValue1 = context.asValue(clazz);
+    var clazzValue2 = clazzValue1.getMember("static");
+    var holderRaw = new TruffleClassLoader();
+    var holderValue = context.asValue(holderRaw);
+    holderValue.putMember("any", clazzValue2);
     java.lang.Object clazzRaw = holderRaw.value;
     return (TruffleObject) clazzRaw;
   }
