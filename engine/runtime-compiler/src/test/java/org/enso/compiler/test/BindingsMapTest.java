ea0ff85094f1cab406fd467b98f4df8cb458b1ae
Pavel Marek <pavel.marek@enso.org>
2025-06-18 20:22:21 +0200

    more specific test

--- engine/runtime-compiler/src/test/java/org/enso/compiler/test/BindingsMapTest.java
+++ engine/runtime-compiler/src/test/java/org/enso/compiler/test/BindingsMapTest.java
@@ -3,8 +3,10 @@ package org.enso.compiler.test;
 import static org.enso.scala.wrapper.ScalaConversions.asScala;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
+import org.enso.compiler.data.BindingsMap;
 import org.enso.compiler.test.mock.WithCompilerContext;
 import org.enso.pkg.QualifiedName;
 import org.junit.Rule;
@@ -45,5 +47,8 @@ public final class BindingsMapTest {
     var nameForResolution = asScala(List.of("local", "Lib", "A", "A", "static_method"));
     var res = projMainBm.resolveQualifiedName(nameForResolution);
     assertThat(res.isRight(), is(true));
+    var resolvedNames = res.toOption().get();
+    assertThat(resolvedNames.size(), is(1));
+    assertTrue(resolvedNames.head() instanceof BindingsMap.ResolvedModuleMethod);
   }
 }
