6fbe0dbd014fe90d584914892eceac7d8da7fcae
Pavel Marek <pavel.marek@enso.org>
2025-06-23 15:57:48 +0200

    Rust build script deduces versions from Version.scala

--- build_tools/build/src/engine.rs
+++ build_tools/build/src/engine.rs
@@ -405,14 +405,14 @@ impl BuiltArtifacts {
 
 pub async fn deduce_graal(
     client: Octocrab,
-    build_sbt: &generated::RepoRootBuildSbt,
+    versions: &generated::RepoRootProjectVersionsScala,
 ) -> Result<ide_ci::cache::goodie::graalvm::GraalVM> {
-    let build_sbt_content = ide_ci::fs::tokio::read_to_string(build_sbt).await?;
+    let versions_content = ide_ci::fs::tokio::read_to_string(versions).await?;
     let graal_edition = env::GRAAL_EDITION.get().map_or(Edition::default(), |e| e);
 
     Ok(ide_ci::cache::goodie::graalvm::GraalVM {
         client,
-        graal_version: get_graal_version(&build_sbt_content)?,
+        graal_version: get_graal_version(&versions_content)?,
         edition: graal_edition,
         os: TARGET_OS,
         arch: TARGET_ARCH,
@@ -420,17 +420,19 @@ pub async fn deduce_graal(
 }
 
 pub async fn deduce_graal_bundle(
-    build_sbt: &generated::RepoRootBuildSbt,
+    versions: &generated::RepoRootProjectVersionsScala,
 ) -> Result<GraalVmVersion> {
-    let build_sbt_content = ide_ci::fs::tokio::read_to_string(build_sbt).await?;
+    let versions_content = ide_ci::fs::tokio::read_to_string(versions).await?;
     Ok(GraalVmVersion {
-        graal:    get_graal_version(&build_sbt_content)?,
-        packages: get_graal_packages_version(&build_sbt_content)?,
+        graal:    get_graal_version(&versions_content)?,
+        packages: get_graal_packages_version(&versions_content)?,
     })
 }
 
 /// Version of `flatc` (the FlatBuffers compiler) that Engine requires.
-pub async fn deduce_flatbuffers(build_sbt: &generated::RepoRootBuildSbt) -> Result<Version> {
-    let build_sbt_content = ide_ci::fs::tokio::read_to_string(build_sbt).await?;
+pub async fn deduce_flatbuffers(
+    versions: &generated::RepoRootProjectVersionsScala,
+) -> Result<Version> {
+    let build_sbt_content = ide_ci::fs::tokio::read_to_string(versions).await?;
     get_flatbuffers_version(&build_sbt_content)
 }
--- build_tools/build/src/engine/context.rs
+++ build_tools/build/src/engine/context.rs
@@ -143,7 +143,7 @@ impl RunContext {
 
         // Setup flatc (FlatBuffers compiler), required for building the engine.
         let flatc_goodie = cache::goodie::flatc::Flatc {
-            version:  engine::deduce_flatbuffers(&self.repo_root.build_sbt).await?,
+            version:  engine::deduce_flatbuffers(&self.repo_root.project.versions_scala).await?,
             platform: TARGET_OS,
         };
         flatc_goodie.install_if_missing(&self.cache).await?;
@@ -170,9 +170,11 @@ impl RunContext {
 
         // Setup GraalVM
         let graalvm =
-            engine::deduce_graal(self.octocrab.clone(), &self.repo_root.build_sbt).await?;
+            engine::deduce_graal(self.octocrab.clone(), &self.repo_root.project.versions_scala)
+                .await?;
         graalvm.install_if_missing(&self.cache).await?;
-        let graal_version = engine::deduce_graal_bundle(&self.repo_root.build_sbt).await?;
+        let graal_version =
+            engine::deduce_graal_bundle(&self.repo_root.project.versions_scala).await?;
         let graalpy_version = graal_version.packages;
 
         // Install GraalPy standalone distribution
@@ -470,7 +472,8 @@ impl RunContext {
             }
         }
 
-        let graal_version = engine::deduce_graal_bundle(&self.repo_root.build_sbt).await?;
+        let graal_version =
+            engine::deduce_graal_bundle(&self.repo_root.project.versions_scala).await?;
         for bundle in ret.bundles() {
             bundle.create(&self.repo_root, &graal_version).await?;
         }
