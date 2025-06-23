13524d809a590687c974179d6cf6ff54cb43a542
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-06-23 13:56:51 +0200

    Need to .value to get real ProjectPath

--- app/gui/src/project-view/components/GraphEditor/widgets/WidgetFunction/widgetFunctionCallInfo.ts
+++ app/gui/src/project-view/components/GraphEditor/widgets/WidgetFunction/widgetFunctionCallInfo.ts
@@ -112,7 +112,7 @@ export function useWidgetFunctionCallInfo(
       Ast.TextLiteral.new(JSON.stringify(args)).code(),
     ]
 
-    const modulePath = project.moduleProjectPath
+    let modulePath = project.moduleProjectPath.value
     const moduleFqn = projectNames.serializeProjectPathForBackend(modulePath)
 
     const expressionId = widgetQuerySubjectExpressionId.value
