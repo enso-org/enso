beb3152a2566c2aee1a3df7c81c5b9d4061ab69c
Pavel Marek <pavel.marek@enso.org>
2025-06-25 19:29:33 +0200

    NI for benchmarks is built with ENSO_LAUNCHER=native,-ls
    And not with ENSO_LAUNCHER=native,test. That caused failures in `Native_Image_Startup.import_world` benchmark.

--- build_tools/build/src/engine.rs
+++ build_tools/build/src/engine.rs
@@ -104,6 +104,9 @@ pub enum Tests {
 pub enum EngineLauncher {
     /// The binary inside the engine distribution will be built as an optimized native image
     Native,
+    /// The binary inside the engine distribution will be built as an optimized native image
+    /// without language server.
+    NativeWithoutLS,
     /// The binary inside the engine distribution will be built as native image with assertions
     /// enabled but no debug information
     TestNative,
@@ -127,6 +130,7 @@ impl Display for EngineLauncher {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
         let str = match self {
             EngineLauncher::Native => "native".to_string(),
+            EngineLauncher::NativeWithoutLS => "native,-ls".to_string(),
             EngineLauncher::TestNative => "native,test".to_string(),
             EngineLauncher::TestDebugNative => "native,test,debug".to_string(),
             EngineLauncher::Shell => "shell".to_string(),
