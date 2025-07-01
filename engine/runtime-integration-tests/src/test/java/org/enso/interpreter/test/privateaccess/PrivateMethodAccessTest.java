9dc390bda75322920d87a5ce4ac73c8ac9b9116b
Pavel Marek <pavel.marek@enso.org>
2025-07-01 12:49:47 +0200

    test calling private method directly or via lambda

--- engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/privateaccess/PrivateMethodAccessTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/privateaccess/PrivateMethodAccessTest.java
@@ -1,14 +1,24 @@
 package org.enso.interpreter.test.privateaccess;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import java.io.IOException;
+import java.util.Set;
 import org.enso.common.LanguageInfo;
 import org.enso.common.MethodNames.Module;
+import org.enso.pkg.QualifiedName;
 import org.enso.test.utils.ContextUtils;
+import org.enso.test.utils.ProjectUtils;
+import org.enso.test.utils.SourceModule;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class PrivateMethodAccessTest {
+  @Rule public TemporaryFolder tempFolder = new TemporaryFolder();
+
   @Test
   public void moduleDoesNotExposePrivateMethodsToPolyglot() {
     try (var ctx = ContextUtils.createDefault()) {
@@ -44,5 +54,93 @@ public class PrivateMethodAccessTest {
       var pubMethod = module.invokeMember(Module.GET_METHOD, myType, "pub_method");
       assertThat("public method is exposed to polyglot", pubMethod.canExecute(), is(true));
     }
+  }
+
+  @Test
+  public void cannotCallPrivateMethod_AsCallback() throws IOException {
+    var libDir = tempFolder.newFolder("Lib").toPath();
+    ProjectUtils.createProject(
+        "Lib",
+        Set.of(
+            new SourceModule(
+                QualifiedName.fromString("Mod"),
+                """
+                private priv_method x = x
+                """),
+            new SourceModule(
+                QualifiedName.fromString("Main"),
+                """
+                from project.Mod import priv_method
+
+                call_method ~callback =
+                    callback priv_method
+                """)),
+        libDir);
+
+    var projDir = tempFolder.newFolder("Proj").toPath();
+    ProjectUtils.createProject(
+        "Proj",
+        """
+        from local.Lib import call_method
+
+        callback priv_method =
+            # Here, we attempt to call `local.Lib.Mod.priv_method`
+            # Which should fail with private access error.
+            priv_method 42
+
+        main =
+            call_method callback
+        """,
+        projDir);
+
+    ProjectUtils.testProjectRunFailure(
+        projDir,
+        ex -> {
+          assertThat(ex.getMessage(), containsString("Private_Access"));
+        });
+  }
+
+  @Test
+  public void canCallPrivateMethod_ViaLambda() throws IOException {
+    var libDir = tempFolder.newFolder("Lib").toPath();
+    ProjectUtils.createProject(
+        "Lib",
+        Set.of(
+            new SourceModule(
+                QualifiedName.fromString("Mod"),
+                """
+                private priv_method =
+                    42
+                """),
+            new SourceModule(
+                QualifiedName.fromString("Main"),
+                """
+                from project.Mod import priv_method
+
+                call_method ~callback =
+                    callback \\_ -> priv_method
+                """)),
+        libDir);
+
+    var projDir = tempFolder.newFolder("Proj").toPath();
+    ProjectUtils.createProject(
+        "Proj",
+        """
+        from local.Lib import call_method
+
+        callback priv_method =
+            # Here, we actually call the lambda from `local.Lib.Main.call_method`
+            priv_method 42
+
+        main =
+            call_method callback
+        """,
+        projDir);
+
+    ProjectUtils.testProjectRun(
+        projDir,
+        res -> {
+          assertThat(res.asInt(), is(42));
+        });
   }
 }
