8b194d421d92138aa4d3f531ecf2976c1deb51b9
Kaz <kaz@lambdaverse.org>
2025-07-09 14:45:12 +0100

    Lint

--- app/gui/src/project-view/composables/componentColors.ts
+++ app/gui/src/project-view/composables/componentColors.ts
@@ -1,11 +1,12 @@
-import { type NodeId } from '@/stores/graph'
-import { type GraphDb } from '@/stores/graph/graphDatabase'
-import { type ToValue } from '@/util/reactivity'
+import type { NodeId } from '@/stores/graph'
+import type { GraphDb } from '@/stores/graph/graphDatabase'
+import type { Opt } from '@/util/data/opt'
+import type { ToValue } from '@/util/reactivity'
 import { computed, toValue } from 'vue'
 
 /** Returns the component's base color, and information about state that may modify it. */
 export function useComponentColors(
-  graphDb: ToValue<GraphDb>,
+  graphDb: ToValue<Opt<GraphDb>>,
   nodeSelection: { isSelected: (nodeId: NodeId) => boolean } | undefined,
   nodeId: ToValue<NodeId | undefined>,
 ) {
@@ -14,7 +15,7 @@ export function useComponentColors(
     () => nodeIdValue.value && toValue(graphDb)?.nodeIdToNode.get(nodeIdValue.value),
   )
   const expressionInfo = computed(
-    () => node.value && toValue(graphDb).getExpressionInfo(node.value.innerExpr.externalId),
+    () => node.value && toValue(graphDb)?.getExpressionInfo(node.value.innerExpr.externalId),
   )
   const executionState = computed(() => expressionInfo.value?.payload.type ?? 'Unknown')
   return {
