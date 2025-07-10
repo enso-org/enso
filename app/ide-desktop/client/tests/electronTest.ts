1e75aa7afc196392ed1c82f2fd75c0f5e71f9866
Paweł Grabarz <frizi09@gmail.com>
2025-07-10 16:57:12 +0200

    fix electron test login

--- app/ide-desktop/client/tests/electronTest.ts
+++ app/ide-desktop/client/tests/electronTest.ts
@@ -84,7 +84,7 @@ export async function loginAsTestUser(page: Page) {
   }
   await page.getByRole('textbox', { name: 'email' }).fill(process.env.ENSO_TEST_USER)
   await page.getByRole('textbox', { name: 'password' }).fill(process.env.ENSO_TEST_USER_PASSWORD)
-  await page.getByRole('button', { name: TEXT.login }).click()
+  await page.getByRole('button', { name: TEXT.login, exact: true }).click()
 
   await page
     .getByRole('group', { name: TEXT.licenseAgreementCheckbox })
