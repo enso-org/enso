014832d9b03a5f21eb4a55a3682d2d22c1531e55
Kaz <kaz@lambdaverse.org>
2025-07-17 11:02:04 +0100

    Update tests

--- app/gui/integration-test/project-view/widgets.spec.ts
+++ app/gui/integration-test/project-view/widgets.spec.ts
@@ -376,9 +376,9 @@ test('Selection widget with text widget as input', async ({ page }) => {
   // Using `type` instead of `inputText` here to catch keydown bugs like #13505.
   await page.keyboard.type('File 1')
   await pathDropdown.expectVisibleWithOptions(['File 1'])
-  // Clearing input should show all text literal options
+  // Clearing input should show all options
   await pathArgInput.clear()
-  await pathDropdown.expectVisibleWithOptions(['File 1', 'File 2'])
+  await pathDropdown.expectVisibleWithOptions([...CHOOSE_FILE_OPTIONS, 'File 1', 'File 2'])
 
   // Esc should cancel editing and close drop down
   await page.keyboard.press('Escape')
