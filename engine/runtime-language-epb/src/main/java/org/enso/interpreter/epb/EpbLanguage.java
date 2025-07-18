a95c74a5a079d721600b5763e94979d4977315ec
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-07-18 17:08:53 +0200

    Documenting the EpbLanguage

--- engine/runtime-language-epb/src/main/java/org/enso/interpreter/epb/EpbLanguage.java
+++ engine/runtime-language-epb/src/main/java/org/enso/interpreter/epb/EpbLanguage.java
@@ -13,19 +13,51 @@ import java.util.function.Consumer;
  *
  * <h3>Generic <code>foreign</code> Support</h3>
  *
- * TBD
+ * Each <em>compliant enough</em> Truffle language implementation that supports {@link
+ * TruffleLanguage#parse(com.oracle.truffle.api.TruffleLanguage.ParsingRequest)} with arguments
+ * provided via {@link ParsingRequest#getArgumentNames()} can be integrated with Enso's {@code
+ * foreign} code snippet concept.
  *
  * <h3><code>foreign js</code> Support</h3>
  *
- * TBD
+ * Graal.js is compliant enough, but comes with a <em>single threaded</em> restriction. Thus it
+ * needs special support that creates a secondary <em>JavaScript only</em> context in {@link
+ * ForeignEvalNode#parseJs()}.
+ *
+ * <p>Another special support provided to {@code foreign js} code is ability to refer to Enso {@code
+ * self} as JavaScript {@code this}. Such a support isn't achievable via standard Truffle means and
+ * requires {@link JsForeignNode#doExecute} to use {@code apply} JavaScript function directly.
  *
  * <h3><code>foreign python</code> Support</h3>
  *
- * TBD
+ * Graal Python had a lot of limitations in the past and as such the {@link PyForeignNode} does a
+ * lot of conversions to make sure Python {@code None} and Enso {@code Nothing} represent the same
+ * <em>null value</em>, that dates are properly transferred, etc.
+ *
+ * <p>Ideally each such limitation is reported to GraalPython guys. List of known issues:
+ *
+ * <ul>
+ *   <li><a href="https://github.com/oracle/graalpython/issues/354">numpy numbers aren't reported as
+ *       numbers</a>
+ *   <li><a href="https://github.com/oracle/graalpython/issues/353">Pandas DataFrame is reported as
+ *       array</a>
+ * </ul>
+ *
+ * Once an official solution is available, workarounds may be removed.
  *
  * <h3><code>polyglot java</code> Support</h3>
  *
- * TBD
+ * {@code EpbLanguage} is responsible for handling loading of Java classes via {@link
+ * JavaPolyglotNode}. There are three modes to load classes:
+ *
+ * <ul>
+ *   <li>{@code hosted} - regular GraalVM Java interop is used to load JVM classes
+ *   <li>{@code guest} - support SubstrateVM/HotspotVM bridge via {@code JVM} and {@code Channel}
+ *   <li>Espresso support - experimental controlled by {@code ENSO_JAVA} environment variable
+ * </ul>
+ *
+ * Each of these mechanisms is supposed to create a {@link TruffleObject} representing the JVM class
+ * that Enso can then operate with.
  */
 @TruffleLanguage.Registration(
     id = EpbLanguage.ID,
