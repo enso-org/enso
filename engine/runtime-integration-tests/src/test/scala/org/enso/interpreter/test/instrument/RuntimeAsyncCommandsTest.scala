f8a6fda3a051582ccc6c81df7e94a2df2e930b3a
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2025-07-30 15:20:23 +0200

    Avoid random failures in RuntimeAsyncCommandsTest
    Sort expected values to avoid spurious failures.

--- engine/runtime-integration-tests/src/test/scala/org/enso/interpreter/test/instrument/RuntimeAsyncCommandsTest.scala
+++ engine/runtime-integration-tests/src/test/scala/org/enso/interpreter/test/instrument/RuntimeAsyncCommandsTest.scala
@@ -930,7 +930,7 @@ class RuntimeAsyncCommandsTest
           ) =>
         data
     }
-    repliesData.map(new String(_)) shouldEqual List("42", "85")
+    repliesData.map(new String(_)).sorted shouldEqual List("42", "85")
   }
 
 }
