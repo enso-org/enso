1f2efb7b9d2edb7b389ee20bf51e68926eeda15c
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-07-30 08:01:17 +0200

    javafmtAll

--- engine/runtime-integration-tests/src/test/java/org/enso/interpreter/node/typecheck/TypeCheckValueTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/node/typecheck/TypeCheckValueTest.java
@@ -40,9 +40,13 @@ public class TypeCheckValueTest {
   private static CallTarget allOfIntegerAndText() {
     var call = new CallTarget[1];
     var builtins = ctxRule.ensoContext().getBuiltins();
-    var intNode = TypeCheckValueNode.single(AscriptionReason.forParameter("int"), builtins.number().getInteger());
-    var textNode = TypeCheckValueNode.single(AscriptionReason.forParameter("text"), builtins.text());
-    var bothNode = TypeCheckValueNode.allOf(AscriptionReason.forParameter("int&text"), intNode, textNode);
+    var intNode =
+        TypeCheckValueNode.single(
+            AscriptionReason.forParameter("int"), builtins.number().getInteger());
+    var textNode =
+        TypeCheckValueNode.single(AscriptionReason.forParameter("text"), builtins.text());
+    var bothNode =
+        TypeCheckValueNode.allOf(AscriptionReason.forParameter("int&text"), intNode, textNode);
     var root =
         new TestRootNode(
             (frame) -> {
