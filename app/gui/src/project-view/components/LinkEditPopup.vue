1f77526661525c05c8ab868c7d9bdf5f21125752
somebody1234 <ehern.lee@gmail.com>
2025-08-01 02:23:16 +1000

    WIP: Modal for opening projects when clicking on link to project

--- app/gui/src/project-view/components/LinkEditPopup.vue
+++ app/gui/src/project-view/components/LinkEditPopup.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import { textEditorsBindings } from '@/bindings'
+import StandaloneButton from '@/components/StandaloneButton.vue'
+import { injectInteractionHandler } from '@/providers/interactionHandler'
 import { autoUpdate, flip, useFloating } from '@floating-ui/vue'
-import { computed, toRef, useTemplateRef } from 'vue'
+import { AnimatePresence, motion } from 'motion-v'
+import { ref, toRef, useTemplateRef, watchEffect } from 'vue'
+import { useRouter } from 'vue-router'
 
 const props = defineProps<{
   referenceElement: HTMLElement
@@ -9,7 +13,23 @@ const props = defineProps<{
   popOut: boolean
 }>()
 
+const router = useRouter()
 const floatingElement = useTemplateRef<HTMLElement>('floating')
+const isModalOpen = ref(false)
+const interaction = injectInteractionHandler()
+
+watchEffect(() => {
+  if (isModalOpen.value) {
+    interaction.setCurrent({
+      cancel() {
+        isModalOpen.value = false
+      },
+      end() {
+        isModalOpen.value = false
+      },
+    })
+  }
+})
 
 const { floatingStyles } = useFloating(toRef(props, 'referenceElement'), floatingElement, {
   placement: 'top-start',
@@ -18,31 +38,52 @@ const { floatingStyles } = useFloating(toRef(props, 'referenceElement'), floatin
   whileElementsMounted: autoUpdate,
 })
 
-function showOpenProjectModal() {
+function openProjectInNewTab() {
+  isModalOpen.value = false
+  router.push(`/${props.href}`)
 }
-
 </script>
 
 <template>
   <teleport to="#floatingLayer">
     <div ref="floating" class="LinkEditPopup" :style="floatingStyles" @pointerdown.stop.prevent>
       <a
-        v-if="href.startsWith('enso:')"
+        v-if="!/^enso:.*[.]project$/.test(href)"
         class="link"
         :href="href"
         target="_blank"
         rel="noopener,noreferrer"
         >Follow link</a
       >
-      <a
-        v-else
-        class="link"
-        @click="showOpenProjectModal"
-        >Follow link</a
-      >
+      <a v-else class="link" @click="isModalOpen = true">Follow link</a>
       ({{ textEditorsBindings.bindings.openLink.humanReadable }})
     </div>
-    </div>
+  </teleport>
+
+  <teleport to="#floatingLayer">
+    <AnimatePresence>
+      <motion.div
+        v-if="isModalOpen"
+        class="modal"
+        :initial="{ opacity: 0, y: '-100px' }"
+        :animate="{ opacity: 1, y: '0' }"
+        :exit="{ opacity: 0, y: '-100px' }"
+        @keydown.esc="isModalOpen = false"
+        @click.self="isModalOpen = false"
+      >
+        <div class="modal-container">
+          <h2>Open Project</h2>
+          <p>
+            Would you like to open the project at '{{ decodeURIComponent(href) }}'? The current
+            project will be closed.
+          </p>
+          <div class="button-bar">
+            <StandaloneButton label="Cancel" @activate="isModalOpen = false" />
+            <StandaloneButton label="Open" @activate="openProjectInNewTab" />
+          </div>
+        </div>
+      </motion.div>
+    </AnimatePresence>
   </teleport>
 </template>
 
@@ -61,8 +102,50 @@ function showOpenProjectModal() {
 .link {
   cursor: pointer;
   color: blue;
+
   &:hover {
     text-decoration: underline;
   }
+}
+
+.modal {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  cursor: pointer;
+  max-height: 100vh;
+
+  &::before {
+    content: '';
+    position: absolute;
+    inset: 0;
+    top: -100px;
+    height: calc(100% + 200px);
+    background: var(--color-dim);
+  }
+
+  > .modal-container {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: var(--color-app-bg);
+    border-radius: var(--radius-default);
+    backdrop-filter: blur(8px);
+    cursor: default;
+    padding: 1em;
+    display: flex;
+    flex-flow: column nowrap;
+    gap: 1em;
+    max-width: 32em;
+  }
+}
+
+.button-bar {
+  display: flex;
+  gap: 1em;
+  justify-content: flex-end;
 }
 </style>
