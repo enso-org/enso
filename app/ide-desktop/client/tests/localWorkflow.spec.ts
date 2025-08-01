883efa35e190621e70a73d68cdae6215d4816305
James Dunkerley <jdunkerley@users.noreply.github.com>
2025-08-01 16:32:20 +0100

    Fix test maybe.

--- app/ide-desktop/client/tests/localWorkflow.spec.ts
+++ app/ide-desktop/client/tests/localWorkflow.spec.ts
@@ -23,6 +23,9 @@ test('Local Workflow', async ({ page, app, projectsDir }) => {
   const TEXT_TO_WRITE = 'Some text'
 
   await loginAsTestUser(page)
+  await page.getByRole('tab')
+      .filter({ has: page.getByText('Data Catalog') })
+      .click()
   await expect(page.getByRole('button', { name: 'New Project', exact: true })).toBeVisible()
   await page.getByRole('button', { name: 'New Project', exact: true }).click()
   await expect(page.locator('.GraphNode')).toHaveCount(1, { timeout: 60000 })
