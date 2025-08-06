ec22ad1cdb611f88d3be9a501f97012019608218
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-08-06 05:31:39 +0200

    More verbose name of the callback function

--- engine/language-server/src/main/scala/org/enso/languageserver/boot/MainModule.scala
+++ engine/language-server/src/main/scala/org/enso/languageserver/boot/MainModule.scala
@@ -187,10 +187,10 @@ class MainModule(serverConfig: LanguageServerConfig, logLevel: Level) {
     languageServerConfig.profiling.profilingEventsLogPath match {
       case Some(path) =>
         val out = new PrintStream(path.toFile, StandardCharsets.UTF_8)
-        def log(at: java.time.Instant, msg: String) = {
+        def logInstantMsg(at: java.time.Instant, msg: String) = {
           out.println(s"$at $msg")
         }
-        new RuntimeEventsMonitor(log) -> Some(())
+        new RuntimeEventsMonitor(logInstantMsg) -> Some(())
       case None =>
         EventsMonitor.NOOP -> None
     }
