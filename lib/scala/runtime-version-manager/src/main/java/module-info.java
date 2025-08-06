bb5c519c58f1d5c2233b30acd9c82cf7738055ba
Pavel Marek <pavel.marek@enso.org>
2025-08-06 19:48:31 +0200

    Add dependencies for runtime-version-manager

--- lib/scala/runtime-version-manager/src/main/java/module-info.java
+++ lib/scala/runtime-version-manager/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module org.enso.runtime.version.manager {
+  // Needed for `org.apache.tika.core` automatic module.
+  requires java.sql;
   requires scala.library;
   requires org.apache.commons.compress;
+  // Automatic module
+  requires org.apache.tika.core;
   requires org.slf4j;
   requires org.enso.cli;
   requires org.enso.distribution;
@@ -14,6 +18,7 @@ module org.enso.runtime.version.manager {
   // For com.typesafe.scalalogging.Logger
   requires org.enso.scala.wrapper;
   requires org.enso.version.output;
+  requires org.yaml.snakeyaml;
 
   exports org.enso.runtimeversionmanager;
   exports org.enso.runtimeversionmanager.cli;
