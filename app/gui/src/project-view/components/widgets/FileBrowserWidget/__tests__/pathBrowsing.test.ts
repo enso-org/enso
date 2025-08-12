dc95f9cebc02f4851481e093d1b214340fc6928d
Ilya Bogdanov <fumlead@gmail.com>
2025-08-12 14:47:50 +0400

    Fix unit tests

--- app/gui/src/project-view/components/widgets/FileBrowserWidget/__tests__/pathBrowsing.test.ts
+++ app/gui/src/project-view/components/widgets/FileBrowserWidget/__tests__/pathBrowsing.test.ts
@@ -4,35 +4,48 @@ import { useEnsoPaths } from '../ensoPath'
 import { usePathBrowsing } from '../pathBrowsing'
 import { MOCK_FS, mockListDirectory } from './mockData'
 
-function pathSegments(initialPath: string, rootPath: string, rootId: DirectoryId): string[] {
+function deriveRootId(rootPath: string): DirectoryId {
+  switch (rootPath) {
+    case 'enso://':
+      return '0' as DirectoryId
+    case 'enso://Users/user':
+      return '3' as DirectoryId
+    default:
+      throw new Error(`Unknown rootPath: ${rootPath}`)
+  }
+}
+
+function pathSegments(initialPath: string, rootPath: string): string[] {
+  const rootId = deriveRootId(rootPath)
   const { parseEnsoPath } = useEnsoPaths({
     rootDirectoryId: rootId,
     rootPath,
   })
   const path = parseEnsoPath(initialPath)
-  return path.ok ? path.value.segments : ['Users', 'user']
+  return path.ok ? path.value.segments : []
 }
 
 test.each`
-  initialPath                                       | rootPath               | rootId | expectedStack           | unenteredPath
-  ${''}                                             | ${'enso://'}           | ${'0'} | ${['0', '1', '3']}      | ${''}
-  ${''}                                             | ${'enso://Users/user'} | ${'3'} | ${['3']}                | ${''}
-  ${'enso://'}                                      | ${'enso://'}           | ${'0'} | ${['0']}                | ${''}
-  ${'enso://'}                                      | ${'enso://Users/user'} | ${'3'} | ${['3']}                | ${''}
-  ${'enso://Users/user'}                            | ${'enso://'}           | ${'0'} | ${['0', '1', '3']}      | ${''}
-  ${'enso://Users/user'}                            | ${'enso://Users/user'} | ${'3'} | ${['3']}                | ${''}
-  ${'enso://Users/user/input.csv'}                  | ${'enso://'}           | ${'0'} | ${['0', '1', '3']}      | ${'input.csv'}
-  ${'enso://Users/user/input.csv'}                  | ${'enso://Users/user'} | ${'3'} | ${['3']}                | ${'input.csv'}
-  ${'enso://Users/user/New Folder 1/input.csv'}     | ${'enso://'}           | ${'0'} | ${['0', '1', '3', '4']} | ${'input.csv'}
-  ${'enso://Users/user/New Folder 1/input.csv'}     | ${'enso://Users/user'} | ${'3'} | ${['3', '4']}           | ${'input.csv'}
-  ${'enso://Users/user/New Folder 2/input.csv'}     | ${'enso://'}           | ${'0'} | ${['0', '1', '3']}      | ${''}
-  ${'enso://Users/user/New Folder 2/input.csv'}     | ${'enso://Users/user'} | ${'3'} | ${['3']}                | ${''}
-  ${'enso://Users/user/New Folder 1/dir/input.csv'} | ${'enso://'}           | ${'0'} | ${['0', '1', '3', '4']} | ${''}
-  ${'enso://Users/user/New Folder 1/dir/input.csv'} | ${'enso://Users/user'} | ${'3'} | ${['3', '4']}           | ${''}
+  initialPath                                       | rootPath               | expectedStack           | unenteredPath
+  ${''}                                             | ${'enso://'}           | ${['0']}                | ${''}
+  ${''}                                             | ${'enso://Users/user'} | ${['3']}                | ${''}
+  ${'enso://'}                                      | ${'enso://'}           | ${['0']}                | ${''}
+  ${'enso://'}                                      | ${'enso://Users/user'} | ${['3']}                | ${''}
+  ${'enso://Users/user'}                            | ${'enso://'}           | ${['0', '1', '3']}      | ${''}
+  ${'enso://Users/user'}                            | ${'enso://Users/user'} | ${['3']}                | ${''}
+  ${'enso://Users/user/input.csv'}                  | ${'enso://'}           | ${['0', '1', '3']}      | ${'input.csv'}
+  ${'enso://Users/user/input.csv'}                  | ${'enso://Users/user'} | ${['3']}                | ${'input.csv'}
+  ${'enso://Users/user/New Folder 1/input.csv'}     | ${'enso://'}           | ${['0', '1', '3', '4']} | ${'input.csv'}
+  ${'enso://Users/user/New Folder 1/input.csv'}     | ${'enso://Users/user'} | ${['3', '4']}           | ${'input.csv'}
+  ${'enso://Users/user/New Folder 2/input.csv'}     | ${'enso://'}           | ${['0', '1', '3']}      | ${'New Folder 2/input.csv'}
+  ${'enso://Users/user/New Folder 2/input.csv'}     | ${'enso://Users/user'} | ${['3']}                | ${'New Folder 2/input.csv'}
+  ${'enso://Users/user/New Folder 1/dir/input.csv'} | ${'enso://'}           | ${['0', '1', '3', '4']} | ${'dir/input.csv'}
+  ${'enso://Users/user/New Folder 1/dir/input.csv'} | ${'enso://Users/user'} | ${['3', '4']}           | ${'dir/input.csv'}
 `(
   'Initializing dir stack: initial path: $initialPath, root path: $rootPath',
-  async ({ initialPath, rootPath, rootId, expectedStack, unenteredPath }) => {
-    const segments = pathSegments(initialPath, rootPath, rootId)
+  async ({ initialPath, rootPath, expectedStack, unenteredPath }) => {
+    const segments = pathSegments(initialPath, rootPath)
+    const rootId = deriveRootId(rootPath)
     const { setBrowsingPath, enteredPath, unenteredPathSuffix, currentDirectory, isPending } =
       usePathBrowsing({
         listDirectory: mockListDirectory,
