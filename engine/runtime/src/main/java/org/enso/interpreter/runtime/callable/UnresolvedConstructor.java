3ab4f3dda8f70ddb17ddfc2f6ad4a32b0a3f2321
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-08-18 18:16:42 +0200

    Accept any EnsoObject as return value from auto constructor

--- engine/runtime/src/main/java/org/enso/interpreter/runtime/callable/UnresolvedConstructor.java
+++ engine/runtime/src/main/java/org/enso/interpreter/runtime/callable/UnresolvedConstructor.java
@@ -32,7 +32,6 @@ import org.enso.interpreter.runtime.callable.argument.CallArgumentInfo;
 import org.enso.interpreter.runtime.callable.function.Function;
 import org.enso.interpreter.runtime.data.EnsoObject;
 import org.enso.interpreter.runtime.data.Type;
-import org.enso.interpreter.runtime.data.atom.Atom;
 import org.enso.interpreter.runtime.data.atom.AtomConstructor;
 import org.enso.interpreter.runtime.error.DataflowError;
 import org.enso.interpreter.runtime.error.PanicException;
@@ -272,7 +271,7 @@ public final class UnresolvedConstructor extends EnsoObject {
       args[0] = fn;
       var helper = Function.ArgumentsHelper.buildArguments(fn, null, args);
       var r = callNode.call(helper);
-      if (r instanceof Atom) {
+      if (r instanceof EnsoObject) {
         return r;
       } else if (r instanceof DataflowError) {
         return r;
