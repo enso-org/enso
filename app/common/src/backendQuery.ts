6edae72f240d992e4974c13dea8582e679ae73aa
somebody1234 <ehern.lee@gmail.com>
2025-08-21 19:12:52 +1000

    Add invalidations for 'remove user' backend API call

--- app/common/src/backendQuery.ts
+++ app/common/src/backendQuery.ts
@@ -76,6 +76,7 @@ export const INVALIDATION_MAP: Partial<
   createUser: ['usersMe'],
   updateUser: [INVALIDATE_ALL_QUERIES],
   deleteUser: ['usersMe', 'listUsers', 'listUserGroups', 'listDirectory', 'getAssetDetails'],
+  removeUser: ['usersMe', 'listUsers', 'listUserGroups', 'listDirectory', 'getAssetDetails'],
   restoreUser: ['usersMe'],
   uploadUserPicture: ['usersMe'],
   updateOrganization: ['getOrganization'],
