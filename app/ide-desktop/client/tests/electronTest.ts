8b612de024cf5d5e8a7bcd89dc906927f84c71a5
somebody1234 <ehern.lee@gmail.com>
2025-08-27 18:12:10 +1000

    Fix E2E test build errors on Windows

--- app/ide-desktop/client/tests/electronTest.ts
+++ app/ide-desktop/client/tests/electronTest.ts
@@ -5,7 +5,13 @@ import { TEXTS } from 'enso-common/src/text'
 import fs from 'node:fs/promises'
 import os from 'node:os'
 import path from 'node:path'
-import { _electron, test as base, ElectronApplication, expect, type Page } from 'playwright/test'
+import {
+  _electron,
+  test as base,
+  expect,
+  type ElectronApplication,
+  type Page,
+} from 'playwright/test'
 
 const LOADING_TIMEOUT = 10000
 const TEXT = TEXTS.english
