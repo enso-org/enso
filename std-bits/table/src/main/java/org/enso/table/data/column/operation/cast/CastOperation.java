322b4edc8bd8f0e61006b7b0c7ed0fac36f74c9e
James Dunkerley <jdunkerley@users.noreply.github.com>
2025-09-03 13:56:16 +0100

    Java format again...

--- std-bits/table/src/main/java/org/enso/table/data/column/operation/cast/CastOperation.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/operation/cast/CastOperation.java
@@ -51,7 +51,8 @@ public class CastOperation {
       case BigIntegerType bigIntegerType -> new ToBigIntegerConverter();
       case BigDecimalType bigDecimalType -> new ToBigDecimalConverter();
       case NullType nullType -> throw new IllegalArgumentException("Cannot cast to Null type.");
-      default -> fromStorageType(StorageType.fromTypeCharAndSize(storageType.typeChar(), storageType.size()));
+      default -> fromStorageType(
+          StorageType.fromTypeCharAndSize(storageType.typeChar(), storageType.size()));
     };
   }
 
