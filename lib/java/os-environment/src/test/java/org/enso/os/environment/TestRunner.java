ecb0f4c97ea65941495c6137c0b4a38bcbe0efcf
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-09-07 14:15:48 +0200

    Allow execution of a single test method

--- lib/java/os-environment/src/test/java/org/enso/os/environment/TestRunner.java
+++ lib/java/os-environment/src/test/java/org/enso/os/environment/TestRunner.java
@@ -1,32 +1,37 @@
 package org.enso.os.environment;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import org.junit.runner.Description;
 import org.junit.runner.JUnitCore;
+import org.junit.runner.Request;
 import org.junit.runner.Result;
+import org.junit.runner.manipulation.Filter;
 import org.junit.runner.notification.Failure;
 
 public class TestRunner {
   public static void main(String[] args) throws Throwable {
     var junit = new JUnitCore();
-    var results = new ArrayList<Result>();
-    for (var testClass : getAllTests()) {
-      var result = junit.run(testClass);
-      results.add(result);
+    var req = Request.classes(getAllTests().toArray(Class[]::new));
+    if (args.length > 0) {
+      req = req.filterWith(new FilterByMethodName(args));
     }
-    printSummary(results);
+    var result = junit.run(req);
+    printSummary(Collections.singletonList(result));
   }
 
   private static void printSummary(List<Result> results) {
     var runTests = results.stream().mapToInt(Result::getRunCount).sum();
     var failedTests = results.stream().map(Result::getFailures).flatMap(List::stream).toList();
     var ignoredTests = results.stream().map(Result::getIgnoreCount).mapToInt(m -> m).sum();
-    System.out.printf("Test run (of %s classes) finished.\n", results.size());
+    System.out.printf("Test run finished.\n");
     System.out.printf("Number of tests: %s\n", runTests);
     System.out.printf("Number of tests failed: %s\n", failedTests.size());
     System.out.printf("Number of tests ignored: %s\n", ignoredTests);
     var success = failedTests.isEmpty();
-    System.out.printf("Test run successful: %s", success);
+    System.out.printf("Test run successful: %s\n", success);
     if (!success) {
       printFailures(failedTests);
       System.exit(1);
@@ -42,6 +47,13 @@ public class TestRunner {
       System.out.println("Stack:");
       failure.getException().printStackTrace(System.out);
     }
+    System.out.println("Re-run as:");
+    for (var failure : failures) {
+      System.out.println(
+          ProcessHandle.current().info().command().get()
+              + " "
+              + failure.getDescription().getMethodName());
+    }
   }
 
   private static List<Class<?>> getAllTests() {
@@ -54,5 +66,35 @@ public class TestRunner {
       }
     }
     return testClasses;
+  }
+
+  private static final class FilterByMethodName extends Filter {
+    private final String[] args;
+
+    FilterByMethodName(String[] args) {
+      this.args = args;
+    }
+
+    @Override
+    public boolean shouldRun(Description description) {
+      if (description.isSuite()) {
+        return true;
+      }
+      var run = Arrays.asList(args).contains(description.getMethodName());
+      if (!run) {
+        System.err.println("skipping " + description.getMethodName());
+      }
+      return run;
+    }
+
+    @Override
+    public String describe() {
+      return "Filter" + Arrays.toString(args);
+    }
+
+    @Override
+    public String toString() {
+      return describe();
+    }
   }
 }
