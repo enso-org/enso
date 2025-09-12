29919b71528cea269dc4cabcb407bc353d074782
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-09-12 08:14:33 +0200

    Removing the explanation 'assert'

--- engine/runtime-parser/src/main/scala/org/enso/compiler/core/ir/expression/errors/Syntax.scala
+++ engine/runtime-parser/src/main/scala/org/enso/compiler/core/ir/expression/errors/Syntax.scala
@@ -165,9 +165,8 @@ object Syntax {
       s"$number is not valid in $base"
   }
 
-  case class UnsupportedSyntax(override val explanation: String) extends Reason {
-    if (explanation.length < 5) throw new IllegalStateException("Missing description: " + explanation)
-  }
+  case class UnsupportedSyntax(override val explanation: String)
+      extends Reason {}
 
   case object InconsistentConstructorVisibility extends Reason {
     override def explanation: String =
