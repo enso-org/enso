d91ed38e15f8e53036523bb16aa8cac456eb3d12
Adam Riley <riley.ad@gmail.com>
2025-09-16 14:21:19 +0100

    Fix tests

--- std-bits/table/src/main/java/org/enso/table/excel/ExcelConnectionPool.java
+++ std-bits/table/src/main/java/org/enso/table/excel/ExcelConnectionPool.java
@@ -11,6 +11,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.function.Function;
+
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.exceptions.OLE2NotOfficeXmlFileException;
@@ -84,13 +85,14 @@ public class ExcelConnectionPool implements ReloadDetector.HasClearableCache {
   public synchronized void closeConnection(File file, ExcelFileFormat format) throws IOException {
     if (isCurrentlyWriting) {
       throw new IllegalStateException(
-          "Cannot open a read-only Excel connection while an Excel file is being "
+          "Cannot close a Excel connection while an Excel file is being "
               + "written to. This is a bug in the Table library.");
     }
     String key = getKeyForFile(file);
     ConnectionRecord existingRecord = records.get(key);
     if (existingRecord != null) {
       existingRecord.close();
+      records.remove(key);
     }
   }
 
