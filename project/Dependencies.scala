2646d457c085322aaae11cbbc93e1f459f89c8cb
Pavel Marek <pavel.marek@enso.org>
2025-09-17 14:57:21 +0200

    Upgrade graalVersion and graalMavenPackageVersion in build.sbt

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -54,13 +54,13 @@ object Dependencies {
   // === project-wide versions =====================================================
   val scalacVersion = "2.13.15"
   // source version of the Java language
-  val javaVersion = "24"
+  val javaVersion = "25"
   // version of the GraalVM JDK
-  val graalVersion = "24.0.1"
+  val graalVersion = "25.0.0"
   // Version used for the Graal/Truffle related Maven packages
   // Keep in sync with GraalVM.version. Do not change the name of this variable,
   // it is used by the Rust build script via regex matching.
-  val graalMavenPackagesVersion = "24.2.0"
+  val graalMavenPackagesVersion = "25.0.0"
   val targetJavaVersion         = "17"
   val defaultDevEnsoVersion     = "0.0.0-dev"
   val ensoVersion = sys.env.getOrElse(
