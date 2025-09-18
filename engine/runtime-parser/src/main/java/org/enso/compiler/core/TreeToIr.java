3fa0ecd6974ca307f68f01bcfa929ace9baeca52
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-09-18 06:21:31 +0200

    Convert all Tree.Invalid into Syntax.UnsupportedSyntax

--- engine/runtime-parser/src/main/java/org/enso/compiler/core/TreeToIr.java
+++ engine/runtime-parser/src/main/java/org/enso/compiler/core/TreeToIr.java
@@ -2020,11 +2020,9 @@ final class TreeToIr {
   }
 
   Syntax translateSyntaxError(Tree where, Syntax.Reason reason) {
-      if (reason == Syntax.UnexpectedExpression$.MODULE$ && where instanceof Tree.Invalid invalid) {
-        if (!invalid.getError().startsWith("Unexpected token:")) {
-          reason = new Syntax.UnsupportedSyntax(invalid.getError());
-        }
-      }
+    if (reason == Syntax.UnexpectedExpression$.MODULE$ && where instanceof Tree.Invalid invalid) {
+      reason = new Syntax.UnsupportedSyntax(invalid.getError());
+    }
     var at = getIdentifiedLocation(where);
     return new Syntax(at, reason, meta());
   }
