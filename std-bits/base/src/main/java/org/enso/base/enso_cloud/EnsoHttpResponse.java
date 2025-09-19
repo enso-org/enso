34ff4613149554821fdd39c8d5af8a723854e173
Adam Riley <riley.ad@gmail.com>
2025-09-19 16:58:37 +0100

    Move down to java level

--- std-bits/base/src/main/java/org/enso/base/enso_cloud/EnsoHttpResponse.java
+++ std-bits/base/src/main/java/org/enso/base/enso_cloud/EnsoHttpResponse.java
@@ -1,8 +1,35 @@
 package org.enso.base.enso_cloud;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpHeaders;
+import java.util.zip.GZIPInputStream;
 
-/** A subset of the HttpResponse to avoid leaking the decrypted Enso secrets. */
-public record EnsoHttpResponse(URI uri, HttpHeaders headers, InputStream body, int statusCode) {}
+/**
+ * A subset of the HttpResponse to avoid leaking the decrypted Enso secrets.
+ *
+ * <p>The {@code body} is transparently decoded based on the {@code Content-Encoding} header. It
+ * supports only the "gzip" encoding. If other encodings are present, the body is left undecoded.
+ */
+public record EnsoHttpResponse(URI uri, HttpHeaders headers, InputStream body, int statusCode) {
+  public EnsoHttpResponse {
+    body = decodeContentEncoding(body, headers);
+  }
+
+  private static InputStream decodeContentEncoding(InputStream stream, HttpHeaders headers) {
+    var encOpt = headers.firstValue("content-encoding");
+    if (!encOpt.isEmpty() && "gzip".equals(encOpt.get().toLowerCase())) 
+    {
+        InputStream ret;
+        try {
+          ret = new GZIPInputStream(stream);
+        } catch (IOException e) {
+          // If we cannot decode, fall back to raw stream; consumers may handle errors.
+          return stream;
+        }
+        return ret;
+    }
+    return stream;
+  }
+}
