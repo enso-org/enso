b929e418f76717670e8e7008313c0ea4b8c3fa1e
Adam Riley <riley.ad@gmail.com>
2025-09-22 11:14:28 +0100

    revert

--- std-bits/base/src/main/java/org/enso/base/enso_cloud/EnsoHttpResponse.java
+++ std-bits/base/src/main/java/org/enso/base/enso_cloud/EnsoHttpResponse.java
@@ -4,10 +4,5 @@ import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpHeaders;
 
-/**
- * A subset of the HttpResponse to avoid leaking the decrypted Enso secrets.
- *
- * <p>The {@code body} is transparently decoded based on the {@code Content-Encoding} header. It
- * supports only the "gzip" encoding. If other encodings are present, the body is left undecoded.
- */
+/** A subset of the HttpResponse to avoid leaking the decrypted Enso secrets. */
 public record EnsoHttpResponse(URI uri, HttpHeaders headers, InputStream body, int statusCode) {}
