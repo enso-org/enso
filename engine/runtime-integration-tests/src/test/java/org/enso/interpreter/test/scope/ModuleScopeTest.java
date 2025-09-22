b9ad2d5d3a273a3643c18c9a3db33b2d30f2aec8
Pavel Marek <pavel.marek@enso.org>
2025-09-22 19:15:35 +0200

    Test that method is registered just once in the scope.

--- engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/scope/ModuleScopeTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/scope/ModuleScopeTest.java
@@ -1,10 +1,12 @@
 package org.enso.interpreter.test.scope;
 
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Set;
 import org.enso.common.LanguageInfo;
 import org.enso.interpreter.runtime.Module;
@@ -22,26 +24,6 @@ import org.junit.rules.TemporaryFolder;
 public class ModuleScopeTest {
   @ClassRule public static final ContextUtils ctxRule = ContextUtils.createDefault();
   @Rule public TemporaryFolder tempFolder = new TemporaryFolder();
-
-  @Test
-  public void extensionMethodIsRegisteredInModuleScope() throws IOException {
-    var mainSrc =
-        Source.newBuilder(
-                LanguageInfo.ID,
-                """
-        type My_Type
-            Value x
-        My_Type.extension_method self = self.x
-        """,
-                "test.enso")
-            .build();
-    var mainMod = ctxRule.eval(mainSrc);
-    var mainRuntimeMod = (Module) ctxRule.unwrapValue(mainMod);
-    var myType = mainRuntimeMod.getScope().getType("My_Type", true);
-    assertThat(myType, is(notNullValue()));
-    var extensionMethod = mainRuntimeMod.getScope().getMethodForType(myType, "extension_method");
-    assertThat(extensionMethod, is(notNullValue()));
-  }
 
   @Test
   public void staticMethodIsInResolvedExports() throws IOException {
@@ -65,25 +47,6 @@ public class ModuleScopeTest {
   }
 
   @Test
-  public void staticMethodIsRegisteredInModuleScope() throws IOException {
-    var mainSrc =
-        Source.newBuilder(
-                LanguageInfo.ID,
-                """
-        type My_Type
-            static_method _ = 42
-        """,
-                "test.enso")
-            .build();
-    var mainMod = ctxRule.eval(mainSrc);
-    var mainRuntimeMod = (Module) ctxRule.unwrapValue(mainMod);
-    var myType = mainRuntimeMod.getScope().getType("My_Type", true);
-    assertThat(myType, is(notNullValue()));
-    var staticMethod = mainRuntimeMod.getScope().getMethodForType(myType, "static_method");
-    assertThat(staticMethod, is(notNullValue()));
-  }
-
-  @Test
   public void moduleMethodIsRegisteredInModuleScope() throws IOException {
     var mainSrc =
         Source.newBuilder(
@@ -99,6 +62,147 @@ public class ModuleScopeTest {
     assertThat(assocType, is(notNullValue()));
     var moduleMethod = mainRuntimeMod.getScope().getMethodForType(assocType, "module_method");
     assertThat(moduleMethod, is(notNullValue()));
+  }
+
+  @Test
+  public void scopeGetType_DoesNotReturn_EigenType() throws IOException {
+    var mainSrc =
+        Source.newBuilder(
+                LanguageInfo.ID,
+                """
+        type My_Type
+            Value x
+            method self = self.x
+        """,
+                "test.enso")
+            .build();
+    var mainMod = ctxRule.eval(mainSrc);
+    var mainRuntimeMod = (Module) ctxRule.unwrapValue(mainMod);
+    var scope = mainRuntimeMod.getScope();
+    var myType = scope.getType("My_Type", true);
+    assertThat(myType.isEigenType(), is(false));
+  }
+
+  @Test
+  public void instanceMethodIsRegistered_OnType() throws IOException {
+    var mainSrc =
+        Source.newBuilder(
+                LanguageInfo.ID,
+                """
+        type My_Type
+            Value x
+            method self = self.x
+        """,
+                "test.enso")
+            .build();
+    var mainMod = ctxRule.eval(mainSrc);
+    var mainRuntimeMod = (Module) ctxRule.unwrapValue(mainMod);
+    var scope = mainRuntimeMod.getScope();
+    var myType = scope.getType("My_Type", true).getEigentype();
+    var myEigenType = myType.getEigentype();
+    var method = scope.getMethodForType(myType, "method");
+    assertThat("My_Type.method is registered", method, is(notNullValue()));
+    var eigenMethod = scope.getMethodForType(myEigenType, "method");
+    assertThat("My_Type.type.method is not registered", eigenMethod, is(nullValue()));
+  }
+
+  @Test
+  public void staticMethodIsRegistered_OnEigenType() throws IOException {
+    var mainSrc =
+        Source.newBuilder(
+                LanguageInfo.ID,
+                """
+        type My_Type
+            static_method = 42
+        """,
+                "test.enso")
+            .build();
+    var mainMod = ctxRule.eval(mainSrc);
+    var mainRuntimeMod = (Module) ctxRule.unwrapValue(mainMod);
+    var scope = mainRuntimeMod.getScope();
+    var myType = scope.getType("My_Type", true);
+    var myEigenType = myType.getEigentype();
+    var staticMethod = scope.getMethodForType(myEigenType, "static_method");
+    assertThat("My_Type.type.static_method is registered", staticMethod, is(notNullValue()));
+    assertThat(
+        "My_Type.static_method is not registered",
+        scope.getMethodForType(myType, "static_method"),
+        is(nullValue()));
+  }
+
+  @Test
+  public void instanceExtensionMethodIsRegistered_OnType() throws IOException {
+    var mainSrc =
+        Source.newBuilder(
+                LanguageInfo.ID,
+                """
+        type My_Type
+            Value x
+        My_Type.extension_method self = self.x
+        """,
+                "test.enso")
+            .build();
+    var mainMod = ctxRule.eval(mainSrc);
+    var mainRuntimeMod = (Module) ctxRule.unwrapValue(mainMod);
+    var scope = mainRuntimeMod.getScope();
+    var myType = scope.getType("My_Type", true);
+    var extensionMethod = scope.getMethodForType(myType, "extension_method");
+    assertThat("My_Type.extension_method is registered", extensionMethod, is(notNullValue()));
+    var myEigenType = myType.getEigentype();
+    assertThat(
+        "My_Type.type.extension_method is not registered",
+        scope.getMethodForType(myEigenType, "extension_method"),
+        is(nullValue()));
+  }
+
+  @Test
+  public void constructorIsRegistered_OnEigenType() throws IOException {
+    var mainSrc =
+        Source.newBuilder(
+                LanguageInfo.ID,
+                """
+        type My_Type
+            Value x
+            method self = self.x
+        """,
+                "test.enso")
+            .build();
+    var mainMod = ctxRule.eval(mainSrc);
+    var mainRuntimeMod = (Module) ctxRule.unwrapValue(mainMod);
+    var scope = mainRuntimeMod.getScope();
+    var myType = scope.getType("My_Type", true);
+    assertThat(myType.isEigenType(), is(false));
+    var myEigenType = myType.getEigentype();
+    var ctor = scope.getMethodForType(myEigenType, "Value");
+    assertThat("My_Type.type.Value is registered", ctor, is(notNullValue()));
+    assertThat(
+        "My_Type.Value is not registered",
+        scope.getMethodForType(myType, "Value"),
+        is(nullValue()));
+  }
+
+  @Test
+  public void fieldGetterIsRegistered_OnType() throws IOException {
+    var mainSrc =
+        Source.newBuilder(
+                LanguageInfo.ID,
+                """
+        type My_Type
+            Value x
+        """,
+                "test.enso")
+            .build();
+    var mainMod = ctxRule.eval(mainSrc);
+    var mainRuntimeMod = (Module) ctxRule.unwrapValue(mainMod);
+    var scope = mainRuntimeMod.getScope();
+    var myType = scope.getType("My_Type", true);
+    var fieldGetter = scope.getMethodForType(myType, "x");
+    assertThat("My_Type.x is registered", fieldGetter, is(notNullValue()));
+    var myEigenType = myType.getEigentype();
+    assertThat(
+        "My_Type.type.x is not registered",
+        scope.getMethodForType(myEigenType, "x"),
+        is(nullValue()));
   }
 
   @Test
@@ -133,6 +237,72 @@ public class ModuleScopeTest {
       var myType = runtimeConcreteMod.getScope().getType("My_Type", true);
       var staticMethod = runtimeConcreteMod.getScope().getMethodForType(myType, "static_method");
       assertThat(staticMethod, is(notNullValue()));
+    }
+  }
+
+  /**
+   * See <a href="https://github.com/enso-org/enso/issues/11686">#11686</a>
+   *
+   * @throws IOException
+   */
+  @Test
+  public void methodsAreNotDuplicated() throws IOException {
+    var src =
+        Source.newBuilder(
+                LanguageInfo.ID,
+                """
+        type My_Type
+            Cons x
+            instance_method self = self.x
+            static_method = 42
+
+        My_Type.extension_instance_method self = self.x
+        My_Type.extension_static_method = 84
+        """,
+                "test.enso")
+            .build();
+    var mod = ctxRule.eval(src);
+    var runtimeMod = (Module) ctxRule.unwrapValue(mod);
+    var scope = runtimeMod.getScope();
+    var myType = scope.getType("My_Type", true);
+    var myEigenType = myType.getEigentype();
+    var moduleAssocType = scope.getAssociatedType();
+    // For all methods we check that they are present on just one of the
+    // types: myType, myEigenType and moduleAssocType.
+    var methodNamesToCheck =
+        List.of(
+            "instance_method",
+            "static_method",
+            "extension_instance_method",
+            "extension_static_method");
+    for (var methodName : methodNamesToCheck) {
+      var onType = scope.getMethodForType(myType, methodName);
+      var onEigenType = scope.getMethodForType(myEigenType, methodName);
+      var onAssocType = scope.getMethodForType(moduleAssocType, methodName);
+      var foundCount = 0;
+      if (onType != null) {
+        foundCount++;
+      }
+      if (onEigenType != null) {
+        foundCount++;
+      }
+      if (onAssocType != null) {
+        foundCount++;
+      }
+      assertThat(
+          "Method '"
+              + methodName
+              + "' is present on just one of the types. "
+              + "Found on My_Type: "
+              + (onType != null)
+              + ", "
+              + "found on My_Type's eigen type: "
+              + (onEigenType != null)
+              + ", "
+              + "found on module associated type: "
+              + (onAssocType != null),
+          foundCount,
+          is(1));
     }
   }
 }
