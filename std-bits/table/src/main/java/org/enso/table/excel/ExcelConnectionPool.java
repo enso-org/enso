35ad55d005b68bc45b61fba946beb51dcd1e8fca
Adam Riley <riley.ad@gmail.com>
2025-09-22 11:30:40 +0100

    Clean up

--- std-bits/table/src/main/java/org/enso/table/excel/ExcelConnectionPool.java
+++ std-bits/table/src/main/java/org/enso/table/excel/ExcelConnectionPool.java
@@ -8,9 +8,9 @@ import java.io.OutputStream;
 import java.nio.file.AccessMode;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.function.Function;
+
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.exceptions.OLE2NotOfficeXmlFileException;
@@ -53,28 +53,17 @@ public class ExcelConnectionPool implements ReloadDetector.HasClearableCache {
         throw new FileNotFoundException(file.toString());
       }
 
-      String key = getKeyForFile(file);
+      String key = getKeyForFile(file, format);
       ConnectionRecord existingRecord = records.get(key);
       if (existingRecord != null) {
         // Adapt the existing record
-        if (existingRecord.format != format) {
-          throw new ExcelFileFormatMismatchException(
-              "Requesting to open "
-                  + file
-                  + " as "
-                  + format
-                  + ", but it was "
-                  + "already opened as "
-                  + existingRecord.format
-                  + ".");
-        }
         return new ReadOnlyExcelConnection(this, key, existingRecord);
       } else {
         // Create the new record
         ConnectionRecord record = new ConnectionRecord();
         record.file = file;
         record.format = format;
-        record.reopen(true);
+        record.reopen();
         records.put(key, record);
         return new ReadOnlyExcelConnection(this, key, record);
       }
@@ -87,7 +76,7 @@ public class ExcelConnectionPool implements ReloadDetector.HasClearableCache {
           "Cannot close a Excel connection while an Excel file is being "
               + "written to. This is a bug in the Table library.");
     }
-    String key = getKeyForFile(file);
+    String key = getKeyForFile(file, format);
     ConnectionRecord existingRecord = records.get(key);
     if (existingRecord != null) {
       existingRecord.close();
@@ -169,8 +158,7 @@ public class ExcelConnectionPool implements ReloadDetector.HasClearableCache {
 
       isCurrentlyWriting = true;
       try {
-        String key = getKeyForFile(file);
-        ArrayList<ConnectionRecord> recordsToReopen = new ArrayList<>(1 + accompanyingFiles.length);
+        String key = getKeyForFile(file, format);
 
         try {
           // Close the existing connection, if any - to avoid the write operation failing due to the
@@ -178,17 +166,15 @@ public class ExcelConnectionPool implements ReloadDetector.HasClearableCache {
           ConnectionRecord existingRecord = records.get(key);
           if (existingRecord != null) {
             existingRecord.close();
-            recordsToReopen.add(existingRecord);
           }
 
           verifyIsWritable(file);
 
           for (File accompanyingFile : accompanyingFiles) {
-            String accompanyingKey = getKeyForFile(accompanyingFile);
+            String accompanyingKey = getKeyForFile(accompanyingFile, format);
             ConnectionRecord accompanyingRecord = records.get(accompanyingKey);
             if (accompanyingRecord != null) {
               accompanyingRecord.close();
-              recordsToReopen.add(accompanyingRecord);
             }
 
             verifyIsWritable(accompanyingFile);
@@ -197,10 +183,6 @@ public class ExcelConnectionPool implements ReloadDetector.HasClearableCache {
           WriteHelper helper = new WriteHelper(format);
           return action.apply(helper);
         } finally {
-          // Reopen the closed connections
-          for (ConnectionRecord record : recordsToReopen) {
-            record.reopen(false);
-          }
         }
 
       } finally {
@@ -220,8 +202,9 @@ public class ExcelConnectionPool implements ReloadDetector.HasClearableCache {
     path.getFileSystem().provider().checkAccess(path, AccessMode.WRITE, AccessMode.READ);
   }
 
-  private String getKeyForFile(File file) throws IOException {
-    return file.getCanonicalPath();
+  private String getKeyForFile(File file, ExcelFileFormat format) throws IOException {
+    String pathPart = file.getCanonicalPath();
+    return pathPart + "::" + format.name();
   }
 
   private final HashMap<String, ConnectionRecord> records = new HashMap<>();
@@ -271,44 +254,29 @@ public class ExcelConnectionPool implements ReloadDetector.HasClearableCache {
       }
     }
 
-    void reopen(boolean throwOnFailure) throws IOException, InterruptedException {
+    void reopen() throws IOException, InterruptedException {
       synchronized (this) {
         if (workbook != null) {
           throw new IllegalStateException("The workbook is already open.");
         }
 
-        try {
           try {
             workbook =
                 format == ExcelFileFormat.XLSX
                     ? new XSSFReaderWorkbook(file.getAbsolutePath())
                     : ExcelWorkbook.forPOIUserModel(openWorkbook(file, format, false));
-          } catch (OLE2NotOfficeXmlFileException e) {
-            throw new IOException(
-                "Invalid format encountered when opening the file " + file + " as " + format + ".",
-                e);
-          } catch (NotOLE2FileException e) {
+          } catch (OLE2NotOfficeXmlFileException | NotOLE2FileException e) {
             throw new IOException(
                 "Invalid format encountered when opening the file " + file + " as " + format + ".",
                 e);
           }
-        } catch (IOException e) {
-          initializationException = e;
-          if (throwOnFailure) {
-            throw e;
-          }
-        }
       }
     }
 
-    private ExcelWorkbook accessCurrentWorkbook() throws IOException {
+    private ExcelWorkbook accessCurrentWorkbook() throws IOException, InterruptedException {
       synchronized (this) {
         if (workbook == null) {
-          if (initializationException != null) {
-            throw initializationException;
-          } else {
-            throw new IllegalStateException("The workbook is used after being closed.");
-          }
+          reopen();
         }
 
         return workbook;
@@ -365,11 +333,5 @@ public class ExcelConnectionPool implements ReloadDetector.HasClearableCache {
       case XLS -> new HSSFWorkbook();
       case XLSX, XLSX_FALLBACK -> new SXSSFWorkbook();
     };
-  }
-
-  public static class ExcelFileFormatMismatchException extends IllegalArgumentException {
-    public ExcelFileFormatMismatchException(String message) {
-      super(message);
-    }
   }
 }
