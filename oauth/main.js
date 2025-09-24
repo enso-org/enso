eb911d0c935a9f4c2e6de01dff10e9ff81557c49
Gregory Travis <greg.travis@enso.org>
2025-09-24 15:11:26 -0400

    initial open

--- oauth/main.js
+++ oauth/main.js
@@ -4,6 +4,16 @@
     //const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
     //const open = require('open');
 
+
+    function make_get(baseUrl, parameters) {
+      const url = new URL(baseUrl);
+      for (const key in parameters) {
+        url.searchParams.append(key, parameters[key]);
+      }
+
+      return url.href;
+    }
+
     if (isMainThread) {
         console.log('Main thread started.');
 
@@ -12,7 +22,14 @@
             workerData: { limit: 1000000000 } // Data to pass to the worker
         });
 
-        open('https://cnn.com/');
+        open(make_get('https://login.microsoftonline.com/557a086b-ff83-4d39-a7d4-3cedd3e30b8c/oauth2/v2.0/authorize', {
+          'client_id': '225e3188-e3ec-4613-b8a5-4e0efac1694a',
+          'response_type': 'code',
+          'redirect_uri': 'https://ensoanalytics.com/msoauthtest',
+          'response_mode': 'query',
+          'scope': 'https://graph.microsoft.com/mail.read',
+          'state': '12345'
+        }));
 
         // Listen for messages from the worker thread
         worker.on('message', (result) => {
