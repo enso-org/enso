5646c0d9e6f08d4efec0d417e085c3de99435414
Gregory Travis <greg.travis@enso.org>
2025-09-25 15:06:17 -0400

    async

--- oauth/main.js
+++ oauth/main.js
@@ -1,11 +1,12 @@
-    // main.js
-    import open from "open";
-    import { Worker, isMainThread, parentPort, workerData } from 'worker_threads';
-    import https from 'https';
-    import axios from 'axios';
-    //const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
-    //const open = require('open');
+// main.js
+import open from "open";
+import { Worker, isMainThread, parentPort, workerData } from 'worker_threads';
+import https from 'https';
+import axios from 'axios';
+//const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+//const open = require('open');
 
+(async () => {
     const client_secret = process.env.OAUTH_CLIENT_SECRET;
 
     function make_get(baseUrl, parameters) {
@@ -17,79 +18,26 @@
       return url.href;
     }
 
-    function get_auth_token(auth_code) {
+    async function get_access_token(auth_code) {
+      const tokenEndpoint = 'https://login.microsoftonline.com/557a086b-ff83-4d39-a7d4-3cedd3e30b8c/oauth2/v2.0/token';
+      //const tokenEndpoint = 'https://login.microsoftonline.com/59c2b5a8-8575-4ce0-9ff5-be8f2b34ad63/oauth2/v2.0/token';
 
-      if (true) {
-        console.log("==== axios");
-        const tokenEndpoint = 'https://login.microsoftonline.com/557a086b-ff83-4d39-a7d4-3cedd3e30b8c/oauth2/v2.0/token';
-        //const tokenEndpoint = 'https://login.microsoftonline.com/59c2b5a8-8575-4ce0-9ff5-be8f2b34ad63/oauth2/v2.0/token';
+      const data = new URLSearchParams();
 
-        const data = new URLSearchParams();
+      data.append('client_id', '225e3188-e3ec-4613-b8a5-4e0efac1694a');
+      //data.append('client_id', '087cad1c-ab83-476d-bb1b-47ed1c5be4ef');
+      data.append('scope', 'openid offline_access https://graph.microsoft.com/mail.read');
+      data.append('code', auth_code);
+      data.append('redirect_uri', 'http://localhost:3000');
+      data.append('grant_type', 'authorization_code');
+      data.append('client_secret', client_secret);
 
-        data.append('client_id', '225e3188-e3ec-4613-b8a5-4e0efac1694a');
-        //data.append('client_id', '087cad1c-ab83-476d-bb1b-47ed1c5be4ef');
-        data.append('scope', 'openid offline_access https://graph.microsoft.com/mail.read');
-        data.append('code', auth_code);
-        data.append('redirect_uri', 'http://localhost:3000');
-        data.append('grant_type', 'authorization_code');
-        data.append('client_secret', client_secret);
-
-        axios.post(tokenEndpoint, data, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        })
-        .then(response => {
-            console.log('Access Token:', response.data.access_token);
-        })
-        .catch(error => {
-            console.error('Error:', error.response.data);
-        });
-        console.log("==== axios done");
-      }
-
-      if (false) {
-        console.log("==== mine");
-        const data = JSON.stringify({
-          'client_id': '225e3188-e3ec-4613-b8a5-4e0efac1694a',
-          //'client_id': '087cad1c-ab83-476d-bb1b-47ed1c5be4ef',
-          'scope': 'openid offline_access https://graph.microsoft.com/mail.read',
-          'code': auth_code,
-          'redirect_uri': 'https://ensoanalytics.com/msoauthtest',
-          'grant_type': 'authorization_code',
-          'client_secret': client_secret,
-        });
-        console.log("data " + data);
-
-        const options = {
-          hostname: 'login.microsoftonline.com',
-          path: '/557a086b-ff83-4d39-a7d4-3cedd3e30b8c/oauth2/v2.0/token',
-          //path: '/59c2b5a8-8575-4ce0-9ff5-be8f2b34ad63/oauth2/v2.0/token',
-          port: 443, // 80 for HTTP
-          method: 'POST',
+      const result = await axios.post(tokenEndpoint, data, {
           headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': data.length
-            //'Content-Type': 'application/x-www-form-urlencoded'
+              'Content-Type': 'application/x-www-form-urlencoded'
           }
-        };
-
-        const req = https.request(options, (res) => {
-          console.log(`auth token request statusCode: ${res.statusCode}`);
-
-          res.on('data', (d) => {
-            process.stdout.write(d);
-            console.log(d);
-          });
-        });
-
-        req.on('error', (error) => {
-          console.error(error);
-        });
-
-        req.write(data);
-        req.end();
-      }
+      });
+      return result.data.access_token;
     }
 
     if (isMainThread) {
@@ -112,11 +60,12 @@
         }));
 
         // Listen for messages from the worker thread
-        worker.on('message', (result) => {
+        worker.on('message', async (result) => {
             console.log('Result from worker:', result);
             const { auth_code } = result;
             console.log('auth_code ' + auth_code);
-            const auth_token = get_auth_token(auth_code);
+            const access_token = await get_access_token(auth_code);
+            console.log('access_token: ' + access_token);
         });
 
         // Listen for errors from the worker thread
@@ -140,3 +89,5 @@
         // (though in this example, it's not designed to be)
         console.log('This code should not be executed in a worker context for this example.');
     }
+})()
+  .catch(err => console.log('Fatal error', err));
