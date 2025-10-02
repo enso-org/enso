7b2abdd533fc987ef2ad4dffb8574ae3d5280777
somebody1234 <ehern.lee@gmail.com>
2025-10-03 00:02:53 +1000

    Address CR

--- app/gui/src/dashboard/services/RemoteBackend.ts
+++ app/gui/src/dashboard/services/RemoteBackend.ts
@@ -1080,12 +1080,11 @@ export default class RemoteBackend extends Backend {
    */
   override async listTags(): Promise<readonly backend.Label[]> {
     const path = remoteBackendPaths.LIST_TAGS_PATH
-    const response = await this.get<backend.Label[] | backend.ListTagsResponseBody>(path)
+    const response = await this.get<backend.ListTagsResponseBody>(path)
     if (!response.ok) {
       return await this.throw(response, 'listLabelsBackendError')
     } else {
-      const result = await response.json()
-      return Array.isArray(result) ? result : result.tags
+      return (await response.json()).tags
     }
   }
 
