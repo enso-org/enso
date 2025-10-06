f61d25e2dcb36678d814d0d59bd12163c344bdd7
Ilya Bogdanov <fumlead@gmail.com>
2025-10-06 11:22:18 +0400

    Using only disable-dev-shm-usage

--- app/electron-client/src/index.ts
+++ app/electron-client/src/index.ts
@@ -257,11 +257,11 @@ class App {
     // Needed to accept localhost self-signed cert
     electron.app.commandLine.appendSwitch('ignore-certificate-errors')
     // TODO: remove
-    electron.app.commandLine.appendSwitch('disable-gpu')
-    electron.app.commandLine.appendSwitch('use-gl', 'swiftshader')
-    electron.app.commandLine.appendSwitch('enable-unsafe-swiftshader')
-    electron.app.commandLine.appendSwitch('ozone-platform', 'x11')
-    // electron.app.commandLine.appendSwitch('disable-dev-shm-usage')
+    // electron.app.commandLine.appendSwitch('disable-gpu')
+    // electron.app.commandLine.appendSwitch('use-gl', 'swiftshader')
+    // electron.app.commandLine.appendSwitch('enable-unsafe-swiftshader')
+    // electron.app.commandLine.appendSwitch('ozone-platform', 'x11')
+    electron.app.commandLine.appendSwitch('disable-dev-shm-usage')
     // electron.app.commandLine.appendSwitch('enable-logging', 'stderr')
     // electron.app.commandLine.appendSwitch('v', '1')
     // electron.app.commandLine.appendSwitch('vmodule', 'renderer=2,gpu=2,viz=2')
