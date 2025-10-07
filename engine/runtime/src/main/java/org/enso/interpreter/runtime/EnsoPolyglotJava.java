596e84e8e7865450d9180a8bb49845f8c8146463
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-10-07 17:14:47 +0200

    Using OtherJvm support is no error!

--- engine/runtime/src/main/java/org/enso/interpreter/runtime/EnsoPolyglotJava.java
+++ engine/runtime/src/main/java/org/enso/interpreter/runtime/EnsoPolyglotJava.java
@@ -215,7 +215,7 @@ final class EnsoPolyglotJava {
     } else {
       var envJava = System.getenv("ENSO_JAVA");
       if (envJava == null) {
-        logger.log(Level.ERROR, "Using experimental OtherJvm support!");
+        logger.log(Level.INFO, "Initializing OtherJvm support!");
         var src = Source.newBuilder("epb", "java:0#guest", "<Bindings>").build();
         var target = ctx.parseInternal(src);
         return target.call();
