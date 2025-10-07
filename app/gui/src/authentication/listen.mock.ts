98f640c025cd86dcc50ac400dee5d5b0b745dd23
somebody1234 <ehern.lee@gmail.com>
2025-10-08 01:21:25 +1000

    Fix CI error

--- app/gui/src/authentication/listen.mock.ts
+++ app/gui/src/authentication/listen.mock.ts
@@ -1,5 +1,22 @@
 /** @file */
-export type AuthEvent = 'customOAuthState' | 'cognitoHostedUI' | 'signIn' | 'signOut'
+
+/**
+ * Authentication state change events.
+ *
+ * These are issues by AWS Amplify when it detects a change in authentication state. For example,
+ * when the user signs in or signs out by accessing a page like `enso://auth?code=...&state=...`.
+ */
+export type AuthEvent = (typeof AuthEvent)[keyof typeof AuthEvent]
+export const AuthEvent = {
+  /** Issued when the user has passed custom OAuth state parameters to some other auth event. */
+  customOAuthState: 'customOAuthState',
+  /** Issued when the user completes the sign-in process (via federated identity provider). */
+  cognitoHostedUi: 'cognitoHostedUI',
+  /** Issued when the user completes the sign-in process (via email/password). */
+  signIn: 'signIn',
+  /** Issued when the user signs out. */
+  signOut: 'signOut',
+} as const
 
 export let authEventListener: ((event: AuthEvent, data?: unknown) => void) | null
 
