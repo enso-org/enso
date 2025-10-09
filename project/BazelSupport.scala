adc8f2531f2519465e0d65e0d2197b15122b71fd
Pavel Marek <pavel.marek@enso.org>
2025-10-09 20:27:10 +0200

    No need for smallJdkDir prop.
    It will be handled via outDir

--- project/BazelSupport.scala
+++ project/BazelSupport.scala
@@ -21,7 +21,6 @@ object BazelSupport extends AutoPlugin {
   val RUST_PARSER_JAVA_SRC_DIR_PROP   = "enso.BazelSupport.parser.javaSrcDir"
   val RUST_PARSER_LIB_PROP            = "enso.BazelSupport.parser.lib"
   val EXTRACTED_PYTHON_RESOURCES_PROP = "enso.BazelSupport.python.resourceDir"
-  val SMALL_JDK_DIR_PROP              = "enso.BazelSupport.smallJdkDir"
 
   object autoImport {
     lazy val wasStartedFromBazel = settingKey[Boolean](
@@ -50,9 +49,6 @@ object BazelSupport extends AutoPlugin {
     )
     lazy val extractedPythonResourceDir = taskKey[File](
       "Directory containing extracted Python resources"
-    )
-    lazy val smallJdkDir = settingKey[Option[File]](
-      "Directory containing the small JDK output"
     )
     lazy val Bazel = config("Bazel")
   }
@@ -153,18 +149,6 @@ object BazelSupport extends AutoPlugin {
         }
         dir
       },
-      Bazel / smallJdkDir := {
-        val prop = System.getProperty(SMALL_JDK_DIR_PROP)
-        if (prop == null) {
-          None
-        } else {
-          val dir = new File(prop)
-          if (!dir.exists()) {
-            IO.createDirectory(dir)
-          }
-          Some(dir)
-        }
-      }
     )
   }
 }
