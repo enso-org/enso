e33c952a548b39337b13030f657e971b9ca6f055
Frizi <frizi09@gmail.com>
2025-10-13 17:17:27 +0200

    move isAuthenticated promise into the setup

--- app/gui/integration-test/setup.ts
+++ app/gui/integration-test/setup.ts
@@ -3,21 +3,17 @@ import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { test as setup } from './base'
 
-/** Get the path to the auth file. */
-function getAuthFilePath() {
-  const __dirname = dirname(fileURLToPath(import.meta.url))
-  return join(__dirname, '../playwright/.auth/user.json')
-}
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const authFilePath = join(__dirname, '../playwright/.auth/user.json')
 
-setup.describe(async () => {
-  const authFilePath = getAuthFilePath()
+setup('authenticate', async ({ page, loginPage }) => {
   const isAuthenticated = await access(authFilePath, constants.R_OK).then(
     () => true,
     () => false,
   )
-  setup.skip(isAuthenticated, 'Already authenticated')
-  setup('authenticate', async ({ page, loginPage }) => {
+
+  if (!isAuthenticated) {
     await loginPage.login()
     await page.context().storageState({ path: authFilePath })
-  })
+  }
 })
