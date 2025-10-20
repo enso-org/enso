2f301f94815882c3ee4411495c6e09356f0be18f
Pavel Marek <pavel.marek@enso.org>
2025-10-20 16:14:54 +0200

    Fix argument for StaticInvokeMethodNode ctor

--- engine/runtime/src/main/java/org/enso/interpreter/node/callable/InvokeMethodNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/callable/InvokeMethodNode.java
@@ -48,7 +48,7 @@ public abstract class InvokeMethodNode extends BaseNode {
       boolean onBoundary) {
     if (isStaticMethodInvocation(schema)) {
       return StaticInvokeMethodNodeGen.create(
-          schema, defaultsExecutionMode, argumentsExecutionMode, thisArgumentPosition, onBoundary);
+          schema, defaultsExecutionMode, argumentsExecutionMode, namedSelfArgPosition(schema), onBoundary);
     } else {
       return InstanceInvokeMethodNodeGen.create(
           schema, defaultsExecutionMode, argumentsExecutionMode, thisArgumentPosition, onBoundary);
