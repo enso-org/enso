ca275c6aaed84a1cdfc23d58762d2a06481bc38a
Ilya Bogdanov <fumlead@gmail.com>
2025-10-21 17:44:31 +0400

    Removing project-manager environment variable and refactoring

--- app/electron-client/esbuild.config.mjs
+++ app/electron-client/esbuild.config.mjs
@@ -1,21 +1,12 @@
-/** @file Esbuild config file. */
+/** @file Esbuild config for building the electron client. */
 import { fileURLToPath } from 'node:url'
 
 import { wasmLoader } from 'esbuild-plugin-wasm'
 
 export default {
-  format: 'esm',
-  platform: 'node',
   outExtension: { '.js': '.mjs' },
   plugins: [wasmLoader()],
-  target: ['node20'], // electron31
   alias: {
     '@': fileURLToPath(new URL('./src', import.meta.url)),
-  },
-  define: {
-    'process.env.ELECTRON_DEV_MODE': 'false',
-    'process.env.GUI_CONFIG_PATH': '"../gui/vite.config.ts"',
-    'process.env.ENSO_IDE_VERSION': '"2025.0.0-dev"',
-    'process.env.ENSO_IDE_COMMIT_HASH': '"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"',
   },
 }
--- app/electron-client/esbuildConfig.ts
+++ app/electron-client/esbuildConfig.ts
@@ -12,24 +12,14 @@ import * as paths from './paths'
  *
  * The following environment variables are required:
  * - `ENSO_BUILD_IDE` - output directory for bundled client files;
- * - `ENSO_BUILD_PROJECT_MANAGER_IN_BUNDLE_PATH` - path to the project manager executable relative
- * to the PM bundle root;
  * @see bundlerOptions
  */
 export function bundlerOptionsFromEnv(devMode = false): esbuild.BuildOptions {
-  return bundlerOptions(
-    path.join(paths.getIdeDirectory(), 'client'),
-    paths.getProjectManagerInBundlePath(),
-    devMode,
-  )
+  return bundlerOptions(path.join(paths.getIdeDirectory(), 'client'), devMode)
 }
 
 /** Get options without relying on the environment. */
-export function bundlerOptions(
-  outdir: string,
-  projectManagerInBundlePath: string,
-  devMode = false,
-): esbuild.BuildOptions {
+export function bundlerOptions(outdir: string, devMode = false): esbuild.BuildOptions {
   return {
     bundle: true,
     outdir,
@@ -44,9 +34,7 @@ export function bundlerOptions(
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
     define: {
-      'process.env.PROJECT_MANAGER_IN_BUNDLE_PATH': JSON.stringify(projectManagerInBundlePath),
       'process.env.ELECTRON_DEV_MODE': JSON.stringify(String(devMode)),
-      'process.env.GUI_CONFIG_PATH': JSON.stringify(path.resolve('../gui/vite.config.ts')),
       'process.env.ENSO_IDE_VERSION': JSON.stringify(process.env.ENSO_IDE_VERSION),
       'process.env.ENSO_IDE_COMMIT_HASH': JSON.stringify(process.env.ENSO_IDE_COMMIT_HASH),
     },
