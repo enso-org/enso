e112643fa78da7f07325fab8d3d91be04bb98750
Ilya Bogdanov <fumlead@gmail.com>
2025-10-29 00:51:04 +0400

    Update setup-bazel action

--- build_tools/ci_utils/src/actions/workflow/definition.rs
+++ build_tools/ci_utils/src/actions/workflow/definition.rs
@@ -94,7 +94,7 @@ pub fn setup_bazel_env() -> Step {
 pub fn setup_bazel() -> Step {
     Step {
         name: Some("Setup bazel environment".into()),
-        uses: Some("bazel-contrib/setup-bazel@0.13.0".into()),
+        uses: Some("bazel-contrib/setup-bazel@0.15.0".into()),
         with: Some(step::Argument::Other(BTreeMap::from([
             (
                 "output-base".to_string(),
