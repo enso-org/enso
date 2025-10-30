d0a4c5166be4aead5cf3597070bfb8e7f60355d4
Pavel Marek <pavel.marek@enso.org>
2025-10-30 17:17:56 +0100

    fix indexes in RuntimeServerTest

--- engine/runtime-integration-tests/src/test/scala/org/enso/interpreter/test/instrument/RuntimeServerTest.scala
+++ engine/runtime-integration-tests/src/test/scala/org/enso/interpreter/test/instrument/RuntimeServerTest.scala
@@ -2341,7 +2341,7 @@ class RuntimeServerTest
     val moduleName = "Enso_Test.Test.Main"
 
     val metadata = new Metadata("import Standard.Base.Data.Numbers\n\n")
-    val id_x1_1  = metadata.addItem(30, 7, "aa")
+    val id_x1_1  = metadata.addItem(30, 14, "aa")
     val id_x1_2  = metadata.addItem(49, 10, "ab")
     val id_x1    = metadata.addItem(69, 6, "ac")
 
