25314c5ac7ead8d66bca108d08b8dd59e7f22ca6
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-10-31 17:46:07 +0100

    = null no longer needed

--- engine/runtime-integration-tests/src/test/java/org/enso/compiler/test/ExportedSymbolsTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/compiler/test/ExportedSymbolsTest.java
@@ -54,7 +54,6 @@ public class ExportedSymbolsTest {
       assertThat(mainExportedSymbols.containsKey("A_Type"), is(true));
       assertThat(
           mainExportedSymbols.get("A_Type").get(0), instanceOf(BindingsMap.ResolvedType.class));
-      mainExportedSymbols = null;
     }
   }
 
@@ -79,7 +78,6 @@ public class ExportedSymbolsTest {
       var mainExportedSymbols = getExportedSymbolsFromModule(ctx, "local.Proj.Main");
       assertThat(mainExportedSymbols.size(), is(2));
       assertThat(mainExportedSymbols.keySet(), containsInAnyOrder("A_Type", "B_Type"));
-      mainExportedSymbols = null;
     }
   }
 
@@ -104,7 +102,6 @@ public class ExportedSymbolsTest {
       var mainExportedSymbols = getExportedSymbolsFromModule(ctx, "local.Proj.Main");
       assertThat(mainExportedSymbols.size(), is(2));
       assertThat(mainExportedSymbols.keySet(), containsInAnyOrder("A_Type", "B_Type"));
-      mainExportedSymbols = null;
     }
   }
 
@@ -128,7 +125,6 @@ public class ExportedSymbolsTest {
       var mainExportedSymbols = getExportedSymbolsFromModule(ctx, "local.Proj.Main");
       assertThat(mainExportedSymbols.size(), is(1));
       assertThat(mainExportedSymbols.keySet(), containsInAnyOrder("Foo"));
-      mainExportedSymbols = null;
     }
   }
 
@@ -156,7 +152,6 @@ public class ExportedSymbolsTest {
           "Just a A_Module submodule should be exported",
           syntheticModExpSymbols,
           hasKey("A_Module"));
-      syntheticModExpSymbols = null;
     }
   }
 
@@ -184,7 +179,6 @@ public class ExportedSymbolsTest {
       assertThat(
           mainExportedSymbols.get("A_Module").get(0),
           is(instanceOf(BindingsMap.ResolvedType.class)));
-      mainExportedSymbols = null;
     }
   }
 
@@ -212,7 +206,6 @@ public class ExportedSymbolsTest {
       assertThat(
           mainExportedSymbols.get("A_Module").get(0),
           is(instanceOf(BindingsMap.ResolvedModule.class)));
-      mainExportedSymbols = null;
     }
   }
 
@@ -240,7 +233,6 @@ public class ExportedSymbolsTest {
       assertThat(
           mainExportedSymbols.get("Synthetic_Module").get(0),
           is(instanceOf(BindingsMap.ResolvedModule.class)));
-      mainExportedSymbols = null;
     }
   }
 
