429e24fa0592b175e3c2b1b8c8144d6b190ad36b
Dmitry Bushev <bushevdv@gmail.com>
2025-11-12 18:24:03 +0000

    update: enso runner environment

--- engine/language-server/src/main/scala/org/enso/languageserver/boot/config/ApplicationConfig.scala
+++ engine/language-server/src/main/scala/org/enso/languageserver/boot/config/ApplicationConfig.scala
@@ -12,7 +12,7 @@ object ApplicationConfig {
   private val ConfigNamespace = "language-server"
 
   /** Load the configuration from the config file. */
-  private def loadConfig(): ApplicationConfig = {
+  def load(): ApplicationConfig = {
     val contextClassLoader = Thread.currentThread().getContextClassLoader
     try {
       Thread.currentThread().setContextClassLoader(getClass.getClassLoader)
@@ -22,26 +22,6 @@ object ApplicationConfig {
         .at(ConfigNamespace)
         .loadOrThrow[ApplicationConfig]
     } finally Thread.currentThread().setContextClassLoader(contextClassLoader)
-  }
-
-  /** Override the configuration with environment variables.
-    *
-    * This is a workaround for the issue that the standard config overloading
-    * does not work in the native image.
-    */
-  private def overrideConfig(config: ApplicationConfig): ApplicationConfig = {
-    val ydocHostname =
-      sys.env.getOrElse("LANGUAGE_SERVER_YDOC_HOSTNAME", config.ydoc.hostname)
-    val ydocPort = sys.env
-      .get("LANGUAGE_SERVER_YDOC_PORT")
-      .map(_.toInt)
-      .getOrElse(config.ydoc.port)
-
-    config.copy(ydoc = YdocConfig(hostname = ydocHostname, port = ydocPort))
-  }
-
-  def load(): ApplicationConfig = {
-    overrideConfig(loadConfig())
   }
 
 }
