38294ef1be732416b34be9fe93ef6105d2dc37e0
Adam Obuchowicz <adam.obuchowicz@enso.org>
2025-11-19 12:01:29 +0100

    Remove debug

--- app/gui/src/project-view/components/DocumentationEditor/DocumentationEditor.vue
+++ app/gui/src/project-view/components/DocumentationEditor/DocumentationEditor.vue
@@ -2,13 +2,11 @@
 import WithCurrentProject from '$/components/WithCurrentProject.vue'
 import { useRightPanelData } from '$/providers/rightPanel'
 import { providePopoverRoot } from '@/providers/popoverRoot'
-import { useTemplateRef, watchEffect } from 'vue'
+import { useTemplateRef } from 'vue'
 import ClosedProjectDocumentationEditor from './ClosedProjectDocumentationEditor.vue'
 import OpenedProjectDocumentationEditor from './OpenedProjectDocumentationEditor.vue'
 
 const rightPanel = useRightPanelData()
-
-watchEffect(() => console.debug('focused project', rightPanel.focusedProject))
 
 const rootElement = useTemplateRef('rootElement')
 providePopoverRoot(rootElement)
