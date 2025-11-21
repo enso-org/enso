ef0bf59f6ad62cd1e8261f1e800ff03a6e4a3857
Ilya Bogdanov <fumlead@gmail.com>
2025-11-21 20:23:30 +0400

    Fix run.cmd script

--- run.cmd
+++ run.cmd
@@ -1,13 +1,13 @@
-@ pushd %~dp0
-@ where bazel >nul 2>nul
-@ if not errorlevel 1 (
-@     bazel build //build_tools/cli:enso_build_cli_bin
-@     if errorlevel 1 goto end
-@     bazel-bin\build_tools\cli\enso_build_cli_bin %*
-@ ) else (
-@     cargo run -p enso-build-cli -- %*
-@ )
-@ set EXITCODE=%ERRORLEVEL%
+@pushd %~dp0
+@where bazel >nul 2>nul
+@if not errorlevel 1 (
+@    bazel build //build_tools/cli:enso_build_cli_bin
+@    if errorlevel 1 goto end
+@    bazel-bin\build_tools\cli\enso_build_cli_bin %*
+) else (
+@    cargo run -p enso-build-cli -- %*
+)
+@set EXITCODE=%ERRORLEVEL%
 :end
-@ popd
-@ exit /b %EXITCODE%
+@popd
+@exit /b %EXITCODE%
