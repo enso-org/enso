9fb203edb22bbe96619f6ba53479e04912e97b4a
Gregory Travis <greg.travis@enso.org>
2025-11-25 13:20:20 -0500

    cleanup

--- build_tools/cli/src/lib.rs
+++ build_tools/cli/src/lib.rs
@@ -418,7 +418,17 @@ impl Processor {
                         Tests::StdCloudRelated => {
                             config.test_standard_library =
                                 Some(StandardLibraryTestsSelection::whitelist(vec![
-                                    "Microsoft_Tests".to_string(),
+                                    "Base_Tests".to_string(),
+                                    // Base Internal tests contain some cloud tests that need
+                                    // access to cloud internals
+                                    "Base_Internal_Tests".to_string(),
+                                    // Table tests check integration of e.g. Postgres datalinks
+                                    "Table_Tests".to_string(),
+                                    // AWS tests check copying between Cloud and S3
+                                    "AWS_Tests".to_string(),
+                                    // Image tests check interaction between Image read/write and
+                                    // datalinks
+                                    "Image_Tests".to_string(),
                                 ]));
                             config.use_native_runner = true;
                         }
