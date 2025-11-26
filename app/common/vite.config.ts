b125eacf3947c179e6005b1cd6254072672e5080
somebody1234 <ehern.lee@gmail.com>
2025-11-26 19:56:26 +1000

    Remove obsolete TODO

--- app/common/vite.config.ts
+++ app/common/vite.config.ts
@@ -7,7 +7,6 @@ import dts from 'vite-plugin-dts'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [dts()],
-  // TODO: vite entrypoint should be */*.ts
   build: {
     lib: {
       entry: Object.fromEntries(
