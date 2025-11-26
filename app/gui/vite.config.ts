d6f4f70e699ceded46cf4d9da23231c6bbf79c2e
somebody1234 <ehern.lee@gmail.com>
2025-11-26 20:06:00 +1000

    Re-inline `await`ed Vite plugins

--- app/gui/vite.config.ts
+++ app/gui/vite.config.ts
@@ -21,26 +21,20 @@ if (isDevMode) {
 // Used by vite middleware inside devtools plugin. Specifying this by an option doesn't work when `componentInspector` is false.
 process.env.LAUNCH_EDITOR ??= 'code'
 
-// These can be inlined, but are refactored out because `defineConfig` (understandably)
-// does not support async functions.
-const vueDevtoolsVitePlugin =
-  isDevMode ?
-    [
-      await VueDevTools({
-        // The JSX transform used by the inspector is causing react to complain and adds significant load time.
-        componentInspector: false,
-      }),
-    ]
-  : []
-const pmShimVitePlugin = process.env.DASHBOARD_TESTS !== 'true' ? [await projectManagerShim()] : []
-
 // https://vitejs.dev/config/
 export default defineConfig({
   ...(IS_ELECTRON_DEV_MODE ? { root: fileURLToPath(new URL('.', import.meta.url)) } : {}),
   cacheDir: fileURLToPath(new URL('../../node_modules/.cache/vite', import.meta.url)),
   plugins: [
     wasm(),
-    ...vueDevtoolsVitePlugin,
+    ...(isDevMode ?
+      [
+        await VueDevTools({
+          // The JSX transform used by the inspector is causing react to complain and adds significant load time.
+          componentInspector: false,
+        }),
+      ]
+    : []),
     vue({
       customElement: ['**/components/visualizations/**', '**/components/shared/**'],
       template: {
@@ -56,7 +50,7 @@ export default defineConfig({
         fileURLToPath(new URL('./src/dashboard/**/*Hooks.ts', import.meta.url)),
       ],
     }),
-    ...pmShimVitePlugin,
+    ...(process.env.DASHBOARD_TESTS !== 'true' ? [await projectManagerShim()] : []),
     ...((
       process.env.SENTRY_AUTH_TOKEN != null &&
       process.env.ENSO_IDE_SENTRY_ORGANIZATION != null &&
