584214918d4a3760f8530dc973b927ee4a9e045d
Pavel Marek <pavel.marek@enso.org>
2025-11-27 11:36:55 +0100

    Remove unnecessary modifiers from interface

--- engine/runtime-parser/src/main/java/org/enso/compiler/core/ConstantsNames.java
+++ engine/runtime-parser/src/main/java/org/enso/compiler/core/ConstantsNames.java
@@ -1,8 +1,8 @@
 package org.enso.compiler.core;
 
 public interface ConstantsNames {
-  public static final String SELF_ARGUMENT = "self";
-  public static final String SELF_TYPE_ARGUMENT = "Self";
-  public static final String THAT_ARGUMENT = "that";
-  public static final String FROM_MEMBER = "from";
+  String SELF_ARGUMENT = "self";
+  String SELF_TYPE_ARGUMENT = "Self";
+  String THAT_ARGUMENT = "that";
+  String FROM_MEMBER = "from";
 }
