519f89298f1c72c7a825816b8ff2589cd0149aa3
Pavel Marek <pavel.marek@enso.org>
2025-11-28 20:27:43 +0100

    Minify project-manager sbt setting

--- lib/scala/project-manager/src/main/java/org/enso/projectmanager/EmptyProjectManager.java
+++ lib/scala/project-manager/src/main/java/org/enso/projectmanager/EmptyProjectManager.java
@@ -1,8 +1,8 @@
 package org.enso.projectmanager;
 
-/**
- * Just an empty class for now to satisfy all the build tools.
- * Will be removed soon.
- */
+/** Just an empty class for now to satisfy all the build tools. Will be removed soon. */
 public final class EmptyProjectManager {
+  public static void main(String[] args) {
+    // Nop
+  }
 }
