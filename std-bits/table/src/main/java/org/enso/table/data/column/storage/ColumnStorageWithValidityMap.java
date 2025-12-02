c2676e8b8cbab539e6d3dd271c78a4167751e7bd
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-12-02 17:54:27 +0100

    Rewording comment
    Co-authored-by: Pavel Marek <pavel.marek@enso.org>

--- std-bits/table/src/main/java/org/enso/table/data/column/storage/ColumnStorageWithValidityMap.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/storage/ColumnStorageWithValidityMap.java
@@ -4,7 +4,7 @@ import java.util.BitSet;
 
 public interface ColumnStorageWithValidityMap {
   /**
-   * Gets the isNothing map for the storage.
+   * Gets the validity map for the storage.
    *
    * @return bit set with {@code false} at null indexes and {@code true} at non-null indexes
    */
