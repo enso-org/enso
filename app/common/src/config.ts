a4a9fcd01159a94491b8f770d56764c6f459cd3a
somebody1234 <ehern.lee@gmail.com>
2025-12-03 20:35:32 +1000

    Address Bazel comments

--- app/common/src/config.ts
+++ app/common/src/config.ts
@@ -1,5 +1,5 @@
 /**
- * @file This file defines a global environemnt config that can be used throughout the app.
+ * @file This file defines a global environment config that can be used throughout the app.
  * It is included directly into index.html and kept as a separate built artifact, so that
  * we can easily replace its contents in a separate build postprocessing step in `BUILD.bazel`.
  */
