a1d07e5e9cd8b3955607bc0d7961b595c69d4490
Pavel Marek <pavel.marek@enso.org>
2025-12-04 15:06:37 +0100

    Revert "Ignore the failing Libraries test"
    This reverts commit 3126b0f7f8fd2a28ab77198564888cfdd0e2da79.

--- engine/language-server/src/test/scala/org/enso/languageserver/websocket/json/LibrariesTest.scala
+++ engine/language-server/src/test/scala/org/enso/languageserver/websocket/json/LibrariesTest.scala
@@ -588,9 +588,8 @@ class LibrariesTest extends BaseServerTest with ReportLogsOnFailure {
   }
 
   "library/preinstall" should {
-    // TODO [PM] Will be re-enabled ASAP - https://github.com/enso-org/enso/pull/14406
     "download the library sending progress notifications " +
-    "and correctly place it in cache" ignore {
+    "and correctly place it in cache" in {
       val client = getInitialisedWsClient()
 
       val repositoryPath = getTestDirectory.resolve("repository_path")
