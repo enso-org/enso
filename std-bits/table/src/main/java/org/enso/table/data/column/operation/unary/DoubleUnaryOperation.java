a30928c7fe528e45a1efca72f69f6e63dfa83292
James Dunkerley <jdunkerley@users.noreply.github.com>
2025-12-05 17:10:07 +0000

    WIP

--- std-bits/table/src/main/java/org/enso/table/data/column/operation/unary/DoubleUnaryOperation.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/operation/unary/DoubleUnaryOperation.java
@@ -11,9 +11,16 @@ public final class DoubleUnaryOperation extends NumericUnaryTypedOperation<Doubl
   public static UnaryOperation SIN_INSTANCE = new DoubleUnaryOperation("sin", Math::sin);
   public static UnaryOperation COS_INSTANCE = new DoubleUnaryOperation("cos", Math::cos);
   public static UnaryOperation TAN_INSTANCE = new DoubleUnaryOperation("tan", Math::tan);
+  public static UnaryOperation SINH_INSTANCE = new DoubleUnaryOperation("sinh", Math::sinh);
+  public static UnaryOperation COSH_INSTANCE = new DoubleUnaryOperation("cosh", Math::cosh);
+  public static UnaryOperation TANH_INSTANCE = new DoubleUnaryOperation("tanh", Math::tanh);
   public static UnaryOperation ASIN_INSTANCE = new DoubleUnaryOperation("asin", Math::asin);
   public static UnaryOperation ACOS_INSTANCE = new DoubleUnaryOperation("acos", Math::acos);
   public static UnaryOperation ATAN_INSTANCE = new DoubleUnaryOperation("atan", Math::atan);
+  public static UnaryOperation SQRT_INSTANCE = new DoubleUnaryOperation("sqrt", Math::sqrt);
+  public static UnaryOperation LN_INSTANCE = new DoubleUnaryOperation("ln", Math::log);
+  public static UnaryOperation EXP_INSTANCE = new DoubleUnaryOperation("exp", Math::exp);
+  public static UnaryOperation LOG10_INSTANCE = new DoubleUnaryOperation("log10", Math::log10);
 
   private final DoubleUnaryOperator function;
 
