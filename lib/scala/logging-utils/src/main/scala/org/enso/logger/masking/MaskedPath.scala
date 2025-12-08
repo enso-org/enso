f5c830fc76b582bf1c648adfff7032d5d68c38eb
Pavel Marek <pavel.marek@enso.org>
2025-12-08 13:24:59 +0100

    [WIP] Do not mask logs

--- lib/scala/logging-utils/src/main/scala/org/enso/logger/masking/MaskedPath.scala
+++ lib/scala/logging-utils/src/main/scala/org/enso/logger/masking/MaskedPath.scala
@@ -15,10 +15,6 @@ case class MaskedPath(value: Path) extends ToLogString {
   /** @inheritdoc */
   override def toLogString(shouldMask: Boolean): String = {
     val path = value.toAbsolutePath.normalize()
-    if (shouldMask) {
-      MaskingUtils.toMaskedPath(path)
-    } else {
-      path.toString
-    }
+    path.toString
   }
 }
