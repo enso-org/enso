551536b6ca7091f6ae49aa8547cf43354060bdcd
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-12-09 06:26:23 +0100

    Also add the BuilderUtil

---
+++ std-bits/table/src/main/java/org/enso/table/data/column/builder/BuilderUtil.java
@@ -0,0 +1,7 @@
+package org.enso.table.data.column.builder;
+
+final class BuilderUtil {
+  private BuilderUtil() {}
+
+  static final System.Logger LOG = System.getLogger(Builder.class.getPackage().getName());
+}
