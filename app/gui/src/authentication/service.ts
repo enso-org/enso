b89ba40d9c5f17d7b4d94bd16ad6cdd1f1149e98
somebody1234 <ehern.lee@gmail.com>
2025-12-10 20:38:38 +1000

    Attempt more proper fix for failing unit test

--- app/gui/src/authentication/service.ts
+++ app/gui/src/authentication/service.ts
@@ -171,12 +171,8 @@ function loadAmplifyConfig(
     supportsDeepLinks ? `${common.DEEP_LINK_SCHEME}://auth` : window.location.origin
   return {
     endpoint: $config.AUTH_ENDPOINT,
-    userPoolId:
-      process.env.NODE_ENV === 'test' ? 'mars_AAAAAAAAA' : ($config.COGNITO_USER_POOL_ID ?? ''),
-    userPoolWebClientId:
-      process.env.NODE_ENV === 'test' ?
-        'zzzzzzzzzzzzzzzzzzzzzzzzzz'
-      : ($config.COGNITO_USER_POOL_WEB_CLIENT_ID ?? ''),
+    userPoolId: $config.COGNITO_USER_POOL_ID ?? '',
+    userPoolWebClientId: $config.COGNITO_USER_POOL_WEB_CLIENT_ID ?? '',
     domain: $config.COGNITO_DOMAIN ?? '',
     region: $config.COGNITO_REGION ?? '',
     redirectSignIn: signInOutRedirect,
