c82c90ccb88b0cff7ca5e4dfbd29dc1c656f51e1
Hubert P <hubert.plociniczak@gmail.com>
2025-12-10 22:23:09 +0100

    nit

--- lib/scala/logging-service/src/main/java/org/enso/logging/service/LoggingServerConfig.java
+++ lib/scala/logging-service/src/main/java/org/enso/logging/service/LoggingServerConfig.java
@@ -4,9 +4,9 @@ import java.net.URI;
 import org.slf4j.event.Level;
 
 /**
- * Configuration for socket appenders where logs should be sent to
+ * Configuration for socket appenders where logs should be sent to.
  *
- * @param minLogLevel minimal required log level to be logged
+ * @param minLogLevel minimal required log level to be logged in socket appender
  * @param uri uri of the logging server
  */
 public record LoggingServerConfig(Level minLogLevel, URI uri) {}
