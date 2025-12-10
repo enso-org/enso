3bfd0bc928e10c385ed2b85d5ff558cdfca6117e
Pavel Marek <pavel.marek@enso.org>
2025-12-10 13:12:35 +0100

    Simplify isSameLong specialization

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/IsSameObjectNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/IsSameObjectNode.java
@@ -36,22 +36,9 @@ public abstract class IsSameObjectNode extends Node {
     }
   }
 
-  @Specialization(
-      guards = {
-        "interop.fitsInLong(left)",
-        "interop.fitsInLong(right)",
-      })
-  boolean isSameLong(
-      Object left,
-      Object right,
-      @Shared("interop") @CachedLibrary(limit = "2") InteropLibrary interop) {
-    try {
-      var leftLong = interop.asLong(left);
-      var rightLong = interop.asLong(right);
-      return leftLong == rightLong;
-    } catch (UnsupportedMessageException e) {
-      return false;
-    }
+  @Specialization
+  boolean isSameLong(long left, long right) {
+    return left == right;
   }
 
   @Specialization(
