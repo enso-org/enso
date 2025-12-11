f91354406c4ffa0ad62ab3fa8cf3366c001e1756
Adam Riley <riley.ad@gmail.com>
2025-12-11 09:29:54 +0000

    Revert "First attempt"
    This reverts commit c15965cb8dacc98d1264f220858010ae3e2006f8.

--- app/gui/src/dashboard/data/serviceCredentials/MS365CredentialsForm.tsx
+++ app/gui/src/dashboard/data/serviceCredentials/MS365CredentialsForm.tsx
@@ -4,54 +4,14 @@
  * Remember to ensure this component is added to `CREDENTIAL_INFOS` in `constants.ts`.
  */
 
-import { Button } from '#/components/Button'
 import { Checkbox } from '#/components/Checkbox'
 import { Form } from '#/components/Form'
 import { Input } from '#/components/Inputs/Input'
-import { Tooltip } from '#/components/Tooltip'
 import { useToastAndLog } from '#/hooks/toastAndLogHooks'
 import { useText } from '$/providers/react'
 import { CredentialsFormFooter } from './CredentialsFormFooter'
 import * as ms365 from './ms365'
 import type { CredentialFormProps } from './types'
-
-const MS365_SCOPE_OPTIONS = [
-  {
-    value: 'User.Read',
-    labelId: 'ms365CredentialUserReadScope',
-    descriptionId: 'ms365CredentialUserReadScopeDescription',
-    defaultSelected: true,
-  },
-  {
-    value: 'Files.ReadWrite.All',
-    labelId: 'ms365CredentialFilesReadWriteAllScope',
-    descriptionId: 'ms365CredentialFilesReadWriteAllScopeDescription',
-    defaultSelected: true,
-  },
-  {
-    value: 'Files.Read.All',
-    labelId: 'ms365CredentialFilesReadAllScope',
-    descriptionId: 'ms365CredentialFilesReadAllScopeDescription',
-    defaultSelected: false,
-  },
-  {
-    value: 'Files.ReadWrite',
-    labelId: 'ms365CredentialFilesReadWriteScope',
-    descriptionId: 'ms365CredentialFilesReadWriteScopeDescription',
-    defaultSelected: false,
-  },
-  {
-    value: 'Files.Read',
-    labelId: 'ms365CredentialFilesReadScope',
-    descriptionId: 'ms365CredentialFilesReadScopeDescription',
-    defaultSelected: false,
-  },
-] as const satisfies ReadonlyArray<{
-  readonly value: string
-  readonly labelId: string
-  readonly descriptionId: string
-  readonly defaultSelected: boolean
-}>
 
 /** Dialog for a MS365 credential. */
 export function MS365CredentialsForm(props: CredentialFormProps) {
@@ -64,9 +24,7 @@ export function MS365CredentialsForm(props: CredentialFormProps) {
       method="dialog"
       schema={ms365.FORM_SCHEMA}
       defaultValues={{
-        scopes: MS365_SCOPE_OPTIONS.filter(({ defaultSelected }) => defaultSelected).map(
-          ({ value }) => value,
-        ),
+        scopes: ['User.Read', 'Files.Read'],
       }}
       className="w-full"
       onSubmit={async (values) => {
@@ -81,25 +39,8 @@ export function MS365CredentialsForm(props: CredentialFormProps) {
         <>
           <Input form={form} name="name" label={getText('name')} defaultValue="Microsoft365" />
           <Checkbox.Group form={form} name="scopes" label={getText('ms365CredentialScopes')}>
-            {MS365_SCOPE_OPTIONS.map(({ value, labelId, descriptionId }) => {
-              const label = getText(labelId)
-              const description = getText(descriptionId)
-              return (
-                <div key={value} className="flex items-center gap-1">
-                  <Checkbox value={value}>{label}</Checkbox>
-                  <Tooltip.Trigger>
-                    <Button
-                      aria-label={getText('ms365CredentialScopeHelpAriaLabel')}
-                      size="xsmall"
-                      variant="icon"
-                      icon="help"
-                      tooltip={false}
-                    />
-                    <Tooltip>{description}</Tooltip>
-                  </Tooltip.Trigger>
-                </div>
-              )
-            })}
+            <Checkbox value="User.Read">{getText('ms365CredentialUserReadScope')}</Checkbox>
+            <Checkbox value="Files.Read">{getText('ms365CredentialFilesReadScope')}</Checkbox>
           </Checkbox.Group>
           <CredentialsFormFooter isCreating={true} canCancel={false} canReset={false} />
         </>
