437847bd5042eef09d1f97552e9d2c456dd89fe5
Pavel Marek <pavel.marek@enso.org>
2025-12-15 18:12:38 +0100

    Replace Literal.scala with Literal.java

---
+++ engine/runtime-parser/src/main/java/org/enso/compiler/core/ir/Literal.java
@@ -0,0 +1,66 @@
+package org.enso.compiler.core.ir;
+
+import org.enso.runtime.parser.dsl.GenerateFields;
+import org.enso.runtime.parser.dsl.GenerateIR;
+import org.enso.runtime.parser.dsl.IRField;
+import scala.Option;
+
+import java.util.function.Function;
+
+public interface Literal extends Expression, IRKind.Primitive {
+
+  @Override
+  Literal mapExpressions(Function<Expression, Expression> fn);
+
+  @Override
+  Literal setLocation(Option<IdentifiedLocation> location);
+
+  @Override
+  Literal duplicate(boolean keepLocations, boolean keepMetadata, boolean keepDiagnostics, boolean keepIdentifiers);
+
+  @GenerateIR(interfaces = {Literal.class})
+  final class Number extends LiteralNumberGen {
+    @GenerateFields
+    public Number(
+        @IRField Option<String> base,
+        @IRField String value,
+        IdentifiedLocation location,
+        MetadataStorage passData
+    ) {
+      super(base, value, location, passData);
+    }
+
+    /** Checks whether the literal represents a fractional value.
+     */
+    public boolean isFractional() {
+      return value().contains(".");
+    }
+
+    @Override
+    public String showCode(int indent) {
+      if (base().isDefined()) {
+        return base().get() + "_" + value();
+      } else {
+        return value();
+      }
+    }
+  }
+
+  @GenerateIR(interfaces = {Literal.class})
+  final class Text extends LiteralTextGen {
+    @GenerateFields
+    public Text(
+        @IRField String text,
+        IdentifiedLocation location,
+        MetadataStorage passData
+    ) {
+      super(text, location, passData);
+    }
+
+    @Override
+    public String showCode(int indent) {
+      var tripleQuotes = "\"\"\"";
+      return tripleQuotes + text() + tripleQuotes;
+    }
+  }
+}
