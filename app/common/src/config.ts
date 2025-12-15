8ab3f7c66344e892693a668279e548810b6b9cf1
somebody1234 <ehern.lee@gmail.com>
2025-12-16 01:30:45 +1000

    Fix CI

--- app/common/src/config.ts
+++ app/common/src/config.ts
@@ -33,37 +33,37 @@ declare global {
 }
 
 const processEnv = typeof process !== 'undefined' ? process.env : {}
-const importEnv = import.meta.env ?? {}
 
 /** When running dev server, the config variables are grabbed from appropriate .env file. */
 export const $config = {
-  ENVIRONMENT: processEnv.ENSO_IDE_ENVIRONMENT ?? importEnv.ENSO_IDE_ENVIRONMENT,
-  ENSO_HOST: processEnv.ENSO_IDE_HOST ?? importEnv.ENSO_IDE_HOST ?? 'https://ensoanalytics.com',
-  API_URL: processEnv.ENSO_IDE_API_URL ?? importEnv.ENSO_IDE_API_URL,
-  SENTRY_DSN: processEnv.ENSO_IDE_SENTRY_DSN ?? importEnv.ENSO_IDE_SENTRY_DSN,
-  STRIPE_KEY: processEnv.ENSO_IDE_STRIPE_KEY ?? importEnv.ENSO_IDE_STRIPE_KEY,
-  AUTH_ENDPOINT: processEnv.ENSO_IDE_AUTH_ENDPOINT ?? importEnv.ENSO_IDE_AUTH_ENDPOINT,
+  ENVIRONMENT: processEnv.ENSO_IDE_ENVIRONMENT ?? import.meta.env?.ENSO_IDE_ENVIRONMENT,
+  ENSO_HOST:
+    processEnv.ENSO_IDE_HOST ?? (import.meta.env?.ENSO_IDE_HOST || 'https://ensoanalytics.com'),
+  API_URL: processEnv.ENSO_IDE_API_URL ?? import.meta.env?.ENSO_IDE_API_URL,
+  SENTRY_DSN: processEnv.ENSO_IDE_SENTRY_DSN ?? import.meta.env?.ENSO_IDE_SENTRY_DSN,
+  STRIPE_KEY: processEnv.ENSO_IDE_STRIPE_KEY ?? import.meta.env?.ENSO_IDE_STRIPE_KEY,
+  AUTH_ENDPOINT: processEnv.ENSO_IDE_AUTH_ENDPOINT ?? import.meta.env?.ENSO_IDE_AUTH_ENDPOINT,
   COGNITO_USER_POOL_ID:
-    processEnv.ENSO_IDE_COGNITO_USER_POOL_ID ?? importEnv.ENSO_IDE_COGNITO_USER_POOL_ID,
+    processEnv.ENSO_IDE_COGNITO_USER_POOL_ID ?? import.meta.env?.ENSO_IDE_COGNITO_USER_POOL_ID,
   COGNITO_USER_POOL_WEB_CLIENT_ID:
     processEnv.ENSO_IDE_COGNITO_USER_POOL_WEB_CLIENT_ID ??
-    importEnv.ENSO_IDE_COGNITO_USER_POOL_WEB_CLIENT_ID,
+    import.meta.env?.ENSO_IDE_COGNITO_USER_POOL_WEB_CLIENT_ID,
   GOOGLE_ANALYTICS_TAG:
-    processEnv.ENSO_IDE_GOOGLE_ANALYTICS_TAG ?? importEnv.ENSO_IDE_GOOGLE_ANALYTICS_TAG,
-  COGNITO_DOMAIN: processEnv.ENSO_IDE_COGNITO_DOMAIN ?? importEnv.ENSO_IDE_COGNITO_DOMAIN,
-  COGNITO_REGION: processEnv.ENSO_IDE_COGNITO_REGION ?? importEnv.ENSO_IDE_COGNITO_REGION,
-  VERSION: processEnv.ENSO_IDE_VERSION ?? importEnv.ENSO_IDE_VERSION,
-  COMMIT_HASH: processEnv.ENSO_IDE_COMMIT_HASH ?? importEnv.ENSO_IDE_COMMIT_HASH,
-  YDOC_SERVER_URL: processEnv.ENSO_IDE_YDOC_SERVER_URL ?? importEnv.ENSO_IDE_YDOC_SERVER_URL,
-  CLOUD_BUILD: processEnv.ENSO_IDE_CLOUD_BUILD ?? importEnv.ENSO_IDE_CLOUD_BUILD,
+    processEnv.ENSO_IDE_GOOGLE_ANALYTICS_TAG ?? import.meta.env?.ENSO_IDE_GOOGLE_ANALYTICS_TAG,
+  COGNITO_DOMAIN: processEnv.ENSO_IDE_COGNITO_DOMAIN ?? import.meta.env?.ENSO_IDE_COGNITO_DOMAIN,
+  COGNITO_REGION: processEnv.ENSO_IDE_COGNITO_REGION ?? import.meta.env?.ENSO_IDE_COGNITO_REGION,
+  VERSION: processEnv.ENSO_IDE_VERSION ?? import.meta.env?.ENSO_IDE_VERSION,
+  COMMIT_HASH: processEnv.ENSO_IDE_COMMIT_HASH ?? import.meta.env?.ENSO_IDE_COMMIT_HASH,
+  YDOC_SERVER_URL: processEnv.ENSO_IDE_YDOC_SERVER_URL ?? import.meta.env?.ENSO_IDE_YDOC_SERVER_URL,
+  CLOUD_BUILD: processEnv.ENSO_IDE_CLOUD_BUILD ?? import.meta.env?.ENSO_IDE_CLOUD_BUILD,
   AG_GRID_LICENSE_KEY:
-    processEnv.ENSO_IDE_AG_GRID_LICENSE_KEY ?? importEnv.ENSO_IDE_AG_GRID_LICENSE_KEY,
+    processEnv.ENSO_IDE_AG_GRID_LICENSE_KEY ?? import.meta.env?.ENSO_IDE_AG_GRID_LICENSE_KEY,
   GOOGLE_OAUTH_CLIENT_ID:
-    processEnv.ENSO_IDE_GOOGLE_OAUTH_CLIENT_ID ?? importEnv.ENSO_IDE_GOOGLE_OAUTH_CLIENT_ID,
+    processEnv.ENSO_IDE_GOOGLE_OAUTH_CLIENT_ID ?? import.meta.env?.ENSO_IDE_GOOGLE_OAUTH_CLIENT_ID,
   STRAVA_OAUTH_CLIENT_ID:
-    processEnv.ENSO_IDE_STRAVA_OAUTH_CLIENT_ID ?? importEnv.ENSO_IDE_STRAVA_OAUTH_CLIENT_ID,
+    processEnv.ENSO_IDE_STRAVA_OAUTH_CLIENT_ID ?? import.meta.env?.ENSO_IDE_STRAVA_OAUTH_CLIENT_ID,
   MS365_OAUTH_CLIENT_ID:
-    processEnv.ENSO_IDE_MS365_OAUTH_CLIENT_ID ?? importEnv.ENSO_IDE_MS365_OAUTH_CLIENT_ID,
+    processEnv.ENSO_IDE_MS365_OAUTH_CLIENT_ID ?? import.meta.env?.ENSO_IDE_MS365_OAUTH_CLIENT_ID,
   MAPBOX_API_TOKEN:
     (typeof window === 'object' &&
       window &&
@@ -73,13 +73,17 @@ export const $config = {
       'mapBoxApiToken' in window.api &&
       typeof window.api.mapBoxApiToken === 'function' &&
       window.api?.mapBoxApiToken()) ||
-    (processEnv.ENSO_IDE_MAPBOX_API_TOKEN ?? importEnv.ENSO_IDE_MAPBOX_API_TOKEN),
+    (processEnv.ENSO_IDE_MAPBOX_API_TOKEN ?? import.meta.env?.ENSO_IDE_MAPBOX_API_TOKEN),
 }
 
 /** Sets the global configuration. */
 export function setConfig(config: typeof $config) {
   for (const k of unsafeKeys(config)) {
     if (config[k] === undefined) {
+      continue
+    }
+    // Special-case as ENSO_HOST may currently be an empty string when it is unset in CI.
+    if (k === 'ENSO_HOST' && config[k] === '') {
       continue
     }
     $config[k] = config[k]
--- app/gui/src/config.ts
+++ app/gui/src/config.ts
@@ -7,37 +7,36 @@ import { $config, setConfig } from 'enso-common/src/config'
 export type { $Config } from 'enso-common/src/config'
 
 const processEnv = typeof process !== 'undefined' ? process.env : {}
-const importEnv = import.meta.env ?? {}
 
 /** When running dev server, the config variables are grabbed from appropriate .env file. */
 setConfig({
-  ENVIRONMENT: processEnv.ENSO_IDE_ENVIRONMENT ?? importEnv.ENSO_IDE_ENVIRONMENT,
-  ENSO_HOST: processEnv.ENSO_IDE_HOST ?? importEnv.ENSO_IDE_HOST,
-  API_URL: processEnv.ENSO_IDE_API_URL ?? importEnv.ENSO_IDE_API_URL,
-  SENTRY_DSN: processEnv.ENSO_IDE_SENTRY_DSN ?? importEnv.ENSO_IDE_SENTRY_DSN,
-  STRIPE_KEY: processEnv.ENSO_IDE_STRIPE_KEY ?? importEnv.ENSO_IDE_STRIPE_KEY,
-  AUTH_ENDPOINT: processEnv.ENSO_IDE_AUTH_ENDPOINT ?? importEnv.ENSO_IDE_AUTH_ENDPOINT,
+  ENVIRONMENT: processEnv.ENSO_IDE_ENVIRONMENT ?? import.meta.env?.ENSO_IDE_ENVIRONMENT,
+  ENSO_HOST: processEnv.ENSO_IDE_HOST ?? import.meta.env?.ENSO_IDE_HOST,
+  API_URL: processEnv.ENSO_IDE_API_URL ?? import.meta.env?.ENSO_IDE_API_URL,
+  SENTRY_DSN: processEnv.ENSO_IDE_SENTRY_DSN ?? import.meta.env?.ENSO_IDE_SENTRY_DSN,
+  STRIPE_KEY: processEnv.ENSO_IDE_STRIPE_KEY ?? import.meta.env?.ENSO_IDE_STRIPE_KEY,
+  AUTH_ENDPOINT: processEnv.ENSO_IDE_AUTH_ENDPOINT ?? import.meta.env?.ENSO_IDE_AUTH_ENDPOINT,
   COGNITO_USER_POOL_ID:
-    processEnv.ENSO_IDE_COGNITO_USER_POOL_ID ?? importEnv.ENSO_IDE_COGNITO_USER_POOL_ID,
+    processEnv.ENSO_IDE_COGNITO_USER_POOL_ID ?? import.meta.env?.ENSO_IDE_COGNITO_USER_POOL_ID,
   COGNITO_USER_POOL_WEB_CLIENT_ID:
     processEnv.ENSO_IDE_COGNITO_USER_POOL_WEB_CLIENT_ID ??
-    importEnv.ENSO_IDE_COGNITO_USER_POOL_WEB_CLIENT_ID,
+    import.meta.env?.ENSO_IDE_COGNITO_USER_POOL_WEB_CLIENT_ID,
   GOOGLE_ANALYTICS_TAG:
-    processEnv.ENSO_IDE_GOOGLE_ANALYTICS_TAG ?? importEnv.ENSO_IDE_GOOGLE_ANALYTICS_TAG,
-  COGNITO_DOMAIN: processEnv.ENSO_IDE_COGNITO_DOMAIN ?? importEnv.ENSO_IDE_COGNITO_DOMAIN,
-  COGNITO_REGION: processEnv.ENSO_IDE_COGNITO_REGION ?? importEnv.ENSO_IDE_COGNITO_REGION,
-  VERSION: processEnv.ENSO_IDE_VERSION ?? importEnv.ENSO_IDE_VERSION,
-  COMMIT_HASH: processEnv.ENSO_IDE_COMMIT_HASH ?? importEnv.ENSO_IDE_COMMIT_HASH,
-  YDOC_SERVER_URL: processEnv.ENSO_IDE_YDOC_SERVER_URL ?? importEnv.ENSO_IDE_YDOC_SERVER_URL,
-  CLOUD_BUILD: processEnv.ENSO_IDE_CLOUD_BUILD ?? importEnv.ENSO_IDE_CLOUD_BUILD,
+    processEnv.ENSO_IDE_GOOGLE_ANALYTICS_TAG ?? import.meta.env?.ENSO_IDE_GOOGLE_ANALYTICS_TAG,
+  COGNITO_DOMAIN: processEnv.ENSO_IDE_COGNITO_DOMAIN ?? import.meta.env?.ENSO_IDE_COGNITO_DOMAIN,
+  COGNITO_REGION: processEnv.ENSO_IDE_COGNITO_REGION ?? import.meta.env?.ENSO_IDE_COGNITO_REGION,
+  VERSION: processEnv.ENSO_IDE_VERSION ?? import.meta.env?.ENSO_IDE_VERSION,
+  COMMIT_HASH: processEnv.ENSO_IDE_COMMIT_HASH ?? import.meta.env?.ENSO_IDE_COMMIT_HASH,
+  YDOC_SERVER_URL: processEnv.ENSO_IDE_YDOC_SERVER_URL ?? import.meta.env?.ENSO_IDE_YDOC_SERVER_URL,
+  CLOUD_BUILD: processEnv.ENSO_IDE_CLOUD_BUILD ?? import.meta.env?.ENSO_IDE_CLOUD_BUILD,
   AG_GRID_LICENSE_KEY:
-    processEnv.ENSO_IDE_AG_GRID_LICENSE_KEY ?? importEnv.ENSO_IDE_AG_GRID_LICENSE_KEY,
+    processEnv.ENSO_IDE_AG_GRID_LICENSE_KEY ?? import.meta.env?.ENSO_IDE_AG_GRID_LICENSE_KEY,
   GOOGLE_OAUTH_CLIENT_ID:
-    processEnv.ENSO_IDE_GOOGLE_OAUTH_CLIENT_ID ?? importEnv.ENSO_IDE_GOOGLE_OAUTH_CLIENT_ID,
+    processEnv.ENSO_IDE_GOOGLE_OAUTH_CLIENT_ID ?? import.meta.env?.ENSO_IDE_GOOGLE_OAUTH_CLIENT_ID,
   STRAVA_OAUTH_CLIENT_ID:
-    processEnv.ENSO_IDE_STRAVA_OAUTH_CLIENT_ID ?? importEnv.ENSO_IDE_STRAVA_OAUTH_CLIENT_ID,
+    processEnv.ENSO_IDE_STRAVA_OAUTH_CLIENT_ID ?? import.meta.env?.ENSO_IDE_STRAVA_OAUTH_CLIENT_ID,
   MS365_OAUTH_CLIENT_ID:
-    processEnv.ENSO_IDE_MS365_OAUTH_CLIENT_ID ?? importEnv.ENSO_IDE_MS365_OAUTH_CLIENT_ID,
+    processEnv.ENSO_IDE_MS365_OAUTH_CLIENT_ID ?? import.meta.env?.ENSO_IDE_MS365_OAUTH_CLIENT_ID,
   MAPBOX_API_TOKEN:
     (typeof window === 'object' &&
       window &&
@@ -47,7 +46,7 @@ setConfig({
       'mapBoxApiToken' in window.api &&
       typeof window.api.mapBoxApiToken === 'function' &&
       window.api?.mapBoxApiToken()) ||
-    (processEnv.ENSO_IDE_MAPBOX_API_TOKEN ?? importEnv.ENSO_IDE_MAPBOX_API_TOKEN),
+    (processEnv.ENSO_IDE_MAPBOX_API_TOKEN ?? import.meta.env?.ENSO_IDE_MAPBOX_API_TOKEN),
 })
 
 Object.defineProperty(window, '$config', {
