16bbc6b5310b69884af9052522184f6272b79934
somebody1234 <ehern.lee@gmail.com>
2025-12-16 00:51:55 +1000

    :(

--- app/gui/src/composables/userAgreements.ts
+++ app/gui/src/composables/userAgreements.ts
@@ -24,6 +24,7 @@ LocalStorage.registerKey('privacyPolicy', { schema: PRIVACY_POLICY_SCHEMA })
 const latestTermsOfServiceQueryOptions = vueQuery.queryOptions({
   queryKey: ['termsOfService', 'currentVersion'],
   queryFn: async () => {
+    console.log($config)
     const response = await fetch(new URL('/eula.json', $config.ENSO_HOST))
     if (!response.ok) {
       throw new Error('Failed to fetch Terms of Service')
@@ -40,6 +41,7 @@ const latestTermsOfServiceQueryOptions = vueQuery.queryOptions({
 const latestPrivacyPolicyQueryOptions = vueQuery.queryOptions({
   queryKey: ['privacyPolicy', 'currentVersion'],
   queryFn: async () => {
+    console.log($config)
     const response = await fetch(new URL('/privacy.json', $config.ENSO_HOST))
     if (!response.ok) {
       throw new Error('Failed to fetch Privacy Policy')
