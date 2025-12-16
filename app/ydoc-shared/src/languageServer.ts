90b265adff52965c1f7d2fc140fe6052e7f471eb
Dmitry Bushev <bushevdv@gmail.com>
2025-12-16 19:44:02 +0000

    DRAFT: debug

--- app/ydoc-shared/src/languageServer.ts
+++ app/ydoc-shared/src/languageServer.ts
@@ -228,7 +228,10 @@ export class LanguageServer extends ObservableV2<Notifications & TransportEvents
 
   /** The {@link ContentRoot}s of this {@link LanguageServer}. */
   get contentRoots(): Promise<ContentRoot[]> {
-    return this.initialized.then((result) => (result.ok ? result.value.contentRoots : []))
+    return this.initialized.then((result) => {
+      console.log("DEBUG contentRoots()", result)
+      return result.ok ? result.value.contentRoots : []
+    })
   }
 
   /** Reconnect the underlying network transport. */
