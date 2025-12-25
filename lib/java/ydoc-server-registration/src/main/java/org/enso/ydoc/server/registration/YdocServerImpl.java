b825e6e05f94ac5bce01e9a91f050ac6f3a433b5
Dmitry Bushev <bushevdv@gmail.com>
2025-12-25 14:34:39 +0000

    feat: pass executor

--- lib/java/ydoc-server-registration/src/main/java/org/enso/ydoc/server/registration/YdocServerImpl.java
+++ lib/java/ydoc-server-registration/src/main/java/org/enso/ydoc/server/registration/YdocServerImpl.java
@@ -3,6 +3,7 @@ package org.enso.ydoc.server.registration;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.concurrent.ScheduledExecutorService;
 import org.enso.jvm.interop.api.OtherJvmClassLoader;
 import org.enso.runner.common.WrongOption;
 import org.enso.runner.common.YdocServerApi;
@@ -13,7 +14,8 @@ public final class YdocServerImpl extends YdocServerApi {
   public YdocServerImpl() {}
 
   @Override
-  protected AutoCloseable runYdocServer(String hostname, int port, MessageCallbacks callbacks)
+  protected AutoCloseable runYdocServer(
+      String hostname, int port, ScheduledExecutorService executor, MessageCallbacks callbacks)
       throws WrongOption, IOException, URISyntaxException {
     // the following shall invoke:
     //   return launch(hostname, port);
@@ -40,7 +42,7 @@ public final class YdocServerImpl extends YdocServerApi {
     var fqn = "org.enso.ydoc.server.Main";
     var impl = loader.loadClass(fqn);
     assert impl != null;
-    impl.invokeMember("launch", hostname, port + "", callbacks);
+    impl.invokeMember("launch", hostname, port + "", executor, callbacks);
     return loader;
   }
 }
