8c861f6603a8cc7d01469d03255fc47821c093a4
Dmitry Bushev <bushevdv@gmail.com>
2025-12-26 12:19:26 +0000

    feat: syncronized callbacks

---
+++ lib/java/ydoc-server/src/main/java/org/enso/ydoc/server/YjsCallbacksSynchronized.java
@@ -0,0 +1,22 @@
+package org.enso.ydoc.server;
+
+import java.util.concurrent.ExecutorService;
+import org.enso.ydoc.api.MessageCallbacks;
+import org.enso.ydoc.api.YjsChannel;
+
+public class YjsCallbacksSynchronized implements MessageCallbacks {
+
+  private final MessageCallbacks callbacks;
+  private final ExecutorService executor;
+
+  public YjsCallbacksSynchronized(MessageCallbacks callbacks, ExecutorService executor) {
+    this.callbacks = callbacks;
+    this.executor = executor;
+  }
+
+  @Override
+  public void onConnect(YjsChannel channel) {
+    var synchronizedChannel = new YjsChannelSynchronized(channel, this.executor);
+    this.callbacks.onConnect(synchronizedChannel);
+  }
+}
