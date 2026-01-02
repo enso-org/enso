edef7917b574c268320a75d805d90c3dcbd60f70
Adam Riley <riley.ad@gmail.com>
2026-01-02 09:07:49 +0000

    Cleanup

--- app/gui/src/dashboard/data/serviceCredentials/salesforce.ts
+++ app/gui/src/dashboard/data/serviceCredentials/salesforce.ts
@@ -6,7 +6,8 @@ import { z } from 'zod'
 import type { CredentialRecipe } from './types'
 import { getOauthRedirectUri } from './utilities'
 
-const EXTRA_SCOPES = ['openid', 'profile', 'offline_access']
+// Salesforce OAuth scopes required by the integration.
+const EXTRA_SCOPES = ['api', 'refresh_token', 'offline_access', 'openid']
 const SALESFORCE_OAUTH_AUTHORIZE_URL = 'https://login.salesforce.com/services/oauth2/authorize'
 
 export const FORM_SCHEMA = z.object({
@@ -14,28 +15,11 @@ export const FORM_SCHEMA = z.object({
   scopes: z.array(z.string()).refine((scopes) => scopes.length > 0, {
     message: i18n.getText(i18n.resolveDictionary(), 'salesforceCredentialScopesEmptyError'),
   }),
-  filesPermission: z.enum([
-    'Files.ReadWrite.All',
-    'Files.Read.All',
-    'Files.ReadWrite',
-    'Files.Read',
-    'NoAccess',
-  ]),
-  sitesPermission: z.enum([
-    'Sites.Read.All',
-    'Sites.ReadWrite.All',
-    'Sites.Manage.All',
-    'NoAccess',
-  ]),
 })
 
-export type SalesforceFormValues = z.infer<typeof FORM_SCHEMA>
-
-export const DEFAULT_FORM_VALUES: SalesforceFormValues = {
+export const DEFAULT_FORM_VALUES: z.infer<typeof FORM_SCHEMA> = {
   name: 'Salesforce',
-  scopes: ['User.Read'],
-  filesPermission: 'Files.ReadWrite.All',
-  sitesPermission: 'NoAccess',
+  scopes: EXTRA_SCOPES,
 }
 
 /**
@@ -43,17 +27,14 @@ export const DEFAULT_FORM_VALUES: SalesforceFormValues = {
  */
 export function submitForm(
   createCredentials: (recipe: CredentialRecipe) => Promise<void>,
-  values: SalesforceFormValues,
+  values: z.infer<typeof FORM_SCHEMA>,
 ): Promise<void> {
   invariant($config.SALESFORCE_OAUTH_CLIENT_ID != null, 'Salesforce OAuth client id is missing')
   const salesforceOauthClientId = $config.SALESFORCE_OAUTH_CLIENT_ID
 
   const valuesWithDefaults = { ...DEFAULT_FORM_VALUES, ...values }
 
-  const permissions = [valuesWithDefaults.filesPermission, valuesWithDefaults.sitesPermission].filter(
-    (permission) => permission !== 'NoAccess',
-  )
-  const oauthScopes: string[] = [...EXTRA_SCOPES, ...valuesWithDefaults.scopes, ...permissions]
+  const oauthScopes: string[] = [...new Set([...EXTRA_SCOPES, ...valuesWithDefaults.scopes])]
   const input: SalesforceCredentialInput = {
     type: 'Salesforce',
     scopes: oauthScopes,
