5fa9a1d9b7845cd2aef52883e63a4df577a232c0
Dmitry Bushev <bushevdv@gmail.com>
2026-01-08 16:01:45 +0000

    refactor: yjs binary channel

--- app/ydoc-channel/src/index.ts
+++ app/ydoc-channel/src/index.ts
@@ -1,6 +1,5 @@
 export {
   YjsChannel,
-  YjsDataChannel,
   type MessageHandler,
   type YjsChannelCallbacks,
 } from './YjsChannel.js'
---
+++ app/ydoc-server/src/YjsBinaryChannel.ts
@@ -0,0 +1,43 @@
+import * as map from 'lib0/map'
+import * as Y from 'yjs'
+import { YjsChannel, type YjsChannelCallbacks, type MessageHandler } from 'ydoc-channel'
+
+export class YjsBinaryChannel<T = unknown> extends YjsChannel<T> {
+  private static channels = new Map<string, YjsBinaryChannel>()
+
+  private readonly callbacks: YjsChannelCallbacks<T>
+  private readonly ByteBuffer: any
+
+  constructor(doc: Y.Doc, channelName: string, callbacks: YjsChannelCallbacks<T>, byteBuffer: any) {
+    super(doc, channelName)
+    this.callbacks = callbacks
+    this.ByteBuffer = byteBuffer
+    this.callbacks.onConnect(this)
+  }
+
+  /** Get a {@link YjsBinaryChannel}. */
+  static get(doc: Y.Doc, channelName: string, callbacks: YjsChannelCallbacks, byteBuffer: any): YjsBinaryChannel {
+    return map.setIfUndefined(YjsBinaryChannel.channels, channelName, () => {
+      return new YjsBinaryChannel(doc, channelName, callbacks, byteBuffer)
+    })
+  }
+
+  override send(message: any): void {
+    console.log('DEBUG YjsBinaryChannel.send', message)
+    const arr = new Uint8Array(new ArrayBuffer(message))
+    super.send(arr as T)
+  }
+
+  override subscribe(handler: MessageHandler<T>): () => void {
+    console.log('YjsBinaryChannel.subscribe', handler)
+    const f = (contents: Uint8Array) => {
+      console.log('YjsBinaryChannel.subscribe f', contents)
+      const bb = this.ByteBuffer.allocateDirect(contents.byteLength)
+      console.log('YjsBinaryChannel.subscribe f bb', bb)
+      const arr = new Uint8Array(new ArrayBuffer(bb))
+      arr.set(contents)
+      return handler(bb)
+    }
+    return super.subscribe(f as MessageHandler<T>)
+  }
+}
