9cd5f1b633cebbcf4cfce6db7a04a10a9000dd15
Jaroslav Tulach <jaroslav.tulach@enso.org>
2026-01-09 08:03:02 +0100

    javafmtAll

--- lib/java/os-environment/src/test/java/org/enso/os/environment/jni/LoadClassTest.java
+++ lib/java/os-environment/src/test/java/org/enso/os/environment/jni/LoadClassTest.java
@@ -1,8 +1,8 @@
 package org.enso.os.environment.jni;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.File;
