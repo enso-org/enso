6026b98950e3a784760dd924aa9928299d820836
James Dunkerley <jdunkerley@users.noreply.github.com>
2026-01-12 10:34:26 +0000

    Formatting.

--- std-bits/table/src/main/java/org/enso/table/read/AlteryxYXDBReader.java
+++ std-bits/table/src/main/java/org/enso/table/read/AlteryxYXDBReader.java
@@ -1,13 +1,5 @@
 package org.enso.table.read;
 
-import org.enso.table.data.column.builder.Builder;
-import org.enso.table.data.column.storage.type.*;
-import org.enso.table.data.table.Column;
-import org.enso.table.data.table.Table;
-import org.enso.table.problems.ProblemAggregator;
-import uk.co.jdunkerley.yxdb.YxdbField;
-import uk.co.jdunkerley.yxdb.YxdbReader;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -15,68 +7,98 @@ import java.nio.file.Path;
 import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.stream.IntStream;
+import org.enso.table.data.column.builder.Builder;
+import org.enso.table.data.column.storage.type.AnyObjectType;
+import org.enso.table.data.column.storage.type.BigDecimalType;
+import org.enso.table.data.column.storage.type.BooleanType;
+import org.enso.table.data.column.storage.type.DateTimeType;
+import org.enso.table.data.column.storage.type.DateType;
+import org.enso.table.data.column.storage.type.FloatType;
+import org.enso.table.data.column.storage.type.IntegerType;
+import org.enso.table.data.column.storage.type.StorageType;
+import org.enso.table.data.column.storage.type.TextType;
+import org.enso.table.data.column.storage.type.TimeOfDayType;
+import org.enso.table.data.table.Column;
+import org.enso.table.data.table.Table;
+import org.enso.table.problems.ProblemAggregator;
+import uk.co.jdunkerley.yxdb.YxdbField;
+import uk.co.jdunkerley.yxdb.YxdbReader;
 
 public final class AlteryxYXDBReader {
-    /**
-     * Reads an Alteryx YXDB file and returns its contents as a Table.
-     *
-     * @param path     the path to the YXDB file.
-     * @return a Table containing the data from the YXDB file.
-     */
-    public static Table read(String path, ProblemAggregator problemAggregator) throws FileNotFoundException, IllegalArgumentException, IllegalStateException {
-        // Test that the path exists
-        if (!Files.exists(Path.of(path))) {
-            throw new FileNotFoundException(path);
-        }
-
-        try (var yxdbReader = new YxdbReader(path)) {
-            var recordCount = yxdbReader.numRecords();
-            var fields = yxdbReader.fields();
-            var storageTypes = Arrays.stream(fields).map(AlteryxYXDBReader::mapYXDBField).toArray(StorageType[]::new);
-            var storages = Arrays.stream(storageTypes).map(st -> st.makeBuilder(recordCount, problemAggregator)).toArray(Builder[]::new);
-
-            while (yxdbReader.next()) {
-                try {
-                    for (int i = 0; i < storages.length; i++) {
-                        storages[i].append(yxdbReader.read(i));
-                    }
-                } catch (IndexOutOfBoundsException _) {
-                    throw new IllegalArgumentException("The YXDB file appears to be corrupted on row " + storages[0].getCurrentSize());
-                } catch (DateTimeParseException _) {
-                    throw new IllegalArgumentException("The YXDB file contains invalid date/time data on row " + storages[0].getCurrentSize());
-                }
-            }
-
-            var columns = IntStream.range(0, storages.length)
-                .mapToObj(i -> new Column(fields[i].name(), storages[i].seal()))
-                .toArray(Column[]::new);
-            return new Table(columns);
-        } catch (IllegalArgumentException exc) {
-            throw exc;
-        } catch (IOException exc) {
-            var message = exc.getMessage();
-            throw new IllegalArgumentException(exc.getMessage(), exc);
-        } catch (Exception exc) {
-            throw new IllegalStateException("An unexpected error occurred: " + exc.getMessage(), exc);
-        }
+  /**
+   * Reads an Alteryx YXDB file and returns its contents as a Table.
+   *
+   * @param path the path to the YXDB file.
+   * @return a Table containing the data from the YXDB file.
+   */
+  public static Table read(String path, ProblemAggregator problemAggregator)
+      throws FileNotFoundException, IllegalArgumentException, IllegalStateException {
+    // Test that the path exists
+    if (!Files.exists(Path.of(path))) {
+      throw new FileNotFoundException(path);
     }
 
-    private static StorageType<?> mapYXDBField(YxdbField field) {
-        return switch (field.yxdbType()) {
-            case "Bool" -> BooleanType.INSTANCE;
-            case "Byte" -> IntegerType.INT_8;
-            case "Int16" -> IntegerType.INT_16;
-            case "Int32" -> IntegerType.INT_32;
-            case "Int64" -> IntegerType.INT_64;
-            case "Float", "Double" -> FloatType.FLOAT_64;
-            case "FixedDecimal" -> BigDecimalType.INSTANCE;
-            case "String", "WString" -> TextType.variableLengthWithLimit(field.size());
-            case "V_String", "V_WString", "SpatialObj" -> TextType.VARIABLE_LENGTH;
-            case "Date" -> DateType.INSTANCE;
-            case "Time" -> TimeOfDayType.INSTANCE;
-            case "DateTime" -> DateTimeType.INSTANCE;
-            case "Blob" -> AnyObjectType.INSTANCE;
-            default -> throw new IllegalStateException("Unsupported YXDB field type: " + field.yxdbType());
-        };
+    try (var yxdbReader = new YxdbReader(path)) {
+      var recordCount = yxdbReader.numRecords();
+      var fields = yxdbReader.fields();
+      var storageTypes =
+          Arrays.stream(fields).map(AlteryxYXDBReader::mapYXDBField).toArray(StorageType[]::new);
+      var storages =
+          Arrays.stream(storageTypes)
+              .map(st -> st.makeBuilder(recordCount, problemAggregator))
+              .toArray(Builder[]::new);
+
+      while (yxdbReader.next()) {
+        try {
+          for (int i = 0; i < storages.length; i++) {
+            var yxdbValue =
+                storageTypes[i] instanceof AnyObjectType
+                    ? yxdbReader.readBlob(i)
+                    : yxdbReader.read(i);
+            storages[i].append(yxdbValue);
+          }
+        } catch (IndexOutOfBoundsException _) {
+          throw new IllegalArgumentException(
+              "The YXDB file appears to be corrupted on row " + storages[0].getCurrentSize());
+        } catch (DateTimeParseException _) {
+          throw new IllegalArgumentException(
+              "The YXDB file contains invalid date/time data on row "
+                  + storages[0].getCurrentSize());
+        }
+      }
+
+      var columns =
+          IntStream.range(0, storages.length)
+              .mapToObj(i -> new Column(fields[i].name(), storages[i].seal()))
+              .toArray(Column[]::new);
+      return new Table(columns);
+    } catch (IllegalArgumentException exc) {
+      throw exc;
+    } catch (IOException exc) {
+      var message = exc.getMessage();
+      throw new IllegalArgumentException(exc.getMessage(), exc);
+    } catch (Exception exc) {
+      throw new IllegalStateException("An unexpected error occurred: " + exc.getMessage(), exc);
     }
+  }
+
+  private static StorageType<?> mapYXDBField(YxdbField field) {
+    return switch (field.yxdbType()) {
+      case "Bool" -> BooleanType.INSTANCE;
+      case "Byte" -> IntegerType.INT_8;
+      case "Int16" -> IntegerType.INT_16;
+      case "Int32" -> IntegerType.INT_32;
+      case "Int64" -> IntegerType.INT_64;
+      case "Float", "Double" -> FloatType.FLOAT_64;
+      case "FixedDecimal" -> BigDecimalType.INSTANCE;
+      case "String", "WString" -> TextType.variableLengthWithLimit(field.size());
+      case "V_String", "V_WString" -> TextType.VARIABLE_LENGTH;
+      case "Date" -> DateType.INSTANCE;
+      case "Time" -> TimeOfDayType.INSTANCE;
+      case "DateTime" -> DateTimeType.INSTANCE;
+      case "Blob", "SpatialObj" -> AnyObjectType.INSTANCE;
+      default ->
+          throw new IllegalStateException("Unsupported YXDB field type: " + field.yxdbType());
+    };
+  }
 }
