a4fec71630ed74a11a213afa4da9c00b37999171
Pavel Marek <pavel.marek@enso.org>
2026-01-16 17:47:39 +0100

    Refactor Diagnostic to Java

--- engine/runtime-parser/src/main/java/org/enso/compiler/core/ir/expression/errors/Syntax.java
+++ engine/runtime-parser/src/main/java/org/enso/compiler/core/ir/expression/errors/Syntax.java
@@ -1,7 +1,7 @@
 package org.enso.compiler.core.ir.expression.errors;
 
 import java.util.function.Function;
-import org.enso.compiler.core.ir.Diagnostic$Kind$Interactive;
+import org.enso.compiler.core.ir.Diagnostic;
 import org.enso.compiler.core.ir.DiagnosticStorage;
 import org.enso.compiler.core.ir.Expression;
 import org.enso.compiler.core.ir.IRKind;
@@ -21,7 +21,7 @@ import scala.Option;
 @GenerateIR(
     interfaces = {
       Error.class,
-      Diagnostic$Kind$Interactive.class,
+      Diagnostic.Kind.Interactive.class,
       IRKind.Primitive.class,
       Definition.class,
       Import.class,
