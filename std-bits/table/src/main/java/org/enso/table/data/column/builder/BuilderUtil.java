b767e3757d8c666979665967ca15bb4109cef4bb
James Dunkerley <jdunkerley@users.noreply.github.com>
2026-01-21 13:21:59 +0000

    Getting back to it.

--- std-bits/table/src/main/java/org/enso/table/data/column/builder/BuilderUtil.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/builder/BuilderUtil.java
@@ -3,8 +3,8 @@ package org.enso.table.data.column.builder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public final class BuilderUtil {
+final class BuilderUtil {
   private BuilderUtil() {}
 
-  public static final Logger LOGGER = LoggerFactory.getLogger(BuilderUtil.class);
+  static final Logger LOGGER = LoggerFactory.getLogger(BuilderUtil.class);
 }
