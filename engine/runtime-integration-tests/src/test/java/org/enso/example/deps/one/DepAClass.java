7e63f8277285783457bf101136694b21b09ab931
Jaroslav Tulach <jaroslav.tulach@enso.org>
2026-01-22 20:42:04 +0100

    Need both classes to start (and not finish) static class initialization at once

--- engine/runtime-integration-tests/src/test/java/org/enso/example/deps/one/DepAClass.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/example/deps/one/DepAClass.java
@@ -1,8 +1,16 @@
 package org.enso.example.deps.one;
 
+import org.enso.example.deps.AwaitLoading;
 import org.enso.example.deps.two.DepBClass;
 
 public class DepAClass {
+  static {
+    System.err.println("Started DepAClass");
+    AwaitLoading.waitForTwo();
+    System.err.println("Loading DepAClass");
+    System.err.println("Finished DepAClass");
+  }
+
   public static double expToNeg(double x) {
     return DepBClass.expPow(-x);
   }
