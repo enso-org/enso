b0050888ce0df368a9691c548e1026196ae52e2c
Hubert P <hubert.plociniczak@gmail.com>
2026-01-23 13:11:08 +0100

    Revert "Need both classes to start (and not finish) static class initialization at once"
    This reverts commit 7e63f8277285783457bf101136694b21b09ab931.

--- engine/runtime-integration-tests/src/test/java/org/enso/example/deps/one/DepAClass.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/example/deps/one/DepAClass.java
@@ -1,16 +1,8 @@
 package org.enso.example.deps.one;
 
-import org.enso.example.deps.AwaitLoading;
 import org.enso.example.deps.two.DepBClass;
 
 public class DepAClass {
-  static {
-    System.err.println("Started DepAClass");
-    AwaitLoading.waitForTwo();
-    System.err.println("Loading DepAClass");
-    System.err.println("Finished DepAClass");
-  }
-
   public static double expToNeg(double x) {
     return DepBClass.expPow(-x);
   }
--- engine/runtime-integration-tests/src/test/java/org/enso/example/deps/two/DepBClass.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/example/deps/two/DepBClass.java
@@ -1,16 +1,8 @@
 package org.enso.example.deps.two;
 
-import org.enso.example.deps.AwaitLoading;
 import org.enso.example.deps.one.DepAClass;
 
 public class DepBClass {
-  static {
-    System.err.println("Started DepBClass");
-    AwaitLoading.waitForTwo();
-    System.err.println("Loading DepBClass");
-    System.err.println("Finished DepBClass");
-  }
-
   public static double sigmoid(double x) {
     return 1 / (1 + DepAClass.expToNeg(x));
   }
