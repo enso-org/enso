8e5c8a24ecdeb2f3b4b985c84070a6e5b415ff6b
James Dunkerley <jdunkerley@users.noreply.github.com>
2026-02-05 15:21:13 +0000

    Removing the extension incorrectly.

--- app/project-manager-shim/src/projectManagement.ts
+++ app/project-manager-shim/src/projectManagement.ts
@@ -369,8 +369,9 @@ function getCommonPrefix(a: string, b: string): string {
  * for the name.
  */
 function generateDirectoryName(name: string, directory = getProjectsDirectory()): string {
-  // Use only the last path component.
-  let baseName = pathModule.parse(name).name
+  // Use only the last path component (but with the extension as we don't want to remove that).
+  const parsedPath = pathModule.parse(name)
+  let baseName = parsedPath.name + (parsedPath.ext ? parsedPath.ext : '')
   logger.log(`Generating directory name for '${name}' in '${directory}'. Base name: '${baseName}'.`)
 
   // If the name already consists a suffix, reuse it.
@@ -383,9 +384,7 @@ function generateDirectoryName(name: string, directory = getProjectsDirectory())
     logger.log(`Base name after stripping suffix: '${baseName}'.`)
   }
 
-  const result = pathModule.join(directory, baseName)
-  logger.log(`Target '${result}'.`)
-  return result
+  return pathModule.join(directory, baseName)
 }
 
 /**
