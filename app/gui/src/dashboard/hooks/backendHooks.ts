966b3bfd547b880d9095e3ee5388c7170b8404de
Paweł Buchowski <pawel.buchowski@enso.org>
2026-02-10 15:14:28 +0100

    bring back run as task

--- app/gui/src/dashboard/hooks/backendHooks.ts
+++ app/gui/src/dashboard/hooks/backendHooks.ts
@@ -529,7 +529,7 @@ export function listProjectExecutionsQueryOptions(
 ) {
   return queryOptions({
     ...backendQueryOptions(backend, 'listProjectExecutions', [id, title, year, month]),
-    select: (executions) => [...executions].reverse(),
+    // select: (executions) => [...executions].reverse(),
     staleTime: PROJECT_EXECUTIONS_STALE_TIME,
   })
 }
