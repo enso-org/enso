54c94e48164460882bbbdd7d727ffe0fa1600ea2
James Dunkerley <jdunkerley@users.noreply.github.com>
2026-02-11 14:13:54 +0000

    Java format.

--- std-bits/table/src/main/java/org/enso/table/data/column/DataQualityMetrics.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/DataQualityMetrics.java
@@ -218,7 +218,7 @@ public abstract class DataQualityMetrics {
           distinctJson =
               "["
                   + distinct.stream()
-                      .map(o-> JsonOperation.objectToJson(o, null))
+                      .map(o -> JsonOperation.objectToJson(o, null))
                       .filter(Objects::nonNull)
                       .sorted()
                       .collect(Collectors.joining())
