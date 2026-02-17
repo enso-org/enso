238c4654553195b3f0987604809c5dda394ed94d
Adam Obuchowicz <adam.obuchowicz@enso.org>
2026-02-17 11:48:27 +0100

    Even more debugs

--- app/gui/src/authentication/listen.ts
+++ app/gui/src/authentication/listen.ts
@@ -55,6 +55,7 @@ export type ListenFunction = (listener: ListenerCallback) => UnsubscribeFunction
 /** Listen to authentication state changes. */
 export function registerAuthEventListener(listener: ListenerCallback): () => void {
   return amplify.Hub.listen(AUTHENTICATION_HUB, (data) => {
+    console.debug('got auth event', data)
     if (isAuthEvent(data.payload.event)) {
       listener(data.payload.event)
     }
