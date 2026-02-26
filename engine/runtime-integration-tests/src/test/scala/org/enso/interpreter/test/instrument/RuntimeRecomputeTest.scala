f764d514576db3f69f2a19248be8648667a69070
Dmitry Bushev <bushevdv@gmail.com>
2026-02-26 08:12:19 +0000

    misc: format

--- engine/runtime-integration-tests/src/test/scala/org/enso/interpreter/test/instrument/RuntimeRecomputeTest.scala
+++ engine/runtime-integration-tests/src/test/scala/org/enso/interpreter/test/instrument/RuntimeRecomputeTest.scala
@@ -783,7 +783,7 @@ class RuntimeRecomputeTest
       )
     )
     context.receiveNIgnoreStdLib(
-      5,
+      5
     ) should contain theSameElementsAs Seq(
       Api.Response(requestId, Api.PushContextResponse(contextId)),
       TestMessages.update(
