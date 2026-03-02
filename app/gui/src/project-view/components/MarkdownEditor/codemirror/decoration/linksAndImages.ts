a817944818bab16052745e76dbaebf6809d35fce
Ilya Bogdanov <fumlead@gmail.com>
2026-03-02 13:18:27 +0400

    Fix warning in console about title prop for videos in documentation panel

--- app/gui/src/project-view/components/MarkdownEditor/codemirror/decoration/linksAndImages.ts
+++ app/gui/src/project-view/components/MarkdownEditor/codemirror/decoration/linksAndImages.ts
@@ -67,8 +67,8 @@ function decorateLink(
   if (!makeAttributes) return
   const parsed =
     nodeRef.name === 'Link' ? parseLinkLike(nodeRef, doc)
-    : nodeRef.name === 'Autolink' ? parseAutolink(nodeRef, doc)
-    : undefined
+      : nodeRef.name === 'Autolink' ? parseAutolink(nodeRef, doc)
+        : undefined
   if (!parsed) return
   const { linkOrImage: link, text, url, title } = parsed
   if (text.empty) return
@@ -117,9 +117,9 @@ export function decorateImageWithRendered(
   if (nodeRef.name === 'Image') {
     const parsed = parseLinkLike(nodeRef, doc)
     if (!parsed) return
-    const { text, url } = parsed
+    const { text, url, title } = parsed
     const alt = doc.sliceString(text.from, text.to)
-    const widget = new MediaWidget({ src: url, alt }, vueHost)
+    const widget = new MediaWidget({ src: url, alt, title }, vueHost)
     emitDecoration(
       Range.emptyAt(nodeRef.to),
       Decoration.widget({
@@ -133,9 +133,10 @@ export function decorateImageWithRendered(
 }
 
 class MediaWidget extends VueDecorationWidget<{ alt: string; src: string }> {
-  constructor(props: { alt: string; src: string }, vueHost: VueHost) {
+  constructor(props: { alt: string; src: string, title?: string | undefined }, vueHost: VueHost) {
     const isVideo = props.src.match(/https:\/\/www\.youtube(-nocookie)?\.com\/embed\/[^/]+/)
     const component = isVideo ? DocumentationVideo : DocumentationImage
+    if (!isVideo) delete props.title
     super(component, props, vueHost, 'cm-media-rendered', 'span')
   }
 
