5185813022e2782e10760e1d0d3903caed4d09fc
James Dunkerley <jdunkerley@users.noreply.github.com>
2026-03-10 10:57:08 +0000

    Remove defaults.

--- std-bits/base/src/main/java/org/enso/base/enso_cloud/EnsoHideableValue.java
+++ std-bits/base/src/main/java/org/enso/base/enso_cloud/EnsoHideableValue.java
@@ -2,8 +2,11 @@ package org.enso.base.enso_cloud;
 
 import java.util.List;
 
-/** This is a Java view of ` Standard.Base.Enso_Cloud.Enso_Secret.Derived_Secret_Value`. It allows one to pass instances of `Derived_Secret_Value` into `HideableValue.from` method.
-*/
+/**
+ * This is a Java view of ` Standard.Base.Enso_Cloud.Enso_Secret.Derived_Secret_Value`.
+ * It allows one to pass instances of `Derived_Secret_Value` into `HideableValue.from`
+ * method getting the core values from the Enso object.
+ */
 public interface EnsoHideableValue {
   String value_type();
 
