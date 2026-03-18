d6f9d9ad049cde4d5c2f949066c7fc6200ea76ff
James Dunkerley <jdunkerley@users.noreply.github.com>
2026-03-18 18:10:26 +0000

    Add support for Array in the JSON read.

--- std-bits/table/src/main/java/org/enso/table/data/column/operation/unary/JsonParseOperation.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/operation/unary/JsonParseOperation.java
@@ -1,7 +1,10 @@
 package org.enso.table.data.column.operation.unary;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.function.Function;
+import org.enso.base.polyglot.EnsoMeta;
 import org.enso.table.data.column.builder.Builder;
 import org.enso.table.data.column.operation.StorageIterators;
 import org.enso.table.data.column.operation.UnaryOperation;
@@ -9,10 +12,9 @@ import org.enso.table.data.column.storage.ColumnStorage;
 import org.enso.table.data.column.storage.type.StorageType;
 import org.enso.table.data.column.storage.type.TextType;
 import org.enso.table.data.table.problems.MapOperationProblemAggregator;
+import org.graalvm.polyglot.Value;
 
-/**
- * Deserializes JSON to Objects in the Table.
- */
+/** Deserializes JSON to Objects in the Table. */
 public class JsonParseOperation implements UnaryOperation {
   @Override
   public String getName() {
@@ -25,7 +27,8 @@ public class JsonParseOperation implements UnaryOperation {
   }
 
   @Override
-  public ColumnStorage<?> apply(ColumnStorage<?> storage, MapOperationProblemAggregator problemAggregator) {
+  public ColumnStorage<?> apply(
+      ColumnStorage<?> storage, MapOperationProblemAggregator problemAggregator) {
     var mapper = new ObjectMapper();
     var inferredBuilder = Builder.getInferredBuilder(storage.getSize(), problemAggregator);
 
@@ -33,26 +36,54 @@ public class JsonParseOperation implements UnaryOperation {
         TextType.VARIABLE_LENGTH.asTypedStorage(storage),
         true,
         inferredBuilder,
-        ( builder, index, value) -> {
+        (builder, index, value) -> {
           try {
             builder.append(parseJson(mapper, value));
           } catch (JsonProcessingException e) {
-            problemAggregator.reportIllegalArgumentError("Failed to parse JSON: " + e.getMessage(), index);
+            problemAggregator.reportIllegalArgumentError(
+                "Failed to parse JSON: " + e.getMessage(), index);
+            builder.appendNulls(1);
+          } catch (IllegalArgumentException e) {
+            problemAggregator.reportIllegalArgumentError(
+                "Unsupported JSON node type: " + e.getMessage() + " when parsing " + value, index);
             builder.appendNulls(1);
           }
         });
   }
 
   private static Object parseJson(ObjectMapper mapper, String json) throws JsonProcessingException {
-    // ToDo: Array
     // ToDo: Object
     var node = mapper.readTree(json);
+    return parseJsonNode(node);
+  }
+
+  private static Object parseJsonNode(JsonNode node) throws JsonProcessingException {
     return switch (node.getNodeType()) {
       case NULL -> null;
       case BOOLEAN -> node.asBoolean();
       case STRING -> node.asText();
-      case NUMBER -> node.isIntegralNumber() ?  node.asLong() : node.asDouble();
-      default -> throw new IllegalArgumentException("Unsupported JSON node type: " + node.getNodeType() + " when parsing " + json);
+      case NUMBER -> node.isIntegralNumber() ? node.asLong() : node.asDouble();
+      case ARRAY -> parseJsonArray(node);
+      default -> throw new IllegalArgumentException(
+          "Unsupported JSON node type: " + node.getNodeType() + " when parsing " + node.asText());
     };
+  }
+
+  private static Function<Object[], Value> vectorConstructor;
+
+  private static Value parseJsonArray(JsonNode node)
+      throws JsonProcessingException {
+    var array = new Object[node.size()];
+    for (int i = 0; i < array.length; i++) {
+      array[i] = parseJsonNode(node.get(i));
+    }
+
+    if (vectorConstructor == null) {
+      var vectorType = EnsoMeta.getType("Standard.Base.Data.Vector", "Vector");
+      var method = vectorType.getMember("from_polyglot_array");
+      vectorConstructor = arr -> method.execute(vectorType, arr);
+    }
+
+    return vectorConstructor.apply(array);
   }
 }
