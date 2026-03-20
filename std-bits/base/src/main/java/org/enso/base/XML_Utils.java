3b0fb0737a75a8c0d09ad613a53b8e990c8ad8d4
James Dunkerley <jdunkerley@users.noreply.github.com>
2026-03-20 17:43:19 +0000

    Complete moving XML code to Java

--- std-bits/base/src/main/java/org/enso/base/XML_Utils.java
+++ std-bits/base/src/main/java/org/enso/base/XML_Utils.java
@@ -5,13 +5,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
-
 import org.enso.base.polyglot.EnsoMeta;
 import org.graalvm.polyglot.Value;
 import org.w3c.dom.*;
@@ -134,11 +135,13 @@ public class XML_Utils {
    * Read an XML document from an input stream and return it as an Enso XML_Document.
    *
    * @param is the input stream to read the XML document from.
-   * @param makeXMLDocument a function that takes a Document and returns an Enso XML_Document instance.
+   * @param makeXMLDocument a function that takes a Document and returns an Enso XML_Document
+   *     instance.
    * @return the parsed XML document or an Enso error if parsing failed.
    * @throws IOException if an IO exception occurs reading the stream.
    */
-  public static Value parseStream(InputStream is, Function<Document, Value> makeXMLDocument) throws IOException {
+  public static Value parseStream(InputStream is, Function<Document, Value> makeXMLDocument)
+      throws IOException {
     try {
       var document = doParse(new InputSource(is));
       var value = makeXMLDocument.apply(document);
@@ -155,7 +158,8 @@ public class XML_Utils {
    * Parses a String value into an XML Document, returning an Enso XML_Document object.
    *
    * @param text the String value to parse.
-   * @param makeXMLDocument a function that takes a Document and returns an Enso XML_Document instance.
+   * @param makeXMLDocument a function that takes a Document and returns an Enso XML_Document
+   *     instance.
    * @return the parsed XML document or an Enso error if parsing failed.
    */
   public static Value parseString(String text, Function<Document, Value> makeXMLDocument) {
@@ -197,43 +201,85 @@ public class XML_Utils {
   private static Function<ArrayList<Value>, Value> vectorConstructor;
 
   /**
-   * Given an XML Element of Document,
-   * @param document
-   * @param xpath
-   * @param makeXMLElement
-   * @param firstOnly
-   * @return
+   * Given an XML Element or Document, an XPath query, get the matching set.
+   *
+   * @param source the element or document to query.
+   * @param xpath an XPath query to execute on the source.
+   * @param makeXMLElement factory function to make Enso XML_Element.
+   * @param firstOnly if true then stop after the first value.
+   * @return an Enso Vector of matching results.
    */
-  public static Value getXPath(Object document, String xpath, Function<Element, Value> makeXMLElement, boolean firstOnly) {
+  public static Value getXPath(
+      Object source, String xpath, Function<Element, Value> makeXMLElement, boolean firstOnly) {
     try {
       var factory = XPathFactory.newInstance().newXPath();
 
-      var nodeSet = factory.evaluate(xpath, document, javax.xml.xpath.XPathConstants.NODESET);
+      var nodeSet = factory.evaluate(xpath, source, javax.xml.xpath.XPathConstants.NODESET);
       if (!(nodeSet instanceof NodeList nodeList)) {
-        throw new IllegalStateException("Unexpected result from XPath evaluation: expected a NodeList");
+        throw new IllegalStateException(
+            "Unexpected result from XPath evaluation: expected a NodeList");
       }
 
-      var result = new ArrayList<Value>();
-      for (int i = 0; i < nodeList.getLength(); i++) {
-        var node = nodeList.item(i);
-        if (isWantedNode(node)) {
-          result.add(convert(node, makeXMLElement));
-          if (firstOnly) {
-            break;
-          }
-        }
-      }
-
-      if (vectorConstructor == null) {
-        var vectorType = EnsoMeta.getType("Standard.Base.Data.Vector", "Vector");
-        var method = vectorType.getMember("from_polyglot_array");
-        vectorConstructor = arr -> method.execute(vectorType, arr);
-      }
-
-      return vectorConstructor.apply(result);
+      return convertNodeListToVector(makeXMLElement, firstOnly, nodeList);
     } catch (Exception e) {
       return handleExceptions(e);
     }
+  }
+
+  /**
+   * Given an XML Element, make an Enso vector of all the child nodes.
+   *
+   * @param element the element to get the children of.
+   * @param makeXMLElement factory function to make Enso XML_Element.
+   * @return an Enso Vector of child nodes.
+   */
+  public static Value getChildren(Element element, Function<Element, Value> makeXMLElement) {
+    try {
+      var children = element.getChildNodes();
+      return convertNodeListToVector(makeXMLElement, false, children);
+    } catch (Exception e) {
+      return handleExceptions(e);
+    }
+  }
+
+  /**
+   * Given an XML Element, make an Enso vector of all the matching descendant tag nodes.
+   *
+   * @param element the element to get the children of.
+   * @param tagName the name of the child nodes to match.
+   * @param makeXMLElement factory function to make Enso XML_Element.
+   * @return an Enso Vector of child nodes.
+   */
+  public static Value getElementsByTagName(
+      Element element, String tagName, Function<Element, Value> makeXMLElement) {
+    try {
+      var children = element.getElementsByTagName(tagName);
+      return convertNodeListToVector(makeXMLElement, false, children);
+    } catch (Exception e) {
+      return handleExceptions(e);
+    }
+  }
+
+  private static Value convertNodeListToVector(
+      Function<Element, Value> makeXMLElement, boolean firstOnly, NodeList nodeList) {
+    var result = new ArrayList<Value>();
+    for (int i = 0; i < nodeList.getLength(); i++) {
+      var node = nodeList.item(i);
+      if (isWantedNode(node)) {
+        result.add(convert(node, makeXMLElement));
+        if (firstOnly) {
+          break;
+        }
+      }
+    }
+
+    if (vectorConstructor == null) {
+      var vectorType = EnsoMeta.getType("Standard.Base.Data.Vector", "Vector");
+      var method = vectorType.getMember("from_polyglot_array");
+      vectorConstructor = arr -> method.execute(vectorType, arr);
+    }
+
+    return vectorConstructor.apply(result);
   }
 
   private static boolean isWantedNode(Node node) {
@@ -249,7 +295,67 @@ public class XML_Utils {
       case Attr attr -> Value.asValue(node.getNodeValue());
       case Text textNode -> Value.asValue(node.getTextContent());
       case Element elt -> makeXMLElement.apply(elt);
-      default -> throw new  IllegalStateException("Unexpected value: " + node.getNodeValue());
+      default -> throw new IllegalStateException("Unexpected value: " + node.getNodeValue());
     };
+  }
+
+  /**
+   * Gets the text content of an XML element, which is the concatenation of the text content of all
+   * its child text nodes. If the element has no text content, returns an empty string.
+   *
+   * @param element the XML element to get the text content of.
+   * @return the text of the XML element, or an Enso error if an exception occurs.
+   */
+  public static Value getText(Element element) {
+    try {
+      var text = element.getTextContent();
+      return Value.asValue(text);
+    } catch (Exception e) {
+      return handleExceptions(e);
+    }
+  }
+
+  /**
+   * Gets the value of an attribute of an XML element. If the attribute does not exist, returns
+   * Nothing.
+   *
+   * @param elt the element to query
+   * @param attrName the name of the attribute to match
+   * @return the value of the attribute, or Nothing if the attribute does not exist
+   */
+  public static Value getAttribute(Element elt, String attrName) {
+    var attrNode = elt.getAttributeNode(attrName);
+    var attrValue = attrNode == null ? null : attrNode.getNodeValue();
+    return Value.asValue(attrValue);
+  }
+
+  /**
+   * Gets a list of attribute names
+   *
+   * @param elt the element to query
+   * @return a list of attribute names
+   */
+  public static List<String> getAttributeNames(Element elt) {
+    var namedNodeMap = elt.getAttributes();
+    var results = new ArrayList<String>(namedNodeMap.getLength());
+    for (int i = 0; i < namedNodeMap.getLength(); i++) {
+      results.add(namedNodeMap.item(i).getNodeName());
+    }
+    return results;
+  }
+
+  /**
+   * Gets a Map attribute names and values for an element.
+   *
+   * @param elt the element to query
+   * @return a Map of attribute names and values
+   */
+  public static Map<String, String> getAttributes(Element elt) {
+    var namedNodeMap = elt.getAttributes();
+    var results = new HashMap<String, String>(namedNodeMap.getLength());
+    for (int i = 0; i < namedNodeMap.getLength(); i++) {
+      results.put(namedNodeMap.item(i).getNodeName(), namedNodeMap.item(i).getNodeValue());
+    }
+    return results;
   }
 }
