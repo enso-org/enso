e65fbb84dbda46f2d739ee6c014d11e861f73813
Jaroslav Tulach <jaroslav.tulach@enso.org>
2026-03-26 14:51:53 +0100

    Renaming the variable to logger

--- std-bits/base/src/main/java/org/enso/base/ProgressHandle.java
+++ std-bits/base/src/main/java/org/enso/base/ProgressHandle.java
@@ -3,14 +3,14 @@ package org.enso.base;
 import org.slf4j.Logger;
 
 public final class ProgressHandle {
-  private final Logger log;
+  private final Logger logger;
   private final String name;
   private final long count;
   private final long then;
   private Long took;
 
-  public ProgressHandle(Logger log, String name, long count) {
-    this.log = log;
+  public ProgressHandle(Logger logger, String name, long count) {
+    this.logger = logger;
     this.name = name;
     this.count = count;
     this.then = System.currentTimeMillis();
@@ -21,7 +21,7 @@ public final class ProgressHandle {
       return;
     }
     took = System.currentTimeMillis() - then;
-    log.trace("ADVANCE {}+{}~{}ms", this, count, took);
+    logger.trace("ADVANCE {}+{}~{}ms", this, count, took);
   }
 
   @Override
