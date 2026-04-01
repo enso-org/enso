ad5045854813c8135f3b936c91db4eb451dfbfcb
Paweł Buchowski <pawel.buchowski@enso.org>
2026-04-01 11:52:32 +0200

    change undefined to display empty string for size

--- app/common/src/utilities/bytes.ts
+++ app/common/src/utilities/bytes.ts
@@ -2,7 +2,8 @@
 
 /** A function to format given bytes into human readable string. */
 export function formatBytes(bytes?: number, decimals = 2) {
-  if (bytes === undefined || bytes < 0) return '0 Bytes'
+  if (bytes === undefined) return ''
+  if (bytes <= 0) return '0 Bytes'
 
   const k = 1024
   const dm = decimals < 0 ? 0 : decimals
