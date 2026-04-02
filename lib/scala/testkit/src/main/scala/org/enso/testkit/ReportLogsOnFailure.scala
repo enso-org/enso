55554d5adffcd37b1c79da0be307dc1cc6fd921d
Jaroslav Tulach <jaroslav.tulach@enso.org>
2026-04-02 06:50:06 +0200

    Using ROOT_LOGGER and System.out

--- lib/scala/testkit/src/main/scala/org/enso/testkit/ReportLogsOnFailure.scala
+++ lib/scala/testkit/src/main/scala/org/enso/testkit/ReportLogsOnFailure.scala
@@ -1,42 +1,21 @@
 package org.enso.testkit
 
-import org.scalatest.{Args, Failed, Outcome, Status, TestSuite}
+import org.scalatest.{Failed, Outcome, TestSuite}
 import org.enso.logger.ObservedMessage
+import org.slf4j.LoggerFactory
+import org.slf4j.Logger
 
 trait ReportLogsOnFailure extends TestSuite {
 
-  abstract override protected def runTest(
-    testName: String,
-    args: Args
-  ): Status = {
-    val log    = org.slf4j.LoggerFactory.getLogger("org")
-    val ps     = System.out
-    val arr    = new java.util.ArrayList[ObservedMessage]()
-    val handle = ObservedMessage.observe(log, arr.add(_))
-    try {
-      super.runTest(testName, args)
-    } catch {
-      case e: Throwable =>
-        arr.forEach {
-          ps.println(_)
-        }
-        throw e
-    } finally {
-      arr.clear()
-      handle.close()
-    }
-  }
-
   abstract override def withFixture(test: NoArgTest): Outcome = {
-    val log    = org.slf4j.LoggerFactory.getLogger("org")
-    val ps     = System.out
+    val log    = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)
     val arr    = new java.util.ArrayList[ObservedMessage]()
     val handle = ObservedMessage.observe(log, arr.add(_))
     try {
       super.withFixture(test) match {
         case outcome @ Failed(_) =>
           arr.forEach {
-            ps.println(_)
+            System.out.println(_)
           }
           outcome
         case outcome =>
