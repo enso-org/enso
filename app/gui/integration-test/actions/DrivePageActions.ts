ef2735986016ffd7b1c1c871e2909164930365b7
Adam Obuchowicz <adam.obuchowicz@enso.org>
2026-04-09 11:40:52 +0200

    Fix tests

--- app/gui/integration-test/actions/DrivePageActions.ts
+++ app/gui/integration-test/actions/DrivePageActions.ts
@@ -1,5 +1,6 @@
 /** @file Actions for the "drive" page. */
 import { expect, type Locator, type Page } from 'integration-test/base'
+import { assert } from 'ydoc-shared/util/assert'
 import type { LocatorCallback } from './BaseActions'
 import { contextMenuActions } from './contextMenuActions'
 import EditorPageActions from './EditorPageActions'
@@ -183,7 +184,9 @@ export default class DrivePageActions<Context = object> extends PageActions<Cont
       /** Click the background to deselect all rows. */
       clickAway() {
         return self.step('Click drive table background', async (page) => {
-          await page.getByTestId('assets-table-assets-unselector').first().click()
+          const lastRowBbox = await page.getByTestId('asset-row').last().boundingBox()
+          assert(lastRowBbox != null)
+          await page.mouse.click(lastRowBbox.x + 40, lastRowBbox.y + lastRowBbox.height + 40)
         })
       },
       /**
