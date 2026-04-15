3a3754161df8aeffa8b586517b3f88d2f8675bbc
Jaroslav Tulach <jaroslav.tulach@enso.org>
2026-04-15 08:44:28 +0200

    Introducing InvokeToTextNode as a standard way to obtain Text representation for any object

---
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/text/InvokeToTextNode.java
@@ -0,0 +1,96 @@
+package org.enso.interpreter.node.expression.builtin.text;
+
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.dsl.NeverDefault;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.Node;
+import org.enso.compiler.core.ConstantsNames;
+import org.enso.interpreter.node.callable.InteropMethodCallNode;
+import org.enso.interpreter.node.callable.InvokeCallableNode;
+import org.enso.interpreter.runtime.EnsoContext;
+import org.enso.interpreter.runtime.builtin.Builtins;
+import org.enso.interpreter.runtime.callable.UnresolvedSymbol;
+import org.enso.interpreter.runtime.callable.argument.CallArgumentInfo;
+
+/**
+ * Converts provided object into Text. This is a generic node to be used to convert any object into
+ * Text. It does so by calling {@code to_text} method or by special means for various builtin
+ * objects.
+ *
+ * @author devel
+ */
+public final class InvokeToTextNode extends Node {
+  private static InvokeToTextNode uncached;
+  @CompilerDirectives.CompilationFinal private UnresolvedSymbol toText;
+  @Child private InteropMethodCallNode methodNode;
+  @Child private InvokeCallableNode invokeCallableNode;
+
+  private InvokeToTextNode() {}
+
+  @NeverDefault
+  public static InvokeToTextNode create() {
+    return new InvokeToTextNode();
+  }
+
+  @NeverDefault
+  public static InvokeToTextNode getUncached() {
+    if (uncached == null) {
+      uncached = new InvokeToTextNode();
+    }
+    return uncached;
+  }
+
+  /**
+   * Converts an object to its textual representation.
+   *
+   * @param frame frame to invoke {@code to_text} at - it can be {@code null}
+   * @param obj the object to compute textual representation for
+   * @return the textual representation of the {@code obj}
+   */
+  public Object executeToText(VirtualFrame frame, Object obj) {
+    if (frame == null || this == uncached) {
+      return executeToTextNoFrame(obj);
+    } else {
+      return executeWitFrame(frame, obj);
+    }
+  }
+
+  private Object executeToTextNoFrame(Object obj) {
+    if (methodNode == null) {
+      CompilerDirectives.transferToInterpreterAndInvalidate();
+      methodNode =
+          insert(
+              this == uncached
+                  ? InteropMethodCallNode.getUncached()
+                  : InteropMethodCallNode.build());
+    }
+    return methodNode.executeOrPanic(ensureToTextSymbol(), obj);
+  }
+
+  private UnresolvedSymbol ensureToTextSymbol() {
+    if (toText == null) {
+      CompilerDirectives.transferToInterpreterAndInvalidate();
+      var textScope = Builtins.get(this).textExtensions().getDefinitionScope();
+      toText = UnresolvedSymbol.build(ConstantsNames.TO_TEXT, textScope);
+    }
+    return toText;
+  }
+
+  private Object executeWitFrame(VirtualFrame frame, Object obj) {
+    if (invokeCallableNode == null) {
+      CompilerDirectives.transferToInterpreterAndInvalidate();
+      invokeCallableNode = insert(buildInvokeCallableNode());
+    }
+    var ctx = EnsoContext.get(this);
+    var state = ctx.currentState();
+    return invokeCallableNode.execute(ensureToTextSymbol(), frame, state, new Object[] {obj});
+  }
+
+  @NeverDefault
+  InvokeCallableNode buildInvokeCallableNode() {
+    return InvokeCallableNode.build(
+        new CallArgumentInfo[] {new CallArgumentInfo()},
+        InvokeCallableNode.DefaultsExecutionMode.EXECUTE,
+        InvokeCallableNode.ArgumentsExecutionMode.PRE_EXECUTED);
+  }
+}
