0b50c11b3775f8718c1c422bc600e546fee8cf65
Adam Obuchowicz <adam.obuchowicz@enso.org>
2026-04-17 14:16:36 +0200

    The implementation

--- app/gui/src/providers/openedProjects/suggestionDatabase/index.ts
+++ app/gui/src/providers/openedProjects/suggestionDatabase/index.ts
@@ -50,6 +50,9 @@ export class SuggestionDb extends ReactiveDb<SuggestionId, SuggestionEntry> {
     return []
   })
   readonly conflictingNames = new ReactiveIndex(this, (id, entry) => [[entry.name, id]])
+  readonly conflictingMethods = new ReactiveIndex(this, (_, entry): [string, string][] =>
+    entry.kind === SuggestionKind.Method ? [[entry.name, entry.memberOf.key()]] : [],
+  )
   private readonly suggestionsByKind = new ReactiveIndex(this, (id, entry) => [[entry.kind, id]])
   private readonly constructorFields = new ReactiveIndex(this, (id, entry) => {
     if (entry.kind !== SuggestionKind.Constructor) return []
@@ -62,6 +65,13 @@ export class SuggestionDb extends ReactiveDb<SuggestionId, SuggestionEntry> {
   /** Constructor. */
   constructor() {
     super()
+  }
+
+  /** Create database filled with entries. */
+  static createMock(entries: SuggestionEntry[]) {
+    const db = new SuggestionDb()
+    entries.forEach((entry, i) => db.set(i, entry))
+    return db
   }
 
   /** Retrieve all suggestions of the given kind stored in the suggestion database. */
