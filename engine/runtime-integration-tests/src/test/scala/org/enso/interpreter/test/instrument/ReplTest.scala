8003f070d1bfd8c3b321f1e1ca92c10644c4cbcc
Jaroslav Tulach <jaroslav.tulach@enso.org>
2026-04-17 15:59:10 +0200

    Adjusting line number after addional to_text import was inserted

--- engine/runtime-integration-tests/src/test/scala/org/enso/interpreter/test/instrument/ReplTest.scala
+++ engine/runtime-integration-tests/src/test/scala/org/enso/interpreter/test/instrument/ReplTest.scala
@@ -109,7 +109,7 @@ class ReplTest
           result.toString shouldEqual "Error in method `to_text` of [Bar 1]: Expected Text but got 42"
         }
         inside(executor.evaluate("C.Baz 1")) { case Right(result) =>
-          result.toString shouldEqual "Error in method `to_text` of [Baz 1]: Expected Text but got C.to_text[Test:19:1-28]"
+          result.toString shouldEqual "Error in method `to_text` of [Baz 1]: Expected Text but got C.to_text[Test:20:1-28]"
         }
         inside(executor.evaluate("Pattern.compile 'foo'")) {
           case Right(result) =>
